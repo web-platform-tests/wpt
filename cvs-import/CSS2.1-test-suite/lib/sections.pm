f22e54fa530abe895634b56da062503be2ff4699
ihickson <none@none>
2006-10-12 00:06:13 +0000

    show README, have sections actually exported by sections.pm
    --HG--
    extra : convert_revision : svn%3Aaa0d474d-f653-0410-985b-e409e6f70509%40193

--- cvs-import/CSS2.1-test-suite/lib/sections.pm
+++ cvs-import/CSS2.1-test-suite/lib/sections.pm
@@ -1,14 +1,11 @@
 package sections;
 use strict;
-use vars qw(@ISA @EXPORT);
+use vars qw(@ISA @EXPORT %sectionURIs %sectionTitles %sectionCodes);
 require Exporter;
 @ISA = qw(Exporter);
 @EXPORT = qw(%sectionURIs %sectionTitles %sectionCodes);
 1;
 
-my %sectionURIs;
-my %sectionTitles;
-my %sectionCodes;
 open(SECTIONS, '<', 'sections.dat') or die "$0: sections.dat: $!\n";
 while (defined($_ = <SECTIONS>)) {
     my($code, $uri, $title) = m/^([^ ]+) ([^ ]+) (.+)\n$/gos or die "$0: sections.dat: invalid format\n";

19ce7f46deee2d99ead0e5a055b0cc4a519fa186
fantasai <none@none>
2010-03-04 23:53:20 +0000

    revert make-html until lib is more stable
    --HG--
    extra : convert_revision : svn%3Aaa0d474d-f653-0410-985b-e409e6f70509%40965

--- tools/make-html.py
+++ tools/make-html.py
@@ -11,14 +11,40 @@ dstExt = '.htm'
 skipDirs = ('contributors/microsoft/submitted/support', # XXXfixme files should be .xht
             'incoming', '.svn', 'CVS', '.hg')
 
+import os.path
+from lxml import etree
+import html5lib # Warning: This uses a patched version of html5lib
+from os.path import join, getmtime
 import sys
 import re
 import os
-import os.path
-from os.path import join, getmtime, exists
 
-sys.path.insert(0, 'lib')
-from CSSTestSource import CSSTestSource
+def xhtml2html(source, dest):
+    """Convert XHTML file given by path `source` into HTML file at path `dest`."""
+
+    # read and parse
+    parser = etree.XMLParser(no_network=True,
+                             remove_comments=False,
+                             strip_cdata=False,
+                             resolve_entities=False)
+    tree = etree.parse(source, parser=parser)
+
+    # serialize
+    o = html5lib.serializer.serialize(tree, tree='lxml',
+                                      format='html',
+                                      emit_doctype='html',
+                                      resolve_entities=False,
+                                      quote_attr_values=True)
+
+    # lxml fixup for eating whitespace outside root element
+    m = re.search('<!DOCTYPE[^>]+>(\s*)<', o)
+    if m.group(1) == '': # run match to avoid perf hit from searching whole doc
+        o = re.sub('(<!DOCTYPE[^>]+>)<', '\g<1>\n<', o)
+
+    # write
+    f = open(dest, 'w')
+    f.write(o.encode('utf-8'))
+    f.close()
 
 if len(sys.argv) == 3:
     clobber = sys.argv[1] == '--clobber'
@@ -47,9 +73,15 @@ for root, dirs, files in os.walk(root):
         elif file.endswith(srcExt):
             source = join(root, file)
             dest = join(root, file[0:-1*len(srcExt)] + dstExt)
-            if not exists(dest) or getmtime(source) > getmtime(dest) or force:
+            if not os.path.exists(dest) or getmtime(source) > getmtime(dest) or force:
                 # print "Processing %s" % source
-                doc = CSSTestSource(source)
-                if doc.error:
-                    print >>sys.stderr, "Parse error on %s:\n%s\n" % (source, doc.error)
-                doc.writeHTML(dest)
+                try:
+                    xhtml2html(source, dest)
+                except etree.ParseError as e:
+                    print >>sys.stderr, "Parse error on %s:\n%s\n" % (source, e)
+                    error = """<!DOCTYPE html><title>Syntax Error</title>
+                        <p>The XHTML file %s contains a syntax error and could not be parsed.
+                        Please correct it and try again.</p>
+                        <p>The parser's error report was:</p>
+                        <pre>%s</pre>""" % (file, e)
+                    open(dest, 'w').write(error.encode('utf-8'))

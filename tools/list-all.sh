4ddc7fda03ba5325f5dc00274adb18bb8c4a6389
fantasai <none@none>
2010-05-26 06:42:20 +0000

    Add shellscript version of list-all.pl, since the server chokes on the memory reqs of the other version
    --HG--
    extra : convert_revision : svn%3Aaa0d474d-f653-0410-985b-e409e6f70509%401107

---
+++ tools/list-all.sh
@@ -0,0 +1,5 @@
+# First arg is directory path to list, second arg is path to output file
+echo '<!DOCTYPE html><html><title>CSS Tests by Filename</title><pre>' > $2
+find $1 -type f ! -ipath '*svn*' ! -ipath '*build-test*' ! -ipath '*selectors3*' ! -ipath '*/support/*' ! -ipath '*boland*' >> $2
+perl -pi -e 's#^\./((?:[^/]+/)*)([^/]+?)(\.[a-z]+)?$#$2\t<a href="$1$2$3">$1$2$3</a>#' $2
+echo '</pre>' >> $2

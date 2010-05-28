27d240ddd53effaab815f8dcbb1447a8ed8ecd9f
kkrueger@tester.krueger.org <none@none>
2010-05-27 18:52:34 -0700

    Move getVideoExtension into common video.js file Update regex and tweak assertion

---
+++ tests/submission/Microsoft/common/video.js
@@ -0,0 +1,19 @@
+//
+// Just appends a .xyz so that the UA
+// gets a video stream that is supported
+//
+function getVideoExtension(videoName)
+{
+    var extension = '.mp4';
+
+    if ( navigator.userAgent.match(/firefox/i)  != null ||
+         navigator.userAgent.match(/chrome/i) != null ||
+         navigator.userAgent.match(/opera/i)  != null )
+    {
+        extension = '.ogv';
+    }
+
+    return videoName + extension;
+
+}
+

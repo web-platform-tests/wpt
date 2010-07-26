51f0244c273efd3c2ad306001fe6401bde8d75d6
fantasai <none@none>
2010-07-26 06:26:55 +0000

    Submit fixes for random problems in generated Mozilla tests
    --HG--
    extra : convert_revision : svn%3Aaa0d474d-f653-0410-985b-e409e6f70509%401268

--- contributors/mozilla/submitted/first-letter-characters/first-letter-characters-001-generator.py
+++ contributors/mozilla/submitted/first-letter-characters/first-letter-characters-001-generator.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python
 
+# This script authored by L. David Baron (Mozilla Corporation)
+
 import re
+from urllib import urlopen
 
 isSurrogateOrPUA = re.compile('^<.*(Private Use|Surrogate).*>$').search
+
+def isValidXMLChar(charcode):
+  return (charcode == 0x9) or \
+         (charcode  == 0xA) or \
+         (charcode == 0xD) or \
+         (charcode >= 0x20 and charcode <= 0xD7FF) or \
+         (charcode >= 0xE000 and charcode <= 0xFFFD) or \
+         (charcode >= 0x10000 and charcode <= 0x10FFFF)
 
 isPunctuation = re.compile('^P(s|e|i|f|o)$').search
 
@@ -25,7 +36,7 @@ def print_info(charcode, ispunct):
           hex(charcode)[2:] + ";C<span class=\"spacer\"></span></div></div"
 
 # http://www.unicode.org/Public/UNIDATA/UnicodeData.txt
-unicodedb = open("/home/dbaron/specs/unicode/UNIDATA-5.1.0/UnicodeData.txt")
+unicodedb = urlopen("http://www.unicode.org/Public/UNIDATA/UnicodeData.txt")
 rangefirst = None
 print "<div"
 for line in unicodedb:
@@ -33,7 +44,7 @@ for line in unicodedb:
     charcode = int(fields[0], 16)
     charname = fields[1]
     ispunct = isPunctuation(fields[2])
-    if isSurrogateOrPUA(charname):
+    if isSurrogateOrPUA(charname) or not isValidXMLChar(charcode):
         pass
     elif charname.endswith(", First>"):
         if rangefirst != None:

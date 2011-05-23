c4cae1449384b0ea345cdb2e94c1b2ac6ed3e556
Peter Linss <peter.linss@hp.com>
2011-05-23 15:25:10 -0700

    ignore .hg paths when listing files

--- tools/list-all.pl
+++ tools/list-all.pl
@@ -9,7 +9,7 @@ use Template;
 
 $top = $ARGV[-1] || '.';
 
-@files = `find $top -type f ! -ipath '*svn*' ! -ipath '*build-test*' ! -ipath '*selectors3*'`;
+@files = `find $top -type f ! -ipath '*.hg*' ! -ipath '*build-test*' ! -ipath '*selectors3*'`;
 foreach (@files) {
   chomp;
   m!^(?:\./)?((?:[^/]+/)*)([^/]+?)(\.[a-z]+)?$!;

1ad991a78d19f8574bea93c6f14da3818aeb8193
Aryeh Gregor <ayg+w3chg@aryeh.name>
2011-06-01 08:06:41 -0600

    Revert 18d2f1031deb, run many tests per attribute
    This means that in IE and Opera, you don't just have every reflection test fail
    because of one bug in all of them (like, say, mishandling nulls in strings).
    Requested by jgraham and others.  This makes the testharness.js-based version
    work more like reflection-original.js, although it's still less useful when
    used standalone because it doesn't aggregate similar errors.  (But it's more
    useful if you want to do machine processing on the test results, or are just
    used to testharness.js.)

--- tests/submission/AryehGregor/reflection/new-harness.js
+++ tests/submission/AryehGregor/reflection/new-harness.js
@@ -2,15 +2,17 @@
 // original-harness.js.  Polymorphism, kind of.
 ReflectionHarness.catchUnexpectedExceptions = false;
 
-ReflectionHarness.testWrapper = function(fn) {
-	test(fn, this.getTypeDescription());
-}
-
 ReflectionHarness.test = function(expected, actual, description) {
-	assert_equals(expected, actual, description);
+	test(function() {
+		assert_equals(expected, actual);
+	}, this.getTypeDescription() + ": " + description);
+	// This is the test suite that will rate conformance, so we don't want to
+	// bail out early if a test fails -- we want all tests to always run.
 	return true;
 }
 
 ReflectionHarness.testException = function(exceptionName, fn, description) {
-	assert_throws(exceptionName, fn, description);
+	test(function() {
+		assert_throws(exceptionName, fn);
+	}, this.getTypeDescription() + ": " + description);
 }

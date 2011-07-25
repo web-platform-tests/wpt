aa0bd765aab553b20e1d824e3ff9a7a0fbd6c528
James Graham <jgraham@opera.com>
2011-07-25 18:49:46 +0200

    Add comment to testharnessreport.js indicating why it exists

---
+++ tests/resources/testharnessreport.js
@@ -0,0 +1,13 @@
+/*
+ * THIS FILE INTENTIONALLY LEFT BLANK
+ *
+ * More specifically, this file is intended for vendors to implement
+ * code needed to integrate testharness.js tests with their own test systems.
+ *
+ * Typically such integration will attach callbacks when each test is
+ * has run, using add_result_callback(callback(test)), or when the whole test file has
+ * completed, using add_completion_callback(callback(tests, harness_status)).
+ *
+ * For more documentation about the callback functions and the
+ * parameters they are called with see testharness.js
+ */

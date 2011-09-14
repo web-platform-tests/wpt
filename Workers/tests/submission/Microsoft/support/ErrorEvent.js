86311248a693b4548a3247dbac8e6b24bc576ef0
krisk@microsoft.com <unknown>
2011-09-13 18:11:21 -0700

    Remove the files and will place them back so that the folder path is consistent submission vs submissions.

--- Workers/tests/submission/Microsoft/support/ErrorEvent.js
+++
@@ -1,9 +0,0 @@
-onmessage = function(evt)
-{
-    throw(evt.data);
-}
-
-onerror = function(err)
-{
-    postMessage( {event: err.toString(), type: err.type, message: err.message, filename: err.filename, lineno: err.lineno} );
-}

3f32ebf222148561d7e1f234dda467585d3d94fc
Aryeh Gregor <AryehGregor+gitcommit@gmail.com>
2011-09-23 13:45:25 -0600

    Streamline test generation more

--- conformancetest/submit.php
+++ conformancetest/submit.php
@@ -1,21 +1,26 @@
 <?php
 # I hate PHP, but it's what I know, so this is fastest . . .
+echo '<!doctype html>';
+
 if (empty($_POST)) {
-	die('<!doctype html><title>Error</title><p>Not POSTed');
+	die('<title>Error</title><p>Not POSTed');
 }
 
 if (sha1($_POST['password']) !== 'd962ad564032fa99ca43e8f0f6875c8efb9e2905') {
 	# I love how hash functions let me leave the source code open with no
 	# database or secret files, without disclosing the password.
-	die('<!doctype html><title>Error</title><p>Incorrect password');
+	die('<title>Error</title><p>Incorrect password');
 }
 
 if ($_POST['ua'] !== 'webkit' && $_POST['ua'] !== 'gecko') {
-	die('<!doctype html><title>Error</title><p>No UA provided: must be either "webkit" or "gecko"');
+	die('<title>Error</title><p>No UA provided: must be either "webkit" or "gecko"');
 }
 
-if (file_put_contents($_POST['ua'] . '-data', $_POST['data']) === false) {
-	die('<!doctype html><title>Error</title><p>Write failed!');
+if (file_put_contents($_POST['ua'] . '-data', $_POST['output']) === false) {
+	die('<title>Error</title><p>Write failed!');
 }
 
-echo '<!doctype html><title>Success</title><p>Successfully wrote ' . $_POST['ua'] . '-data';
+echo '<title>Success</title><p>Successfully wrote ' . $_POST['ua'] . '-data.';
+echo '<p>Time taken for test generation: ' . intval($_POST['elapsed']/60000)
+	. ':' . sprintf('%06.3F', ($_POST['elapsed'] % 60000)/1000) . ' min.';
+echo '<ul>' . $_POST['errors'] . '</ul>';

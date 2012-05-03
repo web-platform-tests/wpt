4456f9e9772b4dbfa499fb49ed1d7120c8e4fa6d
krisk@microsoft.com <unknown>
2012-05-03 13:44:11 -0700

    CORS test have moved to webappsec

--- CORS/tests/submissions/Microsoft/SetCORSUrls.js
+++
@@ -1,3 +0,0 @@
-var CORS_BASE = "http://www1.w3c-test.org/webapps/CORS/tests/submissions/Microsoft/";
-var CORS_SERVER_URL = CORS_BASE + "CORSServer.php";
-var CORS_SET_COOKIE_URL = CORS_BASE + "SetCookie.php";

3975f00f15aaa4e50cc7c14e61f22199cc85af11
James Graham <jgraham@opera.com>
2012-10-09 14:57:01 +0200

    Add Opera's script scheduling testsuite * * * Remove index file that's outdated * * * Cleanup

---
+++ tests/submission/Opera/script_scheduling/scripts/include-10.js
@@ -0,0 +1,1 @@
+document.write("<script src='scripts/delay.php?time=2&url=include-9.js' defer></script>");
---
+++ tests/submission/Opera/script_scheduling/scripts/include-8.js
@@ -0,0 +1,4 @@
+log("external script #8");
+var s = document.createElement("script")
+s.src='scripts/delay.php?time=2&url=include-9.js'
+document.body.appendChild(s);

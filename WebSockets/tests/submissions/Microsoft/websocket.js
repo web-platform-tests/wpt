0760f0255a9a3697aaab130e901d103894c82b85
krisk@microsoft.com <unknown>
2012-10-11 10:55:00 -0700

    Add arraybufferview tests to match the spec changes

--- WebSockets/tests/submissions/Microsoft/websocket.js
+++ WebSockets/tests/submissions/Microsoft/websocket.js
@@ -6,6 +6,7 @@ var __NEW__PORT = 8081;
 // NOT ENABLED, NOT NEEDED AT THIS TIME
 var __NEW__SECURE__PORT = 8444;
 var __PATH = "echo";
+var __CONTROLPATH = "control";
 var __PROTOCOL = "echo";
 var __PROTOCOLS = ["echo", "chat"];
 var __REPEATED__PROTOCOLS = ["echo", "echo"];
@@ -14,13 +15,21 @@ var __IS__WEBSOCKET;
 var __PASS = "Pass";
 var __FAIL = "Fail";
 var wsocket;
+var csocket;
 var data;
 var timeOut;
 var defaultTimeout = 12000;
 
+// variables for testing Close Browser/Navigate Away scenarios
+var isAssociated = false;
+var guid;
+var dataReceived;
+var closeCode;
+var urlToOpen;
+
 function IsWebSocket() {
     if (!window.WebSocket) {
-        BrowserDoesNotSupportWebSocket();
+        assert_true(false, "Browser does not support WebSocket");
     }
 
     timeOut = setTimeout(OnTimeOutFail, defaultTimeout);
@@ -89,16 +98,37 @@ function CreateWebSocket(isSecure, isProtocol, isProtocols) {
     return wsocket;
 }
 
-function BrowserDoesNotSupportWebSocket() {
-    document.getElementById("websocketsupport").firstChild.data = "Browser does not Support WebSocket";
+function CreateControlWebSocket(isSecure) {
+    IsWebSocket();
+    if (isSecure) {
+        __URL = "wss://" + __SERVER__NAME + ":" + __SECURE__PORT + "/" + __CONTROLPATH;
+    }
+    else {
+        __URL = "ws://" + __SERVER__NAME + ":" + __PORT + "/" + __CONTROLPATH;
+    }
+
+    csocket = new WebSocket(__URL);
+    return csocket;
 }
 
 function OnTimeOutFail() {
-    wsocket.close();
-    wsocket.onopen = null;
-    wsocket.onclose = null;
-    wsocket.onerror = null;
-    wsocket.onmessage = null;
-    wsocket = null;
+    if (wsocket) {
+        wsocket.close();
+        wsocket.onopen = null;
+        wsocket.onclose = null;
+        wsocket.onerror = null;
+        wsocket.onmessage = null;
+        wsocket = null;
+    }
+
+    if (csocket) {
+        csocket.close();
+        csocket.onopen = null;
+        csocket.onclose = null;
+        csocket.onerror = null;
+        csocket.onmessage = null;
+        csocket = null;
+    }
+
     assert_true(false, "TimeOut");   
 }

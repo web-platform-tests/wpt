48daf7eb3498f55155e9a9b2f337f54e834ff958
Robin Berjon <robin@berjon.com>
2012-12-20 16:42:25 +0100

    remove submission that has in fact already been integrated

--- old-tests/submission/MathiasBynens/named-character-references/update.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-{
-	echo -n "window.data = "
-	curl -# http://www.whatwg.org/specs/web-apps/current-work/entities.json | sed 's/  /	/g' | sed "s/\"/'/g" | sed "s/\\u00/\\x/g" | perl -pe 'chomp if eof'
-	echo ';'
-} > data.js
-
-cat data.js

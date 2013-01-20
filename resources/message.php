06681366ec048e33a6fe2d4336bbdd731a627dad
Yaffle <vic99999@yandex.ru>
2013-01-20 16:16:19 +0600

    Update resources/message.php
    avoid of warnings

--- resources/message.php
+++ resources/message.php
@@ -1,7 +1,7 @@
 <?php
-  $mime = $_GET["mime"] ? $_GET["mime"] : "text/event-stream";
-  $message = $_GET["message"] ? $_GET["message"] : "data: data";
-  $newline = $_GET["newline"] ? ($_GET["newline"] == "none" ? "" : $_GET["newline"]) : "\n\n";
+  $mime = !empty($_GET["mime"]) ? $_GET["mime"] : "text/event-stream";
+  $message = !empty($_GET["message"]) ? $_GET["message"] : "data: data";
+  $newline = !empty($_GET["newline"]) ? ($_GET["newline"] == "none" ? "" : $_GET["newline"]) : "\n\n";
   header("Content-Type: " . $mime);
   echo $message . $newline;
 ?>

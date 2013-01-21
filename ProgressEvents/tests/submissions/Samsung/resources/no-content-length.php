b3ea57de3884ca36874b71f022e9c49be57ac514
Jungkee Song <jungkee.song@samsung.com>
2013-01-22 05:48:51 +0900

    Address Yves's comment in PE test case: no-content-length.php

--- ProgressEvents/tests/submissions/Samsung/resources/no-content-length.php
+++ ProgressEvents/tests/submissions/Samsung/resources/no-content-length.php
@@ -1,22 +1,18 @@
 <?php
-    header('Content-Encoding', 'chunked');
     header('Transfer-Encoding', 'chunked');
     header('Content-Type', 'text/html');
     header('Connection', 'keep-alive');
 
-    ob_flush();
-    flush();
-
     for ($i = 0; $i < 100000; $i++) {
-        $string = "W3C Progress Events Test";
-        echo strlen($string)."\r\n"; // this is the length
-        echo $string."\r\n"; // this is the date
-        echo "\r\n"; // newline between chunks
-        ob_flush(); // rinse and repeat
+        $string = "W3C";
+        echo strlen($string)."\r\n";
+        echo $string."\r\n";
         flush();
-    } 
+    }
+
+    $string = "";
+    echo strlen($string)."\r\n";
     echo  "\r\n"; // send final empty line
-    ob_flush();
     flush();
 ?>
 

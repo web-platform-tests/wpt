9ab8e09e1b3a84bdc905725f3e8cc9daf86fed77
Robin Berjon <robin@berjon.com>
2013-02-07 17:33:39 +0100

    start coverage display; docs; merge test results into analysis

--- coverage/analyse-specs.js
+++ coverage/analyse-specs.js
@@ -8,6 +8,7 @@ var fs = require("fs")
     ,   canvas2d:   "http://www.w3.org/html/wg/drafts/2dcontext/html5_canvas/Overview.html"
     // ,   microdata:  "http://www.w3.org/html/wg/drafts/microdata/master/Overview.html"
     }
+,   perSec = JSON.parse(fs.readFileSync(pth.join(__dirname, "tests-per-section.json"), "utf8"))
 ,   data = {}
 ,   ranges = {}
 ;
@@ -37,6 +38,10 @@ for (var spec in specs) {
     ranges[spec] = {};
     analyseSpec(spec, function (err, spec) {
         if (err) return console.log("ERROR: " + err);
+        for (var i = 0, n = data[spec].length; i < n; i++) {
+            var sec = data[spec][i];
+            sec.tests = perSec[spec][sec.id] || 0;
+        }
         fs.writeFileSync(pth.join(__dirname, "spec-data-" + spec + ".json"), JSON.stringify(data[spec], null, 4), "utf8");
     });
 }

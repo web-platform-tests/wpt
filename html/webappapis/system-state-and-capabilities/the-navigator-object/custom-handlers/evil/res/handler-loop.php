66539bd34ae690b8e32ae7d8311dfde3758102ee
Simon Pieters <zcorpan@gmail.com>
2013-03-11 15:42:53 +0100

    add the rest of this testsuite

---
+++ html/webappapis/system-state-and-capabilities/the-navigator-object/custom-handlers/evil/res/handler-loop.php
@@ -0,0 +1,5 @@
+<?php
+
+  header("Location: sms:bar");
+
+?>
---
+++ html/webappapis/system-state-and-capabilities/the-navigator-object/custom-handlers/interactive/res/params.php
@@ -0,0 +1,6 @@
+<?php
+
+	echo "<p>".htmlspecialchars($_GET['one'])."</p>\n";
+	echo "<p>".htmlspecialchars($_GET['two'])."</p>\n";
+
+?>

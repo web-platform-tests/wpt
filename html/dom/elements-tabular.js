55ea2f732a1dd546cd6899b3eb41ce187870f456
Ms2ger <ms2ger@gmail.com>
2013-04-12 15:22:21 +0200

    Review and update elements-tabular.js.

--- html/dom/elements-tabular.js
+++ html/dom/elements-tabular.js
@@ -1,13 +1,15 @@
+// Up-to-date as of 2013-04-12.
 var tabularElements = {
 	table: {
 		// Conforming
-		summary: "string",
+		sortable: "boolean",
 
 		// Obsolete
 		align: "string",
 		border: "string",
 		frame: "string",
 		rules: "string",
+		summary: "string",
 		width: "string",
 		bgColor: {type: "string", treatNullAsEmptyString: true},
 		cellPadding: {type: "string", treatNullAsEmptyString: true},
@@ -19,6 +21,13 @@ var tabularElements = {
 	},
 	colgroup: {
 		span: "limited unsigned long",
+
+		// Obsolete
+		align: "string",
+		ch: {type: "string", domAttrName: "char"},
+		chOff: {type: "string", domAttrName: "charoff"},
+		vAlign: "string",
+		width: "string",
 	},
 	col: {
 		// Conforming
@@ -26,10 +35,10 @@ var tabularElements = {
 
 		// Obsolete
 		align: "string",
-		width: "string",
 		ch: {type: "string", domAttrName: "char"},
 		chOff: {type: "string", domAttrName: "charoff"},
 		vAlign: "string",
+		width: "string",
 	},
 	tbody: {
 		// Obsolete
@@ -61,13 +70,12 @@ var tabularElements = {
 		bgColor: {type: "string", treatNullAsEmptyString: true},
 	},
 	td: {
-		// Conforming
-		colSpan: "unsigned long",
-		rowSpan: "unsigned long",
+		// HTMLTableCellElement (Conforming)
+		colSpan: {type: "unsigned long", defaultVal: 1},
+		rowSpan: {type: "unsigned long", defaultVal: 1},
 		headers: "settable tokenlist",
 
-		// Obsolete
-		abbr: "string",
+		// HTMLTableCellElement (Obsolete)
 		align: "string",
 		axis: "string",
 		height: "string",
@@ -77,29 +85,35 @@ var tabularElements = {
 		noWrap: "boolean",
 		vAlign: "string",
 		bgColor: {type: "string", treatNullAsEmptyString: true},
+
+		// HTMLTableDataCellElement (Obsolete)
+		abbr: "string",
 	},
 	th: {
-		// Conforming
+		// HTMLTableCellElement (Conforming)
+		colSpan: {type: "unsigned long", defaultVal: 1},
+		rowSpan: {type: "unsigned long", defaultVal: 1},
+		headers: "settable tokenlist",
+
+		// HTMLTableCellElement (Obsolete)
+		align: "string",
+		axis: "string",
+		height: "string",
+		width: "string",
+		ch: {type: "string", domAttrName: "char"},
+		chOff: {type: "string", domAttrName: "charoff"},
+		noWrap: "boolean",
+		vAlign: "string",
+		bgColor: {type: "string", treatNullAsEmptyString: true},
+
+		// HTMLTableHeaderCellElement (Conforming)
 		// TODO: double-check that the way we're treating missing value
 		// defaults is consistent here.  scope has an auto state with no
 		// associated keyword, which is the missing value default -- is this
 		// the right syntax for that?
 		scope: {type: "enum", keywords: ["row", "col", "rowgroup", "colgroup"]},
-		colSpan: "unsigned long",
-		rowSpan: "unsigned long",
-		headers: "settable tokenlist",
-
-		// Obsolete
 		abbr: "string",
-		align: "string",
-		axis: "string",
-		bgColor: "string",
-		ch: {type: "string", domAttrName: "char"},
-		chOff: {type: "string", domAttrName: "charoff"},
-		height: "string",
-		noWrap: "boolean",
-		vAlign: "string",
-		width: "string",
+		sorted: "string",
 	},
 };
 

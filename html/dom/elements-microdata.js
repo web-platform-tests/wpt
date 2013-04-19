b7169f612e8a82a5bc9ecbb939c8e9fa651598e0
Ms2ger <ms2ger@gmail.com>
2013-04-19 14:28:17 +0200

    Update elements-microdata.js.

--- html/dom/elements-microdata.js
+++ html/dom/elements-microdata.js
@@ -1,25 +1,35 @@
+// Up-to-date as of 2013-04-19.
 // Things defined in the W3C's microdata draft, not the main HTML5 draft.
 // Note: must be included last so that it hits all elements.
 
 for (var element in elements) {
 	elements[element].itemScope = "boolean";
-	elements[element].itemType = "string";
-	elements[element].itemId = "string";
+	elements[element].itemType = "settable tokenlist";
+	elements[element].itemId = "url";
+	elements[element].itemRef = "settable tokenlist";
+	elements[element].itemProp = "settable tokenlist";
 }
 extraTests.push(function() {
 	// itemValue only reflects in certain circumstances.  The syntax for our big
 	// array thing above doesn't currently support one IDL attribute that reflects
 	// different content attributes, so just do this explicitly until that's fixed.
-	ReflectionTests.reflects("string", "itemValue", "meta", "content");
-	ReflectionTests.reflects("url", "itemValue", "audio", "src");
-	ReflectionTests.reflects("url", "itemValue", "embed", "src");
-	ReflectionTests.reflects("url", "itemValue", "iframe", "src");
-	ReflectionTests.reflects("url", "itemValue", "img", "src");
-	ReflectionTests.reflects("url", "itemValue", "source", "src");
-	ReflectionTests.reflects("url", "itemValue", "video", "src");
-	ReflectionTests.reflects("url", "itemValue", "a", "href");
-	ReflectionTests.reflects("url", "itemValue", "area", "href");
-	ReflectionTests.reflects("url", "itemValue", "link", "href");
-	ReflectionTests.reflects("url", "itemValue", "object", "data");
-	ReflectionTests.reflects("url", "itemValue", "time", "datetime");
+	var reflectItemValue = function(data, localName, attribute) {
+		var element = document.createElement(localName);
+		element.setAttribute("itemprop", "");
+		ReflectionTests.reflects(data, "itemValue", element, attribute);
+	}
+	reflectItemValue("string", "meta", "content");
+	reflectItemValue("url", "audio", "src");
+	reflectItemValue("url", "embed", "src");
+	reflectItemValue("url", "iframe", "src");
+	reflectItemValue("url", "img", "src");
+	reflectItemValue("url", "source", "src");
+	reflectItemValue("url", "track", "src");
+	reflectItemValue("url", "video", "src");
+	reflectItemValue("url", "a", "href");
+	reflectItemValue("url", "area", "href");
+	reflectItemValue("url", "link", "href");
+	reflectItemValue("url", "object", "data");
+	reflectItemValue("string", "data", "value");
+	// time is more complex
 });

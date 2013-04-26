0ef4039120bd575c172330b2e547171b390d5e23
Ehsan Akhgari <ehsan.akhgari@gmail.com>
2013-04-26 15:33:59 +0100

    Added Ehsan's GainNode test

--- webaudio/js/helpers.js
+++ webaudio/js/helpers.js
@@ -31,3 +31,34 @@ function test_implements_audio_param_interface(obj) {
         test(fn, "'" + obj.name + "'" + " implements: " + meth + "()");
     });
 };
+
+
+function fuzzyCompare(a, b) {
+  return Math.abs(a - b) < 5e-5;
+}
+
+function compareBuffers(buf1, buf2,
+                        /*optional*/ offset,
+                        /*optional*/ length,
+                        /*optional*/ sourceOffset,
+                        /*optional*/ destOffset) {
+  if (length == undefined) {
+    length = buf1.length - (offset || 0);
+  }
+  sourceOffset = sourceOffset || 0;
+  destOffset = destOffset || 0;
+  var difference = 0;
+  var maxDifference = 0;
+  var firstBadIndex = -1;
+  for (var i = offset || 0; i < Math.min(buf1.length, (offset || 0) + length); ++i) {
+    if (!fuzzyCompare(buf1[i + sourceOffset], buf2[i + destOffset])) {
+      difference++;
+      maxDifference = Math.max(maxDifference, Math.abs(buf1[i + sourceOffset] - buf2[i + destOffset]));
+      if (firstBadIndex == -1) {
+        firstBadIndex = i;
+      }
+    }
+  };
+
+  return difference == 0;
+}

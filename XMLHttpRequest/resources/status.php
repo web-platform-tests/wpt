e5cbb6e3ed8f38211041184c9606bf60dda3fbc2
Hallvord R. M. Steen <gitstuff@hallvord.com>
2013-05-02 13:50:40 +0200

    avoid PHP warnings when PHP is in picky mode

--- XMLHttpRequest/resources/status.php
+++ XMLHttpRequest/resources/status.php
@@ -10,8 +10,8 @@
   }
 
   // This should reasonably work for most response codes.
-  $code = ctype_digit($_GET["code"]) ? $_GET["code"] : "200";
-  $text = $_GET["text"] ? $_GET["text"] : "OMG";
+  $code = isset($_GET['code']) && ctype_digit($_GET["code"]) ? $_GET["code"] : "200";
+  $text = isset($_GET['code']) && $_GET["text"] ? $_GET["text"] : "OMG";
   $content = $_GET["content"] ? $_GET["content"] : "";
   $type = $_GET["type"] ? $_GET["type"] : "";
   header("HTTP/1.1 " . $code . " " . $text, true, $code);

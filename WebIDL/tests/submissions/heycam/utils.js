7322280ae2036dad62244fef87f434c5b45df48f
Cameron McCormack <cam@mcc.id.au>
2013-05-05 10:33:16 +1000

    Add a utility function for asserting a property descriptor's attributes.

--- WebIDL/tests/submissions/heycam/utils.js
+++ WebIDL/tests/submissions/heycam/utils.js
@@ -27,3 +27,27 @@ function operation(interfaceName, operationName, w) {
 function stringifier(interfaceName, w) {
   return operation(interfaceName, "toString", w);
 }
+
+function assert_property(object, property, descriptor, description) {
+  var desc = Object.getOwnPropertyDescriptor(object, property);
+  if ("writable" in descriptor) {
+    test(function() {
+      assert_equals(desc.writable, descriptor.writable, description + " [[Writable]]");
+    }, description + " is " + (descriptor.writable ? "" : "not ") + " writable");
+  }
+  if ("enumerable" in descriptor) {
+    test(function() {
+      assert_equals(desc.enumerable, descriptor.enumerable, description + " [[Enumerable]]");
+    }, description + " is " + (descriptor.enumerable ? "" : "not ") + " enumerable");
+  }
+  if ("configurable" in descriptor) {
+    test(function() {
+      assert_equals(desc.configurable, descriptor.configurable, description + " [[Configurable]]");
+    }, description + " is " + (descriptor.configurable ? "" : "not ") + " configurable");
+  }
+  if ("value" in descriptor) {
+    test(function() {
+      assert_equals(desc.value, descriptor.value, " [[Value]]");
+    }, description + " has the expected value");
+  }
+}

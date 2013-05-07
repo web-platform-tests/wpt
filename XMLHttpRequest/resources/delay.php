07e1b71d975d29134482de1a9c75c5c536e9c6e9
Hallvord Reiar M. Steen <hallvord@opera.com>
2013-05-07 15:18:40 +0200

    New and updated tests

--- XMLHttpRequest/resources/delay.php
+++ XMLHttpRequest/resources/delay.php
@@ -1,7 +1,7 @@
 <?php
   // It will always output a text file for now with the literal string
   // "TEST_DELAY"
-  $delay = ctype_digit($_GET["ms"]) ? $_GET["ms"] : 500;
+  $delay = isset($_GET["ms"]) && ctype_digit($_GET["ms"]) ? $_GET["ms"] : 500;
   usleep($delay);
   header("Content-type: text/plain");
   echo "TEST_DELAY";

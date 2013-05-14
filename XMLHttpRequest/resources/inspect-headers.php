7e080a95969d87062e6f83730f56a288fd9a984a
Hallvord R. M. Steen <gitstuff@hallvord.com>
2013-05-14 23:38:35 +0200

    improved accept-language test, maybe..

--- XMLHttpRequest/resources/inspect-headers.php
+++ XMLHttpRequest/resources/inspect-headers.php
@@ -1,10 +1,13 @@
 <?php
   $filter_value = isset($_GET["filter_value"]) && $_GET["filter_value"] ? $_GET["filter_value"] : "";
+  $filter_name = isset($_GET["filter_name"]) && $_GET["filter_name"] ? strtolower($_GET["filter_name"]) : "";
   $result = "";
   foreach (getallheaders() as $name => $value) {
     if($filter_value != "") {
       if($value == $filter_value)
         $result .= strtolower($name) . ",";
+    }else if (strtolower($name) == $filter_name) {
+       $result .= strtolower($name) . ": ".$value."\n";
     }
   }
   header("content-type:text/plain");

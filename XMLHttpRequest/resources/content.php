74c4c6ee916f309ca8378fb221fb9d8adf7668e1
Hallvord Reiar M. Steen <hallvord@opera.com>
2013-05-21 23:00:06 +0200

    updating helper file to make sure it will work for all new tests

--- XMLHttpRequest/resources/content.php
+++ XMLHttpRequest/resources/content.php
@@ -4,6 +4,13 @@
  header("X-Request-Query: " . (isset($_SERVER["QUERY_STRING"]) ? $_SERVER["QUERY_STRING"] : "NO"));
  header("X-Request-Content-Length: " . (isset($_SERVER["CONTENT_LENGTH"]) ? $_SERVER["CONTENT_LENGTH"] : "NO"));
  header("X-Request-Content-Type: " . (isset($_SERVER["CONTENT_TYPE"]) ? $_SERVER["CONTENT_TYPE"] : "NO"));
- echo file_get_contents("php://input");
- exit;
+if (isset($_GET["content"]))
+{
+    echo $_GET["content"];
+}
+else
+{
+    echo file_get_contents("php://input");
+}
+exit;
 ?>

cf3cb5ca3a1be0ed8bdaabdb9eb77489729d8208
Hallvord Reiar M. Steen <hallvord@opera.com>
2013-05-27 12:02:01 +0200

    give explicit feedback when the client did a pre-request without authorization header

--- XMLHttpRequest/resources/authentication.php
+++ XMLHttpRequest/resources/authentication.php
@@ -7,11 +7,11 @@
 
   $session_user = isset($_SERVER["PHP_AUTH_USER"]) ? $_SERVER["PHP_AUTH_USER"] : null;
   $session_pass = isset($_SERVER["PHP_AUTH_PW"]) ? $_SERVER["PHP_AUTH_PW"] : null;
-  $xhr_user = isset($_SERVER["HTTP_X_USER"]) ? $_SERVER["HTTP_X_USER"] : null;
-  $xhr_pass = isset($_SERVER["HTTP_X_PASS"]) ? $_SERVER["HTTP_X_PASS"] : null;
-  $file = "authentication-temp-" . $xhr_user;
+  $expected_user_name = isset($_SERVER["HTTP_X_USER"]) ? $_SERVER["HTTP_X_USER"] : null;
+  $expected_user_password = isset($_SERVER["HTTP_X_PASS"]) ? $_SERVER["HTTP_X_PASS"] : null;
+  $file = "authentication-temp-" . $expected_user_name;
   if((empty($session_user) && empty($session_pass))/* ||
-     ($session_user != $xhr_user || $session_pass != $xhr_pass)*/
+     ($session_user != $expected_user_name || $session_pass != $expected_user_password)*/
     ) {
     if(file_exists($file)) {
       unlink($file);
@@ -22,16 +22,17 @@
       fclose($handler);
       header('HTTP/1.1 401 Unauthorized');
       header('WWW-Authenticate: Basic realm="test"');
-      header('XHR-USER: ' . $xhr_user);
+      header('XHR-USER: ' . $expected_user_name);
       header('SES-USER: ' . $session_user);
       echo 'FAIL (should be transparent)';
     }
   } else {
-    header('XHR-USER: ' . $xhr_user);
+    header('XHR-USER: ' . $expected_user_name);
     header('SES-USER: ' . $session_user);
     if(!file_exists($file)) {
       header("X-challenge: DID-NOT");
     }else{
+      header("X-challenge: DID");
       unlink($file);
     }
     echo $session_user . "\n" . $session_pass;

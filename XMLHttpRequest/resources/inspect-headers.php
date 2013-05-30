1c31097c36d58719a1295782eb436c829f064717
Hallvord Reiar M. Steen <hallvord@opera.com>
2013-05-30 16:37:10 +0200

    improved accept-language test, maybe..
    Conflicts:
    
    	XMLHttpRequest/resources/inspect-headers.php

--- XMLHttpRequest/resources/inspect-headers.php
+++ XMLHttpRequest/resources/inspect-headers.php
@@ -1,9 +1,6 @@
 <?php
   $filter_value = isset($_GET["filter_value"]) && $_GET["filter_value"] ? $_GET["filter_value"] : "";
-<<<<<<< HEAD
   $filter_name = isset($_GET["filter_name"]) && $_GET["filter_name"] ? strtolower($_GET["filter_name"]) : "";
-=======
->>>>>>> 81204cd... new test for open() algo point 14 bullet point 4
   $result = "";
   foreach (getallheaders() as $name => $value) {
     if($filter_value != "") {

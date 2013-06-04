fd33489151586b55afa0b2d03f0cb1d240de9b9f
Hallvord Reiar M. Steen <hallvord@opera.com>
2013-06-04 15:10:56 +0200

    new LINKs and a few comments

--- XMLHttpRequest/xmlhttprequest-timeout-synconworker.js
+++ XMLHttpRequest/xmlhttprequest-timeout-synconworker.js
@@ -1,5 +1,10 @@
-if (this.document === undefined)
+if (this.document === undefined){
   importScripts("xmlhttprequest-timeout.js");
+}else{
+	throw "This test expects to be run as a Worker";
+}
+
+/* NOT TESTED: setting timeout before calling open( ... , false) in a worker context. The test code always calls open() first. */
 
 runTestRequests([ new RequestTracker(false, "no time out scheduled, load fires normally", 0),
 		  new RequestTracker(false, "load fires normally", TIME_NORMAL_LOAD),

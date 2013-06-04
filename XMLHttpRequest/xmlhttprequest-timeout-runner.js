b4afb8e03076b46375c6bc486ab3962b1415b350
Hallvord Reiar M. Steen <hallvord@opera.com>
2013-06-04 11:44:41 +0200

    improving result listing UI by passing full test descriptions to test() method

--- XMLHttpRequest/xmlhttprequest-timeout-runner.js
+++ XMLHttpRequest/xmlhttprequest-timeout-runner.js
@@ -4,11 +4,11 @@ function testResultCallbackHandler(event) {
         return;
     }
     if (event.data.type == "is") {
-        test(function() { assert_equals(event.data.got, event.data.expected, event.data.msg); });
+        test(function() { assert_equals(event.data.got, event.data.expected); }, "Timeout test: " + event.data.msg);
         return;
     }
     if (event.data.type == "ok") {
-        test(function() { assert_true(event.data.bool, event.data.msg); });
+        test(function() { assert_true(event.data.bool); }, "Timeout test: " + event.data.msg);
         return;
     }
 }

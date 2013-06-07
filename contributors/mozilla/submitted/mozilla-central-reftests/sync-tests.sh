a70b2d74999890206fc3168dea2a72bd3f47ca0d
L. David Baron <dbaron@dbaron.org>
2013-06-07 12:50:30 +0900

    Sync Mozilla tests as of https://hg.mozilla.org/integration/mozilla-inbound/rev/dd81ecba7afd .

--- contributors/mozilla/submitted/mozilla-central-reftests/sync-tests.sh
+++ contributors/mozilla/submitted/mozilla-central-reftests/sync-tests.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
 rsync -avz --delete --filter="- README" --filter="H test-template-001.xht" --filter="P sync-tests.sh" ~/builds/clean-mozilla-central/layout/reftests/w3c-css/submitted/ ./

4b6db80845979a8458ae63c3ab545801317dc525
L. David Baron <dbaron@dbaron.org>
2013-06-07 09:14:11 +0900

    Sync Mozilla tests as of https://hg.mozilla.org/mozilla-central/rev/9ca690835a5e .

---
+++ contributors/mozilla/submitted/mozilla-central-reftests/sync-tests.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rsync -avz --delete --filter="- README" --filter="H test-template-001.xht" --filter="P sync-tests.sh" ~/builds/clean-mozilla-central/layout/reftests/w3c-css/submitted/ ./

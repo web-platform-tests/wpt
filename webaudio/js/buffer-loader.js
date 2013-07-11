ebc4dbbfc6669c06275fd5e22079bc63ad2022d8
Chris Lowis <chris.lowis@gmail.com>
2013-07-11 12:19:48 +0100

    whitespace

--- webaudio/js/buffer-loader.js
+++ webaudio/js/buffer-loader.js
@@ -2,46 +2,46 @@
    https://raw.github.com/WebKit/webkit/master/LayoutTests/webaudio/resources/buffer-loader.js */
 
 function BufferLoader(context, urlList, callback) {
-    this.context = context;
-    this.urlList = urlList;
-    this.onload = callback;
-    this.bufferList = new Array();
-    this.loadCount = 0;
+  this.context = context;
+  this.urlList = urlList;
+  this.onload = callback;
+  this.bufferList = new Array();
+  this.loadCount = 0;
 }
 
 BufferLoader.prototype.loadBuffer = function(url, index) {
-    // Load buffer asynchronously
-    var request = new XMLHttpRequest();
-    request.open("GET", url, true);
-    request.responseType = "arraybuffer";
+  // Load buffer asynchronously
+  var request = new XMLHttpRequest();
+  request.open("GET", url, true);
+  request.responseType = "arraybuffer";
 
-    var loader = this;
+  var loader = this;
 
-    request.onload = function() {
-        var buffer;
-        try {
-            buffer = loader.context.createBuffer(request.response, false);
-        } catch(e) {
-            alert('error decoding file data: ' + url);
-        }
-
-        try {
-            loader.bufferList[index] = buffer;
-            if (++loader.loadCount == loader.urlList.length)
-                loader.onload(loader.bufferList);
-        } catch(e) {
-            alert('BufferLoader: callback problem');
-        }
+  request.onload = function() {
+    var buffer;
+    try {
+      buffer = loader.context.createBuffer(request.response, false);
+    } catch(e) {
+      alert('error decoding file data: ' + url);
     }
 
-    request.onerror = function() {
-        alert('BufferLoader: XHR error');
+    try {
+      loader.bufferList[index] = buffer;
+      if (++loader.loadCount == loader.urlList.length)
+        loader.onload(loader.bufferList);
+    } catch(e) {
+      alert('BufferLoader: callback problem');
     }
+  }
 
-    request.send();
+  request.onerror = function() {
+    alert('BufferLoader: XHR error');
+  }
+
+  request.send();
 }
 
 BufferLoader.prototype.load = function() {
-    for (var i = 0; i < this.urlList.length; ++i)
-        this.loadBuffer(this.urlList[i], i);
+  for (var i = 0; i < this.urlList.length; ++i)
+    this.loadBuffer(this.urlList[i], i);
 }

d9277da9b5847724b45d1979f4dafa0c5c1dac0f
Chris Lowis <chris.lowis@gmail.com>
2013-07-12 14:22:54 +0100

    remove old assert helpers, they're in testharness.js now.

--- webaudio/js/helpers.js
+++ webaudio/js/helpers.js
@@ -1,15 +1,3 @@
-function assert_has_property(obj, name, desc) {
-  assert_true(undefined != obj[name], desc);
-};
-
-function assert_is_method(obj, name, desc) {
-  assert_true("function" === typeof obj[name], desc);
-};
-
-function assert_defined(obj, desc) {
-  assert_true(undefined != obj, desc);
-};
-
 function assert_array_approx_equals(actual, expected, epsilon, description)
 {
   assert_true(actual.length === expected.length,

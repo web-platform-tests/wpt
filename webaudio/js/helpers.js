d5b8864a18b91da2b5b7b9a0fb8c7d56916061cc
Chris Lowis <chris.lowis@gmail.com>
2013-07-12 11:39:25 +0100

    remove interface test helper. We'll use WebIDL now.

--- webaudio/js/helpers.js
+++ webaudio/js/helpers.js
@@ -10,29 +10,6 @@ function assert_defined(obj, desc) {
   assert_true(undefined != obj, desc);
 };
 
-// Test the passed in =obj= has the properties and methods to
-// implement the AudioParam Interface
-// (https://dvcs.w3.org/hg/audio/raw-file/tip/webaudio/specification.html#AudioParam)
-function test_implements_audio_param_interface(obj) {
-    var properties = ["value", "computedValue", "minValue", "maxValue",
-                      "defaultValue"];
-
-    _(properties).forEach(function(property) {
-        fn = function () { assert_has_property(obj, property, "has " + property) };
-        test(fn, "'" + obj.name + "'" + " implements: " + property);
-    });
-
-    var methods = ["setValueAtTime", "linearRampToValueAtTime",
-                   "exponentialRampToValueAtTime", "setTargetAtTime",
-                   "setValueCurveAtTime", "cancelScheduledValues"];
-
-    _(methods).forEach(function(meth) {
-        fn = function () { assert_is_method(obj, meth, "has " + meth + "()") };
-        test(fn, "'" + obj.name + "'" + " implements: " + meth + "()");
-    });
-};
-
-
 function fuzzyCompare(a, b) {
   return Math.abs(a - b) < 5e-5;
 }

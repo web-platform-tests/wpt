031c27365d3938defe8f368b3812eaec108a7c0b
Sergey G. Grekhov <sgrekhov@unipro.ru>
2013-07-17 18:35:14 +0700

    Tests for 'Additions to the "in head" insertion mode' chapter of the spec

--- html-templates/testcommon.js
+++ html-templates/testcommon.js
@@ -195,3 +195,11 @@ function checkBodyTemplateContent(d, html, id, nodeName) {
 function checkHeadTemplateContent(d, html, id, nodeName) {
     checkTemplateContent(d, d.head, html, id, nodeName);
 }
+
+function assert_null(value, description){
+    assert_true(value===null, description);
+}
+
+function assert_not_null(value, description){
+    assert_false(value===null, description);
+}

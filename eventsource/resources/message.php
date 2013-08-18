c436f1c63bf91c48b4173d9ff6088e28fa930265
Yaffle <vic99999@yandex.ru>
2013-08-18 12:26:09 +0600

    Update message.php

--- eventsource/resources/message.php
+++ eventsource/resources/message.php
@@ -2,8 +2,10 @@
   $mime = !empty($_GET["mime"]) ? $_GET["mime"] : "text/event-stream";
   $message = !empty($_GET["message"]) ? $_GET["message"] : "data: data";
   $newline = !empty($_GET["newline"]) ? ($_GET["newline"] == "none" ? "" : $_GET["newline"]) : "\n\n";
+  $sleep = !empty($_GET["sleep"]) ? intval($_GET["sleep"]) : 0;
   header("Content-Type: " . $mime);
   echo $message . $newline;
   echo "\n";
+  usleep($sleep * 1000);
   exit();
 ?>

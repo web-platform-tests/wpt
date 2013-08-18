1cfc3e3b54f1a1355eb0ff82152315c330e469ed
dailichen <formateric@gmail.com>
2013-08-18 17:26:27 +0800

    exception test

---
+++ IndexedDB/submissions/ericdum/support.js
@@ -0,0 +1,39 @@
+﻿if (!window.indexedDB)
+{
+    if (window.msIndexedDB)
+    {
+        window.indexedDB = window.msIndexedDB;
+    }
+    else if (window.mozIndexedDB)
+    {
+        window.indexedDB = window.mozIndexedDB;
+    }
+    else if (window.webkitIndexedDB)
+    {
+        window.indexedDB        = webkitIndexedDB;
+        IDBCursor               = webkitIDBCursor;
+        IDBDatabaseException    = webkitIDBDatabaseException;
+        IDBIndex                = webkitIDBIndex;
+        IDBObjectStore          = webkitIDBObjectStore;
+        IDBRequest              = webkitIDBRequest;
+        IDBKeyRange             = webkitIDBKeyRange;
+        IDBTransaction          = webkitIDBTransaction;
+    }
+}
+
+var generated_name = "";
+function generate_unique_name (prefix) {
+    prefix = prefix || "";
+    var key = "KJHFcjqvhk12e";
+    var name= prefix + key.substr((Math.random()*(key.length-3)), 3) + (new Date()).getTime();
+    if( generated_name.indexOf(name) !== -1 ) {
+        return generate_unique_name(prefix);
+    } else {
+        generated_name += "|" + name;
+        return name;
+    }
+}
+
+
+/*******************************************/
+

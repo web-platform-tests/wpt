3a602edf0b7430cddd45a6e8dbc4dcc8eda99afd
Chris Lowis <chris.lowis@gmail.com>
2013-08-19 16:31:01 +0100

    Add IDL test for DelayNode

--- webaudio/refresh_idl.rb
+++ webaudio/refresh_idl.rb
@@ -37,6 +37,7 @@ end
 idl_test_files = [
   File.join(base_dir, 'the-audio-api', 'the-gainnode-interface', 'idl-test.html'),
   File.join(base_dir, 'the-audio-api', 'the-audiodestinationnode-interface', 'idl-test.html'),
+  File.join(base_dir, 'the-audio-api', 'the-delaynode-interface', 'idl-test.html'),
 ]
 
 idl_test_files.each do |fn|

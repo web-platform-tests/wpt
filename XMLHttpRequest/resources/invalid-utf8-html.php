78fe5c7c4b47a61e7fb1bc15ce89438c67f42d99
Hallvord R. M. Steen <gitstuff@hallvord.com>
2013-10-05 23:43:47 +0200

    avoid using data: URLs in IFRAMEs for document tests due to WebKit/Chromium security policy

---
+++ XMLHttpRequest/resources/invalid-utf8-html.php
@@ -0,0 +1,4 @@
+<?php
+header('Content-type: text/html;charset=utf-8');
+echo chr(0xff);
+?>
---
+++ XMLHttpRequest/resources/win-1252-xml.php
@@ -0,0 +1,4 @@
+<?php
+header('Content-type: application/xml;charset=windows-1252');
+echo '<'.chr(0xff).'/>';
+?>

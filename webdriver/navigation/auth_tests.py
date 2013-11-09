87ec93303c280789b3e6741d56615637ed93e15a
Ethan Malasky <emalasky@adobe.com>
2013-11-09 02:55:19 -0800

    some more cleanup

--- webdriver/navigation/auth_tests.py
+++ webdriver/navigation/auth_tests.py
@@ -12,19 +12,19 @@ from selenium import webdriver
 
 class WebDriverAuthTest(unittest.TestCase):
 
-	# Response functions to test different Authentication Responses
-	
-
+	# Set up class to start HTTP Server that responds to 
+	# test URLs with various 401 responses
 	@classmethod
 	def setUpClass(cls):
 		config = ConfigParser.ConfigParser()
 		config.read('webdriver.cfg')
 		cls.driver_class = getattr(webdriver, config.get("Default", 'browser'))
 		cls.driver = cls.driver_class()
+
 		def basic_response_func( request, *args ):
 				print "basic_response_func!!!"
 				return (401, {"WWW-Authenticate" : "Basic"}, None)
-		
+
 		basic_auth_handler = { 'method': 'GET',
 							'path' : '/navigation/auth_required_basic',
 							'function' : basic_response_func }
@@ -35,23 +35,20 @@ class WebDriverAuthTest(unittest.TestCase):
 
 	@classmethod
 	def tearDownClass(cls):
-	    cls.driver.quit()
-	    cls.webserver.stop()
+		cls.driver.quit()
+		cls.webserver.stop()
 
+	# Test that when 401 is seen by browser, a WebDriver response is still sent
 	def test_response_401_auth_basic(self):
-
 		page = self.webserver.where_is('navigation/auth_required_basic')
-		print( "page url = " + page )
-
-#
 		resp = self.driver.execute( "get", params={"url" : page})
-
 		print("resp = " + str(resp))
 		url = self.driver.current_url
 		print("URL = " + url)
+		self.assertTrue(True)
 
-	#	self.driver.execute( { })
-#		self.assertTrue(False)
+
+
 
 if __name__ == "__main__":
     unittest.main()

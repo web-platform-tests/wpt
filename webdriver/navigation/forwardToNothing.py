307e0ee14c014334648c612dfd42a6ebbf7bc418
Bryan Sullivan <blsaws@gmail.com>
2013-11-09 18:39:23 +0800

    Fix unquoted attributes in navigation refresh test
    refreshPageStatic.html
    refreshPageDynamic.html

---
+++ webdriver/navigation/forwardToNothing.py
@@ -0,0 +1,23 @@
+# -*- mode: python; fill-column: 100; comment-column: 100; -*-
+
+import unittest
+import sys
+import os
+
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+import base_test
+
+class ForwardToNothingTest(base_test.WebDriverBaseTest):
+
+    # Get a static page that must be the same upon refresh
+    def test_forwardToNothing(self):
+        
+        self.driver.get(self.webserver.where_is('navigation/forwardStart.html'))
+        body = self.driver.find_element_by_tag_name("body").text
+        self.driver.forward()
+        currbody = self.driver.find_element_by_tag_name("body").text
+        self.assertEqual(body, currbody)
+
+if __name__ == '__main__':
+    unittest.main()

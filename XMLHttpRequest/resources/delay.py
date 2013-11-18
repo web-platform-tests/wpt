a0241d5d79bb23fab46365b0e1a1959d5a5ba6f9
James Graham <james@hoppipolla.co.uk>
2013-11-18 11:37:04 +0000

    Clean up from review comments

--- XMLHttpRequest/resources/delay.py
+++ XMLHttpRequest/resources/delay.py
@@ -3,4 +3,4 @@ import time
 def main(request, response):
     delay = float(request.GET.first("ms", 500))
     time.sleep(delay / 1E3);
-    return [("Content-type: text/plain")], "TEST_DELAY"
+    return [("Content-type", "text/plain")], "TEST_DELAY"

ea2bc2e2b8e7bbf6857fe12f8a856f295d7ce378
Ethan Malasky <emalasky@adobe.com>
2013-11-18 15:12:20 -0800

    fix indenting, tabs v spaces, extra whitespace

--- webdriver/navigation/auth_tests.py
+++ webdriver/navigation/auth_tests.py
@@ -13,43 +13,43 @@ from selenium.common.exceptions import TimeoutException
 
 class WebDriverAuthTest(unittest.TestCase):
 
-	# Set up class to start HTTP Server that responds to 
-	# test URLs with various 401 responses
-	@classmethod
-	def setUpClass(cls):
-		config = ConfigParser.ConfigParser()
-		config.read('webdriver.cfg')
-		cls.driver_class = getattr(webdriver, config.get("Default", 'browser'))
-		cls.driver = cls.driver_class()
+    # Set up class to start HTTP Server that responds to 
+    # test URLs with various 401 responses
+    @classmethod
+    def setUpClass(cls):
+        config = ConfigParser.ConfigParser()
+        config.read('webdriver.cfg')
+        cls.driver_class = getattr(webdriver, config.get("Default", 'browser'))
+        cls.driver = cls.driver_class()
 
-		def basic_response_func( request, *args ):
-				return (401, {"WWW-Authenticate" : "Basic"}, None)
+        def basic_response_func( request, *args ):
+            return (401, {"WWW-Authenticate" : "Basic"}, None)
 
-		basic_auth_handler = { 'method': 'GET',
-							'path' : '/navigation/auth_required_basic',
-							'function' : basic_response_func }
-		urlhandlers = [ basic_auth_handler ]
+        basic_auth_handler = { 'method': 'GET',
+                               'path' : '/navigation/auth_required_basic',
+                               'function' : basic_response_func }
+        urlhandlers = [ basic_auth_handler ]
 
-		cls.webserver = Httpd( urlhandlers=urlhandlers )
-		cls.webserver.start()
+        cls.webserver = Httpd( urlhandlers=urlhandlers )
+        cls.webserver.start()
 
-	@classmethod
-	def tearDownClass(cls):
-		cls.driver.quit()
-		cls.webserver.stop()
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+        cls.webserver.stop()
 
-	# Test that when 401 is seen by browser, a WebDriver response is still sent
-	def test_response_401_auth_basic(self):
-		page = self.webserver.where_is('navigation/auth_required_basic')
-		self.driver.set_page_load_timeout(5)
-		try:		
-			self.driver.get( page )
-			# if we got a responses instead of timeout, that's success
-			self.assertTrue(True)
-		except TimeoutException:
-			self.fail("Did not get response from browser.")
-		except:
-			self.fail("Unexpected failure. Please investigate.")
+    # Test that when 401 is seen by browser, a WebDriver response is still sent
+    def test_response_401_auth_basic(self):
+        page = self.webserver.where_is('navigation/auth_required_basic')
+        self.driver.set_page_load_timeout(5)
+        try:
+            self.driver.get( page )
+            # if we got a responses instead of timeout, that's success
+            self.assertTrue(True)
+        except TimeoutException:
+            self.fail("Did not get response from browser.")
+        except:
+            self.fail("Unexpected failure. Please investigate.")
 
 if __name__ == "__main__":
     unittest.main()

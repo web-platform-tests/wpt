64eef9fcf41c7ea407361164164da0f34b4ef4fb
James Graham <james@hoppipolla.co.uk>
2013-11-18 11:40:25 +0000

    Clean up from review comments

--- old-tests/submission/Microsoft/async/log.py
+++ old-tests/submission/Microsoft/async/log.py
@@ -2,15 +2,11 @@ import time
 
 def main(request, response):
     response.headers.append(("Content-Type", "text/javascript"))
-    script_id = request.GET.first("id")
-    delay = request.GET.first("sec")
-    print script_id, delay
-    #Check that we just have an integer
     try:
-        int(script_id)
-        int(delay)
+        script_id = int(request.GET.first("id"))
+        delay = int(request.GET.first("sec"))
     except:
-        response.set_error(400, "Invalid ID")
+        response.set_error(400, "Invalid parameter")
 
     time.sleep(int(delay))
 

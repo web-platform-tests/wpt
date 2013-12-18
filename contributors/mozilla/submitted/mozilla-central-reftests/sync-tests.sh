d04b49e929fb2d88120e28738546d46b2a2dd04b
L. David Baron <dbaron@dbaron.org>
2013-12-18 16:11:35 -0500

    Retract css-supports-027 and -028 since they use @-moz-document.

--- contributors/mozilla/submitted/mozilla-central-reftests/sync-tests.sh
+++ contributors/mozilla/submitted/mozilla-central-reftests/sync-tests.sh
@@ -2,6 +2,6 @@
 
 cd "$(dirname "$0")"
 
-rsync -avz --delete --filter="- README" --filter="H test-template-001.xht" --filter="P sync-tests.sh" ~/builds/clean-mozilla-central/layout/reftests/w3c-css/submitted/ ./
+rsync -avz --delete --filter=". ./sync-tests-filter" ~/builds/clean-mozilla-central/layout/reftests/w3c-css/submitted/ ./
 sed -i -e 's/^\(fails\|needs-focus\|random\|skip\|asserts\|slow\|require-or\|silentfail\|pref\|test-pref\|ref-pref\|fuzzy\)[^ ]* \?//g;/^default-preferences /d' $(find . -name reftest.list)
 hg addremove .

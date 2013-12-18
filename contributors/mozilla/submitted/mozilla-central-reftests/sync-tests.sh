a09c4b28de085ec17202319a825c97b8982b11af
L. David Baron <dbaron@dbaron.org>
2013-12-18 15:54:12 -0500

    Sync Mozilla tests as of https://hg.mozilla.org/mozilla-central/rev/1cd56379b9f5 .

--- contributors/mozilla/submitted/mozilla-central-reftests/sync-tests.sh
+++ contributors/mozilla/submitted/mozilla-central-reftests/sync-tests.sh
@@ -3,3 +3,4 @@
 cd "$(dirname "$0")"
 
 rsync -avz --delete --filter="- README" --filter="H test-template-001.xht" --filter="P sync-tests.sh" ~/builds/clean-mozilla-central/layout/reftests/w3c-css/submitted/ ./
+sed -i -e 's/^\(fails\|needs-focus\|random\|skip\|asserts\|slow\|require-or\|silentfail\|pref\|test-pref\|ref-pref\|fuzzy\)[^ ]* \?//g;/^default-preferences /d' $(find . -name reftest.list)

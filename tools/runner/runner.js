7627d98a9f2ca6f3fca4c98793d61b451c837027
James Graham <james@hoppipolla.co.uk>
2014-01-08 13:59:37 +0000

    fixup! Add a test runner and tool for making implementation reports

--- tools/runner/runner.js
+++ tools/runner/runner.js
@@ -192,6 +192,7 @@ VisualOutput.prototype = {
         this.progress.textContent = "Done";
         //add the json serialization of the results
         var a = this.elem.querySelector(".jsonResults");
+        //Using a json content type here causes browsers to freeze/crash
         a.href = "data:text/plain," + encodeURIComponent(this.runner.results.to_json());
         a.textContent = "JSON Results...";
     },

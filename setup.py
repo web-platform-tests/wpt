2a820e1ed31e601a6c5b72a1a83f290b351a31ec
James Graham <james@hoppipolla.co.uk>
2014-02-14 18:14:20 +0000

    Update package version to 1.0

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup
 
-PACKAGE_VERSION = '0.1'
+PACKAGE_VERSION = '1.0'
 deps = []
 
 setup(name='wptserve',
       version=PACKAGE_VERSION,
       description="Python webserver intended for in web browser testing",
       long_description=open("README.md").read(),
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=["Development Status :: 5 - Production/Stable",
+                   "License :: OSI Approved :: BSD License",
+                   "Topic :: Internet :: WWW/HTTP :: HTTP Servers"],
       keywords='',
       author='James Graham',
       author_email='james@hoppipolla.co.uk',

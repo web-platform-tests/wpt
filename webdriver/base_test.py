6c533cafbd65efb5b427f7eeab7683f5fddabcfa
Luke Inman-Semerau <luke.semerau@gmail.com>
2014-02-26 21:37:41 +0000

    adding ability to configure alternate browsers
    Signed-off-by: AutomatedTester <dburns@mozilla.com>

--- webdriver/base_test.py
+++ webdriver/base_test.py
@@ -1,7 +1,8 @@
-# -*- mode: python; fill-column: 100; comment-column: 100; -*-
 
-import unittest
 import ConfigParser
+import json
+import os
+import unittest
 
 from webserver import Httpd
 from selenium import webdriver
@@ -12,8 +13,29 @@ class WebDriverBaseTest(unittest.TestCase):
     def setUpClass(cls):
         config = ConfigParser.ConfigParser()
         config.read('webdriver.cfg')
-        cls.driver_class = getattr(webdriver, config.get("Default", 'browser'))
-        cls.driver = cls.driver_class()
+        section = os.environ.get("WD_BROWSER", 'firefox')
+        cls.driver_class = getattr(webdriver, config.get(section, 'browser'))
+        url = None
+        if config.has_option(section, 'url'):
+            url = config.get(section, "url")
+        capabilities = None
+        if config.has_option(section, 'capabilities'):
+            try:
+                capabilities = json.loads(config.get(section, "capabilities"))
+            except:
+              pass
+
+        if url != None and url.strip() != '':
+            if capabilities != None and type(capabilities) is dict:
+                cls.driver = cls.driver_class(command_executor=url,
+                                              desired_capabilities=capabilities)
+            else:
+                cls.driver = cls.driver_class(command_executor=url)
+        else:
+            if capabilities != None and type(capabilities) is dict:
+                cls.driver = cls.driver_class(desired_capabilities=capabilities)
+            else:
+                cls.driver = cls.driver_class()
         cls.webserver = Httpd()
         cls.webserver.start()
 

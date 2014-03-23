97b8232deb201768b181b5a5b5e33e75273ded92
Andrey Botalov <botalov.andrey@gmail.com>
2014-03-23 14:12:14 +0300

    Use assertIsNone, too long string, don't make requests to example.com

--- webdriver/element_state/method_test.py
+++ webdriver/element_state/method_test.py
@@ -17,7 +17,10 @@ class GetElementAttributeTest(base_test.WebDriverBaseTest):
     def test_style_attribute(self):
         self.driver.get(self.webserver.where_is("element_state/res/element-with-style-attribute.html"))
         el = self.driver.find_element_by_tag_name("div")
-        expected_style = 'font-family: "Gill Sans Extrabold",Helvetica,sans-serif; line-height: 1.2; font-weight: bold;'
+        expected_style = """
+                         font-family: \"Gill Sans Extrabold\",Helvetica,sans-serif; 
+                         line-height: 1.2; font-weight: bold;
+                         """
         self.assertEqual(expected_style, el.get_attribute("style"))
 
     def test_color_serialization_of_style_attribute(self):
@@ -33,7 +36,7 @@ class GetElementAttributeTest(base_test.WebDriverBaseTest):
     def test_none_if_boolean_attribute_absent(self):
         self.driver.get(self.webserver.where_is("element_state/res/input-without-checked-attribute.html"))
         el = self.driver.find_element_by_tag_name("input")
-        self.assertEqual(None, el.get_attribute("checked"))
+        self.assertIsNone(el.get_attribute("checked"))
 
     def test_option_with_attribute_value(self):
         self.driver.get(self.webserver.where_is("element_state/res/option-with-value-attribute.html"))
@@ -48,7 +51,7 @@ class GetElementAttributeTest(base_test.WebDriverBaseTest):
     def test_a_href_attribute(self):
         self.driver.get(self.webserver.where_is("element_state/res/a-with-href-attribute.html"))
         el = self.driver.find_element_by_tag_name("a")
-        self.assertEqual("http://example.com/path#fragment", el.get_attribute("href"))
+        self.assertEqual("http://web-platform.test:8000/path#fragment", el.get_attribute("href"))
 
     def test_img_src_attribute(self):
         self.driver.get(self.webserver.where_is("element_state/res/img-with-src-attribute.html"))
@@ -63,7 +66,7 @@ class GetElementAttributeTest(base_test.WebDriverBaseTest):
     def test_attribute_not_present(self):
         self.driver.get(self.webserver.where_is("element_state/res/element-without-attribute.html"))
         el = self.driver.find_element_by_tag_name("div")
-        self.assertEqual(None, el.get_attribute("class"))
+        self.assertIsNone(el.get_attribute("class"))
 
 if __name__ == "__main__":
     unittest.main()

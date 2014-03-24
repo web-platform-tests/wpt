d54d64cfca20d9bb967d89c3351b70a6238d6667
xiaojunwu <xiaojunx.a.wu@intel.com>
2014-03-24 19:39:14 +0800

    Rename the test filename frameElement.sub.html

--- html/browsers/windows/nested-browsing-contexts/replace.js
+++
@@ -1,4 +0,0 @@
-var src = "http://{{domains[www1]}}:{{ports[http][0]}}";
-src += document.location.pathname.substring(0, document.location.pathname.lastIndexOf("/") + 1);
-src += "test.html";
-document.getElementById("fr2").src = src;

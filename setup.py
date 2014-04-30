c203d52570148a3359e8f48591fe0333a29061c2
James Graham <james@hoppipolla.co.uk>
2014-04-30 12:12:15 +0100

    Don't assume we are in a mozilla tree to get prefs

--- setup.py
+++ setup.py
@@ -16,47 +16,37 @@ with open('requirements.txt') as f:
 
 profile_dest = None
 dest_exists = False
-try:
-    # Hack to include the default profiles in the distribution
-    if "sdist" in sys.argv:
-        profile_src = os.path.abspath(os.path.join("..", "profiles"))
-        profile_dest = os.path.abspath(os.path.join("wptrunner", "prefs"))
-        dest_exists = os.path.exists(profile_dest)
-        shutil.copytree(profile_src, profile_dest)
 
-    setup(name=PACKAGE_NAME,
-          version=PACKAGE_VERSION,
-          description="Harness for running the W3C web-platform-tests against various Mozilla products",
-          author='Mozilla Automation and Testing Team',
-          author_email='tools@lists.mozilla.org',
-          license='MPL 1.1/GPL 2.0/LGPL 2.1',
-          packages=find_packages(exclude=["tests", "metadata", "prefs"]),
-          entry_points = {
-              'console_scripts': [
-                  'wptrunner = wptrunner.wptrunner:main',
-                  'wptupdate = wptrunner.update:main',
-              ]
-          },
-          zip_safe=False,
-          platforms =['Any'],
-          classifiers=['Development Status :: 4 - Beta',
-                       'Environment :: Console',
-                       'Intended Audience :: Developers',
-                       'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
-                       'Operating System :: OS Independent',
-                      ],
-          package_data={"wptrunner": ["testharness.js",
-                                      "reftest.js",
-                                      "testharnessreport.js",
-                                      "gecko_runner.html",
-                                      "config.json",
-                                      "server-locations.txt",
-                                      "device_setup/*",
-                                      "prefs/*"]},
-          include_package_data=True,
-          data_files=[("config", ["config.ini"])],
-          install_requires=deps
-         )
-finally:
-    if profile_dest is not None and not dest_exists and os.path.exists(profile_dest):
-        shutil.rmtree(profile_dest)
+setup(name=PACKAGE_NAME,
+      version=PACKAGE_VERSION,
+      description="Harness for running the W3C web-platform-tests against various Mozilla products",
+      author='Mozilla Automation and Testing Team',
+      author_email='tools@lists.mozilla.org',
+      license='MPL 1.1/GPL 2.0/LGPL 2.1',
+      packages=find_packages(exclude=["tests", "metadata", "prefs"]),
+      entry_points = {
+          'console_scripts': [
+              'wptrunner = wptrunner.wptrunner:main',
+              'wptupdate = wptrunner.update:main',
+          ]
+      },
+      zip_safe=False,
+      platforms =['Any'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Console',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+                   'Operating System :: OS Independent',
+                  ],
+      package_data={"wptrunner": ["testharness.js",
+                                  "reftest.js",
+                                  "testharnessreport.js",
+                                  "gecko_runner.html",
+                                  "config.json",
+                                  "server-locations.txt",
+                                  "device_setup/*",
+                                  "prefs/*"]},
+      include_package_data=True,
+      data_files=[("config", ["config.ini"])],
+      install_requires=deps
+     )

8cd563b478e1948e86ffaa233a5e121395fc21f9
Simon Pieters <zcorpan@gmail.com>
2014-05-28 16:12:59 +0200

    Test behavior for multiple documents; expect HTTP cache to be ignored for same-document loads; Fix HTTP header for 'cachable'

--- html/semantics/embedded-content/the-img-element/update-the-image-data/resources/stash-count-requests.py
+++ html/semantics/embedded-content/the-img-element/update-the-image-data/resources/stash-count-requests.py
@@ -15,7 +15,7 @@ def main(request, response):
         response.writer.write_status(200)
         response.writer.write_header("Content-Type", "image/gif")
         if request.GET.first("cached") == "yes":
-            response.writer.write_header("Max-Age", "3600")
+            response.writer.write_header("Cache-Control", "Max-Age=3600")
         else:
             response.writer.write_header("Cache-Control", "no-store, no-cache, must-revalidate")
             response.writer.write_header("Expires", "Tue, 19 Nov 1985 21:00:00 GMT")

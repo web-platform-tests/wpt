5f5aac2099bd9cb577732ddd41d5c126ee48b4d8
James Graham <james@hoppipolla.co.uk>
2014-06-08 17:02:05 +0100

    Try not using postMessage to communicate results.

--- wptrunner/executors/testharness_marionette.js
+++ wptrunner/executors/testharness_marionette.js
@@ -4,21 +4,18 @@
 
 window.wrappedJSObject.timeout_multiplier = %(timeout_multiplier)d;
 
-function listener(e) {
-    if (e.data.type == "complete") {
-        clearTimeout(timer);
-        removeEventListener("message", listener);
-        var test_results = e.data.tests.map(function(x) {
-            return {name:x.name, status:x.status, message:x.message}
-        });
-        window.wrappedJSObject.win.close();
-        marionetteScriptFinished({test:"%(url)s",
-                                  tests:test_results,
-                                  status: e.data.status.status,
-                                  message: e.data.status.message});
-    }
+window.wrappedJSObject.done = function(tests, status) {
+    clearTimeout(timer);
+    var test_results = tests.map(function(x) {
+        return {name:x.name, status:x.status, message:x.message}
+    });
+    window.wrappedJSObject.win.close();
+    marionetteScriptFinished({test:"%(url)s",
+                              tests:test_results,
+                              status: status.status,
+                              message: status.message});
 }
-addEventListener("message", listener, false);
+//addEventListener("message", listener, false);
 
 window.wrappedJSObject.win = window.open("%(abs_url)s", "%(window_id)s");
 
--- wptrunner/testharnessreport.js
+++ wptrunner/testharnessreport.js
@@ -4,3 +4,4 @@ if (window.opener && "timeout_multiplier" in window.opener) {
     props["timeout_multiplier"] = window.opener.timeout_multiplier;
 }
 setup(props);
+add_completion_callback(function(tests, status) { window.opener.done(tests, status)});

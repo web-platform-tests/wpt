4c7638248640dd5dd466603ae37fe4c14e4b87fb
L. David Baron <dbaron@dbaron.org>
2014-07-15 23:25:01 -0700

    Update path for sync-tests.sh.

--- vendor-imports/mozilla/mozilla-central-reftests/sync-tests.sh
+++ vendor-imports/mozilla/mozilla-central-reftests/sync-tests.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")"
 
-rsync -avz --delete --filter=". ./sync-tests-filter" ../../../../../clean-mozilla-central/layout/reftests/w3c-css/submitted/ ./
+rsync -avz --delete --filter=". ./sync-tests-filter" ~/builds/clean-mozilla-central/layout/reftests/w3c-css/submitted/ ./
 sed -i -e 's/^\(\(fails\|needs-focus\|random\|skip\|asserts\|slow\|require-or\|silentfail\|pref\|test-pref\|ref-pref\|fuzzy\)[^ ]* \?\)\+//;/^default-preferences /d' $(find . -name reftest.list)
 sed -i -e 's/-moz-column/column/g' $(find . -name '*.xht')
 hg addremove .

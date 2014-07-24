a9d60fa939d0040915e330927eb66fdb615a498d
James Graham <james@hoppipolla.co.uk>
2014-07-24 10:15:45 +0100

    Let's call this 1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import textwrap
 from setuptools import setup, find_packages
 
 PACKAGE_NAME = 'wptrunner'
-PACKAGE_VERSION = '0.9'
+PACKAGE_VERSION = '1.0'
 
 # Dependencies
 with open('requirements.txt') as f:

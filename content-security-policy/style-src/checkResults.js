fdd04679d3f52851f22ff6afff9a1daf1cb41cfc
Ian Melven <ian@newrelic.com>
2014-08-03 11:57:11 -0700

    get rid of extra script file and use inline to try and make things cleaner

--- content-security-policy/style-src/checkResults.js
+++
@@ -1,6 +0,0 @@
-onload = function() {
-  test(function() {
-    var text = document.getElementById("content");
-    assert_true(getComputedStyle(text).marginLeft != "2px", "Inline style attribute should not be applied to text");
-  });
-}

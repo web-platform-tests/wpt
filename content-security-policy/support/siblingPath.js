2d7890b83d49e2dd568f65f2796fbe28b6ae8038
hillbrad <hillbrad@gmail.com>
2014-08-03 15:10:49 -0700

    remove logic to check for existing www prefix - tests must be run from eTLD+1 without any www hostname

--- content-security-policy/support/siblingPath.js
+++ content-security-policy/support/siblingPath.js
@@ -1,6 +1,5 @@
  buildSiblingPath = function(hostPrefix, relativePath, newPort) {
   var port = newPort ? newPort : document.location.port;
-  var host = document.location.hostname.indexOf("www") == 0 ? hostPrefix + document.location.hostname.substring(document.location.hostname.indexOf('.')) : hostPrefix + '.' + document.location.hostname;
   var path = document.location.pathname.substring(0, document.location.pathname.lastIndexOf('/') + 1);
-  return (document.location.protocol + '//' + host + ':' + port + path + relativePath);
+  return (document.location.protocol + '//' + hostPrefix + "." + document.location.hostname + ':' + port + path + relativePath);
 };

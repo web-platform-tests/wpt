3defcef99bb52ab6c10802773225e80170f02e0e
James Graham <james@hoppipolla.co.uk>
2014-08-27 15:43:04 +0100

    fixup! Allow symbols_path to be a url

--- wptrunner/wptcommandline.py
+++ wptrunner/wptcommandline.py
@@ -101,7 +101,7 @@ def create_parser(product_choices=None):
                         help="Halt the test runner when an unexpected result is encountered")
 
     parser.add_argument("--symbols-path", action="store", type=url_or_path,
-                        help="Path to symbols file used to analyse crash minidumps.")
+                        help="Path or url to symbols file used to analyse crash minidumps.")
     parser.add_argument("--stackwalk-binary", action="store", type=abs_path,
                         help="Path to stackwalker program used to analyse minidumps.")
 

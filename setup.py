7a6a3cfd92a037380599eeaadd59387f48b455fd
James Graham <james@hoppipolla.co.uk>
2014-10-06 21:20:40 +0100

    Bump version number for release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import textwrap
 from setuptools import setup, find_packages
 
 PACKAGE_NAME = 'wptrunner'
-PACKAGE_VERSION = '1.2'
+PACKAGE_VERSION = '1.3'
 
 # Dependencies
 with open('requirements.txt') as f:

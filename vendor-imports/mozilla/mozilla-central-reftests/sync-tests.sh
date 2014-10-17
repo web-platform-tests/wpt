b66fdc74478c0124b0186445d14e7abba514c317
L. David Baron <dbaron@dbaron.org>
2014-10-17 16:16:55 -0700

    Add substitutition for (future) tests using image-rendering, s/-moz-crisp-edges/pixelated/.

--- vendor-imports/mozilla/mozilla-central-reftests/sync-tests.sh
+++ vendor-imports/mozilla/mozilla-central-reftests/sync-tests.sh
@@ -4,5 +4,5 @@ cd "$(dirname "$0")"
 
 rsync -avz --delete --filter=". ./sync-tests-filter" ~/builds/clean-mozilla-central/layout/reftests/w3c-css/submitted/ ./
 sed -i -e 's/^\(\(fails\|needs-focus\|random\|skip\|asserts\|slow\|require-or\|silentfail\|pref\|test-pref\|ref-pref\|fuzzy\)[^ ]* *\?\)\+//;/^default-preferences /d;s/ \?# \?bug.*//' $(find . -name reftest.list)
-sed -i -e 's/-moz-column/column/g' $(find . -name '*.xht')
+sed -i -e 's/-moz-column/column/g;s/-moz-crisp-edges/pixelated/g' $(find . -regex ".*\.\(xht\|xhtml\|html\|css\)")
 hg addremove .

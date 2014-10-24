70905dd4b086b004bf5baa13fd7a1ce4aecf1c0f
Ms2ger <ms2ger@gmail.com>
2014-10-24 23:25:13 +0200

    Test for ownerElement in attributes_are.
    Attr#ownerElement has been restored to the specification.

--- dom/nodes/attributes.js
+++ dom/nodes/attributes.js
@@ -9,6 +9,6 @@ function attr_is(attr, v, ln, ns, p, n) {
 function attributes_are(el, l) {
   for (var i = 0, il = l.length; i < il; i++) {
     attr_is(el.attributes[i], l[i][1], l[i][0], (l[i].length < 3) ? null : l[i][2], null, l[i][0])
-//    assert_equals(el.attributes[i].ownerElement, el)
+    assert_equals(el.attributes[i].ownerElement, el)
   }
 }

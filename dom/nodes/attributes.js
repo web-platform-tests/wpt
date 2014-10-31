e6a731e444d4f91f3dc14925b777306385a86534
Ms2ger <ms2ger@gmail.com>
2014-10-31 11:59:44 +0100

    Add more tests to attr_is.

--- dom/nodes/attributes.js
+++ dom/nodes/attributes.js
@@ -1,9 +1,11 @@
 function attr_is(attr, v, ln, ns, p, n) {
   assert_equals(attr.value, v)
+  assert_equals(attr.textContent, v)
   assert_equals(attr.localName, ln)
   assert_equals(attr.namespaceURI, ns)
   assert_equals(attr.prefix, p)
   assert_equals(attr.name, n)
+  assert_equals(attr.specified, true)
 }
 
 function attributes_are(el, l) {

fc707422416d3e6e00d857f86f6b432c4787a6e6
Ms2ger <ms2ger@gmail.com>
2014-11-02 11:15:46 +0100

    Move the supported arguments to Document#createEvent into a separate file.

---
+++ dom/nodes/Document-createEvent.js
@@ -0,0 +1,10 @@
+var aliases = [
+  ["CustomEvent", "CustomEvent"],
+  ["Event", "Event"],
+  ["Events", "Event"],
+  ["HTMLEvents", "Event"],
+  ["MouseEvent", "MouseEvent"],
+  ["MouseEvents", "MouseEvent"],
+  ["UIEvent", "UIEvent"],
+  ["UIEvents", "UIEvent"]
+];

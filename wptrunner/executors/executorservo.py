web-platform-tests/wpt

commit 291fa69beeda50a8c5b790c7a48bf9ebbfd81653

Add a finish handler to the Servo executor.
This avoids an unnecessary delay in case the browser crashes or exits before
the result is printed.

wptrunner/executors/executorservo.py
@@ -33,7 +33,8 @@ class ServoTestharnessExecutor(ProcessTestExecutor):
 
 
         self.proc = ProcessHandler(self.command,
-                                   processOutputLine=[self.on_output])
+                                   processOutputLine=[self.on_output],
+                                   onFinish=self.on_finish)
         self.proc.run()
 
         timeout = test.timeout * self.timeout_multiplier
@@ -67,3 +68,6 @@ class ServoTestharnessExecutor(ProcessTestExecutor):
                 self.logger.process_output(self.proc.pid,
                                            line,
                                            " ".join(self.command))
+
+    def on_finish(self):
+        self.result_flag.set()

commit ea4ecfe03472ddb8cb86e5d20708b485d04488e9

Test for self.result_data in ServoTestharnessExecutor.
This fixes a bug introduced by 291fa69beeda50a8c5b790c7a48bf9ebbfd81653 that
made crashes fail the removed assertion.

wptrunner/executors/executorservo.py
@@ -42,8 +42,7 @@ class ServoTestharnessExecutor(ProcessTestExecutor):
         # Now wait to get the output we expect, or until we reach the timeout
         self.result_flag.wait(timeout + 5)
 
-        if self.result_flag.is_set():
-            assert self.result_data is not None
+        if self.result_flag.is_set() and self.result_data is not None:
             self.result_data["test"] = test.url
             result = self.convert_result(test, self.result_data)
             self.proc.kill()
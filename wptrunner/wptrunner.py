a8f260aaba9e2aae0a8de4d961f2c85701d9b0ea
James Graham <james@hoppipolla.co.uk>
2014-11-24 11:02:50 +0000

    fixup! Improve server logging; downgrade server errors to warnings when running tests.

--- wptrunner/wptrunner.py
+++ wptrunner/wptrunner.py
@@ -166,7 +166,7 @@ class TestEnvironment(object):
     def setup_server_logging(self):
         server_logger = get_default_logger(component="wptserve")
         assert server_logger is not None
-        log_filter = handlers.LogLevelFilter(handler, "info")
+        log_filter = handlers.LogLevelFilter(lambda x:x, "info")
         # Downgrade errors to warnings for the server
         log_filter = LogLevelRewriter(log_filter, ["error"], "warning")
         server_logger.component_filter = log_filter

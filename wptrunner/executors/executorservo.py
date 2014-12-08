0098b5cace2589cae8df0dae0124764a7509ac7d
James Graham <james@hoppipolla.co.uk>
2014-12-08 11:37:20 +0000

    Implement a reftest executor for servo

--- wptrunner/executors/executorservo.py
+++ wptrunner/executors/executorservo.py
@@ -2,14 +2,18 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import hashlib
 import json
+import os
 import subprocess
+import tempfile
 import threading
 import urlparse
+from collections import defaultdict
 
 from mozprocess import ProcessHandler
 
-from .base import testharness_result_converter
+from .base import testharness_result_converter, reftest_result_converter
 from .process import ProcessTestExecutor
 
 
@@ -70,3 +74,86 @@ class ServoTestharnessExecutor(ProcessTestExecutor):
 
     def on_finish(self):
         self.result_flag.set()
+
+class TempFilename(object):
+    def __init__(self):
+        self.path = None
+
+    def __enter__(self):
+        fd, self.path = tempfile.mkstemp()
+        os.close(fd)
+        return self.path
+
+    def __exit__(self, *args, **kwargs):
+        try:
+            os.unlink(self.path)
+        except IOError:
+            pass
+
+
+class ServoReftestExecutor(ProcessTestExecutor):
+    convert_result = reftest_result_converter
+
+    def __init__(self, *args, **kwargs):
+        ProcessTestExecutor.__init__(self, *args, **kwargs)
+        self.ref_hashes = {}
+        self.ref_urls_by_hash = defaultdict(set)
+
+    def run_test(self, test):
+        test_url, ref_type, ref_url = test.url, test.ref_type, test.ref_url
+        hashes = {"test": None,
+                  "ref": self.ref_hashes.get(ref_url)}
+
+        status = None
+
+        for url_type, url in [("test", test_url), ("ref", ref_url)]:
+            if hashes[url_type] is None:
+                full_url = urlparse.urljoin(self.http_server_url, url)
+
+                with TempFilename() as output_path:
+                    self.command = [self.binary, "--cpu", "--hard-fail", "--exit",
+                                    "--output=%s" % output_path, full_url]
+
+                    timeout = test.timeout * self.timeout_multiplier
+                    self.proc = ProcessHandler(self.command,
+                                               processOutputLine=[self.on_output])
+                    self.proc.run()
+                    rv = self.proc.wait(timeout=timeout)
+                    if rv is None:
+                        status = "EXTERNAL-TIMEOUT"
+                        self.proc.kill()
+                        break
+                    elif rv < 0:
+                        status = "CRASH"
+                        break
+                    else:
+                        with open(output_path) as f:
+                            # Might need to strip variable headers or something here
+                            data = f.read()
+                            hashes[url_type] = hashlib.sha1(data).hexdigest()
+
+        if status is None:
+            self.ref_urls_by_hash[hashes["ref"]].add(ref_url)
+            self.ref_hashes[ref_url] = hashes["ref"]
+
+            if ref_type == "==":
+                passed = hashes["test"] == hashes["ref"]
+            elif ref_type == "!=":
+                passed = hashes["test"] != hashes["ref"]
+            else:
+                raise ValueError
+
+            status = "PASS" if passed else "FAIL"
+
+        result = self.convert_result(test, {"status": status, "message": None})
+        self.runner.send_message("test_ended", test, result)
+
+
+    def on_output(self, line):
+        line = line.decode("utf8", "replace")
+        if self.interactive:
+            print line
+        else:
+            self.logger.process_output(self.proc.pid,
+                                       line,
+                                       " ".join(self.command))

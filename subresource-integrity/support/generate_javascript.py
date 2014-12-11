cadbc9f5566bdd37a9e0387d5869c3059143f352
Ms2ger <ms2ger@gmail.com>
2014-12-11 09:59:00 +0100

    Follow PEP8 guidelines in generate_javascript.py.

--- subresource-integrity/support/generate_javascript.py
+++ subresource-integrity/support/generate_javascript.py
@@ -5,38 +5,45 @@ import re
 
 JS_DIR = path.normpath(path.join(__file__, "..", ".."))
 
-'''
-Yield each file in the javascript directory
-'''
+
 def js_files():
-  for f in listdir(JS_DIR):
-    if path.isfile(f) and f.endswith(".js"):
-      yield f
+    '''
+    Yield each file in the javascript directory
+    '''
+    for f in listdir(JS_DIR):
+        if path.isfile(f) and f.endswith(".js"):
+            yield f
 
-'''
-URL-safe base64 encode a binary digest and strip any padding.
-'''
+
 def format_digest(digest):
-  return urlsafe_b64encode(digest).rstrip("=")
+    '''
+    URL-safe base64 encode a binary digest and strip any padding.
+    '''
+    return urlsafe_b64encode(digest).rstrip("=")
 
-'''
-Generate an encoded sha256 URI.
-'''
+
 def sha256_uri(content):
-  return "ni:///sha-256;%s" % format_digest(sha256(content).digest())
+    '''
+    Generate an encoded sha256 URI.
+    '''
+    return "ni:///sha-256;%s" % format_digest(sha256(content).digest())
 
-'''
-Generate an encoded md5 digest URI.
-'''
+
 def md5_uri(content):
-  return "ni:///md5;%s" % format_digest(md5(content).digest())
+    '''
+    Generate an encoded md5 digest URI.
+    '''
+    return "ni:///md5;%s" % format_digest(md5(content).digest())
+
 
 def main():
-  for file in js_files():
-    base = path.splitext(path.basename(file))[0]
-    var_name = re.sub(r"[^a-z]", "_", base)
-    content = "%s=true;" % var_name
-    with open(file, "w") as f: f.write(content)
+    for file in js_files():
+        base = path.splitext(path.basename(file))[0]
+        var_name = re.sub(r"[^a-z]", "_", base)
+        content = "%s=true;" % var_name
+        with open(file, "w") as f:
+            f.write(content)
+
 
 if __name__ == "__main__":
-  main()
+    main()

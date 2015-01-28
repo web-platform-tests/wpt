39145b937d0eb9550754be6219185bf507f23c85
Mark Nottingham <mnot@mnot.net>
2015-01-28 15:34:07 +1100

    Update to check Access-Control-Expose-Headers on 304

--- cors/resources/304.py
+++ cors/resources/304.py
@@ -15,20 +15,31 @@ def main(request, response):
     raw_req_num = request.headers.get('content-language', None)    
     if raw_req_num == None:
         return error("no req_num header in request")
-        req_num = None
     else:
         req_num = int(raw_req_num)
-        if req_num > 2:
-            return error("req_num greater than 2")
+        if req_num > 3:
+            return error("req_num %s out of range" % req_num)
+
+    headers.append(("A", req_num))
+    headers.append(("B", req_num))
     
     if inm == etag:
-        if req_num and req_num != 2:
-            return error("inm on wrong request")
+        # A request with an If-None-Match header matching the stored etag
+        if req_num == 1:
+            return error("If-None-Match on first request")
+        elif req_num == 2:
+            headers.append(("Access-Control-Expose-Headers", "a, b"))
+        elif req_num == 3:
+            headers.append(("Access-Control-Expose-Headers", "a"))
         status = 304, "Not Modified"
         return status, headers, ""
     else:
-        if req_num and req_num != 1:
-            return error("no inm on second req")
+        # INM is not present, or does not match
+        if req_num != 1:
+            if inm == None:
+                return error("If-None-Match missing")
+            else:
+                return error("If-None-Match '%s' mismatches")
         status = 200, "OK"
         headers.append(("Access-Control-Allow-Origin", "*"))
         headers.append(("Content-Type", "text/plain"))

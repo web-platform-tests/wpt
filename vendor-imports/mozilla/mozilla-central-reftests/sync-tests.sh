10fa8409d10922dbfc33c81287a7bb5e8c505d7e
L. David Baron <dbaron@dbaron.org>
2015-03-27 22:57:18 -0700

    Update to remove some additional reftest.list annotations.

--- vendor-imports/mozilla/mozilla-central-reftests/sync-tests.sh
+++ vendor-imports/mozilla/mozilla-central-reftests/sync-tests.sh
@@ -3,6 +3,6 @@
 cd "$(dirname "$0")"
 
 rsync -avz --delete --filter=". ./sync-tests-filter" ~/builds/clean-mozilla-central/layout/reftests/w3c-css/submitted/ ./
-sed -i -e 's/^\(\(fails\|needs-focus\|random\|skip\|asserts\|slow\|require-or\|silentfail\|pref\|test-pref\|ref-pref\|fuzzy\)[^ ]* *\?\)\+//;/^default-preferences /d;s/ \?# \?[bB]ug.*//' $(find . -name reftest.list)
+sed -i -e 's/^\(\(fails\|needs-focus\|random\|skip\|asserts\|slow\|require-or\|silentfail\|pref\|test-pref\|ref-pref\|fuzzy\)[^ ]* *\?\)\+//;/^default-preferences /d;s/ \?# \?\(TC: \)\?[bB]ug.*//;s/ # Initial mulet triage:.*//' $(find . -name reftest.list)
 sed -i -e 's/-moz-column/column/g;s/-moz-crisp-edges/pixelated/g' $(find . -regex ".*\.\(xht\|xhtml\|html\|css\)")
 hg addremove .

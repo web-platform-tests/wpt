377f11044de606e5760616f93debbabec9ed0186
Mark Nottingham <mnot@mnot.net>
2015-04-08 19:25:21 +1000

    go async

--- cors/resources/304.py
+++ cors/resources/304.py
@@ -17,35 +17,39 @@ def main(request, response):
         return error("no req_num header in request")
     else:
         req_num = int(raw_req_num)
-        if req_num > 4:
+        if req_num > 8:
             return error("req_num %s out of range" % req_num)
 
+    headers.append(("Content-Language", req_num))
     headers.append(("A", req_num))
     headers.append(("B", req_num))
 
-    if inm == etag:
-        # A request with an If-None-Match header matching the stored etag
-        if req_num == 1:
-            return error("If-None-Match on first request")
-        elif req_num == 2:
-            headers.append(("Access-Control-Expose-Headers", "a, b"))
-        elif req_num == 3:
-            headers.append(("Access-Control-Expose-Headers", "a"))
-        elif req_num == 4:
-            headers.append(("Access-Control-Allow-Origin", "other.origin.example"))
-        status = 304, "Not Modified"
-        return status, headers, ""
-    else:
-        # INM is not present, or does not match
-        if req_num != 1:
+    if req_num % 2:  # odd requests are the first in a test pair
+        if inm:
+            # what are you doing here? This should be a fresh request.
+            return error("If-None-Match on first request")            
+        else:
+            status = 200, "OK"
+            headers.append(("Access-Control-Allow-Origin", "*"))
+            headers.append(("Content-Type", "text/plain"))
+            headers.append(("Cache-Control", "private, max-age=3, must-revalidate"))
+            headers.append(("ETag", etag))
+            return status, headers, "Success"
+    else:  # even requests are the second in a pair, and should have a good INM.
+        if inm != etag:
+            # Bad browser.
             if inm == None:
                 return error("If-None-Match missing")
             else:
                 return error("If-None-Match '%s' mismatches")
-        status = 200, "OK"
-        headers.append(("Access-Control-Allow-Origin", "*"))
-        headers.append(("Content-Type", "text/plain"))
-        headers.append(("Cache-Control", "private, max-age=3600"))
-        headers.append(("ETag", etag))
-        return status, headers, "Success"
-
+        else:
+            if req_num == 2:
+                pass  # basic, vanilla check
+            if req_num == 4:
+                headers.append(("Access-Control-Expose-Headers", "a, b"))
+            elif req_num == 6:
+                headers.append(("Access-Control-Expose-Headers", "a"))
+            elif req_num == 8:
+                headers.append(("Access-Control-Allow-Origin", "other.origin.example"))
+            status = 304, "Not Modified"
+            return status, headers, ""

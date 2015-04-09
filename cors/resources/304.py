2ed1bd3a2cebd5a401572305af2444bd13cd4187
Mark Nottingham <mnot@mnot.net>
2015-04-09 15:35:21 +1000

    Factor out state header, disable checks

--- cors/resources/304.py
+++ cors/resources/304.py
@@ -1,18 +1,24 @@
+#!/usr/bin/env python
+
+# A header used to correlate requests and responses
+state_header = "content-language"
+
+# Static ETag to use (and expect)
 etag = "abcdef"
 
 def error(msg):
     return (299, "Client Error"), [
         ('content-type', 'text/plain'),
         ('access-control-allow-origin', "*"),
+        ('access-control-expose-headers', state_header),
         ('cache-control', 'no-store')
     ], msg
 
 def main(request, response):
     headers = []
 
-
     inm = request.headers.get('if-none-match', None)
-    raw_req_num = request.headers.get('content-language', None)
+    raw_req_num = request.headers.get(state_header, None)
     if raw_req_num == None:
         return error("no req_num header in request")
     else:
@@ -20,7 +26,8 @@ def main(request, response):
         if req_num > 8:
             return error("req_num %s out of range" % req_num)
 
-    headers.append(("Content-Language", req_num))
+    headers.append(("Access-Control-Expose-Headers", state_header))
+    headers.append((state_header, req_num))
     headers.append(("A", req_num))
     headers.append(("B", req_num))
 
@@ -45,11 +52,11 @@ def main(request, response):
         else:
             if req_num == 2:
                 pass  # basic, vanilla check
-            if req_num == 4:
+            elif req_num == 4:
                 headers.append(("Access-Control-Expose-Headers", "a, b"))
             elif req_num == 6:
                 headers.append(("Access-Control-Expose-Headers", "a"))
             elif req_num == 8:
-                headers.append(("Access-Control-Allow-Origin", "other.origin.example"))
+                headers.append(("Access-Control-Allow-Origin", "other.origin.example:80"))
             status = 304, "Not Modified"
             return status, headers, ""

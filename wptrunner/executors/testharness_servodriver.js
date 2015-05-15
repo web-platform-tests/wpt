a6908f90e5c76de14f9816095b8fa89b996ffdb5
James Graham <james@hoppipolla.co.uk>
2015-05-15 11:54:58 +0100

    Don't use the DOM to store results for servodriver.

--- wptrunner/executors/testharness_servodriver.js
+++ wptrunner/executors/testharness_servodriver.js
@@ -2,31 +2,5 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var callback = arguments[arguments.length - 1];
-
-var results_id = "__testharness__results__";
-
-function done() {
-  clearTimeout(timer);
-  callback(document.getElementById(results_id).textContent);
-}
-
-// Work around the fact that load isn't blocked on the scripts
-// actually running
-(function setup() {
-  if (document.getElementById(results_id)) {
-    done();
-  } else {
-    if (window.add_completion_callback) {
-      add_completion_callback(function() {
-        add_completion_callback(done);
-      });
-    } else {
-      setTimeout(setup, 20);
-    }
-  }
-})()
-
-var timer = setTimeout(function() {
-  timeout();
-}, %(timeout)s);
+window.__wd_results_callback__ = arguments[arguments.length - 1];
+window.__wd_results_timer__ = setTimeout(timeout, %(timeout)s);

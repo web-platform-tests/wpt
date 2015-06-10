0d18c60cf499d7ead46b74473ddf8e0a4df87236
Koji Ishii <kojiishi@gmail.com>
2015-06-10 22:29:44 +0900

    [css-writing-modes-3] Minor refactor of tools/generators

--- css-writing-modes-3/tools/generators/gulpfile.js
+++ css-writing-modes-3/tools/generators/gulpfile.js
@@ -4,17 +4,19 @@ var rename = require("gulp-rename");
 var orientation = require("./text-orientation-generator.js");
 
 var templates = [
-    ["orthogonal-parent-shrink-to-fit", -1, 24],
+    ["orthogonal-parent-shrink-to-fit", "orthogonal-parent-shrink-to-fit-001", -1, 24],
 ];
 
 for (var template of templates) {
     var name = template[0];
     gulp.task(name, function () {
-        for (var i = template[1]; i < template[2]; ++i) {
-            var affix = i < 0 ? "" : String.fromCharCode("a".charCodeAt(0) + i);
+        for (var i = template[2]; i < template[3]; ++i) {
+            var target = template[1];
+            if (i >= 0)
+                target += String.fromCharCode("a".charCodeAt(0) + i);
             gulp.src(name + ".ejs")
                 .pipe(ejs({ index: i }))
-                .pipe(rename(name + affix + ".html"))
+                .pipe(rename(target))
                 .pipe(gulp.dest("../.."));
         }
     });

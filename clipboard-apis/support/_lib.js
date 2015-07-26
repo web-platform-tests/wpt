9f98704c5419c21ea8d4beb8d7d43544afe02be1
Hallvord R. M. Steen <hsteen@mozilla.com>
2015-07-26 17:57:23 +0200

    integrating test setup with testharness.js, addresses https://critic.hoppipolla.co.uk/showcomment?chain=12228

--- clipboard-apis/support/_lib.js
+++ clipboard-apis/support/_lib.js
@@ -19,34 +19,34 @@ function setupTest( target, event, dataToPaste, externalPassCondition ){
     if( dataToPaste || event==='paste' ){
         logNode.data+='Please place this on the clipboard before continuing the test: "'+(dataToPaste || 'clipboard text' )+'"\n';
     logNode.parentNode.style.whiteSpace='pre';
-		if(dataToPaste.indexOf('{')==0){ // sorry about the content sniffing, this is likely a JSON string with alternate clipboard formats
-			if(dataToPaste.indexOf('text/html')>-1){
-				logNode.parentNode.appendChild(document.createElement('br'));
-				logNode.parentNode.appendChild(document.createTextNode('Note: copy all body text from '));
-				var tmp=logNode.parentNode.appendChild(document.createElement('a'));
-				tmp.href='support/html_file.htm';
-				tmp.appendChild(document.createTextNode('this support file'));
-				logNode.parentNode.appendChild(document.createTextNode(' for this test.'));
-			}
-		}
-	}
-	if(typeof triggerTestManually==='function'){
-		/* Tests that require user interaction define a "triggerTestManually()" function.
-		These are for example tests that rely on trusted events. */
-		logNode.parentNode.appendChild(document.createTextNode('  '));
-		var btn=logNode.parentNode.appendChild(document.createElement('button'))
-		btn.type='button';
-		btn.onclick=function(){
-			triggerTestManually(event);
-			btn.parentNode.removeChild(btn);
-		}
-		btn.appendChild(document.createTextNode(' Click here to run test: '));
-	}else{
-		logNode.data+='Test in progress, waiting for '+event+' event';
-	}
-	if(typeof onTestSetupReady==='function'){
-		onTestSetupReady(event);
-	}
+        if(dataToPaste.indexOf('{')==0){ // sorry about the content sniffing, this is likely a JSON string with alternate clipboard formats
+            if(dataToPaste.indexOf('text/html')>-1){
+                logNode.parentNode.appendChild(document.createElement('br'));
+                logNode.parentNode.appendChild(document.createTextNode('Note: copy all body text from '));
+                var tmp=logNode.parentNode.appendChild(document.createElement('a'));
+                tmp.href='support/html_file.htm';
+                tmp.appendChild(document.createTextNode('this support file'));
+                logNode.parentNode.appendChild(document.createTextNode(' for this test.'));
+            }
+        }
+    }
+    if(typeof triggerTestManually==='function'){
+        /* Tests that require user interaction define a "triggerTestManually()" function.
+        These are for example tests that rely on trusted events. */
+        logNode.parentNode.appendChild(document.createTextNode('  '));
+        var btn = logNode.parentNode.appendChild(document.createElement('button'))
+        btn.type = 'button';
+        btn.onclick = t.func_step(function(){
+            triggerTestManually(event);
+            btn.parentNode.removeChild(btn);
+        });
+        btn.appendChild(document.createTextNode(' Click here to run test: '));
+    }else{
+        logNode.data+='Test in progress, waiting for '+event+' event';
+    }
+    if(typeof onTestSetupReady==='function'){
+        onTestSetupReady(event);
+    }
 
     function intermediateListener(e){
         e=e||window.event;
@@ -68,24 +68,28 @@ function setupTest( target, event, dataToPaste, externalPassCondition ){
 }
 
 function result(testResult, msg){
-	var logNode=document.getElementsByTagName('p')[0].firstChild;
-	if( testResult === true || testResult === false ){
-		t.step(function(){assert_true(testResult)});
-		t.done();
-		logNode.data= ''; // testResult ? 'PASSED' : 'FAILED';
-	}else if( typeof testResult ==='string' ){
-		logNode.data=testResult;
-	}else if( typeof externalPassCondition==='string' ){
-		logNode.data='\nThis test passes if this text is now on the system clipboard: "'+externalPassCondition+'"';
-		var btn = document.getElementById('log').appendChild(document.createElement('button'));
-		btn.onclick = function(){result(true)};
-		btn.textContent = 'Passed!';
-		btn.type='button';
-		btn = document.getElementById('log').appendChild(document.createElement('button'));
-		btn.onclick = function(){result(false)};
-		btn.textContent = 'Failed!';
-		btn.type='button';
-	}
+    var logNode=document.getElementsByTagName('p')[0].firstChild;
+    if( testResult === true || testResult === false ){
+        t.step(function(){assert_true(testResult)});
+        t.done();
+        logNode.data= '';
+    }else if( typeof testResult === 'function' ){
+        t.step(testResult);
+        t.done();
+        logNode.data= '';
+    }else if( typeof testResult ==='string' ){
+        logNode.data=testResult;
+    }else if( typeof externalPassCondition==='string' ){
+        logNode.data='\nThis test passes if this text is now on the system clipboard: "'+externalPassCondition+'"';
+        var btn = document.getElementById('log').appendChild(document.createElement('button'));
+        btn.onclick = function(){result(true)};
+        btn.textContent = 'Passed!';
+        btn.type='button';
+        btn = document.getElementById('log').appendChild(document.createElement('button'));
+        btn.onclick = function(){result(false)};
+        btn.textContent = 'Failed!';
+        btn.type='button';
+    }
   if( msg )logNode.data+='\n'+msg;
 
     /* another return value - or no return - from test() indicates that it is asyncronous and will call testResult() from a timeout or something */

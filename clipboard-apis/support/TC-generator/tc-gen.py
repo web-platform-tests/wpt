a6f7313b7c5b18a00a583487b075e61605e2976d
Hallvord R. M. Steen <hsteen@mozilla.com>
2015-07-26 18:17:32 +0200

    Remove pointless whitespace in front of test titles

--- clipboard-apis/support/TC-generator/tc-gen.py
+++ clipboard-apis/support/TC-generator/tc-gen.py
@@ -15,7 +15,7 @@ tc_path = "C:\\mozilla\\web-platform-tests\\clipboard-apis\\"
 template = u"""<!DOCTYPE html>
 <html><head>
     <meta charset="UTF-8">
-    <title>  ClipboardEvent - {title}</title>
+    <title>Clipboard API - {title}</title>
     <script src="/resources/testharness.js"></script>
     <script src="/resources/testharnessreport.js"></script>
     <script src="support/_lib.js"></script>

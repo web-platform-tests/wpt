7167eb1d797e628ee327f8bdad80720e6bc6dea4
Hallvord R. M. Steen <hsteen@mozilla.com>
2015-07-26 17:16:01 +0200

    dropping Ruby file listing test cases and code to generate it - legacy stuff

--- clipboard-apis/support/TC-generator/tc-gen.py
+++ clipboard-apis/support/TC-generator/tc-gen.py
@@ -2,11 +2,13 @@
 # This script extracts all tests from the spec text and
 # builds the test suite
 
-import os, re, codecs, sys
+import os
+import re
+import codecs
 
-if len(sys.argv<3):
-	print 'Two arguments are required: path to spec (clipops-source.html) and \nthe path to a folder where generated tests should be stored.'
-	sys.exit()
+script_path = os.path.abspath(os.path.dirname(__file__))
+spec_path = os.path.join(script_path, '..', 'index.html')
+tc_path = os.path.join(script_path, '..', 'testsuite') + os.sep
 
 tc_path = "C:\\mozilla\\web-platform-tests\\clipboard-apis\\"
 
@@ -49,53 +51,56 @@ spec_text = f.read()
 f.close()
 r = re.compile('<script>\s*(/\*\* (?P<title>.+?)\*/\s*\n(?P<script>.+?))</script>', re.I | re.S)
 testcounter = 1
-rubytemplate = """
-describe "Clipboard events testsuite" do
-  before(:all) do
-    @browser = OperaWatir::Browser.new
-    $base = 'file://localhost/d:/projects/spec-dev/copy-paste/W3C-repo/clipops/testsuite/'
-  end
-%s
-end
-"""
-rubycode=""
-rbatom = """
-	it "%i %s" do
-		doSingleTest %i
-	end
-"""
+
 for match in re.finditer(r, spec_text):
-	test_info = {"title":match.groupdict()["title"], "test_javascript_code":match.groupdict()["script"], "data_to_paste":"''", "targets":default_event_target, "external_pass_condition":"null", "test_html":default_test_html}
-	test_info["test_javascript_code"] = "/** %s */\n%s" % (test_info["title"], test_info["test_javascript_code"]) # add title comment back..
-	test_data_map = { "Test HTML":"test_html", "Events":"events", "Targets":"targets", "paste data":"data_to_paste", "External pass condition - clipboard data":"external_pass_condition" }
-	for name in test_data_map:
-		value = test_data_map[name]
-		rx = re.compile(r"\/\*\s*"+name+": (.+?)\*\/", re.S)
-		try:
-			test_info[value] = re.search(rx, test_info['test_javascript_code']).group(1)
-			if name is 'Test HTML' and '<\\/script' in test_info[value]:
-				test_info[value] = test_info[value].replace('<\\/script', '</script')
-		except Exception, e:
-			test_info.setdefault(value, '')
-			continue
-	if 'events' in test_info:
-		test_info['events'] = test_info['events'].strip().split(' ')
-	if 'targets' in test_info:
-		test_info['targets'] = test_info['targets'].strip().split(' ')
-	rubycode += rbatom % (testcounter, test_info['title'], testcounter)
-	#print test_info
-	base_title = test_info['title']
-	for event in test_info['events']:
-		for target in test_info['targets']:
-			test_info['event_type'] = event
-			test_info['event_target'] = target
-			test_info['title'] = base_title + ' - ' + event + ' on ' + target
-			#print template.format(**test_info)
-			fn = tc_path+("%03d"%testcounter)+'.html'
-			f = codecs.open(fn, 'w', 'UTF-8')
-			f.write(template.format(**test_info))
-			f.close()
-			testcounter += 1
-			print 'wrote %s, %s'%(fn, test_info['title'])
-	#print match.groupdict()
+    test_info = {
+        "title": match.groupdict()["title"],
+        "test_js_code": match.groupdict()["script"],
+        "data_to_paste": "''",
+        "targets": default_event_target,
+        "external_pass_condition": "null",
+        "test_html": default_test_html
+    }
+    # add title comment back, it's useful for code review
+    test_info["test_js_code"] = "/** %s */\n%s" % (
+        test_info["title"],
+        test_info["test_js_code"]
+    )
+    test_data_map = {
+        "Test HTML": "test_html",
+        "Events": "events",
+        "Targets": "targets",
+        "paste data": "data_to_paste",
+        "External pass condition - clipboard data": "external_pass_condition"
+    }
+    for name in test_data_map:
+        value = test_data_map[name]
+        rx = re.compile(r"\/\*\s*"+name+": (.+?)\*\/", re.S)
+        try:
+            test_info[value] = re.search(rx, test_info['test_js_code']).group(1)
+            if name is 'Test HTML' and '<\\/script' in test_info[value]:
+                test_info[value] = test_info[value].replace(
+                    '<\\/script',
+                    '</script'
+                )
+        except Exception, e:
+            test_info.setdefault(value, '')
+            continue
+    if 'events' in test_info:
+        test_info['events'] = test_info['events'].strip().split(' ')
+    if 'targets' in test_info:
+        test_info['targets'] = test_info['targets'].strip().split(' ')
+    base_title = test_info['title']
+    for event in test_info['events']:
+        for target in test_info['targets']:
+            test_info['event_type'] = event
+            test_info['event_target'] = target
+            test_info['title'] = base_title + ' - ' + event + ' on ' + target
+            fn = re.sub('[ ]+', '_', re.sub('[^a-zA-Z0-9 ]+', '', test_info['title']))
+            fn = fn.lower() + '.html'
+            f = codecs.open(tc_path + fn, 'w', 'UTF-8')
+            f.write(template.format(**test_info))
+            f.close()
+            testcounter += 1
+            print('wrote %s, %s' % (fn, test_info['title']))
 

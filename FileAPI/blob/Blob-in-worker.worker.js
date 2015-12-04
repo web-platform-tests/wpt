aba2ca1a0ad33b148906dac8f4ddc0d70a94e576
chunywang <chunyanx.wang@intel.com>
2015-12-04 17:46:45 +0800

    Update the tests

--- FileAPI/blob/Blob-in-worker.worker.js
+++ FileAPI/blob/Blob-in-worker.worker.js
@@ -1,24 +1,14 @@
 importScripts("/resources/testharness.js");
 
-promise_test(function() {
-  return new Promise(function(resolve, reject) {
-    var data = "TEST";
-    var blob = new Blob([data], {type: "text/plain"});
-
-    var reader = new FileReader();
-    reader.onload = function (event) {
-      var content = reader.result;
-      assert_equals(content, data);
-      resolve();
-    };
-
-    reader.onerror = function(event) {
-      assert_unreached(event.error.message);
-      reject(event.error.message);
-    };
-    reader.readAsText(blob);
+async_test(function() {
+  var data = "TEST";
+  var blob = new Blob([data], {type: "text/plain"});
+  var reader = new FileReader();
+  reader.onload = this.step_func_done(function() {
+    assert_equals(reader.result, data);
   });
+  reader.onerror = this.unreached_func("Unexpected error event");
+  reader.readAsText(blob);
 }, "Create Blob in Worker");
 
 done();
-
--- FileAPI/file/Worker-read-file-constructor.worker.js
+++ FileAPI/file/Worker-read-file-constructor.worker.js
@@ -1,24 +1,15 @@
 importScripts("/resources/testharness.js");
 
-promise_test(function() {
-  return new Promise(function(resolve, reject) {
-    var file = new File(["bits"], "dummy", { 'type': 'text/plain', lastModified: 42 });
-
-    var reader = new FileReader();
-    reader.onload = function (event) {
-      var content = reader.result;
-      assert_equals(file.name, "dummy", "file name");
-      assert_equals(content, "bits", "file content");
-      assert_equals(file.lastModified, 42, "file lastModified");
-      resolve();
-    };
-
-    reader.onerror = function(event) {
-      assert_unreached(event.error.message);
-      reject(event.error.message);
-    };
-    reader.readAsText(file);
+async_test(function() {
+  var file = new File(["bits"], "dummy", { 'type': 'text/plain', lastModified: 42 });
+  var reader = new FileReader();
+  reader.onload = this.step_func_done(function() {
+    assert_equals(file.name, "dummy", "file name");
+    assert_equals(reader.result, "bits", "file content");
+    assert_equals(file.lastModified, 42, "file lastModified");
   });
+  reader.onerror = this.unreached_func("Unexpected error event");
+  reader.readAsText(file);
 }, "FileReader in Worker");
 
 done();

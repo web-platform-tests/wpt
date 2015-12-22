d471a8e1ca16658797a7ac25e657a08a490cdc80
Koji Ishii <kojiishi@gmail.com>
2015-12-23 01:50:43 +0900

    [css-writing-modes-3] tools/generators/text-orientation-generator.js refactoring

--- css-writing-modes-3/tools/generators/gulpfile.js
+++ css-writing-modes-3/tools/generators/gulpfile.js
@@ -1,66 +1,92 @@
+'use strict';
+
+var browserSync = null;
 var gulp = require("gulp");
 var ejs = require("gulp-ejs");
 var rename = require("gulp-rename");
 var minimist = require('minimist');
 var argv = minimist(process.argv.slice(2));
-var orientation = require("./text-orientation-generator.js");
 
 gulp.task("default", [
-    "orthogonal-parent-shrink-to-fit",
-    "text-orientation-ref",
-    "text-orientation-script"]);
+  "orthogonal-parent-shrink-to-fit",
+  "text-orientation",
+]);
 
-gulp.task("test", ["server", "watch"]);
+gulp.task("test", ["browser-sync", "watch"]);
 
 gulp.task("watch", function () {
-    gulp.watch("orthogonal-parent-shrink-to-fit.ejs", ["orthogonal-parent-shrink-to-fit"]);
-    gulp.watch("text-orientation-generator.js", ["text-orientation-ref", "text-orientation-script"]);
-    gulp.watch("text-orientation-ref.ejs", ["text-orientation-ref"]);
-    gulp.watch("text-orientation-script.ejs", ["text-orientation-script"]);
+  gulp.watch("orthogonal-parent-shrink-to-fit.ejs", ["orthogonal-parent-shrink-to-fit"]);
+  gulp.watch(
+    [
+      "text-orientation-ref.ejs",
+      "text-orientation-script.ejs",
+      "text-orientation-generator.js",
+      "unicode-data.js",
+    ],
+    ["text-orientation"])
+    .on("change", file => delete require.cache[require.resolve(file.path)]);
 });
 
+gulp.task("browser-sync", function () {
+  if (!browserSync)
+    browserSync = require("browser-sync");
+  browserSync({
+    server: {
+      baseDir: "../../..",
+      directory: true,
+    },
+    startPath: "css-writing-modes-3/",
+  });
+});
+
+function reload() {
+  if (browserSync)
+    browserSync.reload();
+}
+
 gulp.task("server", function () {
-    var connect = require("connect");
-    var serveIndex = require("serve-index");
-    var serveStatic = require("serve-static");
-    var directory = "../../..";
-    var port = 8000;
-    connect()
-        .use(serveIndex(directory))
-        .use(serveStatic(directory))
-        .listen(port);
-    console.log("Listening on port " + port);
+  var connect = require("connect");
+  var serveIndex = require("serve-index");
+  var serveStatic = require("serve-static");
+  var directory = "../../..";
+  var port = 8000;
+  connect()
+    .use(serveIndex(directory))
+    .use(serveStatic(directory))
+    .listen(port);
+  console.log("Listening on port " + port);
 })
 
 gulpTaskFromTemplateWithAffixes("orthogonal-parent-shrink-to-fit", "-001", -1, 24);
 
-gulp.task("text-orientation-ref", function () {
-    return orientation.generateRefTest(argv);
-});
-
-gulp.task("text-orientation-script", function () {
-    return orientation.generate(argv);
+gulp.task("text-orientation", function () {
+  var orientation = require("./text-orientation-generator.js");
+  Promise.all([
+    orientation.generate(argv),
+    orientation.generateRefTest(argv),
+  ]).then(reload);
 });
 
 function gulpTaskFromTemplateWithAffixes(name, suffix, min, lim) {
-    if (argv.nocombo && min < 0)
-        min = 0;
-    if (argv.nochild && lim > 0)
-        lim = 0;
-    gulp.task(name, function () {
-        for (var i = min; i < lim; ++i) {
-            gulp.src(name + ".ejs")
-                .pipe(ejs({ index: i }))
-                .pipe(rename(name + suffix + affixFromIndex(i) + ".html"))
-                .pipe(gulp.dest("../.."));
-        }
-    });
+  if (argv.nocombo && min < 0)
+    min = 0;
+  if (argv.nochild && lim > 0)
+    lim = 0;
+  gulp.task(name, function () {
+    for (var i = min; i < lim; ++i) {
+      gulp.src(name + ".ejs")
+        .pipe(ejs({ index: i }))
+        .pipe(rename(name + suffix + affixFromIndex(i) + ".html"))
+        .pipe(gulp.dest("../.."));
+    }
+    reload();
+  });
 }
 
 function affixFromIndex(index) {
-    if (index < 0)
-        return "";
-    if (index >= 26)
-        throw new Error("Affix index too large (" + index + ")");
-    return String.fromCharCode("a".charCodeAt(0) + index);
+  if (index < 0)
+    return "";
+  if (index >= 26)
+    throw new Error("Affix index too large (" + index + ")");
+  return String.fromCharCode("a".charCodeAt(0) + index);
 }

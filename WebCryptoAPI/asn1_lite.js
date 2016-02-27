29beae8c3b3dc8fe048bff97cad1e8fdb7d77a28
Jim Schaad <ietf@augustcellars.com>
2016-02-26 17:00:35 -0800

    Update as we now build all of the import structures
    Make life more understandable as all import structures are built from
    scratch

--- WebCryptoAPI/asn1_lite.js
+++ WebCryptoAPI/asn1_lite.js
@@ -51,3 +51,68 @@ function asn1_to_fields(asn1Data)
     var x = new Uint8Array(asn1Data)
     return asn1_recurse_fields(x, 0, x.length);
 }
+
+function asn1_to_uint8(dataArray)
+{
+    var c = 0;
+    var item;
+    
+    dataArray.forEach(function(item) { c += item.length; });
+
+    var asn = new Uint8Array(c);
+    var i = 0;
+
+    dataArray.forEach(function(item) {
+        asn.set(item, i);
+        i += item.length;
+    });
+
+    return asn;
+}
+
+function asn1_encode_length(dataLength)
+{
+    if (dataLength < 0x80) {
+        return new Uint8Array([dataLength]);
+    }
+    else if (dataLength < 256) {
+        return new Uint8Array([0x81, dataLength]);
+    }
+    else if (dataLength < 65536) {
+        return new Uint8Array([0x82, dataLength/256, dataLength % 256]);
+    }
+    return Uint8Array([0x83, dataLength/65536, (dataLength/256)%256, dataLength%256]);
+}
+
+function asn1_encode(tag, dataArray)
+{
+    var data = asn1_to_uint8(dataArray);
+    var length = asn1_encode_length(data.length);
+
+    var result = [new Uint8Array([tag]), length, data];
+
+    return result;
+}
+
+function asn1_encode_bitstring(dataArray)
+{
+    var data = asn1_to_uint8(dataArray);
+    var length = asn1_encode_length(data.length+1);
+
+    var result = [new Uint8Array([0x03]), length, new Uint8Array([0]), data];
+
+    return result;
+}
+
+function asn1_encode_integer(dataArray)
+{
+    var data = asn1_to_uint8(dataArray);
+    if (data[0] > 0x7f) {
+        data = asn1_to_uint8([new Uint8Array([0]), data]);
+    }
+    var length = asn1_encode_length(data.length);
+
+    var result = [new Uint8Array([0x02]), length, data];
+
+    return result;
+}

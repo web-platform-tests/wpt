cdfdb1a28bf86441c18762e4f07715b82fc517da
Brad Hill <hillbrad@gmail.com>
2016-04-04 12:23:06 -0700

    add timeouts for when logs/alerts don't fire

--- content-security-policy/support/alertAssert.sub.js
+++ content-security-policy/support/alertAssert.sub.js
@@ -1,5 +1,9 @@
 // note, this template substitution is XSS, but no way to avoid it in this framework
 var expected_alerts = {{GET[alerts]}};
+var timeout= "{{GET[timeout]}}";
+if (timeout == "") {
+  timeout = 2;
+}
 
 if(expected_alerts.length == 0) {
   function alert_assert(msg) {
@@ -7,6 +11,12 @@ if(expected_alerts.length == 0) {
  }
 } else {
  var t_alert = async_test('Expecting alerts: {{GET[alerts]}}');
+ setTimeout(function() {
+   if(t_alert.phase != t_alert.phases.COMPLETE) {
+     t_alert.step(function() { assert_unreached('Alert timeout, expected alerts {{GET[alerts]}} not fired.') });
+     t_alert.done();
+    }
+ }, timeout * 100); 
  var alert_assert = function (msg) {
      t_alert.step(function () {
          if(msg && msg instanceof Error) {

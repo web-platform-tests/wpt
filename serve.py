42759b349436e91eba7134a5fff89dcc0f33334a
Quinten Yearsley <qyearsley@google.com>
2016-04-27 14:30:46 -0700

    Added a warning to serve.py when tools.serve cannot be imported

--- serve.py
+++ serve.py
@@ -1,4 +1,11 @@
-from tools.serve import serve
+import sys
+
+try:
+  from tools.serve import serve
+except ImportError:
+    print("tools.serve not found.  Did you forget to run "
+          '"git submodule update --init --recursive"?')
+    sys.exit(2)
 
 def main():
     serve.main()

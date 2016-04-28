2e1dc89393faa11c4a5bc65a37392f0c04b6f9db
Quinten Yearsley <qyearsley@google.com>
2016-04-28 13:30:49 -0700

    Fix indentation + use logging instead of print.

--- serve.py
+++ serve.py
@@ -1,10 +1,11 @@
 import sys
+import logging
 
 try:
-  from tools.serve import serve
+    from tools.serve import serve
 except ImportError:
-    print("tools.serve not found.  Did you forget to run "
-          '"git submodule update --init --recursive"?')
+    logging.error("tools.serve not found.  Did you forget to run "
+                  '"git submodule update --init --recursive"?')
     sys.exit(2)
 
 def main():

62dd272d3f839e9fd3ca4531cb7e823a4e649c46
Charles Engelke <git@engelke.com>
2016-05-11 17:11:08 -0400

    Removing interactive control of tests. Test harness can't work with it. Tests very slow again, due to always running RSA.

--- WebCryptoAPI/generateKey/failures.worker.js
+++ WebCryptoAPI/generateKey/failures.worker.js
@@ -1,4 +1,5 @@
 importScripts("/resources/testharness.js");
+importScripts("../util/helper.js");
 importScripts("failures.js");
 run_test();
 done();
--- WebCryptoAPI/generateKey/successes.worker.js
+++ WebCryptoAPI/generateKey/successes.worker.js
@@ -1,4 +1,5 @@
 importScripts("/resources/testharness.js");
+importScripts("../util/helper.js");
 importScripts("successes.js");
 run_test();
 done();

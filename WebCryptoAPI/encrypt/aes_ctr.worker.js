c96e056b302bd56d1d1b8ee4eb23817bb9635840
Charles Engelke <git@engelke.com>
2016-06-01 17:01:04 -0400

    Added AES-CTR tests

---
+++ WebCryptoAPI/encrypt/aes_ctr.worker.js
@@ -0,0 +1,8 @@
+importScripts("/resources/testharness.js");
+importScripts("aes_ctr_vectors.js");
+importScripts("aes_ctr.js");
+
+run_test();
+
+// Give all the promise_tests a little time, then tell the framework we're done.
+step_timeout(function() {done();}, 1000);

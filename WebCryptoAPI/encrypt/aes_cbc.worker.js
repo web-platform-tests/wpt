81d2a12881c574e6678ec9c1907c2b1e0c3ad1f5
Charles Engelke <git@engelke.com>
2016-06-01 16:25:49 -0400

    Worker and timeouts fixed

--- WebCryptoAPI/encrypt/aes_cbc.worker.js
+++ WebCryptoAPI/encrypt/aes_cbc.worker.js
@@ -1,5 +1,8 @@
 importScripts("/resources/testharness.js");
+importScripts("aes_cbc_vectors.js");
 importScripts("aes_cbc.js");
 
 run_test();
-done();
+
+// Give all the promise_tests a little time, then tell the framework we're done.
+step_timeout(function() {done();}, 1000);

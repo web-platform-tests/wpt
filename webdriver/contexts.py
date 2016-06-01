8009a968ea320392dc585ea734bef8c2fdd0e2d3
Andreas Tolfsen <ato@mozilla.com>
2016-06-01 19:33:48 +0100

    Lint webdriver resize (#3102)
    * webdriver: lint window resize test
    
    No functional changes.
    
    * webdriver: rename resize.py to contexts.py
    
    We ought to name files after the corresponding chapters in the
    specification.

--- webdriver/contexts.py
+++ webdriver/contexts.py
@@ -1,20 +1,14 @@
-def test_init(session):
-
-    session.start()
-
 def test_resize(session):
-
     # setting the window size by webdriver is synchronous
     # so we should see the results immediately
 
     session.window.size = (200, 100)
-    assert session.window.size == { "width": 100, "height": 200 }
+    assert session.window.size == {"width": 100, "height": 200}
 
     session.window.size = (100, 200)
-    assert session.window.size == { "width": 200, "height": 100 }
+    assert session.window.size == {"width": 200, "height": 100}
 
 def test_resize_by_script(session):
-
     # setting the window size by JS is asynchronous
     # so we poll waiting for the results
 
@@ -22,11 +16,12 @@ def test_resize_by_script(session):
 
     session.execute_script("window.resizeTo(100, 200)")
     size1 = session.window.size
-    while (size0 == size1): size1 = session.window.size
-    assert size1 == { "width": 100, "height": 200 }
+    while size0 == size1:
+        size1 = session.window.size
+    assert size1 == {"width": 100, "height": 200}
 
     session.execute_script("window.resizeTo(200, 100)")
     size2 = session.window.size
-    while (size1 == size2): size2 = session.window.size
-    assert size2 == { "width": 200, "height": 100 }
-
+    while size1 == size2:
+        size2 = session.window.size
+    assert size2 == {"width": 200, "height": 100}

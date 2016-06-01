14bf5501644b1c1fec69c190447c99c476427e4c
Charles Engelke <git@engelke.com>
2016-06-01 16:25:49 -0400

    Worker and timeouts fixed

--- WebCryptoAPI/encrypt/aes_gcm.worker.js
+++ WebCryptoAPI/encrypt/aes_gcm.worker.js
@@ -1,5 +1,8 @@
 importScripts("/resources/testharness.js");
+importScripts("aes_gcm_vectors.js");
 importScripts("aes_gcm.js");
 
 run_test();
-done();
+
+// Give all the promise_tests a little time, then tell the framework we're done.
+step_timeout(function() {done();}, 1000);

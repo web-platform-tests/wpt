59174dbffe1afa75705b83aec09d75f9affe38e7
Charles Engelke <git@engelke.com>
2016-06-07 16:38:41 -0400

    Properly detect test completion and call done() method.

--- WebCryptoAPI/encrypt_decrypt/aes_cbc.worker.js
+++ WebCryptoAPI/encrypt_decrypt/aes_cbc.worker.js
@@ -3,6 +3,3 @@ importScripts("aes_cbc_vectors.js");
 importScripts("aes.js");
 
 run_test();
-
-// Give all the promise_tests a little time, then tell the framework we're done.
-step_timeout(function() {done();}, 1000);

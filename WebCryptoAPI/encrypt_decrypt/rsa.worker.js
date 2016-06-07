42192a94c4678bebed520f64f9f5adaeb409947a
Charles Engelke <git@engelke.com>
2016-06-07 16:45:20 -0400

    Removed step_timeout to detect completion.

--- WebCryptoAPI/encrypt_decrypt/rsa.worker.js
+++ WebCryptoAPI/encrypt_decrypt/rsa.worker.js
@@ -3,6 +3,3 @@ importScripts("rsa_vectors.js");
 importScripts("rsa.js");
 
 run_test();
-
-// Give all the promise_tests a little time, then tell the framework we're done.
-step_timeout(function() {done();}, 1000);

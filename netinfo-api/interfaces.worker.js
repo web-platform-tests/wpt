177d68b0994109ee3260c8592c8d9aaff7648ac2
Zhiqiang Zhang <yanshan.zzq@alibaba-inc.com>
2016-07-26 15:29:54 +0800

    Create idlharness tests for Network Information API
    https://wicg.github.io/netinfo/

---
+++ netinfo-api/interfaces.worker.js
@@ -0,0 +1,26 @@
+"use strict";
+
+importScripts("/resources/testharness.js");
+importScripts("/resources/WebIDLParser.js", "/resources/idlharness.js");
+
+var request = new XMLHttpRequest();
+request.open("GET", "interfaces.idl");
+request.send();
+request.onload = function() {
+  var idlArray = new IdlArray();
+  var idls = request.responseText;
+
+  idlArray.add_untested_idls("interface WorkerGlobalScope {};");
+  idlArray.add_untested_idls("interface WorkerNavigator {};");
+  idlArray.add_untested_idls("interface Event {};");
+  idlArray.add_untested_idls("interface EventTarget {};");
+
+  idlArray.add_idls(idls);
+
+  idlArray.add_objects({
+    Navigator: ['navigator'],
+    NetworkInformation: ['navigator.connection']
+  });
+  idlArray.test();
+  done();
+};

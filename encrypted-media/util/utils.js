dd391dc0555479644c25ae731eda9b734da04ad7
Mark Watson <watsonm@netflix.com>
2016-07-27 13:19:45 -0700

    Updated utilities

--- encrypted-media/util/utils.js
+++ encrypted-media/util/utils.js
@@ -1,14 +1,27 @@
 function getInitData(initDataType) {
+
+    // FIXME: This is messed up, because here we are hard coding the key ids for the different content
+    //        that we use for clearkey testing: webm and mp4. For keyids we return the mp4 one
+    //
+    //        The content used with the DRM today servers has a different key id altogether
+
+    if (initDataType == 'webm') {
+      return new Uint8Array([
+          0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+          0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F
+      ]);
+    }
+
     if (initDataType == 'cenc') {
         return new Uint8Array([
-            0x00, 0x00, 0x00, 0x00, // size = 0
+            0x00, 0x00, 0x00, 0x34,   // size
             0x70, 0x73, 0x73, 0x68, // 'pssh'
             0x01, // version = 1
             0x00, 0x00, 0x00, // flags
             0x10, 0x77, 0xEF, 0xEC, 0xC0, 0xB2, 0x4D, 0x02, // Common SystemID
             0xAC, 0xE3, 0x3C, 0x1E, 0x52, 0xE2, 0xFB, 0x4B,
             0x00, 0x00, 0x00, 0x01, // key count
-            0x00, 0x00, 0x00, 0x00, 0x03, 0xd2, 0xfc, 0x41, // key
+            0x00, 0x00, 0x00, 0x00, 0x03, 0xd2, 0xfc, 0x41, // key id
             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
             0x00, 0x00, 0x00, 0x00 // datasize
         ]);
@@ -61,7 +74,7 @@ function createKeyIDs() {
 function getSupportedKeySystem() {
     var userAgent = navigator.userAgent.toLowerCase();
     var keysystem = undefined;
-    if(userAgent.indexOf('chrome') > -1) {
+    if(userAgent.indexOf('chrome') > -1 || userAgent.indexOf('firefox') > -1 ) {
         keysystem = 'com.widevine.alpha';
     }
 
@@ -107,4 +120,106 @@ function forceTestFailureFromPromise(test, error, message)
     test.force_timeout();
     test.done();
 }
+
+// Returns an array of audioCapabilities that includes entries for a set of
+// codecs that should cover all user agents.
+function getPossibleAudioCapabilities()
+{
+    return [
+        { contentType: 'audio/mp4; codecs="mp4a.40.2"' },
+        { contentType: 'audio/webm; codecs="opus"' },
+    ];
+}
+
+// Returns a trivial MediaKeySystemConfiguration that should be accepted,
+// possibly as a subset of the specified capabilities, by all user agents.
+function getSimpleConfiguration()
+{
+    return [ {
+        initDataTypes : [ 'webm', 'cenc', 'keyids' ],
+        audioCapabilities: getPossibleAudioCapabilities()
+    } ];
+}
+
+// Returns a MediaKeySystemConfiguration for |initDataType| that should be
+// accepted, possibly as a subset of the specified capabilities, by all
+// user agents.
+function getSimpleConfigurationForInitDataType(initDataType)
+{
+    return [ {
+        initDataTypes: [ initDataType ],
+        audioCapabilities: getPossibleAudioCapabilities()
+    } ];
+}
+
+// Returns a promise that is fulfilled with true if |initDataType| is supported,
+// by keysystem or false if not.
+function isInitDataTypeSupported(keysystem,initDataType)
+{
+    return navigator.requestMediaKeySystemAccess(
+                        keysystem, getSimpleConfigurationForInitDataType(initDataType))
+        .then(function() { return true; }, function() { return false; });
+}
+
+function getSupportedInitDataTypes( keysystem )
+{
+    return [ 'cenc', 'keyids', 'webm' ].filter( isInitDataTypeSupported.bind( null, keysystem ) );
+}
+
+function arrayBufferAsString(buffer)
+{
+    var array = [];
+    Array.prototype.push.apply( array, new Uint8Array( buffer ) );
+    return '0x' + array.map( function( x ) { return x < 16 ? '0'+x.toString(16) : x.toString(16); } ).join('');
+}
+
+function dumpKeyStatuses(keyStatuses)
+{
+    consoleWrite("for (var entry of keyStatuses)");
+    for (var entry of keyStatuses) {
+        consoleWrite(arrayBufferAsString(entry[0]) + ": " + entry[1]);
+    }
+    consoleWrite("for (var keyId of keyStatuses.keys())");
+    for (var keyId of keyStatuses.keys()) {
+        consoleWrite(arrayBufferAsString(keyId));
+    }
+    consoleWrite("for (var status of keyStatuses.values())");
+    for (var status of keyStatuses.values()) {
+        consoleWrite(status);
+    }
+    consoleWrite("for (var entry of keyStatuses.entries())");
+    for (var entry of keyStatuses.entries()) {
+        consoleWrite(arrayBufferAsString(entry[0]) + ": " + entry[1]);
+    }
+    consoleWrite("keyStatuses.forEach()");
+    keyStatuses.forEach(function(status, keyId) {
+        consoleWrite(arrayBufferAsString(keyId) + ": " + status);
+    });
+}
+
+// Verify that |keyStatuses| contains just the keys in |keys.expected|
+// and none of the keys in |keys.unexpected|. All keys should have status
+// 'usable'. Example call: verifyKeyStatuses(mediaKeySession.keyStatuses,
+// { expected: [key1], unexpected: [key2] });
+function verifyKeyStatuses(keyStatuses, keys)
+{
+    var expected = keys.expected || [];
+    var unexpected = keys.unexpected || [];
+
+    // |keyStatuses| should have same size as number of |keys.expected|.
+    assert_equals(keyStatuses.size, expected.length);
+
+    // All |keys.expected| should be found.
+    expected.map(function(key) {
+        assert_true(keyStatuses.has(key));
+        assert_equals(keyStatuses.get(key), 'usable');
+    });
+
+    // All |keys.unexpected| should not be found.
+    unexpected.map(function(key) {
+        assert_false(keyStatuses.has(key));
+        assert_equals(keyStatuses.get(key), undefined);
+    });
+}
+
 

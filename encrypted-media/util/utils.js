dca74b8dfc7cf1c16f97f2451bae89a9f2789495
Mark Watson <watsonm@netflix.com>
2016-07-27 10:43:13 -0700

    Use Widevine on Firefox

--- encrypted-media/util/utils.js
+++ encrypted-media/util/utils.js
@@ -74,7 +74,7 @@ function createKeyIDs() {
 function getSupportedKeySystem() {
     var userAgent = navigator.userAgent.toLowerCase();
     var keysystem = undefined;
-    if(userAgent.indexOf('chrome') > -1) {
+    if(userAgent.indexOf('chrome') > -1 || userAgent.indexOf('firefox') > -1 ) {
         keysystem = 'com.widevine.alpha';
     }
 

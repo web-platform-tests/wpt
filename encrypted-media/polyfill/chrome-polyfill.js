a87bbfb79356639855b12b0b4a250ce0bf757c02
Mark Watson <watsonm@netflix.com>
2016-07-29 20:50:41 -0700

    Fix lint issues

--- encrypted-media/polyfill/chrome-polyfill.js
+++ encrypted-media/polyfill/chrome-polyfill.js
@@ -1,8 +1,8 @@
 (function(){
     if(navigator.userAgent.toLowerCase().indexOf('chrome') > -1){
- 
+
         if ( ( /chrome\/([0-9]*)\./.exec( navigator.userAgent.toLowerCase() )[1] | 0 ) < 54 ) {
- 
+
             // Work around https://bugs.chromium.org/p/chromium/issues/detail?id=622956
             // Chrome does not fire the empty keystatuschange event when a session is closed
             var _mediaKeySessionClose = MediaKeySession.prototype.close;
@@ -14,22 +14,22 @@
                                             keys:   function() { return []; },
                                             values: function() { return []; },
                                             forEach:function() { return; } };
-     
+
             MediaKeySession.prototype.close = function close()
             {
                 this.__closed = true;
- 
+
                 setTimeout( function() {
                         this.dispatchEvent( new Event( 'keystatuseschange' ) );
                 }.bind( this ), 0 );
 
                 return _mediaKeySessionClose.call( this );
             };
-     
+
             Object.defineProperty( MediaKeySession.prototype, 'keyStatuses', { get: function() {
-            
+
                 return this.__closed ? _emptyMediaKeyStatusMap : _mediaKeySessionKeyStatusesGetter.call( this );
-            
+
             } } );
         }
     }

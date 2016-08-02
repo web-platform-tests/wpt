970ed213cb93a87a2915ece502f5d66af55addad
Mark Watson <watsonm@netflix.com>
2016-08-02 14:23:30 -0700

    Address review comments, simplify to single-key case

--- encrypted-media/scripts/playback-temporary-waitingforkey.js
+++ encrypted-media/scripts/playback-temporary-waitingforkey.js
@@ -1,8 +1,12 @@
 function runTest(config) {
 
+    // config.initData contains a list of keys. We expect those to be needed in order and get
+    // one waitingforkey for each one.
+
     var testname = config.keysystem + ', successful playback, temporary, '
                                     + /video\/([^;]*)/.exec( config.videoType )[ 1 ]
-                                    + ', waitingforkey event';
+                                    + ', waitingforkey event, '
+                                    + config.initData.length + ' key' + ( config.initData.length > 1 ? 's' : '' );
 
     var configuration = {   initDataTypes: [ config.initDataType ],
                             audioCapabilities: [ { contentType: config.audioType } ],
@@ -16,52 +20,30 @@ function runTest(config) {
             _mediaKeySessions = [ ],
             _mediaSource;
 
+        function onFailure( error ) {
+            forceTestFailureFromPromise(test, error);
+        }
+
         function onMessage(event) {
-            assert_any( assert_equals, event.target, _mediaKeySessions );
-            assert_true( event instanceof window.MediaKeyMessageEvent );
-            assert_equals( event.type, 'message');
-
-            assert_any( assert_equals,
-                        event.messageType,
-                        [ 'license-request', 'individualization-request' ] );
-
+        
             config.messagehandler( config.keysystem, event.messageType, event.message )
             .then( function( response ) {
-
                 event.target.update( response )
-                .catch(function(error) {
-                    forceTestFailureFromPromise(test, error);
-                });
+                .catch(onFailure);
             });
         }
 
         function onWaitingForKey(event) {
-
-            // Not using waitForEventAndRunStep() to avoid too many
-            // EVENT(onTimeUpdate) logs.
-            _video.addEventListener('timeupdate', onTimeupdate, true);
-
-            // Create sessions now if we do not have them
-            if ( _mediaKeySessions.length === 0 ) {
-                config.initData.forEach( function( initData ) {
-
-                    var mediaKeySession = _mediaKeys.createSession( 'temporary' );
-
-                    waitForEventAndRunStep('message', mediaKeySession, onMessage, test);
-
-                    _mediaKeySessions.push( mediaKeySession );
-
-                    mediaKeySession.generateRequest( config.initDataType, initData )
-                    .catch(function(error) {
-                        forceTestFailureFromPromise(test, error);
-                    });
-                } );
-            }
+            assert_less_than( _mediaKeySessions.length, config.initData.length );
+            var mediaKeySession = _mediaKeys.createSession( 'temporary' );
+            waitForEventAndRunStep('message', mediaKeySession, onMessage, test);
+            _mediaKeySessions.push( mediaKeySession );
+            mediaKeySession.generateRequest( config.initDataType, config.initData[ _mediaKeySessions.length - 1 ] )
+            .catch(onFailure);
         }
 
         function onTimeupdate(event) {
             if ( _video.currentTime > ( config.duration || 5 ) ) {
-
                 _video.removeEventListener('timeupdate', onTimeupdate);
                 _video.pause();
                 test.done();
@@ -72,19 +54,18 @@ function runTest(config) {
             return access.createMediaKeys();
         }).then(function(mediaKeys) {
             _mediaKeys = mediaKeys;
-
             _video.setMediaKeys(_mediaKeys);
-
             waitForEventAndRunStep('waitingforkey', _video, onWaitingForKey, test);
-
+            
+            // Not using waitForEventAndRunStep() to avoid too many
+            // EVENT(onTimeUpdate) logs.
+            _video.addEventListener('timeupdate', onTimeupdate, true);
         }).then(function() {
             return testmediasource(config);
         }).then(function(source) {
             _mediaSource = source;
             _video.src = URL.createObjectURL(_mediaSource);
             _video.play();
-        }).catch(function(error) {
-            forceTestFailureFromPromise(test, error);
-        });
+        }).catch(onFailure);
     }, testname);
 }

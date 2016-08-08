47df25d75a3089242dce61e469644cb8db8aae06
Ms2ger <Ms2ger@gmail.com>
2016-08-08 11:10:05 +0200

    Test for parse errors in test_file_lints.py.

--- lint/tests/test_file_lints.py
+++ lint/tests/test_file_lints.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from ..lint import check_file_contents
+import os
 import pytest
 import six
 
@@ -8,10 +9,12 @@ INTERESTING_FILE_NAMES = {
     "python": [
         "test.py",
     ],
-    "web": [
+    "web-lax": [
         "test.htm",
         "test.html",
         "test.js",
+    ],
+    "web-strict": [
         "test.svg",
         "test.xht",
         "test.xhtml",
@@ -21,40 +24,56 @@ INTERESTING_FILE_NAMES = {
 def check_with_files(input_bytes):
     return {
         filename: (check_file_contents("", filename, six.BytesIO(input_bytes)), kind)
-        for (kind, filenames) in INTERESTING_FILE_NAMES.items()
-        for filename in filenames
+        for (filename, kind) in
+        (
+            (os.path.join("html", filename), kind)
+            for (kind, filenames) in INTERESTING_FILE_NAMES.items()
+            for filename in filenames
+        )
     }
 
 
 def test_trailing_whitespace():
     error_map = check_with_files(b"test; ")
 
-    for (filename, (errors, _)) in error_map.items():
-        assert errors == [("TRAILING WHITESPACE", "Whitespace at EOL", filename, 1)]
+    for (filename, (errors, kind)) in error_map.items():
+        expected = [("TRAILING WHITESPACE", "Whitespace at EOL", filename, 1)]
+        if kind == "web-strict":
+            expected.append(("PARSE-FAILED", "Unable to parse file", filename, None))
+        assert errors == expected
 
 
 def test_indent_tabs():
     error_map = check_with_files(b"def foo():\n\x09pass")
 
-    for (filename, (errors, _)) in error_map.items():
-        assert errors == [("INDENT TABS", "Tabs used for indentation", filename, 2)]
+    for (filename, (errors, kind)) in error_map.items():
+        expected = [("INDENT TABS", "Tabs used for indentation", filename, 2)]
+        if kind == "web-strict":
+            expected.append(("PARSE-FAILED", "Unable to parse file", filename, None))
+        assert errors == expected
 
 
 def test_cr_not_at_eol():
     error_map = check_with_files(b"line1\rline2\r")
 
-    for (filename, (errors, _)) in error_map.items():
-        assert errors == [("CR AT EOL", "CR character in line separator", filename, 1)]
+    for (filename, (errors, kind)) in error_map.items():
+        expected = [("CR AT EOL", "CR character in line separator", filename, 1)]
+        if kind == "web-strict":
+            expected.append(("PARSE-FAILED", "Unable to parse file", filename, None))
+        assert errors == expected
 
 
 def test_cr_at_eol():
     error_map = check_with_files(b"line1\r\nline2\r\n")
 
-    for (filename, (errors, _)) in error_map.items():
-        assert errors == [
+    for (filename, (errors, kind)) in error_map.items():
+        expected = [
             ("CR AT EOL", "CR character in line separator", filename, 1),
             ("CR AT EOL", "CR character in line separator", filename, 2),
         ]
+        if kind == "web-strict":
+            expected.append(("PARSE-FAILED", "Unable to parse file", filename, None))
+        assert errors == expected
 
 
 def test_w3c_test_org():
@@ -64,6 +83,8 @@ def test_w3c_test_org():
         expected = [("W3C-TEST.ORG", "External w3c-test.org domain used", filename, 1)]
         if kind == "python":
             expected.append(("PARSE-FAILED", "Unable to parse file", filename, 1))
+        elif kind == "web-strict":
+            expected.append(("PARSE-FAILED", "Unable to parse file", filename, None))
         assert errors == expected
 
 
@@ -74,21 +95,22 @@ def test_webidl2_js():
         expected = [("WEBIDL2.JS", "Legacy webidl2.js script used", filename, 1)]
         if kind == "python":
             expected.append(("PARSE-FAILED", "Unable to parse file", filename, 1))
+        elif kind == "web-strict":
+            expected.append(("PARSE-FAILED", "Unable to parse file", filename, None))
         assert errors == expected
 
 
 def test_console():
-    error_map = check_with_files(b"console.log('error');\nconsole.error ('log')\n")
+    error_map = check_with_files(b"<script>\nconsole.log('error');\nconsole.error ('log')\n</script>")
 
     for (filename, (errors, kind)) in error_map.items():
-        if kind == "web":
+        if kind in ["web-lax", "web-strict"]:
             assert errors == [
-                ("CONSOLE", "Console logging API used", filename, 1),
                 ("CONSOLE", "Console logging API used", filename, 2),
+                ("CONSOLE", "Console logging API used", filename, 3),
             ]
         else:
-            assert errors == []
-
+            assert errors == [("PARSE-FAILED", "Unable to parse file", filename, 1)]
 
 @pytest.mark.skipif(six.PY3, reason="Cannot parse print statements from python 3")
 def test_print_statement():
@@ -99,6 +121,10 @@ def test_print_statement():
             assert errors == [
                 ("PRINT STATEMENT", "Print function used", filename, 2),
                 ("PRINT STATEMENT", "Print function used", filename, 3),
+            ]
+        elif kind == "web-strict":
+            assert errors == [
+                ("PARSE-FAILED", "Unable to parse file", filename, None),
             ]
         else:
             assert errors == []
@@ -111,6 +137,10 @@ def test_print_function():
         if kind == "python":
             assert errors == [
                 ("PRINT STATEMENT", "Print function used", filename, 2),
+            ]
+        elif kind == "web-strict":
+            assert errors == [
+                ("PARSE-FAILED", "Unable to parse file", filename, None),
             ]
         else:
             assert errors == []

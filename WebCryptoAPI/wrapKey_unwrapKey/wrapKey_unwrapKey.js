e0b0f240452ae12eb89ab70867bdab1d5016af4f
Charles Engelke <github@engelke.com>
2016-08-23 18:57:49 -0700

    Fillout out test bodies

--- WebCryptoAPI/wrapKey_unwrapKey/wrapKey_unwrapKey.js
+++ WebCryptoAPI/wrapKey_unwrapKey/wrapKey_unwrapKey.js
@@ -49,19 +49,49 @@ function run_test() {
 
 
     function generateKeysToWrap() {
-        var generators = [];
+        var parameters = [
+            {algorithm: {name: "RSASSA-PKCS1-v1_5", modulusLength: 1024, publicExponent: new Uint8Array([1,0,1]), hash: "SHA-256"}, usages: ["sign", "verify"]},
+            {algorithm: {name: "RSA-PSS", modulusLength: 1024, publicExponent: new Uint8Array([1,0,1]), hash: "SHA-256"}, usages: ["sign", "verify"]},
+            {algorithm: {name: "RSA-OAEP", modulusLength: 1024, publicExponent: new Uint8Array([1,0,1]), hash: "SHA-256"}, usages: ["encrypt", "decrypt"]},
+            {algorithm: {name: "ECDSA", namedCurve: "P-256"}, usages: ["sign", "verify"]},
+            {algorithm: {name: "ECDH", namedCurve: "P-256"}, usages: ["deriveBits"]},
+            {algorithm: {name: "AES-CTR", length: 128}, usages: ["encrypt", "decrypt"]},
+            {algorithm: {name: "AES-CBC", length: 128}, usages: ["encrypt", "decrypt"]},
+            {algorithm: {name: "AES-GCM", length: 128}, usages: ["encrypt", "decrypt"]},
+            {algorithm: {name: "AES-KW", length: 128}, usages: ["wrapKey", "unwrapKey"]},
+            {algorithm: {name: "HMAC", length: 128, hash: "SHA-256"}, usages: ["sign", "verify"]}
+        ];
 
-        return new Promise((resolve, reject) => {
-            resolve("okay");
-        });
-
-        //generators.push(subtle.generateKey().then());
-
-        return Promise.all(generators);
+        return Promise.all(parameters.map(function(params) {
+            return subtle.generateKey(params.algorithm, true, params.usages)
+            .then(function(result) {
+                if (result.constructor === CryptoKey) {
+                    keys.push({name: params.algorithm.name, key: result});
+                } else {
+                    keys.push({name: params.algorithm.name + " public key", key: result.publicKey});
+                    keys.push({name: params.algorithm.name + " private key", key: result.privateKey});
+                }
+                return true;
+            });
+        }));
     }
 
 
-    function testWrapping(wrapper, key) {
+    function testWrapping(wrapper, toWrap) {
         var x;
+        return;
+
+        // TODO: Fill out
+        promise_test(function(test) {
+            var originalJwk;
+            return subtle.exportKey("jwk", toWrap.key)
+            .then(function(jwk) {
+                originalJwk = jwk;
+            }).then(function() {
+                return subtle.wrapKey("jwk", toWrap.key, wrapper.key, wrapper.algorithm);
+            }).then(function(wrappedResult) {
+                return subtle.unwrapKey()
+            })
+        });
     }
 }

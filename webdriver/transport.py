c845a9e8b17a21b43a697f50f55cf4144f6dc926
Andreas Tolfsen <ato@mozilla.com>
2016-09-01 14:36:01 +0100

    remove unused libraries from transport

--- webdriver/transport.py
+++ webdriver/transport.py
@@ -2,12 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-
-import errno
 import httplib
 import json
-import socket
-import time
 import urlparse
 
 import error

1f2080f9ffa00ad27eb764b83fbc10960a95d6aa
mwatson2 <watsonm@netflix.com>
2016-09-16 08:31:23 -0700

    [eme] Updates to multi-session sequential tests (#3629)

--- encrypted-media/scripts/playback-temporary-multisession.js
+++ encrypted-media/scripts/playback-temporary-multisession.js
@@ -1,9 +1,11 @@
 function runTest(config,qualifier) {
 
+    // This test assumes one session is required for each provided initData
+
     var testname = testnamePrefix( qualifier, config.keysystem )
                                     + ', temporary, '
                                     + /video\/([^;]*)/.exec( config.videoType )[ 1 ]
-                                    + ', playback with multiple keys, single session, '
+                                    + ', playback with multiple sessions, '
                                     + config.testcase;
 
     var configuration = {   initDataTypes: [ config.initDataType ],
@@ -18,36 +20,8 @@ function runTest(config,qualifier) {
             _mediaKeySessions = [ ],
             _mediaSource;
 
-        function onEncrypted(event) {
-            assert_equals(event.target, _video);
-            assert_true(event instanceof window.MediaEncryptedEvent);
-            assert_equals(event.type, 'encrypted');
-
-            // Only create a session for the first encrypted event
-            if ( _mediaKeySessions.length > 0 ) return;
-
-            var mediaKeySession = _mediaKeys.createSession( 'temporary' );
-
-            waitForEventAndRunStep('message', mediaKeySession, onMessage, test);
-
-            var initDataType, initData;
-            if ( config.initDataType && config.initData )
-            {
-                initDataType = config.initDataType;
-                initData = config.initData;
-            }
-            else
-            {
-                initDataType = event.initDataType;
-                initData = event.initData;
-            }
-
-            _mediaKeySessions.push( mediaKeySession );
-
-            mediaKeySession.generateRequest( initDataType, initData )
-            .catch(function(error) {
-                forceTestFailureFromPromise(test, error);
-            });
+        function onFailure( error ) {
+            forceTestFailureFromPromise(test, error);
         }
 
         function onMessage(event) {
@@ -55,22 +29,14 @@ function runTest(config,qualifier) {
             assert_true( event instanceof window.MediaKeyMessageEvent );
             assert_equals( event.type, 'message');
 
-            assert_any( assert_equals,
-                        event.messageType,
-                        [ 'license-request', 'individualization-request' ] );
+            assert_in_array( event.messageType, [ 'license-request', 'individualization-request' ] );
 
-            config.messagehandler( event.messageType, event.message )
-            .then( function( response ) {
-
-                event.target.update( response )
-                .catch(function(error) {
-                    forceTestFailureFromPromise(test, error);
-                });
+            config.messagehandler( event.messageType, event.message ).then( function( response ) {
+                event.target.update( response ).catch(onFailure);
             });
         }
 
         function onPlaying(event) {
-
             // Not using waitForEventAndRunStep() to avoid too many
             // EVENT(onTimeUpdate) logs.
             _video.addEventListener('timeupdate', onTimeupdate, true);
@@ -78,10 +44,6 @@ function runTest(config,qualifier) {
 
         function onTimeupdate(event) {
             if ( _video.currentTime > ( config.duration || 2 ) ) {
-
-                consoleWrite("Session 0:");
-                dumpKeyStatuses( _mediaKeySessions[ 0 ].keyStatuses );
-
                 _video.removeEventListener('timeupdate', onTimeupdate);
                 _video.pause();
                 test.done();
@@ -92,20 +54,22 @@ function runTest(config,qualifier) {
             return access.createMediaKeys();
         }).then(function(mediaKeys) {
             _mediaKeys = mediaKeys;
-
-            _video.setMediaKeys(_mediaKeys);
-
-            waitForEventAndRunStep('encrypted', _video, onEncrypted, test);
+            return _video.setMediaKeys(_mediaKeys);
+        }).then(function() {
             waitForEventAndRunStep('playing', _video, onPlaying, test);
 
-        }).then(function() {
+            config.initData.forEach( function( initData ) {
+                var mediaKeySession = _mediaKeys.createSession( 'temporary' );
+                waitForEventAndRunStep('message', mediaKeySession, onMessage, test);
+                _mediaKeySessions.push( mediaKeySession );
+                mediaKeySession.generateRequest( config.initDataType, initData ).catch( onFailure );
+            } );
+
             return testmediasource(config);
         }).then(function(source) {
             _mediaSource = source;
             _video.src = URL.createObjectURL(_mediaSource);
             _video.play();
-        }).catch(function(error) {
-            forceTestFailureFromPromise(test, error);
-        });
+        }).catch(onFailure);
     }, testname);
 }

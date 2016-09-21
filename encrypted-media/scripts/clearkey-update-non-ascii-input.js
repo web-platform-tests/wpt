ceca1857d72f3e1267e5a390db1c0aca3a3c79c5
vju <volker.jung@fokus.fraunhofer.de>
2016-09-21 11:54:00 +0200

    encrypted-media-clearkey-update-non-ascii-input.html migration
    original jwk set in google/encrypted-media-clearkey-update-non-ascii-input.html is incorrect due to missing close brace of jwkset. New jwk set fails to non-ascii char.

---
+++ encrypted-media/scripts/clearkey-update-non-ascii-input.js
@@ -0,0 +1,42 @@
+//this test is only applicable to clearkey
+function runTest(config, qualifier) {
+    var testname = testnamePrefix(qualifier, config.keysystem) + ' test handling of non-ASCII responses for update()';
+
+    var configuration = getSimpleConfigurationForContent(config.content);
+
+    if (config.initDataType && config.initData) configuration.initDataTypes = [config.initDataType];
+
+    async_test(function (test) {
+        var initDataType;
+        var initData;
+        var mediaKeySession;
+
+        //clearkey JSON Web Key needs to be Ascii encoded
+        function processMessage(event) {
+                // |jwkSet| contains a  non-ASCII character \uDC00.
+                var jwkSet = '{"keys":[{'
+                    +     '"kty":"oct",'
+                    +     '"k":"MDEyMzQ1Njc4OTAxMjM0NQ",'
+                    +     '"kid":"MDEyMzQ1Njc4O\uDC00TAxMjM0NQ"'
+                    + '}]}';
+
+
+                event.target.update(stringToUint8Array(jwkSet)).then(function () {
+                    forceTestFailureFromPromise(test, 'Error: update() succeeded');
+                }, function (error) {
+                    assert_equals(error.name, 'InvalidAccessError');
+                    test.done();
+                });
+        }
+
+        navigator.requestMediaKeySystemAccess(config.keysystem, [configuration]).then(function (access) {
+            initDataType = access.getConfiguration().initDataTypes[0];
+            initData = getInitData(config.content, initDataType);
+            return access.createMediaKeys();
+        }).then(function (mediaKeys) {
+            mediaKeySession = mediaKeys.createSession();
+            waitForEventAndRunStep('message', mediaKeySession, processMessage, test);
+            return mediaKeySession.generateRequest(initDataType, initData);
+        })
+    }, testname);
+}

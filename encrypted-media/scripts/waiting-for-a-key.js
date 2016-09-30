9465a808bdc1a94f878991fcb8f9318298d2aa9a
Louay Bassbouss <louay.bassbouss@fokus.fraunhofer.de>
2016-09-30 18:08:15 +0200

    Encrypted Media Extensions: several updates in waiting-for-a-key.js

--- encrypted-media/scripts/waiting-for-a-key.js
+++ encrypted-media/scripts/waiting-for-a-key.js
@@ -1,18 +1,29 @@
-function runTest(config) {
+function runTest(config)
+{
     // For debugging timeouts, keep track of the number of the
     // various events received.
-    var debugEncryptedEventCount = 0;
-    var debugWaitingForKeyEventCount = 0;
+    var debugEncryptedEventFired = false;
+    var debugWaitingForKeyEventFired = false;
     var debugTimeUpdateEventCount = 0;
     var debugMessage = '';
+
+    // Set global option explicit_timeout to true and control
+    // the timeout in the promise test below.
+    setup({
+        explicit_timeout: true
+    });
 
     promise_test(function (test) {
         var video = config.video;
         var keysystem = config.keysystem;
         var configuration = {
             initDataTypes: [config.initDataType],
-            audioCapabilities: [{contentType: config.audioType}],
-            videoCapabilities: [{contentType: config.videoType}],
+            audioCapabilities: [{
+                contentType: config.audioType
+            }],
+            videoCapabilities: [{
+                contentType: config.videoType
+            }],
             sessionTypes: ['temporary']
         };
         var initData;
@@ -28,107 +39,106 @@ function runTest(config) {
 
         test.timeout = function () {
             var message = 'timeout. message = ' + debugMessage
-              + ', encrypted: ' + debugEncryptedEventCount
-              + ', waitingforkey: ' + debugWaitingForKeyEventCount
-              + ', timeupdate: ' + debugTimeUpdateEventCount;
+              + ', encrypted: ' + debugEncryptedEventFired
+              + ', waitingforkey: ' + debugWaitingForKeyEventFired
+              + ', timeupdate count: ' + debugTimeUpdateEventCount;
+            // It seems there is a bug in testharness: According to the function name and testharness
+            // docs: " it is possible to use test.force_timeout() in place of assert_unreached(),
+            // to immediately fail the test but with a status of TIMEOUT. ", but it doesn't fail the test
+            // immediately. If this will be fixed, then nothing after force_timeout will be executed.
+            // Need to check this again once the bug is fixed
             test.force_timeout();
             test.timeout_id = null;
-            test.set_status(2, message);
+            test.set_status(test.TIMEOUT, message);
             test.done();
         };
-        return navigator.requestMediaKeySystemAccess(keysystem, [configuration])
-          .then(function (access) {
-              debugMessage = 'createMediaKeys()';
-              return access.createMediaKeys();
-          })
-          .then(function (mediaKeys) {
+
+        return navigator.requestMediaKeySystemAccess(keysystem, [configuration]).then(function (access) {
+            debugMessage = 'createMediaKeys()';
+            return access.createMediaKeys();
+        }).then(function (mediaKeys) {
               debugMessage = 'setMediaKeys()';
               return video.setMediaKeys(mediaKeys);
-          })
-          .then(function () {
-              return testmediasource(config);
-          })
-          .then(function (source) {
-              debugMessage = 'wait_for_encrypted_event()';
-              mediaSource = source;
-              video.src = URL.createObjectURL(mediaSource);
-              video.play();
-              return wait_for_encrypted_event(video);
-          })
-          .then(function (e) {
-              // Received the 'encrypted' event(s), so keep a copy of
-              // the initdata for use when creating the session later.
-              initDataType = config.initData ? config.initDataType : e.initDataType;
-              initData = config.initData || e.initData;
-              // Wait until the video indicates that it needs a key to
-              // continue.
-              debugMessage = 'wait_for_waitingforkey_event()';
-              return wait_for_waitingforkey_event(video);
-          })
-          .then(function () {
-              // Make sure the video is NOT paused and not progressing
-              // before a key is provided. This requires the video
-              // to NOT have a clear lead.
-              assert_false(video.paused);
-              assert_equals(video.currentTime, 0);
-              // Create a session.
-              mediaKeySession = video.mediaKeys.createSession('temporary');
-              debugMessage = 'generateRequest()';
-              return mediaKeySession.generateRequest(initDataType, initData);
-          })
-          .then(function () {
-              // generateRequest() will cause a 'message' event to
-              // occur specifying the keyId that is needed
-              // Add the key needed to decrypt.
-              return wait_for_message_event(mediaKeySession, handler, test)
-          })
-          .then(function () {
-              // Video should start playing now that it can decrypt the
-              // streams, so wait until a little bit of the video has
-              // played.
-              debugMessage = 'wait_for_timeupdate_event()';
-              return wait_for_timeupdate_event(video);
-          })
-          .catch(function (error) {
-              forceTestFailureFromPromise(test, error);
-          });
+        }).then(function () {
+            return testmediasource(config);
+        }).then(function (source) {
+            debugMessage = 'wait_for_encrypted_event()';
+            mediaSource = source;
+            video.src = URL.createObjectURL(mediaSource);
+            video.play();
+            return wait_for_encrypted_event(video);
+        }).then(function (e) {
+            // Received the 'encrypted' event(s), so keep a copy of
+            // the initdata for use when creating the session later.
+            initDataType = config.initData ? config.initDataType : e.initDataType;
+            initData = config.initData || e.initData;
+            // Wait until the video indicates that it needs a key to
+            // continue.
+            debugMessage = 'wait_for_waitingforkey_event()';
+            return wait_for_waitingforkey_event(video);
+        }).then(function () {
+            // Make sure the video is NOT paused and not progressing
+            // before a key is provided. This requires the video
+            // to NOT have a clear lead.
+            assert_false(video.paused);
+            assert_less_than(video.currentTime, 0.2);
+            // Create a session.
+            mediaKeySession = video.mediaKeys.createSession('temporary');
+            debugMessage = 'generateRequest()';
+            return mediaKeySession.generateRequest(initDataType, initData);
+        }).then(function () {
+            // generateRequest() will cause a 'message' event to
+            // occur specifying the keyId that is needed
+            // Add the key needed to decrypt.
+            return wait_for_message_event(mediaKeySession, handler, test);
+        }).then(function () {
+            // Video should start playing now that it can decrypt the
+            // streams, so wait until a little bit of the video has
+            // played.
+            debugMessage = 'wait_for_timeupdate_event()';
+            return wait_for_timeupdate_event(video);
+        }).catch(function (error) {
+            assert_unreached('Error: '+error.name);
+        });
 
         // Typical test duration is 6 seconds on release builds
-        // (12 seconds on debug). Since the test is timing out anyway,
-        // make the duration 5 seconds so that the timeout function
-        // is actually called (instead of simply aborting the test).
-    }, 'Waiting for a key.', {timeout: 5000});
+        // (12 seconds on debug).
+    }, 'Waiting for a key.', {timeout: 20000});
 
-// Wait for an 'encrypted' event
-    function wait_for_encrypted_event(video) {
+    // Wait for an 'encrypted' event
+    function wait_for_encrypted_event(video)
+    {
         return new Promise(function (resolve) {
             video.addEventListener('encrypted', function listener(e) {
                 assert_equals(e.target, video);
                 assert_true(e instanceof window.MediaEncryptedEvent);
                 assert_equals(e.type, 'encrypted');
+                debugEncryptedEventFired = true;
                 video.removeEventListener('encrypted', listener);
                 resolve(e);
             });
         });
     };
 
-// Wait for a 'waitingforkey' event. Promise resolved when the
-// event is received.
-    function wait_for_waitingforkey_event(video) {
+    // Wait for a 'waitingforkey' event. Promise resolved when the
+    // event is received.
+    function wait_for_waitingforkey_event(video)
+    {
         return new Promise(function (resolve) {
             video.addEventListener('waitingforkey', function listener(e) {
                 assert_equals(e.target, video);
                 assert_equals(e.type, 'waitingforkey');
-                ++debugWaitingForKeyEventCount;
+                debugWaitingForKeyEventFired = true;
                 video.removeEventListener('waitingforkey', listener);
                 resolve(e);
             });
         });
     };
 
-// Wait for a 'timeupdate' event. Promise resolved if |video| has
-// played for more than 0.2 seconds.
-    function wait_for_timeupdate_event(video) {
+    // Wait for a 'timeupdate' event. Promise resolved if |video| has
+    // played for more than 0.2 seconds.
+    function wait_for_timeupdate_event(video)
+    {
         return new Promise(function (resolve) {
             video.addEventListener('timeupdate', function listener(e) {
                 assert_equals(e.target, video);
@@ -140,8 +150,10 @@ function runTest(config) {
             });
         });
     };
-// We need to wait for the message even if for non clearkey DRMs.
-    function wait_for_message_event(mediaKeySession, handler, test) {
+
+    // We need to wait for the message even if for non clearkey DRMs.
+    function wait_for_message_event(mediaKeySession, handler, test)
+    {
         return new Promise(function (resolve) {
             mediaKeySession.addEventListener('message', function listener(e) {
                 assert_equals(e.target, mediaKeySession);

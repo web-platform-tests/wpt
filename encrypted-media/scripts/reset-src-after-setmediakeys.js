768e0a34e9d8c280f597fe0ebcc6c1a601d079ac
Louay Bassbouss <louay.bassbouss@fokus.fraunhofer.de>
2016-09-30 15:37:02 +0200

    Encrypted Media Extensions: minor changes in reset-src-after-setmediakeys.js

--- encrypted-media/scripts/reset-src-after-setmediakeys.js
+++ encrypted-media/scripts/reset-src-after-setmediakeys.js
@@ -1,4 +1,5 @@
-function runTest(config) {
+function runTest(config)
+{
     async_test(function (test) {
         var mediaKeys;
         var mediaSource;
@@ -7,8 +8,12 @@ function runTest(config) {
         var keysystem = config.keysystem;
         var configuration = {
             initDataTypes: [config.initDataType],
-            audioCapabilities: [{contentType: config.audioType}],
-            videoCapabilities: [{contentType: config.videoType}],
+            audioCapabilities: [{
+                contentType: config.audioType
+            }],
+            videoCapabilities: [{
+                contentType: config.videoType
+            }],
             sessionTypes: ['temporary']
         };
 
@@ -16,50 +21,41 @@ function runTest(config) {
 
         var onEncrypted = function (event) {
             ++encryptedEventIndex;
-            assert_not_equals(video.mediaKeys, null);
-            assert_true(video.mediaKeys === mediaKeys);
+            assert_equals(video.mediaKeys, mediaKeys);
 
             // This event is fired once for the audio stream and once
             // for the video stream each time .src is set.
-            if (encryptedEventIndex == 2) {
+            if (encryptedEventIndex === 2) {
                 // Finished first video; Create new media source and wait for two more encrypted events
-                return testmediasource(config)
-                  .then(function (source) {
-                      video.src = URL.createObjectURL(source);
-                  })
-                  .catch(function (error) {
-                      forceTestFailureFromPromise(test, error)
-                  })
-            } else if (encryptedEventIndex == 4) {
+                return testmediasource(config).then(function (source) {
+                    video.src = URL.createObjectURL(source);
+                }).catch(function (error) {
+                    forceTestFailureFromPromise(test, error)
+                });
+            } else if (encryptedEventIndex === 4) {
                 // Finished second video.
                 test.done();
             }
         };
 
         // Create a MediaKeys object and assign it to video.
-        return navigator.requestMediaKeySystemAccess(keysystem, [configuration])
-          .then(function (access) {
-              assert_equals(access.keySystem, keysystem);
-              return access.createMediaKeys();
-          })
-          .then(function (result) {
-              mediaKeys = result;
-              assert_not_equals(mediaKeys, null);
-              return video.setMediaKeys(mediaKeys);
-          })
-          .then(function (result) {
-              assert_not_equals(video.mediaKeys, null, 'not set initially');
-              assert_true(video.mediaKeys === mediaKeys);
-              return testmediasource(config);
-          })
-          .then(function (source) {
-              waitForEventAndRunStep('encrypted', video, onEncrypted, test);
-              mediaSource = source;
-              video.src = URL.createObjectURL(mediaSource);
-          })
-          .catch(function (error) {
-              forceTestFailureFromPromise(test, error);
-          });
+        return navigator.requestMediaKeySystemAccess(keysystem, [configuration]).then(function (access) {
+            assert_equals(access.keySystem, keysystem);
+            return access.createMediaKeys();
+        }).then(function (result) {
+            mediaKeys = result;
+            assert_not_equals(mediaKeys, null);
+            return video.setMediaKeys(mediaKeys);
+        }).then(function (result) {
+            assert_equals(video.mediaKeys, mediaKeys);
+            return testmediasource(config);
+        }).then(function (source) {
+            waitForEventAndRunStep('encrypted', video, onEncrypted, test);
+            mediaSource = source;
+            video.src = URL.createObjectURL(mediaSource);
+        }).catch(function (error) {
+            forceTestFailureFromPromise(test, error);
+        });
 
     }, 'Reset src after setMediaKeys().');
 }

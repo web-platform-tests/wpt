9f431ab0742d04395776e6bdd05d931ac591a677
François Daoust <fd@tidoust.net>
2016-10-20 14:31:36 +0200

    Cast URL fixed and mention of w3c-test.org removed
    The lint tool complains about the fact that the test mentions w3c-test.org
    because tests should remain independent of the test server that hosts them,
    and should in particular never depend on something that directly or indirectly
    maps to a resource on the W3C test server.
    
    I dropped the reference (which appeared in a comment), but note the issue
    remains. One possible solution would be to have the Cast App ID turned into
    a configuration parameter of the test server. I'm not sure how to do that at
    this point though, so suggest to address that part later on.

--- presentation-api/controlling-ua/common.js
+++ presentation-api/controlling-ua/common.js
@@ -1,14 +1,18 @@
 (function (window) {
   // Cast ID of the main custom receiver application linked with the test suite
   // That application ID, maintained by W3C team, points at:
-  // https://w3c-test.org/presentation-api/controlling-ua/support/presentation.html
+  // https://[W3C test server]/presentation-api/controlling-ua/support/presentation.html
+  //
+  // NB: this mechanism should be improved later on as tests should not depend
+  // on something that directly or indirectly maps to a resource on the W3C test
+  // server.
   var castAppId = '915D2A2C';
 
   // NB: the Cast-friendly URL will likely need to be adjusted afterwards as
   // well, e.g. to use another scheme.
   var castClientId = String(new Date().getTime()) +
     String(Math.floor(Math.random() * 1e5));
-  var castUrl = 'http://google.com/cast/#' +
+  var castUrl = 'support/presentation.html#' +
     '__castAppId__=' + castAppId +
     '/__castClientId__=' + castClientId;
 

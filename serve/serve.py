9dc91ab4e181595e074a1dbb1408c84f80367896
TAMURA, Kent <tkent@chromium.org>
2016-10-25 17:07:20 +0900

    wptserve: Add capability to add url-directory mappings (#128)
    * wptserve: Add capability to add url-directory mappings
    
    Add "aliases" dict to a config json file. It looks like:
    
        "aliases" : {
            "/resources/testharnessreport.js" : "/Users/tkent/blink/LayoutTests/resources",
            "/wpt_automation/" : "/Users/tkent/blink/LayoutTests/imported/wpt_automation"
        },
    
    This feature is useful to override files in web-platform-tests or to inject
    additional resources. For example, browser vendor can replace its own
    testharnessreport.js without modifying web-platform-tests checkout.
    
    * Stop using line continuation characters.

--- serve/serve.py
+++ serve/serve.py
@@ -154,9 +154,23 @@ class RoutesBuilder(object):
                  b"%s%s" % (str(url_base) if url_base != "/" else "", str(suffix)),
                  handler_cls(base_path=path, url_base=url_base)))
 
+    def add_file_mount_point(self, file_url, base_path):
+        assert file_url.startswith("/")
+        url_base = file_url[0:file_url.rfind("/") + 1]
+        self.mountpoint_routes[file_url] = [("GET", file_url, handlers.FileHandler(base_path=base_path, url_base=url_base))]
 
-def default_routes():
-    return RoutesBuilder().get_routes()
+
+def build_routes(aliases):
+    builder = RoutesBuilder()
+    for url, directory in aliases.items():
+        if not url.startswith("/") or len(directory) == 0:
+            logger.error("A map entry of 'aliases' must be \"/<url-path>\": \"<local-directory>\"")
+            continue
+        if url.endswith("/"):
+            builder.add_mount_point(url, directory)
+        else:
+            builder.add_file_mount_point(url, directory)
+    return builder.get_routes()
 
 
 def setup_logger(level):
@@ -234,12 +248,12 @@ class ServerProc(object):
         return self.proc.is_alive()
 
 
-def check_subdomains(host, paths, bind_hostname, ssl_config):
+def check_subdomains(host, paths, bind_hostname, ssl_config, aliases):
     port = get_port()
     subdomains = get_subdomains(host)
 
     wrapper = ServerProc()
-    wrapper.start(start_http_server, host, port, paths, default_routes(), bind_hostname,
+    wrapper.start(start_http_server, host, port, paths, build_routes(aliases), bind_hostname,
                   None, ssl_config)
 
     connected = False
@@ -468,7 +482,7 @@ def start(config, ssl_environment, routes, **kwargs):
     ssl_config = get_ssl_config(config, external_config["domains"].values(), ssl_environment)
 
     if config["check_subdomains"]:
-        check_subdomains(host, paths, bind_hostname, ssl_config)
+        check_subdomains(host, paths, bind_hostname, ssl_config, config["aliases"])
 
     servers = start_servers(host, ports, paths, routes, bind_hostname, external_config,
                             ssl_config, **kwargs)
@@ -497,6 +511,9 @@ def set_computed_defaults(config):
     if not value_set(config, "ws_doc_root"):
         root = get_value_or_default(config, "doc_root", default=repo_root)
         config["ws_doc_root"] = os.path.join(root, "websockets", "handlers")
+
+    if not value_set(config, "aliases"):
+        config["aliases"] = {}
 
 
 def merge_json(base_obj, override_obj):
@@ -584,7 +601,7 @@ def main():
 
     with stash.StashServer((config["host"], get_port()), authkey=str(uuid.uuid4())):
         with get_ssl_environment(config) as ssl_env:
-            config_, servers = start(config, ssl_env, default_routes(), **kwargs)
+            config_, servers = start(config, ssl_env, build_routes(config["aliases"]), **kwargs)
 
             try:
                 while any(item.is_alive() for item in iter_procs(servers)):

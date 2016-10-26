3a651f6c6a9b719c7883c4d4c46c54c09fa20e79
Aryeh Gregor <ayg@aryeh.name>
2016-10-26 18:16:45 +0300

    Run one reflection test per property
    Blink is having trouble with these tests because they're too big, even
    after splitting up.  #4078 makes a slight dent, but it looks like my
    original intent was that each property should be one test, but I didn't
    wind up implementing it.  This cuts the number of tests run by a factor
    of something like 80 (e.g., 15,065 to 191 for embedded).
    
    Naturally, it has the disadvantage that if an implementation fails
    multiple assertions in a property, there will be no more test coverage
    for that property, neither for regressions nor standards conformance.
    In some cases a minor bug that's hard for a certain implementation to
    fix could indefinitely disable further test coverage for a wide array of
    properties.  As such, I'm not sure this is a good idea.  If the problem
    that Blink has is that the log messages are too verbose, perhaps they
    shouldn't print log lines for expected passes/fails, which I believe is
    what Mozilla does (speak to jgraham).
    
    If we do want this change, we could also probably recombine the test
    files.

--- html/dom/new-harness.js
+++ html/dom/new-harness.js
@@ -2,21 +2,23 @@
 // original-harness.js.  Polymorphism, kind of.
 ReflectionHarness.catchUnexpectedExceptions = false;
 
+ReflectionHarness.testWrapper = function(fun) {
+  test(fun, this.getTypeDescription());
+}
+
 ReflectionHarness.test = function(expected, actual, description) {
-  test(function() {
-    assert_equals(expected, actual);
-  }, this.getTypeDescription() + ": " + description);
+  assert_equals(expected, actual,
+                this.getTypeDescription() + ": " + description);
   // This is the test suite that will rate conformance, so we don't want to
   // bail out early if a test fails -- we want all tests to always run.
   return true;
 }
 
 ReflectionHarness.run = function(fun, description) {
-  test(fun, this.getTypeDescription() + ": " + description);
+  fun();
 }
 
 ReflectionHarness.testException = function(exceptionName, fn, description) {
-  test(function() {
-    assert_throws(exceptionName, fn);
-  }, this.getTypeDescription() + ": " + description);
+  assert_throws(exceptionName, fn,
+                this.getTypeDescription() + ": " + description);
 }

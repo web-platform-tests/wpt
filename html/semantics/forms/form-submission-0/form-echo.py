53981297ee1706a902d4ebd5c9a4196ae13cf574
Domenic Denicola <d@domenic.me>
2016-12-07 11:45:26 -0800

    Test the bytes more directly

--- html/semantics/forms/form-submission-0/form-echo.py
+++ html/semantics/forms/form-submission-0/form-echo.py
@@ -1,5 +1,7 @@
 def main(request, response):
+    bytes = bytearray(request.raw_input.read())
+    bytes_string = " ".join("%02x" % b for b in bytes)
     return (
         [("Content-Type", "text/plain")],
-        request.body
+        bytes_string
     )

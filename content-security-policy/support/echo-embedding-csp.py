80e0faa177892cc1569fed21b05aedb453a26743
Malika Aubakirova <amalika@google.com>
2016-12-09 13:43:00 +0100

    Simplyfying tests

--- content-security-policy/support/echo-embedding-csp.py
+++ content-security-policy/support/echo-embedding-csp.py
@@ -3,9 +3,13 @@ def main(request, response):
     header = request.headers.get("Embedding-CSP");
     message = {}
     message['embedding_csp'] = header if header else None
-    return [("Content-Type", "text/html")], '''
+    return [("Content-Type", "text/html"), ("Allow-CSP-From", "*")], '''
 <!DOCTYPE html>
-<script>
-  window.parent.postMessage({0}, '*');
-</script>
+<html>
+<head>
+    <script>
+      window.parent.postMessage({0}, '*');
+    </script>
+</head>
+</html>
 '''.format(json.dumps(message))

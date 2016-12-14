1876eb65e8e6e4c2def7072584a9475230969812
Malika Aubakirova <amalika@google.com>
2016-12-14 13:25:51 +0100

    Removing extra

--- content-security-policy/support/echo-policy-multiple.py
+++ content-security-policy/support/echo-policy-multiple.py
@@ -1,32 +1,19 @@
 def main(request, response):
     headers = [("Content-Type", "text/html")]
     if "policy" in request.GET:
-        headers.append(("Content-Security-Policy", request.GET["policy"].replace("8", ";")))
+        headers.append(("Content-Security-Policy", request.GET["policy"]))
     if "policy2" in request.GET:
-        headers.append(("Content-Security-Policy", request.GET["policy2"].replace("8", ";")))
+        headers.append(("Content-Security-Policy", request.GET["policy2"]))
+    if "policy3" in request.GET:
+        headers.append(("Content-Security-Policy", request.GET["policy3"]))
     message = request.GET["id"]
     return headers, '''
 <!DOCTYPE html>
 <html>
 <head>
-    <title>This page enforces embedder's policies</title>
-    <script nonce="123">
-        document.addEventListener("securitypolicyviolation", function(e) {
-            var response = {};
-            response["id"] = "%s";
-            response["securitypolicyviolation"] = true;
-            response["blockedURI"] = e.blockedURI;
-            response["lineNumber"] = e.lineNumber;
-            window.top.postMessage(response, '*');
-        });
-    </script>
+    <title>his page sets given CSP upon itself.</title>
 </head>
 <body>
-    <style>
-        body {
-            background-color: maroon;
-        }
-    </style>
     <script nonce="abc"> 
         var response = {};
         response["id"] = "%s";
@@ -35,4 +22,4 @@ def main(request, response):
     </script>
 </body>
 </html>
-''' % (message, message)
+''' % (message)

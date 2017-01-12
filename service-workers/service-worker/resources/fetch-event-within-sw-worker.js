f65db4399b7ee8dc589f17a686ce37eb7a5f40c2
Jake Archibald <jaffathecake@gmail.com>
2017-01-12 14:22:21 +0000

    Removing need for timeout in test

--- service-workers/service-worker/resources/fetch-event-within-sw-worker.js
+++ service-workers/service-worker/resources/fetch-event-within-sw-worker.js
@@ -3,42 +3,45 @@ skipWaiting();
 addEventListener('fetch', event => {
   const url = new URL(event.request.url);
 
-  if (url.origin == location.origin) {
-    if (url.pathname.endsWith('/dummy.txt')) {
-      event.respondWith(new Response('intercepted'));
-      return;
-    }
+  if (url.origin != location.origin) return;
 
-    if (url.pathname.endsWith('/dummy.txt-inner-fetch')) {
-      event.respondWith(fetch('dummy.txt'));
-      return;
-    }
+  if (url.pathname.endsWith('/dummy.txt')) {
+    event.respondWith(new Response('intercepted'));
+    return;
+  }
 
-    if (url.pathname.endsWith('/dummy.txt-inner-cache')) {
-      event.respondWith(
-        caches.open('test-inner-cache').then(cache =>
-          cache.add('dummy.txt').then(() => cache.match('dummy.txt'))
-        )
-      );
-      return;
-    }
+  if (url.pathname.endsWith('/dummy.txt-inner-fetch')) {
+    event.respondWith(fetch('dummy.txt'));
+    return;
+  }
 
-    if (url.pathname.endsWith('/show-notification')) {
-      event.respondWith(
-        registration.showNotification('test', {
-          icon: 'notification-icon.png'
-        }).then(() => registration.getNotifications()).then(notifications => {
-          for (const n of notifications) n.close();
-          return new Response('done');
-        })
-      );
-      return;
-    }
+  if (url.pathname.endsWith('/dummy.txt-inner-cache')) {
+    event.respondWith(
+      caches.open('test-inner-cache').then(cache =>
+        cache.add('dummy.txt').then(() => cache.match('dummy.txt'))
+      )
+    );
+    return;
+  }
 
-    if (url.pathname.endsWith('/notification-icon.png')) {
-      new BroadcastChannel('icon-request').postMessage('yay');
-      event.respondWith(new Response('done'));
-      return;
-    }
+  if (url.pathname.endsWith('/show-notification')) {
+    const iconURL = new URL('notification_icon.py', location);
+    iconURL.search = url.search;
+
+    event.respondWith(
+      registration.showNotification('test', {
+        icon: iconURL
+      }).then(() => registration.getNotifications()).then(notifications => {
+        for (const n of notifications) n.close();
+        return new Response('done');
+      })
+    );
+    return;
+  }
+
+  if (url.pathname.endsWith('/notification_icon.py')) {
+    new BroadcastChannel('icon-request').postMessage('yay');
+    event.respondWith(new Response('done'));
+    return;
   }
 });

5372e7567ea91657145dc7697c0ab4aa0f9707f9
Mike Pennisi <mike@mikepennisi.com>
2017-01-13 12:57:01 -0500

    Switch from Chromium to Chrome

--- ci_stability.sh
+++ ci_stability.sh
@@ -20,6 +20,13 @@ hosts_fixup() {
     echo "travis_fold:end:hosts_fixup"
 }
 
+install_chrome() {
+    deb_archive=google-chrome-stable_current_amd64.deb
+    wget https://dl.google.com/linux/direct/$deb_archive
+    sudo dpkg --install $deb_archive || true
+    sudo apt-get install --fix-broken
+    sudo dpkg --install $deb_archive
+}
 
 test_stability() {
     python check_stability.py $PRODUCT
@@ -27,6 +34,9 @@ test_stability() {
 
 main() {
     hosts_fixup
+    if [ "$PRODUCT" == "chrome" ]; then
+        install_chrome
+    fi
     test_stability
 }
 

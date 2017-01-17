0b51d4e562d85253e04b26a1bfdbb4a8656259d4
Shubhie Panicker <panicker@chromium.org>
2017-01-16 18:19:44 -0800

    address round #1 review comments

--- longtask-timing/resources/raflongtask.js
+++ longtask-timing/resources/raflongtask.js
@@ -1,5 +1,5 @@
 window.requestAnimationFrame(function() {
-  /* Generate a slow task */
+  /* Generate a slow task. */
   var begin = window.performance.now();
   while (window.performance.now() < begin + 51);
 });

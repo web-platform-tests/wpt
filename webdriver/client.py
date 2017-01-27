16d1463bf146f11eb45dd1d6774f248f1257c6ad
Mike Pennisi <mike@mikepennisi.com>
2017-01-27 12:57:37 -0500

    client, transport: implement ability to send raw requests
    Introduce a new method to allow tests to explicitly inspect the
    status code and complete body of HTTP responses received when issuing
    commands. In addition, extend the behavior of the higher-order
    `send_response` method with assertions for HTTP response headers.

--- webdriver/client.py
+++ webdriver/client.py
@@ -215,13 +215,13 @@ class Session(object):
         #body["capabilities"] = caps
         body = caps
 
-        resp = self.transport.send("POST", "session", body=body)
-        self.session_id = resp["sessionId"]
+        response = self.transport.send("POST", "session", body=body)
+        self.session_id = response.body["value"]["sessionId"]
 
         if self.extension_cls:
             self.extension = self.extension_cls(self)
 
-        return resp["value"]
+        return response.body["value"]
 
     def end(self):
         if self.session_id is None:
@@ -236,11 +236,48 @@ class Session(object):
         self.find = None
         self.extension = None
 
+    def send_raw_command(self, method, url, body=None, headers=None):
+        """Send a command to the remote end.
+
+        :param method: HTTP method to use in request
+        :param url: "command part" of the requests URL path
+        :param body: body of the HTTP request
+        :param headers: Additional headers to include in the HTTP request
+
+        :return: an instance of wdclient.Response describing the HTTP response
+            received from the remote end
+        """
+        url = urlparse.urljoin("session/%s/" % self.session_id, url)
+        return self.transport.send(method, url, body, headers)
+
     def send_command(self, method, url, body=None, key=None):
+        """Send a command to the remote end and validate its success.
+
+        :param method: HTTP method to use in request
+        :param url: "command part" of the requests URL path
+        :param body: body of the HTTP request
+        :param key: (deprecated) when specified, this string value will be used
+            to de-reference the HTTP response body following JSON parsing
+
+        :return: None if the HTTP response body was empty, otherwise the
+            result of parsing the HTTP response body as JSON
+        """
+
         if self.session_id is None:
             raise error.SessionNotCreatedException()
-        url = urlparse.urljoin("session/%s/" % self.session_id, url)
-        return self.transport.send(method, url, body, key=key)
+
+        response = self.send_raw_command(method, url, body)
+
+        if response.status != 200:
+            cls = error.get(response.body["value"].get("error"))
+            raise cls(response.body["value"].get("message"))
+
+        if key is not None:
+            response.body = response.body[key]
+        if not response.body:
+            response.body = None
+
+        return response.body
 
     @property
     @command

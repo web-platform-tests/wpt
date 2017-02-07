8f912b44ed9d8a3dc4ebc4b1bfd09dd0acc11144
Ms2ger <Ms2ger@gmail.com>
2017-02-07 11:14:34 +0100

    Reformat request-upload.js.

--- fetch/api/basic/request-upload.js
+++ fetch/api/basic/request-upload.js
@@ -31,39 +31,85 @@ function testUploadFailure(desc, url, method, body) {
 
 var url = RESOURCES_DIR + "echo-content.py"
 
-testUpload("Fetch with PUT with body", url, "PUT", "Request's body", "Request's body");
-testUpload("Fetch with POST with text body", url, "POST", "Request's body", "Request's body");
-testUpload("Fetch with POST with URLSearchParams body", url, "POST", function() { return new URLSearchParams("name=value"); }, "name=value");
-testUpload("Fetch with POST with Blob body", url, "POST", new Blob(["Test"]), "Test");
-testUpload("Fetch with POST with ArrayBuffer body", url, "POST", new ArrayBuffer(4), "\0\0\0\0");
-testUpload("Fetch with POST with Uint8Array body", url, "POST", new Uint8Array(4), "\0\0\0\0");
-testUpload("Fetch with POST with Int8Array body", url, "POST", new Int8Array(4), "\0\0\0\0");
-testUpload("Fetch with POST with Float32Array body", url, "POST", new Float32Array(1), "\0\0\0\0");
-testUpload("Fetch with POST with Float64Array body", url, "POST", new Float64Array(1), "\0\0\0\0\0\0\0\0");
-testUpload("Fetch with POST with DataView body", url, "POST", new DataView(new ArrayBuffer(8), 0, 4), "\0\0\0\0");
-testUpload("Fetch with POST with Blob body with mime type", url, "POST", new Blob(["Test"], { type: "text/maybe" }), "Test");
-testUpload("Fetch with POST with ReadableStream", url, "POST", new ReadableStream({start: controller => {
+testUpload("Fetch with PUT with body", url,
+  "PUT",
+  "Request's body",
+  "Request's body");
+testUpload("Fetch with POST with text body", url,
+  "POST",
+  "Request's body",
+  "Request's body");
+testUpload("Fetch with POST with URLSearchParams body", url,
+  "POST",
+  function() { return new URLSearchParams("name=value"); },
+  "name=value");
+testUpload("Fetch with POST with Blob body", url,
+  "POST",
+  new Blob(["Test"]),
+  "Test");
+testUpload("Fetch with POST with ArrayBuffer body", url,
+  "POST",
+  new ArrayBuffer(4),
+  "\0\0\0\0");
+testUpload("Fetch with POST with Uint8Array body", url,
+  "POST",
+  new Uint8Array(4),
+  "\0\0\0\0");
+testUpload("Fetch with POST with Int8Array body", url,
+  "POST",
+  new Int8Array(4),
+  "\0\0\0\0");
+testUpload("Fetch with POST with Float32Array body", url,
+  "POST",
+  new Float32Array(1),
+  "\0\0\0\0");
+testUpload("Fetch with POST with Float64Array body", url,
+  "POST",
+  new Float64Array(1),
+  "\0\0\0\0\0\0\0\0");
+testUpload("Fetch with POST with DataView body", url,
+  "POST",
+  new DataView(new ArrayBuffer(8), 0, 4),
+  "\0\0\0\0");
+testUpload("Fetch with POST with Blob body with mime type", url,
+  "POST",
+  new Blob(["Test"], { type: "text/maybe" }),
+  "Test");
+testUpload("Fetch with POST with ReadableStream", url,
+  "POST",
+  new ReadableStream({start: controller => {
     const encoder = new TextEncoder();
     controller.enqueue(encoder.encode("Test"));
     controller.close();
-  }}), "Test");
-testUploadFailure("Fetch with POST with ReadableStream containing String", url, "POST", new ReadableStream({start: controller => {
+  }}),
+  "Test");
+testUploadFailure("Fetch with POST with ReadableStream containing String", url,
+  "POST",
+  new ReadableStream({start: controller => {
     controller.enqueue("Test");
     controller.close();
   }}));
-testUploadFailure("Fetch with POST with ReadableStream containing null", url, "POST", new ReadableStream({start: controller => {
+testUploadFailure("Fetch with POST with ReadableStream containing null", url,
+  "POST",
+  new ReadableStream({start: controller => {
     controller.enqueue(null);
     controller.close();
   }}));
-testUploadFailure("Fetch with POST with ReadableStream containing number", url, "POST", new ReadableStream({start: controller => {
+testUploadFailure("Fetch with POST with ReadableStream containing number", url,
+  "POST",
+  new ReadableStream({start: controller => {
     controller.enqueue(99);
     controller.close();
   }}));
-testUploadFailure("Fetch with POST with ReadableStream containing ArrayBuffer", url, "POST", new ReadableStream({start: controller => {
+testUploadFailure("Fetch with POST with ReadableStream containing ArrayBuffer", url,
+  "POST",
+  new ReadableStream({start: controller => {
     controller.enqueue(new ArrayBuffer());
     controller.close();
   }}));
-testUploadFailure("Fetch with POST with ReadableStream containing Blob", url, "POST", new ReadableStream({start: controller => {
+testUploadFailure("Fetch with POST with ReadableStream containing Blob", url,
+  "POST",
+  new ReadableStream({start: controller => {
     controller.enqueue(new Blob());
     controller.close();
   }}));

902a2b5b72a212172aca33aa4bd2bb02de9a1823
Yoav Weiss <yoav@yoav.ws>
2017-02-08 10:31:11 +0100

    Review comments

--- preload/resources/preload_helper.js
+++ preload/resources/preload_helper.js
@@ -1,8 +1,8 @@
 function verifyPreloadAndRTSupport()
 {
     var link = window.document.createElement("link");
-    var preloadSupported = link.relList && link.relList.supports("preload");
-    assert_true(preloadSupported && !!window.PerformanceResourceTiming, "Browser doesn't support preload or ResourceTiming.");
+    assert_true(link.relList && link.relList.supports("preload"), "Preload not supported");
+    assert_true(!!window.PerformanceResourceTiming, "ResourceTiming not supported");
 }
 
 function getAbsoluteURL(url)

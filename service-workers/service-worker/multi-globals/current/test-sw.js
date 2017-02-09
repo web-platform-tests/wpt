d66e126edbc4120671775a891ae7222c1b6e85f2
Domenic Denicola <d@domenic.me>
2017-02-09 13:46:52 -0500

    Respond to review feedback

--- service-workers/service-worker/multi-globals/current/test-sw.js
+++ service-workers/service-worker/multi-globals/current/test-sw.js
@@ -1,3 +1,13 @@
+'use strict';
+
+this.addEventListener('install', event => {
+    this.skipWaiting();
+});
+
+this.addEventListener('activate', event => {
+    clients.claim();
+});
+
 this.addEventListener('fetch', event => {
     if (event.request.url.includes('test.txt')) {
         event.respondWith(new Response('current'));

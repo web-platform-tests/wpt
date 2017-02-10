688f5f85cbfa5c56326551753ba86735dcf60393
Domenic Denicola <d@domenic.me>
2017-02-10 13:14:18 -0500

    Add multi-global tests for service worker URL parsing
    Follows https://github.com/slightlyoff/ServiceWorker/issues/922.

---
+++ service-workers/service-worker/multi-globals/test-sw.js
@@ -0,0 +1,15 @@
+'use strict';
+
+this.addEventListener('install', event => {
+    this.skipWaiting();
+});
+
+this.addEventListener('activate', event => {
+    clients.claim();
+});
+
+this.addEventListener('fetch', event => {
+    if (event.request.url.includes('test.txt')) {
+        event.respondWith(new Response('entry'));
+    }
+});

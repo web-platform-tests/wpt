da207f35be6e785366286b51a8300f01399f35ed
Jungkee Song <jungkee.song@samsung.com>
2017-02-13 19:31:39 +0900

    Remove interception case from service worker URL parsing tests
    Multi-global tests for service worker URL parsing intend to test whether
    the right global is used for parsing the script URL and scope URL. This
    replaces the unnecessary interception scenario with just checking the
    registration's scope.
    
    Follow-up on https://github.com/w3c/web-platform-tests/commit/688f5f85cbfa5c56326551753ba86735dcf60393.

--- service-workers/service-worker/multi-globals/current/test-sw.js
+++ service-workers/service-worker/multi-globals/current/test-sw.js
@@ -1,15 +1,1 @@
-'use strict';
-
-this.addEventListener('install', event => {
-    this.skipWaiting();
-});
-
-this.addEventListener('activate', event => {
-    clients.claim();
-});
-
-this.addEventListener('fetch', event => {
-    if (event.request.url.includes('test.txt')) {
-        event.respondWith(new Response('current'));
-    }
-});
+// Service worker for current/

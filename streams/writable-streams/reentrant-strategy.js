2908b1e18814942608fa2d53df3d32d5b0e61017
Adam Rice <ricea@chromium.org>
2017-02-13 19:01:14 +0900

    Update tests to reflect fixed implementation

--- streams/writable-streams/reentrant-strategy.js
+++ streams/writable-streams/reentrant-strategy.js
@@ -57,9 +57,9 @@ promise_test(() => {
       .then(() => flushAsyncEvents())
       .then(() => {
         assert_array_equals(events, ['size', 2, 'size', 1, 'size', 0,
-                                     'sink.write', 0, 'sink.write', 1, 'writer.write done', 2,
+                                     'sink.write', 0, 'sink.write', 1, 'writer.write done', 0,
                                      'sink.write', 2, 'writer.write done', 1,
-                                     'writer.write done', 0],
+                                     'writer.write done', 2],
                             'events should happen in standard order');
       });
 }, 'writer.write() promises should resolve in the standard order');
@@ -80,8 +80,8 @@ promise_test(t => {
   const resolved = [];
   const writer = ws.getWriter();
   const readyPromise1 = writer.ready.then(() => resolved.push('ready1'));
-  const writePromise = promise_rejects(t, error1, writer.write(),
-                                       'write() should reject with error1')
+  const writePromise = promise_rejects(t, new TypeError(), writer.write(),
+                                       'write() should reject with a TypeError')
                                            .then(() => resolved.push('write'));
   const readyPromise2 = promise_rejects(t, error1, writer.ready, 'ready should reject with error1')
       .then(() => resolved.push('ready2'));
@@ -95,21 +95,70 @@ promise_test(t => {
       });
 }, 'controller.error() should work when called from within strategy.size()');
 
-promise_test(() => {
+promise_test(t => {
   let writer;
   const strategy = {
     size() {
-      writer.close('C');
+      writer.close();
       return 1;
     }
   };
 
   const ws = recordingWritableStream({}, strategy);
   writer = ws.getWriter();
-  return writer.write('W')
+  return promise_rejects(t, new TypeError(), writer.write('a'), 'write() promise should reject')
       .then(() => {
-        assert_array_equals(ws.events, ['write', 'W', 'close', 'C'], 'close should happen after write');
+        assert_array_equals(ws.events, ['close'], 'sink.write() should not be called');
       });
-}, 'close() should be work when called from within strategy.size()');
+}, 'close() should work when called from within strategy.size()');
+
+promise_test(t => {
+  let writer;
+  const strategy = {
+    size() {
+      writer.releaseLock();
+      return 1;
+    }
+  };
+
+  const ws = recordingWritableStream({}, strategy);
+  writer = ws.getWriter();
+  const writePromise = promise_rejects(t, new TypeError(), writer.write('a'), 'write() promise should reject');
+  const readyPromise = promise_rejects(t, new TypeError(), writer.ready, 'ready promise should reject');
+  const closedPromise = promise_rejects(t, new TypeError(), writer.closed, 'closed promise should reject');
+  return Promise.all([writePromise, readyPromise, closedPromise])
+      .then(() => {
+        assert_array_equals(ws.events, [], 'sink.write() should not be called');
+      });
+}, 'releaseLock() should abort the write() when called within strategy.size()');
+
+promise_test(t => {
+  let writer1;
+  let ws;
+  let writePromise2;
+  let closePromise;
+  let closedPromise2;
+  const strategy = {
+    size(chunk) {
+      if (chunk > 0) {
+        writer1.releaseLock();
+        const writer2 = ws.getWriter();
+        writePromise2 = writer2.write(0);
+        closePromise = writer2.close();
+        closedPromise2 = writer2.closed;
+      }
+      return 1;
+    }
+  };
+  ws = recordingWritableStream({}, strategy);
+  writer1 = ws.getWriter();
+  const writePromise1 = promise_rejects(t, new TypeError(), writer1.write(1), 'write() promise should reject');
+  const readyPromise = promise_rejects(t, new TypeError(), writer1.ready, 'ready promise should reject');
+  const closedPromise1 = promise_rejects(t, new TypeError(), writer1.closed, 'closed promise should reject');
+  return Promise.all([writePromise1, readyPromise, closedPromise1, writePromise2, closePromise, closedPromise2])
+      .then(() => {
+        assert_array_equals(ws.events, ['write', 0, 'close'], 'sink.write() should only be called once');
+      });
+}, 'original reader should error when new reader is created within strategy.size()');
 
 done();

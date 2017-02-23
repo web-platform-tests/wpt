a1e20458ad5d933affa7a8451841423efff00f27
Domenic Denicola <d@domenic.me>
2017-02-23 18:07:51 -0500

    Factor a bunch of stuff out

---
+++ html/infrastructure/safe-passing-of-structured-data/shared-array-buffers/resources/test-incrementer.js
@@ -0,0 +1,58 @@
+"use strict";
+
+self.testSharingViaIncrementerScript = (t, whereToListen, whereToSend, whereToSendLabel, origin) => {
+  return new Promise(resolve => {
+    const sab = new SharedArrayBuffer(4);
+    const view = new Int32Array(sab);
+    view[0] = 1;
+
+    whereToListen.onmessage = t.step_func(({ data }) => {
+      switch (data.message) {
+        case "initial payload received": {
+          assert_equals(data.value, 1, `The ${whereToSendLabel} must see the same value in the SharedArrayBuffer`);
+          break;
+        }
+
+        case "changed to 2": {
+          assert_equals(view[0], 2, `The window must see changes made in the ${whereToSendLabel}`);
+
+          view[0] = 3;
+          whereToSend.postMessage({ message: "changed to 3" }, origin);
+
+          break;
+        }
+
+        case "changed to 3 received": {
+          assert_equals(data.value, 3, `The ${whereToSendLabel} must see changes made in the window`);
+          resolve();
+          break;
+        }
+      }
+    });
+
+    whereToSend.postMessage({ message: "initial payload", sab }, origin);
+  });
+};
+
+self.setupDestinationIncrementer = (whereToSendBackTo, origin) => {
+  let view;
+  self.onmessage = ({ data }) => {
+    switch (data.message) {
+      case "initial payload": {
+        view = new Int32Array(data.sab);
+        whereToSendBackTo.postMessage({ message: "initial payload received", value: view[0] }, origin);
+
+        view[0] = 2;
+        whereToSendBackTo.postMessage({ message: "changed to 2" }, origin);
+
+        break;
+      }
+
+      case "changed to 3": {
+        whereToSendBackTo.postMessage({ message: "changed to 3 received", value: view[0] }, origin);
+
+        break;
+      }
+    }
+  };
+};

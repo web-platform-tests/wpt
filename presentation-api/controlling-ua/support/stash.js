01668b327523186592351dc40e7573dcd23492d4
Tomoyuki Shimizu <tomoyuki.labs@gmail.com>
2017-02-24 15:52:59 +0900

    fix several bug and improve stash

--- presentation-api/controlling-ua/support/stash.js
+++ presentation-api/controlling-ua/support/stash.js
@@ -1,54 +1,83 @@
-(window => {
-  // Note: Tentatively, Only one stash ID is defined, due to a single presentationUrls
-  // common to any test cases
-  const stashPath = '/presentation-api/controlling-ua/support/stash.py?id=';
-  const stashId = '59d947f0-31ba-4bc3-8b28-691e6bbb05a9';
+var Stash = function(inbound, outbound) {
+  this.stashPath = '/presentation-api/controlling-ua/support/stash.py?id=';
+  this.inbound = inbound;
+  this.outbound = outbound;
+}
 
-  // clean up a stash on wptserve
-  window.initStash = () => {
-    return fetch(stashPath + stashId).then(response => {
+// initialize a stash on wptserve
+Stash.prototype.init = function() {
+  return Promise.all([
+    fetch(this.stashPath + this.inbound).then(response => {
       return response.text();
-    });
-  };
-
-  // upload a test result to a stash on wptserve
-  window.uploadStash = result => {
-    return fetch(stashPath + stashId, {
-      method: 'POST',
-      body: result
-    }).then(response => {
+    }),
+    fetch(this.stashPath + this.outbound).then(response => {
       return response.text();
-    }).then(text => {
-      return text === 'ok' ? null : Promise.reject();
     })
-  };
+  ]);
+}
 
-  // wait until a test result is uploaded to a stash on wptserve
-  window.waitForStash = () => {
-    return new Promise((resolve, reject) => {
-      let intervalId;
-      const interval = 500; // msec
-      const polling = () => {
-        return fetch(stashPath + stashId).then(response => {
-          return response.text();
-        }).then(text => {
-          if(text) {
+// upload a test result to a stash on wptserve
+Stash.prototype.send = function(result) {
+  return fetch(this.stashPath + this.outbound, {
+    method: 'POST',
+    body: JSON.stringify({ type: 'data', data: result })
+  }).then(response => {
+    return response.text();
+  }).then(text => {
+    return text === 'ok' ? null : Promise.reject();
+  })
+};
+
+// wait until a test result is uploaded to a stash on wptserve
+Stash.prototype.receive = function() {
+  return new Promise((resolve, reject) => {
+    let intervalId;
+    const interval = 500; // msec
+    const polling = () => {
+      return fetch(this.stashPath + this.inbound).then(response => {
+        return response.text();
+      }).then(text => {
+        if (text) {
+          try {
+            const json = JSON.parse(text);
+            if (json.type === 'data')
+              resolve(json.data);
+            else
+              reject();
+          } catch(e) {
             resolve(text);
-            clearInterval(intervalId);
           }
-        });
-      };
-      intervalId = setInterval(polling, interval);
-    });
-  };
+          clearInterval(intervalId);
+        }
+      });
+    };
+    intervalId = setInterval(polling, interval);
+  });
+};
 
-  // stop waiting for a stash on wptserve
-  window.stopWaitingStash = () => {
-    return fetch(stashPath + stashId, {
-      method: 'POST',
-      body: 'stopped'
-    }).then(response => {
+// reset a stash on wptserve
+Stash.prototype.stop = function() {
+  return Promise.all([
+    fetch(this.stashPath + this.inbound).then(response => {
       return response.text();
-    });
-  }
-})(window);
+    }),
+    fetch(this.stashPath + this.outbound).then(response => {
+      return response.text();
+    })
+  ]).then(() => {
+    return Promise.all([
+      fetch(this.stashPath + this.inbound, {
+        method: 'POST',
+        body: JSON.stringify({ type: 'stop' })
+      }).then(response => {
+        return response.text();
+      }),
+      fetch(this.stashPath + this.outbound, {
+        method: 'POST',
+        body: JSON.stringify({ type: 'stop' })
+      }).then(response => {
+        return response.text();
+      })
+    ]);
+  });
+}

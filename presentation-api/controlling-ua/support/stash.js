06f564650901bb4035eb22794c26c3ad3749c5e8
Tomoyuki Shimizu <tomoyuki.labs@gmail.com>
2017-02-24 12:00:36 +0900

    cleanup and restructuring

--- presentation-api/controlling-ua/support/stash.js
+++ presentation-api/controlling-ua/support/stash.js
@@ -1,32 +1,4 @@
 (window => {
-  //
-  // Common Parameters and Functions for test cases which use a stash
-  //
-  window.message1 = '1st';
-  window.message2 = '2nd';
-  window.message3 = new Uint8Array([51, 114, 100]);      // "3rd"
-  window.message4 = new Uint8Array([52, 116, 104]);      // "4th"
-  window.message5 = new Uint8Array([108, 97, 115, 116]); // "last"
-
-  Uint8Array.prototype.toText = function() {
-      return this.reduce(function(result, item) {
-          return result + String.fromCharCode(item);
-      }, '');
-  };
-
-  Uint8Array.prototype.equals = function(a) {
-      let array = a instanceof Uint8Array ? a : (a instanceof ArrayBuffer ? new Uint8Array(a) : null);
-      return !!array && this.every((item, index) => { return item === a[index]; });
-  };
-
-  ArrayBuffer.prototype.toText = function() {
-      return new Uint8Array(this).toText();
-  };
-
-  ArrayBuffer.prototype.equals = function(a) {
-      return new Uint8Array(this).equals(a);
-  };
-
   // Note: Tentatively, Only one stash ID is defined, due to a single presentationUrls
   // common to any test cases
   const stashPath = '/presentation-api/controlling-ua/support/stash.py?id=';

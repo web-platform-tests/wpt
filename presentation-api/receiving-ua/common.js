940b135e595dd30334e3f05b92f518284e095790
Tomoyuki Shimizu <tomoyuki.labs@gmail.com>
2017-03-09 13:15:22 +0900

    revise a mechanism to handle receiver's result

--- presentation-api/receiving-ua/common.js
+++ presentation-api/receiving-ua/common.js
@@ -7,4 +7,40 @@
     toController: '0c382524-5738-4df0-837d-4f53ea8addc2',
     toReceiver: 'a9618cd1-ca2b-4155-b7f6-630dce953c44'
   }
+
+  // handle a test result received from a receiving page
+  const parseValue = value => {
+    let r;
+
+    // String
+    if (r = value.match(/^"(.*)"$/))
+      return r[1];
+    // Object
+    else if (r = value.match(/^object\s+"\[object\s+(.*)\]"$/))
+      return window[r[1]].prototype;
+    // Number, boolean, null, undefined
+    else
+      return JSON.parse(value);
+  };
+
+  window.parseResult = message => {
+    let r = message.match(/^(assert_.*):\s+(.*)$/);
+    const assertion = r[1];
+    const body = r[2];
+    switch (assertion) {
+      case 'assert_equals':
+        r = body.match(/^(.*)\s+expected\s+(true|false|null|\d+|"(.*)")\s+but\s+got\s+(true|false|null|\d+|(object\s+)?"(.*)")$/);
+        window[assertion](parseValue(r[4]), parseValue(r[2]), r[1]);
+        break;
+      case 'assert_true':
+      case 'assert_false':
+        r = body.match(/^(.*)\s+expected\s+(true|false)\s+got\s+(true|false)$/);
+        window[assertion](parseValue(r[3]), r[1]);
+        break;
+      case 'assert_unreached':
+        r = body.match(/^(.*)\s+Reached\s+unreachable\s+code$/);
+        window[assertion](r[1]);
+        break;
+    }
+  };
 })(window);

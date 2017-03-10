c26a91716000a6214eb23bf12f92c087c721b772
BenjaminSchaaf <ben.schaaf@gmail.com>
2017-03-10 23:11:53 +1100

    Updated parser for https://github.com/w3c/webvtt/pull/337

--- webvtt/parsing/file-parsing/tools/parser.py
+++ webvtt/parsing/file-parsing/tools/parser.py
@@ -507,25 +507,29 @@ class VTTCueParser(W3CParser):
                     number = float(linepos)
 
                 # 5.
-                self.cue.line = number
+                if linealign == "start":
+                    self.cue.line_alignment = 'start'
 
                 # 6.
+                elif linealign == "center":
+                    self.cue.line_alignment = 'center'
+
+                # 7.
+                elif linealign == "end":
+                    self.cue.line_alignment = 'end'
+
+                # 8.
+                elif linealign != "":
+                    continue
+
+                # 9.
+                self.cue.line = number
+
+                # 10.
                 if linepos[-1] == '%':
                     self.cue.snap_to_lines = False
                 else:
                     self.cue.snap_to_lines = True
-
-                # 7.
-                if linealign == "start":
-                    self.cue.line_alignment = 'start'
-
-                # 8.
-                if linealign == "center":
-                    self.cue.line_alignment = 'center'
-
-                # 9.
-                if linealign == "end":
-                    self.cue.line_alignment = 'end'
 
             elif name == 'position':
                 # 1.
@@ -545,19 +549,23 @@ class VTTCueParser(W3CParser):
                     continue
 
                 # 4.
-                self.cue.position = number
-
-                # 5.
                 if colalign == "line-left":
                     self.cue.line_alignment = 'line-left'
 
-                # 6.
-                if colalign == "center":
+                # 5.
+                elif colalign == "center":
                     self.cue.line_alignment = 'center'
 
-                # 7.
-                if colalign == "line-right":
+                # 6.
+                elif colalign == "line-right":
                     self.cue.line_alignment = 'line-right'
+
+                # 7.
+                elif colalign != "":
+                    continue
+
+                # 8.
+                self.cue.position = number
 
             elif name == 'size':
                 # 1.

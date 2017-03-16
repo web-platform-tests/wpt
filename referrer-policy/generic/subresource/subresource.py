d09cc34bb2f8b7850857b92cd5569951a41cf6ff
Jochen Eisinger <jochen@chromium.org>
2017-03-16 16:24:35 +0100

    [REFERRER] Tests for CSS integration

--- referrer-policy/generic/subresource/subresource.py
+++ referrer-policy/generic/subresource/subresource.py
@@ -72,7 +72,8 @@ def respond(request,
             content_type = "text/html",
             payload_generator = __noop,
             cache_control = "no-cache; must-revalidate",
-            access_control_allow_origin = "*"):
+            access_control_allow_origin = "*",
+            maybe_additional_headers = None):
     if preprocess_redirection(request, response):
         return
 
@@ -84,11 +85,18 @@ def respond(request,
                                      access_control_allow_origin)
     response.writer.write_header("content-type", content_type)
     response.writer.write_header("cache-control", cache_control)
+
+    additional_headers = maybe_additional_headers or {}
+    for header, value in additional_headers.items():
+        response.writer.write_header(header, value)
+
     response.writer.end_headers()
 
     server_data = {"headers": json.dumps(request.headers, indent = 4)}
 
     payload = payload_generator(server_data)
+    if payload == None:
+        print content_type
     response.writer.write(payload)
 
 

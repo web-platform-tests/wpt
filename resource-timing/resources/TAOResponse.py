419a4991a8512fb21d8bc5f2b3ccbe75ecc359b1
siusin <siusinng@gmail.com>
2017-03-16 23:28:18 +0800

    added test for list with comma or space

--- resource-timing/resources/TAOResponse.py
+++ resource-timing/resources/TAOResponse.py
@@ -5,14 +5,29 @@ def main(request, response):
     tao = request.GET.first('tao')
 
     if tao == 'zero':
+    # zero TAO value, fail
         pass
-    elif tao == 'multi':
-        response.headers.set('Timing-Allow-Origin', ['*', '*'])
     elif tao == '*':
+    # wildcard, pass
         response.headers.set('Timing-Allow-Origin', '*')
+    elif tao == 'null':
+    # null, fail
+        response.headers.set('Timing-Allow-Origin', 'null')
     elif tao == 'origin':
+    # case-sensitive match for origin, pass
         response.headers.set('Timing-Allow-Origin', origin)
+    elif tao == 'comma':
+    # comma seperated list of origin and wildcard, pass
+        response.headers.set('Timing-Allow-Origin', ('*, ' + origin))
+    elif tao == 'space':
+    # space seperated list of origin and wildcard, fail
+        response.headers.set('Timing-Allow-Origin', (origin + ' *'))
+    elif tao == 'multi':
+    # more than one TAO values, fail
+        response.headers.set('Timing-Allow-Origin', origin)
+        response.headers.append('Timing-Allow-Origin', '*')
     elif tao == 'uppercase':
+    # non-case-sensitive match for origin, fail
         response.headers.set('Timing-Allow-Origin', origin.upper())
     else:
         pass

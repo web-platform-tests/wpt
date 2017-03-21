bfc66cfbb8ef02670126675bb5a95d6473c606ae
James Graham <james@hoppipolla.co.uk>
2017-03-21 14:58:34 +0000

    WebDriver new_session tests.
    Tests for the new session command, covering the behaviour of
    alwaysMatch and firstMatch, along with the supported properties.

---
+++ webdriver/newsession/create.py
@@ -0,0 +1,24 @@
+#META: timeout=long
+
+import pytest
+
+from conftest import product, flatten
+
+
+# Note that we can only test things here all implementations must support
+valid_data = [
+    ("acceptInsecureCerts", [False, None]),
+    ("browserName", [None]),
+    ("browserVersion", [None]),
+    ("platformName", [None]),
+    ("pageLoadStrategy", ["none", "eager", "normal", None]),
+]
+
+
+@pytest.mark.parametrize("body", [lambda key, value: {"alwaysMatch": {key: value}},
+                                  lambda key, value: {"firstMatch": [{key: value}]}])
+@pytest.mark.parametrize("key,value", flatten(product(*item) for item in valid_data))
+def test_valid(new_session, body, key, value):
+    resp = new_session({"capabilities": body(key, value)})
+
+# Continued in create-1.py to avoid timeouts

c1d53e9dd972ffd2e49b3ed63290c409569da05e
Anne van Kesteren <annevk@annevk.nl>
2017-03-30 10:26:13 +0200

    ancestorOrigins: add tests and update Location IDL
    See https://github.com/whatwg/html/issues/1918 for the HTML Standard
    discussion and https://github.com/whatwg/html/pull/2480 for the HTML
    Standard change.

---
+++ html/browsers/history/the-location-interface/resources/ancestororigins-embed.py
@@ -0,0 +1,22 @@
+def main(request, response):
+    body = ""
+
+    if "iframe" in request.GET:
+        frame = request.GET.first("iframe").split("|")
+        frameReferrer = frame[0]
+        frameURL = frame[1]
+        if frameReferrer != "":
+            frameReferrer = " referrerpolicy=" + frameReferrer
+
+        body = ("<iframe src=\"%s\"%s></iframe>" % (frameURL, frameReferrer)) + "\n"
+
+    if "id" in request.GET:
+        body = body + """<script>
+let output = [...self.location.ancestorOrigins]
+top.postMessage({ id: %s, output }, "*")
+</script>
+""" % request.GET.first("id")
+
+    if body == "":
+        return "Please specify either or both the 'iframe' and 'id' GET parameters."
+    return body

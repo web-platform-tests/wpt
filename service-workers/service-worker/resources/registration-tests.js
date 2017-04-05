ad8c7ba36fcb359e91815b7c43c5779b0e5ebfc8
Jake Archibald <jaffathecake@gmail.com>
2017-04-05 16:34:19 +0900

    Service worker: test valid mime types

--- service-workers/service-worker/resources/registration-tests.js
+++ service-workers/service-worker/resources/registration-tests.js
@@ -137,6 +137,40 @@ function registration_tests(register_method, check_error_types) {
           'Registration of plain text script should fail.');
     }, 'Registering script with bad MIME type');
 
+  const validMimeTypes = [
+    'application/ecmascript',
+    'application/javascript',
+    'application/x-ecmascript',
+    'application/x-javascript',
+    'text/ecmascript',
+    'text/javascript',
+    'text/javascript1.0',
+    'text/javascript1.1',
+    'text/javascript1.2',
+    'text/javascript1.3',
+    'text/javascript1.4',
+    'text/javascript1.5',
+    'text/jscript',
+    'text/livescript',
+    'text/x-ecmascript',
+    'text/x-javascript'
+  ];
+
+  for (const validMimeType of validMimeTypes) {
+    promise_test(() => {
+      var script = `resources/mime-type-worker.py?mime=${validMimeType}`;
+      var scope = 'resources/scope/good-mime-type-worker/';
+
+      return register_method(script, {scope}).then(registration => {
+        assert_true(
+          registration instanceof ServiceWorkerRegistration,
+          'Successfully registered.');
+        return registration.unregister();
+      });
+    }, `Registering script with good MIME type ${validMimeType}`);
+  }
+
+
   promise_test(function(t) {
       var script = 'resources/redirect.py?Redirect=' +
                     encodeURIComponent('/resources/registration-worker.js');

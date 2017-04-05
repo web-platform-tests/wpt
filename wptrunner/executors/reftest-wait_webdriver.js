bea0e994482bf1856b9f406fda699e296eb0e6e4
Geoffrey Sneddon <me@gsnedders.com>
2017-04-05 22:09:30 +0100

    Add workaround for Chrome not painting images (#248)
    See <https://github.com/jugglinmike/chrome-screenshot-race/issues/1>.
    
    This appears to avoid it from happening, and guarantees we've painted
    after the load event and (potentially) reftest-wait being removed.
    
    r=jgraham

--- wptrunner/executors/reftest-wait_webdriver.js
+++ wptrunner/executors/reftest-wait_webdriver.js
@@ -7,7 +7,21 @@ var callback = arguments[arguments.length - 1];
 function test(x) {
   if (!root.classList.contains("reftest-wait")) {
     observer.disconnect();
-    callback()
+    
+    // As of 2017-04-05, the Chromium web browser exhibits a rendering bug
+    // (https://bugs.chromium.org/p/chromium/issues/detail?id=708757) that
+    // produces instability during screen capture. The following use of
+    // `requestAnimationFrame` is intended as a short-term workaround, though
+    // it is not guaranteed to resolve the issue.
+    //
+    // For further detail, see:
+    // https://github.com/jugglinmike/chrome-screenshot-race/issues/1
+    
+    requestAnimationFrame(function() {
+      requestAnimationFrame(function() {
+	callback();
+      });
+    });
   }
 }
 

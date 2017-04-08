6605ee7400ab20c23f9a912e83241502e350969e
Jake Archibald <jaffathecake@gmail.com>
2017-04-08 09:36:14 +0100

    Tests for updateViaCache (previously useCache)

--- service-workers/service-worker/resources/update-max-aged-worker-imported-script.py
+++ service-workers/service-worker/resources/update-max-aged-worker-imported-script.py
@@ -1,22 +1,12 @@
 import time
 
 def main(request, response):
-    headers = [('Cache-Control', 'max-age=86400'),
-               ('Content-Type', 'application/javascript'),
-               ('Last-Modified', time.strftime("%a, %d %b %Y %H:%M:%S GMT",
-                                               time.gmtime()))]
-
-
-    revalidate = request.headers.has_key('if-modified-since');
+    headers = [('Content-Type', 'application/javascript'),
+               ('Cache-Control', 'max-age=86400'),
+               ('Last-Modified', time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime()))]
 
     body = '''
-    self.addEventListener('message', function(e) {
-        e.data.port.postMessage({
-            from: "imported",
-            type: "%s",
-            value: %s
-        });
-    });
-    ''' % ('revalidate' if revalidate else 'normal', time.time())
+        const importTime = {time};
+    '''.format(time=time.time())
 
     return headers, body
--- service-workers/service-worker/resources/update-max-aged-worker.py
+++ service-workers/service-worker/resources/update-max-aged-worker.py
@@ -1,26 +1,28 @@
 import time
+import json
 
 def main(request, response):
     headers = [('Content-Type', 'application/javascript'),
                ('Cache-Control', 'max-age=86400'),
                ('Last-Modified', time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime()))]
 
-    test = '';
-    if 'Test' in request.GET:
-      test = request.GET['Test'];
-
-    revalidate = request.headers.has_key('if-modified-since');
-
+    test = request.GET['test'];
+  
     body = '''
-    importScripts('update-max-aged-worker-imported-script.py?Test=%s');
+        const mainTime = {time};
+        const testName = {test};
+        importScripts('update-max-aged-worker-imported-script.py');
 
-    self.addEventListener('message', function(e) {
-        e.data.port.postMessage({
-            from: "main",
-            type: "%s",
-            value: %s
-        });
-    });
-    ''' % (test, 'revalidate' if revalidate else 'normal', time.time())
+        addEventListener('message', event => {{
+            event.source.postMessage({{
+                mainTime,
+                importTime,
+                test: {test}
+            }});
+        }});
+    '''.format(
+        time=time.time(),
+        test=json.dumps(test)
+    )
 
     return headers, body

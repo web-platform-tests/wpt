6f35ff2ef3e28573062860bcb58be2f9fd98b010
Anne van Kesteren <annevk@annevk.nl>
2017-05-03 11:04:16 +0200

    flip pass condition, test <area> too

--- html/semantics/links/following-hyperlinks/activation-behavior.window.js
+++ html/semantics/links/following-hyperlinks/activation-behavior.window.js
@@ -1,35 +1,37 @@
-async_test(t => {
-  const target = document.createElement("iframe"),
-        link = document.createElement("a");
-  t.add_cleanup(() => target.remove());
-  target.name = "certifiedrandom";
-  link.target = "certifiedrandom";
-  link.href = "/";
-  document.body.appendChild(target);
-  target.onload = t.step_func(() => {
-    if(target.contentWindow.location.href === "about:blank")
-      return;
-    assert_equals(target.contentWindow.location.pathname, "/");
-    t.done();
-  });
-  link.click();
-  t.step_timeout(() => assert_unreached(), 500);
-}, "<a> that is not connected should still be followed");
+["a",
+ "area"].forEach(type => {
+  async_test(t => {
+    const target = document.createElement("iframe"),
+          link = document.createElement(type);
+    t.add_cleanup(() => target.remove());
+    target.name = "certifiedrandom";
+    link.target = "certifiedrandom";
+    link.href = "/";
+    document.body.appendChild(target);
+    target.onload = t.step_func(() => {
+      if(target.contentWindow.location.href === "about:blank")
+        return;
+      assert_unreached();
+    });
+    link.click();
+    t.step_timeout(() => t.done(), 500);
+  }, "<" + type + "> that is not connected should not be followed");
 
-async_test(t => {
-  const target = document.createElement("iframe"),
-        doc = document.implementation.createDocument("", ""),
-        link = doc.createElementNS("http://www.w3.org/1999/xhtml", "a");
-  t.add_cleanup(() => target.remove());
-  target.name = "certifiedrandom2";
-  link.target = "certifiedrandom2";
-  link.href = "/";
-  document.body.appendChild(target);
-  target.onload = t.step_func(() => {
-    if(target.contentWindow.location.href === "about:blank")
-      return;
-    assert_unreached();
-  });
-  link.click();
-  t.step_timeout(() => t.done(), 500);
-}, "<a> that is from an inactive document should not be followed");
+  async_test(t => {
+    const target = document.createElement("iframe"),
+          doc = document.implementation.createDocument("", ""),
+          link = doc.createElementNS("http://www.w3.org/1999/xhtml", type);
+    t.add_cleanup(() => target.remove());
+    target.name = "certifiedrandom2";
+    link.target = "certifiedrandom2";
+    link.href = "/";
+    document.body.appendChild(target);
+    target.onload = t.step_func(() => {
+      if(target.contentWindow.location.href === "about:blank")
+        return;
+      assert_unreached();
+    });
+    link.click();
+    t.step_timeout(() => t.done(), 500);
+  }, "<" + type + "> that is from an inactive document should not be followed");
+});

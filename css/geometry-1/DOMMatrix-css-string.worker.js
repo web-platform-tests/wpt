75868927f2d53d1169683838268a89494f635e23
Simon Pieters <zcorpan@gmail.com>
2017-05-04 13:26:23 +0200

    Address review comments

--- css/geometry-1/DOMMatrix-css-string.worker.js
+++ css/geometry-1/DOMMatrix-css-string.worker.js
@@ -4,7 +4,7 @@ importScripts("/resources/testharness.js");
 
 ['DOMMatrix', 'DOMMatrixReadOnly'].forEach(constr => {
   test(() => {
-    assert_true(constr in self, `${constr} is not supported`);
+    assert_true(constr in self, `${constr} should exist`);
     assert_throws(new TypeError(), () => new self[constr]('matrix(1,0,0,1,0,0)') );
   }, `${constr} constructor with string argument in worker`);
 });

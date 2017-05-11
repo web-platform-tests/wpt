e7560d75d8e3ff0968ec5a7cd1150eb450465a04
Mike Pennisi <mike@mikepennisi.com>
2017-05-11 18:17:42 -0400

    fixup! Introduce automated tests for testharness.js

--- ci_unittest.sh
+++ ci_unittest.sh
@@ -2,6 +2,8 @@
 set -e
 
 ROOT=$PWD
+geckodriver_url=https://github.com/mozilla/geckodriver/releases/download/v0.16.1/geckodriver-v0.16.1-linux64.tar.gz
+
 pip install -U tox codecov
 cd tools
 tox
@@ -9,6 +11,10 @@ tox
 if [ $TOXENV == "py27" ] || [ $TOXENV == "pypy" ]; then
   cd wptrunner
   tox
+
+  cd $ROOT/resources/test
+  curl --location $geckodriver_url | tar -xvz
+  PATH=$PATH:. tox
 fi
 
 cd $ROOT

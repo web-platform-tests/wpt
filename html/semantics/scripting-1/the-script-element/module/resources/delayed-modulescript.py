16af726c86c08b07d4a63f07bc9dd1d9d0358b85
kolos <kolos@chromium.org>
2017-05-17 02:59:14 -0700

    Revert of [ES6 modules] ModuleMap::GetFetchedModuleScript to return nullptr when entry not found / "fetching" (patchset #5 id:80001 of https://codereview.chromium.org/2860993002/ )
    Reason for revert:
    This CL probably causes test failures
     external/wpt/html/semantics/scripting-1/the-script-element/module/import-subgraph-404.html
    
    The first failed build:
    https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux%20Trusty%20Leak/builds/4742
    
    Original issue's description:
    > [ES6 modules] ModuleMap::GetFetchedModuleScript to return nullptr when entry not found / "fetching"
    >
    > Before this CL, we asserted that ModuleMap::GetFetchedModuleScript always queried load completed module scripts, but it was not always the case.
    > This CL changes the member function to return nullptr if the entry doesn't exist or the entry is being fetched.
    >
    > This CL fixes a crash that happens when we early-exit module script graph fetch by a sub-graph instantiation failed, but there are still partial subgraph in flight.
    >
    > BUG=718442,594639
    >
    > Review-Url: https://codereview.chromium.org/2860993002
    > Cr-Commit-Position: refs/heads/master@{#472192}
    > Committed: https://chromium.googlesource.com/chromium/src/+/1f92cfbdedaecb97e8a87773deb32f98c6997351
    
    TBR=domenic@chromium.org,module-dev@chromium.org,neis@chromium.org,hiroshige@chromium.org,kouhei@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=718442,594639
    
    Review-Url: https://codereview.chromium.org/2886123002
    Cr-Commit-Position: refs/heads/master@{#472402}

--- html/semantics/scripting-1/the-script-element/module/resources/delayed-modulescript.py
+++
@@ -1,7 +0,0 @@
-import time
-
-def main(request, response):
-    delay = float(request.GET.first("ms", 500))
-    time.sleep(delay / 1E3);
-
-    return [("Content-type", "text/javascript")], "export let delayedLoaded = true;"

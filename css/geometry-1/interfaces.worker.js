030bb3595494ee1a8baf1b0d115487a70b6ee518
Simon Pieters <zcorpan@gmail.com>
2017-05-18 21:42:41 +0200

    Fix lint errors

--- css/geometry-1/interfaces.worker.js
+++ css/geometry-1/interfaces.worker.js
@@ -1,5 +1,9 @@
 "use strict";
+// https://drafts.fxtf.org/geometry/#DOMPoint
+// https://drafts.fxtf.org/geometry/#DOMRect
+// https://drafts.fxtf.org/geometry/#DOMQuad
+// https://drafts.fxtf.org/geometry/#DOMMatrix
 
 importScripts("/resources/testharness.js");
 importScripts("/resources/WebIDLParser.js", "/resources/idlharness.js");
-importScripts("interfaces.js");
+importScripts("support/interfaces.js");

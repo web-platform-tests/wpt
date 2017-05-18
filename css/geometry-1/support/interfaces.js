6d7f85da00d13a8c2762f61b194c6c07513cf0dc
Simon Pieters <zcorpan@gmail.com>
2017-05-18 21:49:11 +0200

    Add objects for all interfaces; also 3D matrices

--- css/geometry-1/support/interfaces.js
+++ css/geometry-1/support/interfaces.js
@@ -5,9 +5,13 @@ var idlArray = new IdlArray();
 function doTest(idl) {
   idlArray.add_idls(idl);
   idlArray.add_objects({
+    DOMPointReadOnly: ["new DOMPointReadOnly()"],
     DOMPoint: ["new DOMPoint()"],
+    DOMRectReadOnly: ["new DOMRectReadOnly()"],
     DOMRect: ["new DOMRect()"],
-    DOMMatrix: ["new DOMMatrix()"],
+    DOMQuad: ["new DOMQuad()"],
+    DOMMatrixReadOnly: ["new DOMMatrixReadOnly()", "DOMMatrixReadOnly.fromMatrix({is2D: false})"],
+    DOMMatrix: ["new DOMMatrix()", "DOMMatrix.fromMatrix({is2D: false})"],
   });
   idlArray.test();
   done();

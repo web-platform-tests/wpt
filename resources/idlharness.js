b8ba2ca9db7a824482ee1df887763544acdbbc68
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2017-05-19 12:02:31 +0200

    idlharness: Do not negate descriptions in assert_true/false() calls.
    An assertion in the form of `assert_true(foo.enumerable, "foo is not
    enumerable")` leads to a confusing error message like this:
    
        assert_true: foo is not enumerable expected true got false
    
    which is the opposite of what we are actually expecting.
    
    Conversely, `assert_false(foo.enumerable, "foo is not enumerable")` leads to
    an equally confusing message such as:
    
        assert_false: foo is not enumerable expected false got true
    
    which can be read as "expected 'foo not to be enumerable' to be false, hence
    expected foo to be enumerable" which is also the opposite of what we want.
    
    Standardize on not including negations in the descriptions to avoid these
    pitfalls.

--- resources/idlharness.js
+++ resources/idlharness.js
@@ -489,7 +489,7 @@ IdlArray.prototype.assert_type_is = function(value, type)
 
     if (type.sequence)
     {
-        assert_true(Array.isArray(value), "is not array");
+        assert_true(Array.isArray(value), "is an Array");
         if (!value.length)
         {
             // Nothing we can do.
@@ -534,37 +534,37 @@ IdlArray.prototype.assert_type_is = function(value, type)
         case "byte":
             assert_equals(typeof value, "number");
             assert_equals(value, Math.floor(value), "not an integer");
-            assert_true(-128 <= value && value <= 127, "byte " + value + " not in range [-128, 127]");
+            assert_true(-128 <= value && value <= 127, "byte " + value + " is in range [-128, 127]");
             return;
 
         case "octet":
             assert_equals(typeof value, "number");
             assert_equals(value, Math.floor(value), "not an integer");
-            assert_true(0 <= value && value <= 255, "octet " + value + " not in range [0, 255]");
+            assert_true(0 <= value && value <= 255, "octet " + value + " is in range [0, 255]");
             return;
 
         case "short":
             assert_equals(typeof value, "number");
             assert_equals(value, Math.floor(value), "not an integer");
-            assert_true(-32768 <= value && value <= 32767, "short " + value + " not in range [-32768, 32767]");
+            assert_true(-32768 <= value && value <= 32767, "short " + value + " is in range [-32768, 32767]");
             return;
 
         case "unsigned short":
             assert_equals(typeof value, "number");
             assert_equals(value, Math.floor(value), "not an integer");
-            assert_true(0 <= value && value <= 65535, "unsigned short " + value + " not in range [0, 65535]");
+            assert_true(0 <= value && value <= 65535, "unsigned short " + value + " is in range [0, 65535]");
             return;
 
         case "long":
             assert_equals(typeof value, "number");
             assert_equals(value, Math.floor(value), "not an integer");
-            assert_true(-2147483648 <= value && value <= 2147483647, "long " + value + " not in range [-2147483648, 2147483647]");
+            assert_true(-2147483648 <= value && value <= 2147483647, "long " + value + " is in range [-2147483648, 2147483647]");
             return;
 
         case "unsigned long":
             assert_equals(typeof value, "number");
             assert_equals(value, Math.floor(value), "not an integer");
-            assert_true(0 <= value && value <= 4294967295, "unsigned long " + value + " not in range [0, 4294967295]");
+            assert_true(0 <= value && value <= 4294967295, "unsigned long " + value + " is in range [0, 4294967295]");
             return;
 
         case "long long":
@@ -574,7 +574,7 @@ IdlArray.prototype.assert_type_is = function(value, type)
         case "unsigned long long":
         case "DOMTimeStamp":
             assert_equals(typeof value, "number");
-            assert_true(0 <= value, "unsigned long long is negative");
+            assert_true(0 <= value, "unsigned long long is positive");
             return;
 
         case "float":
@@ -638,7 +638,7 @@ IdlArray.prototype.assert_type_is = function(value, type)
         && !this.members[type].has_extended_attribute("NoInterfaceObject")
         && type in self)
         {
-            assert_true(value instanceof self[type], "not instanceof " + type);
+            assert_true(value instanceof self[type], "instanceof " + type);
         }
     }
     else if (this.members[type] instanceof IdlEnum)
@@ -840,9 +840,9 @@ IdlInterface.prototype.test_self = function()
         var desc = Object.getOwnPropertyDescriptor(self, this.name);
         assert_false("get" in desc, "self's property " + format_value(this.name) + " has getter");
         assert_false("set" in desc, "self's property " + format_value(this.name) + " has setter");
-        assert_true(desc.writable, "self's property " + format_value(this.name) + " is not writable");
+        assert_true(desc.writable, "self's property " + format_value(this.name) + " is writable");
         assert_false(desc.enumerable, "self's property " + format_value(this.name) + " is enumerable");
-        assert_true(desc.configurable, "self's property " + format_value(this.name) + " is not configurable");
+        assert_true(desc.configurable, "self's property " + format_value(this.name) + " is configurable");
 
         if (this.is_callback()) {
             // "The internal [[Prototype]] property of an interface object for
@@ -939,7 +939,7 @@ IdlInterface.prototype.test_self = function()
             assert_false("set" in desc, this.name + ".length has setter");
             assert_false(desc.writable, this.name + ".length is writable");
             assert_false(desc.enumerable, this.name + ".length is enumerable");
-            assert_true(desc.configurable, this.name + ".length is not configurable");
+            assert_true(desc.configurable, this.name + ".length is configurable");
 
             var constructors = this.extAttrs
                 .filter(function(attr) { return attr.name == "Constructor"; });
@@ -967,7 +967,7 @@ IdlInterface.prototype.test_self = function()
             assert_false("set" in desc, this.name + ".name has setter");
             assert_false(desc.writable, this.name + ".name is writable");
             assert_false(desc.enumerable, this.name + ".name is enumerable");
-            assert_true(desc.configurable, this.name + ".name is not configurable");
+            assert_true(desc.configurable, this.name + ".name is configurable");
             assert_equals(self[this.name].name, this.name, "wrong value for " + this.name + ".name");
         }.bind(this), this.name + " interface object name");
     }
@@ -1010,15 +1010,15 @@ IdlInterface.prototype.test_self = function()
                     assert_equals(self[alias], self[this.name], "self." + alias + " should be the same value as self." + this.name);
                     var desc = Object.getOwnPropertyDescriptor(self, alias);
                     assert_equals(desc.value, self[this.name], "wrong value in " + alias + " property descriptor");
-                    assert_true(desc.writable, alias + " is not writable");
+                    assert_true(desc.writable, alias + " is writable");
                     assert_false(desc.enumerable, alias + " is enumerable");
-                    assert_true(desc.configurable, alias + " is not configurable");
+                    assert_true(desc.configurable, alias + " is configurable");
                     assert_false('get' in desc, alias + " has a getter");
                     assert_false('set' in desc, alias + " has a setter");
                 }
             } else {
                 for (alias of aliases) {
-                    assert_false(alias in self, alias + " should not exist");
+                    assert_false(alias in self, alias + " exists");
                 }
             }
 
@@ -1040,7 +1040,7 @@ IdlInterface.prototype.test_self = function()
 
         if (this.is_callback()) {
             assert_false("prototype" in self[this.name],
-                         this.name + ' should not have a "prototype" property');
+                         this.name + ' should have a "prototype" property');
             return;
         }
 
@@ -1148,7 +1148,7 @@ IdlInterface.prototype.test_self = function()
 
         if (this.is_callback()) {
             assert_false("prototype" in self[this.name],
-                         this.name + ' should not have a "prototype" property');
+                         this.name + ' has a "prototype" property');
             return;
         }
 
@@ -1165,9 +1165,9 @@ IdlInterface.prototype.test_self = function()
         var desc = Object.getOwnPropertyDescriptor(self[this.name].prototype, "constructor");
         assert_false("get" in desc, this.name + ".prototype.constructor has getter");
         assert_false("set" in desc, this.name + ".prototype.constructor has setter");
-        assert_true(desc.writable, this.name + ".prototype.constructor is not writable");
+        assert_true(desc.writable, this.name + ".prototype.constructor is writable");
         assert_false(desc.enumerable, this.name + ".prototype.constructor is enumerable");
-        assert_true(desc.configurable, this.name + ".prototype.constructor in not configurable");
+        assert_true(desc.configurable, this.name + ".prototype.constructor is configurable");
         assert_equals(self[this.name].prototype.constructor, self[this.name],
                       this.name + '.prototype.constructor is not the same object as ' + this.name);
     }.bind(this), this.name + ' interface: existence and properties of interface prototype object\'s "constructor" property');
@@ -1201,7 +1201,7 @@ IdlInterface.prototype.test_member_const = function(member)
         assert_false("get" in desc, "property has getter");
         assert_false("set" in desc, "property has setter");
         assert_false(desc.writable, "property is writable");
-        assert_true(desc.enumerable, "property is not enumerable");
+        assert_true(desc.enumerable, "property is enumerable");
         assert_false(desc.configurable, "property is configurable");
     }.bind(this), this.name + " interface: constant " + member.name + " on interface object");
 
@@ -1214,7 +1214,7 @@ IdlInterface.prototype.test_member_const = function(member)
 
         if (this.is_callback()) {
             assert_false("prototype" in self[this.name],
-                         this.name + ' should not have a "prototype" property');
+                         this.name + ' has a "prototype" property');
             return;
         }
 
@@ -1228,7 +1228,7 @@ IdlInterface.prototype.test_member_const = function(member)
         assert_false("get" in desc, "property has getter");
         assert_false("set" in desc, "property has setter");
         assert_false(desc.writable, "property is writable");
-        assert_true(desc.enumerable, "property is not enumerable");
+        assert_true(desc.enumerable, "property is enumerable");
         assert_false(desc.configurable, "property is configurable");
     }.bind(this), this.name + " interface: constant " + member.name + " on interface prototype object");
 };
@@ -1261,7 +1261,7 @@ IdlInterface.prototype.test_member_attribute = function(member)
                 "The global object must have a property " +
                 format_value(member.name));
             assert_false(member.name in self[this.name].prototype,
-                "The prototype object must not have a property " +
+                "The prototype object has a property " +
                 format_value(member.name));
 
             var getter = Object.getOwnPropertyDescriptor(self, member.name).get;
@@ -1345,7 +1345,7 @@ IdlInterface.prototype.test_member_operation = function(member)
 
         if (this.is_callback()) {
             assert_false("prototype" in self[this.name],
-                         this.name + ' should not have a "prototype" property');
+                         this.name + ' has a "prototype" property');
             a_test.done();
             return;
         }
@@ -1400,7 +1400,7 @@ IdlInterface.prototype.do_member_operation_asserts = function(memberHolderObject
     assert_false("set" in desc, "property has setter");
     assert_equals(desc.writable, !operationUnforgeable,
                   "property should be writable if and only if not unforgeable");
-    assert_true(desc.enumerable, "property is not enumerable");
+    assert_true(desc.enumerable, "property is enumerable");
     assert_equals(desc.configurable, !operationUnforgeable,
                   "property should be configurable if and only if not unforgeable");
     // "The value of the property is a Function object whose
@@ -1483,8 +1483,8 @@ IdlInterface.prototype.test_member_iterable = function(member)
     test(function()
     {
         var descriptor = Object.getOwnPropertyDescriptor(self[interfaceName].prototype, Symbol.iterator);
-        assert_true(descriptor.writable, "property is not writable");
-        assert_true(descriptor.configurable, "property is not configurable");
+        assert_true(descriptor.writable, "property is writable");
+        assert_true(descriptor.configurable, "property is configurable");
         assert_false(descriptor.enumerable, "property is enumerable");
         assert_equals(self[interfaceName].prototype[Symbol.iterator].name, isPairIterator ? "entries" : "values", "@@iterator function does not have the right name");
     }, "Testing Symbol.iterator property of iterable interface " + interfaceName);
@@ -1517,7 +1517,7 @@ IdlInterface.prototype.test_member_stringifier = function(member)
 
         if (this.is_callback()) {
             assert_false("prototype" in self[this.name],
-                         this.name + ' should not have a "prototype" property');
+                         this.name + ' has a "prototype" property');
             return;
         }
 
@@ -1538,7 +1538,7 @@ IdlInterface.prototype.test_member_stringifier = function(member)
         assert_false("set" in desc, "property has setter");
         assert_equals(desc.writable, !stringifierUnforgeable,
                       "property should be writable if and only if not unforgeable");
-        assert_true(desc.enumerable, "property is not enumerable");
+        assert_true(desc.enumerable, "property is enumerable");
         assert_equals(desc.configurable, !stringifierUnforgeable,
                       "property should be configurable if and only if not unforgeable");
         // "The value of the property is a Function object, which behaves as
@@ -1594,10 +1594,10 @@ IdlInterface.prototype.test_members = function()
             test(function() {
                 // It's not exposed, so we shouldn't find it anywhere.
                 assert_false(member.name in self[this.name],
-                             "The interface object must not have a property " +
+                             "The interface object has a property " +
                              format_value(member.name));
                 assert_false(member.name in self[this.name].prototype,
-                             "The prototype object must not have a property " +
+                             "The prototype object has a property " +
                              format_value(member.name));
             }.bind(this), this.name + " interface: member " + member.name);
             continue;
@@ -1906,12 +1906,12 @@ IdlInterface.prototype.do_interface_attribute_asserts = function(obj, member, a_
     // "G is the attribute getter, defined below; and
     // "S is the attribute setter, also defined below."
     var desc = Object.getOwnPropertyDescriptor(obj, member.name);
-    assert_false("value" in desc, 'property descriptor has value but is supposed to be accessor');
-    assert_false("writable" in desc, 'property descriptor has "writable" field but is supposed to be accessor');
-    assert_true(desc.enumerable, "property is not enumerable");
+    assert_false("value" in desc, 'property descriptor has "value" field');
+    assert_false("writable" in desc, 'property descriptor has "writable" field');
+    assert_true(desc.enumerable, "property is enumerable");
     if (member.isUnforgeable)
     {
-        assert_false(desc.configurable, "[Unforgeable] property must not be configurable");
+        assert_false(desc.configurable, "[Unforgeable] property is configurable");
     }
     else
     {

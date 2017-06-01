636edb8d2055885c04b8e973616b61fe7994a9ff
Mike Pennisi <mike@mikepennisi.com>
2017-06-01 19:37:55 -0400

    [testharness] Handle errors during cleanup
    Prior to application of this patch, `testharness.js` did not catch
    errors thrown by cleanup functions. In this case, script execution was
    interrupted, so additional cleanup functions were not invoked. Further,
    any subsequently-defined tests were not run, but neither were they
    detected or reported. The framework reported the error via the global
    `onerror` event, exposing the implementation-defined `message` property
    in the resulting failure report.
    
    Ensure that script evaluation proceeds following an error thrown by a
    cleanup function. This includes invocation of any remaining cleanup
    functions and detection of remaining tests. Additional tests are
    explicitly not executed due to the general instability of the global
    state under these conditions, but they are reported with the status
    "NOTRUN". Update the reported test harness error message for this
    situation to describe the situation in terms that are consistent across
    implementations.

--- resources/testharness.js
+++ resources/testharness.js
@@ -1323,7 +1323,8 @@ policies and contribution forms [3].
         }
         this.name = name;
 
-        this.phase = this.phases.INITIAL;
+        this.phase = tests.phase === tests.phases.ABORTED ?
+            this.phases.COMPLETE : this.phases.INITIAL;
 
         this.status = this.NOTRUN;
         this.timeout_id = null;
@@ -1519,11 +1520,35 @@ policies and contribution forms [3].
         this.cleanup();
     };
 
+    /*
+     * Invoke all specified cleanup functions. If one or more produce an error,
+     * the context is in an unpredictable state, so all further testing should
+     * be cancelled.
+     */
     Test.prototype.cleanup = function() {
+        var error_count = 0;
+        var total;
+
         forEach(this.cleanup_callbacks,
                 function(cleanup_callback) {
-                    cleanup_callback();
+                    try {
+                        cleanup_callback();
+                    } catch (e) {
+                        // Set test phase immediately so that tests declared
+                        // within subsequent cleanup functions are not run.
+                        tests.phase = tests.phases.ABORTED;
+                        error_count += 1;
+                    }
                 });
+
+        if (error_count > 0) {
+            total = this.cleanup_callbacks.length;
+            tests.status.status = tests.status.ERROR;
+            tests.status.message = "Test named '" + this.name +
+                "' specified " + total + " 'cleanup' function" +
+                (total > 1 ? "s" : "") + ", and " + error_count + " failed.";
+            tests.status.stack = null;
+        }
     };
 
     /*
@@ -1707,7 +1732,8 @@ policies and contribution forms [3].
             SETUP:1,
             HAVE_TESTS:2,
             HAVE_RESULTS:3,
-            COMPLETE:4
+            ABORTED:4,
+            COMPLETE:5
         };
         this.phase = this.phases.INITIAL;
 
@@ -1841,7 +1867,8 @@ policies and contribution forms [3].
     };
 
     Tests.prototype.all_done = function() {
-        return (this.tests.length > 0 && test_environment.all_loaded &&
+        return this.phase === this.phases.ABORTED ||
+            (this.tests.length > 0 && test_environment.all_loaded &&
                 this.num_pending === 0 && !this.wait_for_finish &&
                 !this.processing_callbacks &&
                 !this.pending_remotes.some(function(w) { return w.running; }));

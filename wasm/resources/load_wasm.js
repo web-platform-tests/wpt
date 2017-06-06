50810697e85c07d917425ad1f258e9d31b3dbc70
Mircea Trofin <mtrofin@chromium.org>
2017-06-06 13:50:58 -0700

    [wasm] move out of wpt chrome-specific downgrade test.
    Testing downgrades uses chrome-specific internal test APIs.
    
    Also removed redundant comment about a utility wasm file.
    
    Bug: chromium:724151
    Change-Id: I8c690de0c5884ad3323c8d634691330860f2aee6
    Reviewed-on: https://chromium-review.googlesource.com/524575
    Commit-Queue: Brad Nelson <bradnelson@chromium.org>
    Reviewed-by: Brad Nelson <bradnelson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477100}

--- wasm/resources/load_wasm.js
+++ wasm/resources/load_wasm.js
@@ -3,12 +3,6 @@
 // found in the LICENSE file.
 
 function createWasmModule() {
-    // the file incrementer.wasm is copied from
-    // //v8/test/mjsunit/wasm. This is because currently we cannot
-    // reference files outside the LayoutTests folder. When wasm format
-    // changes require that file to be updated, there is a test on the
-    // v8 side (same folder), ensure-wasm-binaries-up-to-date.js, which
-    // fails and will require incrementer.wasm to be updated on that side.
     return fetch('incrementer.wasm')
         .then(response => {
             if (!response.ok) throw new Error(response.statusText);

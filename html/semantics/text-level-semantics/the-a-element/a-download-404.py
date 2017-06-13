4fde5a36076616d8fed4e60c48aabcc3149895e7
PhistucK <phistuck@gmail.com>
2017-06-13 19:32:24 +0300

    Added callback support

--- html/semantics/text-level-semantics/the-a-element/a-download-404.py
+++ html/semantics/text-level-semantics/the-a-element/a-download-404.py
@@ -1,2 +1,8 @@
 def main(request, response):
-    return 404, [("Content-Type", "text/html")], 'Some content for the masses.' * 100
+    return 404, [("Content-Type", "text/html")], """<!DOCTYPE html>
+<html>
+   <head>
+    <script>parent.%s()</script>
+   </head>
+</html>
+""" % request.GET.first('callbackName')

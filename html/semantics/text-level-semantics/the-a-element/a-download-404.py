e197534537784e3a859d652d7e4654d2fa9a5218
PhistucK <phistuck@gmail.com>
2017-06-14 19:36:16 +0300

    Revert "Added callback support"
    This reverts commit 4fde5a36076616d8fed4e60c48aabcc3149895e7.

--- html/semantics/text-level-semantics/the-a-element/a-download-404.py
+++ html/semantics/text-level-semantics/the-a-element/a-download-404.py
@@ -1,8 +1,2 @@
 def main(request, response):
-    return 404, [("Content-Type", "text/html")], """<!DOCTYPE html>
-<html>
-   <head>
-    <script>parent.%s()</script>
-   </head>
-</html>
-""" % request.GET.first('callbackName')
+    return 404, [("Content-Type", "text/html")], 'Some content for the masses.' * 100

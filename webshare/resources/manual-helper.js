b03a563a38e430d9170f06576925d57db4f91eeb
Matt Giuca <mgiuca@chromium.org>
2017-06-20 14:05:56 +1000

    Added new tests directory: webshare.
    Adds comprehensive tests for Web Share API drafted here:
    https://wicg.github.io/web-share/
    
    Most tests are manual, which is necessary, given the nature of the API
    (requires user gesture and interaction with user interface).

---
+++ webshare/resources/manual-helper.js
@@ -0,0 +1,67 @@
+// Sets up the page for running manual tests. Automatically creates the
+// instructions (based on the parameters) and the share button.
+function setupManualShareTest(
+    user_should_cancel, expected_title, expected_text, expected_url) {
+  let div = document.createElement('div');
+  document.body.appendChild(div);
+
+  let instruction = document.createElement('div');
+  instruction.id = 'instruction';
+  div.appendChild(instruction);
+
+  let shareButton = document.createElement('input');
+  shareButton.id = 'share_button';
+  shareButton.value = 'Share button';
+  shareButton.type = 'button';
+  div.appendChild(shareButton);
+
+  let heading = document.createElement('h2');
+  heading.innerText = 'Instructions:';
+  instruction.appendChild(heading);
+  let list = document.createElement('ol');
+  instruction.appendChild(list);
+  let item = document.createElement('li');
+  list.appendChild(item);
+  item.innerText = 'Click the Share button.';
+  item = document.createElement('li');
+
+  if (user_should_cancel) {
+    list.appendChild(item);
+    item.innerText = 'Cancel the share dialog.';
+    return;
+  }
+
+  list.appendChild(item);
+  item.innerText = 'Choose a valid share target.';
+
+  heading = document.createElement('h2');
+  heading.innerText = 'Pass the test iff the target app received:';
+  instruction.appendChild(heading);
+
+  list = document.createElement('ul');
+  instruction.appendChild(list);
+
+  item = document.createElement('li');
+  list.appendChild(item);
+  item.innerText = `title = "${expected_title}"`;
+  item = document.createElement('li');
+  list.appendChild(item);
+  item.innerText = `text = "${expected_text}"`;
+  item = document.createElement('li');
+  list.appendChild(item);
+  item.innerText = `url = "${expected_url}"`;
+}
+
+// Returns a promise. When the user clicks the button, calls
+// |click_handler| and resolves the promise with the result.
+function callWhenButtonClicked(click_handler) {
+  return new Promise(resolve => {
+    document.querySelector('#share_button').onclick = () => {
+      resolve(click_handler());
+    };
+  });
+}
+
+function getAbsoluteUrl(url) {
+  return new URL(url, location).toString();
+}

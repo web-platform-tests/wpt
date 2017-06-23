9bf14822b507f9d29c4e5244fb67717defa2c564
James Graham <james@hoppipolla.co.uk>
2017-06-23 13:52:06 +0100

    Use wpt.testfiles module for getting affected files in stability checker

--- check_stability.py
+++ check_stability.py
@@ -15,7 +15,10 @@ from cStringIO import StringIO as CStringIO
 from collections import defaultdict, OrderedDict
 from io import BytesIO, StringIO
 
+from tools.wpt import testfiles
+
 import requests
+
 
 BaseHandler = None
 LogActionFilter = None
@@ -434,163 +437,18 @@ def build_manifest():
     from tools.manifest import update
     update.run(**vars(update.create_parser().parse_args([])))
 
+
 def install_wptrunner():
     """Install wptrunner."""
     call("pip", "install", wptrunner_root)
 
 
-def get_branch_point(user):
-    git = get_git_cmd(wpt_root)
-    if os.environ.get("TRAVIS_PULL_REQUEST", "false") != "false":
-        # This is a PR, so the base branch is in TRAVIS_BRANCH
-        travis_branch = os.environ.get("TRAVIS_BRANCH")
-        assert travis_branch, "TRAVIS_BRANCH environment variable is defined"
-        branch_point = git("rev-parse", travis_branch)
-    else:
-        # Otherwise we aren't on a PR, so we try to find commits that are only in the
-        # current branch c.f.
-        # http://stackoverflow.com/questions/13460152/find-first-ancestor-commit-in-another-branch
-        head = git("rev-parse", "HEAD")
-        # To do this we need all the commits in the local copy
-        fetch_args = [user, "+refs/heads/*:refs/remotes/origin/*"]
-        if os.path.exists(os.path.join(wpt_root, ".git", "shallow")):
-            fetch_args.insert(1, "--unshallow")
-        fetch_wpt(*fetch_args)
-        not_heads = [item for item in git("rev-parse", "--not", "--all").split("\n")
-                     if item.strip() and not head in item]
-        commits = git("rev-list", "HEAD", *not_heads).split("\n")
-        branch_point = None
-        if len(commits):
-            first_commit = commits[-1]
-            if first_commit:
-                branch_point = git("rev-parse", first_commit + "^")
-
-        # The above heuristic will fail in the following cases:
-        #
-        # - The current branch has fallen behind the version retrieved via the above
-        #   `fetch` invocation
-        # - Changes on the current branch were rebased and therefore do not exist on any
-        #   other branch. This will result in the selection of a commit that is earlier
-        #   in the history than desired (as determined by calculating the later of the
-        #   branch point and the merge base)
-        #
-        # In either case, fall back to using the merge base as the branch point.
-        merge_base = git("merge-base", "HEAD", "origin/master")
-        if (branch_point is None or
-            (branch_point != merge_base and
-             not git("log", "--oneline", "%s..%s" % (merge_base, branch_point)).strip())):
-            logger.debug("Using merge-base as the branch point")
-            branch_point = merge_base
-        else:
-            logger.debug("Using first commit on another branch as the branch point")
-
-    logger.debug("Branch point from master: %s" % branch_point)
-    return branch_point
-
-
-def get_files_changed(branch_point, ignore_changes):
-    """Get and return files changed since current branch diverged from master,
-    excluding those that are located within any directory specifed by
-    `ignore_changes`."""
-    root = os.path.abspath(os.curdir)
-    git = get_git_cmd(wpt_root)
-    files = git("diff", "--name-only", "-z", "%s..." % branch_point)
-    if not files:
-        return [], []
-    assert files[-1] == "\0"
-
-    changed = []
-    ignored = []
-    for item in files[:-1].split("\0"):
-        fullpath = os.path.join(wpt_root, item)
-        topmost_dir = item.split(os.sep, 1)[0]
-        if topmost_dir in ignore_changes:
-            ignored.append(fullpath)
-        else:
-            changed.append(fullpath)
-
-    return changed, ignored
-
-
-def _in_repo_root(full_path):
-    rel_path = os.path.relpath(full_path, wpt_root)
-    path_components = rel_path.split(os.sep)
-    return len(path_components) < 2
-
-
-def get_affected_testfiles(files_changed, skip_tests):
-    """Determine and return list of test files that reference changed files."""
-    affected_testfiles = set()
-    # Exclude files that are in the repo root, because
-    # they are not part of any test.
-    files_changed = [f for f in files_changed if not _in_repo_root(f)]
-    nontests_changed = set(files_changed)
-    manifest_file = os.path.join(wpt_root, "MANIFEST.json")
-    test_types = ["testharness", "reftest", "wdspec"]
-
-    wpt_manifest = manifest.load(wpt_root, manifest_file)
-
-    support_files = {os.path.join(wpt_root, path)
-                     for _, path, _ in wpt_manifest.itertypes("support")}
-    wdspec_test_files = {os.path.join(wpt_root, path)
-                         for _, path, _ in wpt_manifest.itertypes("wdspec")}
-    test_files = {os.path.join(wpt_root, path)
-                  for _, path, _ in wpt_manifest.itertypes(*test_types)}
-
-    nontests_changed = nontests_changed.intersection(support_files)
-
-    nontest_changed_paths = set()
-    for full_path in nontests_changed:
-        rel_path = os.path.relpath(full_path, wpt_root)
-        path_components = rel_path.split(os.sep)
-        top_level_subdir = path_components[0]
-        if top_level_subdir in skip_tests:
-            continue
-        repo_path = "/" + os.path.relpath(full_path, wpt_root).replace(os.path.sep, "/")
-        nontest_changed_paths.add((full_path, repo_path))
-
-    def affected_by_wdspec(test):
-        affected = False
-        if test in wdspec_test_files:
-            for support_full_path, _ in nontest_changed_paths:
-                # parent of support file or of "support" directory
-                parent = os.path.dirname(support_full_path)
-                if os.path.basename(parent) == "support":
-                    parent = os.path.dirname(parent)
-                relpath = os.path.relpath(test, parent)
-                if not relpath.startswith(os.pardir):
-                    # testfile is in subtree of support file
-                    affected = True
-                    break
-        return affected
-
-    for root, dirs, fnames in os.walk(wpt_root):
-        # Walk top_level_subdir looking for test files containing either the
-        # relative filepath or absolute filepath to the changed files.
-        if root == wpt_root:
-            for dir_name in skip_tests:
-                dirs.remove(dir_name)
-        for fname in fnames:
-            test_full_path = os.path.join(root, fname)
-            # Skip any file that's not a test file.
-            if test_full_path not in test_files:
-                continue
-            if affected_by_wdspec(test_full_path):
-                affected_testfiles.add(test_full_path)
-                continue
-
-            with open(test_full_path, "rb") as fh:
-                file_contents = fh.read()
-                if file_contents.startswith("\xfe\xff"):
-                    file_contents = file_contents.decode("utf-16be")
-                elif file_contents.startswith("\xff\xfe"):
-                    file_contents = file_contents.decode("utf-16le")
-                for full_path, repo_path in nontest_changed_paths:
-                    rel_path = os.path.relpath(full_path, root).replace(os.path.sep, "/")
-                    if rel_path in file_contents or repo_path in file_contents:
-                        affected_testfiles.add(test_full_path)
-                        continue
-    return affected_testfiles
+def deepen_checkout():
+    """Convert from a shallow checkout to a full one"""
+    fetch_args = [user, "+refs/heads/*:refs/remotes/origin/*"]
+    if os.path.exists(os.path.join(wpt_root, ".git", "shallow")):
+        fetch_args.insert(1, "--unshallow")
+    fetch_wpt(*fetch_args)
 
 
 def wptrunner_args(root, files_changed, iterations, browser):
@@ -855,6 +713,11 @@ def get_parser():
     return parser
 
 
+def pr():
+    pr = os.environ.get("TRAVIS_PULL_REQUEST", "false")
+    return pr if pr != "false" else None
+
+
 def main():
     """Perform check_stability functionality and return exit code."""
     global wpt_root
@@ -892,6 +755,8 @@ def main():
         logger.warning("Cannot run tests on Sauce Labs. No access key.")
         return retcode
 
+    pr_number = pr()
+
     with TravisFold("browser_setup"):
         logger.info(format_comment_title(args.product))
 
@@ -907,11 +772,14 @@ def main():
         head_sha1 = get_sha1()
         logger.info("Testing web-platform-tests at revision %s" % head_sha1)
 
-        branch_point = get_branch_point(args.user)
+        if pr is not None:
+            deepen_checkout()
+
+        branch_point = testfiles.branch_point(args.user)
 
         # For now just pass the whole list of changed files to wptrunner and
         # assume that it will run everything that's actually a test
-        files_changed, files_ignored = get_files_changed(branch_point, ignore_changes)
+        files_changed, files_ignored = testfiles.files_changed("%s..HEAD" % branch_point, ignore_changes)
 
         if files_ignored:
             logger.info("Ignoring %s changed files:\n%s" % (len(files_ignored),
@@ -937,14 +805,14 @@ def main():
 
         logger.debug("Files changed:\n%s" % "".join(" * %s\n" % item for item in files_changed))
 
-        affected_testfiles = get_affected_testfiles(files_changed, skip_tests)
+        tests_changed, affected_testfiles = get_affected_testfiles(files_changed, skip_tests)
 
         logger.debug("Affected tests:\n%s" % "".join(" * %s\n" % item for item in affected_testfiles))
 
-        files_changed.extend(affected_testfiles)
+        wptrunner_files = list(itertools.chain(tests_changed, affected_testfiles)
 
         kwargs = wptrunner_args(args.root,
-                                files_changed,
+                                wptrunner_files,
                                 args.iterations,
                                 browser)
 

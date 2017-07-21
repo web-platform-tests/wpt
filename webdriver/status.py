91e716acca1db4994f052d3dc1abdb91d3679fba
Jonathan Lipps <jlipps@gmail.com>
2017-07-21 10:13:22 -0700

    fix non-utf8 copy/paste from spec, and incorrect kwarg

--- webdriver/status.py
+++ webdriver/status.py
@@ -6,7 +6,7 @@ import json
 # currently response body for GET /status without a session is
 # coming back empty
 def test_get_status_no_session(http):
-    with http.get("status", use_json=True) as response:
+    with http.get("status") as response:
         # GET /status should never return an error
         assert response.status == 200
 
@@ -14,9 +14,9 @@ def test_get_status_no_session(http):
 
         # Let body be a new JSON Object with the following properties:
         # "ready"
-        #       The remote end’s readiness state.
+        #       The remote end's readiness state.
         assert parsed_obj["ready"] in [True, False]
         # "message"
-        #       An implementation-defined string explaining the remote end’s
+        #       An implementation-defined string explaining the remote end's
         #       readiness state.
         assert isinstance(parsed_obj["message"], basestring)

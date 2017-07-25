ef8a53835dff7b58a579ec03bbfb8b3593dc7408
Andy Paicu <andypaicu@chromium.org>
2017-07-25 13:17:01 +0200

    RevBaseline for changed files to start step by step identification of issue

--- content-security-policy/support/report.py
+++ content-security-policy/support/report.py
@@ -1,66 +1,34 @@
 import time
 import json
 import re
-import math
-
-def retrieve_from_stash(request, key, attempts, default_value):
-  for attempt_no in range(attempts):
-    value = request.server.stash.take(key=key)
-    if value is not None:
-      return value
-    if attempt_no != attempts - 1:
-      time.sleep(3)
-
-  return default_value
 
 def main(request, response):
-  op = request.GET.first("op");
-  key = request.GET.first("reportID")
-  cookie_key = re.sub('^....', 'cccc', key)
-  count_key = re.sub('^....', 'dddd', key)
+    op = request.GET.first("op");
+    key = request.GET.first("reportID")
 
-  try:
-    timeout = float(request.GET.first("timeout"))
-    attempts = int(math.floor(timeout / 3))
-    attempts += 1
-  except:
-    attempts = 1
+    if op == "take":
+        timeout = float(request.GET.first("timeout"))
+        t0 = time.time()
+        while time.time() - t0 < timeout:
+            time.sleep(0.5)
+            value = request.server.stash.take(key=key)
+            if value is not None:
+                return [("Content-Type", "application/json")], value
 
-  attempts = 1
+        return [("Content-Type", "application/json")], json.dumps({'error': 'No such report.' , 'guid' : key})
 
-  if op == "retrieve_report":
-    return [("Content-Type", "application/json")], retrieve_from_stash(request, key, attempts, json.dumps({'error': 'No such report.' , 'guid' : key}))
+    if op == "cookies":
+        cval = request.server.stash.take(key=re.sub('^...', 'ccc', key))
+        if cval is None:
+            cval = "\"None\""
 
-  if op == "retrieve_cookies":
-    return [("Content-Type", "application/json")], "{ \"reportCookies\" : " + str(retrieve_from_stash(request, cookie_key, attempts, "\"None\"")) + "}"
+        return [("Content-Type", "application/json")], "{ \"reportCookies\" : " + cval + "}"
 
-  if op == "retrieve_count":
-    return [("Content-Type", "application/json")], json.dumps({'report_count': str(retrieve_from_stash(request, count_key, attempts, 0))})
+    if hasattr(request, 'Cookies'):
+        request.server.stash.put(key=re.sub('^...', 'ccc', key), value=request.Cookies)
 
-  # save cookies
-  if hasattr(request, 'cookies') and len(request.cookies.keys()) > 0:
-   # convert everything into strings and dump it into a dict so it can be jsoned
-    temp_cookies_dict = {}
-    for dict_key in request.cookies.keys():
-      temp_cookies_dict[str(dict_key)] = str(request.cookies.get_list(dict_key))
-    with request.server.stash.lock:
-      request.server.stash.take(key=cookie_key)
-      request.server.stash.put(key=cookie_key, value=json.dumps(temp_cookies_dict))
-
-  # save latest report
-  report = request.body
-  report.rstrip()
-  with request.server.stash.lock:
+    report = request.body
+    report.rstrip()
     request.server.stash.take(key=key)
     request.server.stash.put(key=key, value=report)
-
-  with request.server.stash.lock:
-    # increment report count
-    count = request.server.stash.take(key=count_key)
-    if count is None:
-      count = 0
-    count += 1
-    request.server.stash.put(key=count_key, value=count)
-
-  # return acknowledgement report
-  return [("Content-Type", "text/plain")], "Recorded report " + report
+    return [("Content-Type", "text/plain")], "Recorded report " + report

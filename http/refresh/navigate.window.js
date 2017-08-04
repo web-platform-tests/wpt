543d93c2f88ddcc6b76edaf2628cb10e46ff2a9c
Anne van Kesteren <annevk@annevk.nl>
2017-08-04 13:18:44 +0200

    More tests (we might want to organize differently, dunno)

--- http/refresh/navigate.window.js
+++ http/refresh/navigate.window.js
@@ -3,9 +3,21 @@ async_test(t => {
   frame.src = "resources/refresh.py"
   frame.onload = t.step_func(() => {
     // Could be better by verifying that resources/refresh.py loads too
-    if(frame.contentWindow.location.href === (new URL("resources/refreshed.txt", self.location)).href) {
+    if(frame.contentWindow.location.href === (new URL("resources/refreshed.txt?ÿ", self.location)).href) {
       t.done();
     }
   });
   document.body.appendChild(frame)
 }, "When navigating the Refresh header needs to be followed");
+
+async_test(t => {
+  const frame = document.createElement("iframe");
+  frame.src = "resources/multiple.asis"
+  frame.onload = t.step_func(() => {
+    // Could be better by verifying that resources/refresh.py loads too
+    if(frame.contentWindow.location.href === (new URL("resources/refreshed.txt", self.location)).href) {
+      t.done();
+    }
+  });
+  document.body.appendChild(frame)
+}, "When there's both a Refresh header and <meta> the Refresh header wins")
--- http/refresh/resources/refresh.py
+++ http/refresh/resources/refresh.py
@@ -1,4 +1,4 @@
 def main(request, response):
     response.headers.set("Content-Type", "text/plain")
-    response.headers.set("Refresh", "0;./refreshed.txt")
+    response.headers.set("Refresh", "0;./refreshed.txt?\xFF")
     response.content = "Not refreshed.\n"

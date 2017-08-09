410d8579c5e28336cb5b5549e1985bb42efb67c4
Anne van Kesteren <annevk@annevk.nl>
2017-08-09 11:27:17 +0200

    add some documentation

--- http/refresh/navigate.window.js
+++ http/refresh/navigate.window.js
@@ -3,7 +3,7 @@ async_test(t => {
   frame.src = "resources/refresh.py"
   frame.onload = t.step_func(() => {
     // Could be better by verifying that resources/refresh.py loads too
-    if(frame.contentWindow.location.href === (new URL("resources/refreshed.txt?ÿ", self.location)).href) {
+    if(frame.contentWindow.location.href === (new URL("resources/refreshed.txt?\u0080\u00FF", self.location)).href) { // Make sure bytes got mapped to code points of the same value
       t.done();
     }
   });

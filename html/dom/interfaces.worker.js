7892a84d32cb93203f191d0bce1772bb9935af9c
Marcos Caceres <marcos@marcosc.com>
2017-09-12 13:26:32 +1000

    make EventHandler its own IDL file

--- html/dom/interfaces.worker.js
+++ html/dom/interfaces.worker.js
@@ -9,28 +9,30 @@ function doTest([html, dom, cssom, touchevents, uievents]) {
   idlArray.add_idls(html);
 
   idlArray.add_objects({
-    WorkerNavigator: ['self.navigator'],
+    WorkerNavigator: ["self.navigator"],
     WebSocket: ['new WebSocket("ws://foo")'],
     CloseEvent: ['new CloseEvent("close")'],
     Worker: [],
     MessageEvent: ['new MessageEvent("message", { data: 5 })'],
-    DedicatedWorkerGlobalScope: ['self'],
+    DedicatedWorkerGlobalScope: ["self"],
   });
 
   idlArray.test();
-};
+}
 
 function fetchData(url) {
-  return fetch(url).then((response) => response.text());
+  return fetch(url).then(response => response.text());
 }
 
 promise_test(function() {
-  return Promise.all([fetchData("/interfaces/html.idl"),
-                      fetchData("/interfaces/dom.idl"),
-                      fetchData("/interfaces/cssom.idl"),
-                      fetchData("/interfaces/touchevents.idl"),
-                      fetchData("/interfaces/uievents.idl")])
-                .then(doTest);
+  return Promise.all([
+    fetchData("/interfaces/eventhandler.idl"),
+    fetchData("/interfaces/html.idl"),
+    fetchData("/interfaces/dom.idl"),
+    fetchData("/interfaces/cssom.idl"),
+    fetchData("/interfaces/touchevents.idl"),
+    fetchData("/interfaces/uievents.idl"),
+  ]).then(doTest);
 }, "Test driver");
 
 done();

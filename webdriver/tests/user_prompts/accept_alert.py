79b60852f7fdd23c2cc842d2bbd99c7237fec95d
Alex Chiem <alchiem@microsoft.com>
2017-09-19 15:22:15 -0700

    Add tests for alert, confirm and prompt dialogs.

--- webdriver/tests/user_prompts/accept_alert.py
+++ webdriver/tests/user_prompts/accept_alert.py
@@ -24,14 +24,22 @@ def test_no_user_prompt(session):
     assert_error(response, "no such alert")
 
 
-def test_accept_user_prompt(session):
-    # 18.2 step 3
-    session.url = inline("""
-        <SCRIPT Language="JavaScript">
-            document.write(prompt("Enter Your Name: ", "Federer"));
-        </SCRIPT>
-        """)
+def test_accept_alert(session):
+    # 18.1 step 3
+    session.execute_script("window.alert(\"Hello\");")
     response = accept_alert(session)
-    element = session.find.css("body", all=False)
-    assert element.text == "Federer"
+    assert_success(response)
+
+
+def test_accept_confirm(session):
+    # 18.1 step 3
+    session.execute_script("window.confirm(\"Hello\");")
+    response = accept_alert(session)
+    assert_success(response)
+
+
+def test_accept_prompt(session):
+    # 18.1 step 3
+    session.execute_script("window.prompt(\"Enter Your Name: \", \"Federer\");")
+    response = accept_alert(session)
     assert_success(response)

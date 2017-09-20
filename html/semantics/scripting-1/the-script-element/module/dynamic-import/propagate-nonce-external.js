8b040542377417817affc5f075a4636f3a23ff6d
Kouhei Ueno <kouhei@chromium.org>
2017-09-20 00:54:28 -0700

    [ES6 modules] WPT: dynamically imported modules from valid nonce should eval
    This CL adds web-platform-tests which verify that module scripts dynamically
    imported from {inline,out-of-line} {module,classic} script should eval.
    
    Bug: 711706
    Change-Id: Id2a8b17b97c050162bdfec57d0338443adf77eb4

---
+++ html/semantics/scripting-1/the-script-element/module/dynamic-import/propagate-nonce-external.js
@@ -0,0 +1,6 @@
+promise_test(t => {
+  return import("./../imports-a.js").then(module => {
+    assert_true(window.evaluated_imports_a);
+    assert_equals(module.A["from"], "imports-a.js");
+  });
+}, "Dynamically imported module should eval when imported from script w/ a valid nonce.");

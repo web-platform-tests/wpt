d1288910fcec5ff218a1ddc156845618a156d504
Geoffrey Sneddon <me@gsnedders.com>
2017-09-22 18:49:07 +0900

    fixup! Add some way to automate user-initiated click

--- resources/testautomation.js
+++ resources/testautomation.js
@@ -1,19 +1,78 @@
-/**
- * @namespace
- */
-var test_automation = {
-    /**
-     * Triggers a user-initiated click
-     *
-     * This matches the behaviour of the {@link
-     * https://www.w3.org/TR/webdriver/#element-click|WebDriver
-     * Element Click command}.
-     * 
-     * @param {Element} element - element to be clicked
-     * @returns {Promise} fulfilled after click occurs, or rejected in
-     *                    the cases the WebDriver command errors
-     */
-    click: function(element) {
-        return Promise.reject(new Error("not implemented"));
+(function() {
+    "use strict";
+
+    function getInViewCenterPoint(rect) {
+        var left = Math.max(0, Math.min(rect.x, rect.x + rect.width));
+        var right = Math.min(rect.innerWidth, Math.max(rect.x, rect.x + rect.width));
+        var top = Math.max(0, Math.min(rect.y, rect.y + rect.height));
+        var bottom = Math.min(rect.innerHeight, Math.max(rect.y, rect.y + rect.height));
+
+        var x = 0.5 * (left + right);
+        var y = 0.5 * (top + bottom);
+
+        return [x, y];
     }
-};
+
+    function getPointerInteractablePaintTree(element) {
+        if (!window.document.contains(element)) {
+            return [];
+        }
+
+        var rectangles = element.getClientRects();
+
+        if (rectangles.length === 0) {
+            return [];
+        }
+
+        var centerPoint = getInViewCenterPoint(rectangles[0]);
+
+        return document.elementsFromPoint(centerPoint[0], centerPoint[1]);
+    }
+
+    function inView(element) {
+        var pointerInteractablePaintTree = getPointerInteractablePaintTree(element);
+        return pointerInteractablePaintTree.indexOf(element) !== -1;
+    }
+
+
+    /**
+     * @namespace
+     */
+    window.test_automation = {
+        /**
+         * Triggers a user-initiated click
+         *
+         * This matches the behaviour of the {@link
+         * https://w3c.github.io/webdriver/webdriver-spec.html#element-click|WebDriver
+         * Element Click command}.
+         *
+         * @param {Element} element - element to be clicked
+         * @returns {Promise} fulfilled after click occurs, or rejected in
+         *                    the cases the WebDriver command errors
+         */
+        click: function(element) {
+            if (window.top !== window) {
+                return Promise.reject(new Error("can only click in top-level window"));
+            }
+
+            if (!window.document.contains(element)) {
+                return Promise.reject(new Error("element in different document or shadow tree"));
+            }
+
+            if (!inView(element)) {
+                element.scrollIntoView({behavior: "instant",
+                                        block: "end",
+                                        inline: "nearest"});
+            }
+
+            var pointerInteractablePaintTree = getPointerInteractablePaintTree(element);
+            if (pointerInteractablePaintTree.length === 0 ||
+                !element.contains(pointerInteractablePaintTree[0])) {
+                return Promise.reject(new Error("element click intercepted error"));
+            }
+
+            return window.test_automation_internal.click(element);
+        }
+    };
+
+})();

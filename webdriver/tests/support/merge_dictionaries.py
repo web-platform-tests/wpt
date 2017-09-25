3cf48abed8e7774d0d50297e162f77b6d561c802
Carlos Garcia Campos <cgarcia@igalia.com>
2017-09-25 09:32:07 +0200

    Fix wdspec tests using new_session fixture
    Tests are passing capabilities that should be used when creating the
    session, but they are passed directly as body of new session command,
    without adding them to a capabilities object. This is also ignoring all
    other configuration capabilities, preventing the browser from being
    launched in cases where the browser binary is passed to the driver as a
    capability. We should use the configuration capabilities, overwrite
    the ones given by the test and pass them to the Session at construction.
    This way we can use Session::start() instead of using send_command()
    directly.

--- webdriver/tests/support/merge_dictionaries.py
+++ webdriver/tests/support/merge_dictionaries.py
@@ -24,6 +24,27 @@ def merge_dictionaries(first, second):
 
     return result
 
+def overwrite_dictionary(first, second):
+    """Given two dictionaries, create a third that defines all specified
+    key/value pairs. This merge_dictionaries is performed "deeply" on any nested
+    dictionaries. If a value is defined for the same key by both dictionaries,
+    the vale of the second one is used."""
+    result = dict(first)
+
+    for key, value in iteritems(first):
+        if key not in second or second[key] == value:
+            continue
+        if isinstance(second[key], dict) and isinstance(value, dict):
+            result[key] = overwrite_dictionary(result[key], second[key])
+        else:
+            result[key] = second[key]
+
+    for key, value in iteritems(second):
+        if key not in result:
+            result[key] = value
+
+    return result
+
 if __name__ == "__main__":
     assert merge_dictionaries({}, {}) == {}
     assert merge_dictionaries({}, {"a": 23}) == {"a": 23}
@@ -47,3 +68,12 @@ if __name__ == "__main__":
     except Exception as _e:
         e = _e
     assert isinstance(e, TypeError)
+
+    assert overwrite_dictionary({}, {}) == {}
+    assert overwrite_dictionary({}, {"a": 23}) == {"a": 23}
+    assert overwrite_dictionary({"a": 23}, {}) == {"a": 23}
+    assert overwrite_dictionary({"a": 23}, {"b": 45}) == {"a": 23, "b": 45}
+
+    assert overwrite_dictionary({"a": 23, "b": 45}, {"b": 55}) == {"a": 23, "b": 55}
+    assert overwrite_dictionary({"a": {"b": 23, "d": 60}}, {"a": {"b": 45, "c": 55}}) == {"a": {"b": 45, "c": 55, "d": 60}}
+    assert overwrite_dictionary({"a": {"b": 23, "d": 60}}, {"a": {"b": 23, "d": 65, "f": {"g": 75}}, "z": -10}) == {"a": {"b": 23, "d": 65, "f": {"g": 75}}, "z": -10}

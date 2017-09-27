1a7e54c0ff2ee64a2f95c20ff8b54eea10ed837d
Marcos Cáceres <marcos@marcosc.com>
2017-09-27 18:43:28 +1000

    Refactor: just let .toJSON() throw

--- payment-request/payment-response/helpers.js
+++ payment-request/payment-response/helpers.js
@@ -115,11 +115,7 @@ async function runManualTest(button, options, expected = {}, id = undefined) {
     }
     assert_idl_attribute(response, "details");
     assert_equals(typeof response.details, "object", "Expected an object");
-    try {
-      response.toJSON();
-    } catch (err) {
-      assertion_unreached("Unexpected error calling response.toJSON()");
-    }
+    response.toJSON();
     if (options && options.requestShipping) {
       assert_equals(
         response.shippingOption,

373d338028c705dab033b2fad678449e03703c06
Jake Archibald <jaffathecake@gmail.com>
2017-10-11 14:17:56 +0100

    Adding navigation preload test

---
+++ service-workers/service-worker/navigation-preload/resources/preload-abort-worker.js
@@ -0,0 +1,42 @@
+skipWaiting();
+
+function wait(ms) {
+  return new Promise(r => setTimeout(r, ms));
+}
+
+addEventListener('activate', event => {
+  event.waitUntil(self.registration.navigationPreload.enable());
+});
+
+addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  const channelName = url.searchParams.get('bc');
+  const bc = new BroadcastChannel(channelName);
+
+  event.waitUntil(
+    Promise.race([
+      (async function test() {
+        if (!event.preloadResponse) {
+          return 'no-preload-response';
+        }
+        console.log('hey')
+        try {
+          await event.preloadResponse;
+          return 'resolved';
+        }
+        catch (err) {
+          if (err.name == 'AbortError') {
+            return 'rejected-aborterror';
+          }
+          return 'rejected-other-error'
+        }
+      }()),
+      wait(2000).then(() => 'timeout')
+    ]).then(
+      val => bc.postMessage(val),
+      err => bc.postMessage(`Err: ${err.message}`)
+    )
+  );
+
+  event.respondWith(event.preloadResponse);
+});

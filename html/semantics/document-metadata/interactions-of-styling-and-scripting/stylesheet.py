e3e134a22c647e7ab5018287aa168c8e4b777cef
Dominic Farolino <domfarolino@gmail.com>
2017-10-16 10:21:47 -0400

    Strengthen test with getComputedStyles and make colors non-indicative of pass/fail

--- html/semantics/document-metadata/interactions-of-styling-and-scripting/stylesheet.py
+++ html/semantics/document-metadata/interactions-of-styling-and-scripting/stylesheet.py
@@ -5,6 +5,6 @@ def main(request, response):
     sleep(delay)
 
   if "stylesNotMatchingEnvironment" in request.GET:
-    return 'h1 {color: green}'
+    return 'h1 {color: brown}'
   else:
-    return 'h1 {color: red}'
+    return 'h1 {color: purple}'

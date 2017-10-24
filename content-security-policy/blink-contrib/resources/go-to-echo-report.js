008e696f14adb05c0044710cec02708a8f16068a
Andy Paicu <andypaicu@chromium.org>
2017-10-24 09:16:09 +0200

    Revert "Testing removing blink-contrib"
    This reverts commit 029b57fccf5fa5f16210f73f29e899b786130f3f.

---
+++ content-security-policy/blink-contrib/resources/go-to-echo-report.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    var test = window.location.pathname.replace(/^.+\//, '');
+    var match = window.location.search.match(/^\?test=([^&]+)/);
+    if (match)
+        test = match[1];
+    window.location = "/security/contentSecurityPolicy/resources/echo-report.php?test=" + test;
+}

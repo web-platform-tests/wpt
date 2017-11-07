60c618cc380dddf4529ebd7d3733b6e7fe548474
Adam Rice <ricea@chromium.org>
2017-11-07 21:27:08 +0900

    Add missing test to patched-global.js
    The test added in https://github.com/whatwg/streams/pull/852 was missing
    from the previous commit. Add it.

--- streams/transform-streams/patched-global.js
+++ streams/transform-streams/patched-global.js
@@ -20,4 +20,34 @@ test(() => {
   assert_not_equals(new TransformStream(), null, 'constructor should work');
 }, 'TransformStream constructor should not call setters for highWaterMark or size');
 
+test(t => {
+  /* eslint-disable no-native-reassign */
+
+  const oldReadableStream = ReadableStream;
+  const oldWritableStream = WritableStream;
+  const getReader = ReadableStream.prototype.getReader;
+  const getWriter = WritableStream.prototype.getWriter;
+
+  // Replace ReadableStream and WritableStream with broken versions.
+  ReadableStream = function () {
+    throw new Error('Called the global ReadableStream constructor');
+  };
+  WritableStream = function () {
+    throw new Error('Called the global WritableStream constructor');
+  };
+  t.add_cleanup(() => {
+    ReadableStream = oldReadableStream;
+    WritableStream = oldWritableStream;
+  });
+
+  const ts = new TransformStream();
+
+  // Just to be sure, ensure the readable and writable pass brand checks.
+  assert_not_equals(getReader.call(ts.readable), undefined,
+                    'getReader should work when called on ts.readable');
+  assert_not_equals(getWriter.call(ts.writable), undefined,
+                    'getWriter should work when called on ts.writable');
+  /* eslint-enable no-native-reassign */
+}, 'TransformStream should use the original value of ReadableStream and WritableStream');
+
 done();

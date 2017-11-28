5aa8d3f51090e3cc2271b366ba9102ca9f5dec50
Dale Bustad <dabustad@microsoft.com>
2017-11-28 15:49:05 -0800

    Add tests for time-origin.
    Closes https://github.com/w3c/hr-time/issues/32.

---
+++ hr-time/resources/unload.js
@@ -0,0 +1,42 @@
+const syncDelay = ms => {
+  const start = performance.now();
+  let elapsedTime;
+  do {
+    elapsedTime = performance.now() - start;
+  } while (elapsedTime < ms);
+};
+
+const markTime = (docName, lifecycleEventName) => window.opener.postMessage({
+  docName,
+  lifecycleEventName,
+  time: performance.now()
+}, "*");
+
+const setupUnloadPrompt = (docName, msg) => {
+  window.addEventListener("beforeunload", ev => {
+    markTime(docName, "beforeunload");
+    return ev.returnValue = msg || "Click OK to continue test."
+  });
+};
+
+const setupListeners = (docName, nextDocument) => {
+  window.addEventListener("load", () => {
+    markTime(docName, "load");
+    document.getElementById("proceed").addEventListener("click", ev => {
+      ev.preventDefault();
+      if (nextDocument) {
+        document.location = nextDocument;
+      } else {
+        window.close();
+      }
+    })
+  });
+
+  setupUnloadPrompt(docName);
+
+  window.addEventListener("unload", () => {
+    markTime(docName, "unload");
+    if (docName !== "c") { syncDelay(2000); }
+  });
+};
+

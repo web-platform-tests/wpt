0ff80cca9d7b24f9edf7bad276fc4053e8b020b8
Geoffrey Sneddon <me@gsnedders.com>
2017-12-04 14:54:41 +0000

    fixup! Standardise our flake8 setup

--- tools/wpt/install.py
+++ tools/wpt/install.py
@@ -39,9 +39,3 @@ def install(name, component, destination):
     subclass = getattr(browser, name.title())
     sys.stdout.write('Now installing %s %s...\n' % (name, component))
     getattr(subclass(), method)(dest=destination)
-
-
-if __name__ == '__main__':
-    parser = get_parser()
-    args = parser.parse_args()
-    run(None, **vars(args))

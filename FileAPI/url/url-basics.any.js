a601af605fcbb93ac899b51f323dc7061af9d001
Marijn Kruisselbrink <mek@chromium.org>
2017-12-07 06:22:33 -0800

    fix reference in sandboxed-iframe.html and add more url format checks

--- FileAPI/url/url-basics.any.js
+++ FileAPI/url/url-basics.any.js
@@ -36,6 +36,24 @@ test(() => {
 }, 'Verify if origin of Blob URL matches our origin');
 
 test(() => {
+  const url = URL.createObjectURL(blob);
+  const url_record = new URL(url);
+  assert_equals(url_record.protocol, 'blob:');
+  assert_equals(url_record.origin, location.origin);
+  assert_equals(url_record.host, '');
+  assert_equals(url_record.port, '');
+  const path_re = /\/[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  assert_true(path_re.test(url_record.pathname), 'Path must end with a valid UUID');
+  if (location.origin != 'null') {
+    const nested_url = new URL(url_record.pathname);
+    assert_equals(nested_url.origin, location.origin);
+    assert_equals(nested_url.pathname.search(path_re), 0, 'Path must be a valid UUID');
+    assert_true(url.includes(location.origin));
+    assert_true(url.startsWith('blob:' + location.protocol));
+  }
+}, 'Verify if Blob URL parses correctly');
+
+test(() => {
   const url = URL.createObjectURL(file);
   assert_equals(new URL(url).origin, location.origin);
   if (location.origin != 'null') {

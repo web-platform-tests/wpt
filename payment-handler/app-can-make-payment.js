65baffe8b8760787519827567cc938856d5f3cfe
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2017-12-14 13:52:13 -0500

    Test methodData, modifiers, topLevelOrigin, and paymentRequestOrigin.

--- payment-handler/app-can-make-payment.js
+++ payment-handler/app-can-make-payment.js
@@ -1,6 +1,112 @@
 self.addEventListener('canmakepayment', canMakePaymentEvent => {
-  const methodName = canMakePaymentEvent.methodData[0].supportedMethods[0];
-  const behavior = methodName.split('/')[3];
+  if (canMakePaymentEvent.methodData.length !== 1) {
+    const msg = 'Worker says: expected exactly one method data';
+    canMakePaymentEvent.respondWith(Promise.reject(new Error(msg)));
+    return;
+  }
+
+  const methodData = canMakePaymentEvent.methodData[0];
+  if (methodData.supportedMethods.length !== 1) {
+    const msg = 'Worker says: expected exactly one supported method name';
+    canMakePaymentEvent.respondWith(Promise.reject(new Error(msg)));
+    return;
+  }
+
+  if (methodData.data.defaultParameter !== 'defaultValue') {
+    const msg = `Worker says: unexpected value for 'defaultParameter': ${
+      methodData.data.defaultParameter
+    }`;
+    canMakePaymentEvent.respondWith(Promise.reject(new Error(msg)));
+    return;
+  }
+
+  if ('defaultUnsupportedParameter' in methodData.data) {
+    const msg = "Worker says: unexpected 'defaultUnsupportedParameter'";
+    canMakePaymentEvent.respondWith(Promise.reject(new Error(msg)));
+    return;
+  }
+
+  if ('total' in canMakePaymentEvent) {
+    const msg = "Worker says: unexpected 'total'";
+    canMakePaymentEvent.respondWith(Promise.reject(new Error(msg)));
+    return;
+  }
+
+  if ('displayItems' in canMakePaymentEvent) {
+    const msg = "Worker says: unexpected 'displayItems'";
+    canMakePaymentEvent.respondWith(Promise.reject(new Error(msg)));
+    return;
+  }
+
+  if (canMakePaymentEvent.modifiers.length !== 1) {
+    const msg = 'Worker says: expected exactly one modifier';
+    canMakePaymentEvent.respondWith(Promise.reject(new Error(msg)));
+    return;
+  }
+
+  const modifier = canMakePaymentEvent.modifiers[0];
+  if (modifier.supportedMethods.length !== 1) {
+    const msg =
+      'Worker says: expected exactly one supported method name in modifier';
+    canMakePaymentEvent.respondWith(Promise.reject(new Error(msg)));
+    return;
+  }
+
+  const methodName = methodData.supportedMethods[0];
+  if (modifier.supportedMethods[0] !== methodName) {
+    const msg = `Worker says: unexpected modifier method name ${
+      modifier.supportedMethods[0]
+    }`;
+    canMakePaymentEvent.respondWith(Promise.reject(new Error(msg)));
+    return;
+  }
+
+  if (modifier.data.modifiedParameter !== 'modifiedValue') {
+    const msg = `Worker says: unexpected value for 'modifiedParameter': ${
+      modifier.data.modifiedParameter
+    }`;
+    canMakePaymentEvent.respondWith(Promise.reject(new Error(msg)));
+    return;
+  }
+
+  if ('modifiedUnsupportedParameter' in modifier) {
+    const msg =
+      "Worker says: unexpected 'modifiedUnsupportedParameter' in modifier";
+    canMakePaymentEvent.respondWith(Promise.reject(new Error(msg)));
+    return;
+  }
+
+  if ('total' in modifier) {
+    const msg = "Worker says: unexpected 'total' in modifier";
+    canMakePaymentEvent.respondWith(Promise.reject(new Error(msg)));
+    return;
+  }
+
+  if ('additionalDisplayItems' in modifier) {
+    const msg = "Worker says: unexpected 'additionalDisplayItems' in modifier";
+    canMakePaymentEvent.respondWith(Promise.reject(new Error(msg)));
+    return;
+  }
+
+  const methodNameParts = methodName.split('/');
+  const origin = methodNameParts.slice(0, 3).join('/');
+  if (canMakePaymentEvent.topLevelOrigin !== origin) {
+    const msg = `Worker says: unexpected top level origin ${
+      canMakePaymentEvent.topLevelOrigin
+    }`;
+    canMakePaymentEvent.respondWith(Promise.reject(new Error(msg)));
+    return;
+  }
+
+  if (canMakePaymentEvent.paymentRequestOrigin !== origin) {
+    const msg = `Worker says: unexpected iframe origin ${
+      canMakePaymentEvent.paymentRequestOrigin
+    }`;
+    canMakePaymentEvent.respondWith(Promise.reject(new Error(msg)));
+    return;
+  }
+
+  const behavior = methodNameParts[3];
   switch (behavior) {
     case 'canMakePayment-true':
       canMakePaymentEvent.respondWith(true);

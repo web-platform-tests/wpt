d865b84e12ae782d43020b32e93445cc7ddeeac5
Victor Costan <pwnall@chromium.org>
2017-12-18 15:54:37 -0800

    Cookie Store API: Add more WPT coverage for Service Worker.
    Bug: 729800
    Change-Id: I5de81be0eb6cd2f90722e42655ce58ef52d3d26f
    Reviewed-on: https://chromium-review.googlesource.com/831254
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524860}

--- cookie-store/cookieStore_has_basic.tentative.window.js
+++ cookie-store/cookieStore_has_basic.tentative.window.js
@@ -1,11 +1,3 @@
-<!doctype html>
-<meta charset="utf-8">
-<title>Async Cookies: cookieStore.has()</title>
-<link rel="help" href="https://github.com/WICG/cookie-store">
-<link rel="author" href="pwnall@chromium.org" title="Victor Costan">
-<script src="/resources/testharness.js"></script>
-<script src="/resources/testharnessreport.js"></script>
-<script>
 'use strict';
 
 // Workaround because add_cleanup doesn't support async functions yet.
@@ -31,5 +23,3 @@ promise_test(async testCase => {
 
   await async_cleanup(() => cookieStore.delete('cookie-name'));
 }, 'cookieStore.has returns false for cookie deleted by cookieStore.delete()');
-
-</script>

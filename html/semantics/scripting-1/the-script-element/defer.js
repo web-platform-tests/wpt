0b2c763fae96c6b08134be3b20552577cb1c47e3
kaixinjxq <xiuqix.jiang@intel.com>
2017-12-20 11:19:38 +0800

    modify the test with async_test/step

--- html/semantics/scripting-1/the-script-element/defer.js
+++ html/semantics/scripting-1/the-script-element/defer.js
@@ -1,7 +1,4 @@
-function afterLoad(){
-  flag += NUMBER_SECOND;
-  test(function(){
-    assert_equals(flag, EXPECTED, "The value of variable is incorrect.");
-  }, "the defer script run later");
-}
-afterLoad();
+t.step(() => {
+  assert_false(ran_deferred_script);
+});
+t.done();

ed04ceb91ee52565169910a20112608b9a38536b
Yoav Weiss <yoav@yoav.ws>
2017-12-28 11:06:10 +0100

    Tested the tests

--- fetch/api/request/destination/resources/fetch-destination-worker.js
+++ fetch/api/request/destination/resources/fetch-destination-worker.js
@@ -2,7 +2,7 @@ self.addEventListener('fetch', function(event) {
     if (event.request.url.indexOf('dummy') != -1) {
         let destination = new URL(event.request.url).searchParams.get("dest");
         if (event.request.destination == destination)
-            event.respondWith(new Response());
+            event.respondWith(fetch(event.request));
         else
             event.respondWith(Response.error());
     }

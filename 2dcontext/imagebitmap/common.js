e21d26f33e1dde47882836a5384c5025d956e65e
Ms2ger <Ms2ger@gmail.com>
2018-01-03 15:48:18 +0100

    Inline single-use initializeTestCanvas and initializeImageData functions.

--- 2dcontext/imagebitmap/common.js
+++ 2dcontext/imagebitmap/common.js
@@ -1,7 +1,17 @@
 function makeCanvas() {
     return new Promise(resolve => {
         var testCanvas = document.createElement("canvas");
-        initializeTestCanvas(testCanvas);
+        testCanvas.width = 20;
+        testCanvas.height = 20;
+        var testCtx = testCanvas.getContext("2d");
+        testCtx.fillStyle = "rgb(255, 0, 0)";
+        testCtx.fillRect(0, 0, 10, 10);
+        testCtx.fillStyle = "rgb(0, 255, 0)";
+        testCtx.fillRect(10, 0, 10, 10);
+        testCtx.fillStyle = "rgb(0, 0, 255)";
+        testCtx.fillRect(0, 10, 10, 10);
+        testCtx.fillStyle = "rgb(0, 0, 0)";
+        testCtx.fillRect(10, 10, 10, 10);
         resolve(testCanvas);
     });
 }
@@ -30,7 +40,24 @@ function makeImageData() {
     return new Promise(function(resolve, reject) {
         var width = 20, height = 20;
         var imgData = new ImageData(width, height);
-        initializeImageData(imgData, width, height);
+        for (var i = 0; i < width * height * 4; i += 4) {
+            imgData.data[i] = 0;
+            imgData.data[i + 1] = 0;
+            imgData.data[i + 2] = 0;
+            imgData.data[i + 3] = 255; //alpha channel: 255
+        }
+        var halfWidth = width / 2;
+        var halfHeight = height / 2;
+        // initialize to R, G, B, Black, with each one 10*10 pixels
+        for (var i = 0; i < halfHeight; i++)
+            for (var j = 0; j < halfWidth; j++)
+                imgData.data[i * width * 4 + j * 4] = 255;
+        for (var i = 0; i < halfHeight; i++)
+            for (var j = halfWidth; j < width; j++)
+                imgData.data[i * width * 4 + j * 4 + 1] = 255;
+        for (var i = halfHeight; i < height; i++)
+            for (var j = 0; j < halfWidth; j++)
+                imgData.data[i * width * 4 + j * 4 + 2] = 255;
         resolve(imgData);
     });
 }
@@ -51,41 +78,4 @@ function makeBlob() {
             resolve(xhr.response);
         };
     });
-}
-
-function initializeTestCanvas(testCanvas)
-{
-    testCanvas.width = 20;
-    testCanvas.height = 20;
-    var testCtx = testCanvas.getContext("2d");
-    testCtx.fillStyle = "rgb(255, 0, 0)";
-    testCtx.fillRect(0, 0, 10, 10);
-    testCtx.fillStyle = "rgb(0, 255, 0)";
-    testCtx.fillRect(10, 0, 10, 10);
-    testCtx.fillStyle = "rgb(0, 0, 255)";
-    testCtx.fillRect(0, 10, 10, 10);
-    testCtx.fillStyle = "rgb(0, 0, 0)";
-    testCtx.fillRect(10, 10, 10, 10);
-}
-
-function initializeImageData(imgData, width, height)
-{
-    for (var i = 0; i < width * height * 4; i+=4) {
-        imgData.data[i] = 0;
-        imgData.data[i + 1] = 0;
-        imgData.data[i + 2] = 0;
-        imgData.data[i + 3] = 255; //alpha channel: 255
-    }
-    var halfWidth = width/2;
-    var halfHeight = height/2;
-    // initialize to R, G, B, Black, with each one 10*10 pixels
-    for (var i = 0; i < halfHeight; i++)
-        for (var j = 0; j < halfWidth; j++)
-            imgData.data[i * width * 4 + j * 4] = 255;
-    for (var i = 0; i < halfHeight; i++)
-        for (var j = halfWidth; j < width; j++)
-            imgData.data[i * width * 4 + j * 4 + 1] = 255;
-    for (var i = halfHeight; i < height; i++)
-        for (var j = 0; j < halfWidth; j++)
-            imgData.data[i * width * 4 + j * 4 + 2] = 255;
 }

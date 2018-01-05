b1a6b03cc6498a980aa0a3a62c8b93423e66d302
Yoav Weiss <yoav@yoav.ws>
2018-01-05 18:50:22 +0100

    Got the iframe test working

--- fetch/api/request/destination/resources/fetch-destination-worker-no-load-event.js
+++ fetch/api/request/destination/resources/fetch-destination-worker-no-load-event.js
@@ -1,5 +1,4 @@
 self.addEventListener('fetch', function(event) {
-    console.log(event.request.url);
     if (event.request.url.includes('dummy')) {
         event.waitUntil(async function() {
             let destination = new URL(event.request.url).searchParams.get("dest");
@@ -7,8 +6,6 @@ self.addEventListener('fetch', function(event) {
             if (event.request.destination == destination) {
                 client.postMessage("PASS");
             } else {
-                console.log("FAIL " + event.clientId);
-                console.log(client);
                 client.postMessage("FAIL");
             }
         }())

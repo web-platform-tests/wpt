6f7951aab0e4fad0abb3ea85f2b1f38a643e1954
Yoav Weiss <yoav@yoav.ws>
2018-01-05 11:44:11 +0100

    Added tests for a bunch of resources without a load event

---
+++ fetch/api/request/destination/resources/fetch-destination-worker-no-load-event.js
@@ -0,0 +1,17 @@
+self.addEventListener('fetch', function(event) {
+    if (event.request.url.includes('dummy')) {
+        console.log(event.request.url + ":" + event.request.destination);
+        event.waitUntil(async function() {
+            let destination = new URL(event.request.url).searchParams.get("dest");
+            let client = await self.clients.get(event.clientId);
+            if (event.request.destination == destination) {
+                client.postMessage("PASS");
+            } else {
+                client.postMessage("FAIL");
+            }
+        }())
+    }
+    event.respondWith(fetch(event.request));
+});
+
+

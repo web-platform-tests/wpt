fc3f850a09da0b71be560a4ff3aa68aec7c0dd07
Yoav Weiss <yoav@yoav.ws>
2018-01-05 22:05:47 +0100

    Addressed nits

--- fetch/api/request/destination/resources/fetch-destination-worker-iframe.js
+++ fetch/api/request/destination/resources/fetch-destination-worker-iframe.js
@@ -2,19 +2,17 @@ self.addEventListener('fetch', function(event) {
     if (event.request.url.includes('dummy')) {
         event.waitUntil(async function() {
             let destination = new URL(event.request.url).searchParams.get("dest");
-            await self.clients.matchAll({"includeUncontrolled": true})
-                .then(function(clients) {
-                    clients.forEach(function(client) {
-                        if (client.url.includes("fetch-destination-iframe")) {
-                            if (event.request.destination == destination) {
-                                client.postMessage("PASS");
-                            } else {
-                                client.postMessage("FAIL");
-                            }
-                        }
-                    })
-                });
-        }())
+            let clients = await self.clients.matchAll({"includeUncontrolled": true});
+            clients.forEach(function(client) {
+                if (client.url.includes("fetch-destination-iframe")) {
+                    if (event.request.destination == destination) {
+                        client.postMessage("PASS");
+                    } else {
+                        client.postMessage("FAIL");
+                    }
+                }
+            })
+        }());
     }
     event.respondWith(fetch(event.request));
 });

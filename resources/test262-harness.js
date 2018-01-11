323d4cadaccb832dd0b8210afebb1ebc2515ff67
Diego Pino Garcia <dpino@igalia.com>
2018-01-11 10:37:24 +0100

    Remove tabs

--- resources/test262-harness.js
+++ resources/test262-harness.js
@@ -1,32 +1,32 @@
 // Source: https://github.com/bakkot/test262-web-runner
 function installAPI(global) {
-	global.$262 = {
-		createRealm: function() {
-			var iframe = global.document.createElement('iframe');
+    global.$262 = {
+        createRealm: function() {
+            var iframe = global.document.createElement('iframe');
             iframe.style.cssText = "display: none";
-			iframe.src = ""; // iframeSrc;
-			global.document.body.appendChild(iframe);
-			return installAPI(iframe.contentWindow);
-		},
-		evalScript: function(src) {
-			var script = global.document.createElement('script');
-			script.text = src;
-			global.document.body.appendChild(script);
-		},
-		detachArrayBuffer: function(buffer) {
-			if (typeof postMessage !== 'function') {
-				throw new Error('No method available to detach an ArrayBuffer');
-			} else {
-                postMessage(null, '*', [buffer]);
-				/*
-				  See https://html.spec.whatwg.org/multipage/comms.html#dom-window-postmessage
-				  which calls https://html.spec.whatwg.org/multipage/infrastructure.html#structuredclonewithtransfer
-				  which calls https://html.spec.whatwg.org/multipage/infrastructure.html#transfer-abstract-op
-				  which calls the DetachArrayBuffer abstract operation https://tc39.github.io/ecma262/#sec-detacharraybuffer
-                */
-	        }
+            iframe.src = ""; // iframeSrc;
+            global.document.body.appendChild(iframe);
+            return installAPI(iframe.contentWindow);
         },
-	    global: global
+        evalScript: function(src) {
+            var script = global.document.createElement('script');
+            script.text = src;
+            global.document.body.appendChild(script);
+        },
+        detachArrayBuffer: function(buffer) {
+            if (typeof postMessage !== 'function') {
+                throw new Error('No method available to detach an ArrayBuffer');
+            } else {
+                postMessage(null, '*', [buffer]);
+                /*
+                  See https://html.spec.whatwg.org/multipage/comms.html#dom-window-postmessage
+                  which calls https://html.spec.whatwg.org/multipage/infrastructure.html#structuredclonewithtransfer
+                  which calls https://html.spec.whatwg.org/multipage/infrastructure.html#transfer-abstract-op
+                  which calls the DetachArrayBuffer abstract operation https://tc39.github.io/ecma262/#sec-detacharraybuffer
+                */
+            }
+        },
+        global: global
     };
     return global.$262;
 }

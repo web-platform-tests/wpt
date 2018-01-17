235abce9391caa1a8362c9d3cd7c43c30c9d171d
Manuel Rego Casasnovas <rego@igalia.com>
2018-01-17 21:55:59 +0100

    [css-grid] Fix grid-alignment-implies-size-change tests
    This should fix w3c/csswg-drafts#2194 as it's moving the style-change.js script
    under css/css-grid/alignment/support/ folder.
    
    On top of that it's fixing those tests as the second checkLayout() call
    was never checked.
    This was because by default checkLayout() calls done(), and the test thinks
    it has already finished.
    Modified the call to checkLayout() in the script style-change.js
    and calling manually done() in the tests fixes the problem.

--- css/css-grid/alignment/support/style-change.js
+++ css/css-grid/alignment/support/style-change.js
@@ -1,5 +1,5 @@
 function evaluateStyleChange(element, phase, expectedProperty, expectedResult) {
     element.className += " " + phase;
     element.setAttribute(expectedProperty, expectedResult);
-    checkLayout("." + phase);
+    checkLayout("." + phase, false);
 }

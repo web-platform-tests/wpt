ab20739fda1993e88ce9b3fb413667b7ae710f8c
James Graham <james@hoppipolla.co.uk>
2018-01-29 11:54:54 +0000

    Don't return failure for wpt run if not all tests pass.
    This isn't very useful unless there is expectation data avaiable, which is
    not the common case

--- tools/wpt/run.py
+++ tools/wpt/run.py
@@ -420,7 +420,8 @@ def run(venv, **kwargs):
 
 def run_single(venv, **kwargs):
     from wptrunner import wptrunner
-    return wptrunner.start(**kwargs)
+    wptrunner.start(**kwargs)
+    return
 
 
 def main():

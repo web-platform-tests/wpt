6d532c42afcaeb554e41b3543e669aea4b348c5f
Anne van Kesteren <annevk@annevk.nl>
2018-01-31 16:07:42 +0100

    nit

--- fetch/api/response/response-fragment.window.js
+++ fetch/api/response/response-fragment.window.js
@@ -2,6 +2,7 @@ promise_test(() => {
   return fetch("./#test").then(res => {
     assert_equals(res.url, new URL("./", location).href);
     return fetch("../resources/redirect.py?location=/#test").then(res2 => {
+      // redirect.py changes the URL a bit; the important bit here is that the fragment is dropped
       assert_equals(res2.url, new URL("/?location=%2F&count=1", location).href);
     });
   });

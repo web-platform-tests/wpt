48a297bef34d2dea675d8df8c65f69f551405cb1
Anne van Kesteren <annevk@annevk.nl>
2018-02-05 16:48:20 +0100

    semicolons for everyone

--- html/browsers/history/the-location-interface/location-ancestororigins-involved.window.js
+++ html/browsers/history/the-location-interface/location-ancestororigins-involved.window.js
@@ -6,32 +6,31 @@ const embedPath = new URL("resources/ancestororigins-embed.py", location.href).p
       localEmbed = localOrigin + embedPath,
       remoteOrigin = info.HTTP_REMOTE_ORIGIN,
       remoteEmbed = remoteOrigin + embedPath,
-      remoteOrigin2 = info.HTTP_REMOTE_ORIGIN_WITH_DIFFERENT_PORT
-      remoteEmbed2 = remoteOrigin2 + embedPath
-let id = 0
+      remoteOrigin2 = info.HTTP_REMOTE_ORIGIN_WITH_DIFFERENT_PORT,
+      remoteEmbed2 = remoteOrigin2 + embedPath;
+let id = 0;
 
 async_test(t => {
   const iframe = document.createElement("iframe"),
-        localId = ++id
-  iframe.sandbox = "allow-scripts"
-  iframe.src = localEmbed + "?iframe=|" + remoteEmbed + "?id=" + localId
-  document.body.appendChild(iframe)
-  t.add_cleanup(() => iframe.remove())
-
+        localId = ++id;
+  iframe.sandbox = "allow-scripts";
+  iframe.src = localEmbed + "?iframe=|" + remoteEmbed + "?id=" + localId;
+  document.body.appendChild(iframe);
+  t.add_cleanup(() => iframe.remove());
 
   self.addEventListener("message", t.step_func(e => {
     if(e.data.id === localId) {
-      assert_array_equals(e.data.output, ["null", localOrigin])
-      t.done()
+      assert_array_equals(e.data.output, ["null", localOrigin]);
+      t.done();
     }
-  }))
-}, "Ensure sandboxed iframes show up as null")
+  }));
+}, "Ensure sandboxed iframes show up as null");
 
 // The following code ends up generating multiple tests each with multiple nested <iframe>s. The
 // variables in the array below seed various scenarios, described by "desc". The capital letters
 // describe the <iframe>s in play. When the same letter is used at different nesting levels that
 // means it is same-origin. Some defaulting is used to avoid too much duplication.
-;[
+[
   {
     outerPolicy: true,
     desc: "A uses no-referrer -> B -> A",
@@ -72,51 +71,51 @@ async_test(t => {
 ].forEach(val => {
   async_test(t => {
     if(!val.intermediateResults) {
-      val.intermediateResults = [val.results[1]]
+      val.intermediateResults = [val.results[1]];
     }
     if(!val.outerEmbed) {
-      val.outerEmbed = remoteEmbed
+      val.outerEmbed = remoteEmbed;
     }
     if(!val.innerEmbed) {
-      val.innerEmbed = localEmbed
+      val.innerEmbed = localEmbed;
     }
 
     const iframe = document.createElement("iframe"),
           innerId = ++id,
-          innermostId = ++id
+          innermostId = ++id;
     if(val.outerPolicy) {
-      iframe.referrerPolicy = "no-referrer"
+      iframe.referrerPolicy = "no-referrer";
     }
-    let innerPolicy = ""
+    let innerPolicy = "";
     if(val.innerPolicy) {
-      innerPolicy = "no-referrer"
+      innerPolicy = "no-referrer";
     }
-    iframe.src = val.outerEmbed + "?id=" + innerId + "&iframe=" + innerPolicy + "|" + val.innerEmbed + "?id=" + innermostId
-    document.body.appendChild(iframe)
-    t.add_cleanup(() => iframe.remove())
+    iframe.src = val.outerEmbed + "?id=" + innerId + "&iframe=" + innerPolicy + "|" + val.innerEmbed + "?id=" + innermostId;
+    document.body.appendChild(iframe);
+    t.add_cleanup(() => iframe.remove());
 
-    let almostDone = false
+    let almostDone = false;
     function localDone () {
       if(almostDone) {
-        t.done()
+        t.done();
       }
-      almostDone = true
+      almostDone = true;
     }
 
     self.addEventListener("message", t.step_func(e => {
       if(e.data.id === innerId) {
-        assert_array_equals(e.data.output, val.intermediateResults)
-        localDone()
+        assert_array_equals(e.data.output, val.intermediateResults);
+        localDone();
       }
       else if(e.data.id === innermostId) {
-        assert_array_equals(e.data.output, val.results)
-        localDone()
+        assert_array_equals(e.data.output, val.results);
+        localDone();
       }
-    }))
-  }, val.desc)
-})
+    }));
+  }, val.desc);
+});
 
-;[
+[
   {
     desc: "A -> B -> B uses no-referrer -> A",
     innerinnerEmbed: localEmbed
@@ -132,36 +131,36 @@ async_test(t => {
 ].forEach(val => {
   async_test(t => {
     const iframe = document.createElement("iframe"),
-          localId = ++id
-    iframe.src = remoteEmbed + "?iframe=|" + remoteEmbed + "?iframe=no-referrer%257C" + val.innerinnerEmbed + "?id=" + localId
-    document.body.appendChild(iframe)
-    t.add_cleanup(() => iframe.remove())
+          localId = ++id;
+    iframe.src = remoteEmbed + "?iframe=|" + remoteEmbed + "?iframe=no-referrer%257C" + val.innerinnerEmbed + "?id=" + localId;
+    document.body.appendChild(iframe);
+    t.add_cleanup(() => iframe.remove());
 
     self.addEventListener("message", t.step_func(e => {
       if(e.data.id === localId) {
-        assert_array_equals(e.data.output, ["null", "null", localOrigin])
-        t.done()
+        assert_array_equals(e.data.output, ["null", "null", localOrigin]);
+        t.done();
       }
-    }))
-  }, val.desc)
-})
+    }));
+  }, val.desc);
+});
 
 async_test(t => {
   const iframe = document.createElement("iframe"),
-        localId = ++id
-  document.body.appendChild(iframe)
-  t.add_cleanup(() => iframe.remove())
+        localId = ++id;
+  document.body.appendChild(iframe);
+  t.add_cleanup(() => iframe.remove());
 
-  const a = document.createElement("a")
-  a.rel = "noreferrer"
-  a.href = localEmbed + "?id=" + localId
-  iframe.contentDocument.body.appendChild(a)
-  a.click()
+  const a = document.createElement("a");
+  a.rel = "noreferrer";
+  a.href = localEmbed + "?id=" + localId;
+  iframe.contentDocument.body.appendChild(a);
+  a.click();
 
   self.addEventListener("message", t.step_func(e => {
     if(e.data.id === localId) {
-      assert_array_equals(e.data.output, ["null"])
-      t.done()
+      assert_array_equals(e.data.output, ["null"]);
+      t.done();
     }
-  }))
-}, "rel=noreferrer should redact")
+  }));
+}, "rel=noreferrer should redact");
--- html/browsers/history/the-location-interface/location-ancestororigins.window.js
+++ html/browsers/history/the-location-interface/location-ancestororigins.window.js
@@ -1,72 +1,70 @@
 test(() => {
   assert_equals(location.ancestorOrigins.length, 0)
-}, "location.ancestorOrigins basic check")
+}, "location.ancestorOrigins basic check");
 
 async_test(t => {
   const frame = document.createElement("iframe"),
-        rp = document.createElement("meta")
+        rp = document.createElement("meta");
   frame.onload = t.step_func_done(() => {
-    const ancestorOrigins = frame.contentWindow.location.ancestorOrigins
-    assert_equals(ancestorOrigins[0], self.origin)
-    assert_equals(ancestorOrigins.length, 1)
+    const ancestorOrigins = frame.contentWindow.location.ancestorOrigins;
+    assert_equals(ancestorOrigins[0], self.origin);
+    assert_equals(ancestorOrigins.length, 1);
 
-    rp.name = "referrer"
-    rp.content = "no-referrer"
-    document.head.appendChild(rp)
-    assert_equals(self[0].location.ancestorOrigins, ancestorOrigins)
-    assert_equals(ancestorOrigins[0], self.origin)
-    assert_equals(ancestorOrigins.length, 1)
-    rp.remove()
+    rp.name = "referrer";
+    rp.content = "no-referrer";
+    document.head.appendChild(rp);
+    assert_equals(self[0].location.ancestorOrigins, ancestorOrigins);
+    assert_equals(ancestorOrigins[0], self.origin);
+    assert_equals(ancestorOrigins.length, 1);
+    rp.remove();
 
-    frame.referrerPolicy = "no-referrer"
-    assert_equals(self[0].location.ancestorOrigins, ancestorOrigins)
-    assert_equals(ancestorOrigins[0], self.origin)
-    assert_equals(ancestorOrigins.length, 1)
+    frame.referrerPolicy = "no-referrer";
+    assert_equals(self[0].location.ancestorOrigins, ancestorOrigins);
+    assert_equals(ancestorOrigins[0], self.origin);
+    assert_equals(ancestorOrigins.length, 1);
 
-    frame.remove()
+    frame.remove();
   })
-  frame.src = "/common/blank.html"
-  document.body.appendChild(frame)
-}, "location.ancestorOrigins cannot be masked by a dynamic referrer policy")
+  frame.src = "/common/blank.html";
+  document.body.appendChild(frame);
+}, "location.ancestorOrigins cannot be masked by a dynamic referrer policy");
 
 async_test(t => {
-  const frame = document.createElement("iframe")
+  const frame = document.createElement("iframe");
   frame.onload = t.step_func_done(() => {
-    const ancestorOrigins = frame.contentWindow.location.ancestorOrigins
-    assert_equals(ancestorOrigins[0], "null")
-    assert_equals(ancestorOrigins.length, 1)
-    frame.remove()
+    const ancestorOrigins = frame.contentWindow.location.ancestorOrigins;
+    assert_equals(ancestorOrigins[0], "null");
+    assert_equals(ancestorOrigins.length, 1);
+    frame.remove();
   })
-  frame.src = "/common/blank.html"
-  frame.referrerPolicy = "no-referrer"
-  document.body.appendChild(frame)
-}, "location.ancestorOrigins can be masked by a predetermined referrer policy")
+  frame.src = "/common/blank.html";
+  frame.referrerPolicy = "no-referrer";
+  document.body.appendChild(frame);
+}, "location.ancestorOrigins can be masked by a predetermined referrer policy");
 
 async_test(t => {
-  const frame = document.createElement("iframe")
-  t.add_cleanup(() => {
-    frame.remove()
-  })
-  frame.src = new URL("resources/ancestororigins-embed.py?id=123&iframe=|./ancestororigins-embed.py%3Fid=1234", location.href.replace("://", "://天気の良い日.")).href
-  document.body.appendChild(frame)
+  const frame = document.createElement("iframe");
+  t.add_cleanup(() => frame.remove());
+  frame.src = new URL("resources/ancestororigins-embed.py?id=123&iframe=|./ancestororigins-embed.py%3Fid=1234", location.href.replace("://", "://天気の良い日.")).href;
+  document.body.appendChild(frame);
 
-  let almostDone = false
+  let almostDone = false;
   function localDone() {
     if(almostDone) {
-      t.done()
+      t.done();
     }
-    almostDone = true
+    almostDone = true;
   }
 
   self.onmessage = t.step_func(e => {
     if(e.data.id === 123) {
-      assert_array_equals(e.data.output, [location.origin])
-      localDone()
+      assert_array_equals(e.data.output, [location.origin]);
+      localDone();
     } else if(e.data.id === 1234) {
-      assert_array_equals(e.data.output, [location.origin.replace("://", "://xn--n8j6ds53lwwkrqhv28a."), location.origin])
-      localDone()
+      assert_array_equals(e.data.output, [location.origin.replace("://", "://xn--n8j6ds53lwwkrqhv28a."), location.origin]);
+      localDone();
     } else {
-      assert_unreached()
+      assert_unreached();
     }
-  })
-}, "location.ancestorOrigins and IDNA")
+  });
+}, "location.ancestorOrigins and IDNA");

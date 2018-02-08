9ee8a0a2e92f8568630281b234c4405f8675e048
Marijn Kruisselbrink <mek@chromium.org>
2018-02-08 13:45:33 -0800

    Use async_test rather than promise_test.

--- FileAPI/url/fetch-navigate.window.js
+++ FileAPI/url/fetch-navigate.window.js
@@ -1,22 +1,28 @@
 // META: script=resources/test-helpers.sub.js
-// META: timeout=long
 
 // We can't really detect when a cross origin navigation has failed, so instead
 // detect when it succeeds, and assume it failed if it didn't succeed before
-// this timeout.
-const load_timeout = 2000;
+// this timeout. All tests in this file as async_test rather than promise_test
+// to make sure these timeouts at least overlap, and prevent the whole file from
+// taking a very long time.
+const load_timeout = 3000;
 
-promise_test(t => {
+// Rejection-specific helper that provides more details
+function unreached_rejection(test, prefix) {
+  return test.step_func(function(error) {
+      var reason = error.message || error.name || error;
+      assert_unreached('Unexpected rejection: ' + reason);
+    });
+}
+
+async_test(t => {
   const blob_contents = 'hello world';
   const blob = new Blob([blob_contents], {type: 'text/html'});
-  return create_cross_origin_url(t, blob)
-    .then(url => {
-      with_iframe(url)
-        .then(t.unreached_func('Loading should have failed'));
-      return new Promise(resolve => {
-        t.step_timeout(resolve, load_timeout);
-      });
-    });
+  create_cross_origin_url(t, blob)
+    .then(url => with_iframe(url))
+    .then(t.unreached_func('Loading should have failed'))
+    .catch(unreached_rejection(t));
+  t.step_timeout(t.step_func_done(), load_timeout);
 }, 'Can not load a cross-origin blob URL in an iframe.');
 
 function receive_message_on_cross_origin_channel(t, channel_name) {
@@ -41,71 +47,66 @@ function window_contents_for_channel(channel_name) {
     '</script>';
 }
 
-promise_test(t => {
+async_test(t => {
   const channel_name = 'cross-origin-iframe';
   const blob_contents = window_contents_for_channel(channel_name);
   const blob = new Blob([blob_contents], {type: 'text/html'});
   receive_message_on_cross_origin_channel(t, channel_name)
-    .then(t.unreached_func('Loading should have failed'));;
-  return with_iframe('/common/blank.html')
+    .then(t.unreached_func('Loading should have failed'))
+    .catch(unreached_rejection(t));
+  with_iframe('/common/blank.html')
     .then(frame => {
       return create_cross_origin_url(t, blob)
         .then(url => {
           frame.contentWindow.location = url;
-          return new Promise(resolve => {
-            t.step_timeout(resolve, load_timeout);
-          });
         });
-    });
+    }).catch(unreached_rejection(t));
+  t.step_timeout(t.step_func_done(), load_timeout);
 }, 'Can not navigate an iframe to a cross-origin blob URL.');
 
-promise_test(t => {
+async_test(t => {
   const channel_name = 'cross-origin-window';
   const window_contents = window_contents_for_channel(channel_name);
   const blob = new Blob([window_contents], {type: 'text/html'});
   receive_message_on_cross_origin_channel(t, channel_name)
-    .then(t.unreached_func('Loading should have failed'));;
-  return create_cross_origin_url(t, blob)
+    .then(t.unreached_func('Loading should have failed'))
+    .catch(unreached_rejection(t));
+  create_cross_origin_url(t, blob)
     .then(url => {
       const win = window.open(url, 'blank_');
       add_completion_callback(() => { win.close(); });
-      return new Promise(resolve => {
-        t.step_timeout(resolve, load_timeout);
-      });
-    });
+    }).catch(unreached_rejection(t));
+  t.step_timeout(t.step_func_done(), load_timeout);
 }, 'Can not load a cross-origin blob URL in a window.');
 
-promise_test(t => {
+async_test(t => {
   const channel_name = 'navigate-cross-origin-window';
   const blob_contents = window_contents_for_channel(channel_name);
   const blob = new Blob([blob_contents], {type: 'text/html'});
   receive_message_on_cross_origin_channel(t, channel_name)
-    .then(t.unreached_func('Loading should have failed'));;
-  return create_cross_origin_url(t, blob)
+    .then(t.unreached_func('Loading should have failed'))
+    .catch(unreached_rejection(t));
+  create_cross_origin_url(t, blob)
     .then(url => {
       const win = window.open('/common/blank.html');
       add_completion_callback(() => { win.close(); });
       win.onload = t.step_func(() => {
         win.location = url;
       });
-      return new Promise(resolve => {
-        t.step_timeout(resolve, load_timeout);
-      });
-    });
+    }).catch(unreached_rejection(t));
+  t.step_timeout(t.step_func_done(), load_timeout);
 }, 'Can not navigate a window to a cross-origin blob URL.');
 
 
-promise_test(t => {
+async_test(t => {
   const channel_name = 'cross-origin-window-noopener';
   const window_contents = window_contents_for_channel(channel_name);
   const blob = new Blob([window_contents], {type: 'text/html'});
   receive_message_on_cross_origin_channel(t, channel_name)
-    .then(t.unreached_func('Loading should have failed'));;
-  return create_cross_origin_url(t, blob)
-    .then(url => {
-      window.open(url, 'blank_', 'noopener');
-      return new Promise(resolve => {
-        t.step_timeout(resolve, load_timeout);
-      });
-    });
+    .then(t.unreached_func('Loading should have failed'))
+    .catch(unreached_rejection(t));
+  create_cross_origin_url(t, blob)
+    .then(url => window.open(url, 'blank_', 'noopener'))
+    .catch(unreached_rejection(t));
+  t.step_timeout(t.step_func_done(), load_timeout);
 }, 'Can not load a cross-origin blob URL in a no-opener window.');

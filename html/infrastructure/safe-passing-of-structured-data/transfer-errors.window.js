85f1ac3cbe11635b13b439be7a70ee95d8adbb44
Anne van Kesteren <annevk@annevk.nl>
2018-03-14 15:57:07 +0100

    align tests with updated spec proposal

--- html/infrastructure/safe-passing-of-structured-data/transfer-errors.window.js
+++ html/infrastructure/safe-passing-of-structured-data/transfer-errors.window.js
@@ -1,5 +1,5 @@
-function assert_transfer_error(transferList) {
-  assert_throws("DataCloneError", () => self.postMessage({ get whatever() { throw 1 } }, "*", transferList));
+function assert_transfer_error(transferList, exception = "DataCloneError") {
+  assert_throws(exception, () => self.postMessage({ get whatever() { throw exception } }, "*", transferList));
 }
 
 test(() => {
@@ -8,20 +8,35 @@ test(() => {
 }, "Cannot transfer the same object twice");
 
 test(() => {
-  const b = new ArrayBuffer(1);
-  self.postMessage(null, "*", [b]);
-  assert_transfer_error([b]);
-}, "Cannot transfer a detached ArrayBuffer");
-
-test(() => {
   [self, self.document, new Image()].forEach(val => {
     assert_transfer_error([val]);
   });
 }, "Cannot transfer all objects");
 
+test(() => {
+  const b = new ArrayBuffer(1);
+  self.postMessage(null, "*", [b]);
+  assert_transfer_error([b], new Error("hi"));
+  assert_throws("DataCloneError", () => self.postMessage(null, '*', [b]));
+}, "Serialize throws before a transferred detached ArrayBuffer is found");
+
 promise_test(() => {
   return self.createImageBitmap(document.createElement("canvas")).then(bitmap => {
     bitmap.close();
-    assert_transfer_error([bitmap]);
+    assert_transfer_error([bitmap], new Error("hi"));
+    assert_throws("DataCloneError", () => self.postMessage(null, '*', [bitmap]));
   })
-}, "Cannot transfer a detached ImageBitmap");
+}, "Serialize throws before a transferred detached ImageBitmap is found");
+
+test(() => {
+  let seen = false;
+  const b = new ArrayBuffer(32),
+        message = {
+    get a() {
+      self.postMessage(null, '*', [b]);
+      seen = true;
+    }
+  };
+  assert_throws("DataCloneError", () => self.postMessage(message, '*', [b]));
+  assert_true(seen);
+}, "Cannot transfer an object detached while message was serialized")

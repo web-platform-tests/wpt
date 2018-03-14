662730cc4069da15045ea23c864cb106cabc954b
Brian Birtles <birtles@gmail.com>
2018-03-14 15:57:23 +0900

    [web-animations] Tidy up tests for duration property

--- web-animations/resources/timing-tests.js
+++ web-animations/resources/timing-tests.js
@@ -14,3 +14,17 @@
 const gBadDelayValues = [
   NaN, Infinity, -Infinity
 ];
+
+// ------------------------------
+//  Duration values
+// ------------------------------
+
+const gGoodDurationValues = [
+  { specified: 123.45, computed: 123.45 },
+  { specified: 'auto', computed: 0 },
+  { specified: Infinity, computed: Infinity },
+];
+
+const gBadDurationValues = [
+  -1, NaN, -Infinity, 'abc', '100'
+];

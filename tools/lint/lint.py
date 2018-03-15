687636279b0fba5781e6d066e46610881a378b68
teh-f00l <36428156+teh-f00l@users.noreply.github.com>
2018-03-15 02:06:01 -0400

    Update lint.py
    fixed a small variable error, and made the variable names more distinct

--- tools/lint/lint.py
+++ tools/lint/lint.py
@@ -748,19 +748,19 @@ def changed_files(wpt_root):
 def lint_paths(kwargs, wpt_root):
     if kwargs.get("paths"):
         #make new list in case there are multiple added directories
-        all_Paths=[]
+        paths=[]
         for path in kwargs.get("paths"):
             if os.path.isdir(path):
             # create extension for called directory and merge it with the existing path
                 new_Path = os.path.realpath(os.path.join(wpt_root, path))
                 extension = path+'/'
                 # get all files from new path
-                paths = list(all_filesystem_paths(new_Path))
+                path_dir = list(all_filesystem_paths(new_Path))
                 # add the extension to the strings for all files from the new path,
                 # so they may be called from the current directory
-                paths = [extension + path for path in paths]
+                paths = [extension + extended_path for extended_path in path_dir]
                 # add all files from a given directory to our master list of files to check
-                all_Paths=paths+all_Paths
+                paths=paths+path_dir
 
     elif kwargs["all"]:
         paths = list(all_filesystem_paths(wpt_root))

ccd6ec8cfd1bbc38ff0ad8e87cad5814b9b70613
Geoffrey Sneddon <me@gsnedders.com>
2018-03-16 16:45:04 +0000

    Get rid of redundant host/certificate_domain definitions

--- tools/wptrunner/wptrunner/browsers/servodriver.py
+++ tools/wptrunner/wptrunner/browsers/servodriver.py
@@ -54,7 +54,6 @@ def env_extras(**kwargs):
 
 def env_options():
     return {"host_ip": "127.0.0.1",
-            "host": "web-platform.test",
             "testharnessreport": "testharnessreport-servodriver.js",
             "supports_debugger": True}
 

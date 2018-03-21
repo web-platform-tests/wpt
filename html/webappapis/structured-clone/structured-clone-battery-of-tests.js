b475b2a45c1def4c9024bf731c669cae1a4f66c2
Surma <surma@surma.link>
2018-03-21 11:30:55 +0000

    Add documentation

--- html/webappapis/structured-clone/structured-clone-battery-of-tests.js
+++ html/webappapis/structured-clone/structured-clone-battery-of-tests.js
@@ -1,3 +1,15 @@
+/**
+ * Runs a collection of tests that determine if an API implements structured clone
+ * correctly.
+ *
+ * The `runner` parameter has the following properties:
+ * - `setup()`: An optional function run once before testing starts
+ * - `teardown()`: An option function run once after all tests are done
+ * - `preTest()`: An optional, async function run before a test
+ * - `postTest()`: An optional, async function run after a test is done
+ * - `structuredClone(obj)`: Required function that somehow structurally clones
+ *                               an object.
+ */
 async function structuredCloneBatteryOfTests(runner) {
   const tests = [
     {

51dc15d84da187e6e5e0569e80a738561db7eecb
Anne van Kesteren <annevk@annevk.nl>
2018-04-05 15:46:49 +0200

    Extra window.event tests
    To compliment #4790.

---
+++ dom/events/event-global-extra.window.js
@@ -0,0 +1,43 @@
+const otherWindow = document.body.appendChild(document.createElement("iframe")).contentWindow;
+
+[otherWindow.EventTarget, otherWindow.XMLHttpRequest].forEach(eventTargetConstructor => {
+  async_test(t => {
+    const eventTarget = new eventTargetConstructor();
+    eventTarget.addEventListener("hi", t.step_func_done(e => {
+      assert_equals(e, otherWindow.event);
+      assert_equals(undefined, self.event);
+    }));
+    eventTarget.dispatchEvent(new Event("hi"));
+  }, "window.event for constructors from another global: " + eventTargetConstructor);
+});
+
+// XXX: It would be good to test a subclass of EventTarget once we sort out
+// https://github.com/heycam/webidl/issues/540
+
+async_test(t => {
+  const host = document.createElement("div"),
+        shadow = host.attachShadow({ mode: "open" }),
+        child = shadow.appendChild(document.createElement("trala")),
+        furtherChild = child.appendChild(document.createElement("waddup"));
+  let counter = 0;
+  host.addEventListener("hi", t.step_func(e => {
+    assert_equals(window.event, e);
+    assert_equals(counter++, 3);
+  }));
+  child.addEventListener("hi", t.step_func(() => {
+    assert_equals(window.event, undefined);
+    assert_equals(counter++, 2);
+  }));
+  furtherChild.addEventListener("hi", t.step_func(() => {
+    host.appendChild(child);
+    assert_equals(window.event, undefined);
+    assert_equals(counter++, 0);
+  }));
+  furtherChild.addEventListener("hi", t.step_func(() => {
+    assert_equals(window.event, undefined);
+    assert_equals(counter++, 1);
+  }));
+  furtherChild.dispatchEvent(new Event("hi", { composed: true, bubbles: true }));
+  assert_equals(counter, 4);
+  t.done();
+}, "window.event should not be affected by nodes moving post-dispatch");

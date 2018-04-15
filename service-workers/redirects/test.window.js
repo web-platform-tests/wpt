8c7b71b32814453bddc9f8e4f4250ca3f3f781b0
Anne van Kesteren <annevk@annevk.nl>
2018-04-15 14:54:17 +0200

    address wait_for_state, cleanup, and style issues

--- service-workers/redirects/test.window.js
+++ service-workers/redirects/test.window.js
@@ -1,14 +1,13 @@
 // META: script=../service-worker/resources/test-helpers.sub.js
 
-promise_test(t => {
-  return service_worker_unregister_and_register(t, "test.window.js-serviceworker.js", "./").then(() => {
-    return with_iframe("test.window.js-redirect.py");
-  }).then(frame => {
-    assert_equals(frame.contentDocument.body.textContent, "stage 1 completed\n");
-    frame.remove();
-    return with_iframe("x/");
-  }).then(frame => {
-    assert_equals(frame.contentDocument.body.textContent, "YAY!\n");
-    frame.remove();
-  });
-});
+promise_test(async t => {
+  const reg = await service_worker_unregister_and_register(t, "test.window.js-serviceworker.js", "./");
+  t.add_cleanup(async () => await reg.unregister());
+  await wait_for_state(t, reg.installing, 'activated');
+  const frame = await with_iframe("test.window.js-redirect.py");
+  assert_equals(frame.contentDocument.body.textContent, "stage 1 completed\n");
+  frame.remove();
+  const frame2 = await with_iframe("x/");
+  assert_equals(frame2.contentDocument.body.textContent, "YAY!\n");
+  frame2.remove();
+}, "Passing an (opaque) redirect with a relative URL in its Location header to a second navigation with a different base URL");

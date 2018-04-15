8e87d5cf5e2e2d51c9f86e22a25eb2cb91d1d3de
Anne van Kesteren <annevk@annevk.nl>
2018-04-15 15:08:53 +0200

    rename resources

--- service-workers/redirects/opaque-redirect.window.js
+++ service-workers/redirects/opaque-redirect.window.js
@@ -1,13 +1,13 @@
 // META: script=../service-worker/resources/test-helpers.sub.js
 
 promise_test(async t => {
-  const reg = await service_worker_unregister_and_register(t, "test.window.js-serviceworker.js", "./");
+  const reg = await service_worker_unregister_and_register(t, "opaque-redirect-serviceworker.js", "./");
   t.add_cleanup(async () => await reg.unregister());
   await wait_for_state(t, reg.installing, 'activated');
-  const frame = await with_iframe("test.window.js-redirect.py");
+  const frame = await with_iframe("resources/opaque-redirect.py");
   assert_equals(frame.contentDocument.body.textContent, "stage 1 completed\n");
   frame.remove();
-  const frame2 = await with_iframe("x/");
+  const frame2 = await with_iframe("resources/x/");
   assert_equals(frame2.contentDocument.body.textContent, "YAY!\n");
   frame2.remove();
 }, "Passing an (opaque) redirect with a relative URL in its Location header to a second navigation with a different base URL");
--- service-workers/redirects/test.window.js-redirect.py
+++
@@ -1,3 +0,0 @@
-def main(request, response):
-    response.status = 302
-    response.headers.set("Location", "test.window.js-newlocation.txt")

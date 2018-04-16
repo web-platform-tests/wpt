f697d7f44055ac06feac4a2723a100623862cea1
Anne van Kesteren <annevk@annevk.nl>
2018-04-16 06:59:06 +0200

    clearer function name

--- fetch/api/basic/scheme-about.any.js
+++ fetch/api/basic/scheme-about.any.js
@@ -1,6 +1,6 @@
 // META: script=../resources/utils.js
 
-function checkKoUrl(url, method) {
+function checkNetworkError(url, method) {
   method = method || "GET";
   const desc = "Fetching " + url.substring(0, 45) + " with method " + method + " is KO"
   promise_test(function(test) {
@@ -9,9 +9,9 @@ function checkKoUrl(url, method) {
   }, desc);
 }
 
-checkKoUrl("about:blank", "GET");
-checkKoUrl("about:blank", "PUT");
-checkKoUrl("about:blank", "POST");
-checkKoUrl("about:invalid.com");
-checkKoUrl("about:config");
-checkKoUrl("about:unicorn");
+checkNetworkError("about:blank", "GET");
+checkNetworkError("about:blank", "PUT");
+checkNetworkError("about:blank", "POST");
+checkNetworkError("about:invalid.com");
+checkNetworkError("about:config");
+checkNetworkError("about:unicorn");

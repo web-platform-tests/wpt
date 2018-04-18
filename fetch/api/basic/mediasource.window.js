d6f0da6dc02e751eddf2808bde6164d4fb51146e
Anne van Kesteren <annevk@annevk.nl>
2018-04-18 11:42:39 +0200

    nit

--- fetch/api/basic/mediasource.window.js
+++ fetch/api/basic/mediasource.window.js
@@ -1,3 +1,3 @@
 promise_test(t => {
-    return promise_rejects(t, new TypeError(), fetch(URL.createObjectURL(new MediaSource())));
+  return promise_rejects(t, new TypeError(), fetch(URL.createObjectURL(new MediaSource())));
 }, "Cannot fetch blob: URL from a MediaSource");

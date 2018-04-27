1e51e1722a294830c85866462757d6924de7bac4
James Graham <james@hoppipolla.co.uk>
2018-04-27 16:19:58 +0100

    Explicitly set Firefox to use the fast reftest runner.
    This should happen by default on Linux, but it doesn't hurt to be explicit.

--- tools/ci/ci_taskcluster.sh
+++ tools/ci/ci_taskcluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $1 == "firefox" ]; then
-    ./wpt run firefox --log-tbpl=- --log-tbpl-level=info --log-wptreport=../artifacts/wpt_report.json --this-chunk=$3 --total-chunks=$4 --test-type=$2 -y --install-browser --no-pause --no-restart-on-unexpected
+    ./wpt run firefox --log-tbpl=- --log-tbpl-level=info --log-wptreport=../artifacts/wpt_report.json --this-chunk=$3 --total-chunks=$4 --test-type=$2 -y --install-browser --no-pause --no-restart-on-unexpected --reftest-internal
 elif [ $1 == "chrome" ]; then
     ./wpt run chrome --log-tbpl=- --log-tbpl-level=info --log-wptreport=../artifacts/wpt_report.json --this-chunk=$3 --total-chunks=$4 --test-type=$2 -y  --no-pause --no-restart-on-unexpected
 fi

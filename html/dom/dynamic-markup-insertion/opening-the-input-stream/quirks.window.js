3dba59525b8f88a17f41daa02b92a66b0b708793
Anne van Kesteren <annevk@annevk.nl>
2018-04-27 14:43:40 +0200

    things are a little weird

--- html/dom/dynamic-markup-insertion/opening-the-input-stream/quirks.window.js
+++ html/dom/dynamic-markup-insertion/opening-the-input-stream/quirks.window.js
@@ -2,7 +2,13 @@ test(() => {
   const frame = document.body.appendChild(document.createElement("iframe"));
   assert_equals(frame.contentDocument.compatMode, "BackCompat");
   frame.contentDocument.open();
-  assert_equals(frame.contentDocument.compatMode, "BackCompat"); // If you remove this assert the test passes in Chrome/Edge/Safari
+  assert_equals(frame.contentDocument.compatMode, "CSS1Compat");
+  frame.contentDocument.write("<!doctype html public");
+  assert_equals(frame.contentDocument.compatMode, "CSS1Compat");
+  frame.contentDocument.write(" \"-//IETF//DTD HTML 3//\"");
+  assert_equals(frame.contentDocument.compatMode, "CSS1Compat");
+  frame.contentDocument.write(">");
+  assert_equals(frame.contentDocument.compatMode, "BackCompat");
   frame.contentDocument.close();
   assert_equals(frame.contentDocument.compatMode, "BackCompat");
   frame.remove();

c190b1564afd081d2a7b341f06e0f2c5372f5b64
Philip Jägenstedt <philip@foolip.org>
2018-04-27 10:48:16 +0200

    Drive-by: use the sha instead of "master" in log output

--- tools/ci/tag_master.py
+++ tools/ci/tag_master.py
@@ -17,9 +17,9 @@ logging.basicConfig()
 logger = logging.getLogger(__name__)
 
 
-def get_pr(repo, owner, rev):
+def get_pr(repo, owner, sha):
     url = ("https://api.github.com/search/issues?q=type:pr+is:merged+repo:%s/%s+%s" %
-           (repo, owner, rev))
+           (repo, owner, sha))
     try:
         resp = urllib2.urlopen(url)
     except Exception as e:
@@ -38,10 +38,10 @@ def get_pr(repo, owner, rev):
 
     items = data["items"]
     if len(items) == 0:
-        logger.error("No PR found for master")
+        logger.error("No PR found for %s" % sha)
         return None
     if len(items) > 1:
-        logger.warning("Found multiple PRs for master")
+        logger.warning("Found multiple PRs for %s" % sha)
 
     pr = items[0]
 
@@ -69,7 +69,7 @@ def tag(repo, owner, sha, tag):
         logger.error("Got HTTP status %s" % resp.code)
         return False
 
-    logger.info("Tagged master as %s" % tag)
+    logger.info("Tagged %s as %s" % (sha, tag))
     return True
 
 

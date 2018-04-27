2bba72c8c008a441136aad0248fb77979daa367b
Anne van Kesteren <annevk@annevk.nl>
2018-04-27 13:26:23 +0200

    nit

--- fetch/api/idl.any.js
+++ fetch/api/idl.any.js
@@ -6,8 +6,8 @@ promise_test(async() => {
   const text = await (await fetch("/interfaces/fetch.idl")).text();
   const idl_array = new IdlArray();
   idl_array.add_idls(text);
-  idl_array.add_untested_idls("interface AbortSignal {};");
-  idl_array.add_untested_idls("interface ReadableStream {};");
+  idl_array.add_untested_idls("[Exposed=(Window,Worker)] interface AbortSignal {};");
+  idl_array.add_untested_idls("[Exposed=(Window,Worker)] interface ReadableStream {};");
   idl_array.add_untested_idls("enum ReferrerPolicy {};");
   idl_array.add_objects({
     Headers: ["new Headers()"],

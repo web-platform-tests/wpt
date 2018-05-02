ef1cdebc46f6a49632a6015bc36b0ca9115b6770
Mike Pennisi <mike@mikepennisi.com>
2018-05-02 16:45:06 -0400

    [infra] Remove reference to non-existent directory

--- tools/ci/jobs.py
+++ tools/ci/jobs.py
@@ -27,7 +27,6 @@ job_path_map = {
     "wptrunner_unittest": ["tools/wptrunner/*"],
     "build_css": ["css/"],
     "update_built": ["2dcontext/",
-                     "assumptions/",
                      "html/",
                      "offscreen-canvas/"],
     "wpt_integration": ["tools/"],

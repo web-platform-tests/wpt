37ae6e7655e7dfc3cfd46888065fe68cd2740f38
James Graham <james@hoppipolla.co.uk>
2018-05-02 21:36:54 +0100

    Check out the required commit directly in start.sh.
    Also optimise things so we don't download Chrome unless actually required.

--- tools/docker/start.sh
+++ tools/docker/start.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 set -ex
 
-cd web-platform-tests
-git fetch origin -n -q --depth=50
-git checkout -b master origin/master
+REPO=${1:-origin}
+BRANCH=${2:-master}
+REV=${3:-master}
+BROWSER=${4:-all}
+
+cd ~/web-platform-tests
+git fetch ${REPO} ${BRANCH} -n -q --depth=50
+git checkout -b build ${REV}
 
 sudo sh -c './wpt make-hosts-file >> /etc/hosts'
 
-# Install Chome dev
-deb_archive=google-chrome-unstable_current_amd64.deb
-wget https://dl.google.com/linux/direct/$deb_archive
+if [[ $BROWSER == "chrome"* ]] || [[ "$BROWSER" == all ]]
+then
+    # Install Chome dev
+    deb_archive=google-chrome-unstable_current_amd64.deb
+    wget https://dl.google.com/linux/direct/$deb_archive
 
-sudo gdebi -n $deb_archive
+    sudo gdebi -n $deb_archive
+fi
 
 sudo Xvfb $DISPLAY -screen 0 ${SCREEN_WIDTH}x${SCREEN_HEIGHT}x${SCREEN_DEPTH} &

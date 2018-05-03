904da22855cdcf22d76ac514eb7c4f699ab6f389
Anne van Kesteren <annevk@annevk.nl>
2018-05-03 10:59:36 +0200

    correct test

--- html/dom/dynamic-markup-insertion/opening-the-input-stream/load.window.js
+++ html/dom/dynamic-markup-insertion/opening-the-input-stream/load.window.js
@@ -1,21 +1,25 @@
 async_test(t => {
   const frame = document.createElement("iframe");
-  frame.onload = t.step_func(() => {
-    frame.contentDocument.open();
-    frame.contentDocument.write("x");
-  });
+  let happened = false;
 
-  self.addEventListener("load", t.step_func_done(e => {
+  self.addEventListener("load", t.step_func(e => {
     assert_true(e.whoa);
     assert_equals(frame.contentDocument.body.textContent, "x");
+    happened = true;
+    frame.contentDocument.close();
   }));
 
   document.body.appendChild(frame);
+  frame.contentDocument.open();
+  frame.contentDocument.write("x");
+  frame.onload = t.step_func_done(() => {
+    assert_true(happened);
+  });
 
   t.step_timeout(() => {
     const event = new Event("load");
     event.whoa = true;
     // Note: this also triggers testharness.js
     self.dispatchEvent(event);
-  })
+  }, 100);
 }, "document.open() delays the load event");

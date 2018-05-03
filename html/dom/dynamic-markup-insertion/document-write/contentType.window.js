29b04755495d52e69baaf162056b2d5df6d090c9
Anne van Kesteren <annevk@annevk.nl>
2018-05-03 08:23:00 +0200

    add more tests, add close() to make testing across UAs easier

--- html/dom/dynamic-markup-insertion/document-write/contentType.window.js
+++ html/dom/dynamic-markup-insertion/document-write/contentType.window.js
@@ -1,6 +1,13 @@
+// META: script=/common/media.js
+
+const videoURL = getVideoURI("/images/pattern"),
+      videoMIMEType = getMediaContentType(videoURL);
+
 [
+  [videoURL, videoMIMEType],
   ["/images/red.png", "image/png"],
-  ["/common/text-plain.txt", "text/plain"]
+  ["/common/text-plain.txt", "text/plain"],
+  ["/common/blank.html", "text/html"]
 ].forEach(val => {
   async_test(t => {
     const frame = document.body.appendChild(document.createElement("iframe"));
@@ -11,6 +18,9 @@
       assert_equals(frame.contentDocument.body.firstChild.localName, "b");
       assert_equals(frame.contentDocument.body.firstChild.textContent, "Heya");
       assert_equals(frame.contentDocument.contentType, val[1]);
+
+      // Make sure a load event is fired across browsers
+      frame.contentDocument.close();
     });
   }, "document.write() in a " + val[1] + " document");
 });

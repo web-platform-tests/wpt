a9924557d36c435cc9ed69bc8eb3706262dde394
Simon Pieters <zcorpan@gmail.com>
2018-05-03 17:00:02 +0200

    Deduplicate code in check-primitives.js

--- workers/semantics/structured-clone/support/check-primitive.js
+++ workers/semantics/structured-clone/support/check-primitive.js
@@ -1,59 +1,25 @@
-check('primitive undefined', undefined, compare_primitive);
-check('primitive null', null, compare_primitive);
-check('primitive true', true, compare_primitive);
-check('primitive false', false, compare_primitive);
-check('primitive string, empty string', '', compare_primitive);
-check('primitive string, lone high surrogate', '\uD800', compare_primitive);
-check('primitive string, lone low surrogate', '\uDC00', compare_primitive);
-check('primitive string, NUL', '\u0000', compare_primitive);
-check('primitive string, astral character', '\uDBFF\uDFFD', compare_primitive);
-check('primitive number, 0.2', 0.2, compare_primitive);
-check('primitive number, 0', 0, compare_primitive);
-check('primitive number, -0', -0, compare_primitive);
-check('primitive number, NaN', NaN, compare_primitive);
-check('primitive number, Infinity', Infinity, compare_primitive);
-check('primitive number, -Infinity', -Infinity, compare_primitive);
-check('primitive number, 9007199254740992', 9007199254740992, compare_primitive);
-check('primitive number, -9007199254740992', -9007199254740992, compare_primitive);
-check('primitive number, 9007199254740994', 9007199254740994, compare_primitive);
-check('primitive number, -9007199254740994', -9007199254740994, compare_primitive);
+var primitives = {'undefined':undefined,
+                  'null':null,
+                  'true':true,
+                  'false':false,
+                  'string, empty string':'',
+                  'string, lone high surrogate':'\uD800',
+                  'string, lone low surrogate':'\uDC00',
+                  'string, NUL':'\u0000',
+                  'string, astral character':'\uDBFF\uDFFD',
+                  'number, 0.2':0.2,
+                  'number, 0':0,
+                  'number, -0':-0,
+                  'number, NaN':NaN,
+                  'number, Infinity':Infinity,
+                  'number, -Infinity':-Infinity,
+                  'number, 9007199254740992':9007199254740992,
+                  'number, -9007199254740992':-9007199254740992,
+                  'number, 9007199254740994':9007199254740994,
+                  'number, -9007199254740994':-9007199254740994};
 
-check('Array primitives', [undefined,
-                           null,
-                           true,
-                           false,
-                           '',
-                           '\uD800',
-                           '\uDC00',
-                           '\u0000',
-                           '\uDBFF\uDFFD',
-                           0.2,
-                           0,
-                           -0,
-                           NaN,
-                           Infinity,
-                           -Infinity,
-                           9007199254740992,
-                           -9007199254740992,
-                           9007199254740994,
-                           -9007199254740994], compare_Array(enumerate_props(compare_primitive)));
-
-check('Object primitives', {'undefined':undefined,
-                           'null':null,
-                           'true':true,
-                           'false':false,
-                           'empty':'',
-                           'high surrogate':'\uD800',
-                           'low surrogate':'\uDC00',
-                           'nul':'\u0000',
-                           'astral':'\uDBFF\uDFFD',
-                           '0.2':0.2,
-                           '0':0,
-                           '-0':-0,
-                           'NaN':NaN,
-                           'Infinity':Infinity,
-                           '-Infinity':-Infinity,
-                           '9007199254740992':9007199254740992,
-                           '-9007199254740992':-9007199254740992,
-                           '9007199254740994':9007199254740994,
-                           '-9007199254740994':-9007199254740994}, compare_Object(enumerate_props(compare_primitive)));
+for (const [key, value] of Object.entries(primitives)) {
+  check(`primitive ${key}`, value, compare_primitive);
+}
+check('Array primitives', Object.values(primitives), compare_Array(enumerate_props(compare_primitive)));
+check('Object primitives', primitives, compare_Object(enumerate_props(compare_primitive)));

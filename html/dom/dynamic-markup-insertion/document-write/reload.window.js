1e7d904d39eeb95877e4ae60011ca2ab874d5f7f
Anne van Kesteren <annevk@annevk.nl>
2018-05-11 08:10:04 +0200

    add variants where .close() is called

--- html/dom/dynamic-markup-insertion/document-write/reload.window.js
+++ html/dom/dynamic-markup-insertion/document-write/reload.window.js
@@ -1,35 +1,43 @@
-async_test(t => {
-  const frame = document.body.appendChild(document.createElement("iframe"));
-  let reloaded = false;
-  frame.src = "/common/blank.html";
-  frame.onload = t.step_func(() => {
-    if (reloaded) {
+[false, true].forEach(callClose => {
+  async_test(t => {
+    const frame = document.body.appendChild(document.createElement("iframe"));
+    let reloaded = false;
+    frame.src = "/common/blank.html";
+    frame.onload = t.step_func(() => {
+      if (reloaded) {
         assert_equals(frame.contentDocument.body.textContent, "");
         t.done();
         return;
-    }
-    assert_false(reloaded);
+      }
+      assert_false(reloaded);
+      assert_equals(frame.contentDocument.body.textContent, "");
+      frame.contentDocument.write("Hey");
+      assert_equals(frame.contentDocument.body.textContent, "Hey");
+      if (callClose) {
+        frame.contentDocument.close();
+      }
+      reloaded = true;
+      frame.contentWindow.location.reload();
+    });
+  }, "document.write() and reloading" + (callClose ? " (and close()d)" : ""));
+
+  async_test(t => {
+    const frame = document.body.appendChild(document.createElement("iframe"));
+    let reloaded = false;
+    frame.onload = t.step_func(() => {
+      if (reloaded) {
+        assert_equals(frame.contentDocument.body.textContent, "");
+        t.done();
+        return;
+      }
+    });
     assert_equals(frame.contentDocument.body.textContent, "");
     frame.contentDocument.write("Hey");
     assert_equals(frame.contentDocument.body.textContent, "Hey");
+    if (callClose) {
+      frame.contentDocument.close();
+    }
     reloaded = true;
     frame.contentWindow.location.reload();
-  });
-}, "document.write() and reloading");
-
-async_test(t => {
-  const frame = document.body.appendChild(document.createElement("iframe"));
-  let reloaded = false;
-  frame.onload = t.step_func(() => {
-    if (reloaded) {
-        assert_equals(frame.contentDocument.body.textContent, "");
-        t.done();
-        return;
-    }
-  });
-  assert_equals(frame.contentDocument.body.textContent, "");
-  frame.contentDocument.write("Hey");
-  assert_equals(frame.contentDocument.body.textContent, "Hey");
-  reloaded = true;
-  frame.contentWindow.location.reload();
-}, "document.write() and reloading, part 2");
+  }, "document.write() and reloading, part 2" + (callClose ? " (and close()d)" : ""));
+});

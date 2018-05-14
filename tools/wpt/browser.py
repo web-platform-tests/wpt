362f156f59435822be6026aba9696e5d16bfc7fa
Robert Ma <robertma@chromium.org>
2018-05-14 17:59:31 -0400

    Find Chrome binary on more platforms and fail gracefully
    Also make similar changes to Opera.  Fixes #10992.

--- tools/wpt/browser.py
+++ tools/wpt/browser.py
@@ -4,6 +4,7 @@ import platform
 import re
 import shutil
 import stat
+import subprocess
 import sys
 import tempfile
 from abc import ABCMeta, abstractmethod
@@ -206,7 +207,6 @@ class Firefox(Browser):
 
     def install_prefs(self, binary, dest=None):
         version, channel = self.get_version_and_channel(binary)
-
         if dest is None:
             dest = os.pwd
 
@@ -293,8 +293,16 @@ class Chrome(Browser):
     """
 
     product = "chrome"
-    binary = "/usr/bin/google-chrome"
     requirements = "requirements_chrome.txt"
+
+    @property
+    def binary(self):
+        if uname[0] == "Linux":
+            return "/usr/bin/google-chrome"
+        if uname[0] == "Darwin":
+            return "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+        # TODO Windows?
+        return None
 
     def install(self, dest=None):
         raise NotImplementedError
@@ -339,7 +347,10 @@ class Chrome(Browser):
 
     def version(self, binary=None):
         binary = binary or self.binary
-        version_string = call(binary, "--version").strip()
+        try:
+            version_string = call(binary, "--version").strip()
+        except subprocess.CalledProcessError:
+            return None
         m = re.match(r"Google Chrome (.*)", version_string)
         if not m:
             return None
@@ -379,8 +390,14 @@ class Opera(Browser):
     """
 
     product = "opera"
-    binary = "/usr/bin/opera"
     requirements = "requirements_opera.txt"
+
+    @property
+    def binary(self):
+        if uname[0] == "Linux":
+            return "/usr/bin/opera"
+        # TODO Windows, Mac?
+        return None
 
     def install(self, dest=None):
         raise NotImplementedError
@@ -430,7 +447,10 @@ class Opera(Browser):
     def version(self, binary):
         """Retrieve the release version of the installed browser."""
         binary = binary or self.binary
-        output = call(binary, "--version")
+        try:
+            output = call(binary, "--version")
+        except subprocess.CalledProcessError:
+            return None
         return re.search(r"[0-9\.]+( [a-z]+)?$", output.strip()).group(0)
 
 

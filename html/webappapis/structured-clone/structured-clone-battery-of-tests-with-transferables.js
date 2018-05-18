ac2b79de7e547c3c06b45aba802daf129b152740
Surma <surma@surma.link>
2018-05-18 12:34:41 +0100

    Run transferable tests on dedicated worker

--- html/webappapis/structured-clone/structured-clone-battery-of-tests-with-transferables.js
+++ html/webappapis/structured-clone/structured-clone-battery-of-tests-with-transferables.js
@@ -3,15 +3,15 @@ structuredCloneBatteryOfTests.push({
   async f(runner) {
     const buffer = new Uint8Array([1]).buffer;
     const copy = await runner.structuredClone(buffer, [buffer]);
-    assert_equals(buffer.length, 0);
-    assert_equals(copy.length, 1);
+    assert_equals(buffer.byteLength, 0);
+    assert_equals(copy.byteLength, 1);
   }
 });
 
 structuredCloneBatteryOfTests.push({
   description: 'MessagePort',
   async f(runner) {
-    const {port1, port2} = new MessagePort();
+    const {port1, port2} = new MessageChannel();
     const copy = await runner.structuredClone(port2, [port2]);
     const msg = new Promise(resolve => port1.onmessage = resolve);
     copy.postMessage('ohai');

609013ec98392267c97c8f7341b47e8182b239ec
Geoffrey Sneddon <me@gsnedders.com>
2018-05-22 16:31:48 +0100

    Make it possible to override subdomains/not_subdomains in serve.Config

--- tools/serve/serve.py
+++ tools/serve/serve.py
@@ -752,9 +752,11 @@ class ConfigBuilder(config.ConfigBuilder):
     computed_properties = ["ws_doc_root"] + config.ConfigBuilder.computed_properties
 
     def __init__(self, *args, **kwargs):
+        if "subdomains" not in kwargs:
+            kwargs["subdomains"] = _subdomains
+        if "not_subdomains" not in kwargs:
+            kwargs["not_subdomains"] = _not_subdomains
         super(ConfigBuilder, self).__init__(
-            subdomains=_subdomains,
-            not_subdomains=_not_subdomains,
             *args,
             **kwargs
         )

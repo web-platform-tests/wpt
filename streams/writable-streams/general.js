eb54b34479c697209aa1c380c3046899cce02100
Adam Rice <ricea@chromium.org>
2018-05-22 14:48:35 +0900

    Apply domenic's patch

--- streams/writable-streams/general.js
+++ streams/writable-streams/general.js
@@ -111,26 +111,34 @@ promise_test(() => {
   );
 }, 'closed and ready on a released writer');
 
-promise_test(() => {
-  var thisObject = null;
+promise_test(t => {
+  let thisObject = null;
   // Calls to Sink methods after the first are implicitly ignored. Only the first value that is passed to the resolver
   // is used.
   class Sink {
     start() {
       // Called twice
-      assert_equals(thisObject, this, 'start should be called as a method');
+      t.step(() => {
+        assert_equals(this, thisObject, 'start should be called as a method');
+      });
     }
 
     write() {
-      assert_equals(thisObject, this, 'write should be called as a method');
+      t.step(() => {
+        assert_equals(this, thisObject, 'write should be called as a method');
+      });
     }
 
     close() {
-      assert_equals(thisObject, this, 'close should be called as a method');
+      t.step(() => {
+        assert_equals(this, thisObject, 'close should be called as a method');
+      });
     }
 
     abort() {
-      assert_equals(thisObject, this, 'abort should be called as a method');
+      t.step(() => {
+        assert_equals(this, thisObject, 'abort should be called as a method');
+      });
     }
   }
 
@@ -141,11 +149,11 @@ promise_test(() => {
   const writer = ws.getWriter();
 
   writer.write('a');
-  var closePromise = writer.close();
+  const closePromise = writer.close();
 
   const ws2 = new WritableStream(theSink);
   const writer2 = ws2.getWriter();
-  var abortPromise = writer2.abort();
+  const abortPromise = writer2.abort();
 
   return Promise.all([
     closePromise,

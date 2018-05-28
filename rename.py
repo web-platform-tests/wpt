ea404959c5700c5155d261e07e48400f9f89b811
Jonathon Kereliuk <kereliuk@chromium.org>
2018-05-28 12:20:11 -0400

    removed irrelavent file

--- rename.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-path = sys.argv[1]
-path_minus_owner = path.split('/')[:-1]
-path_minus_owner.append('REVIEWERS')
-new_path = '/'.join(path_minus_owner)
-
-os.rename(path, new_path)

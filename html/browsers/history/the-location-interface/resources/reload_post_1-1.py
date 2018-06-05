3c1a95454bc22fa69ddf06e6b84791bcd7705fc0
David Heiberg <dheiberg@mozilla.com>
2018-06-05 14:12:12 +0100

    Made the py file return a response body instead of redirecting. added -manual flag to test

--- html/browsers/history/the-location-interface/resources/reload_post_1-1.py
+++ html/browsers/history/the-location-interface/resources/reload_post_1-1.py
@@ -1,15 +1,13 @@
 def main(request, response):
-    # Redirects a POST request to the reload_post_1-1.html file
-    # Returns 404 on GET request
-
-    if request.method == "GET":
-        response.status = 404
-        return
-
-    status = 302
-
-    response.status = status
-
-    location = "/html/browsers/history/the-location-interface/reload_post_1-1.html"
-
-    response.headers.set("Location", location)
+    headers = [("Content-Type", "text/html")]
+    return headers, '''
+        <script>
+            onload = function() {opener.next()}
+            document.write(Math.random());
+        </script>
+        <form method="POST" action="">
+            <input type=hidden name=test value=test>
+            <input type=submit>
+        </form>
+        <button onclick="location.reload()">Reload</button>
+    '''

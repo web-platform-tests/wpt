web-platform-tests/wpt

commit d6c29bef8d4bcdfe4f689defca73360b07647d71

Refactor and split up with `variant` all encoding tests
Fixes #11015.

encoding/legacy-mb-japanese/euc-jp/eucjp-encode-href-errors-han.html
@@ -4,97 +4,56 @@
 <meta charset="euc-jp"> <!-- test breaks if the server overrides this -->
 <title>EUC-JP encoding errors (href, han)</title>
 <meta name="timeout" content="long">
+<meta name="variant" content="?1-1000">
+<meta name="variant" content="?1001-2000">
+<meta name="variant" content="?2001-3000">
+<meta name="variant" content="?3001-4000">
+<meta name="variant" content="?4001-5000">
+<meta name="variant" content="?5001-6000">
+<meta name="variant" content="?6001-7000">
+<meta name="variant" content="?7001-8000">
+<meta name="variant" content="?8001-9000">
+<meta name="variant" content="?9001-10000">
+<meta name="variant" content="?10001-10000">
+<meta name="variant" content="?11001-12000">
+<meta name="variant" content="?12001-13000">
+<meta name="variant" content="?13001-14000">
+<meta name="variant" content="?14001-15000">
+<meta name="variant" content="?15001-16000">
+<meta name="variant" content="?16001-17000">
+<meta name="variant" content="?17001-18000">
+<meta name="variant" content="?18001-19000">
+<meta name="variant" content="?19001-20000">
+<meta name="variant" content="?20001-21000">
+<meta name="variant" content="?21001-last">
 <script src="/resources/testharness.js"></script>
 <script src="/resources/testharnessreport.js"></script>
+<script src="/common/subset-tests.js"></script>
 <script src="jis0208_index.js"></script>
 <script src="eucjp-encoder.js"></script>
 <link rel="author" title="Richard Ishida" href="mailto:ishida@w3.org">
 <link rel="help" href="https://encoding.spec.whatwg.org/#euc-jp">
 <meta name="assert" content="The browser produces percent-escaped character references when writing characters to an href value and encoding han characters that are not in the euc-jp encoding.">
-<script>
-function encode(input, expected, desc) {
-    // tests whether a Unicode character is converted to an equivalent byte sequence by href
-    // input: a Unicode character
-    // expected: expected byte sequence
-    // desc: what's being tested
-    test(function() {
-        var a = document.createElement("a"); // <a> uses document encoding for URL's query
-        a.href = "https://example.com/?" + input;
-        result = a.search.substr(1); // remove leading "?"
-        assert_equals(result, expected);
-    }, desc);
-}
-
-// set up a simple array of unicode codepoints that are not encoded
-var codepoints = [];
-
-for (i = 0x4e00; i < 0x9fba; i++) {
-    result = eucjpEncoder(String.fromCodePoint(i));
-    if (!result) {
-        var item = {};
-        codepoints.push(item);
-        item.cp = i;
-        item.expected = "%26%23" + item.cp + "%3B";
-        item.desc = "cjk ";
-    }
-}
-
-for (i = 0xf900; i < 0xfa6e; i++) {
-    // compatibility
-    result = eucjpEncoder(String.fromCodePoint(i));
-    if (!result) {
-        var item = {};
-        codepoints.push(item);
-        item.cp = i;
-        item.expected = "%26%23" + item.cp + "%3B";
-        item.desc = "compatibility ";
-    }
-}
-
-for (i = 0xfa70; i < 0xfada; i++) {
-    result = eucjpEncoder(String.fromCodePoint(i));
-    if (!result) {
-        var item = {};
-        codepoints.push(item);
-        item.cp = i;
-        item.expected = "%26%23" + item.cp + "%3B";
-        item.desc = "compatibility ";
-    }
-}
-
-for (i = 0x3400; i < 0x4dbf; i++) {
-    // cjk extension A
-    result = eucjpEncoder(String.fromCodePoint(i));
-    if (!result) {
-        var item = {};
-        codepoints.push(item);
-        item.cp = i;
-        item.expected = "%26%23" + item.cp + "%3B";
-        item.desc = "extension A ";
-    }
-}
-
-// run the tests
-for (var x = 0; x < codepoints.length; x++) {
-    encode(
-        String.fromCodePoint(codepoints[x].cp),
-        codepoints[x].expected,
-        codepoints[x].desc +
-            " U+" +
-            codepoints[x].cp.toString(16).toUpperCase() +
-            " " +
-            String.fromCodePoint(codepoints[x].cp) +
-            " " +
-            codepoints[x].expected
-    );
-}
-
-// NOTES
-// this test relies on support for String.fromCodePoint, which appears to be supported by major desktop browsers
-// the tests exclude ASCII characters
-</script>
+<style>
+ iframe { display:none }
+ form { display:none }
+</style>
 </head>
 <body>
 <div id="log"></div>
+<script src="../../resources/ranges.js"></script>
+<script>
+var errors = true;
+var encoder = eucjpEncoder;
+var ranges = rangesHan;
+function expect(result, codepoint) {
+  return "%26%23" + codepoint + "%3B";
+}
+// Overwrite normalizeStr
+function normalizeStr(str) {
+  return str;
+}
+</script>
+<script src="../../resources/encode-href-common.js"></script>
 </body>
 </html>

encoding/legacy-mb-japanese/euc-jp/eucjp-encode-href-errors-hangul.html
@@ -4,62 +4,46 @@
 <meta charset="euc-jp"> <!-- test breaks if the server overrides this -->
 <title>EUC-JP encoding errors (href, hangul)</title>
 <meta name="timeout" content="long">
+<meta name="variant" content="?1-1000">
+<meta name="variant" content="?1001-2000">
+<meta name="variant" content="?2001-3000">
+<meta name="variant" content="?3001-4000">
+<meta name="variant" content="?4001-5000">
+<meta name="variant" content="?5001-6000">
+<meta name="variant" content="?6001-7000">
+<meta name="variant" content="?7001-8000">
+<meta name="variant" content="?8001-9000">
+<meta name="variant" content="?9001-10000">
+<meta name="variant" content="?10001-10000">
+<meta name="variant" content="?11001-last">
 <script src="/resources/testharness.js"></script>
 <script src="/resources/testharnessreport.js"></script>
+<script src="/common/subset-tests.js"></script>
 <script src="jis0208_index.js"></script>
 <script src="eucjp-encoder.js"></script>
 <link rel="author" title="Richard Ishida" href="mailto:ishida@w3.org">
 <link rel="help" href="https://encoding.spec.whatwg.org/#euc-jp">
 <meta name="assert" content="The browser produces percent-escaped character references when writing characters to an href value and encoding hangul characters that are not in the euc-jp encoding.">
-<script>
-function encode(input, expected, desc) {
-    // tests whether a Unicode character is converted to an equivalent byte sequence by href
-    // input: a Unicode character
-    // expected: expected byte sequence
-    // desc: what's being tested
-    test(function() {
-        var a = document.createElement("a"); // <a> uses document encoding for URL's query
-        a.href = "https://example.com/?" + input;
-        result = a.search.substr(1); // remove leading "?"
-        assert_equals(result, expected);
-    }, desc);
-}
-
-// set up a simple array of  unicode codepoints that are not encoded
-var codepoints = [];
-
-for (i = 0xac00; i < 0xd7af; i++) {
-    result = eucjpEncoder(String.fromCodePoint(i));
-    if (!result) {
-        var item = {};
-        codepoints.push(item);
-        item.cp = i;
-        item.expected = "%26%23" + item.cp + "%3B";
-        item.desc = "hangul ";
-    }
-}
-
-// run the tests
-for (var x = 0; x < codepoints.length; x++) {
-    encode(
-        String.fromCodePoint(codepoints[x].cp),
-        codepoints[x].expected,
-        codepoints[x].desc +
-            " U+" +
-            codepoints[x].cp.toString(16).toUpperCase() +
-            " " +
-            String.fromCodePoint(codepoints[x].cp) +
-            " " +
-            codepoints[x].expected
-    );
-}
-
-// NOTES
-// this test relies on support for String.fromCodePoint, which appears to be supported by major desktop browsers
-// the tests exclude ASCII characters
-</script>
+<style>
+ iframe { display:none }
+ form { display:none }
+</style>
 </head>
 <body>
 <div id="log"></div>
+<script src="../../resources/ranges.js"></script>
+<script>
+var errors = true;
+var encoder = eucjpEncoder;
+var ranges = rangesHangul;
+function expect(result, codepoint) {
+  return "%26%23" + codepoint + "%3B";
+}
+// Overwrite normalizeStr
+function normalizeStr(str) {
+  return str;
+}
+</script>
+<script src="../../resources/encode-href-common.js"></script>
 </body>
 </html>

encoding/legacy-mb-japanese/iso-2022-jp/iso2022jp-encode-form-errors-han.html
@@ -4,8 +4,30 @@
 <meta charset="iso-2022-jp"> <!-- test breaks if the server overrides this -->
 <title>ISO 2022 JP encoding errors (form, han)</title>
 <meta name="timeout" content="long">
+<meta name="variant" content="?1-1000">
+<meta name="variant" content="?1001-2000">
+<meta name="variant" content="?2001-3000">
+<meta name="variant" content="?3001-4000">
+<meta name="variant" content="?4001-5000">
+<meta name="variant" content="?5001-6000">
+<meta name="variant" content="?6001-7000">
+<meta name="variant" content="?7001-8000">
+<meta name="variant" content="?8001-9000">
+<meta name="variant" content="?9001-10000">
+<meta name="variant" content="?10001-11000">
+<meta name="variant" content="?11001-12000">
+<meta name="variant" content="?12001-13000">
+<meta name="variant" content="?13001-14000">
+<meta name="variant" content="?14001-15000">
+<meta name="variant" content="?15001-16000">
+<meta name="variant" content="?16001-17000">
+<meta name="variant" content="?17001-18000">
+<meta name="variant" content="?18001-19000">
+<meta name="variant" content="?19001-20000">
+<meta name="variant" content="?20001-last">
 <script src="/resources/testharness.js"></script>
 <script src="/resources/testharnessreport.js"></script>
+<script src="/common/subset-tests.js"></script>
 <script src="jis0208_index.js"></script>
 <script src="iso2022jp-encoder.js"></script>
 <link rel="author" title="Richard Ishida" href="mailto:ishida@w3.org">
@@ -18,167 +40,20 @@
 </head>
 <body>
 <div id="log"></div>
+<script src="../../resources/ranges.js"></script>
 <script>
-var tests = [];
-var cplist = [];
-var numTests = null;
-var numFrames = 2;
-var chunkSize = 400;
-var numChunks = null;
-var frames = null;
-var frames = null;
-var forms = null;
-var seperator = "\u0019";
-var encodedSeperator = encodeURIComponent("\u0019");
-var currentChunkIndex = 0;
-
-setup(function() {
-    // set up a simple array of unicode codepoints that are not encoded
-    var codepoints = [];
-
-    for (i = 0x4e00; i < 0x9fba; i++) {
-        result = iso2022jpEncoder(String.fromCodePoint(i));
-        if (!result) {
-            var item = {};
-            codepoints.push(item);
-            item.cp = i;
-            item.expected = "%26%23" + item.cp + "%3B";
-            item.desc = "cjk ";
-        }
-    }
-
-    for (i = 0xf900; i < 0xfa6e; i++) {
-        // compatibility
-        result = iso2022jpEncoder(String.fromCodePoint(i));
-        if (!result) {
-            var item = {};
-            codepoints.push(item);
-            item.cp = i;
-            item.expected = "%26%23" + item.cp + "%3B";
-            item.desc = "compatibility ";
-        }
-    }
-
-    for (i = 0xfa70; i < 0xfada; i++) {
-        result = iso2022jpEncoder(String.fromCodePoint(i));
-        if (!result) {
-            var item = {};
-            codepoints.push(item);
-            item.cp = i;
-            item.expected = "%26%23" + item.cp + "%3B";
-            item.desc = "compatibility ";
-        }
-    }
-
-    for (i = 0x3400; i < 0x4dbf; i++) {
-        // cjk extension A
-        result = iso2022jpEncoder(String.fromCodePoint(i));
-        if (!result) {
-            var item = {};
-            codepoints.push(item);
-            item.cp = i;
-            item.expected = "%26%23" + item.cp + "%3B";
-            item.desc = "extension A ";
-        }
-    }
-
-    // convert the information into a simple array of objects that can be easily traversed
-    var currentChunk = [];
-    var currentTests = [];
-    cplist = [currentChunk];
-    tests = [currentTests];
-    for (i = 0; i < codepoints.length; i++) {
-        if (currentChunk.length == chunkSize) {
-            currentChunk = [];
-            cplist.push(currentChunk);
-            currentTests = [];
-            tests.push(currentTests);
-        }
-        var item = {};
-        currentChunk.push(item);
-        item.cp = codepoints[i].cp;
-        item.expected = codepoints[i].expected;
-        item.desc = codepoints[i].desc;
-        currentTests.push(
-            async_test(
-                item.desc +
-                    " U+" +
-                    item.cp.toString(16).toUpperCase() +
-                    " " +
-                    String.fromCodePoint(item.cp) +
-                    " " +
-                    normalizeStr(item.expected)
-            )
-        );
-    }
-
-    numChunks = cplist.length;
-
-    for (var i = 0; i < numFrames; i++) {
-        var frame = document.createElement("iframe");
-        frame.id = frame.name = "frame-" + i;
-        document.body.appendChild(frame);
-        var form = document.createElement("form");
-        form.id = "form-" + i;
-        form.method = "GET";
-        form.action = "/common/blank.html";
-        form.acceptCharset = "iso-2022-jp";
-        form.target = frame.id;
-        var input = document.createElement("input");
-        input.id = input.name = "input-" + i;
-        form.appendChild(input);
-        document.body.appendChild(form);
-    }
-
-    addEventListener("load", function() {
-        frames = Array.prototype.slice.call(
-            document.getElementsByTagName("iframe")
-        );
-        forms = Array.prototype.slice.call(
-            document.getElementsByTagName("form")
-        );
-        inputs = Array.prototype.slice.call(
-            document.getElementsByTagName("input")
-        );
-        for (var i = 0; i < Math.min(numFrames, numChunks); i++) {
-            runNext(i);
-        }
-    });
-});
-
-function runNext(id) {
-    var i = currentChunkIndex;
-    currentChunkIndex += 1;
-
-    var iframe = frames[id];
-    var form = forms[id];
-    var input = inputs[id];
-
-    input.value = cplist[i]
-        .map(function(x) {
-            return String.fromCodePoint(x.cp);
-        })
-        .join(seperator);
-    form.submit();
-
-    iframe.onload = function() {
-        var url = iframe.contentWindow.location;
-        var query = url.search;
-        var result_string = query.substr(query.indexOf("=") + 1);
-        var results = result_string.split(encodedSeperator);
-
-        for (var j = 0; j < cplist[i].length; j++) {
-            var t = tests[i][j];
-            t.step(function() {
-                assert_equals(results[j], cplist[i][j].expected);
-            });
-            t.done();
-        }
-        if (currentChunkIndex < numChunks) {
-            runNext(id);
-        }
-    };
+var errors = true;
+var encoder = iso2022jpEncoder;
+var ranges = rangesHan;
+var separator = "\u0019";
+function expect(result, codepoint) {
+  return "%26%23" + codepoint + "%3B";
+}
+// Overwrite normalizeStr
+function normalizeStr(str) {
+  return str;
 }
 </script>
+<script src="../../resources/encode-form-common.js"></script>
 </body>
 </html>

commit 5e0fea392c570e81ec7932466ac4c142ff83606d

Fix bugs in split-up encoding tests
Bugs were introduced in d6c29bef8d4bcdfe4f689defca73360b07647d71

* Some script paths were wrong
* A big5 test referenced eucjpEncoder instead of big5Encoder
* That big5 test also referenced encode-form-common.js instead of
  encode-href-common.js

Fixes #11015.

Also add code to generate variants.

encoding/legacy-mb-japanese/euc-jp/eucjp-encode-href-errors-han.html
@@ -14,7 +14,7 @@
 <meta name="variant" content="?7001-8000">
 <meta name="variant" content="?8001-9000">
 <meta name="variant" content="?9001-10000">
-<meta name="variant" content="?10001-10000">
+<meta name="variant" content="?10001-11000">
 <meta name="variant" content="?11001-12000">
 <meta name="variant" content="?12001-13000">
 <meta name="variant" content="?13001-14000">

encoding/legacy-mb-japanese/euc-jp/eucjp-encode-href-errors-hangul.html
@@ -14,7 +14,7 @@
 <meta name="variant" content="?7001-8000">
 <meta name="variant" content="?8001-9000">
 <meta name="variant" content="?9001-10000">
-<meta name="variant" content="?10001-10000">
+<meta name="variant" content="?10001-11000">
 <meta name="variant" content="?11001-last">
 <script src="/resources/testharness.js"></script>
 <script src="/resources/testharnessreport.js"></script>

encoding/legacy-mb-japanese/iso-2022-jp/iso2022jp-encode-form-errors-han.html
@@ -24,7 +24,8 @@
 <meta name="variant" content="?17001-18000">
 <meta name="variant" content="?18001-19000">
 <meta name="variant" content="?19001-20000">
-<meta name="variant" content="?20001-last">
+<meta name="variant" content="?20001-21000">
+<meta name="variant" content="?21001-last">
 <script src="/resources/testharness.js"></script>
 <script src="/resources/testharnessreport.js"></script>
 <script src="/common/subset-tests.js"></script>

encoding/legacy-mb-japanese/iso-2022-jp/iso2022jp-encode-href-errors-han.html
@@ -24,7 +24,8 @@
 <meta name="variant" content="?17001-18000">
 <meta name="variant" content="?18001-19000">
 <meta name="variant" content="?19001-20000">
-<meta name="variant" content="?20001-last">
+<meta name="variant" content="?20001-21000">
+<meta name="variant" content="?21001-last">
 <script src="/resources/testharness.js"></script>
 <script src="/resources/testharnessreport.js"></script>
 <script src="/common/subset-tests.js"></script>
@@ -33,6 +34,9 @@
 <link rel="author" title="Richard Ishida" href="mailto:ishida@w3.org">
 <link rel="help" href="https://encoding.spec.whatwg.org/#iso-2022-jp">
 <meta name="assert" content="The browser produces percent-escaped character references when writing characters to an href value and encoding han characters that are not in the iso-2022-jp encoding.">
+</head>
+<body>
+<div id="log"></div>
 <script src="../../resources/ranges.js"></script>
 <script>
 var errors = true;
@@ -47,8 +51,5 @@ function normalizeStr(str) {
 }
 </script>
 <script src="../../resources/encode-href-common.js"></script>
-</head>
-<body>
-<div id="log"></div>
 </body>
 </html>

encoding/legacy-mb-japanese/shift_jis/sjis-encode-form-errors-han.html
@@ -24,7 +24,8 @@
 <meta name="variant" content="?17001-18000">
 <meta name="variant" content="?18001-19000">
 <meta name="variant" content="?19001-20000">
-<meta name="variant" content="?20001-last">
+<meta name="variant" content="?20001-21000">
+<meta name="variant" content="?21001-last">
 <script src="/resources/testharness.js"></script>
 <script src="/resources/testharnessreport.js"></script>
 <script src="/common/subset-tests.js"></script>

encoding/legacy-mb-japanese/shift_jis/sjis-encode-href-errors-han.html
@@ -14,7 +14,7 @@
 <meta name="variant" content="?7001-8000">
 <meta name="variant" content="?8001-9000">
 <meta name="variant" content="?9001-10000">
-<meta name="variant" content="?10001-10000">
+<meta name="variant" content="?10001-11000">
 <meta name="variant" content="?11001-12000">
 <meta name="variant" content="?12001-13000">
 <meta name="variant" content="?13001-14000">

encoding/legacy-mb-korean/euc-kr/euckr-encode-form-csksc56011987.html
@@ -37,7 +37,7 @@
 </head>
 <body>
 <div id="log"></div>
-<script src="../../ranges.js"></script>
+<script src="../../resources/ranges.js"></script>
 <script>
 var errors = false;
 var encoder = euckrEncoder;
@@ -47,6 +47,6 @@ function expect(result) {
   return "%" + result.replace(/ /g, "%");
 }
 </script>
-<script src="../../encode-form-common.js"></script>
+<script src="../../resources/encode-form-common.js"></script>
 </body>
 </html>

encoding/legacy-mb-korean/euc-kr/euckr-encode-form-errors-han.html
@@ -52,9 +52,6 @@ var separator = ",";
 function expect(result, codepoint) {
   return "%26%23" + codepoint + "%3B";
 }
-function normalizeStr(str) {
-  return str;
-}
 </script>
 <script src="../../resources/encode-form-common.js"></script>
 </body>

encoding/legacy-mb-korean/euc-kr/euckr-encode-form-iso-ir-149.html
@@ -37,7 +37,7 @@
 </head>
 <body>
 <div id="log"></div>
-<script src="../../ranges.js"></script>
+<script src="../../resources/ranges.js"></script>
 <script>
 var errors = false;
 var encoder = euckrEncoder;
@@ -47,6 +47,6 @@ function expect(result) {
   return "%" + result.replace(/ /g, "%");
 }
 </script>
-<script src="../../encode-form-common.js"></script>
+<script src="../../resources/encode-form-common.js"></script>
 </body>
 </html>

encoding/legacy-mb-korean/euc-kr/euckr-encode-form-korean.html
@@ -37,7 +37,7 @@
 </head>
 <body>
 <div id="log"></div>
-<script src="../../ranges.js"></script>
+<script src="../../resources/ranges.js"></script>
 <script>
 var errors = false;
 var encoder = euckrEncoder;
@@ -47,6 +47,6 @@ function expect(result) {
   return "%" + result.replace(/ /g, "%");
 }
 </script>
-<script src="../../encode-form-common.js"></script>
+<script src="../../resources/encode-form-common.js"></script>
 </body>
 </html>

encoding/legacy-mb-korean/euc-kr/euckr-encode-form-ks_c_5601-1987.html
@@ -37,7 +37,7 @@
 </head>
 <body>
 <div id="log"></div>
-<script src="../../ranges.js"></script>
+<script src="../../resources/ranges.js"></script>
 <script>
 var errors = false;
 var encoder = euckrEncoder;
@@ -47,6 +47,6 @@ function expect(result) {
   return "%" + result.replace(/ /g, "%");
 }
 </script>
-<script src="../../encode-form-common.js"></script>
+<script src="../../resources/encode-form-common.js"></script>
 </body>
 </html>

encoding/legacy-mb-korean/euc-kr/euckr-encode-form-ks_c_5601-1989.html
@@ -37,7 +37,7 @@
 </head>
 <body>
 <div id="log"></div>
-<script src="../../ranges.js"></script>
+<script src="../../resources/ranges.js"></script>
 <script>
 var errors = false;
 var encoder = euckrEncoder;
@@ -47,6 +47,6 @@ function expect(result) {
   return "%" + result.replace(/ /g, "%");
 }
 </script>
-<script src="../../encode-form-common.js"></script>
+<script src="../../resources/encode-form-common.js"></script>
 </body>
 </html>

encoding/legacy-mb-korean/euc-kr/euckr-encode-form-ksc_5601.html
@@ -37,7 +37,7 @@
 </head>
 <body>
 <div id="log"></div>
-<script src="../../ranges.js"></script>
+<script src="../../resources/ranges.js"></script>
 <script>
 var errors = false;
 var encoder = euckrEncoder;
@@ -47,6 +47,6 @@ function expect(result) {
   return "%" + result.replace(/ /g, "%");
 }
 </script>
-<script src="../../encode-form-common.js"></script>
+<script src="../../resources/encode-form-common.js"></script>
 </body>
 </html>

encoding/legacy-mb-korean/euc-kr/euckr-encode-href-errors-han.html
@@ -27,7 +27,7 @@
 <meta name="variant" content="?20001-21000">
 <meta name="variant" content="?21001-22000">
 <meta name="variant" content="?22001-23000">
-<meta name="variant" content="?23001-last">
+<<meta name="variant" content="?23001-last">
 <script src="/resources/testharness.js"></script>
 <script src="/resources/testharnessreport.js"></script>
 <script src="/common/subset-tests.js"></script>

encoding/legacy-mb-korean/euc-kr/euckr-encode-href.html
@@ -37,7 +37,7 @@
 </head>
 <body>
 <div id="log"></div>
-<script src="../../ranges.js"></script>
+<script src="../../resources/ranges.js"></script>
 <script>
 var errors = false;
 var encoder = euckrEncoder;
@@ -46,6 +46,6 @@ function expect(result, codepoint) {
   return "%" + result.replace(/ /g, "%");
 }
 </script>
-<script src="../../encode-href-common.js"></script>
+<script src="../../resources/encode-href-common.js"></script>
 </body>
 </html>

encoding/legacy-mb-tchinese/big5/big5-encode-href-errors-han.html
@@ -18,16 +18,7 @@
 <meta name="variant" content="?11001-12000">
 <meta name="variant" content="?12001-13000">
 <meta name="variant" content="?13001-14000">
-<meta name="variant" content="?14001-15000">
-<meta name="variant" content="?15001-16000">
-<meta name="variant" content="?16001-17000">
-<meta name="variant" content="?17001-18000">
-<meta name="variant" content="?18001-19000">
-<meta name="variant" content="?19001-20000">
-<meta name="variant" content="?20001-21000">
-<meta name="variant" content="?21001-22000">
-<meta name="variant" content="?22001-23000">
-<meta name="variant" content="?23001-last">
+<meta name="variant" content="?14001-last">
 <script src="/resources/testharness.js"></script>
 <script src="/resources/testharnessreport.js"></script>
 <script src="/common/subset-tests.js"></script>

encoding/legacy-mb-tchinese/big5/big5-encode-href-errors-hangul.html
@@ -14,7 +14,7 @@
 <meta name="variant" content="?7001-8000">
 <meta name="variant" content="?8001-9000">
 <meta name="variant" content="?9001-10000">
-<meta name="variant" content="?10001-10000">
+<meta name="variant" content="?10001-11000">
 <meta name="variant" content="?11001-last">
 <script src="/resources/testharness.js"></script>
 <script src="/resources/testharnessreport.js"></script>

encoding/legacy-mb-tchinese/big5/big5-encode-href-errors-misc.html
@@ -26,12 +26,12 @@
 <script src="../../resources/ranges.js"></script>
 <script>
 var errors = true;
-var encoder = eucjpEncoder;
+var encoder = big5Encoder;
 var ranges = rangesMisc;
 function expect(result, codepoint) {
   return "%26%23" + codepoint + "%3B";
 }
 </script>
-<script src="../../resources/encode-form-common.js"></script>
+<script src="../../resources/encode-href-common.js"></script>
 </body>
 </html>

encoding/legacy-mb-tchinese/big5/big5-encode-href.html
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta name="timeout" content="long">
-<title>Big5 encoding (href)</title>
 <meta charset="big5"> <!-- test breaks if the server overrides this -->
+<title>Big5 encoding (href)</title>
+<meta name="timeout" content="long">
 <meta name="variant" content="?1-1000">
 <meta name="variant" content="?1001-2000">
 <meta name="variant" content="?2001-3000">
@@ -11,7 +11,14 @@
 <meta name="variant" content="?4001-5000">
 <meta name="variant" content="?5001-6000">
 <meta name="variant" content="?6001-7000">
-<meta name="variant" content="?7001-last">
+<meta name="variant" content="?7001-8000">
+<meta name="variant" content="?8001-9000">
+<meta name="variant" content="?9001-10000">
+<meta name="variant" content="?10001-11000">
+<meta name="variant" content="?11001-12000">
+<meta name="variant" content="?12001-13000">
+<meta name="variant" content="?13001-14000">
+<meta name="variant" content="?14001-last">
 <script src="/resources/testharness.js"></script>
 <script src="/resources/testharnessreport.js"></script>
 <script src="/common/subset-tests.js"></script>

common/subset-tests.js
@@ -9,13 +9,33 @@
   var subTestEnd = Infinity;
   var match;
   if (location.search) {
-      match = /(?:^\?|&)(\d+)-(\d+|last)(?:&|$)/.exec(location.search);
-      if (match) {
-        subTestStart = parseInt(match[1], 10);
-        if (match[2] !== "last") {
-            subTestEnd = parseInt(match[2], 10);
-        }
+    match = /(?:^\?|&)(\d+)-(\d+|last)(?:&|$)/.exec(location.search);
+    if (match) {
+      subTestStart = parseInt(match[1], 10);
+      if (match[2] !== "last") {
+          subTestEnd = parseInt(match[2], 10);
       }
+    }
+    // Below is utility code to generate <meta> for copy/paste into tests.
+    // Sample usage:
+    // test.html?split=1000
+    match = /(?:^\?|&)split=(\d+)(?:&|$)/.exec(location.search);
+    if (match) {
+      var testsPerVariant = parseInt(match[1], 10);
+      add_completion_callback(tests => {
+        var total = tests.length;
+        var template = '<meta name="variant" content="?%s-%s">';
+        var metas = [];
+        for (var i = 1; i < total - testsPerVariant; i = i + testsPerVariant) {
+          metas.push(template.replace("%s", i).replace("%s", i + testsPerVariant - 1));
+        }
+        metas.push(template.replace("%s", i).replace("%s", "last"));
+        var pre = document.createElement('pre');
+        pre.textContent = metas.join('\n');
+        document.body.insertBefore(pre, document.body.firstChild);
+        document.getSelection().selectAllChildren(pre);
+      });
+    }
   }
   function shouldRunSubTest(currentSubTest) {
     return currentSubTest >= subTestStart && currentSubTest <= subTestEnd;
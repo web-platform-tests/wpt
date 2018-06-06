aa9930c87575c12fe4cf052301760817651a8a96
David Heiberg <dheiberg@mozilla.com>
2018-06-06 16:16:21 +0100

    Setting the TemporaryFile mode to 'rw+b' in request.py causes an IOError when trying to write to the _buf. Removing the mode, and using the default 'w+b', fixes this issue. Added 2 tests that test for this

--- tools/wptserve/tests/functional/test_request.py
+++ tools/wptserve/tests/functional/test_request.py
@@ -4,6 +4,8 @@ import pytest
 
 wptserve = pytest.importorskip("wptserve")
 from .base import TestUsingServer
+from wptserve.request import InputFile
+from urllib2 import HTTPError
 
 
 class TestInputFile(TestUsingServer):
@@ -35,6 +37,37 @@ class TestInputFile(TestUsingServer):
                           "12345ab\ncdef", "12345ab\n", "cdef"],
                          resp.read().split(" "))
 
+    def test_seek_input_longer_than_buffer(self):
+        old_max_buf = InputFile.max_buffer_size
+        InputFile.max_buffer_size = 10
+
+        @wptserve.handlers.handler
+        def handler(request, response):
+            rv = []
+            f = request.raw_input
+            f.seek(5)
+            rv.append(f.read(2))
+            rv.append(f.tell())
+            f.seek(0)
+            rv.append(f.tell())
+            rv.append(f.tell())
+            return " ".join(str(item) for item in rv)
+
+        route = ("POST", "/test/test_seek", handler)
+        self.server.router.register(*route)
+
+        try:
+            resp = self.request(route[1], method="POST", body="1"*20)
+        except HTTPError:
+            # Cleanup and fail test
+            InputFile.max_buffer_size = old_max_buf
+            self.assertTrue(False, 'This should not be reached. Server failed.')
+        
+        self.assertEqual(200, resp.getcode())
+        self.assertEqual(["11", "7", "0", "0"],
+                         resp.read().split(" "))
+        InputFile.max_buffer_size = old_max_buf
+
     def test_iter(self):
         @wptserve.handlers.handler
         def handler(request, response):
@@ -46,6 +79,29 @@ class TestInputFile(TestUsingServer):
         resp = self.request(route[1], method="POST", body="12345\nabcdef\r\nzyxwv")
         self.assertEqual(200, resp.getcode())
         self.assertEqual(["12345\n", "abcdef\r\n", "zyxwv"], resp.read().split(" "))
+
+    def test_iter_input_longer_than_buffer(self):
+        old_max_buf = InputFile.max_buffer_size
+        InputFile.max_buffer_size = 10
+
+        @wptserve.handlers.handler
+        def handler(request, response):
+            f = request.raw_input
+            return " ".join(line for line in f)
+
+        route = ("POST", "/test/test_iter", handler)
+        self.server.router.register(*route)
+
+        try:
+            resp = self.request(route[1], method="POST", body="12345\nabcdef\r\nzyxwv")
+        except HTTPError:
+            # Cleanup and fail test
+            InputFile.max_buffer_size = old_max_buf
+            self.assertTrue(False, 'This should not be reached. Server failed.')
+        
+        self.assertEqual(200, resp.getcode())
+        self.assertEqual(["12345\n", "abcdef\r\n", "zyxwv"], resp.read().split(" "))
+        InputFile.max_buffer_size = old_max_buf
 
 class TestRequest(TestUsingServer):
     def test_body(self):

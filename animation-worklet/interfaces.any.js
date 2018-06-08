4861604c2a1791817dfb37124428d37f0618c36c
Philip Jägenstedt <philip@foolip.org>
2018-06-08 16:13:43 +0200

    Add html dep for partial interface Window

--- animation-worklet/interfaces.any.js
+++ animation-worklet/interfaces.any.js
@@ -7,8 +7,10 @@
 
 promise_test(async () => {
   const idl = await (await fetch('/interfaces/animation-worklet.idl')).text();
+  const html = await (await fetch('/interfaces/html.idl')).text();
   const idlArray = new IdlArray();
   idlArray.add_idls(idl);
+  idlArray.add_dependency_idls(html);
   idlArray.test();
   done();
 }, 'Test driver');

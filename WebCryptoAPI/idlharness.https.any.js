b91e0bb568a07ad411ba11c1710a9cb6c5420ec0
Simon Pieters <zcorpan@gmail.com>
2018-06-11 11:59:24 -0400

    Fix order, pull in webidl.idl

--- WebCryptoAPI/idlharness.https.any.js
+++ WebCryptoAPI/idlharness.https.any.js
@@ -4,8 +4,9 @@
 
 promise_test(async() => {
   const idl_array = new IdlArray();
-  idl_array.add_dependency_idls(await (await fetch('/interfaces/html.idl')).text());
   idl_array.add_idls(await (await fetch('/interfaces/WebCryptoAPI.idl')).text());
+  idl_array.add_dependency_idls(await (await fetch('/interfaces/webidl.idl')).text());
+  idl_array.add_dependency_idls(await (await fetch('/interfaces/html.idl')).text());
   idl_array.add_objects({"Crypto":["crypto"], "SubtleCrypto":["crypto.subtle"]});
   idl_array.test();
 }, 'WebCrypto API IDL');

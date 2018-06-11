76f9737e4063f8e59f3274557a514374fc36f32a
Philip Jägenstedt <philip@foolip.org>
2018-06-11 10:47:26 +0200

    Indent

--- FileAPI/idlharness.worker.js
+++ FileAPI/idlharness.worker.js
@@ -4,12 +4,12 @@ importScripts("/resources/WebIDLParser.js", "/resources/idlharness.js");
 'use strict';
 
 promise_test(async () => {
-const idl = await fetch('/interfaces/FileAPI.idl').then(r => r.text());
-const dom = await fetch('/interfaces/dom.idl').then(r => r.text());
-const html = await fetch('/interfaces/html.idl').then(r => r.text());
-const url = await fetch('/interfaces/url.idl').then(r => r.text());
+  const idl = await fetch('/interfaces/FileAPI.idl').then(r => r.text());
+  const dom = await fetch('/interfaces/dom.idl').then(r => r.text());
+  const html = await fetch('/interfaces/html.idl').then(r => r.text());
+  const url = await fetch('/interfaces/url.idl').then(r => r.text());
 
-const idl_array = new IdlArray();
+  const idl_array = new IdlArray();
   idl_array.add_idls(idl);
   idl_array.add_dependency_idls(url);
   idl_array.add_dependency_idls(html);

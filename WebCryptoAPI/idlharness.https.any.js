15cdbbcd56cba5da188a86ee7cd73d08ebe95655
Simon Pieters <zcorpan@gmail.com>
2018-06-11 11:27:32 -0400

    Use html.idl

--- WebCryptoAPI/idlharness.https.any.js
+++ WebCryptoAPI/idlharness.https.any.js
@@ -3,12 +3,9 @@
 // META: script=/resources/idlharness.js
 
 promise_test(async() => {
-  const text = await (await fetch('/interfaces/WebCryptoAPI.idl')).text();
   const idl_array = new IdlArray();
-  idl_array.add_untested_idls("[Global] interface Window { };");
-  idl_array.add_untested_idls("interface ArrayBuffer {};");
-  idl_array.add_untested_idls("interface ArrayBufferView {};");
-  idl_array.add_idls(text);
+  idl_array.add_dependency_idls(await (await fetch('/interfaces/html.idl')).text());
+  idl_array.add_idls(await (await fetch('/interfaces/WebCryptoAPI.idl')).text());
   idl_array.add_objects({"Crypto":["crypto"], "SubtleCrypto":["crypto.subtle"]});
   idl_array.test();
 }, 'WebCrypto API IDL');

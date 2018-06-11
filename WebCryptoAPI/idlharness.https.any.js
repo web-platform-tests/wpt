c3ca96fa540b046f8acec04fed8bc6cfe7c51297
Simon Pieters <zcorpan@gmail.com>
2018-06-11 10:28:57 -0400

    Use promise_test in idlharness test

--- WebCryptoAPI/idlharness.https.any.js
+++ WebCryptoAPI/idlharness.https.any.js
@@ -1,23 +1,14 @@
+// META: global=window,worker
 // META: script=/resources/WebIDLParser.js
 // META: script=/resources/idlharness.js
-setup(function() {
-    var request = new XMLHttpRequest();
-    request.open("GET", "../interfaces/WebCryptoAPI.idl");
-    request.send();
-    request.onload = function() {
-        var idl_array = new IdlArray();
-        var idls = request.responseText;
 
-        idl_array.add_untested_idls("[Global] interface Window { };");
-
-        idl_array.add_untested_idls("interface ArrayBuffer {};");
-        idl_array.add_untested_idls("interface ArrayBufferView {};");
-
-        idl_array.add_idls(idls);
-
-        idl_array.add_objects({"Crypto":["crypto"], "SubtleCrypto":["crypto.subtle"]});
-
-        idl_array.test();
-        done();
-    };
-}, {explicit_done: true});
+promise_test(async() => {
+  const text = await (await fetch('/interfaces/WebCryptoAPI.idl')).text();
+  const idl_array = new IdlArray();
+  idl_array.add_untested_idls("[Global] interface Window { };");
+  idl_array.add_untested_idls("interface ArrayBuffer {};");
+  idl_array.add_untested_idls("interface ArrayBufferView {};");
+  idl_array.add_idls(text);
+  idl_array.add_objects({"Crypto":["crypto"], "SubtleCrypto":["crypto.subtle"]});
+  idl_array.test();
+}, 'WebCrypto API IDL');

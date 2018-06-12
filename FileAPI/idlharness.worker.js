a41c22d06129c9eeb289656cc1dd20dce479cbc9
Luke Bjerring <lukebjerring@google.com>
2018-06-12 09:27:37 -0400

    Restore done() call

--- FileAPI/idlharness.worker.js
+++ FileAPI/idlharness.worker.js
@@ -1,8 +1,6 @@
 importScripts("/resources/testharness.js");
 importScripts("/resources/WebIDLParser.js", "/resources/idlharness.js");
 
-'use strict';
-
 promise_test(async () => {
   const idl = await fetch('/interfaces/FileAPI.idl').then(r => r.text());
   const dom = await fetch('/interfaces/dom.idl').then(r => r.text());
@@ -11,9 +9,9 @@ promise_test(async () => {
 
   const idl_array = new IdlArray();
   idl_array.add_idls(idl);
-  idl_array.add_dependency_idls(url);
-  idl_array.add_dependency_idls(html);
   idl_array.add_dependency_idls(dom);
+  idl_array.add_dependency_idls(html);
+  idl_array.add_dependency_idls(url);
   idl_array.add_untested_idls("[Exposed=(Window,Worker)] interface ArrayBuffer {};");
   idl_array.add_objects({
     Blob: ['new Blob(["TEST"])'],
@@ -24,3 +22,4 @@ promise_test(async () => {
 
   idl_array.test();
 }, 'Test FileAPI IDL implementation');
+done();

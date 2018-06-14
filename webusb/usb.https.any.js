32992681cd9559e0096624e333a2532033837d40
Ovidio Henriquez <odejesush@chromium.org>
2018-06-14 09:28:40 -0700

    Reland "Multi-global WebUSB WPT for window and worker"
    This is a reland of 1ff37758f07612906228581dbc808df4b282e250
    
    Original change's description:
    > Multi-global WebUSB WPT for window and worker
    >
    > This change updates the existing web platform tests for WebUSB to use
    > multi-global tests to test the API in the supported contexts. The
    > change updates the following tests:
    > * idlharness
    > * usb
    > * usbConnectionEvent
    >
    > Bug: 841510
    > Change-Id: I6b118aa4121109cd6a48054c2ebc69b831660afc
    > Reviewed-on: https://chromium-review.googlesource.com/1087209
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Commit-Queue: Ovidio Henriquez <odejesush@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#564981}
    
    Bug: 841510
    Change-Id: Ie354fdd54a4cd94a4801ee634c8d3263e0aa6e93
    Reviewed-on: https://chromium-review.googlesource.com/1089647
    Commit-Queue: Ovidio Henriquez <odejesush@chromium.org>
    Reviewed-by: Chong Zhang <chongz@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567290}

--- webusb/usb.https.any.js
+++ webusb/usb.https.any.js
@@ -1,18 +1,9 @@
+// META: script=/webusb/resources/fake-devices.js
+// META: script=/webusb/resources/usb-helpers.js
+// META: global=sharedworker
 'use strict';
-importScripts('/resources/testharness.js');
-importScripts('/webusb/resources/fake-devices.js');
-importScripts('/webusb/resources/usb-helpers.js');
 
 let usbDevice, devicesFirstTime, fakeDevice, removedDevice;
-
-test(() => {
-  assert_true(navigator.usb instanceof USB,
-      'navigator.usb should be defined as a USB object');
-  assert_true(navigator.usb.getDevices instanceof Function,
-      'navigator.usb.getDevices should be defined as a function');
-  assert_equals(typeof navigator.usb.requestDevice, 'undefined',
-      'navigator.usb.requestDevice should not be defined');
-}, 'Web workers should have the WebUSB API exposed as defined in the spec.');
 
 usb_test(() => getFakeDevice()
     .then(_ => usbDevice = _.device)

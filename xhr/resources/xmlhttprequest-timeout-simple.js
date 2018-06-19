e70b6d931e204a0cf50c1ef408e412eb58199482
Simon Pieters <zcorpan@gmail.com>
2018-06-19 16:49:26 +0200

    Split up xhr/xmlhttprequest-timeout-worker-simple.html

--- xhr/resources/xmlhttprequest-timeout-simple.js
+++ xhr/resources/xmlhttprequest-timeout-simple.js
@@ -1,6 +1,14 @@
 if (this.document === undefined)
   importScripts("xmlhttprequest-timeout.js");
 
-runTestRequests([ new RequestTracker(true, "no time out scheduled, load fires normally", 0),
-                  new RequestTracker(true, "load fires normally", TIME_NORMAL_LOAD),
-                  new RequestTracker(true, "timeout hit before load", TIME_REGULAR_TIMEOUT) ]);
+var tests = [
+  [true, "no time out scheduled, load fires normally", 0],
+  [true, "load fires normally", TIME_NORMAL_LOAD],
+  [true, "timeout hit before load", TIME_REGULAR_TIMEOUT],
+];
+
+if (location.search) {
+  tests = tests.filter(test => test[1] == decodeURIComponent(location.search.substr(1)));
+}
+
+runTestRequests(tests.map(test => new RequestTracker(...test)));

028e48a2bcf59457162215d2af7fae697e1863f3
Simon Pieters <zcorpan@gmail.com>
2018-06-20 14:12:25 +0200

    Fix remaining XHR tests

--- xhr/resources/xmlhttprequest-timeout-overrides.js
+++ xhr/resources/xmlhttprequest-timeout-overrides.js
@@ -7,6 +7,6 @@ Sets up three requests to a resource that will take 0.6 seconds to load:
 3) timeout first set to 0, after TIME_REGULAR_TIMEOUT it is set to TIME_REGULAR_TIMEOUT * 10, asserts load fires
 */
 
-runTestRequests([ [true, "timeout disabled after initially set", TIME_NORMAL_LOAD, TIME_REGULAR_TIMEOUT, 0],
-                  [true, "timeout overrides load after a delay", TIME_NORMAL_LOAD, TIME_DELAY, TIME_REGULAR_TIMEOUT],
-                  [true, "timeout enabled after initially disabled", 0, TIME_REGULAR_TIMEOUT, TIME_NORMAL_LOAD * 10] ]);
+runTestRequests([ ["RequestTracker", true, "timeout disabled after initially set", TIME_NORMAL_LOAD, TIME_REGULAR_TIMEOUT, 0],
+                  ["RequestTracker", true, "timeout overrides load after a delay", TIME_NORMAL_LOAD, TIME_DELAY, TIME_REGULAR_TIMEOUT],
+                  ["RequestTracker", true, "timeout enabled after initially disabled", 0, TIME_REGULAR_TIMEOUT, TIME_NORMAL_LOAD * 10] ]);

04cd06d7abbd8d26e6096df5d6681a87dc06f4c1
Kagami Sascha Rosylight <saschanaz@outlook.com>
2018-06-26 19:28:35 +0900

    remove noqa annotation

--- tools/wptserve/wptserve/sslutils/__init__.py
+++ tools/wptserve/wptserve/sslutils/__init__.py
@@ -1,4 +1,3 @@
-# flake8: noqa (not ideal, but nicer than adding noqa: F401 to every line!)
 from .base import NoSSLEnvironment
 from .openssl import OpenSSLEnvironment
 from .pregenerated import PregeneratedSSLEnvironment

ebe5a18b1822c8e82dabb2d11e8cab793a263921
Luke Bjerring <lukebjerring@google.com>
2018-07-09 16:30:09 -0400

    Add test, fix syntax errors

---
+++ mediaont-api/idlharness.window.js
@@ -0,0 +1,12 @@
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+'use strict';
+
+// https://www.w3.org/TR/mediaont-api-1.0/
+
+idl_test(
+  ['mediaont-api'],
+  [], // No deps
+  null, // No object setup
+  'mediaont-api interfaces.');

85f23b4f045e66103f640bef10cd782eaaca9f25
Mark Nottingham <mnot@mnot.net>
2018-07-09 12:08:11 +1000

    move docs to README

--- fetch/http-cache/README.md
+++ fetch/http-cache/README.md
@@ -22,3 +22,43 @@ A few notes:
 
 * At the moment, Edge doesn't appear to using HTTP caching in conjunction
   with Fetch at all.
+
+
+## Test Format
+
+Each test run gets its own URL and randomized content and operates independently.
+
+Each test is an an array of objects, with the following members:
+
+- `name` - The name of the test.
+- `requests` - a list of request objects (see below).
+
+Possible members of a request object:
+
+- template - A template object for the request, by name.
+- request_method - A string containing the HTTP method to be used.
+- request_headers - An array of `[header_name_string, header_value_string]` arrays to
+                    emit in the request.
+- request_body - A string to use as the request body.
+- mode - The mode string to pass to `fetch()`.
+- credentials - The credentials string to pass to `fetch()`.
+- cache - The cache string to pass to `fetch()`.
+- pause_after - Boolean controlling a 3-second pause after the request completes.
+- response_status - A `[number, string]` array containing the HTTP status code
+                    and phrase to return.
+- response_headers - An array of `[header_name_string, header_value_string]` arrays to
+                     emit in the response. These values will also be checked like
+                     expected_response_headers, unless there is a third value that is
+                     `false`.
+- response_body - String to send as the response body. If not set, it will contain
+                  the test identifier.
+- expected_type - One of `["cached", "not_cached", "lm_validate", "etag_validate", "error"]`
+- expected_status - A number representing a HTTP status code to check the response for.
+                    If not set, the value of `response_status[0]` will be used; if that
+                    is not set, 200 will be used.
+- expected_request_headers - An array of `[header_name_string, header_value_string]` representing
+                              headers to check the request for.
+- expected_response_headers - An array of `[header_name_string, header_value_string]` representing
+                              headers to check the response for. See also response_headers.
+- expected_response_text - A string to check the response body against.
+
--- fetch/http-cache/http-cache.js
+++ fetch/http-cache/http-cache.js
@@ -1,42 +1,6 @@
 /* global btoa fetch token promise_test step_timeout */
 /* global assert_equals assert_true assert_false assert_own_property assert_throws assert_unreached assert_less_than */
 
-/**
- * Each test run gets its own URL and randomized content and operates independently.
- *
- * Tests are an array of objects, each representing a request to make and check.
- * The cache.py server script stashes an entry containing observed headers for
- * each request it receives.  When the test fetches have run, this state is retrieved
- * and the expected_* lists are checked, including their length.
- *
- * Request object keys:
- * - template - A template object for the request, by name -- see "templates" below.
- * - request_method - A string containing the HTTP method to be used.
- * - request_headers - An array of [header_name_string, header_value_string] arrays to
- *                     emit in the request.
- * - request_body - A string to use as the request body.
- * - mode - The mode string to pass to fetch().
- * - credentials - The credentials string to pass to fetch().
- * - cache - The cache string to pass to fetch().
- * - pause_after - Boolean controlling a 3-second pause after the request completes.
- * - response_status - A [number, string] array containing the HTTP status code
- *                     and phrase to return.
- * - response_headers - An array of [header_name_string, header_value_string] arrays to
- *                      emit in the response. These values will also be checked like
- *                      expected_response_headers, unless there is a third value that is
- *                      false.
- * - response_body - String to send as the response body. If not set, it will contain
- *                   the test identifier.
- * - expected_type - One of ["cached", "not_cached", "lm_validate", "etag_validate", "error"]
- * - expected_status - A number representing a HTTP status code to check the response for.
- *                     If not set, the value of response_status[0] will be used; if that
- *                     is not set, 200 will be used.
- * - expected_request_headers - An array of [header_name_string, header_value_string] representing
- *                               headers to check the request for.
- * - expected_response_headers - An array of [header_name_string, header_value_string] representing
- *                               headers to check the response for. See also response_headers.
- * - expected_response_text - A string to check the response body against.
- */
 
 var templates = {
   'fresh': {

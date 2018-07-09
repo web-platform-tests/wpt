94020cb8a1084a784bb6217974f4b45d177d37d3
Luke Bjerring <lukebjerring@google.com>
2018-07-09 10:44:31 -0400

    Add test

---
+++ intersection-observer/idlharness.window.js
@@ -0,0 +1,27 @@
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+'use strict';
+
+// https://w3c.github.io/IntersectionObserver/
+
+idl_test(
+  ['intersection-observer'],
+  [], // No deps :)
+  idl_array => {
+    let observer;
+    try {
+      var options = {
+        root: document.body,
+        rootMargin: '0px',
+        threshold: 1.0
+      }
+      observer = new IntersectionObserver(() => {}, {});
+    } catch (e) {
+      // Will be surfaced by idlharness.js's test_object below.
+    }
+    idl_array.add_objects({
+      IntersectionObserver: [observer],
+    });
+  },
+  'intersection-observer interfaces.');

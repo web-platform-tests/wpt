7783dc85fee2e86eb4e2e166856fb0b91f75c553
Luke Bjerring <lukebjerring@chromium.org>
2018-07-10 16:45:12 -0400

    Clean up test

---
+++ selection/idlharness.window.js
@@ -0,0 +1,28 @@
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+'use strict';
+
+// https://w3c.github.io/selection-api/
+
+idl_test(
+  ['selection-api'],
+  ['dom', 'cssom', 'touchevents', 'uievents', 'html'],
+  idlArray => {
+    // "Cast" window as GlobalEventHandlers
+    try {
+      window.global =
+        Object.assign(Object.create(GlobalEventHandlers), window);
+    } catch (e) {
+      // Will be surfaced in idlharness.js's test_object below.
+    }
+
+    idlArray.add_objects({
+      Window: ['window'],
+      Document: ['document'],
+      Selection: ['getSelection()'],
+      GlobalEventHandlers: ['global'],
+    });
+  },
+  'selection-api interfaces'
+);

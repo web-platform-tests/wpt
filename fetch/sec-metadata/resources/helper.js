655ee10dd13659e0faac2088ed2861764c8698e6
Maciek Trzos <mtrzos@google.com>
2018-07-10 11:00:51 +0200

    Added support for testing empty header.

--- fetch/sec-metadata/resources/helper.js
+++ fetch/sec-metadata/resources/helper.js
@@ -9,6 +9,8 @@ function parse_metadata(value) {
 }
 
 function assert_header_equals(value, expected) {
+  // check that the returned value is an object, not a String
+  assert_not_equals(value, "", "Empty Sec-Metadata header.");
   let result = parse_metadata(value);
   assert_equals(result.cause, expected.cause, "cause");
   assert_equals(result.destination, expected.destination, "destination");

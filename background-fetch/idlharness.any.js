eedd69bb16b2bf4a3f23395246acdb5bc54c968b
Luke Bjerring <lukebjerring@chromium.org>
2018-07-11 19:22:02 -0400

    Update test

---
+++ background-fetch/idlharness.any.js
@@ -0,0 +1,22 @@
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+'use strict';
+
+// https://wicg.github.io/background-fetch/
+
+idl_test(
+  ['background-fetch'],
+  ['service-workers', 'dedicated-workers', 'dom'],
+  idl_array => {
+    const isServiceWorker = location.pathname.includes('.serviceworker.');
+    if (isServiceWorker) {
+      idl_array.add_objects({
+        ServiceWorkerGlobalScope: ['self'],
+        ServiceWorkerRegistration: ['registration'],
+        BackgroundFetchManager: ['registration.backgroundFetch'],
+      });
+    }
+  },
+  'background-fetch interfaces'
+);

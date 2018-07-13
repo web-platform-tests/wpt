49898603acdd52308e17dc14db6d6ec558e094c9
James Graham <james@hoppipolla.co.uk>
2018-07-13 14:54:31 +0100

    Convert the config to a builder pattern.
    We have two sets of problems with the config:
    
    * It has to go to some lengths to be thread/process safe since several
      attributes are lazilly computed, and cached, by whichever thread
      happens to be first to access them.
    
    * The OpenSSL-based certificate implementation is totally broken
      because this class needs to be used as a context manager in order to
      initialise some resources associated with the fake CA used to
      generate the certificates.
    
    This suggests that the design is not really correct. We really want to
    generate the configuration once in a single process, and then share the
    generated configuration across multiple processes. We also want to
    ensure that the ssl environment is initialised once in the main
    process using a context manager.
    
    This patch introduces a ConfigBuilder object which allows
    configuration properties to be set. When used as a context manager, it
    computes the generated configuration, initalises the SSL environment,
    and updates the configuration with that data, and returns a config
    object with the final, immutable, configuration which may then be
    shared across processes.

--- tools/wptserve/wptserve/sslutils/openssl.py
+++ tools/wptserve/wptserve/sslutils/openssl.py
@@ -6,6 +6,8 @@ import subprocess
 import tempfile
 from datetime import datetime, timedelta
 
+from six import iteritems
+
 # Amount of time beyond the present to consider certificates "expired." This
 # allows certificates to be proactively re-generated in the "buffer" period
 # prior to their exact expiration time.
@@ -67,7 +69,7 @@ class OpenSSL(object):
         # StartProcess is picky about all the keys/values being plain strings,
         # but at least in MSYS shells, the os.environ dictionary can be mixed.
         env = {}
-        for k, v in os.environ.iteritems():
+        for k, v in iteritems(os.environ):
             try:
                 env[k.encode("utf8")] = v.encode("utf8")
             except UnicodeDecodeError:

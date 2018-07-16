f422a40d65c6b7301bd5e11af2e7915af7f5f349
Luke Bjerring <lukebjerring@chromium.org>
2018-07-16 19:14:35 -0400

    Add test for PerformanceObserverEntryList

--- performance-timeline/idlharness.any.js
+++ performance-timeline/idlharness.any.js
@@ -6,22 +6,45 @@
 
 'use strict';
 
-idl_test(
-  ['performance-timeline'],
-  ['hr-time', 'dom'],
-  idl_array => {
+promise_test(async t => {
+  const observe = new Promise((resolve, reject) => {
     try {
-      const callback = (e, o) => {};
-      self.observerEntry = new ResizeObserverEntry(callback);
+      const callback = (e, o) => { };
+      self.observer = new PerformanceObserver((entries, observer) => {
+        self.entryList = entries;
+        resolve();
+      });
+      observer.observe({ entryTypes: ['mark'] });
+      performance.mark('test');
     } catch (e) {
-      // Will be surfaced when entry is undefined below.
+      reject(e);
     }
+  });
+  const timeout = new Promise((_, reject) => {
+    t.step_timeout(() => reject('Timed out waiting for observation'), 3000);
+  });
+  const execute_test = () => {
+    idl_test(
+      ['performance-timeline'],
+      ['hr-time', 'dom'],
+      idl_array => {
+        idl_array.add_objects({
+          Performance: ['performance'],
+          PerformanceObserver: ['observer'],
+          // NOTE: PerformanceEntry is implicitly tested in user-timing.
+          PerformanceObserverEntryList: ['entryList'],
+          PerformanceObserverEntry: ['entryList[0]'],
+        });
+      },
+      'Test IDL implementation of performance-timeline API'
+    );
+  };
 
-    idl_array.add_objects({
-      Performance: ['performance'],
-      // PerformanceEntry implicitly tested in user-timing.
-      ResizeObserverEntry: ['observerEntry'],
-    });
-  },
-  'Test IDL implementation of performance-timeline API'
-);
+  return Promise.race([observe, timeout]).then(
+    execute_test,
+    reason => {
+      execute_test();
+      return Promise.reject(reason);
+    }
+  );
+})

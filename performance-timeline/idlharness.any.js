4c2881a1f102e3e7c79ebea7dd0a90ced805061b
Luke Bjerring <lukebjerring@google.com>
2018-07-17 10:45:07 -0400

    Test PerformanceEntry via PerformanceMark

--- performance-timeline/idlharness.any.js
+++ performance-timeline/idlharness.any.js
@@ -6,11 +6,16 @@
 
 'use strict';
 
+function cast(i, t) {
+  return Object.assign(Object.create(t.prototype), i);
+}
+
 promise_test(async t => {
   const observe = new Promise((resolve, reject) => {
     try {
       self.observer = new PerformanceObserver((entries, observer) => {
         self.entryList = entries;
+        self.mark = entries.getEntries()[0];
         resolve();
       });
       observer.observe({ entryTypes: ['mark'] });
@@ -22,17 +27,19 @@ promise_test(async t => {
   const timeout = new Promise((_, reject) => {
     t.step_timeout(() => reject('Timed out waiting for observation'), 3000);
   });
+  const user = await fetch('/interfaces/user-timing.idl').then(r => r.text());
   const execute_test = () => {
     idl_test(
       ['performance-timeline'],
       ['hr-time', 'dom'],
       idl_array => {
+        idl_array.add_idls(user, {only: ['PerformanceMark']});
         idl_array.add_objects({
           Performance: ['performance'],
+          // NOTE: PerformanceMark cascadingly tests PerformanceEntry
+          PerformanceMark: ['mark'],
           PerformanceObserver: ['observer'],
-          // NOTE: PerformanceEntry is implicitly tested in user-timing.
           PerformanceObserverEntryList: ['entryList'],
-          PerformanceObserverEntry: ['entryList[0]'],
         });
       },
       'Test IDL implementation of performance-timeline API'

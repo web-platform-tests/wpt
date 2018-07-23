659635f707d0ce5352fc957392658a5147419679
Luke Bjerring <lukebjerring@google.com>
2018-07-23 09:54:57 -0400

    Fix syntax error

--- resize-observer/idlharness.window.js
+++ resize-observer/idlharness.window.js
@@ -6,35 +6,33 @@
 
 // https://wicg.github.io/ResizeObserver/
 
-promise_test(async () => {
-  const div = document.createElement('div');
-  document.body.appendChild(div);
-  let helper = new ResizeTestHelper(
-    "ResizeObserverEntry creator",
-    [
-      {
-        setup: observer => {
-          self.observer = observer;
-          observer.observe(div);
-          div.style.width = "5px";
-        },
-        notify: entries => {
-          self.entry = entries[0];
-          assert_equals(entries[0].contentRect.width, 5, "target width");
-        }
-      }
-    ]);
-  await helper.start();
+idl_test(
+  ['ResizeObserver'],
+  ['dom', 'geometry'],
+  async idl_array => {
+    idl_array.add_objects({
+      ResizeObserver: ['observer'],
+      ResizeObserverEntry: ['entry'],
+    });
 
-  idl_test(
-    ['ResizeObserver'],
-    ['dom', 'geometry'],
-    idl_array => {
-      idl_array.add_objects({
-        ResizeObserver: ['observer'],
-        ResizeObserverEntry: ['entry'],
-      });
-    },
-    'Test IDL implementation of ResizeObserver'
-  );
-});
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    let helper = new ResizeTestHelper(
+      "ResizeObserverEntry creator",
+      [
+        {
+          setup: observer => {
+            self.observer = observer;
+            observer.observe(div);
+            div.style.width = "5px";
+          },
+          notify: entries => {
+            self.entry = entries[0];
+            assert_equals(entries[0].contentRect.width, 5, "target width");
+          }
+        }
+      ]);
+    await helper.start();
+  },
+  'Test IDL implementation of ResizeObserver'
+);

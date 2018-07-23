8a117ad29869b01682d2486dfb1c4ec29b9b33c2
Luke Bjerring <lukebjerring@google.com>
2018-07-23 09:43:27 -0400

    Update test

--- hr-time/idlharness.any.js
+++ hr-time/idlharness.any.js
@@ -1,3 +1,4 @@
+// META: global=window,worker
 // META: script=/resources/WebIDLParser.js
 // META: script=/resources/idlharness.js
 
@@ -11,10 +12,25 @@ function cast(i, t) {
 
 idl_test(
   ['hr-time'],
-  ['html', 'dom'],
-  idl_array => {
+  [], // Deps handled below.
+  async idl_array => {
+    const [html, dom] = await Promise.all(['html', 'dom'].map(
+      i => fetch(`/interfaces/${i}.idl`).then(r => r.text())));
+
+    // NOTE(lukebjerring): Manually adding untested parent interfaces until
+    // https://github.com/web-platform-tests/wpt/issues/8053 is resolved.
+    if (self.GLOBAL.isWorker()) {
+      idl_array.add_untested_idls(html, { only: ['WorkerGlobalScope'] });
+      idl_array.add_objects({ WorkerGlobalScope: ['self'] });
+    } else {
+      idl_array.add_untested_idls(html, { only: ['Window'] });
+      idl_array.add_objects({ Window: ['self'] });
+    }
     idl_array.add_objects({
       Performance: ['performance'],
     });
+
+    idl_array.add_dependency_idls(html);
+    idl_array.add_dependency_idls(dom);
   },
   'hr-time interfaces.');

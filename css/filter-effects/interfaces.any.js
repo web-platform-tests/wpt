4a3b4b10d89fe713c8558e5034f27598fcc5c398
Luke Bjerring <lukebjerring@chromium.org>
2018-07-28 09:58:07 +0200

    Clean up filter-effects test

--- css/filter-effects/interfaces.any.js
+++ css/filter-effects/interfaces.any.js
@@ -5,15 +5,11 @@
 
 // https://drafts.fxtf.org/filter-effects/
 
-promise_test(async () => {
-  const srcs = ['filter-effects', 'SVG', 'html', 'dom'];
-  const [filterEffectsIdl, svg, html, dom] = await Promise.all(
-      srcs.map(i => fetch(`/interfaces/${i}.idl`).then(r => r.text())));
-
-  const idlArray = new IdlArray();
-  idlArray.add_idls(filterEffectsIdl);
-  idlArray.add_dependency_idls(svg);
-  idlArray.add_dependency_idls(html);
-  idlArray.add_dependency_idls(dom);
-  idlArray.test();
-}, 'Filter effects interfaces.');
+idl_test(
+  ['filter-effects'],
+  ['SVG', 'html', 'dom'],
+  idl_array => {
+    // TODO: objects
+  },
+  'Filter effects interfaces.'
+);

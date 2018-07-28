069aa1a61876416bba189cc4cee96b0c21e91946
Timothy Gu <timothygu99@gmail.com>
2018-07-28 09:57:29 -0700

    No doctype + limited-quirks test

--- html/webappapis/dynamic-markup-insertion/opening-the-input-stream/quirks.window.js
+++ html/webappapis/dynamic-markup-insertion/opening-the-input-stream/quirks.window.js
@@ -1,5 +1,16 @@
-test(() => {
+test(t => {
   const frame = document.body.appendChild(document.createElement("iframe"));
+  t.add_cleanup(() => frame.contentDocument.close());
+  assert_equals(frame.contentDocument.compatMode, "BackCompat");
+  frame.contentDocument.open();
+  assert_equals(frame.contentDocument.compatMode, "CSS1Compat");
+  frame.contentDocument.close();
+  assert_equals(frame.contentDocument.compatMode, "BackCompat");
+}, "document.open() and quirks mode (iframe, no doctype)");
+
+test(t => {
+  const frame = document.body.appendChild(document.createElement("iframe"));
+  t.add_cleanup(() => frame.contentDocument.close());
   assert_equals(frame.contentDocument.compatMode, "BackCompat");
   frame.contentDocument.open();
   assert_equals(frame.contentDocument.compatMode, "CSS1Compat");
@@ -11,11 +22,11 @@ test(() => {
   assert_equals(frame.contentDocument.compatMode, "BackCompat");
   frame.contentDocument.close();
   assert_equals(frame.contentDocument.compatMode, "BackCompat");
-  frame.remove();
 }, "document.open() and quirks mode (iframe, old doctype)");
 
-test(() => {
+test(t => {
   const frame = document.body.appendChild(document.createElement("iframe"));
+  t.add_cleanup(() => frame.contentDocument.close());
   assert_equals(frame.contentDocument.compatMode, "BackCompat");
   frame.contentDocument.open();
   assert_equals(frame.contentDocument.compatMode, "CSS1Compat");
@@ -25,5 +36,34 @@ test(() => {
   assert_equals(frame.contentDocument.compatMode, "CSS1Compat");
   frame.contentDocument.close();
   assert_equals(frame.contentDocument.compatMode, "CSS1Compat");
-  frame.remove();
 }, "document.open() and quirks mode (iframe, new doctype)");
+
+// Test derived from quirks/blocks-ignore-line-height.html.
+test(t => {
+  const frame = document.body.appendChild(document.createElement("iframe"));
+  t.add_cleanup(() => frame.contentDocument.close());
+  assert_equals(frame.contentDocument.compatMode, "BackCompat");
+  frame.contentDocument.open();
+  assert_equals(frame.contentDocument.compatMode, "CSS1Compat");
+
+  // Create the DOM tree manually rather than going through document.write() to
+  // bypass the parser, which resets the document mode.
+  const html = frame.contentDocument.appendChild(frame.contentDocument.createElement("html"));
+  const body = html.appendChild(frame.contentDocument.createElement("body"));
+  assert_equals(frame.contentDocument.body, body);
+  body.innerHTML = `
+    <style>#ref { display:block }</style>
+    <div id=test><font size=1>x</font></div>
+    <font id=ref size=1>x</font>
+    <div id=s_ref>x</div>
+  `;
+  assert_equals(frame.contentDocument.compatMode, "CSS1Compat");
+
+  const idTest = frame.contentDocument.getElementById("test");
+  const idRef = frame.contentDocument.getElementById("ref");
+  const idSRef = frame.contentDocument.getElementById("s_ref");
+  assert_equals(frame.contentWindow.getComputedStyle(idTest).height,
+                frame.contentWindow.getComputedStyle(idSRef).height);
+  assert_not_equals(frame.contentWindow.getComputedStyle(idTest).height,
+                    frame.contentWindow.getComputedStyle(idRef).height);
+}, "document.open() and quirks mode (iframe, not limited-quirks)");

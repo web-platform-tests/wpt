76d1afcb887b5911e0151ff0be991a267d70d0a1
James Graham <james@hoppipolla.co.uk>
2018-07-30 15:25:43 +0100

    Create a dictionary of {item_type: ManifestItemClass}
    Sometimes we need to know all the types of test that exist. Typically
    this has either been done by hardcoding a list or using some custom
    approach to finding all the subclasses of the original. Instead it
    makes sense to have the classes register themselves on creation so
    that we can simply look in the registry for the types.
    
    To do this we use a cusom metaclass that first constucts the class
    object and then adds it to the registry.

--- tools/wptrunner/wptrunner/metadata.py
+++ tools/wptrunner/wptrunner/metadata.py
@@ -329,10 +329,7 @@ def create_test_tree(metadata_path, test_manifest, property_order=None,
     """
     id_test_map = {}
     exclude_types = frozenset(["stub", "helper", "manual", "support", "conformancechecker"])
-    all_types = [item.item_type for item in manifestitem.__dict__.itervalues()
-                 if type(item) == type and
-                 issubclass(item, manifestitem.ManifestItem) and
-                 item.item_type is not None]
+    all_types = manifestitem.item_types.keys()
     include_types = set(all_types) - exclude_types
     for _, test_path, tests in test_manifest.itertypes(*include_types):
         expected_data = load_or_create_expected(test_manifest, metadata_path, test_path, tests,

5f22c3b5392bcb48247407e81da7afa314619d73
Timothy Gu <timothygu99@gmail.com>
2018-07-31 14:18:09 -0400

    HTML event handlers: Use synchronous test() where able

--- html/webappapis/scripting/events/event-handler-spec-example.window.js
+++ html/webappapis/scripting/events/event-handler-spec-example.window.js
@@ -1,60 +1,51 @@
 var objects = [{}, function() {}, new Number(42), new String()];
 var primitives = [42, null, undefined, ""];
 objects.forEach(function(object) {
-  var t = async_test("Event handler listeners should be registered when they " +
-                     "are first set to an object value (" +
-                     format_value(object) + ").");
-  t.step(function() {
+  test(function() {
     var i = 0;
-    var uncalled = "t.step(function() { assert_unreached('First event handler.') })"
+    var uncalled = "assert_unreached('First event handler.');"
     var button = document.createElement('button');
     button.onclick = object; // event handler listener is registered here
     assert_equals(button.onclick, object);
-    button.addEventListener('click', t.step_func(function () { assert_equals(++i, 2) }), false);
+    button.addEventListener('click', function () { assert_equals(++i, 2) }, false);
     button.setAttribute('onclick', uncalled);
-    button.addEventListener('click', t.step_func(function () { assert_equals(++i, 3) }), false);
-    button.onclick = t.step_func(function () { assert_equals(++i, 1); });
-    button.addEventListener('click', t.step_func(function () { assert_equals(++i, 4) }), false);
+    button.addEventListener('click', function () { assert_equals(++i, 3) }, false);
+    button.onclick = function () { assert_equals(++i, 1); };
+    button.addEventListener('click', function () { assert_equals(++i, 4) }, false);
     button.click()
     assert_equals(button.getAttribute("onclick"), uncalled)
     assert_equals(i, 4);
-    t.done()
-  });
+  }, "Event handler listeners should be registered when they are first set to an object value " +
+     "(" + format_value(object) + ").");
 });
 primitives.forEach(function(primitive) {
-  var t = async_test("Event handler listeners should be registered when they " +
-                     "are first set to an object value (" +
-                     format_value(primitive) + ").");
-  t.step(function() {
+  test(function() {
     var i = 0;
-    var uncalled = "t.step(function() { assert_unreached('First event handler.') })"
+    var uncalled = "assert_unreached('First event handler.');"
     var button = document.createElement('button');
     button.onclick = primitive;
     assert_equals(button.onclick, null);
-    button.addEventListener('click', t.step_func(function () { assert_equals(++i, 1) }), false);
+    button.addEventListener('click', function () { assert_equals(++i, 1) }, false);
     button.setAttribute('onclick', uncalled); // event handler listener is registered here
-    button.addEventListener('click', t.step_func(function () { assert_equals(++i, 3) }), false);
-    button.onclick = t.step_func(function () { assert_equals(++i, 2); });
-    button.addEventListener('click', t.step_func(function () { assert_equals(++i, 4) }), false);
+    button.addEventListener('click', function () { assert_equals(++i, 3) }, false);
+    button.onclick = function () { assert_equals(++i, 2); };
+    button.addEventListener('click', function () { assert_equals(++i, 4) }, false);
     button.click()
     assert_equals(button.getAttribute("onclick"), uncalled)
     assert_equals(i, 4);
-    t.done()
-  });
+  }, "Event handler listeners should be registered when they are first set to an object value " +
+     "(" + format_value(primitive) + ").");
 });
-var t = async_test("Event handler listeners should be registered when they " +
-                   "are first set to an object value.");
-t.step(function() {
+test(function() {
   var i = 0;
-  var uncalled = "t.step(function() { assert_unreached('First event handler.') })"
+  var uncalled = "assert_unreached('First event handler.');"
   var button = document.createElement('button');
-  button.addEventListener('click', t.step_func(function () { assert_equals(++i, 1) }), false);
+  button.addEventListener('click', function () { assert_equals(++i, 1) }, false);
   button.setAttribute('onclick', uncalled); // event handler listener is registered here
-  button.addEventListener('click', t.step_func(function () { assert_equals(++i, 3) }), false);
-  button.onclick = t.step_func(function () { assert_equals(++i, 2); });
-  button.addEventListener('click', t.step_func(function () { assert_equals(++i, 4) }), false);
+  button.addEventListener('click', function () { assert_equals(++i, 3) }, false);
+  button.onclick = function () { assert_equals(++i, 2); };
+  button.addEventListener('click', function () { assert_equals(++i, 4) }, false);
   button.click()
   assert_equals(button.getAttribute("onclick"), uncalled)
   assert_equals(i, 4);
-  t.done()
-});
+}, "Event handler listeners should be registered when they are first set to an object value.");

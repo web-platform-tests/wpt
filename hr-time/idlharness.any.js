0229a365a344d585fca7b7c5b02fbdcf86acf7e0
Luke Bjerring <lukebjerring@chromium.org>
2018-08-02 15:08:45 +0200

    Fix test

--- hr-time/idlharness.any.js
+++ hr-time/idlharness.any.js
@@ -12,25 +12,15 @@ function cast(i, t) {
 
 idl_test(
   ['hr-time'],
-  [], // Deps handled below.
+  ['html', 'dom'],
   async idl_array => {
-    const [html, dom] = await Promise.all(['html', 'dom'].map(
-      i => fetch(`/interfaces/${i}.idl`).then(r => r.text())));
-
-    // NOTE(lukebjerring): Manually adding untested parent interfaces until
-    // https://github.com/web-platform-tests/wpt/issues/8053 is resolved.
     if (self.GLOBAL.isWorker()) {
-      idl_array.add_untested_idls(html, { only: ['WorkerGlobalScope'] });
       idl_array.add_objects({ WorkerGlobalScope: ['self'] });
     } else {
-      idl_array.add_untested_idls(html, { only: ['Window'] });
       idl_array.add_objects({ Window: ['self'] });
     }
     idl_array.add_objects({
       Performance: ['performance'],
     });
-
-    idl_array.add_dependency_idls(html);
-    idl_array.add_dependency_idls(dom);
   },
   'hr-time interfaces.');

5014413012ad577eeca9d918217e754add881a5a
Jim Evans <james.h.evans.jr@gmail.com>
2018-08-07 08:46:41 -0700

    Moving WebDriver test prompt-specific dialog creation code into the script
    Acting on comments from code review.

--- webdriver/tests/support/fixtures.py
+++ webdriver/tests/support/fixtures.py
@@ -262,20 +262,22 @@ def create_dialog(session):
 
         assert isinstance(text, basestring), "`text` parameter must be a string"
 
-        dialog_params = "text"
-        if dialog_type == "prompt":
-            # For prompt() dialogs, add a value for the 'default' argument
-            dialog_params += ", ''"
-
         # Script completes itself when the user prompt has been opened.
+        # For prompt() dialogs, add a value for the 'default' argument,
+        # as some user agents (IE, for example) do not produce consistent
+        # values for the default.
         session.execute_async_script("""
             let dialog_type = arguments[0];
             let text = arguments[1];
 
-            setTimeout(function() {{
-              window.dialog_return_value = window[dialog_type]({});
-            }}, 0);
-            """.format(dialog_params), args=(dialog_type, text))
+            setTimeout(function() {
+              if (dialog_type == 'prompt') {
+                window.dialog_return_value = window[dialog_type](text, '');
+              } else {
+                window.dialog_return_value = window[dialog_type](text);
+              }
+            }, 0);
+            """, args=(dialog_type, text))
 
         wait(session,
              lambda s: s.alert.text == text,

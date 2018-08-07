98ce16f20df3d1d252f857026d80f96cbee53c14
David Heiberg <dheiberg@mozilla.com>
2018-08-07 17:58:53 +0100

    [WIP] Changed the way Python handlers work for H2, it now returns a FunctionHandler that has methods for specific frame handling. No longer requires a thread to parse the request. Refactored `finish_handling` to be cleaner split between h1 and h2.

--- tools/wptserve/wptserve/utils.py
+++ tools/wptserve/wptserve/utils.py
@@ -113,5 +113,5 @@ def http2_compatible():
     # Currently, the HTTP/2.0 server is only working in python 2.7.10+ and OpenSSL 1.0.2+
     import ssl
     ssl_v = ssl.OPENSSL_VERSION_INFO
-    return (sys.version_info[0] == 2 and sys.version_info[1] == 7 and sys.version_info[2] >= 10) \
-        and (ssl_v[0] == 1 and (ssl_v[1] == 1 or (ssl_v[1] == 0 and ssl_v[2] >= 2)))
+    return ((sys.version_info[0] == 2 and sys.version_info[1] == 7 and sys.version_info[2] >= 10) and
+            (ssl_v[0] == 1 and (ssl_v[1] == 1 or (ssl_v[1] == 0 and ssl_v[2] >= 2))))

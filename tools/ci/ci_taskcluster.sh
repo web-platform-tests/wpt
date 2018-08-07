541acf33e94a7fee8dc48cbcca4f067ed866a8d7
Mike Pennisi <mike@mikepennisi.com>
2018-08-07 17:33:19 -0400

    [infra] Validate pull requests in TaskCluster

--- tools/ci/ci_taskcluster.sh
+++ tools/ci/ci_taskcluster.sh
@@ -2,9 +2,43 @@
 
 ./wpt manifest-download
 
-if [ $1 == "firefox" ]; then
-    ./wpt run firefox --log-tbpl=../artifacts/log_tbpl.log --log-tbpl-level=info --log-wptreport=../artifacts/wpt_report.json --log-mach=- --this-chunk=$3 --total-chunks=$4 --test-type=$2 -y --install-browser --no-pause --no-restart-on-unexpected --reftest-internal --install-fonts --no-fail-on-unexpected
-elif [ $1 == "chrome" ]; then
-    ./wpt run chrome --log-tbpl=../artifacts/log_tbpl.log --log-tbpl-level=info --log-wptreport=../artifacts/wpt_report.json --log-mach=- --this-chunk=$3 --total-chunks=$4 --test-type=$2 -y  --no-pause --no-restart-on-unexpected --install-fonts --no-fail-on-unexpected
+# The first script argument that is not prefixed with a dash (`-`) is assumed
+# to be the name of the browser under test. This restricts the syntax available
+# to consumers: value-accepting options must be specified using the equals sign
+# (`=`).
+for argument in $@; do
+  if [ ${argument:0:1} = '-' ]; then
+    continue
+  fi
+
+  browser_name=$argument
+
+  break
+done
+
+browser_specific_args=''
+
+if [ $browser_name = 'firefox' ]; then
+  browser_specific_args='--install-browser --reftest-internal'
 fi
-gzip ../artifacts/wpt_report.json
+
+./wpt run \
+  --log-tbpl=../artifacts/log_tbpl.log \
+  --log-tbpl-level=info \
+  --log-wptreport=../artifacts/wpt_report.json \
+  --log-mach=- \
+  -y \
+  --no-pause \
+  --no-restart-on-unexpected \
+  --install-fonts \
+  --no-fail-on-unexpected \
+  $browser_specific_args \
+  $@
+
+run_status=$?
+
+if [ -f ../artifacts/wpt_report.json ]; then
+  gzip ../artifacts/wpt_report.json
+fi
+
+exit $run_status

1f21af729657eb653c397f19a1531119a760b078
Geoffrey Sneddon <me@gsnedders.com>
2018-08-09 19:10:52 +0100

    fixup! [FIX COMMIT MSG] Implement WebDriver executor for wptrunner

--- tools/wptrunner/wptrunner/browsers/chrome_webdriver.py
+++ tools/wptrunner/wptrunner/browsers/chrome_webdriver.py
@@ -1,11 +1,12 @@
+from copy import deepcopy
+
 from . import chrome
 
 from ..executors import executor_kwargs as base_executor_kwargs
-
 from ..executors.executorwebdriver import (WebDriverTestharnessExecutor,  # noqa: F401
                                            WebDriverRefTestExecutor)  # noqa: F401
 
-__wptrunner__ = chrome.__wptrunner__
+__wptrunner__ = deepcopy(chrome.__wptrunner__)
 __wptrunner__["product"] = "chrome_webdriver"
 __wptrunner__["executor"]["testharness"] = "WebDriverTestharnessExecutor"
 __wptrunner__["executor"]["reftest"] = "WebDriverRefTestExecutor"

1e3736ca27dbe6d3953872796a9dafcf60466280
Adam Rice <ricea@chromium.org>
2018-08-13 19:41:04 +0900

    Tests for TextEncoderStream and TextDecoderStream
    The standard change that adds these classes is
    https://github.com/whatwg/encoding/pull/149.

---
+++ encoding/streams/decode-bad-chunks.any.js
@@ -0,0 +1,73 @@
+// META: global=worker
+
+'use strict';
+
+const badChunks = [
+  {
+    name: 'undefined',
+    value: undefined
+  },
+  {
+    name: 'null',
+    value: null
+  },
+  {
+    name: 'numeric',
+    value: 3.14
+  },
+  {
+    name: 'object, not BufferSource',
+    value: {}
+  },
+  {
+    name: 'array',
+    value: [65]
+  },
+  {
+    name: 'detached ArrayBufferView',
+    value: (() => {
+      const u8 = new Uint8Array([65]);
+      const ab = u8.buffer;
+      const mc = new MessageChannel();
+      mc.port1.postMessage(ab, [ab]);
+      return u8;
+    })()
+  },
+  {
+    name: 'detached ArrayBuffer',
+    value: (() => {
+      const u8 = new Uint8Array([65]);
+      const ab = u8.buffer;
+      const mc = new MessageChannel();
+      mc.port1.postMessage(ab, [ab]);
+      return ab;
+    })()
+  }
+];
+
+let badShared = [];
+if (typeof SharedArrayBuffer !== 'undefined') {
+  badShared = [
+    {
+      name: 'SharedArrayBuffer',
+      value: new SharedArrayBuffer()
+    },
+    {
+      name: 'shared Uint8Array',
+      value: new Uint8Array(new SharedArrayBuffer())
+    }
+  ]
+}
+
+for (const chunk of badChunks.concat(badShared)) {
+  promise_test(async t => {
+    const tds = new TextDecoderStream();
+    const reader = tds.readable.getReader();
+    const writer = tds.writable.getWriter();
+    const writePromise = writer.write(chunk.value);
+    const readPromise = reader.read();
+    await promise_rejects(t, new TypeError(), writePromise, 'write should reject');
+    await promise_rejects(t, new TypeError(), readPromise, 'read should reject');
+    const rejection = await writePromise.catch(_ => _);
+  }, `chunk of type ${chunk.name} should error the stream`);
+}

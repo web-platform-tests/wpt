2e483da1fe3f6726ae2fb95cff9785b8154d9a1d
Timothy Gu <timothygu99@gmail.com>
2018-08-15 11:54:27 -0400

    Address review comments

--- html/webappapis/dynamic-markup-insertion/opening-the-input-stream/no-new-global.window.js
+++ html/webappapis/dynamic-markup-insertion/opening-the-input-stream/no-new-global.window.js
@@ -1,32 +1,35 @@
+// In an earlier version of the HTML Standard, document open steps created a
+// new JavaScript realm and migrated the existing objects to use the new realm.
+// Test that this no longer happens.
+
 async_test(t => {
   const frame = document.body.appendChild(document.createElement("iframe"));
+  // Ensure a load event gets dispatched to unblock testharness
   t.add_cleanup(() => frame.remove());
   frame.src = "resources/global-variables-frame.html";
   frame.onload = t.step_func_done(() => {
     assert_equals(frame.contentWindow.hey, "You", "precondition");
     frame.contentDocument.open();
     assert_equals(frame.contentWindow.hey, "You", "actual check");
-    // Ensure a load event gets dispatched to unblock testharness
-    frame.contentDocument.close();
   });
 }, "Obtaining a variable from a global whose document had open() invoked");
 
 function testIdentity(desc, frameToObject, frameToConstructor) {
   async_test(t => {
     const frame = document.body.appendChild(document.createElement("iframe"));
+    // Ensure a load event gets dispatched to unblock testharness
     t.add_cleanup(() => frame.remove());
     frame.src = "/common/blank.html";
     frame.onload = t.step_func_done(() => {
       const obj = frameToObject(frame);
       frame.contentDocument.open();
       assert_equals(frameToObject(frame), obj);
-      // Ensure a load event gets dispatched to unblock testharness
-      frame.contentDocument.close();
     });
   }, `${desc} maintains object identity through open()`);
 
   async_test(t => {
     const frame = document.body.appendChild(document.createElement("iframe"));
+    // Ensure a load event gets dispatched to unblock testharness
     t.add_cleanup(() => frame.remove());
     frame.src = "/common/blank.html";
     frame.onload = t.step_func_done(() => {
@@ -40,12 +43,11 @@ function testIdentity(desc, frameToObject, frameToConstructor) {
       assert_true(frameToObject(frame) instanceof origCtor);
       assert_equals(Object.getPrototypeOf(frameToObject(frame)), origProto);
       assert_equals(frameToConstructor(frame), origCtor);
-      // Ensure a load event gets dispatched to unblock testharness
-      frame.contentDocument.close();
     });
   }, `${desc} maintains its prototype and properties through open()`);
 }
 
 testIdentity("Document", frame => frame.contentDocument, frame => frame.contentWindow.Document);
 testIdentity("WindowProxy", frame => frame.contentWindow, frame => frame.contentWindow.Window);
+testIdentity("Location", frame => frame.contentWindow.location, frame => frame.contentWindow.Location);
 testIdentity("Navigator", frame => frame.contentWindow.navigator, frame => frame.contentWindow.Navigator);

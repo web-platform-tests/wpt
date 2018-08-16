2cc0d2388066a51e28533a36f11cdb729825d5ea
James Graham <james@hoppipolla.co.uk>
2018-08-16 17:25:51 +0100

    Allow the gitignore filter to work on name components only.
    We end up with a lot of rules like (?:.*)/.*\.ext which are basically
    trying to find the last component in a path and match against
    that. These are rather slow to run so the easiest thing tdo is just
    pass in the last component of the path when we know that's the only
    thing the rule can match.
    
    The changes to surrounding code to use this API will be made in future
    commits.

--- tools/manifest/manifest.py
+++ tools/manifest/manifest.py
@@ -43,12 +43,13 @@ item_classes = {"testharness": TestharnessTest,
 
 
 class TypeData(object):
-    def __init__(self, manifest, type_cls):
+    def __init__(self, manifest, type_cls, meta_filters):
         self.manifest = manifest
         self.type_cls = type_cls
-        self.data = {}
-        self.json_data = None
+        self.json_data = {}
         self.tests_root = None
+        self.data = {}
+        self.meta_filters = meta_filters or []
 
     def __getitem__(self, key):
         if key not in self.data:
@@ -96,14 +97,13 @@ class TypeData(object):
 
     def iteritems(self):
         self.load_all()
-        for path, tests in iteritems(self.data):
-            yield path, tests
+        return iteritems(self.data)
 
     def load(self, key):
         if self.json_data is not None:
             data = set()
             path = from_os_path(key)
-            for test in self.json_data.get(path, []):
+            for test in iterfilter(self.meta_filters, self.json_data.get(path, [])):
                 manifest_item = self.type_cls.from_json(self.manifest,
                                                         self.tests_root,
                                                         path,
@@ -120,7 +120,7 @@ class TypeData(object):
                 if key in self.data:
                     continue
                 data = set()
-                for test in self.json_data.get(path, []):
+                for test in iterfilter(self.meta_filters, self.json_data.get(path, [])):
                     manifest_item = self.type_cls.from_json(self.manifest,
                                                             self.tests_root,
                                                             path,
@@ -138,7 +138,6 @@ class TypeData(object):
         if self.json_data:
             rv |= set(to_os_path(item) for item in iterkeys(self.json_data))
         return rv
-
 
 class ManifestData(dict):
     def __init__(self, manifest, meta_filters=None):
@@ -161,10 +160,10 @@ class ManifestData(dict):
 
 
 class Manifest(object):
-    def __init__(self, url_base="/"):
+    def __init__(self, url_base="/", meta_filters=None):
         assert url_base is not None
         self._path_hash = {}
-        self._data = ManifestData(self)
+        self._data = ManifestData(self, meta_filters)
         self._reftest_nodes_by_url = None
         self.url_base = url_base
 
@@ -175,7 +174,8 @@ class Manifest(object):
         if not types:
             types = sorted(self._data.keys())
         for item_type in types:
-            for path, tests in sorted(self._data[item_type]):
+            for path in sorted(self._data[item_type]):
+                tests = self._data[item_type][path]
                 yield item_type, path, tests
 
     def iterpath(self, path):
@@ -330,13 +330,11 @@ class Manifest(object):
         if version != CURRENT_VERSION:
             raise ManifestVersionMismatch
 
-        self = cls(url_base=obj.get("url_base", "/"))
+        self = cls(url_base=obj.get("url_base", "/"), meta_filters=meta_filters)
         if not hasattr(obj, "items") and hasattr(obj, "paths"):
             raise ManifestError
 
         self._path_hash = {to_os_path(k): v for k, v in iteritems(obj["paths"])}
-
-        meta_filters = meta_filters or []
 
         for test_type, type_paths in iteritems(obj["items"]):
             if test_type not in item_classes:
@@ -361,7 +359,10 @@ def load(tests_root, manifest, types=None, meta_filters=None):
             logger.debug("Creating new manifest at %s" % manifest)
         try:
             with open(manifest) as f:
-                rv = Manifest.from_json(tests_root, json.load(f), types=types, meta_filters=meta_filters)
+                rv = Manifest.from_json(tests_root,
+                                        json.load(f),
+                                        types=types,
+                                        meta_filters=meta_filters)
         except IOError:
             return None
         except ValueError:
@@ -371,7 +372,10 @@ def load(tests_root, manifest, types=None, meta_filters=None):
             return None
         return rv
 
-    return Manifest.from_json(tests_root, json.load(manifest), types=types, meta_filters=meta_filters)
+    return Manifest.from_json(tests_root,
+                              json.load(manifest),
+                              types=types,
+                              meta_filters=meta_filters)
 
 
 def write(manifest, manifest_path):

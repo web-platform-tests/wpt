bc8cc571c95e581d53f86d82eed08e3da126dca0
Timothy Gu <timothygu99@gmail.com>
2018-08-23 13:01:38 -0400

    xml vs active parser doesn't need promise.resolve

--- html/webappapis/dynamic-markup-insertion/opening-the-input-stream/bailout-exception-vs-return-xml.window.js
+++ html/webappapis/dynamic-markup-insertion/opening-the-input-stream/bailout-exception-vs-return-xml.window.js
@@ -1,16 +1,10 @@
 async_test(t => {
   const iframe = document.body.appendChild(document.createElement("iframe"));
   t.add_cleanup(() => { iframe.remove(); });
-  self.testSynchronousScript = t.step_func(() => {
-    // Here, the entry settings object is still the iframe's. Delay it in such
-    // a way that makes the entry settings object the top-level page's, but
-    // without delaying too much that the parser becomes inactive. A microtask
-    // is perfect as it's executed in "clean up after running script".
-    Promise.resolve().then(t.step_func_done(() => {
-      assert_throws("InvalidStateError", () => {
-        iframe.contentDocument.open();
-      }, "opening an XML document should throw");
-    }));
+  self.testSynchronousScript = t.step_func_done(() => {
+    assert_throws("InvalidStateError", () => {
+      iframe.contentDocument.open();
+    }, "opening an XML document should throw");
   });
   iframe.src = "resources/bailout-order-xml-with-synchronous-script-frame.xhtml";
 }, "document.open should throw an InvalidStateError with XML document even when there is an active parser executing script");

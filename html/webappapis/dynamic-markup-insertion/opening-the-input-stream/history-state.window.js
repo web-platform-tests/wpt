aab575e9090b4196608770a016c16f625957f192
Timothy Gu <timothygu99@gmail.com>
2018-08-23 11:17:52 -0400

    HTML: add test for history.state after document.open()

---
+++ html/webappapis/dynamic-markup-insertion/opening-the-input-stream/history-state.window.js
@@ -0,0 +1,25 @@
+async_test(t => {
+  const iframe = document.body.appendChild(document.createElement("iframe"));
+  t.add_cleanup(() => iframe.remove());
+  iframe.src = "/common/blank.html";
+  iframe.onload = t.step_func_done(() => {
+    assert_equals(iframe.contentWindow.history.state, null);
+    iframe.contentWindow.history.replaceState("state", "");
+    assert_equals(iframe.contentWindow.history.state, "state");
+    iframe.contentDocument.open();
+    assert_equals(iframe.contentWindow.history.state, "state");
+  });
+}, "history.state is kept by document.open()");
+
+async_test(t => {
+  const iframe = document.body.appendChild(document.createElement("iframe"));
+  t.add_cleanup(() => iframe.remove());
+  iframe.src = "/common/blank.html";
+  iframe.onload = t.step_func_done(() => {
+    assert_equals(iframe.contentWindow.history.state, null);
+    iframe.contentWindow.history.replaceState("state", "");
+    assert_equals(iframe.contentWindow.history.state, "state");
+    iframe.contentDocument.open("", "replace");
+    assert_equals(iframe.contentWindow.history.state, "state");
+  });
+}, "history.state is kept by document.open() (with legacy replace parameter set)");

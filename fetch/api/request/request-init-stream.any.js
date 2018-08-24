180d396f222f885dbefee262e9a077754da7f623
Anne van Kesteren <annevk@annevk.nl>
2018-08-24 08:59:35 +0200

    async/await more

--- fetch/api/request/request-init-stream.any.js
+++ fetch/api/request/request-init-stream.any.js
@@ -2,21 +2,21 @@
 
 "use strict";
 
-function assert_request(input, init) {
+async function assert_request(input, init) {
   assert_throws(new TypeError(), () => new Request(input, init), "new Request()");
-  assert_throws(new TypeError(), () => fetch(input, init), "fetch()");
+  assert_throws(new TypeError(), async () => await fetch(input, init), "fetch()");
 }
 
-test(() => {
+promise_test(async () => {
   const stream = new ReadableStream();
   stream.getReader();
-  assert_request("...", { method:"POST", body: stream });
+  await assert_request("...", { method:"POST", body: stream });
 }, "Constructing a Request with a stream on which getReader() is called");
 
-test(() => {
+promise_test(async () => {
   const stream = new ReadableStream();
   stream.getReader().read();
-  assert_request("...", { method:"POST", body: stream });
+  await assert_request("...", { method:"POST", body: stream });
 }, "Constructing a Request with a stream on which read() is called");
 
 promise_test(async () => {
@@ -24,20 +24,20 @@ promise_test(async () => {
         reader = stream.getReader();
   await reader.read();
   reader.releaseLock();
-  assert_request("...", { method:"POST", body: stream });
+  await assert_request("...", { method:"POST", body: stream });
 }, "Constructing a Request with a stream on which read() and releaseLock() are called");
 
-test(() => {
+promise_test(async () => {
   const request = new Request("...", { method: "POST", body: "..." });
   request.body.getReader();
-  assert_request(request);
+  await assert_request(request);
   assert_class_string(new Request(request, { body: "..." }), "Request");
 }, "Constructing a Request with a Request on which body.getReader() is called");
 
-test(() => {
+promise_test(async () => {
   const request = new Request("...", { method: "POST", body: "..." });
   request.body.getReader().read();
-  assert_request(request);
+  await assert_request(request);
   assert_class_string(new Request(request, { body: "..." }), "Request");
 }, "Constructing a Request with a Request on which body.getReader().read() is called");
 
@@ -46,6 +46,6 @@ promise_test(async () => {
         reader = request.body.getReader();
   await reader.read();
   reader.releaseLock();
-  assert_request(request);
+  await assert_request(request);
   assert_class_string(new Request(request, { body: "..." }), "Request");
 }, "Constructing a Request with a Request on which read() and releaseLock() are called");

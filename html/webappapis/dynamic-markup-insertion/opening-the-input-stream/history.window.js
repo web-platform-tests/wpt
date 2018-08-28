45152ea20a0f4b88a3abe19944ef6170b6f66c91
Timothy Gu <timothygu99@gmail.com>
2018-08-28 12:13:56 -0400

    Add some comments to address review comments

--- html/webappapis/dynamic-markup-insertion/opening-the-input-stream/history.window.js
+++ html/webappapis/dynamic-markup-insertion/opening-the-input-stream/history.window.js
@@ -1,6 +1,10 @@
 // Historically, document.open() created an entry in the session history so
 // that the original page could be seen by going back. Test that this behavior
 // no longer occurs.
+//
+// This test uses window.open() for variety, as most other tests in this
+// directory use document.open(). An <iframe> would probably work also. We can
+// always add an <iframe>-based test later if it is deemed necessary.
 
 const t = async_test("document.open should not add an entry to the session history");
 

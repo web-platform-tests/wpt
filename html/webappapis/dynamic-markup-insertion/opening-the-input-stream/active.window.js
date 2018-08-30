11cd703981febf0bd51ac5b470e61b2a80a939d7
Timothy Gu <timothygu99@gmail.com>
2018-08-30 19:17:53 -0400

    Add implicit document.open() tests

--- html/webappapis/dynamic-markup-insertion/opening-the-input-stream/active.window.js
+++ html/webappapis/dynamic-markup-insertion/opening-the-input-stream/active.window.js
@@ -1,11 +1,27 @@
+function assertOpenIsEffective(doc, initialNodeCount) {
+  assert_equals(doc.childNodes.length, initialNodeCount);
+
+  // Test direct document.open() call.
+  assert_equals(doc.open(), doc);
+  assert_equals(doc.childNodes.length, 0);
+  doc.write("<!DOCTYPE html>");
+  assert_equals(doc.childNodes.length, 1);
+  doc.close();
+  assert_equals(doc.childNodes.length, 2);
+
+  // Test implicit document.open() call through write().
+  doc.write();
+  assert_equals(doc.childNodes.length, 0);
+  doc.write("<!DOCTYPE html>");
+  assert_equals(doc.childNodes.length, 1);
+  doc.close();
+  assert_equals(doc.childNodes.length, 2);
+}
+
 test(t => {
   const frame = document.body.appendChild(document.createElement("iframe"));
   t.add_cleanup(() => frame.remove());
-  assert_equals(frame.contentDocument.childNodes.length, 1);
-  assert_equals(frame.contentDocument.open(), frame.contentDocument);
-  assert_equals(frame.contentDocument.childNodes.length, 0);
-  frame.contentDocument.write("<!DOCTYPE html>");
-  assert_equals(frame.contentDocument.childNodes.length, 1);
+  assertOpenIsEffective(frame.contentDocument, 1);
 }, "document.open() removes the document's children (fully active document)");
 
 async_test(t => {
@@ -20,11 +36,7 @@ async_test(t => {
 
     frame.onload = t.step_func_done(() => {
       // Now childDoc is still active but no longer fully active.
-      assert_equals(childDoc.childNodes.length, 1);
-      assert_equals(childDoc.open(), childDoc);
-      assert_equals(childDoc.childNodes.length, 0);
-      childDoc.write("<!DOCTYPE html>");
-      assert_equals(childDoc.childNodes.length, 1);
+      assertOpenIsEffective(childDoc, 1);
     });
     frame.src = "/common/blank.html";
   });
@@ -40,11 +52,7 @@ test(t => {
   frame.remove();
 
   // Now the frame is no longer connected. Its document is no longer active.
-  assert_equals(doc.childNodes.length, 1);
-  assert_equals(doc.open(), doc);
-  assert_equals(doc.childNodes.length, 0);
-  doc.write("<!DOCTYPE html>");
-  assert_equals(doc.childNodes.length, 1);
+  assertOpenIsEffective(doc, 1);
 }, "document.open() removes the document's children (non-active document with an associated Window object; frame is removed)");
 
 async_test(t => {
@@ -60,11 +68,7 @@ async_test(t => {
       // Now even though the frame is still connected, its document is no
       // longer active.
       assert_not_equals(frame.contentDocument, doc);
-      assert_equals(doc.childNodes.length, 2);
-      assert_equals(doc.open(), doc);
-      assert_equals(doc.childNodes.length, 0);
-      doc.write("<!DOCTYPE html>");
-      assert_equals(doc.childNodes.length, 1);
+      assertOpenIsEffective(doc, 2);
     });
 
     frame.src = "/common/blank.html";
@@ -75,20 +79,12 @@ test(t => {
   const frame = document.body.appendChild(document.createElement("iframe"));
   t.add_cleanup(() => frame.remove());
   const doc = frame.contentDocument.implementation.createHTMLDocument();
-  assert_equals(doc.childNodes.length, 2);
-  assert_equals(doc.open(), doc);
-  assert_equals(doc.childNodes.length, 0);
-  doc.write("<!DOCTYPE html>");
-  assert_equals(doc.childNodes.length, 1);
+  assertOpenIsEffective(doc, 2);
 }, "document.open() does not change document's URL (non-active document without an associated Window object; createHTMLDocument)");
 
 test(t => {
   const frame = document.body.appendChild(document.createElement("iframe"));
   t.add_cleanup(() => frame.remove());
   const doc = new frame.contentWindow.DOMParser().parseFromString("", "text/html");
-  assert_equals(doc.childNodes.length, 1);
-  assert_equals(doc.open(), doc);
-  assert_equals(doc.childNodes.length, 0);
-  doc.write("<!DOCTYPE html>");
-  assert_equals(doc.childNodes.length, 1);
+  assertOpenIsEffective(doc, 1);
 }, "document.open() does not change document's URL (non-active document without an associated Window object; DOMParser)");

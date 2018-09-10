0d0afba85c2d4e02c770d8db3b63f8ca2e1294de
Eric Willigers <ericwilligers@chromium.org>
2018-09-10 22:27:51 +1000

    Test initial values of Inline Layout properties

--- css/support/inheritance-testcommon.js
+++ css/support/inheritance-testcommon.js
@@ -1,40 +1,53 @@
 'use strict';
 
-function assert_inherited(property, value, expected) {
-  if (expected === undefined) {
-    expected = value;
-  }
+function assert_initial(property, initial) {
+  test(() => {
+    if (!getComputedStyle(target)[property])
+      return;
+    target.style[property] = 'initial';
+    assert_equals(getComputedStyle(target)[property], initial);
+    target.style[property] = '';
+  }, 'Property ' + property + ' has initial value ' + initial);
+}
+
+function assert_inherited(property, initial, other) {
+  assert_initial(property, initial);
 
   test(() => {
     if (!getComputedStyle(target)[property])
       return;
-    assert_not_equals(getComputedStyle(container)[property], expected);
-    assert_not_equals(getComputedStyle(target)[property], expected);
-    container.style[property] = value;
-    assert_equals(getComputedStyle(container)[property], expected);
-    assert_equals(getComputedStyle(target)[property], expected);
+    container.style[property] = 'initial';
+    target.style[property] = 'unset';
+    assert_not_equals(getComputedStyle(container)[property], other);
+    assert_not_equals(getComputedStyle(target)[property], other);
+    container.style[property] = other;
+    assert_equals(getComputedStyle(container)[property], other);
+    assert_equals(getComputedStyle(target)[property], other);
     target.style[property] = 'initial';
-    assert_not_equals(getComputedStyle(container)[property], expected);
-    assert_not_equals(getComputedStyle(target)[property], expected);
+    assert_not_equals(getComputedStyle(container)[property], other);
+    assert_not_equals(getComputedStyle(target)[property], other);
+    target.style[property] = 'inherit';
+    assert_equals(getComputedStyle(container)[property], other);
     container.style[property] = '';
     target.style[property] = '';
   }, 'Property ' + property + ' inherits');
 }
 
-function assert_not_inherited(property, value, expected) {
-  if (expected === undefined) {
-    expected = value;
-  }
+function assert_not_inherited(property, initial, other) {
+  assert_initial(property, initial);
 
   test(() => {
     if (!getComputedStyle(target)[property])
       return;
-    assert_not_equals(getComputedStyle(target)[property], expected);
-    container.style[property] = value;
-    assert_equals(getComputedStyle(container)[property], expected);
-    assert_not_equals(getComputedStyle(target)[property], expected);
+    container.style[property] = 'initial';
+    target.style[property] = 'unset';
+    assert_not_equals(getComputedStyle(container)[property], other);
+    assert_not_equals(getComputedStyle(target)[property], other);
+    container.style[property] = other;
+    assert_equals(getComputedStyle(container)[property], other);
+    assert_not_equals(getComputedStyle(target)[property], other);
     target.style[property] = 'inherit';
-    assert_equals(getComputedStyle(target)[property], expected);
+    assert_equals(getComputedStyle(target)[property], other);
     container.style[property] = '';
     target.style[property] = '';
   }, 'Property ' + property + ' does not inherit');

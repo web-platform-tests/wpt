9a673e68c8ff4faac1038839c5d9d5623080ef1c
Asami Doi <asamidoi@google.com>
2018-09-09 18:58:39 -0700

    Worker: Implement ES modules for ServiceWorker scripts
    This CL allows to import and evaluate as a module script at
    ServiceWorker. Users can use it by adding type 'module' when
    register a service worker script.
    
    As the first step, this CL implements ES Modules for new ServiceWorker
    scripts. ES Modules for installed ServiceWorker scripts will be
    implemented in separate CLs. Also, this doesn't implement dynamic
    import yet.
    
    i.e.
    // index.html
    const registration = await navigator.serviceWorker.register(
                             "service_worker.js",
                             { type: "module" });
    
    Spec: https://w3c.github.io/ServiceWorker/#serviceworkercontainer-interface
    
    Bug: 824647
    Change-Id: I023c7ca881a90327730011699a8db879653cafdf

--- workers/modules/resources/static-import-worker.js
+++ workers/modules/resources/static-import-worker.js
@@ -1,2 +1,9 @@
 import * as module from './export-on-load-script.js';
+
+/*
+self.addEventListener('message', (e) => {
+  e.source.postMessage(module.importedModules);
+});
+*/
+
 postMessage(module.importedModules);

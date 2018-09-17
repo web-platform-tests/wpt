a69af5481cdd976c9696f97d9c6547ac87ba83d0
Ms2ger <Ms2ger@gmail.com>
2018-09-17 16:56:50 +0200

    Move wasm/idlharness.any.js to the jsapi subdirectory.
    It only tests the IDL from the jsapi specification.

--- wasm/jsapi/idlharness.any.js
+++ wasm/jsapi/idlharness.any.js
@@ -1,6 +1,6 @@
 // META: script=/resources/WebIDLParser.js
 // META: script=/resources/idlharness.js
-// META: script=resources/load_wasm.js
+// META: script=../resources/load_wasm.js
 
 'use strict';
 

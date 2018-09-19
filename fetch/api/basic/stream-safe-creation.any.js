5b973e3d6697c1daeb19c987abd5551e02636212
Adam Rice <ricea@chromium.org>
2018-09-20 08:37:58 +0900

    Make add_cleanup return a Promise
    Also make the test additionally run on shared workers and service
    workers.

--- fetch/api/basic/stream-safe-creation.any.js
+++ fetch/api/basic/stream-safe-creation.any.js
@@ -1,3 +1,5 @@
+// META: global=worker
+
 // These tests verify that stream creation is not affected by changes to
 // Object.prototype.
 
@@ -19,7 +21,10 @@ for (creationCase of Object.keys(creationCases)) {
         get() { throw Error(`Object.prototype.${accessorName} was accessed`); },
         configurable: true
       });
-      t.add_cleanup(() => delete Object.prototype[accessorName]);
+      t.add_cleanup(() => {
+        delete Object.prototype[accessorName];
+        return Promise.resolve();
+      });
       await creationCases[creationCase]();
     }, `throwing Object.prototype.${accessorName} accessor should not affect ` +
        `stream creation by '${creationCase}'`);
@@ -28,7 +33,10 @@ for (creationCase of Object.keys(creationCases)) {
       // -1 is a convenient value which is invalid, and should cause the
       // constructor to throw, for all four fields.
       Object.prototype[accessorName] = -1;
-      t.add_cleanup(() => delete Object.prototype[accessorName]);
+      t.add_cleanup(() => {
+        delete Object.prototype[accessorName];
+        return Promise.resolve();
+      });
       await creationCases[creationCase]();
     }, `Object.prototype.${accessorName} accessor returning invalid value ` +
        `should not affect stream creation by '${creationCase}'`);
@@ -36,7 +44,10 @@ for (creationCase of Object.keys(creationCases)) {
 
   promise_test(async t => {
     Object.prototype.start = controller => controller.error(new Error('start'));
-      t.add_cleanup(() => delete Object.prototype.start);
+      t.add_cleanup(() => {
+        delete Object.prototype.start;
+        return Promise.resolve();
+      });
       await creationCases[creationCase]();
     }, `Object.prototype.start function which errors the stream should not ` +
        `affect stream creation by '${creationCase}'`);

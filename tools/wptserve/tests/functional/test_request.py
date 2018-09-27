cb0d7dac41e5f84c6004e571eefbb603cf8dcb08
Robert Ma <robertma@chromium.org>
2018-09-27 16:45:30 -0400

    Add a Unicode smoke test to wptserve
    The test sends a request to wptserve with non-ASCII characters in a
    header and sets up a simple handler to return the value of that header.
    The server shouldn't crash in either Python 2 or 3, and the response
    should not be garbled. The server crashes in Python 2 (#13204).

--- tools/wptserve/tests/functional/test_request.py
+++ tools/wptserve/tests/functional/test_request.py
@@ -1,5 +1,4 @@
-import sys
-
+# -*- coding: utf-8 -*-
 import pytest
 
 wptserve = pytest.importorskip("wptserve")
@@ -114,6 +113,25 @@ class TestRequest(TestUsingServer):
         self.server.router.register(*route)
         resp = self.request("/test/some_route")
         self.assertEqual(b"some route", resp.read())
+
+    def test_non_ascii_in_headers(self):
+        @wptserve.handlers.handler
+        def handler(request, response):
+            return request.headers["foo"]
+
+        route = ("GET", "/test/test_unicode_in_headers", handler)
+        self.server.router.register(*route)
+
+        # Try some non-ASCII characters and the server shouldn't crash.
+        encoded_text = u"你好".encode("utf-8")
+        resp = self.request(route[1], headers={"foo": encoded_text})
+        self.assertEqual(encoded_text, resp.read())
+
+        # Try a different encoding from utf-8 to make sure the binary value is
+        # returned in verbatim.
+        encoded_text = u"どうも".encode("shift-jis")
+        resp = self.request(route[1], headers={"foo": encoded_text})
+        self.assertEqual(encoded_text, resp.read())
 
 
 class TestAuth(TestUsingServer):

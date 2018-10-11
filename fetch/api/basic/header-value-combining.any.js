8e78ffd39b4de3929ee6352df82185a2a3a04400
Anne van Kesteren <annevk@annevk.nl>
2018-10-11 15:37:22 +0200

    Fetch: combining header values
    Tests for both request and response combining.
    
    Needed for https://github.com/whatwg/fetch/issues/757.

--- fetch/api/basic/header-value-combining.any.js
+++ fetch/api/basic/header-value-combining.any.js
@@ -9,3 +9,8 @@ promise_test(async t => {
   const response = await fetch("../../../xhr/resources/headers-www-authenticate.asis");
   assert_equals(response.headers.get("www-authenticate"), "1, 2, 3, 4");
 }, "response.headers.get('www-authenticate')");
+
+promise_test(async t => {
+  const response = await fetch("../../../xhr/resources/headers-some-are-empty.asis");
+  assert_equals(response.headers.get("heya"), "1, 2");
+}, "response.headers.get('heya')");

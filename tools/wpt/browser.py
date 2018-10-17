web-platform-tests/wpt

commit 2ad1215fe18f7941f26bf0911b7a310bc9c0c8af

Don't try to normalise the channel if we don't have data for a product (#12877)

tools/wpt/browser.py
@@ -262,7 +262,9 @@ class Firefox(Browser):
         if dest is None:
             dest = os.pwd
 
-        dest = os.path.join(dest, "profiles", channel, version)
+        dest = os.path.join(dest, "profiles", channel)
+        if version:
+            dest = dest.join(version)
         have_cache = False
         if os.path.exists(dest):
             if channel != "nightly":
@@ -569,7 +571,9 @@ class Opera(Browser):
         except subprocess.CalledProcessError:
             logger.warn("Failed to call %s", binary)
             return None
-        return re.search(r"[0-9\.]+( [a-z]+)?$", output.strip()).group(0)
+        m = re.search(r"[0-9\.]+( [a-z]+)?$", output.strip())
+        if m:
+            return m.group(0)
 
 
 class Edge(Browser):
@@ -707,7 +711,9 @@ class Servo(Browser):
     def version(self, binary):
         """Retrieve the release version of the installed browser."""
         output = call(binary, "--version")
-        return re.search(r"[0-9\.]+( [a-z]+)?$", output.strip()).group(0)
+        m = re.search(r"[0-9\.]+( [a-z]+)?$", output.strip())
+        if m:
+            return m.group(0)
 
 
 class Sauce(Browser):

tools/wpt/run.py
@@ -436,6 +436,7 @@ product_setup = {
     "safari": Safari,
     "safari_webdriver": SafariWebDriver,
     "servo": Servo,
+    "servodriver": Servo,
     "sauce": Sauce,
     "opera": Opera,
     "webkit": WebKit,
@@ -477,10 +478,13 @@ def setup_wptrunner(venv, prompt=True, install_browser=False, **kwargs):
 
     if kwargs["channel"]:
         channel = install.get_channel(kwargs["product"], kwargs["channel"])
-        if channel != kwargs["channel"]:
-            logger.info("Interpreting channel '%s' as '%s'" % (kwargs["channel"],
-                                                               channel))
-        kwargs["browser_channel"] = channel
+        if channel is not None:
+            if channel != kwargs["channel"]:
+                logger.info("Interpreting channel '%s' as '%s'" % (kwargs["channel"],
+                                                                   channel))
+                kwargs["browser_channel"] = channel
+        else:
+            logger.info("Valid channels for %s not known; using argument unmodified" % kwargs["product"])
     del kwargs["channel"]
 
     if install_browser:

tools/wpt/install.py
@@ -48,7 +48,7 @@ def get_parser():
 def get_channel(browser, channel):
     channel = channel_by_name[channel]
     if isinstance(channel, dict):
-        channel = channel[browser]
+        channel = channel.get(browser)
     return channel
 
 

tools/wpt/tests/test_run.py
@@ -1,8 +1,42 @@
 import mock
+import tempfile
+import shutil
+import sys
 
 import pytest
 
 from tools.wpt import run
+from tools import localpaths  # noqa: F401
+from wptrunner.browsers import product_list
+
+
+@pytest.fixture(scope="module")
+def venv():
+    from tools.wpt import virtualenv
+
+    class Virtualenv(virtualenv.Virtualenv):
+        def __init__(self):
+            self.path = tempfile.mkdtemp()
+
+        def create(self):
+            return
+
+        def activate(self):
+            return
+
+        def start(self):
+            return
+
+        def install(self, *requirements):
+            return
+
+        def install_requirements(self, requirements_path):
+            return
+
+    venv = Virtualenv()
+    yield venv
+
+    shutil.rmtree(venv.path)
 
 
 @pytest.mark.parametrize("platform", ["Windows", "Linux", "Darwin"])
@@ -16,3 +50,16 @@ def test_check_environ_fail(platform):
                 run.check_environ("foo")
 
     assert "wpt make-hosts-file" in excinfo.value.message
+
+
+@pytest.mark.parametrize("product", product_list)
+def test_setup_wptrunner(venv, product):
+    parser = run.create_parser()
+    kwargs = vars(parser.parse_args(["--channel=nightly", product]))
+    kwargs["prompt"] = False
+    kwargs["webdriver_binary"] = "fake"
+    # Hack to get a real existing path
+    kwargs["binary"] = sys.argv[0]
+    if kwargs["product"] == "sauce":
+        kwargs["product"] = "sauce:firefox:63"
+    run.setup_wptrunner(venv, **kwargs)

commit 04211017860a7a1a49e666f8b0d271e8ad792cf7

Fix two bugs introduced in #12877

tools/wpt/browser.py
@@ -264,7 +264,7 @@ class Firefox(Browser):
 
         dest = os.path.join(dest, "profiles", channel)
         if version:
-            dest = dest.join(version)
+            dest = os.path.join(dest, version)
         have_cache = False
         if os.path.exists(dest):
             if channel != "nightly":

tools/wpt/run.py
@@ -482,7 +482,7 @@ def setup_wptrunner(venv, prompt=True, install_browser=False, **kwargs):
             if channel != kwargs["channel"]:
                 logger.info("Interpreting channel '%s' as '%s'" % (kwargs["channel"],
                                                                    channel))
-                kwargs["browser_channel"] = channel
+            kwargs["browser_channel"] = channel
         else:
             logger.info("Valid channels for %s not known; using argument unmodified" % kwargs["product"])
     del kwargs["channel"]
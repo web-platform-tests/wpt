b5b38b09980a3db588c8212336e655679b5f3feb
Philip Jägenstedt <philip@foolip.org>
2018-10-24 13:56:24 +0800

    Add a comment for checking UCS-4 in for Python
    Co-Authored-By: qiuzhong <zhongx.qiu@intel.com>

--- css/build-css-testsuites.sh
+++ css/build-css-testsuites.sh
@@ -35,6 +35,7 @@ main() {
             exit 1
         fi
 
+        # The maximum Unicode code point is U+10FFFF = 1114111
         if [ `$PYTHON -c 'import sys; print(sys.maxunicode)'` != "1114111" ]; then
             echo "UCS-4 support for Python is required"
             exit 1

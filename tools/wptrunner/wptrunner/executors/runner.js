47402eac1ec44479c6318943d90ea229cdcd34c6
Mike Pennisi <mike@mikepennisi.com>
2018-11-02 21:47:51 -0400

    fixup! [wptrunner] Transmit test results via postMessage

--- tools/wptrunner/wptrunner/executors/runner.js
+++ tools/wptrunner/wptrunner/executors/runner.js
@@ -1,23 +1,35 @@
 document.title = '%(title)s';
 
 var message_queue = [];
+var source = null;
 
 /**
  * In preparation to execute a test, testdriver issues a number of messages
  * which should be queued until execution time. During execution, testdriver
  * consumes the queued messages by repeatedly "resuming" via `do_testharness`.
+ * The sequence of these events is variable, so the handler must account for
+ * cases where "resume" requests arrive before any messages are available (by
+ * caching the request's source) and where messages arrive before "resume"
+ * requests (by queuing the messages).
  */
 window.addEventListener("message", function(event) {
-  if (!event.data || !event.data.type) {
+  if (!event.data) {
     return;
   }
 
   if (event.data.type === "testdriver-resume") {
-    var next_message = message_queue.shift();
-    reply(event.source, next_message);
+    source = event.source;
   } else {
     message_queue.push(event.data);
   }
+
+  if (!source || !message_queue.length) {
+    return;
+  }
+
+  reply(source, message_queue[0]);
+  source = null;
+  message_queue.shift();
 }, false);
 
 function reply(source, data) {

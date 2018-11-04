web-platform-tests/wpt

commit cd17e3bcc05a5446bcb0f725551be27bafc59751

Rename generateOffer to generateDataChannelOffer and remove use of legacy optionsRename generateOffer to generateDataChannelOffer and remove use of legacy options (#13893)
Rename generateOffer to generateDataChannelOffer and remove use of legacy options

webrtc/RTCPeerConnection-helper.js
@@ -103,61 +103,11 @@ function assert_session_desc_not_similar(sessionDesc1, sessionDesc2) {
     'Expect both session descriptions to have different count of media lines');
 }
 
-// Helper function to generate offer using a freshly created RTCPeerConnection
-// object with any audio, video, data media lines present
-function generateOffer(options={}) {
-  const {
-    audio = false,
-    video = false,
-    data = false,
-    pc,
-  } = options;
-
-  if (data) {
-    pc.createDataChannel('test');
-  }
-
-  const setup = {};
-
-  if (audio) {
-    setup.offerToReceiveAudio = true;
-  }
-
-  if (video) {
-    setup.offerToReceiveVideo = true;
-  }
-
-  return pc.createOffer(setup).then(offer => {
-    // Guard here to ensure that the generated offer really
-    // contain the number of media lines we want
-    const { sdp } = offer;
-
-    if(audio) {
-      assert_equals(countAudioLine(sdp), 1,
-        'Expect m=audio line to be present in generated SDP');
-    } else {
-      assert_equals(countAudioLine(sdp), 0,
-        'Expect m=audio line to be present in generated SDP');
-    }
-
-    if(video) {
-      assert_equals(countVideoLine(sdp), 1,
-        'Expect m=video line to be present in generated SDP');
-    } else {
-      assert_equals(countVideoLine(sdp), 0,
-        'Expect m=video line to not present in generated SDP');
-    }
-
-    if(data) {
-      assert_equals(countApplicationLine(sdp), 1,
-        'Expect m=application line to be present in generated SDP');
-    } else {
-      assert_equals(countApplicationLine(sdp), 0,
-        'Expect m=application line to not present in generated SDP');
-    }
-
-    return offer;
-  });
+async function generateDataChannelOffer(pc) {
+  pc.createDataChannel('test');
+  const offer = pc.createOffer();
+  assert_equals(countApplicationLine(sdp), 1, 'Expect m=application line to be present in generated SDP');
+  return offer;
 }
 
 async function generateAudioReceiveOnlyOffer(pc)
@@ -182,14 +132,12 @@ async function generateVideoReceiveOnlyOffer(pc)
 
 // Helper function to generate answer based on given offer using a freshly
 // created RTCPeerConnection object
-function generateAnswer(offer) {
+async function generateAnswer(offer) {
   const pc = new RTCPeerConnection();
-  return pc.setRemoteDescription(offer)
-  .then(() => pc.createAnswer())
-  .then((answer) => {
-    pc.close();
-    return answer;
-  });
+  await pc.setRemoteDescription(offer);
+  const answer = pc.createAnswer();
+  pc.close();
+  return answer;
 }
 
 // Run a test function that return a promise that should

webrtc/RTCPeerConnection-createAnswer.html
@@ -11,8 +11,7 @@
   // https://w3c.github.io/webrtc-pc/archives/20170515/webrtc.html
 
   // The following helper functions are called from RTCPeerConnection-helper.js:
-  //   generateOffer()
-  //   generateAnswer()
+  //   generateDataChannelOffer()
 
   /*
    * 4.3.2. createAnswer()
@@ -56,7 +55,7 @@
 
     t.add_cleanup(() => pc.close());
 
-    return generateOffer({ pc, data: true })
+    return generateDataChannelOffer(pc)
     .then(offer => pc.setRemoteDescription(offer))
     .then(() => {
       pc.close();

webrtc/RTCPeerConnection-createOffer.html
@@ -11,8 +11,6 @@
   // https://w3c.github.io/webrtc-pc/archives/20170515/webrtc.html
 
   // The following helper functions are called from RTCPeerConnection-helper.js:
-  //   generateOffer()
-  //   generateAnswer()
   //   countAudioLine()
   //   countVideoLine()
   //   assert_session_desc_similar()

webrtc/RTCPeerConnection-setLocalDescription-answer.html
@@ -11,7 +11,6 @@
   // https://w3c.github.io/webrtc-pc/archives/20170605/webrtc.html
 
   // The following helper functions are called from RTCPeerConnection-helper.js:
-  //   generateOffer
   //   generateAnswer
   //   assert_session_desc_similar
 

webrtc/RTCPeerConnection-setLocalDescription-offer.html
@@ -11,7 +11,7 @@
   // https://w3c.github.io/webrtc-pc/archives/20170605/webrtc.html
 
   // The following helper functions are called from RTCPeerConnection-helper.js:
-  //   generateOffer
+  //   generateDataChannelOffer
   //   assert_session_desc_not_similar
   //   assert_session_desc_similar
 
@@ -109,7 +109,7 @@
 
     t.add_cleanup(() => pc2.close());
 
-    return generateOffer({ pc, data: true })
+    return generateDataChannelOffer(pc)
     .then(offer =>
       promise_rejects(t, 'InvalidModificationError',
         pc2.setLocalDescription(offer)));

webrtc/RTCPeerConnection-setLocalDescription-pranswer.html
@@ -11,8 +11,6 @@
   // https://w3c.github.io/webrtc-pc/archives/20170605/webrtc.html
 
   // The following helper functions are called from RTCPeerConnection-helper.js:
-  //   generateOffer
-  //   generateAnswer
   //   assert_session_desc_similar
 
   /*

webrtc/RTCPeerConnection-setLocalDescription.html
@@ -11,7 +11,7 @@
   // https://w3c.github.io/webrtc-pc/archives/20170605/webrtc.html
 
   // The following helper functions are called from RTCPeerConnection-helper.js:
-  //   generateOffer
+  //   generateDataChannelOffer
   //   assert_session_desc_not_similar
   //   assert_session_desc_similar
 
@@ -79,7 +79,7 @@
     const states = [];
     pc.addEventListener('signalingstatechange', () => states.push(pc.signalingState));
 
-    return generateOffer({ pc, data: true })
+    return generateDataChannelOffer(pc)
     .then(offer => pc.setRemoteDescription(offer))
     .then(() => pc.createAnswer())
     .then(answer =>

webrtc/RTCPeerConnection-setRemoteDescription-answer.html
@@ -11,7 +11,6 @@
   // https://w3c.github.io/webrtc-pc/archives/20170605/webrtc.html
 
   // The following helper functions are called from RTCPeerConnection-helper.js:
-  //   generateOffer()
   //   generateAnswer()
   //   assert_session_desc_similar()
 

commit 58364be5f8ad43e0056c39afbf65f56b289ae2d0

Fix regressions introduced in https://github.com/web-platform-tests/wpt/pull/13893

webrtc/RTCPeerConnection-helper.js
@@ -105,8 +105,8 @@ function assert_session_desc_not_similar(sessionDesc1, sessionDesc2) {
 
 async function generateDataChannelOffer(pc) {
   pc.createDataChannel('test');
-  const offer = pc.createOffer();
-  assert_equals(countApplicationLine(sdp), 1, 'Expect m=application line to be present in generated SDP');
+  const offer = await pc.createOffer();
+  assert_equals(countApplicationLine(offer.sdp), 1, 'Expect m=application line to be present in generated SDP');
   return offer;
 }
 
@@ -135,7 +135,7 @@ async function generateVideoReceiveOnlyOffer(pc)
 async function generateAnswer(offer) {
   const pc = new RTCPeerConnection();
   await pc.setRemoteDescription(offer);
-  const answer = pc.createAnswer();
+  const answer = await pc.createAnswer();
   pc.close();
   return answer;
 }
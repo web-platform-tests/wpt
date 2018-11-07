362f947574e4d048571d4bdebc0f8d7fad3f6002
Anne van Kesteren <annevk@annevk.nl>
2018-11-07 12:19:27 +0100

    add more tests

--- cors/cors-safelisted-request-header.any.js
+++ cors/cors-safelisted-request-header.any.js
@@ -13,23 +13,31 @@ function safelist(headers, expectPreflight = false) {
     if (expectPreflight) {
       await promise_rejects(t, TypeError(), request());
     } else {
-      const response = await fetch(url, { method: "POST", headers, body: "data" });
+      const response = await request();
       assert_equals(response.headers.get("content-type"), "text/plain");
-      const body = await response.text();
-      assert_equals(body, "NO");
+      assert_equals(await response.text(), "NO");
     }
     const checkResponse = await fetch(checkURL, { method: "POST", body: "data" });
-    const checkResponseBody = await checkResponse.text();
-    assert_equals(checkResponseBody, (expectPreflight ? "1" : "0"));
+    assert_equals(await checkResponse.text(), (expectPreflight ? "1" : "0"));
   }, (expectPreflight ? "Preflight" : "No preflight") + " for " + JSON.stringify(headers));
 }
 
-safelist({"content-type": "text/plain;"});
-safelist({"content-type": "text/plain;garbage"});
-safelist({"content-type": "text/plain;garbage\u0001\u0002"});
-safelist({"content-type": "text/plain,"}, true);
-safelist({"content-type": ",text/plain"}, true);
-safelist({"content-type": "text/plain,text/plain"}, true);
-safelist({"content-type": "text/plain,x/x"}, true);
-safelist({"content-type": "text/plain\u000B"}, true);
-safelist({"content-type": "text/plain\u000C"}, true);
+[
+  ["text /plain", true],
+  ["text\t/\tplain", true],
+  ["text/plain;"],
+  ["text/plain;garbage"],
+  ["text/plain;garbage\u0001\u0002", true],
+  ["text/plain,", true],
+  [",text/plain", true],
+  ["text/plain,text/plain", true],
+  ["text/plain,x/x", true],
+  ["text/plain\u000B", true],
+  ["text/plain\u000C", true],
+  ["application/www-form-urlencoded", true],
+  ["application/x-www-form-urlencoded;\u007F", true],
+  ["multipart/form-data"],
+  ["multipart/form-data;\"", true]
+].forEach(([mimeType, preflight = false]) => {
+  safelist({"content-type": mimeType}, preflight);
+})

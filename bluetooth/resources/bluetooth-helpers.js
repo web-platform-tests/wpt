f45c992c2cad60ea26546707423f8a18d6efcb43
Mustaq Ahmed <mustaq@google.com>
2018-11-22 06:48:26 -0800

    WIP: Fix testdriver clicks to make bluetooth wpt pass with UAv2.
    Blocker: testdriver doens't let click inside iframes!
    https://cs.chromium.org/chromium/src/third_party/WebKit/LayoutTests/external/wpt/resources/testdriver.js?rcl=5d1bd990e4c21787e6371ab8db5b4fd711eaa7c3&l=102
    
    Bug: 860713
    Change-Id: I83b80908b7adc2a65446e5b27000dc67e37d4b9a

--- bluetooth/resources/bluetooth-helpers.js
+++ bluetooth/resources/bluetooth-helpers.js
@@ -237,19 +237,23 @@ function waitForDocumentReady() {
   });
 }
 
-function callWithTrustedClick(callback) {
+function callWithTrustedClick(callback, targetDocument) {
   return waitForDocumentReady()
     .then(() => new Promise(resolve => {
+      if (!targetDocument)
+        targetDocument = document;
+
       let button = document.createElement('button');
       button.textContent = 'click to continue test';
       button.style.display = 'block';
       button.style.fontSize = '20px';
       button.style.padding = '10px';
       button.onclick = () => {
-        document.body.removeChild(button);
+        targetDocument.body.removeChild(button);
         resolve(callback());
       };
-      document.body.appendChild(button);
+      targetDocument.body.appendChild(button);
+
       test_driver.click(button);
     }));
 }

02ab5ba5a15002dfcf2108fe6e05b7b5bfab1c87
Mike Pennisi <mike@mikepennisi.com>
2018-12-06 22:14:45 -0500

    Avoid another race condition

--- tools/wptrunner/wptrunner/executors/reftest-wait_webdriver.js
+++ tools/wptrunner/wptrunner/executors/reftest-wait_webdriver.js
@@ -37,9 +37,19 @@ function ready_for_screenshot() {
   });
 }
 
-var root = document.documentElement;
-var observer = new MutationObserver(root_wait);
+var root, observer;
 
-observer.observe(root, {attributes: true});
+(function begin() {
+  root = document.documentElement;
 
-root_wait();
+  // This script may be evaluated before the document element is available.
+  if (!root) {
+    setTimeout(begin, 0);
+    return;
+  }
+  observer = new MutationObserver(root_wait);
+
+  observer.observe(root, {attributes: true});
+
+  root_wait();
+}());

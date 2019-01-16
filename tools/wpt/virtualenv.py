f0cecf5237c2cd8f2d8af2615287b56da38764eb
Luke Zielinski <lpz@chromium.org>
2019-01-16 13:37:45 -0500

    Remove skipping logic from virtualenv.py, let the caller decide which methods to call.
    Also don't bother with any activation, assume that a well-setup
    virtualenv has already been activated.

--- tools/wpt/virtualenv.py
+++ tools/wpt/virtualenv.py
@@ -8,29 +8,11 @@ from tools.wpt.utils import call
 
 logger = logging.getLogger(__name__)
 
-
-def add_site_package_path(root_path):
-    """Update system path to include packages from existing virtualenv"""
-    for root, dirs, files in os.walk(root_path + "/lib"):
-        if "site-packages" in dirs:
-            site_package_path = root + "/site-packages"
-            logger.info("Using site packages from %s" % site_package_path)
-            sys.path.append(site_package_path)
-            return
-
-
 class Virtualenv(object):
     def __init__(self, path, skip_virtualenv_setup):
         self.path = path
         self.skip_virtualenv_setup = skip_virtualenv_setup
-        if skip_virtualenv_setup:
-            # Adjust paths so we can locate things like installed packages and
-            # downloaded webdrivers
-            expanded_venv_path = os.path.abspath(path)
-            add_site_package_path(expanded_venv_path)
-            bin_path = expanded_venv_path + "/bin"
-            os.environ["PATH"] += os.pathsep + bin_path
-        else:
+        if not skip_virtualenv_setup:
             self.virtualenv = find_executable("virtualenv")
             if not self.virtualenv:
                 raise ValueError("virtualenv must be installed and on the PATH")
@@ -40,8 +22,6 @@ class Virtualenv(object):
         return os.path.isdir(self.path)
 
     def create(self):
-        if self.skip_virtualenv_setup:
-            return
         if os.path.exists(self.path):
             shutil.rmtree(self.path)
         call(self.virtualenv, self.path, "-p", sys.executable)
@@ -60,24 +40,16 @@ class Virtualenv(object):
         return path
 
     def activate(self):
-        if self.skip_virtualenv_setup:
-            return
         path = os.path.join(self.bin_path, "activate_this.py")
         execfile(path, {"__file__": path})  # noqa: F821
 
     def start(self):
-        if self.skip_virtualenv_setup:
-            return
         if not self.exists:
             self.create()
         self.activate()
 
     def install(self, *requirements):
-        if self.skip_virtualenv_setup:
-            return
         call(self.pip_path, "install", *requirements)
 
     def install_requirements(self, requirements_path):
-        if self.skip_virtualenv_setup:
-            return
         call(self.pip_path, "install", "-r", requirements_path)

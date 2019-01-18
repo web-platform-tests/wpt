045bdd5f563eaebbc2f1684311e8d6312e1fa5c5
John Rummell <jrummell@chromium.org>
2019-01-17 17:36:01 -0800

    Fix issues raised in pull request.

--- encrypted-media/scripts/check-hdcp-status.js
+++ encrypted-media/scripts/check-hdcp-status.js
@@ -1,6 +1,6 @@
 function runTest(config, qualifier)
 {
-  function checkHdcpPolicy(hdcpPolicy)
+  function checkStatusForMinHdcpVersionPolicy(hdcpVersion)
   {
     return navigator.requestMediaKeySystemAccess(config.keysystem, getSimpleConfiguration())
         .then(function(access) {
@@ -11,16 +11,16 @@ function runTest(config, qualifier)
           // don't bother checking the result returned as it may or
           // may not be supported. This simply verifies that
           // getStatusForPolicy() exists and doesn't blow up.
-          return mediaKeys.getStatusForPolicy({minHdcpVersion: hdcpPolicy});
+          return mediaKeys.getStatusForPolicy({minHdcpVersion: hdcpVersion});
         });
   }
 
   promise_test(
-      () => checkHdcpPolicy(''),
+      () => checkStatusForMinHdcpVersionPolicy(''),
       testnamePrefix(qualifier, config.keysystem) +
-          ' support for empty HDCP policy.');
+          ' support for empty HDCP version.');
 
   promise_test(
-      () => checkHdcpPolicy('1.0'),
+      () => checkStatusForMinHdcpVersionPolicy('1.0'),
       testnamePrefix(qualifier, config.keysystem) + ' support for HDCP 1.0.');
 }

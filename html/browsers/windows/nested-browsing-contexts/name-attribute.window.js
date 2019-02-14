136ef854050b6536e8e636e7afd1008f586c2033
Anne van Kesteren <annevk@annevk.nl>
2019-02-14 17:29:14 +0100

    test cross-origin too without flakyness

--- html/browsers/windows/nested-browsing-contexts/name-attribute.window.js
+++ html/browsers/windows/nested-browsing-contexts/name-attribute.window.js
@@ -1,3 +1,5 @@
+// META: script=/common/get-host-info.sub.js
+
 [
   "frame", // This works without <frameset>, so great
   "iframe",
@@ -9,32 +11,48 @@
     "",
     "initialvalue"
   ].forEach(initialNameValue => {
-    async_test(t => {
-      const ident = element + initialNameValue,
-            frame = document.createElement(element),
-            expectedNameValue = initialNameValue || "",
-            listener = t.step_func(e => {
-              // This is the only way to get hold of some WindowProxy objects
-              const frameW = e.source;
-              if (e.data.ident === ident) {
-                assert_equals(frameW.name, expectedNameValue);
-                frame.setAttribute("name", "meh");
-                assert_equals(frameW.name, expectedNameValue);
-                frame.removeAttribute("name");
-                assert_equals(frameW.name, expectedNameValue);
-                t.done();
-              }
-            });
-      frame.setAttribute(element === "object" ? "data" : "src", `resources/post-to-parent.html?ident=${ident}`);
-      if (initialNameValue !== null) {
-        frame.setAttribute("name", initialNameValue);
-      }
-      t.add_cleanup(() => {
-        self.removeEventListener("message", listener);
-        frame.remove();
-      });
-      self.addEventListener("message", listener);
-      document.body.append(frame);
-    }, `<${element}${initialNameValue !== null ? ' name=' + initialNameValue : ''}>`);
+    [
+      "same-origin",
+      "cross-origin"
+    ].forEach(originType => {
+      async_test(t => {
+        const ident = element + initialNameValue + originType,
+              file = `${new URL("resources/post-to-parent.html", location.href).pathname}?ident=${ident}`,
+              child = originType === "same-origin" ? file : `${get_host_info().HTTP_REMOTE_ORIGIN}${file}`,
+              frame = document.createElement(element),
+              expectedNameValue = initialNameValue || "";
+        let state = "set";
+        const listener = t.step_func(e => {
+          if (e.data.ident === ident) {
+            assert_equals(e.data.name, expectedNameValue); // This check is always the same
+            if (state === "set") {
+              frame.setAttribute("name", "meh");
+              state = "remove"
+              e.source.postMessage(null, "*");
+              return;
+            }
+            if (state === "remove") {
+              frame.removeAttribute("name");
+              state = "done";
+              e.source.postMessage(null, "*");
+              return;
+            }
+            if (state === "done") {
+              t.done();
+            }
+          }
+        });
+        frame.setAttribute(element === "object" ? "data" : "src", child);
+        if (initialNameValue !== null) {
+          frame.setAttribute("name", initialNameValue);
+        }
+        t.add_cleanup(() => {
+          self.removeEventListener("message", listener);
+          frame.remove();
+        });
+        self.addEventListener("message", listener);
+        document.body.append(frame);
+      }, `${originType} <${element}${initialNameValue !== null ? ' name=' + initialNameValue : ''}>`);
+    });
   });
 });

c8d0e7301f5cd2c5af7b9aa580ed19b7b48a8b01
Sam Sneddon <me@gsnedders.com>
2019-02-15 11:23:58 +0000

    Remove the EqualTimeChunker
    This hasn't been used by anyone in several years, and trying to fix
    the tests as part of the work for #13507 tends to suggest that the
    chunker is itself broken. It is, in many ways, the least useful
    chunker as it results in test execution order changing every time the
    test execution time metadata is updated, and this leads to further
    flakiness.

--- tools/wptrunner/wptrunner/tests/test_chunker.py
+++ tools/wptrunner/wptrunner/tests/test_chunker.py
@@ -5,7 +5,6 @@ from mozlog import structured
 
 sys.path.insert(0, join(dirname(__file__), "..", "..", ".."))
 
-from wptrunner.testloader import EqualTimeChunker
 from manifest.sourcefile import SourceFile
 
 structured.set_default_logger(structured.structuredlog.StructuredLogger("TestChunker"))
@@ -35,63 +34,6 @@ def make_mock_manifest(*items):
                        dir_path + filename,
                        set([MockTest("%i.html" % i, dir_path + filename)])))
     return rv
-
-
-class TestEqualTimeChunker(unittest.TestCase):
-
-    def test_include_all(self):
-        tests = make_mock_manifest(("test", "a", 10), ("test", "a/b", 10),
-                                   ("test", "c", 10))
-
-        chunk_1 = list(EqualTimeChunker(3, 1)(tests))
-        chunk_2 = list(EqualTimeChunker(3, 2)(tests))
-        chunk_3 = list(EqualTimeChunker(3, 3)(tests))
-
-        self.assertEquals(tests[:10], chunk_1)
-        self.assertEquals(tests[10:20], chunk_2)
-        self.assertEquals(tests[20:], chunk_3)
-
-    def test_include_all_1(self):
-        tests = make_mock_manifest(("test", "a", 5), ("test", "a/b", 5),
-                                   ("test", "c", 10), ("test", "d", 10))
-
-        chunk_1 = list(EqualTimeChunker(3, 1)(tests))
-        chunk_2 = list(EqualTimeChunker(3, 2)(tests))
-        chunk_3 = list(EqualTimeChunker(3, 3)(tests))
-
-        self.assertEquals(tests[:10], chunk_1)
-        self.assertEquals(tests[10:20], chunk_2)
-        self.assertEquals(tests[20:], chunk_3)
-
-    def test_long(self):
-        tests = make_mock_manifest(("test", "a", 100), ("test", "a/b", 1),
-                                   ("test", "c", 1))
-
-        chunk_1 = list(EqualTimeChunker(3, 1)(tests))
-        chunk_2 = list(EqualTimeChunker(3, 2)(tests))
-        chunk_3 = list(EqualTimeChunker(3, 3)(tests))
-
-        self.assertEquals(tests[:100], chunk_1)
-        self.assertEquals(tests[100:101], chunk_2)
-        self.assertEquals(tests[101:102], chunk_3)
-
-    def test_long_1(self):
-        tests = make_mock_manifest(("test", "a", 1), ("test", "a/b", 100),
-                                   ("test", "c", 1))
-
-        chunk_1 = list(EqualTimeChunker(3, 1)(tests))
-        chunk_2 = list(EqualTimeChunker(3, 2)(tests))
-        chunk_3 = list(EqualTimeChunker(3, 3)(tests))
-
-        self.assertEquals(tests[:1], chunk_1)
-        self.assertEquals(tests[1:101], chunk_2)
-        self.assertEquals(tests[101:102], chunk_3)
-
-    def test_too_few_dirs(self):
-        with self.assertRaises(ValueError):
-            tests = make_mock_manifest(("test", "a", 1), ("test", "a/b", 100),
-                                       ("test", "c", 1))
-            list(EqualTimeChunker(4, 1)(tests))
 
 
 if __name__ == "__main__":

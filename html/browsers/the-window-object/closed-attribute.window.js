1f8f73de34723145738a456147405b3c0e773377
Anne van Kesteren <annevk@annevk.nl>
2019-03-01 14:26:41 +0100

    final tests

--- html/browsers/the-window-object/closed-attribute.window.js
+++ html/browsers/the-window-object/closed-attribute.window.js
@@ -9,23 +9,59 @@ function closedTest(newWindow, closeNewWindowsBrowsingContext) {
 test(() => {
   const frame = document.body.appendChild(document.createElement("iframe"));
   closedTest(frame.contentWindow, () => frame.remove());
-});
+}, "closed and same-origin nested browsing context");
 
 test(() => {
   const openee = window.open();
   closedTest(openee, () => openee.close());
-});
 
-async_test(t => {
-  const frame = document.createElement("iframe"),
-        support = new URL("support/closed.html", location.href).pathname,
-        ident = "nestedframe";
-  frame.src = `${get_host_info().HTTP_REMOTE_ORIGIN}${support}?window=parent&ident=${ident}`;
-  const listener = t.step_func_done(() => {
-    closedTest(frame.contentWindow, () => frame.remove());
-    self.removeEventListener("message", listener);
-  });
-  self.addEventListener("message", listener);
-  document.body.append(frame);
-});
+  // This should not matter
+  openee.close();
+  assert_equals(openee.closed, true);
+}, "closed/close() and same-origin auxiliary browsing context");
 
+const support = new URL("support/closed.html", location.href).pathname;
+[
+  {
+    type: "cross-origin",
+    url: `${get_host_info().HTTP_REMOTE_ORIGIN}${support}`
+  },
+  {
+    type: "cross-site",
+    url: `${get_host_info().HTTP_NOTSAMESITE_ORIGIN}${support}`
+  }
+].forEach(val => {
+  async_test(t => {
+    const frame = document.createElement("iframe"),
+          ident = `${val.type}-nestedframe`;
+    frame.src = `${val.url}?window=parent&ident=${ident}`;
+    const listener = t.step_func(e => {
+      if (e.data === ident) {
+        closedTest(frame.contentWindow, () => frame.remove());
+        self.removeEventListener("message", listener);
+        t.done();
+      }
+    });
+    self.addEventListener("message", listener);
+    document.body.append(frame);
+  }, `closed and ${val.type} nested browsing context`);
+
+  async_test(t => {
+    const ident = `${val.type}-auxiliaryframe`,
+          support = new URL("support/closed.html", location.href).pathname,
+          openee = window.open(`${val.url}?window=opener&ident=${ident}`),
+          listener = t.step_func(e => {
+      if (e.data === ident) {
+        closedTest(openee, () => openee.close());
+
+        // This should not matter
+        openee.close();
+        assert_equals(openee.closed, true);
+
+        self.removeEventListener("message", listener);
+        t.done();
+      }
+    });
+    self.addEventListener("message", listener);
+  }, `closed/close() and ${val.type} auxiliary browsing context`);
+});

f19f5fc5b8a2cf7f7c99987758b52f9b04fab758
Anne van Kesteren <annevk@annevk.nl>
2019-03-01 11:36:10 +0100

    more wip

--- html/browsers/the-window-object/closed-attribute.window.js
+++ html/browsers/the-window-object/closed-attribute.window.js
@@ -1,3 +1,5 @@
+// META: script=/common/get-host-info.sub.js
+
 function closedTest(newWindow, closeNewWindowsBrowsingContext) {
   assert_equals(newWindow.closed, false);
   closeNewWindowsBrowsingContext();
@@ -13,3 +15,17 @@ test(() => {
   const openee = window.open();
   closedTest(openee, () => openee.close());
 });
+
+async_test(t => {
+  const frame = document.createElement("iframe"),
+        support = new URL("support/closed.html", location.href).pathname,
+        ident = "nestedframe";
+  frame.src = `${get_host_info().HTTP_REMOTE_ORIGIN}${support}?window=parent&ident=${ident}`;
+  const listener = t.step_func_done(() => {
+    closedTest(frame.contentWindow, () => frame.remove());
+    self.removeEventListener("message", listener);
+  });
+  self.addEventListener("message", listener);
+  document.body.append(frame);
+});
+

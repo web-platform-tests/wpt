8351df8ec4128c3248abfc479837a43f79824cd2
Sam Sneddon <me@gsnedders.com>
2019-03-08 13:28:46 +0000

    fixup! Move Travis CI to Xenial (16.04 LTS)

--- tools/ci/before_install.sh
+++ tools/ci/before_install.sh
@@ -7,7 +7,7 @@ if [[ $RUN_JOB -eq 1 ]] || ./wpt test-jobs --includes $JOB; then
     export RUN_JOB=1
     git submodule update --init --recursive 1>&2
     export DISPLAY=:99.0
-    systemctl start xvfb
+    sudo systemctl start xvfb
     # For uploading the manifest
     export WPT_MANIFEST_FILE=$HOME/meta/MANIFEST-$(git rev-parse HEAD).json
 else

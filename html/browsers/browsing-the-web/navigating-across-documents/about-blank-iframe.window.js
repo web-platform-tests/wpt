fb68675f792a4ddd48a17a3b270aec31d7872abb
Anne van Kesteren <annevk@annevk.nl>
2019-03-13 13:31:56 +0100

    add some clarity (browsers agree on this)

--- html/browsers/browsing-the-web/navigating-across-documents/about-blank-iframe.window.js
+++ html/browsers/browsing-the-web/navigating-across-documents/about-blank-iframe.window.js
@@ -1,9 +1,11 @@
 async_test(t => {
   const frame = document.body.appendChild(document.createElement("iframe")),
-        frameW = frame.contentWindow;
+        frameW = frame.contentWindow,
+        path = "/common/blank.html";
   frameW.perGlobalVariable = "heya";
   frame.onload = t.step_func_done(() => {
+    assert_equals(frameW.location.pathname, path);
     assert_equals(frameW.perGlobalVariable, "heya");
   });
-  frame.src = "/common/blank.html";
+  frame.src = path;
 }, "Navigating from initial about:blank and the global object");

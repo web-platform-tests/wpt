689494af916d199630e5710e663093928b318029
Anne van Kesteren <annevk@annevk.nl>
2019-03-18 17:39:12 +0100

    textContent tests

--- html/semantics/scripting-1/the-script-element/mutations.window.js
+++ html/semantics/scripting-1/the-script-element/mutations.window.js
@@ -12,6 +12,19 @@ function createGlobals() {
   test(() => {
     const script = document.createElement("script"),
           globalsIndex = createGlobals();
+    script.textContent = `self.globalLogs[${globalsIndex}].push(1); self.globalScripts[${globalsIndex}].textContent = 'self.globalLogs[${globalsIndex}].push(2)'; self.globalLogs[${globalsIndex}].push(3);`;
+    self.globalScripts[globalsIndex].append(initialScriptValue);
+    document.body.append(script, self.globalScripts[globalsIndex]);
+
+    // TODO: decide on correct behavior
+    assert_array_equals(self.globalLogs[globalsIndex], [1, 2, 3], `Gotten order: ${self.globalLogs[globalsIndex]}`);
+  }, `Modifying the textContent (initially "${initialScriptValue}") of the 2nd inserted script from the 1st inserted script`);
+});
+
+["", " ", ";"].forEach(initialScriptValue => {
+  test(() => {
+    const script = document.createElement("script"),
+          globalsIndex = createGlobals();
     script.textContent = `self.globalLogs[${globalsIndex}].push(1); self.globalScripts[${globalsIndex}].firstChild.data = 'self.globalLogs[${globalsIndex}].push(2)'; self.globalLogs[${globalsIndex}].push(3);`;
     self.globalScripts[globalsIndex].append(initialScriptValue);
     document.body.append(script, self.globalScripts[globalsIndex]);

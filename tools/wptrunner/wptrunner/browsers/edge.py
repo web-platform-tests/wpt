1d5c070b95370cc5bac29ed5ced1d2ea9ae5d7cf
Mustapha Jaber <mustjab@windows.microsoft.com>
2019-03-26 12:00:47 -0700

    Removed closing of driver process

--- tools/wptrunner/wptrunner/browsers/edge.py
+++ tools/wptrunner/wptrunner/browsers/edge.py
@@ -91,7 +91,6 @@ class EdgeBrowser(Browser):
         procs = subprocess.check_output(['tasklist', '/fi', 'ImageName eq ' + edge_proc_name])
         if edge_proc_name in procs:
             subprocess.call(['taskkill.exe', '/f', '/im', 'microsoftedge*']) # close all Edge browser instances
-        subprocess.call(['taskkill.exe', '/f', '/im', 'microsoftwebdriver.exe']) # close all Edge driver instances
         self.logger.info("stopped")
 
     def pid(self):

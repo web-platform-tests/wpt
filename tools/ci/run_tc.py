4d4bd61adabce8410213d62d3547de815fa24480
Sam Sneddon <me@gsnedders.com>
2019-04-02 16:15:17 +0100

    Add more logging in run_tc.py failure case

--- tools/ci/run_tc.py
+++ tools/ci/run_tc.py
@@ -233,6 +233,9 @@ def setup_repository():
         else:
             print("ERROR: Pull request HEAD wasn't a 2-parent merge commit; "
                   "expected to test the merge of PR into the base")
+            commit = run(["git", "show", "--no-patch", "--format=%H", "task_head"], return_stdout=True).strip()
+            print("HEAD: %s" % commit)
+            print("Parents: %s" % ", ".join(parents))
             sys.exit(1)
 
     branch = os.environ.get("GITHUB_BRANCH")

70b5024977f86bf012bdaa83f581012ad1307d49
wanghongjuan <hongjuan.wang@intel.com>
2019-04-11 14:59:53 +0800

    address Ms2ger's comments

--- service-workers/service-worker/resources/import-scripts-diff-resource-map-worker.js
+++ service-workers/service-worker/resources/import-scripts-diff-resource-map-worker.js
@@ -1,9 +1,9 @@
 importScripts('/resources/testharness.js');
 
 let echo1 = null;
-    echo2 = null;
-    arg1 = 'import-scripts-get.py?output=echo1&msg=test1';
-    arg2 = 'import-scripts-get.py?output=echo2&msg=test2';
+let echo2 = null;
+let arg1 = 'import-scripts-get.py?output=echo1&msg=test1';
+let arg2 = 'import-scripts-get.py?output=echo2&msg=test2';
 
 importScripts(arg1, arg2);
 assert_equals(echo1, 'test1');
--- service-workers/service-worker/resources/import-scripts-get.py
+++ service-workers/service-worker/resources/import-scripts-get.py
@@ -3,4 +3,4 @@ def main(req, res):
         ('Cache-Control', 'no-cache, must-revalidate'),
         ('Pragma', 'no-cache'),
         ('Content-Type', 'application/javascript')],
-        req.GET['output'] + ' = "%s";\n' % req.GET['msg'])
+        '%s = "%s";\n' % (req.GET['output'], req.GET['msg']))

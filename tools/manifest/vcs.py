87f25e32dd166d6ba74b9daaa416bb75a120cbce
Sam Sneddon <me@gsnedders.com>
2019-04-16 16:18:33 +0100

    Get mypy running on lint

--- tools/manifest/vcs.py
+++ tools/manifest/vcs.py
@@ -78,7 +78,7 @@ class GitHasher(object):
 
 class FileSystem(object):
     def __init__(self, root, url_base, cache_path, manifest_path=None, rebuild=False):
-        from gitignore import gitignore
+        from gitignore import gitignore  # type: ignore
         self.root = os.path.abspath(root)
         self.url_base = url_base
         self.ignore_cache = None

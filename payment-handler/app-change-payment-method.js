d1ab40147233eeb5039b6c1fdf12f3111dd4c6a7
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2019-04-25 17:12:05 -0400

    Add PaymentRequestEvent.changePaymentMethod(methodName, details) test.

---
+++ payment-handler/app-change-payment-method.js
@@ -0,0 +1,24 @@
+self.addEventListener('canmakepayment', event => {
+  event.respondWith(true);
+});
+
+self.addEventListener('paymentrequest', event => {
+  const methodName = event.methodData[0].supportedMethods;
+  event.respondWith(new Promise(resolve => {
+    const result = {methodName, details: {changePaymentMethodReturned: 'Nothing'}};
+    if (!event.changePaymentMethod) {
+      result.details.changePaymentMethodReturned = 'The changePaymentMethod() method is not implemented.';
+      resolve(details);
+    } else {
+      event.changePaymentMethod(methodName, {country: 'US'})
+      .then(response => {
+        result.details.changePaymentMethodReturned = response;
+        resolve(result);
+      })
+      .catch(error => {
+        result.details.changePaymentMethodReturned = error.message;
+        resolve(result);
+      });
+    }
+  }));
+});

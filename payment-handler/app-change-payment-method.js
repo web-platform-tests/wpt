30f1b1de144200089a0673447e83da6514cbd36b
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2019-04-26 10:03:29 -0400

    Prettier

--- payment-handler/app-change-payment-method.js
+++ payment-handler/app-change-payment-method.js
@@ -4,21 +4,28 @@ self.addEventListener('canmakepayment', event => {
 
 self.addEventListener('paymentrequest', event => {
   const methodName = event.methodData[0].supportedMethods;
-  event.respondWith(new Promise(resolve => {
-    const result = {methodName, details: {changePaymentMethodReturned: 'Nothing'}};
-    if (!event.changePaymentMethod) {
-      result.details.changePaymentMethodReturned = 'The changePaymentMethod() method is not implemented.';
-      resolve(details);
-    } else {
-      event.changePaymentMethod(methodName, {country: 'US'})
-      .then(response => {
-        result.details.changePaymentMethodReturned = response;
-        resolve(result);
-      })
-      .catch(error => {
-        result.details.changePaymentMethodReturned = error.message;
-        resolve(result);
-      });
-    }
-  }));
+  event.respondWith(
+    new Promise(resolve => {
+      const result = {
+        methodName,
+        details: {changePaymentMethodReturned: 'Nothing'},
+      };
+      if (!event.changePaymentMethod) {
+        result.details.changePaymentMethodReturned =
+          'The changePaymentMethod() method is not implemented.';
+        resolve(details);
+      } else {
+        event
+          .changePaymentMethod(methodName, {country: 'US'})
+          .then(response => {
+            result.details.changePaymentMethodReturned = response;
+            resolve(result);
+          })
+          .catch(error => {
+            result.details.changePaymentMethodReturned = error.message;
+            resolve(result);
+          });
+      }
+    }),
+  );
 });

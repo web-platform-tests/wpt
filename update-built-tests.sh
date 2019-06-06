3abff8037749bc4ab20936b1842ecca882fe1ba8
Luke Zielinski <lpz@chromium.org>
2019-06-06 11:22:17 -0400

    Also fix update-built-tests.sh

--- update-built-tests.sh
+++ update-built-tests.sh
@@ -2,7 +2,6 @@
 set -ex
 
 2dcontext/tools/build.sh
-infrastructure/assumptions/tools/build.sh
 html/tools/build.sh
 offscreen-canvas/tools/build.sh
 python mimesniff/mime-types/resources/generated-mime-types.py

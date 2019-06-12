df7f249e84d8c28547f53a54cd17001b0ed327c6
Jake Archibald <jaffathecake@gmail.com>
2019-06-12 15:57:52 +0100

    Swap order

--- service-workers/service-worker/global-serviceworker.https.any.js
+++ service-workers/service-worker/global-serviceworker.https.any.js
@@ -31,7 +31,7 @@ async_test((t) => {
 async_test((t) => {
   addEventListener('install', t.step_func_done(() => {
     assert_true('serviceWorker' in self, 'self.serviceWorker exists');
-    assert_equals(initialServiceWorker, serviceWorker, `self.serviceWorker hasn't changed`);
+    assert_equals(serviceWorker, initialServiceWorker, `self.serviceWorker hasn't changed`);
     assert_equals(registration.installing, serviceWorker, 'registration.installing');
     assert_equals(registration.waiting, null, 'registration.waiting');
     assert_equals(registration.active, null, 'registration.active');
@@ -44,7 +44,7 @@ async_test((t) => {
 async_test((t) => {
   addEventListener('activate', t.step_func_done(() => {
     assert_true('serviceWorker' in self, 'self.serviceWorker exists');
-    assert_equals(initialServiceWorker, serviceWorker, `self.serviceWorker hasn't changed`);
+    assert_equals(serviceWorker, initialServiceWorker, `self.serviceWorker hasn't changed`);
     assert_equals(registration.installing, null, 'registration.installing');
     assert_equals(registration.waiting, null, 'registration.waiting');
     assert_equals(registration.active, serviceWorker, 'registration.active');

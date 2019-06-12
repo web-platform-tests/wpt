33e5fc221b3732e96e084ec82f4df2ea73d739d8
Jake Archibald <jaffathecake@gmail.com>
2019-06-12 10:13:18 +0100

    Don't need `self`

--- service-workers/service-worker/global-serviceworker.https.any.js
+++ service-workers/service-worker/global-serviceworker.https.any.js
@@ -23,7 +23,7 @@ async_test((t) => {
 async_test((t) => {
   addEventListener('install', t.step_func_done(() => {
     assert_true('serviceWorker' in self, 'self.serviceWorker exists');
-    assert_equals(registration.installing, self.serviceWorker, 'registration.installing');
+    assert_equals(registration.installing, serviceWorker, 'registration.installing');
     assert_equals(registration.waiting, null, 'registration.waiting');
     assert_equals(registration.active, null, 'registration.active');
     assert_equals(serviceWorker.state, 'installing', 'serviceWorker.state');
@@ -35,7 +35,7 @@ async_test((t) => {
     assert_true('serviceWorker' in self, 'self.serviceWorker exists');
     assert_equals(registration.installing, null, 'registration.installing');
     assert_equals(registration.waiting, null, 'registration.waiting');
-    assert_equals(registration.active, self.serviceWorker, 'registration.active');
+    assert_equals(registration.active, serviceWorker, 'registration.active');
     assert_equals(serviceWorker.state, 'activating', 'serviceWorker.state');
   }));
 }, 'During activate');

48fe0cd9880ef624c92fc74b398d4969246c0354
Lan Wei <lanwei@chromium.org>
2019-06-24 11:07:53 -0400

    create frame

--- tools/webdriver/webdriver/client.py
+++ tools/webdriver/webdriver/client.py
@@ -218,7 +218,9 @@ class Actions(object):
                         ``ActionSequence.dict``.
         """
         body = {"actions": [] if actions is None else actions}
-        return self.session.send_session_command("POST", "actions", body)
+        actions = self.session.send_session_command("POST", "actions", body)
+        self.session.switch_frame("parent")
+        return actions
 
     @command
     def release(self):
@@ -308,8 +310,11 @@ class Find(object):
         self.session = session
 
     @command
-    def css(self, selector, all=True):
-        return self._find_element("css selector", selector, all)
+    def css(self, element_selector, frame, all=True):
+        if (frame != "window"):
+            self.session.switch_frame(frame)
+        elements = self._find_element("css selector", element_selector, all)
+        return elements
 
     def _find_element(self, strategy, selector, all):
         route = "elements" if all else "element"

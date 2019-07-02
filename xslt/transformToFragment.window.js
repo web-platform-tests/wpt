32c07e34c7f2c7b3f042a4fc0685064c6a36f6e5
Luke Bjerring <lukebjerring@google.com>
2019-07-02 12:56:35 -0400

    Wait for window.load event

--- xslt/transformToFragment.window.js
+++ xslt/transformToFragment.window.js
@@ -3,28 +3,34 @@ const cases = {
   external: '<script src="externalScript.js"></script>',
 };
 
+const loaded = new Promise(resolve => {
+  window.addEventListener('load', resolve);
+});
+
 Object.entries(cases).forEach(([k, v]) => {
-  test(() => {
-    const xsltSrc = `<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-  <xsl:output method="html" encoding="utf-8" version="5"/>
-  <xsl:template match="/">
-    <section>
-      ${v}
-    </section>
-  </xsl:template>
+  const xsltSrc = `<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+<xsl:output method="html" encoding="utf-8" version="5"/>
+<xsl:template match="/">
+  <section>
+    ${v}
+  </section>
+</xsl:template>
 </xsl:stylesheet>`;
 
-    const processor = new XSLTProcessor();
-    const parser = new DOMParser();
-    processor.importStylesheet(
-      parser.parseFromString(xsltSrc, 'application/xml')
-    );
-    document.body.appendChild(
-      processor.transformToFragment(
-        parser.parseFromString('<x/>', 'application/xml'),
-        document
-      )
-    );
+  const processor = new XSLTProcessor();
+  const parser = new DOMParser();
+  processor.importStylesheet(
+    parser.parseFromString(xsltSrc, 'application/xml')
+  );
+  document.body.appendChild(
+    processor.transformToFragment(
+      parser.parseFromString('<x/>', 'application/xml'),
+      document
+    )
+  );
+
+  promise_test(async () => {
+    await Promise.resolve(loaded);
     assert_true(
       window[`${k}Script`],
       'script element from XSLTProcessor.transformToFragment() is evaluated'

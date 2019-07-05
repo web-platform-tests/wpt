4cf35a460d7aeac1a78a4f49844ddb90fe7ea7b3
Anne van Kesteren <annevk@annevk.nl>
2019-07-05 15:16:03 +0200

    Restructure COOP some more, add couple COOP+COEP tests

--- html/cross-origin-opener-policy/resources/common.js
+++ html/cross-origin-opener-policy/resources/common.js
@@ -2,23 +2,27 @@ const SAME_ORIGIN = {origin: get_host_info().HTTP_ORIGIN, name: "SAME_ORIGIN"};
 const SAME_SITE = {origin: get_host_info().HTTP_REMOTE_ORIGIN, name: "SAME_SITE"};
 const CROSS_ORIGIN = {origin: get_host_info().HTTP_NOTSAMESITE_ORIGIN, name: "CROSS_ORIGIN"}
 
-function coop_test(t, host, coop, channelName, hasOpener) {
-  let bc = new BroadcastChannel(channelName);
+function coop_coep_test(t, host, coop, coep, channelName, hasOpener) {
+  const bc = new BroadcastChannel(channelName);
   bc.onmessage = t.step_func_done((event) => {
-    let payload = event.data;
+    const payload = event.data;
     assert_equals(payload.name, hasOpener ? channelName : "");
     assert_equals(payload.opener, hasOpener);
   });
 
-  let w = window.open(`${host.origin}/html/cross-origin-opener-policy/resources/coop_window.py?path=window.sub.html&coop=${escape(coop)}&channel=${channelName}`, channelName);
+  const w = window.open(`${host.origin}/html/cross-origin-opener-policy/resources/coop-coep.py?coop=${coop}&coep${coep}&channel=${channelName}`, channelName);
 
   // w will be closed by its postback iframe. When out of process,
   // window.close() does not work.
   t.add_cleanup(() => w.close());
 }
 
+function coop_test(t, host, coop, channelName, hasOpener) {
+  coop_coep_test(t, host, coop, "", channelName, hasOpener);
+}
+
 function run_coop_tests(mainTest, testArray) {
-  for (let test of tests) {
+  for (const test of tests) {
     async_test(t => {
       coop_test(t, test[0], test[1],
                 `${mainTest}_to_${test[0].name}_${test[1].replace(/ /g,"-")}`,
---
+++ html/cross-origin-opener-policy/resources/coop-coep.py
@@ -0,0 +1,24 @@
+def main(request, response):
+    coop = request.GET.first("coop", "same-origin")
+    coep = request.GET.first("coep", "require-corp")
+    if coop != "":
+        response.headers.set("Cross-Origin-Opener-Policy", coop)
+    if coep != "":
+        response.headers.set("Cross-Origin-Embedder-Policy", coep)
+
+    # This uses an <iframe> as BroadcastChannel is same-origin bound.
+    response.content = """
+<!doctype html>
+<meta charset=utf-8>
+<script src="/common/get-host-info.sub.js"></script>
+<iframe></iframe>
+<script>
+  const iframe = document.querySelector("iframe");
+  iframe.onload = () => {
+    const payload = { name: self.name, opener: !!self.opener };
+    iframe.contentWindow.postMessage(payload, "*");
+  };
+  const channelName = new URL(location).searchParams.get("channel");
+  iframe.src = `${get_host_info().HTTP_ORIGIN}/html/cross-origin-opener-policy/resources/postback.html?channel=${channelName}`;
+</script>
+"""

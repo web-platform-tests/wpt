c4fae85e7b976041cfefb9374ba12af5c09e95a0
Anne van Kesteren <annevk@annevk.nl>
2019-07-09 12:16:20 +0200

    HTML: cleanup [[CanBlock]] tests
    (These do not require COOP/COEP treatment as they test SharedArrayBuffer on a single thread, but still seems nice to reduce clutter.)

--- html/webappapis/scripting/processing-model-2/integration-with-the-javascript-agent-formalism/requires-failure.https.any.js
+++ html/webappapis/scripting/processing-model-2/integration-with-the-javascript-agent-formalism/requires-failure.https.any.js
@@ -1,11 +1,12 @@
-"use strict";
-importScripts("/resources/testharness.js");
+// META: global=!default,window,serviceworker
 
 test(() => {
   const sab = new SharedArrayBuffer(16);
   const ta = new Int32Array(sab);
 
-  assert_equals(Atomics.wait(ta, 0, 0, 10), "timed-out");
+  assert_throws(new TypeError(), () => {
+    Atomics.wait(ta, 0, 0, 10);
+  });
 }, `[[CanBlock]] in a ${self.constructor.name}`);
 
 done();
--- html/webappapis/scripting/processing-model-2/integration-with-the-javascript-agent-formalism/requires-success.any.js
+++ html/webappapis/scripting/processing-model-2/integration-with-the-javascript-agent-formalism/requires-success.any.js
@@ -1,13 +1,10 @@
-"use strict";
-importScripts("/resources/testharness.js");
+// META: global=!default,dedicatedworker,sharedworker
 
 test(() => {
   const sab = new SharedArrayBuffer(16);
   const ta = new Int32Array(sab);
 
-  assert_throws(new TypeError(), () => {
-    Atomics.wait(ta, 0, 0, 10);
-  });
+  assert_equals(Atomics.wait(ta, 0, 0, 10), "timed-out");
 }, `[[CanBlock]] in a ${self.constructor.name}`);
 
 done();

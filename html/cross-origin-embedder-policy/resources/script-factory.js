60bff626c565b1d212945ad615f95b7469d8145e
Anne van Kesteren <annevk@annevk.nl>
2019-08-09 14:37:22 +0200

    feedback

--- html/cross-origin-embedder-policy/resources/script-factory.js
+++ html/cross-origin-embedder-policy/resources/script-factory.js
@@ -2,21 +2,22 @@
 
 function createScript(sameOrigin, crossOrigin, parent="parent", id="") {
   return `<script>
-let i = 0;
 const data = { id: "${id}",
                origin: window.origin,
                sameOriginNoCORPSuccess: false,
                crossOriginNoCORPFailure: false };
-function report(field, success) {
-  i++;
-  data[field] = success;
-  if (i === 2 || "${sameOrigin}" === "null") {
-    window.${parent}.postMessage(data, "*");
-  }
+function record(promise, token, expectation) {
+  return promise.then(() => data[token] = expectation, () => data[token] = !expectation);
 }
+
+const records = [
+  record(fetch("${crossOrigin}/common/blank.html", { mode: "no-cors" }), "crossOriginNoCORPFailure", false)
+];
+
 if ("${sameOrigin}" !== "null") {
-  fetch("${sameOrigin}/common/blank.html", { mode: "no-cors" }).then(() => report("sameOriginNoCORPSuccess", true), () => report("sameOriginNoCORPSuccess", false));
+  records.push(record(fetch("${sameOrigin}/common/blank.html", { mode: "no-cors" }), "sameOriginNoCORPSuccess", true));
 }
-fetch("${crossOrigin}/common/blank.html", { mode: "no-cors" }).then(() => report("crossOriginNoCORPFailure", false), () => report("crossOriginNoCORPFailure", true));
+
+Promise.all(records).then(() => window.${parent}.postMessage(data, "*"));
 <\/script>`;
 }

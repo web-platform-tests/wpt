9eb651e347868eb5acb24a863da055973441e423
Yutaka Hirano <yhirano@chromium.org>
2019-08-21 12:45:27 +0900

    fix

--- html/cross-origin-embedder-policy/resources/sw.js
+++ html/cross-origin-embedder-policy/resources/sw.js
@@ -1,8 +1,8 @@
-self.addEventListener('activate', event => {
-  event.waitUntil(clients.claim());
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch', (e) => {
   const url = new URL(e.request.url);
   if (url.searchParams.has('passthrough')) {
     return;

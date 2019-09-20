0a0c032f9ec205306efbb7c72b6c9cf0d67b07a4
Philip Jägenstedt <philip@foolip.org>
2019-09-20 20:58:39 +0200

    [testharness.js] remove superfluous `test.done()` call (#19192)

--- resources/testharness.js
+++ resources/testharness.js
@@ -3384,9 +3384,6 @@ policies and contribution forms [3].
                 }
                 test.set_status(test.FAIL, e.message, stack);
                 test.phase = test.phases.HAS_RESULT;
-                // The following function invocation is superfluous.
-                // TODO: Remove.
-                test.done();
             } else if (!tests.allow_uncaught_exception) {
                 tests.status.status = tests.status.ERROR;
                 tests.status.message = e.message;

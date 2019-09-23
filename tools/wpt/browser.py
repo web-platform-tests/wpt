e8f8841e627df7381176ad77f3c8483a03f2c44d
Carlos Alberto Lopez Perez <clopez@igalia.com>
2019-09-23 19:43:17 +0200

    Add a webkitgtk_minibrowser product/browser.
     * The current "webkit" product is a bit of confusing because
       it is a too much generic name, and it can be used for
       running webkitgtk or wpewebkit based browsers (but not for
       safari/mac based ones)
    
     * This adds a webkitgtk_minibrowser product that makes much
       clearer what is the product about. It also defines default
       values for all variables that previously the user had to
       fill when running the "webkit" product. Now it simply
       suffices with running: "/wpt run webkitgtk_minibrowser"

--- tools/wpt/browser.py
+++ tools/wpt/browser.py
@@ -1104,6 +1104,38 @@ class WebKit(Browser):
         return None
 
 
+class WebKitGTKMiniBrowser(WebKit):
+
+    def find_binary(self, venv_path=None, channel=None):
+        libexecpaths = ["/usr/libexec/webkit2gtk-4.0",
+                        "/usr/lib/x86_64-linux-gnu/webkit2gtk-4.0"]
+        # If GCC is available use it to detect this machine triplet,
+        # and try to look into its multi-arch libexecdir.
+        gcc = find_executable("gcc")
+        if gcc:
+            try:
+                triplet = call(gcc, "-dumpmachine").strip()
+            except subprocess.CalledProcessError:
+                triplet = None
+            if triplet:
+                libexecpaths.insert(0, "/usr/lib/%s/webkit2gtk-4.0" % triplet)
+        return find_executable("MiniBrowser", os.pathsep.join(libexecpaths))
+
+    def find_webdriver(self, channel=None):
+        return find_executable("WebKitWebDriver")
+
+    def version(self, binary=None, webdriver_binary=None):
+        if binary is None:
+            return None
+        try: # WebKitGTK MiniBrowser before 2.26.0 doesn't support --version
+            output = call(binary, "--version").strip()
+        except subprocess.CalledProcessError:
+            output = None
+        if output:
+            return output.split()[1]
+        return None
+
+
 class Epiphany(Browser):
     """Epiphany-specific interface."""
 

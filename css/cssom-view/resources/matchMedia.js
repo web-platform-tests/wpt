dff41aa2e9fa7dc3a39a35f157a50ddc2413ed1b
Alexey Shvayka <shvaikalesh@gmail.com>
2019-10-24 16:38:14 +0300

    Remove "HARNESS ERROR" from error messages

--- css/cssom-view/resources/matchMedia.js
+++ css/cssom-view/resources/matchMedia.js
@@ -4,13 +4,13 @@ const iframes = new WeakMap();
 async function createMQL(t) {
     const iframe = await createIFrame(t);
     const mql = iframe.contentWindow.matchMedia(`(max-width: ${IFRAME_DEFAULT_SIZE}px)`);
-    assert_true(mql.matches, "HARNESS ERROR: invariant");
+    assert_true(mql.matches, "MQL should match on newly created <iframe>");
     iframes.set(mql, iframe);
     return mql;
 }
 
 function createIFrame(t, width = IFRAME_DEFAULT_SIZE, height = width) {
-    assert_not_equals(document.body, null, "HARNESS ERROR: <body> is missing");
+    assert_not_equals(document.body, null, "<body> element is missing");
 
     const iframe = document.createElement("iframe");
     iframe.srcdoc = "";
@@ -34,7 +34,7 @@ function createIFrame(t, width = IFRAME_DEFAULT_SIZE, height = width) {
 
 function triggerMQLEvent(mql) {
     const iframe = iframes.get(mql);
-    assert_not_equals(iframe, undefined, "HARNESS ERROR: passed MQL instance was not created with createMQL");
+    assert_not_equals(iframe, undefined, "Passed MQL instance was not created with createMQL");
     iframe.width = iframe.width === IFRAME_DEFAULT_SIZE ? "250" : IFRAME_DEFAULT_SIZE;
 }
 

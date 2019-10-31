9a755bccfc76dfba5726b483740d927d757b28b4
Kent Tamura <tkent@chromium.org>
2019-10-31 17:39:41 +0900

    nullable

--- html/semantics/forms/form-submission-0/SubmitEvent.window.js
+++ html/semantics/forms/form-submission-0/SubmitEvent.window.js
@@ -4,7 +4,6 @@ test(() => {
   let button = document.createElement('button');
   let typeError = new TypeError();
   assert_throws(typeError, () => { new SubmitEvent() }, '0 arguments');
-  assert_throws(typeError, () => { new SubmitEvent('foo') }, '1 argument');
   assert_throws(typeError, () => { new SubmitEvent('bar', button) }, '1 invalid arguments');
   assert_throws(typeError, () => { new SubmitEvent(button, button) }, '2 invalid arguments');
   assert_throws(typeError, () => { new SubmitEvent('foo', null) }, 'Null dictionary');
@@ -20,3 +19,18 @@ test(() => {
   assert_equals(event.submitter, button);
   assert_true(event.bubbles);
 }, 'Successful SubmitEvent constructor');
+
+test(() => {
+  let event = new SubmitEvent('bar', { submitter: null});
+  assert_equals(event.submitter, null);
+}, 'Successful SubmitEvent constructor; null submitter');
+
+test(() => {
+  let event = new SubmitEvent('baz', {});
+  assert_equals(event.submitter, null);
+}, 'Successful SubmitEvent constructor; empty dictionary');
+
+test(() => {
+  let event = new SubmitEvent('baz');
+  assert_equals(event.submitter, null);
+}, 'Successful SubmitEvent constructor; missing dictionary');

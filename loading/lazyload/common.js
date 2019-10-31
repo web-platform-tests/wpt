2f6fcd2600b7c386922fcb475548e34cc0632059
Rob Buis <rbuis@igalia.com>
2019-10-31 02:02:00 -0400

    Address review comments

--- loading/lazyload/common.js
+++ loading/lazyload/common.js
@@ -26,6 +26,6 @@ function is_image_fully_loaded(image) {
   let data = canvasContext.getImageData(0, 0, canvas.width, canvas.height).data;
 
   // Fully loaded image should not be a placeholder which is drawn as a
-  // translucent gray rectangle in placeholder_image.cc
+  // translucent gray rectangle.
   return data[0] != 0xd9 || data[1] != 0xd9 || data[2] != 0xd9;
 }

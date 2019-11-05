1e8b6355cb7391944a8ef246de5e525249d5b8cd
Mike Pennisi <mike@mikepennisi.com>
2019-11-04 21:35:05 -0500

    fixup! [xhr] Promote determinism in test for invalid HTTP

--- xhr/resources/echo-method.py
+++ xhr/resources/echo-method.py
@@ -1,7 +1,7 @@
 # This handler is designed to verify that UAs correctly discard the body of
 # responses to HTTP HEAD requests. If the response body is written to a
 # separate TCP packet, then this behavior cannot be verified. This handler uses
-# the response writer to ensure that the body is tranmitted in the same packet
+# the response writer to ensure that the body is transmitted in the same packet
 # as the headers. In this way, non-conforming UAs will consistently fail the
 # associated test.
 

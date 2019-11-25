d842401f3374ba9ec092517ba4e59a15792d58ce
Nina Satragno <nsatragno@gmail.com>
2019-11-25 14:47:49 -0500

    Reject promise with the correct error on fail.

--- tools/wptrunner/wptrunner/testdriver-extra.js
+++ tools/wptrunner/wptrunner/testdriver-extra.js
@@ -19,7 +19,7 @@
             result = JSON.parse(data.message).result
             pending_resolve(result);
         } else {
-            pending_reject(data.status);
+            pending_reject({ status: data.status, message: data.message });
         }
     });
 

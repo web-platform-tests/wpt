6dcb2628cf625eccef11ac7adc2304720aa64bde
Nina Satragno <nsatragno@gmail.com>
2019-11-26 15:59:41 -0500

    Silence unused import error

--- tools/wptrunner/wptrunner/tests/__init__.py
+++ tools/wptrunner/wptrunner/tests/__init__.py
@@ -4,4 +4,4 @@ import sys
 here = os.path.abspath(os.path.split(__file__)[0])
 sys.path.insert(0, os.path.join(here, os.pardir, os.pardir, os.pardir))
 
-import localpaths
+import localpaths as _localpaths  # noqa: F401

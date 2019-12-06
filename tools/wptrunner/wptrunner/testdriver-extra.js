955947afbc12ff9607d5e07a495c652050a3b235
Nina Satragno <nsatragno@gmail.com>
2019-12-06 11:13:06 -0500

    Add more descriptive error message.

--- tools/wptrunner/wptrunner/testdriver-extra.js
+++ tools/wptrunner/wptrunner/testdriver-extra.js
@@ -19,7 +19,7 @@
             result = JSON.parse(data.message).result
             pending_resolve(result);
         } else {
-            pending_reject({ status: data.status, message: data.message });
+            pending_reject(`${data.status}: ${data.message}`);
         }
     });
 

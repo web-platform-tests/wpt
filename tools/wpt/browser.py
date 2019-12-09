26b1e29e62645479f95e29dcd273ca24bce417be
James Graham <james@hoppipolla.co.uk>
2019-12-09 11:54:18 +0000

    Fix installing servo nightly

--- tools/wpt/browser.py
+++ tools/wpt/browser.py
@@ -1108,7 +1108,7 @@ class Servo(Browser):
 
         _, _, decompress = self.platform_components()
 
-        resp = self._get(dest, channel)
+        resp = self._get(channel)
         decompress(resp.raw, dest=dest)
         path = find_executable("servo", os.path.join(dest, "servo"))
         st = os.stat(path)

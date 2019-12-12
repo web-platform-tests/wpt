04e428e0bc4d2dbefd9dec0393be4cc7fac3a055
Anne van Kesteren <annevk@annevk.nl>
2019-12-12 17:28:55 +0100

    proof there are only two objects involved, part two

--- html/semantics/embedded-content/the-iframe-element/cross-origin-to-whom-part-2.window.js
+++ html/semantics/embedded-content/the-iframe-element/cross-origin-to-whom-part-2.window.js
@@ -7,19 +7,33 @@ async_test(t => {
     "iframe": ["getSVGDocument", "contentDocument"],
     "object": ["getSVGDocument", "contentDocument"]
   };
-  function assert_apis(instance) {
+  function element_to_document(element) {
+     const api = elements[element.localName][0];
+     if (api == "getSVGDocument") {
+        return element[api]();
+     }
+     return element[api];
+  }
+  function assert_apis(instance, assertNull = false) {
     const name = instance.localName;
     let priorPossibleDocument = null;
     elements[name].forEach(api => {
+      const assertReason = `${name}[${api}]`;
       let possibleDocument = null;
       if (api == "getSVGDocument") {
         possibleDocument = instance[api]();
       } else {
         possibleDocument = instance[api];
       }
-      assert_not_equals(possibleDocument, null, `${name}[${api}]`);
-      // This needs standardizing still
-      // assert_class_string(possibleDocument, "XMLDocument");
+      if (assertNull) {
+        assert_equals(possibleDocument, null, assertReason);
+        return;
+      } else {
+        assert_not_equals(possibleDocument, null, assertReason);
+
+        // This needs standardizing still
+        // assert_class_string(possibleDocument, "XMLDocument");
+      }
 
       // Ensure getSVGDocument() and contentDocument if both available return the same
       if (priorPossibleDocument === null) {
@@ -32,9 +46,13 @@ async_test(t => {
   frame.onload = t.step_func_done(() => {
     const instances = Object.keys(elements).map(element => frame.contentDocument.querySelector(element));
     instances.forEach(instance => assert_apis(instance));
+    instances.forEach(instance => {
+      const svgDocument = element_to_document(instance);
+      svgDocument.domain = svgDocument.domain;
+    });
     document.domain = document.domain;
     assert_equals(frame.contentDocument, null);
-    instances.forEach(instance => assert_apis(instance));
+    instances.forEach(instance => assert_apis(instance, true));
   });
   document.body.appendChild(frame);
-}, "Test embed/frame/iframe/object nested document APIs for same origin-domain and cross origin-domain current settings object");
+}, "Test embed/frame/iframe/object nested document APIs for same origin-domain and cross origin-domain embedder document");

13c145a6435c70d783f14b9c098e4f8f54f99878
James Graham <james@hoppipolla.co.uk>
2019-12-13 14:17:30 +0000

    Factor out code for building servo command line

--- tools/wptrunner/wptrunner/executors/executorservo.py
+++ tools/wptrunner/wptrunner/executors/executorservo.py
@@ -35,6 +35,30 @@ def write_hosts_file(config):
     return hosts_path
 
 
+def build_servo_command(test, test_url_func, browser, binary, pause_after_test, debug_info,
+                        extra_args=None, debug_opts="replace-surrogates"):
+    args = [
+        "--hard-fail", "-u", "Servo/wptrunner",
+        "-z", test_url_func(test),
+    ]
+    if debug_opts:
+        args += ["-Z", debug_opts]
+    for stylesheet in browser.user_stylesheets:
+        args += ["--user-stylesheet", stylesheet]
+    for pref, value in test.environment.get('prefs', {}).iteritems():
+        args += ["--pref", "%s=%s" % (pref, value)]
+    if browser.ca_certificate_path:
+        args += ["--certificate-path", browser.ca_certificate_path]
+    if extra_args:
+        args += extra_args
+    args += browser.binary_args
+    debug_args, command = browser_command(binary, args, debug_info)
+    if pause_after_test:
+        command.remove("-z")
+    return debug_args + command
+
+
+
 class ServoTestharnessExecutor(ProcessTestExecutor):
     convert_result = testharness_result_converter
 
@@ -60,25 +84,12 @@ class ServoTestharnessExecutor(ProcessTestExecutor):
         self.result_data = None
         self.result_flag = threading.Event()
 
-        args = [
-            "--hard-fail", "-u", "Servo/wptrunner",
-            "-Z", "replace-surrogates", "-z", self.test_url(test),
-        ]
-        for stylesheet in self.browser.user_stylesheets:
-            args += ["--user-stylesheet", stylesheet]
-        for pref, value in test.environment.get('prefs', {}).iteritems():
-            args += ["--pref", "%s=%s" % (pref, value)]
-        if self.browser.ca_certificate_path:
-            args += ["--certificate-path", self.browser.ca_certificate_path]
-        args += self.browser.binary_args
-        debug_args, command = browser_command(self.binary, args, self.debug_info)
-
-        self.command = command
-
-        if self.pause_after_test:
-            self.command.remove("-z")
-
-        self.command = debug_args + self.command
+        self.command = build_servo_command(test,
+                                           self.test_url,
+                                           self.browser,
+                                           self.binary,
+                                           self.pause_after_test,
+                                           self.debug_info)
 
         env = os.environ.copy()
         env["HOST_FILE"] = self.hosts_path
@@ -195,37 +206,23 @@ class ServoRefTestExecutor(ProcessTestExecutor):
         ProcessTestExecutor.teardown(self)
 
     def screenshot(self, test, viewport_size, dpi):
-        full_url = self.test_url(test)
-
         with TempFilename(self.tempdir) as output_path:
-            debug_args, command = browser_command(
-                self.binary,
-                [
-                    "--hard-fail", "--exit",
-                    "-u", "Servo/wptrunner",
-                    "-Z", "disable-text-aa,load-webfonts-synchronously,replace-surrogates",
-                    "--output=%s" % output_path, full_url
-                ] + self.browser.binary_args,
-                self.debug_info)
-
-            for stylesheet in self.browser.user_stylesheets:
-                command += ["--user-stylesheet", stylesheet]
-
-            for pref, value in test.environment.get('prefs', {}).iteritems():
-                command += ["--pref", "%s=%s" % (pref, value)]
-
-            command += ["--resolution", viewport_size or "800x600"]
-
-            if self.browser.ca_certificate_path:
-                command += ["--certificate-path", self.browser.ca_certificate_path]
+            extra_args = ["--exit",
+                          "--output=%s" % output_path,
+                          "--resolution", viewport_size or "800x600"]
+            debug_opts = "disable-text-aa,load-webfonts-synchronously,replace-surrogates"
 
             if dpi:
-                command += ["--device-pixel-ratio", dpi]
+                extra_args += ["--device-pixel-ratio", dpi]
 
-            # Run ref tests in headless mode
-            command += ["-z"]
-
-            self.command = debug_args + command
+            self.command = build_servo_command(test,
+                                               self.test_url,
+                                               self.browser,
+                                               self.binary,
+                                               False,
+                                               self.debug_info,
+                                               extra_args,
+                                               debug_opts)
 
             env = os.environ.copy()
             env["HOST_FILE"] = self.hosts_path
@@ -327,8 +324,9 @@ class ServoCrashtestExecutor(ProcessTestExecutor):
                    else None)
 
         test_url = self.test_url(test)
-        self.prefs =  test.environment.get('prefs', {})
-
+        # We want to pass the full test object into build_servo_command,
+        # so stash it in the class
+        self.test = test
         success, data = ServoTimedRunner(self.logger, self.do_crashtest, self.protocol,
                                          test_url, timeout, self.extra_timeout).run()
 
@@ -342,27 +340,13 @@ class ServoCrashtestExecutor(ProcessTestExecutor):
         env["HOST_FILE"] = self.hosts_path
         env["RUST_BACKTRACE"] = "1"
 
-        args = [
-            "--hard-fail", "-u", "Servo/wptrunner",
-            "-x",
-            "-Z", "replace-surrogates", "-z", url,
-        ]
-        for stylesheet in self.browser.user_stylesheets:
-            args += ["--user-stylesheet", stylesheet]
-        for pref, value in self.prefs.iteritems():
-            args += ["--pref", "%s=%s" % (pref, value)]
-        if self.browser.ca_certificate_path:
-            args += ["--certificate-path", self.browser.ca_certificate_path]
-        args += self.browser.binary_args
-        debug_args, command = browser_command(self.binary, args, self.debug_info)
-
-        command = command
-
-        if self.pause_after_test:
-            command.remove("-z")
-
-        command = debug_args + command
-
+        command = build_servo_command(self.test,
+                                      self.test_url,
+                                      self.browser,
+                                      self.binary,
+                                      False,
+                                      self.debug_info,
+                                      extra_args=["-x"])
 
         if not self.interactive:
             self.proc = ProcessHandler(command,

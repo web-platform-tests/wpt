bda9359245a79ea8dbe91d919023b7fa5ae595a3
James Graham <james@hoppipolla.co.uk>
2020-01-07 17:31:17 +0000

    Make assert_array_equals message maximise the size of array slice
    When the output array is longer than the maximum size, try to choose a
    slice centred on the offset with the maximum possible size. For
    example if the array has N_MAX + 10 elements and the offset is 2 from
    the end, we choose the final 2 elements, plus the offset plus the
    N_MAX - 3 elements before the offset. If the offset is in the middle
    of the same array, we take N_MAX/2 elements before and after the
    offset.

--- resources/testharness.js
+++ resources/testharness.js
@@ -1186,22 +1186,32 @@ policies and contribution forms [3].
 
     function assert_array_equals(actual, expected, description)
     {
-        const shorter_array_length_limit = 20;
+        const max_array_length = 20;
         function shorten_array(arr, offset = 0) {
             // Make ", …" only show up when it would likely reduce the length, not accounting for
             // fonts.
-            if (arr.length < shorter_array_length_limit + 2) {
+            if (arr.length < max_array_length + 2) {
                 return arr;
             }
-            const shorterArrayEnd = shorter_array_length_limit + offset;
-            const shorterArray = arr.slice(offset, shorterArrayEnd);
-            if (offset !== 0) {
-                shorterArray.beginEllipsis = true;
+            // By default we want half the elements after the offset and half before
+            // But if that takes us past the end of the array, we have more before, and
+            // if it takes us before the start we have more after.
+            const length_after_offset = Math.floor(max_array_length / 2);
+            let upper_bound = Math.min(length_after_offset + offset, arr.length);
+            const lower_bound = Math.max(upper_bound - max_array_length, 0);
+
+            if (lower_bound === 0) {
+                upper_bound = max_array_length;
             }
-            if (arr.length > shorterArrayEnd) {
-                shorterArray.endEllipsis = true;
+
+            const output = arr.slice(lower_bound, upper_bound);
+            if (lower_bound > 0) {
+                output.beginEllipsis = true;
             }
-            return shorterArray;
+            if (upper_bound < arr.length) {
+                output.endEllipsis = true;
+            }
+            return output;
         }
 
         assert(typeof actual === "object" && actual !== null && "length" in actual,
@@ -1210,21 +1220,23 @@ policies and contribution forms [3].
                {actual:actual});
         assert(actual.length === expected.length,
                "assert_array_equals", description,
-               "lengths differ, expected array ${expected} got ${actual}",
-               {expected:shorten_array(expected), actual:shorten_array(actual)});
+               "lengths differ, expected array ${expected} length ${expectedLength}, got ${actual} length ${actualLength}",
+               {expected:shorten_array(expected, expected.length - 1), expectedLength:expected.length,
+                actual:shorten_array(actual, actual.length - 1), actualLength:actual.length
+               });
 
         for (var i = 0; i < actual.length; i++) {
             assert(actual.hasOwnProperty(i) === expected.hasOwnProperty(i),
                    "assert_array_equals", description,
                    "expected property ${i} to be ${expected} but was ${actual} (expected array ${arrayExpected} got ${arrayActual})",
                    {i:i, expected:expected.hasOwnProperty(i) ? "present" : "missing",
-                   actual:actual.hasOwnProperty(i) ? "present" : "missing",
-                   arrayExpected:shorten_array(expected, i), arrayActual:shorten_array(actual, i)});
+                    actual:actual.hasOwnProperty(i) ? "present" : "missing",
+                    arrayExpected:shorten_array(expected, i), arrayActual:shorten_array(actual, i)});
             assert(same_value(expected[i], actual[i]),
                    "assert_array_equals", description,
                    "expected property ${i} to be ${expected} but got ${actual} (expected array ${arrayExpected} got ${arrayActual})",
                    {i:i, expected:expected[i], actual:actual[i],
-                   arrayExpected:shorten_array(expected, i), arrayActual:shorten_array(actual, i)});
+                    arrayExpected:shorten_array(expected, i), arrayActual:shorten_array(actual, i)});
         }
     }
     expose(assert_array_equals, "assert_array_equals");
@@ -1244,7 +1256,7 @@ policies and contribution forms [3].
                    "assert_array_approx_equals", description,
                    "property ${i}, property expected to be ${expected} but was ${actual}",
                    {i:i, expected:expected.hasOwnProperty(i) ? "present" : "missing",
-                   actual:actual.hasOwnProperty(i) ? "present" : "missing"});
+                    actual:actual.hasOwnProperty(i) ? "present" : "missing"});
             assert(typeof actual[i] === "number",
                    "assert_array_approx_equals", description,
                    "property ${i}, expected a number but got a ${type_actual}",

bc00fa7c11a4ab27b4864093adebcbabee39b9bd
Ulan Degenbaev <ulan@chromium.org>
2020-02-12 07:29:01 -0800

    Update performance.measureMemory to the latest proposal
    This changes the result format of the API to the latest version of
    the proposal at https://github.com/ulan/performance-measure-memory.
    Specifically, the result format changes from JS only and per-frame:
    {
      total: {
        jsMemoryEstimate: 200*MB,
        jsMemoryRange: [100*MB, 300*MB]
      },
      current: {..},
      other: [..]
    }
    
    to more a generic format with breakdown:
    {
      bytes: 70*MB,
      breakdown: [
        {bytes: 40*MB, globals: 2, type: 'js', origins: ['foo.com']},
        {bytes: 30*MB, globals: 1, type: 'js', origins: ['bar.com']}
      ]
    }
    
    Additionally this patch skips extension contexts and relaxes
    cross-origin memory measurement for site-isolated pages.
    
    Bug: 1049093
    Change-Id: I997bfe9f6008eaedd0ce5010839232ef3540cad0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2047028
    Commit-Queue: Ulan Degenbaev <ulan@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740671}

--- measure-memory/measure-memory-same-origin-iframe.tentative.window.js
+++ measure-memory/measure-memory-same-origin-iframe.tentative.window.js
@@ -3,6 +3,10 @@
 'use strict';
 
 promise_test(async testCase => {
+  const frame = document.createElement("iframe");
+  const path = new URL("resources/iframe.sub.html", window.location).pathname;
+  frame.src = `${SAME_ORIGIN}${path}`;
+  document.body.append(frame);
   try {
     let result = await performance.measureMemory();
     checkMeasureMemory(result);
@@ -12,5 +16,4 @@ promise_test(async testCase => {
     }
     assert_equals(error.name, 'SecurityError');
   }
-}, 'Well-formed result of performance.measureMemory.');
-
+}, 'Well-formed result of performance.measureMemory with same-origin iframe.');

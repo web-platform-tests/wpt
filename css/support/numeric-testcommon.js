b6d2224b06954e350a41dbae0c45ad25e178a8dd
Tab Atkins-Bittner <jackalmage@gmail.com>
2020-02-14 14:23:02 -0800

    Improve the documentation for numeric-testcommon.js.

--- css/support/numeric-testcommon.js
+++ css/support/numeric-testcommon.js
@@ -1,20 +1,47 @@
 'use strict';
 
 /*
-Tests to verify that numeric values
-(math functions, generally),
-are handled correctly.
+Provides functions to help test that two numeric values are equivalent.
+These *do not* rely on you predicting what one value will serialize to;
+instead, they set and serialize *both* values,
+and just ensure that they serialize to the same thing.
 
-Relies on a #target element existing in the document,
+They rely on a #target element existing in the document,
 as this might rely on layout to resolve styles,
 and so it needs to be in the document.
+
+Two main functions are defined, with the same signatures:
+test_math_used() (for testing used values)
+and test_math_computed() (for testing computed values).
+Signature for both is:
+
+test_math_X(
+    testString, // A numeric value; required.
+    expectedString, // A hopefully-equivalent numeric value; required.
+    { // all of these are optional
+        type, // "number", "length", etc. See impl for full list. Defaults to "length".
+        base, // a valid value that neither test nor expected should serialize to.
+              // Defaults to an appropriate value for the given 'type'.
+        msg, // The message to display for the test; autogenned if not provided.
+        msgExtra, // Extra info to put after the auto-genned message.
+        prop, // If you want to override the automatic choice of tested property.
+        extraStyle, // Styles that need to be set at the same time to properly test the value.
+    }
+);
+
+
+Additionally, five specialized functions are provided
+to test that a given value is ±∞, ±0, or NaN:
+
+* test_plus_infinity(testString)
+* test_minus_infinity(testString)
+* test_plus_zero(testString)
+* test_minus_zero(testString)
+* test_nan(testString)
+
 */
 
 
-/*
-By default, assumes testString evaluates to a <length>.
-If this isn't true, override {base, prop} accordingly.
-*/
 
 function test_math_used(testString, expectedString, {base, msg, msgExtra, prop, type, extraStyle={}}={}) {
     if(type === undefined) type = "length";
@@ -98,14 +125,14 @@ function test_nan(testString) {
 }
 
 
-function _test_math({stage, testEl, testString, expectedString, base, msg, msgExtra, prop, prefix, suffix, extraStyles}={}) {
+function _test_math({stage, testEl, testString, expectedString, base, msg, msgExtra, prop, prefix, suffix, extraStyle}={}) {
     // Find the test element
     if(!testEl) testEl = document.getElementById('target');
     if(testEl == null) throw "Couldn't find #target element to run tests on.";
     // Then reset its styles
     testEl.style = "";
-    for(const p in extraStyles) {
-        testEl.style[p] = extraStyles[p];
+    for(const p in extraStyle) {
+        testEl.style[p] = extraStyle[p];
     }
     if(!msg) {
         msg = `${testString} should be ${stage}-value-equivalent to ${expectedString}`;

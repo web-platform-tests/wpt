194b140aa8402bf09f118db286619cd4aa155953
Sergio Villar Senin <svillar@igalia.com>
2020-02-20 10:55:03 +0100

    AnyStr->Text

--- tools/manifest/utils.py
+++ tools/manifest/utils.py
@@ -57,7 +57,7 @@ def to_os_path(path):
 def git(path):
     # type: (bytes) -> Optional[Callable[..., bytes]]
     def gitfunc(cmd, *args):
-        # type: (bytes, *bytes) -> AnyStr
+        # type: (bytes, *bytes) -> Text
         full_cmd = ["git", cmd] + list(args)
         try:
             return subprocess.check_output(full_cmd, cwd=path, stderr=subprocess.STDOUT).decode('utf8')

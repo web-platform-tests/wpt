ab12b5de7d680516bec5f1fde0581b0eb9aef5d7
Sergio Villar Senin <svillar@igalia.com>
2020-03-10 17:07:50 +0100

    xfail for Windows and python3

--- tools/wpt/tests/test_wpt.py
+++ tools/wpt/tests/test_wpt.py
@@ -1,5 +1,6 @@
 import errno
 import os
+import platform
 import shutil
 import socket
 import subprocess
@@ -43,8 +44,12 @@ def get_persistent_manifest_path():
 
 @pytest.fixture(scope="module", autouse=True)
 def init_manifest():
+    # See https://bugzilla.mozilla.org/show_bug.cgi?id=1621226
     if sys.version_info >= (3,8):
         pytest.xfail(reason="mozbase does not completely support python >= 3.8")
+    # See https://github.com/pypa/virtualenv/issues/1710
+    if sys.version_info[0] >= 3 and platform.system() == "Windows":
+        pytest.xfail(reason="virtualenv activation fails in Windows for python3")
     with pytest.raises(SystemExit) as excinfo:
         wpt.main(argv=["manifest", "--no-download",
                        "--path", get_persistent_manifest_path()])

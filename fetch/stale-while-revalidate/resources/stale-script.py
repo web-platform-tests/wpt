d534b0e500342d977253f369ace84da5c89f0060
Rob Buis <rbuis@igalia.com>
2020-03-17 09:37:30 +0100

    Add test to verify that frame removal triggers revalidation cancellation
    Add test to verify that frame removal triggers revalidation
    cancellation.

--- fetch/stale-while-revalidate/resources/stale-script.py
+++ fetch/stale-while-revalidate/resources/stale-script.py
@@ -1,12 +1,35 @@
-import random, string, datetime
+import random, string, datetime, time
 
 def id_token():
    letters = string.ascii_lowercase
    return ''.join(random.choice(letters) for i in range(20))
 
 def main(request, response):
+    is_revalidation = request.headers.get("If-None-Match", None)
+    if is_revalidation is not None:
+      time.sleep(3);
     token = request.GET.first("token", None)
     is_query = request.GET.first("query", None) != None
+    query_delay = int(request.GET.first("query_delay", '0'))
+    if is_query and query_delay > 0:
+      time.sleep(query_delay);
+
+    if not is_query:
+      unique_id = id_token()
+      response.add_required_headers = False
+      response.writer.write_status(200)
+      response.writer.write_header("Content-Type", "text/javascript")
+      response.writer.write_header("Cache-Control", "private, max-age=0, stale-while-revalidate=60")
+      response.writer.write_header("ETag", "swr")
+      response.writer.write_header("Unique-Id", unique_id)
+      response.writer.end_headers()
+      content = "report('{}')".format(unique_id)
+      response.writer.write(content)
+      success = response.writer.flush();
+      # revalidation should be canceled and connection closed
+      if is_revalidation and not success:
+        return;
+
     with request.server.stash.lock:
       value = request.server.stash.take(token)
       count = 0
@@ -22,11 +45,4 @@ def main(request, response):
     if is_query:
       headers = [("Count", count)]
       content = ""
-      return 200, headers, content
-    else:
-      unique_id = id_token()
-      headers = [("Content-Type", "text/javascript"),
-                 ("Cache-Control", "private, max-age=0, stale-while-revalidate=60"),
-                 ("Unique-Id", unique_id)]
-      content = "report('{}')".format(unique_id)
       return 200, headers, content

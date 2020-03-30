b35aa312285f578b357908650303ed5502dfc740
Anne van Kesteren <annevk@annevk.nl>
2020-03-30 15:09:43 +0200

    cover variant

--- dom/nodes/adoption.window.js
+++ dom/nodes/adoption.window.js
@@ -1,10 +1,15 @@
 // Testing DocumentFragment with host separately as it has a different node document by design
-test(() => {
+function create(doc, localName = "div") {
+  return doc.createElementNS("http://www.w3.org/1999/xhtml", localName);
+}
+
+test(t => {
   const df = document.createElement("template").content;
   const child = df.appendChild(new Text('hi'));
   assert_not_equals(df.ownerDocument, document);
   const nodeDocument = df.ownerDocument;
   document.body.appendChild(df);
+  t.add_cleanup(() => child.remove());
   assert_equals(df.childNodes.length, 0);
   assert_equals(child.ownerDocument, document);
   assert_equals(df.ownerDocument, nodeDocument);
@@ -27,16 +32,16 @@ test(() => {
   },
   {
     "name": "ShadowRoot",
-    "creator": doc => doc.createElementNS("http://www.w3.org/1999/xhtml", "div").attachShadow({mode: "closed"})
+    "creator": doc => create(doc).attachShadow({mode: "closed"})
   }
 ].forEach(({ name, creator }) => {
-  test(() => {
+  test(t => {
     const doc = new Document();
     const df = creator(doc);
     const child = df.appendChild(new Text('hi'));
     assert_equals(df.ownerDocument, doc);
-
     document.body.appendChild(df);
+    t.add_cleanup(() => child.remove());
     assert_equals(df.childNodes.length, 0);
     assert_equals(child.ownerDocument, document);
     if (name === "ShadowRoot") {
@@ -60,3 +65,18 @@ test(() => {
     }
   }, `adoptNode() and ${name}`);
 });
+
+test(t => {
+  const doc = new Document();
+  const host = create(doc);
+  const shadow = host.attachShadow({mode: "closed"});
+  const childHost = shadow.appendChild(create(doc, "body"));
+  const childShadow = childHost.attachShadow({mode: "closed"});
+  const descendant = childShadow.appendChild(new Text("hi"));
+  document.body.appendChild(shadow);
+  t.add_cleanup(() => childHost.remove());
+  assert_equals(shadow.childNodes.length, 0);
+  assert_equals(childHost.ownerDocument, document);
+  assert_equals(childShadow.ownerDocument, document);
+  assert_equals(descendant.ownerDocument, document);
+}, "Nested ShadowRoots");

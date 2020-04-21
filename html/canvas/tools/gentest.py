7d64bdc078837e172ef09cb371efb6aa4a31fafa
Aaron Krajeski <krajeski@gmail.com>
2020-04-21 10:13:56 -0400

    Don't generate into dom-element

--- html/canvas/tools/gentest.py
+++ html/canvas/tools/gentest.py
@@ -1,4 +1,4 @@
 from gentestutils import genTestUtils
 
-genTestUtils('../dom-element', '../dom-element', 'templates.yaml', 'name2dir.yaml', False)
+genTestUtils('../element', '../element', 'templates.yaml', 'name2dir.yaml', False)
 genTestUtils('../offscreen', '../offscreen', 'templates-offscreen.yaml', 'name2dir-offscreen.yaml', True)

dadd0856c672d47dabcbbea23d58d028a99285b1
Aaron Krajeski <krajeski@gmail.com>
2020-04-28 08:09:18 -0400

    Remove unused files from the build

--- update-built-tests.sh
+++ update-built-tests.sh
@@ -4,7 +4,6 @@ set -ex
 html/canvas/tools/build.sh
 infrastructure/assumptions/tools/build.sh
 html/tools/build.sh
-offscreen-canvas/tools/build.sh
 python mimesniff/mime-types/resources/generated-mime-types.py
 python3 css/css-ui/tools/appearance-build-webkit-reftests.py
 python3 WebIDL/tools/generate-setlike.py

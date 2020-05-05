1a2dfa7b9f5cd5777ba4214656bb22039a5a92fa
Ms2ger <Ms2ger@gmail.com>
2020-05-05 18:01:01 +0200

    More tests for the order of operations in constructors.

--- wasm/jsapi/memory/constructor.any.js
+++ wasm/jsapi/memory/constructor.any.js
@@ -121,6 +121,171 @@ test(() => {
 }, "Order of evaluation for descriptor");
 
 test(() => {
+  const order = [];
+
+  assert_throws_js(TypeError, () => {
+    new WebAssembly.Memory({
+      get maximum() {
+        order.push("maximum");
+        return {
+          valueOf() {
+            order.push("maximum valueOf");
+            return 1;
+          },
+        };
+      },
+
+      get initial() {
+        order.push("initial");
+        return undefined;
+      },
+    });
+  });
+
+  assert_array_equals(order, [
+    "initial",
+  ]);
+}, "Order of evaluation for descriptor (initial=undefined)");
+
+test(() => {
+  const order = [];
+
+  assert_throws_js(TypeError, () => {
+    new WebAssembly.Memory({
+      get maximum() {
+        order.push("maximum");
+        return {
+          valueOf() {
+            order.push("maximum valueOf");
+            return 0x100000000;
+          },
+        };
+      },
+
+      get initial() {
+        order.push("initial");
+        return {
+          valueOf() {
+            order.push("initial valueOf");
+            return 1;
+          },
+        };
+      },
+    });
+  });
+
+  assert_array_equals(order, [
+    "initial",
+    "initial valueOf",
+    "maximum",
+    "maximum valueOf",
+  ]);
+}, "Order of evaluation for descriptor (maximum out of range)");
+
+test(() => {
+  const order = [];
+
+  assert_throws_js(RangeError, () => {
+    new WebAssembly.Memory({
+      get maximum() {
+        order.push("maximum");
+        return {
+          valueOf() {
+            order.push("maximum valueOf");
+            return 0x10001;
+          },
+        };
+      },
+
+      get initial() {
+        order.push("initial");
+        return {
+          valueOf() {
+            order.push("initial valueOf");
+            return 1;
+          },
+        };
+      },
+    });
+  });
+
+  assert_array_equals(order, [
+    "initial",
+    "initial valueOf",
+    "maximum",
+    "maximum valueOf",
+  ]);
+}, "Order of evaluation for descriptor (maximum too large)");
+
+test(() => {
+  const order = [];
+
+  assert_throws_js(TypeError, () => {
+    new WebAssembly.Memory({
+      get maximum() {
+        order.push("maximum");
+        return {
+          valueOf() {
+            order.push("maximum valueOf");
+            return 1;
+          },
+        };
+      },
+
+      get initial() {
+        order.push("initial");
+        return {
+          valueOf() {
+            order.push("initial valueOf");
+            return 0x100000000;
+          },
+        };
+      },
+    });
+  });
+
+  assert_array_equals(order, [
+    "initial",
+    "initial valueOf",
+  ]);
+}, "Order of evaluation for descriptor (initial out of range)");
+
+test(() => {
+  const order = [];
+
+  assert_throws_js(RangeError, () => {
+    new WebAssembly.Memory({
+      get maximum() {
+        order.push("maximum");
+        return {
+          valueOf() {
+            order.push("maximum valueOf");
+            return 1;
+          },
+        };
+      },
+
+      get initial() {
+        order.push("initial");
+        return {
+          valueOf() {
+            order.push("initial valueOf");
+            return 0x10001;
+          },
+        };
+      },
+    });
+  });
+
+  assert_array_equals(order, [
+    "initial",
+    "initial valueOf",
+    "maximum",
+    "maximum valueOf",
+  ]);
+}, "Order of evaluation for descriptor (initial too large)");
+
+test(() => {
   const argument = { "initial": 0 };
   const memory = new WebAssembly.Memory(argument);
   assert_Memory(memory, { "size": 0 });

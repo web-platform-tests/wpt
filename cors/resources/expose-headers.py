05dd863caf60f756b1a970f3fd4e0b1d00dfa324
Ziran Sun <zsun@igalia.com>
2020-05-22 11:08:59 +0100

    Python 3: port tests in cors

--- cors/resources/expose-headers.py
+++ cors/resources/expose-headers.py
@@ -1,10 +1,10 @@
 def main(request, response):
     response.add_required_headers = False
-    output =  "HTTP/1.1 221 ALL YOUR BASE BELONG TO H1\r\n"
-    output += "Access-Control-Allow-Origin: *\r\n"
-    output += "BB-8: hey\r\n"
-    output += "Content-Language: mkay\r\n"
-    output += request.GET.first("expose") + "\r\n"
-    output += "\r\n"
+    output =  u"HTTP/1.1 221 ALL YOUR BASE BELONG TO H1\r\n"
+    output += u"Access-Control-Allow-Origin: *\r\n"
+    output += u"BB-8: hey\r\n"
+    output += u"Content-Language: mkay\r\n"
+    output += request.GET.first(b"expose").decode("iso-8859-1") + u"\r\n"
+    output += u"\r\n"
     response.writer.write(output)
     response.close_connection = True

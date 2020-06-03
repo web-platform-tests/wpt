4d55f7b22d6bb957009a60a7ac0f0d0ce01bc92b
Anne van Kesteren <annevk@annevk.nl>
2020-06-03 11:35:54 +0200

    nit

--- workers/semantics/multiple-workers/exposure.any.js
+++ workers/semantics/multiple-workers/exposure.any.js
@@ -1,13 +1,11 @@
 // META: global=window,worker
 
 test(() => {
-  // Use onfetch as service worker detector
-  const assert = "onfetch" in globalThis ? assert_equals : assert_not_equals;
+  const assert = "ServiceWorkerGlobalScope" in globalThis ? assert_equals : assert_not_equals;
   assert(globalThis.Worker, undefined);
-}, "Worker exposure")
+}, "Worker exposure");
 
 test(() => {
   const assert = globalThis.GLOBAL.isWindow() ? assert_not_equals : assert_equals;
   assert(globalThis.SharedWorker, undefined);
-}, "SharedWorker exposure")
-
+}, "SharedWorker exposure");

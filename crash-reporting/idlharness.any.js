82c976417c8a9e155a2bdf355f37c221f83ac5b9
Stephen McGruer <smcgruer@chromium.org>
2020-06-17 12:55:23 -0400

    Add interfaces/crash-reporting.idl and test
    Closes https://github.com/web-platform-tests/wpt/pull/24190

---
+++ crash-reporting/idlharness.any.js
@@ -0,0 +1,14 @@
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+'use strict';
+
+idl_test(
+  ['crash-reporting'],
+  ['reporting'],
+  idl_array => {
+    idl_array.add_objects({
+      // TODO: objects
+    });
+  }
+);

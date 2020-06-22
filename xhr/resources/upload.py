904a2ae2a0630c6de9befaf2895c90581fbb9606
Ziran Sun <zsun@igalia.com>
2020-06-22 09:38:28 +0100

    Python 3: port xhr tests [part 7]

--- xhr/resources/upload.py
+++ xhr/resources/upload.py
@@ -1,15 +1,17 @@
+from wptserve.utils import isomorphic_decode
+
 def main(request, response):
     content = []
 
-    for key, values in sorted(item for item in request.POST.items() if not hasattr(item[1][0], "filename")):
-        content.append("%s=%s," % (key, values[0]))
-    content.append("\n")
+    for key, values in sorted(item for item in request.POST.items() if not hasattr(item[1][0], u"filename")):
+        content.append(u"%s=%s," % (isomorphic_decode(key), str(values[0])))
+    content.append(u"\n")
 
-    for key, values in sorted(item for item in request.POST.items() if hasattr(item[1][0], "filename")):
+    for key, values in sorted(item for item in request.POST.items() if hasattr(item[1][0], u"filename")):
         value = values[0]
-        content.append("%s=%s:%s:%s," % (key,
-                                         value.filename,
-                                         value.headers["Content-Type"],
-                                         len(value.file.read())))
+        content.append(u"%s=%s:%s:%s," % (isomorphic_decode(key),
+                                          isomorphic_decode(value.filename),
+                                          value.headers[u"Content-Type"] if value.headers[u"Content-Type"] != None else None,
+                                          len(value.file.read())))
 
-    return "".join(content)
+    return u"".join(content)

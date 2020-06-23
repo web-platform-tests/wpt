ae5ff7932c1729f99b6936f02e17799899abecc5
Ziran Sun <zsun@igalia.com>
2020-06-23 10:01:34 +0100

    Make content a bytes string

--- xhr/resources/upload.py
+++ xhr/resources/upload.py
@@ -1,17 +1,17 @@
-from wptserve.utils import isomorphic_decode
+from wptserve.utils import isomorphic_encode
 
 def main(request, response):
     content = []
 
     for key, values in sorted(item for item in request.POST.items() if not hasattr(item[1][0], u"filename")):
-        content.append(u"%s=%s," % (isomorphic_decode(key), str(values[0])))
-    content.append(u"\n")
+        content.append(b"%s=%s," % (key, values[0]))
+    content.append(b"\n")
 
     for key, values in sorted(item for item in request.POST.items() if hasattr(item[1][0], u"filename")):
         value = values[0]
-        content.append(u"%s=%s:%s:%s," % (isomorphic_decode(key),
-                                          isomorphic_decode(value.filename),
-                                          value.headers[u"Content-Type"] if value.headers[u"Content-Type"] != None else None,
+        content.append(b"%s=%s:%s:%d," % (key,
+                                          isomorphic_encode(value.filename),
+                                          isomorphic_encode(value.headers[u"Content-Type"]) if value.headers[u"Content-Type"] != None else None,
                                           len(value.file.read())))
 
-    return u"".join(content)
+    return b"".join(content)

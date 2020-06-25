448b452e9a26855f9073aafbc6b9b14a11ae8132
Ziran Sun <zsun@igalia.com>
2020-06-25 14:50:50 +0100

    remove whitespace

--- cookies/resources/helpers.py
+++ cookies/resources/helpers.py
@@ -25,7 +25,7 @@ def makeCookieHeader(name, value, otherAttrs):
     def makeAV(a, v):
         if None == v or b"" == v:
             return a
-        if isinstance(v, integer_types): 
+        if isinstance(v, integer_types):
             return b"%s=%i" % (a, v)
         else:
             return b"%s=%s" % (a, v)

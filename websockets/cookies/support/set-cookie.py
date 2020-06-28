eb95fb751f621467d1b13e6b7f085f6783bda8e5
Ziran Sun <zsun@igalia.com>
2020-06-28 07:54:00 +0100

    Add missing prefix

--- websockets/cookies/support/set-cookie.py
+++ websockets/cookies/support/set-cookie.py
@@ -4,4 +4,4 @@ from wptserve.utils import isomorphic_encode
 
 def main(request, response):
     response.headers.set(b'Set-Cookie', isomorphic_encode(parse.unquote(request.url_parts.query)))
-    return [(b"Content-Type", b"text/plain")], ""
+    return [(b"Content-Type", b"text/plain")], b""

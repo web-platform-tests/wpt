a03e9fcc039a634e32ae55f4f86aabbd687fe9d7
Ziran Sun <zsun@igalia.com>
2020-06-30 09:17:57 +0100

    Use utf-8 encoding for uuid and json.dump(data)

--- service-workers/service-worker/resources/test-request-headers-worker.py
+++ service-workers/service-worker/resources/test-request-headers-worker.py
@@ -11,8 +11,8 @@ def main(request, response):
   body = open(path, u"rb").read()
 
   data = {isomorphic_decode(key):isomorphic_decode(request.headers[key]) for key, value in request.headers.items()}
-  body = body.replace(b"%HEADERS%", json.dumps(data).encode(sys.getdefaultencoding()))
-  body = body.replace(b"%UUID%", str(uuid.uuid4()).encode(sys.getdefaultencoding()))
+  body = body.replace(b"%HEADERS%", json.dumps(data).encode("utf-8"))
+  body = body.replace(b"%UUID%", str(uuid.uuid4()).encode("utf-8"))
 
   headers = []
   headers.append((b"ETag", b"etag"))

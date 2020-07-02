c5f44575547dbe7fa2b21eb9330501e9f871b14f
Ziran Sun <zsun@igalia.com>
2020-07-02 15:20:51 +0100

    Avoid unnecessary decode() call

--- service-workers/service-worker/resources/import-mime-type-worker.py
+++ service-workers/service-worker/resources/import-mime-type-worker.py
@@ -1,12 +1,10 @@
-from wptserve.utils import isomorphic_decode
-
 def main(request, response):
     if b'mime' in request.GET:
         return (
             [(b'Content-Type', b'application/javascript')],
-            u"importScripts('./mime-type-worker.py?mime={0}');".format(isomorphic_decode(request.GET[b'mime']))
+            b"importScripts('./mime-type-worker.py?mime=%s');" % request.GET[b'mime']
         )
     return (
         [(b'Content-Type', b'application/javascript')],
-        u"importScripts('./mime-type-worker.py');"
+        b"importScripts('./mime-type-worker.py');"
     )

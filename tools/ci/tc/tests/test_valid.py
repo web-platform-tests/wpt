adf2323934b4f6b90d88cc2fb0fa2a8afd3bb8b3
Robert Ma <robertma@chromium.org>
2020-07-10 11:28:36 -0400

    Mark a test as expected to fail on Windows+py3
    See #24561 for details.

--- tools/ci/tc/tests/test_valid.py
+++ tools/ci/tc/tests/test_valid.py
@@ -1,5 +1,6 @@
 import json
 import os
+import sys
 from io import open
 
 import jsone
@@ -8,6 +9,7 @@ import pytest
 import requests
 import yaml
 from jsonschema import validate
+from six import PY3
 
 from tools.ci.tc import decision
 
@@ -19,6 +21,8 @@ def data_path(filename):
     return os.path.join(here, "..", "testdata", filename)
 
 
+@pytest.mark.xfail(sys.platform == "win32" and PY3,
+                   reason="https://github.com/web-platform-tests/wpt/issues/24561")
 def test_verify_taskcluster_yml():
     """Verify that the json-e in the .taskcluster.yml is valid"""
     with open(os.path.join(root, ".taskcluster.yml"), encoding="utf8") as f:

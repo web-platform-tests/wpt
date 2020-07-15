3e452eb9250cd872dd5bb785645fe475cd93b510
Thomas Guilbert <tguilbert@chromium.org>
2020-07-15 08:25:10 +0000

    Deflake preservesPitch tests
    This CL changes preservesPitch tests to wait until the audioElement's
    currentTime is at least 0.5 seconds before trying to detect the pitch.
    Without this check, the test can try to detect the pitch before the
    audio has played enough, which can return a dominant frequency of 0Hz
    and fail the test.
    
    The CL also makes a few stylistic changes, and fixes an off-by-one error
    in the number of increments used to calculate frequencies.
    
    Bug: 1096238
    Change-Id: I6e98e172862a47bea1c4026737138293914f7906
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2298281
    Auto-Submit: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788535}

--- html/semantics/embedded-content/media-elements/pitch-detector.js
+++ html/semantics/embedded-content/media-elements/pitch-detector.js
@@ -16,14 +16,15 @@ function getPitchDetector(media, t) {
   sourceNode.connect(analyser);
   analyser.connect(audioContext.destination);
 
-  // Returns the frequency value for the nth FFT bin.
-  var binConverter = (bin) => audioContext.sampleRate*(bin/FFT_SIZE);
-
-  return () => getPitch(analyser, binConverter);
+  return () => getPitch(analyser);
 }
 
-function getPitch(analyser, binConverter) {
-  var buf = new Uint8Array(FFT_SIZE/2);
+function getPitch(analyser) {
+  // Returns the frequency value for the nth FFT bin.
+  var binConverter = (bin) =>
+    (analyser.context.sampleRate/2)*((bin)/(analyser.frequencyBinCount-1));
+
+  var buf = new Uint8Array(analyser.frequencyBinCount);
   analyser.getByteFrequencyData(buf);
   return findDominantFrequency(buf, binConverter);
 }
@@ -40,9 +41,8 @@ function findDominantFrequency(buf, binConverter) {
     }
   }
 
-  // The distance between bins is always constant and corresponds to
-  // (1/FFT_SIZE)th of the sample rate. Use the frequency value of the 1st bin
-  // as the margin directly, instead of calculating an average from the values
-  // of the neighboring bins.
+  // The spread of frequencies within bins is constant and corresponds to
+  // (1/(FFT_SIZE-1))th of the sample rate. Use the value of bin #1 as a
+  // shorthand for that value.
   return { value:binConverter(bin), margin:binConverter(1) };
 }

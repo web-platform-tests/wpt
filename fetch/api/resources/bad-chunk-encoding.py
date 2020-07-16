c0810f91f56ad28ef7e82efb3710bfbd41297c78
Ziran Sun <zsun@igalia.com>
2020-07-16 10:30:38 +0100

    Python 3: Port tests in fetch/api [part 2]

--- fetch/api/resources/bad-chunk-encoding.py
+++ fetch/api/resources/bad-chunk-encoding.py
@@ -1,13 +1,15 @@
 import time
 
+from six.moves import xrange
+
 def main(request, response):
-    delay = float(request.GET.first("ms", 1000)) / 1E3
-    count = int(request.GET.first("count", 50))
+    delay = float(request.GET.first(b"ms", 1000)) / 1E3
+    count = int(request.GET.first(b"count", 50))
     time.sleep(delay)
-    response.headers.set("Transfer-Encoding", "chunked")
+    response.headers.set(b"Transfer-Encoding", b"chunked")
     response.write_status_headers()
     time.sleep(delay)
     for i in xrange(count):
-        response.writer.write_content("a\r\nTEST_CHUNK\r\n")
+        response.writer.write_content(b"a\r\nTEST_CHUNK\r\n")
         time.sleep(delay)
-    response.writer.write_content("garbage")
+    response.writer.write_content(b"garbage")

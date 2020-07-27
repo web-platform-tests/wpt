web-platform-tests/wpt

commit d7452723d0eb5637446afc5ef18c68d3768a5392

Add Chrome nightly (Chromium trunk) to wpt (#24702)
* Add nightly channel to Chrome

* Implement install{,_webdriver} for Chrome nightly

* Support Chrome nightly in "wpt run"

* Add and fix some tests

Add a test for installing Chrome nightly.

In addition, create a safer version of shutil.rmtree based on existing
implementation in browser.py and reuse it in more places in place of
shutil.rmtree.

tools/wpt/browser.py
@@ -3,7 +3,6 @@ import platform
 import re
 import shutil
 import stat
-import errno
 import subprocess
 import tempfile
 from abc import ABCMeta, abstractmethod
@@ -13,7 +12,7 @@ from distutils.spawn import find_executable
 from six.moves.urllib.parse import urlsplit
 import requests
 
-from .utils import call, get, untar, unzip
+from .utils import call, get, rmtree, untar, unzip
 
 uname = platform.uname()
 
@@ -29,15 +28,6 @@ def _get_fileversion(binary, logger=None):
         if logger is not None:
             logger.warning("Failed to call %s in PowerShell" % command)
         return None
-
-
-def handle_remove_readonly(func, path, exc):
-    excvalue = exc[1]
-    if func in (os.rmdir, os.remove) and excvalue.errno == errno.EACCES:
-        os.chmod(path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)  # 0777
-        func(path)
-    else:
-        raise
 
 
 def get_ext(filename):
@@ -62,6 +52,18 @@ class Browser(object):
 
     def __init__(self, logger):
         self.logger = logger
+
+    def _get_dest(self, dest, channel):
+        if dest is None:
+            # os.getcwd() doesn't include the venv path
+            dest = os.path.join(os.getcwd(), "_venv")
+
+        dest = os.path.join(dest, "browsers", channel)
+
+        if not os.path.exists(dest):
+            os.makedirs(dest)
+
+        return dest
 
     @abstractmethod
     def download(self, dest=None, channel=None, rename=None):
@@ -155,18 +157,6 @@ class Firefox(Browser):
             bits = ""
 
         return "%s%s" % (self.platform, bits)
-
-    def _get_dest(self, dest, channel):
-        if dest is None:
-            # os.getcwd() doesn't include the venv path
-            dest = os.path.join(os.getcwd(), "_venv")
-
-        dest = os.path.join(dest, "browsers", channel)
-
-        if not os.path.exists(dest):
-            os.makedirs(dest)
-
-        return dest
 
     def download(self, dest=None, channel="nightly", rename=None):
         product = {
@@ -374,7 +364,7 @@ class Firefox(Browser):
         # If we don't have a recent download, grab and extract the latest one
         if not have_cache:
             if os.path.exists(dest):
-                shutil.rmtree(dest)
+                rmtree(dest)
             os.makedirs(dest)
 
             url = self.get_profile_bundle_url(version, channel)
@@ -389,7 +379,7 @@ class Firefox(Browser):
                     path = os.path.join(profiles, name)
                     shutil.move(path, dest)
             finally:
-                shutil.rmtree(extract_dir)
+                rmtree(extract_dir)
         else:
             self.logger.info("Using cached test prefs from %s" % dest)
 
@@ -534,22 +524,48 @@ class Chrome(Browser):
 
     product = "chrome"
     requirements = "requirements_chrome.txt"
+    platforms = {
+        "Linux": "Linux",
+        "Windows": "Win",
+        "Darwin": "Mac",
+    }
+
+    def __init__(self, logger):
+        super(Chrome, self).__init__(logger)
+        self._last_change = None
 
     def download(self, dest=None, channel=None, rename=None):
-        raise NotImplementedError
+        if channel != "nightly":
+            raise NotImplementedError("We can only download Chrome Nightly (Chromium ToT) for you.")
+        if dest is None:
+            dest = self._get_dest(None, channel)
+
+        filename = self._chromium_package_name() + ".zip"
+        url = self._latest_chromium_snapshot_url() + filename
+        self.logger.info("Downloading Chrome from %s" % url)
+        resp = get(url)
+        installer_path = os.path.join(dest, filename)
+        with open(installer_path, "wb") as f:
+            f.write(resp.content)
+        return installer_path
 
     def install(self, dest=None, channel=None):
-        raise NotImplementedError
+        if channel != "nightly":
+            raise NotImplementedError("We can only install Chrome Nightly (Chromium ToT) for you.")
+        dest = self._get_dest(dest, channel)
 
-    def platform_string(self):
-        platform = {
-            "Linux": "linux",
-            "Windows": "win",
-            "Darwin": "mac"
-        }.get(uname[0])
+        installer_path = self.download(dest, channel)
+        with open(installer_path, "rb") as f:
+            unzip(f, dest)
+        os.remove(installer_path)
+        return self.find_nightly_binary(dest, channel)
+
+    def _chromedriver_platform_string(self):
+        platform = self.platforms.get(uname[0])
 
         if platform is None:
             raise ValueError("Unable to construct a valid Chrome package name for current platform")
+        platform = platform.lower()
 
         if platform == "linux":
             bits = "64" if uname[4] == "x86_64" else "32"
@@ -560,12 +576,8 @@ class Chrome(Browser):
 
         return "%s%s" % (platform, bits)
 
-    def chromium_platform_string(self):
-        platform = {
-            "Linux": "Linux",
-            "Windows": "Win",
-            "Darwin": "Mac"
-        }.get(uname[0])
+    def _chromium_platform_string(self):
+        platform = self.platforms.get(uname[0])
 
         if platform is None:
             raise ValueError("Unable to construct a valid Chromium package name for current platform")
@@ -575,7 +587,26 @@ class Chrome(Browser):
 
         return platform
 
+    def _chromium_package_name(self):
+        return "chrome-%s" % self.platforms.get(uname[0]).lower()
+
+    def _latest_chromium_snapshot_url(self):
+        # Make sure we use the same revision in an invocation.
+        architecture = self._chromium_platform_string()
+        if self._last_change is None:
+            revision_url = "https://storage.googleapis.com/chromium-browser-snapshots/%s/LAST_CHANGE" % architecture
+            self._last_change = get(revision_url).text.strip()
+        return "https://storage.googleapis.com/chromium-browser-snapshots/%s/%s/" % (architecture, self._last_change)
+
+    def find_nightly_binary(self, dest, channel):
+        binary = "Chromium" if uname[0] == "Darwin" else "chrome"
+        # find_executable will add .exe on Windows automatically.
+        return find_executable(binary, os.path.join(dest, self._chromium_package_name()))
+
     def find_binary(self, venv_path=None, channel=None):
+        if channel == "nightly":
+            return self.find_nightly_binary(self._get_dest(venv_path, channel))
+
         if uname[0] == "Linux":
             name = "google-chrome"
             if channel == "stable":
@@ -613,25 +644,23 @@ class Chrome(Browser):
             except requests.RequestException:
                 return None
         return "https://chromedriver.storage.googleapis.com/%s/chromedriver_%s.zip" % (
-            latest, self.platform_string())
+            latest, self._chromedriver_platform_string())
 
     def _chromium_chromedriver_url(self, chrome_version):
-        try:
-            # Try to find the Chromium build with the same revision.
-            omaha = get("https://omahaproxy.appspot.com/deps.json?version=" + chrome_version).json()
-            revision = omaha['chromium_base_position']
-            url = "https://storage.googleapis.com/chromium-browser-snapshots/%s/%s/chromedriver_%s.zip" % (
-                self.chromium_platform_string(), revision, self.platform_string())
-            # Check the status without downloading the content (this is a streaming request).
-            get(url)
-        except requests.RequestException:
-            # Fall back to the tip-of-tree Chromium build.
-            revision_url = "https://storage.googleapis.com/chromium-browser-snapshots/%s/LAST_CHANGE" % (
-                self.chromium_platform_string())
-            revision = get(revision_url).text.strip()
-            url = "https://storage.googleapis.com/chromium-browser-snapshots/%s/%s/chromedriver_%s.zip" % (
-                self.chromium_platform_string(), revision, self.platform_string())
-        return url
+        if chrome_version:
+            try:
+                # Try to find the Chromium build with the same revision.
+                omaha = get("https://omahaproxy.appspot.com/deps.json?version=" + chrome_version).json()
+                revision = omaha['chromium_base_position']
+                url = "https://storage.googleapis.com/chromium-browser-snapshots/%s/%s/chromedriver_%s.zip" % (
+                    self._chromium_platform_string(), revision, self._chromedriver_platform_string())
+                # Check the status without downloading the content (this is a streaming request).
+                get(url)
+                return url
+            except requests.RequestException:
+                pass
+        # Fall back to the tip-of-tree Chromium build.
+        return "%schromedriver_%s.zip" % (self._latest_chromium_snapshot_url(), self._chromedriver_platform_string())
 
     def _latest_chromedriver_url(self, chrome_version):
         # Remove channel suffixes (e.g. " dev").
@@ -640,20 +669,25 @@ class Chrome(Browser):
                 self._chromium_chromedriver_url(chrome_version))
 
     def install_webdriver_by_version(self, version, dest=None):
-        assert version, "Cannot install ChromeDriver without Chrome version"
         if dest is None:
             dest = os.pwd
-        url = self._latest_chromedriver_url(version)
+        url = self._latest_chromedriver_url(version) if version \
+            else self._chromium_chromedriver_url(None)
         self.logger.info("Downloading ChromeDriver from %s" % url)
         unzip(get(url).raw, dest)
         chromedriver_dir = os.path.join(
-            dest, 'chromedriver_%s' % self.platform_string())
-        if os.path.isfile(os.path.join(chromedriver_dir, "chromedriver")):
-            shutil.move(os.path.join(chromedriver_dir, "chromedriver"), dest)
-            shutil.rmtree(chromedriver_dir)
+            dest, 'chromedriver_%s' % self._chromedriver_platform_string())
+        unzipped_path = find_executable("chromedriver", chromedriver_dir)
+        assert unzipped_path is not None
+        shutil.move(unzipped_path, dest)
+        rmtree(chromedriver_dir)
         return find_executable("chromedriver", dest)
 
     def install_webdriver(self, dest=None, channel=None, browser_binary=None):
+        if channel == "nightly":
+            # The "nightly" channel is not an official channel, so we simply download ToT.
+            return self.install_webdriver_by_version(None, dest)
+
         if browser_binary is None:
             browser_binary = self.find_binary(channel)
         return self.install_webdriver_by_version(
@@ -878,7 +912,7 @@ class Opera(Browser):
 
         operadriver_dir = os.path.join(dest, "operadriver_%s" % self.platform_string())
         shutil.move(os.path.join(operadriver_dir, "operadriver"), dest)
-        shutil.rmtree(operadriver_dir)
+        rmtree(operadriver_dir)
 
         path = find_executable("operadriver")
         st = os.stat(path)
@@ -975,7 +1009,7 @@ class EdgeChromium(Browser):
         driver_notes_path = os.path.join(dest, "Driver_notes")
         if os.path.isdir(driver_notes_path):
             print("Delete %s folder" % driver_notes_path)
-            shutil.rmtree(driver_notes_path, ignore_errors=False, onerror=handle_remove_readonly)
+            rmtree(driver_notes_path)
 
         self.logger.info("Downloading MSEdgeDriver from %s" % url)
         unzip(get(url).raw, dest)

tools/wpt/tests/test_wpt.py
@@ -16,7 +16,7 @@ except ImportError:
 
 import pytest
 
-from tools.wpt import wpt
+from tools.wpt import utils, wpt
 
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -61,7 +61,7 @@ def manifest_dir():
                         os.path.join(path, "MANIFEST.json"))
         yield path
     finally:
-        shutil.rmtree(path)
+        utils.rmtree(path)
 
 
 @pytest.fixture
@@ -86,7 +86,7 @@ def temp_test():
 
     yield make_test
 
-    shutil.rmtree("../../.tools-tests")
+    utils.rmtree("../../.tools-tests")
 
 
 def test_missing():
@@ -185,6 +185,7 @@ def test_run_zero_tests():
                        "chrome", "/non-existent-dir/non-existent-file.html"])
     assert excinfo.value.code != 0
 
+
 @pytest.mark.slow
 @pytest.mark.remote_network
 def test_run_failing_test():
@@ -240,6 +241,25 @@ def test_run_verify_unstable(temp_test):
 
 @pytest.mark.slow
 @pytest.mark.remote_network
+def test_install_chromium():
+    if sys.platform == "win32":
+        chromium_path = os.path.join(wpt.localpaths.repo_root, wpt.venv_dir(), "browsers", "nightly", "chrome-win")
+    elif sys.platform == "darwin":
+        chromium_path = os.path.join(wpt.localpaths.repo_root, wpt.venv_dir(), "browsers", "nightly", "chrome-mac")
+    else:
+        chromium_path = os.path.join(wpt.localpaths.repo_root, wpt.venv_dir(), "browsers", "nightly", "chrome-linux")
+
+    if os.path.exists(chromium_path):
+        utils.rmtree(chromium_path)
+    with pytest.raises(SystemExit) as excinfo:
+        wpt.main(argv=["install", "chrome", "browser", "--channel=nightly"])
+    assert excinfo.value.code == 0
+    assert os.path.exists(chromium_path)
+    utils.rmtree(chromium_path)
+
+
+@pytest.mark.slow
+@pytest.mark.remote_network
 def test_install_chromedriver():
     if sys.platform == "win32":
         chromedriver_path = os.path.join(wpt.localpaths.repo_root, wpt.venv_dir(), "Scripts", "chromedriver.exe")
@@ -251,7 +271,13 @@ def test_install_chromedriver():
         wpt.main(argv=["install", "chrome", "webdriver"])
     assert excinfo.value.code == 0
     assert os.path.exists(chromedriver_path)
-    os.unlink(chromedriver_path)
+    # FIXME: On Windows, this may sometimes fail (access denied), possibly
+    # because the file handler is not released immediately.
+    try:
+        os.unlink(chromedriver_path)
+    except OSError:
+        if sys.platform != "win32":
+            raise
 
 
 @pytest.mark.slow
@@ -264,12 +290,12 @@ def test_install_firefox():
     else:
         fx_path = os.path.join(wpt.localpaths.repo_root, wpt.venv_dir(), "browsers", "nightly", "firefox")
     if os.path.exists(fx_path):
-        shutil.rmtree(fx_path)
+        utils.rmtree(fx_path)
     with pytest.raises(SystemExit) as excinfo:
         wpt.main(argv=["install", "firefox", "browser", "--channel=nightly"])
     assert excinfo.value.code == 0
     assert os.path.exists(fx_path)
-    shutil.rmtree(fx_path)
+    utils.rmtree(fx_path)
 
 
 def test_files_changed(capsys):

tools/wpt/install.py
@@ -3,7 +3,7 @@ from . import browser
 
 latest_channels = {
     'firefox': 'nightly',
-    'chrome': 'dev',
+    'chrome': 'nightly',
     'chrome_android': 'dev',
     'edgechromium': 'dev',
     'safari': 'preview',
@@ -14,11 +14,11 @@ channel_by_name = {
     'stable': 'stable',
     'release': 'stable',
     'beta': 'beta',
+    'dev': 'dev',
+    'canary': 'canary',
     'nightly': latest_channels,
-    'dev': latest_channels,
     'preview': latest_channels,
     'experimental': latest_channels,
-    'canary': 'canary',
 }
 
 channel_args = argparse.ArgumentParser(add_help=False)

tools/wpt/run.py
@@ -343,8 +343,8 @@ class Chrome(BrowserSetup):
                 kwargs["webdriver_binary"] = webdriver_binary
             else:
                 raise WptrunError("Unable to locate or install chromedriver binary")
-        if browser_channel in ("dev", "canary"):
-            logger.info("Automatically turning on experimental features for Chrome Dev/Canary")
+        if browser_channel in ("dev", "canary", "nightly"):
+            logger.info("Automatically turning on experimental features for Chrome Dev/Canary or Chromium trunk")
             kwargs["binary_args"].append("--enable-experimental-web-platform-features")
             # HACK(Hexcles): work around https://github.com/web-platform-tests/wpt/issues/16448
             kwargs["webdriver_args"].append("--disable-build-check")

tools/wpt/utils.py
@@ -1,5 +1,8 @@
+import errno
 import logging
 import os
+import shutil
+import stat
 import subprocess
 import tarfile
 import zipfile
@@ -95,3 +98,19 @@ def get(url):
     resp = requests.get(url, stream=True)
     resp.raise_for_status()
     return resp
+
+
+def rmtree(path):
+    # This works around two issues:
+    # 1. Cannot delete read-only files owned by us (e.g. files extracted from tarballs)
+    # 2. On Windows, we sometimes just need to retry in case the file handler
+    #    hasn't been fully released (a common issue).
+    def handle_remove_readonly(func, path, exc):
+        excvalue = exc[1]
+        if func in (os.rmdir, os.remove) and excvalue.errno == errno.EACCES:
+            os.chmod(path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)  # 0777
+            func(path)
+        else:
+            raise
+
+    return shutil.rmtree(path, onerror=handle_remove_readonly)

commit 01d5af4714b472bb90fa91e0fedddec5b0f0630b

[wpt] Fix the installation of ChromeDriver stable
This was accidentally regressed in #24702 due to lack of comments or
tests, both of which are also added in this change.

Fixes #24753.

tools/wpt/browser.py
@@ -680,13 +680,21 @@ class Chrome(Browser):
             else self._chromium_chromedriver_url(None)
         self.logger.info("Downloading ChromeDriver from %s" % url)
         unzip(get(url).raw, dest)
+
+        # The two sources of ChromeDriver have different zip structures:
+        # * Chromium archives the binary inside a chromedriver_* directory;
+        # * Chrome archives the binary directly.
+        # We want to make sure the binary always ends up directly in bin/.
         chromedriver_dir = os.path.join(
             dest, 'chromedriver_%s' % self._chromedriver_platform_string())
-        unzipped_path = find_executable("chromedriver", chromedriver_dir)
-        assert unzipped_path is not None
-        shutil.move(unzipped_path, dest)
-        rmtree(chromedriver_dir)
-        return find_executable("chromedriver", dest)
+        binary_path = find_executable("chromedriver", chromedriver_dir)
+        if binary_path is not None:
+            shutil.move(binary_path, dest)
+            rmtree(chromedriver_dir)
+
+        binary_path = find_executable("chromedriver", dest)
+        assert binary_path is not None
+        return binary_path
 
     def install_webdriver(self, dest=None, channel=None, browser_binary=None):
         if channel == "nightly":

tools/wpt/tests/test_install.py
@@ -1,0 +1,86 @@
+import logging
+import os
+import sys
+
+import pytest
+
+from tools.wpt import browser, utils, wpt
+
+
+@pytest.mark.slow
+@pytest.mark.remote_network
+def test_install_chromium():
+    if sys.platform == "win32":
+        chromium_path = os.path.join(wpt.localpaths.repo_root, wpt.venv_dir(), "browsers", "nightly", "chrome-win")
+    elif sys.platform == "darwin":
+        chromium_path = os.path.join(wpt.localpaths.repo_root, wpt.venv_dir(), "browsers", "nightly", "chrome-mac")
+    else:
+        chromium_path = os.path.join(wpt.localpaths.repo_root, wpt.venv_dir(), "browsers", "nightly", "chrome-linux")
+
+    if os.path.exists(chromium_path):
+        utils.rmtree(chromium_path)
+    with pytest.raises(SystemExit) as excinfo:
+        wpt.main(argv=["install", "chrome", "browser", "--channel=nightly"])
+    assert excinfo.value.code == 0
+    assert os.path.exists(chromium_path)
+    utils.rmtree(chromium_path)
+
+
+@pytest.mark.slow
+@pytest.mark.remote_network
+def test_install_chromedriver_official():
+    # This is not technically an integration test as we do not want to require Chrome Stable to run it.
+    chrome = browser.Chrome(logging.getLogger("Chrome"))
+    if sys.platform == "win32":
+        dest = os.path.join(wpt.localpaths.repo_root, wpt.venv_dir(), "Scripts")
+        chromedriver_path = os.path.join(dest, "chromedriver.exe")
+    else:
+        dest = os.path.join(wpt.localpaths.repo_root, wpt.venv_dir(), "bin")
+        chromedriver_path = os.path.join(dest, "chromedriver")
+    if os.path.exists(chromedriver_path):
+        os.unlink(chromedriver_path)
+    # This is a stable version.
+    binary_path = chrome.install_webdriver_by_version("84.0.4147.89", dest=dest)
+    assert binary_path == chromedriver_path
+    assert os.path.exists(chromedriver_path)
+    os.unlink(chromedriver_path)
+
+
+@pytest.mark.slow
+@pytest.mark.remote_network
+def test_install_chromedriver_nightly():
+    if sys.platform == "win32":
+        chromedriver_path = os.path.join(wpt.localpaths.repo_root, wpt.venv_dir(), "Scripts", "chromedriver.exe")
+    else:
+        chromedriver_path = os.path.join(wpt.localpaths.repo_root, wpt.venv_dir(), "bin", "chromedriver")
+    if os.path.exists(chromedriver_path):
+        os.unlink(chromedriver_path)
+    with pytest.raises(SystemExit) as excinfo:
+        wpt.main(argv=["install", "chrome", "webdriver"])
+    assert excinfo.value.code == 0
+    assert os.path.exists(chromedriver_path)
+    # FIXME: On Windows, this may sometimes fail (access denied), possibly
+    # because the file handler is not released immediately.
+    try:
+        os.unlink(chromedriver_path)
+    except OSError:
+        if sys.platform != "win32":
+            raise
+
+
+@pytest.mark.slow
+@pytest.mark.remote_network
+@pytest.mark.xfail(sys.platform == "win32",
+                   reason="https://github.com/web-platform-tests/wpt/issues/17074")
+def test_install_firefox():
+    if sys.platform == "darwin":
+        fx_path = os.path.join(wpt.localpaths.repo_root, wpt.venv_dir(), "browsers", "nightly", "Firefox Nightly.app")
+    else:
+        fx_path = os.path.join(wpt.localpaths.repo_root, wpt.venv_dir(), "browsers", "nightly", "firefox")
+    if os.path.exists(fx_path):
+        utils.rmtree(fx_path)
+    with pytest.raises(SystemExit) as excinfo:
+        wpt.main(argv=["install", "firefox", "browser", "--channel=nightly"])
+    assert excinfo.value.code == 0
+    assert os.path.exists(fx_path)
+    utils.rmtree(fx_path)

tools/wpt/tests/test_wpt.py
@@ -239,65 +239,6 @@ def test_run_verify_unstable(temp_test):
     assert excinfo.value.code == 0
 
 
-@pytest.mark.slow
-@pytest.mark.remote_network
-def test_install_chromium():
-    if sys.platform == "win32":
-        chromium_path = os.path.join(wpt.localpaths.repo_root, wpt.venv_dir(), "browsers", "nightly", "chrome-win")
-    elif sys.platform == "darwin":
-        chromium_path = os.path.join(wpt.localpaths.repo_root, wpt.venv_dir(), "browsers", "nightly", "chrome-mac")
-    else:
-        chromium_path = os.path.join(wpt.localpaths.repo_root, wpt.venv_dir(), "browsers", "nightly", "chrome-linux")
-
-    if os.path.exists(chromium_path):
-        utils.rmtree(chromium_path)
-    with pytest.raises(SystemExit) as excinfo:
-        wpt.main(argv=["install", "chrome", "browser", "--channel=nightly"])
-    assert excinfo.value.code == 0
-    assert os.path.exists(chromium_path)
-    utils.rmtree(chromium_path)
-
-
-@pytest.mark.slow
-@pytest.mark.remote_network
-def test_install_chromedriver():
-    if sys.platform == "win32":
-        chromedriver_path = os.path.join(wpt.localpaths.repo_root, wpt.venv_dir(), "Scripts", "chromedriver.exe")
-    else:
-        chromedriver_path = os.path.join(wpt.localpaths.repo_root, wpt.venv_dir(), "bin", "chromedriver")
-    if os.path.exists(chromedriver_path):
-        os.unlink(chromedriver_path)
-    with pytest.raises(SystemExit) as excinfo:
-        wpt.main(argv=["install", "chrome", "webdriver"])
-    assert excinfo.value.code == 0
-    assert os.path.exists(chromedriver_path)
-    # FIXME: On Windows, this may sometimes fail (access denied), possibly
-    # because the file handler is not released immediately.
-    try:
-        os.unlink(chromedriver_path)
-    except OSError:
-        if sys.platform != "win32":
-            raise
-
-
-@pytest.mark.slow
-@pytest.mark.remote_network
-@pytest.mark.xfail(sys.platform == "win32",
-                   reason="https://github.com/web-platform-tests/wpt/issues/17074")
-def test_install_firefox():
-    if sys.platform == "darwin":
-        fx_path = os.path.join(wpt.localpaths.repo_root, wpt.venv_dir(), "browsers", "nightly", "Firefox Nightly.app")
-    else:
-        fx_path = os.path.join(wpt.localpaths.repo_root, wpt.venv_dir(), "browsers", "nightly", "firefox")
-    if os.path.exists(fx_path):
-        utils.rmtree(fx_path)
-    with pytest.raises(SystemExit) as excinfo:
-        wpt.main(argv=["install", "firefox", "browser", "--channel=nightly"])
-    assert excinfo.value.code == 0
-    assert os.path.exists(fx_path)
-    utils.rmtree(fx_path)
-
-
 def test_files_changed(capsys):
     commit = "9047ac1d9f51b1e9faa4f9fad9c47d109609ab09"
     with pytest.raises(SystemExit) as excinfo:
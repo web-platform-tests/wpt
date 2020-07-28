aa24e02bfc536b5016f104dab5fcc86209dd14d3
Yoichi Osato <yoichio@chromium.org>
2020-07-27 21:20:03 -0700

    [fetch] Request constructor should throw an error if ReadableStream is locked.
    This CL make Request constructor follow
    https://fetch.spec.whatwg.org/#dom-request:
    "38. If inputBody is body and input is disturbed or locked, then throw a
    TypeError."
    
    Additionally this CL does:
    1. Refactors the constructor around body extracting following the last
    spec.
    2. Removes the wrong case in WPT because no object is created
    due to TypeError.
    
    Bug: 1105704
    Change-Id: Ie2ba890828e2091dbc0c55fa7120fb7ec37ebc13

--- fetch/api/request/request-init-stream.any.js
+++ fetch/api/request/request-init-stream.any.js
@@ -37,14 +37,12 @@ promise_test(async (t) => {
   const request = new Request("...", { method: "POST", body: "..." });
   request.body.getReader();
   await assert_request(t, request);
-  assert_class_string(new Request(request, { body: "..." }), "Request");
 }, "Constructing a Request with a Request on which body.getReader() is called");
 
 promise_test(async (t) => {
   const request = new Request("...", { method: "POST", body: "..." });
   request.body.getReader().read();
   await assert_request(t, request);
-  assert_class_string(new Request(request, { body: "..." }), "Request");
 }, "Constructing a Request with a Request on which body.getReader().read() is called");
 
 promise_test(async (t) => {
@@ -53,5 +51,4 @@ promise_test(async (t) => {
   await reader.read();
   reader.releaseLock();
   await assert_request(t, request);
-  assert_class_string(new Request(request, { body: "..." }), "Request");
 }, "Constructing a Request with a Request on which read() and releaseLock() are called");

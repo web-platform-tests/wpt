482bdb5ecedf66b808ed22f2beadbbeead57b54a
Robert Ma <robertma@chromium.org>
2020-07-30 10:33:11 -0400

    Reenable test_verify_taskcluster_yml on Windows

--- tools/ci/tc/tests/test_valid.py
+++ tools/ci/tc/tests/test_valid.py
@@ -21,8 +21,6 @@ def data_path(filename):
     return os.path.join(here, "..", "testdata", filename)
 
 
-@pytest.mark.xfail(sys.platform == "win32" and PY3,
-                   reason="https://github.com/web-platform-tests/wpt/issues/24561")
 def test_verify_taskcluster_yml():
     """Verify that the json-e in the .taskcluster.yml is valid"""
     with open(os.path.join(root, ".taskcluster.yml"), encoding="utf8") as f:

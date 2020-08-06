96422795cbc8546f87c58974d9391d7ce96eff0c
Domenic Denicola <d@domenic.me>
2020-08-06 10:06:00 -0400

    Even moar test generation

--- x-frame-options/support/helper.sub.js
+++ x-frame-options/support/helper.sub.js
@@ -1,22 +1,61 @@
 function xfo_simple_tests({ headerValue, headerValue2, cspValue, sameOriginAllowed, crossOriginAllowed }) {
+  simpleXFOTestsInner({
+    urlPrefix: "",
+    allowed: sameOriginAllowed,
+    headerValue,
+    headerValue2,
+    cspValue,
+    sameOrCross: "same-origin"
+  });
+
+  simpleXFOTestsInner({
+    urlPrefix: "http://{{domains[www]}}:{{ports[http][0]}}",
+    allowed: crossOriginAllowed,
+    headerValue,
+    headerValue2,
+    cspValue,
+    sameOrCross: "cross-origin"
+  });
+}
+
+function simpleXFOTestsInner({ urlPrefix, allowed, headerValue, headerValue2, cspValue, sameOrCross }) {
   const value2QueryString = headerValue2 !== undefined ? `&value2=${headerValue2}` : ``;
   const cspQueryString = cspValue !== undefined ? `&csp_value=${cspValue}` : ``;
 
   const valueMessageString = headerValue === "" ? "(the empty string)" : headerValue;
-  const value2MessageString = headerValue2 !== undefined ? `;${headerValue2}` : ``;
+  const value2MessageString = headerValue2 === "" ? "(the empty string)" : headerValue2;
+  const value2MaybeMessageString = headerValue2 !== undefined ? `;${headerValue2}` : ``;
   const cspMessageString = cspValue !== undefined ? ` with CSP ${cspValue}` : ``;
 
+  // This will test the multi-header variant, if headerValue2 is not undefined.
   xfo_test({
-    url: `/x-frame-options/support/xfo.py?value=${headerValue}${value2QueryString}${cspQueryString}`,
-    check: sameOriginAllowed ? "loaded message" : "no message",
-    message: `${valueMessageString}${value2MessageString} ${sameOriginAllowed ? "allows" : "blocks"} same-origin framing${cspMessageString}`
+    url: `${urlPrefix}/x-frame-options/support/xfo.py?value=${headerValue}${value2QueryString}${cspQueryString}`,
+    check: allowed ? "loaded message" : "no message",
+    message: `${valueMessageString}${value2MaybeMessageString} ${allowed ? "allows" : "blocks"} ${sameOrCross} framing${cspMessageString}`
   });
 
-  xfo_test({
-    url: `http://{{domains[www]}}:{{ports[http][0]}}/x-frame-options/support/xfo.py?value=${headerValue}${value2QueryString}${cspQueryString}`,
-    check: crossOriginAllowed ? "loaded message" : "no message",
-    message: `${valueMessageString}${value2MessageString} ${crossOriginAllowed ? "allows" : "blocks"} cross-origin framing${cspMessageString}`
-  });
+  if (headerValue2 !== undefined && headerValue2 !== headerValue) {
+    // Reversed variant
+    xfo_test({
+      url: `${urlPrefix}/x-frame-options/support/xfo.py?value=${headerValue2}&value2=${headerValue}${cspQueryString}`,
+      check: allowed ? "loaded message" : "no message",
+      message: `${value2MessageString};${valueMessageString} ${allowed ? "allows" : "blocks"} ${sameOrCross} framing${cspMessageString}`
+    });
+
+    // Comma variant
+    xfo_test({
+      url: `${urlPrefix}/x-frame-options/support/xfo.py?value=${headerValue},${headerValue2}${cspQueryString}`,
+      check: allowed ? "loaded message" : "no message",
+      message: `${valueMessageString},${value2MessageString} ${allowed ? "allows" : "blocks"} ${sameOrCross} framing${cspMessageString}`
+    });
+
+    // Comma + reversed variant
+    xfo_test({
+      url: `${urlPrefix}/x-frame-options/support/xfo.py?value=${headerValue2},${headerValue}${cspQueryString}`,
+      check: allowed ? "loaded message" : "no message",
+      message: `${value2MessageString},${valueMessageString} ${allowed ? "allows" : "blocks"} ${sameOrCross} framing${cspMessageString}`
+    });
+  }
 }
 
 function xfo_test({ url, check, message }) {

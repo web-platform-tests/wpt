4c8d1f68dba590f8a828a788744881d104ac3308
Simon Pieters <zcorpan@gmail.com>
2020-08-18 15:52:20 +0200

    Update per current specs

--- html/cross-origin-embedder-policy/resources/empty-coep.py
+++ html/cross-origin-embedder-policy/resources/empty-coep.py
@@ -1,7 +1,7 @@
 def main(request, response):
-    headers = [('Content-Type', 'text/html')]
+    headers = [(b'Content-Type', b'text/html')]
 
-    for value in request.GET.get_list('value'):
-        headers.append(('Cross-Origin-Embedder-Policy', value))
+    for value in request.GET.get_list(b'value'):
+        headers.append((b'Cross-Origin-Embedder-Policy', value))
 
-    return (200, headers, '')
+    return (200, headers, u'')

4b6ec0edafdb093829baf1eb654fbe9eb0914a74
Anne van Kesteren <annevk@annevk.nl>
2020-09-01 16:52:53 +0200

    Worklets: use idl_test and .any.js

---
+++ worklets/idlharness.https.any.js
@@ -0,0 +1,10 @@
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+// https://drafts.css-houdini.org/worklets/
+
+'use strict';
+
+idl_test(
+  ['worklets']
+);

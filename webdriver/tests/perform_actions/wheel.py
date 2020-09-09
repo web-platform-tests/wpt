2053852b3c1ecbb669960e84f68accfd950124f4
Lan Wei <lanwei@chromium.org>
2020-09-08 23:51:49 -0400

    wheel input

--- webdriver/tests/perform_actions/wheel.py
+++ webdriver/tests/perform_actions/wheel.py
@@ -1,31 +1,26 @@
 import pytest
 
-# from webdriver.error import NoSuchWindowException
+from webdriver.error import NoSuchWindowException
 
-from tests.perform_actions.support.mouse import get_inview_center, get_viewport_rect
 from tests.perform_actions.support.refine import filter_dict, get_events
 from tests.support.asserts import assert_move_to_coordinates
 
 
-# def test_null_response_value(session, wheel_chain):
-#     value = wheel_chain.scroll(0, 0, 0, 10).perform()
-#     assert value is None
+def test_null_response_value(session, wheel_chain):
+    value = wheel_chain.scroll(0, 0, 0, 10).perform()
+    assert value is None
 
 
-# def test_no_browsing_context(session, closed_window, wheel_chain):
-#     with pytest.raises(NoSuchWindowException):
-#         wheel_chain.scroll(0, 0, 0, 10).perform()
+def test_no_browsing_context(session, closed_window, wheel_chain):
+    with pytest.raises(NoSuchWindowException):
+        wheel_chain.scroll(0, 0, 0, 10).perform()
 
 
-def test_wheel_scroll(session, test_actions_page, wheel_chain):
+def test_wheel_scroll(session, test_actions_scroll_page, wheel_chain):
     session.execute_script("document.scrollingElement.scrollTop = 0")
-    # outer = session.find.css("#outer", all=False)
-    # center = get_inview_center(outer.rect, get_viewport_rect(session))
-    div_point = { "x": 82, "y": 187}
 
-    wheel_chain \
-        .scroll(div_point["x"], div_point["y"], 5, 10, origin="viewport") \
-        .perform()
+    outer = session.find.css("#outer", all=False)
+    wheel_chain.scroll(0, 0, 5, 10, origin=outer).perform()
     events = get_events(session)
     assert len(events) > 0
     event_types = [e["type"] for e in events]
@@ -35,11 +30,27 @@ def test_wheel_scroll(session, test_actions_page, wheel_chain):
             assert e["deltaX"] >= 5
             assert e["deltaY"] >= 10
             assert e["deltaZ"] == 0
+            assert e["target"] == "outer"
 
-def test_wheel_scroll_iframe(session, test_actions_page, wheel_chain):
+def test_wheel_scroll_overflow(session, test_actions_scroll_page, wheel_chain):
     session.execute_script("document.scrollingElement.scrollTop = 0")
-    # outer = session.find.css("#outer", all=False)
-    # center = get_inview_center(outer.rect, get_viewport_rect(session))
+
+    scrollable = session.find.css("#scrollable", all=False)
+    wheel_chain.scroll(0, 0, 5, 10, origin=scrollable).perform()
+    events = get_events(session)
+    assert len(events) > 0
+    event_types = [e["type"] for e in events]
+    assert "wheel" in event_types
+    for e in events:
+        if e["type"] == "wheel":
+            assert e["deltaX"] >= 5
+            assert e["deltaY"] >= 10
+            assert e["deltaZ"] == 0
+            assert e["target"] == "scrollable"
+
+def test_wheel_scroll_iframe(session, test_actions_scroll_page, wheel_chain):
+    session.execute_script("document.scrollingElement.scrollTop = 0")
+
     div_point = { "x": 82, "y": 187}
 
     wheel_chain \

77cca6f5c1ef1e5154e69c5f2cbe3fe5cfbd992c
Lan Wei <weilanweilan@gmail.com>
2020-09-09 01:34:10 -0400

    Update wheel.py

--- webdriver/tests/perform_actions/wheel.py
+++ webdriver/tests/perform_actions/wheel.py
@@ -16,11 +16,11 @@ def test_no_browsing_context(session, closed_window, wheel_chain):
         wheel_chain.scroll(0, 0, 0, 10).perform()
 
 
-def test_wheel_scroll(session, test_actions_page, wheel_chain):
+def test_wheel_scroll(session, test_actions_scroll_page, wheel_chain):
     session.execute_script("document.scrollingElement.scrollTop = 0")
-    div_point = { "x": 82, "y": 187}
 
-    wheel_chain.scroll(div_point["x"], div_point["y"], 5, 10, origin="viewport").perform()
+    outer = session.find.css("#outer", all=False)
+    wheel_chain.scroll(0, 0, 5, 10, origin=outer).perform()
     events = get_events(session)
     assert len(events) > 0
     event_types = [e["type"] for e in events]
@@ -30,3 +30,38 @@ def test_wheel_scroll(session, test_actions_page, wheel_chain):
             assert e["deltaX"] >= 5
             assert e["deltaY"] >= 10
             assert e["deltaZ"] == 0
+            assert e["target"] == "outer"
+
+
+def test_wheel_scroll_overflow(session, test_actions_scroll_page, wheel_chain):
+    session.execute_script("document.scrollingElement.scrollTop = 0")
+
+    scrollable = session.find.css("#scrollable", all=False)
+    wheel_chain.scroll(0, 0, 5, 10, origin=scrollable).perform()
+    events = get_events(session)
+    assert len(events) > 0
+    event_types = [e["type"] for e in events]
+    assert "wheel" in event_types
+    for e in events:
+        if e["type"] == "wheel":
+            assert e["deltaX"] >= 5
+            assert e["deltaY"] >= 10
+            assert e["deltaZ"] == 0
+            assert e["target"] == "scrollContent"
+
+
+def test_wheel_scroll_iframe(session, test_actions_scroll_page, wheel_chain):
+    session.execute_script("document.scrollingElement.scrollTop = 0")
+
+    subframe = session.find.css("#subframe", all=False)
+    wheel_chain.scroll(0, 0, 5, 10, origin=subframe).perform()
+    events = get_events(session)
+    assert len(events) > 0
+    event_types = [e["type"] for e in events]
+    assert "wheel" in event_types
+    for e in events:
+        if e["type"] == "wheel":
+            assert e["deltaX"] >= 5
+            assert e["deltaY"] >= 10
+            assert e["deltaZ"] == 0
+            assert e["target"] == "iframeContent"

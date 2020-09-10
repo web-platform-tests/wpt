b6fb4c97746e7584b0f05efa67c76d7cf711d104
Lan Wei <weilanweilan@gmail.com>
2020-09-10 03:07:31 -0400

    Update conftest.py

--- webdriver/tests/perform_actions/conftest.py
+++ webdriver/tests/perform_actions/conftest.py
@@ -39,7 +39,7 @@ def none_chain(session):
 def wheel_chain(session):
     return session.actions.sequence("wheel", "wheel_id")
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(autouse=False)
 def release_actions(session, request):
     # release all actions after each test
     # equivalent to a teardown_function, but with access to session fixture

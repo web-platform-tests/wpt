366f2708881c8df808e49e537f4e3a9413160581
Lan Wei <weilanweilan@gmail.com>
2020-09-10 01:50:58 -0400

    Update wheel.py

--- webdriver/tests/perform_actions/wheel.py
+++ webdriver/tests/perform_actions/wheel.py
@@ -20,8 +20,9 @@ def test_wheel_scroll(session, test_actions_page, wheel_chain):
     session.execute_script("document.scrollingElement.scrollTop = 0")
     div_point = { "x": 82, "y": 187}
 
-    # outer = session.find.css("#outer", all=False)
-    wheel_chain.scroll(div_point["x"], div_point["y"], 5, 10, origin="viewport").perform()
+    wheel_chain \
+        .scroll(div_point["x"], div_point["y"], 5, 10, origin="viewport") \
+        .perform()
     events = get_events(session)
     assert len(events) > 0
     event_types = [e["type"] for e in events]
@@ -31,38 +32,3 @@ def test_wheel_scroll(session, test_actions_page, wheel_chain):
             assert e["deltaX"] >= 5
             assert e["deltaY"] >= 10
             assert e["deltaZ"] == 0
-            assert e["target"] == "outer"
-
-
-# def test_wheel_scroll_overflow(session, test_actions_scroll_page, wheel_chain):
-#     session.execute_script("document.scrollingElement.scrollTop = 0")
-
-#     scrollable = session.find.css("#scrollable", all=False)
-#     wheel_chain.scroll(0, 0, 5, 10, origin=scrollable).perform()
-#     events = get_events(session)
-#     assert len(events) > 0
-#     event_types = [e["type"] for e in events]
-#     assert "wheel" in event_types
-#     for e in events:
-#         if e["type"] == "wheel":
-#             assert e["deltaX"] >= 5
-#             assert e["deltaY"] >= 10
-#             assert e["deltaZ"] == 0
-#             assert e["target"] == "scrollContent"
-
-
-# def test_wheel_scroll_iframe(session, test_actions_scroll_page, wheel_chain):
-#     session.execute_script("document.scrollingElement.scrollTop = 0")
-
-#     subframe = session.find.css("#subframe", all=False)
-#     wheel_chain.scroll(0, 0, 5, 10, origin=subframe).perform()
-#     events = get_events(session)
-#     assert len(events) > 0
-#     event_types = [e["type"] for e in events]
-#     assert "wheel" in event_types
-#     for e in events:
-#         if e["type"] == "wheel":
-#             assert e["deltaX"] >= 5
-#             assert e["deltaY"] >= 10
-#             assert e["deltaZ"] == 0
-#             assert e["target"] == "iframeContent"

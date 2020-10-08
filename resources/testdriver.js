c4311f67a2f5da354d78a92d21319a4b4aa58172
James Graham <james@hoppipolla.co.uk>
2020-10-08 14:25:31 +0100

    Update the documentation for testdriver in other contexts

--- resources/testdriver.js
+++ resources/testdriver.js
@@ -52,8 +52,7 @@
         /**
          * Set the context in which testharness.js is loaded
          *
-         * @param {Window} context - the window containing testharness.js
-         *
+         * @param {WindowProxy} context - the window containing testharness.js
          **/
         set_test_context: function(context) {
           if (window.test_driver_internal.set_test_context) {
@@ -66,7 +65,6 @@
          * postMessage to the context containing testharness.js
          *
          * @param {Object} msg - the data to POST
-         *
          **/
         message_test: function(msg) {
             let target = testharness_context;
@@ -85,6 +83,9 @@
          * @param {String} intent - a description of the action which much be
          *                          triggered by user interaction
          * @param {Function} action - code requiring escalated privileges
+         * @param {WindowProxy} context - Browsing context in which
+         *                                to run the call, or null for the current
+         *                                browsing context.
          *
          * @returns {Promise} fulfilled following user interaction and
          *                    execution of the provided `action` function;
@@ -181,6 +182,10 @@
          * https://github.com/WICG/page-lifecycle/blob/master/README.md|Lifecycle API
          * for Web Pages}
          *
+         * @param {WindowProxy} context - Browsing context in which
+         *                                to run the call, or null for the current
+         *                                browsing context.
+         *
          * @returns {Promise} fulfilled after the freeze request is sent, or rejected
          *                    in case the WebDriver command errors
          */
@@ -196,14 +201,18 @@
          * https://w3c.github.io/webdriver/#actions|WebDriver Actions Command}
          *
          * @param {Array} actions - an array of actions. The format is the same as the actions
-                                    property of the WebDriver command {@link
-                                    https://w3c.github.io/webdriver/#perform-actions|Perform
-                                    Actions} command. Each element is an object representing an
-                                    input source and each input source itself has an actions
-                                    property detailing the behaviour of that source at each timestep
-                                    (or tick). Authors are not expected to construct the actions
-                                    sequence by hand, but to use the builder api provided in
-                                    testdriver-actions.js
+         *                          property of the WebDriver command {@link
+         *                          https://w3c.github.io/webdriver/#perform-actions|Perform
+         *                          Actions} command. Each element is an object representing an
+         *                          input source and each input source itself has an actions
+         *                          property detailing the behaviour of that source at each timestep
+         *                          (or tick). Authors are not expected to construct the actions
+         *                          sequence by hand, but to use the builder api provided in
+         *                          testdriver-actions.js
+         * @param {WindowProxy} context - Browsing context in which
+         *                                to run the call, or null for the current
+         *                                browsing context.
+         *
          * @returns {Promise} fufiled after the actions are performed, or rejected in
          *                    the cases the WebDriver command errors
          */
@@ -217,6 +226,10 @@
          * The generate_test_report function generates a report (to be observed
          * by ReportingObserver) for testing purposes, as described in
          * {@link https://w3c.github.io/reporting/#generate-test-report-command}
+         *
+         * @param {WindowProxy} context - Browsing context in which
+         *                                to run the call, or null for the current
+         *                                browsing context.
          *
          * @returns {Promise} fulfilled after the report is generated, or
          *                    rejected if the report generation fails
@@ -236,6 +249,9 @@
          *                              object
          * @param {String} state - the state of the permission
          * @param {boolean} one_realm - Optional. Whether the permission applies to only one realm
+         * @param {WindowProxy} context - Browsing context in which
+         *                                to run the call, or null for the current
+         *                                browsing context.
          *
          * The above params are used to create a [PermissionSetParameters]{@link
          * https://w3c.github.io/permissions/#dictdef-permissionsetparameters} object
@@ -262,6 +278,10 @@
          * @param {Object} config - an [Authenticator Configuration]{@link
          *                          https://w3c.github.io/webauthn/#authenticator-configuration}
          *                          object
+         * @param {WindowProxy} context - Browsing context in which
+         *                                to run the call, or null for the current
+         *                                browsing context.
+         *
          * @returns {Promise} fulfilled after the authenticator is added, or
          *                    rejected in the cases the WebDriver command
          *                    errors. Returns the ID of the authenticator
@@ -279,6 +299,9 @@
          *
          * @param {String} authenticator_id - the ID of the authenticator to be
          *                                    removed.
+         * @param {WindowProxy} context - Browsing context in which
+         *                                to run the call, or null for the current
+         *                                browsing context.
          *
          * @returns {Promise} fulfilled after the authenticator is removed, or
          *                    rejected in the cases the WebDriver command
@@ -297,6 +320,9 @@
          * @param {Object} credential - A [Credential Parameters]{@link
          *                              https://w3c.github.io/webauthn/#credential-parameters}
          *                              object
+         * @param {WindowProxy} context - Browsing context in which
+         *                                to run the call, or null for the current
+         *                                browsing context.
          *
          * @returns {Promise} fulfilled after the credential is added, or
          *                    rejected in the cases the WebDriver command
@@ -315,6 +341,9 @@
          * https://w3c.github.io/webauthn/#sctn-automation-get-credentials
          *
          * @param {String} authenticator_id - the ID of the authenticator
+         * @param {WindowProxy} context - Browsing context in which
+         *                                to run the call, or null for the current
+         *                                browsing context.
          *
          * @returns {Promise} fulfilled after the credentials are returned, or
          *                    rejected in the cases the WebDriver command
@@ -333,6 +362,9 @@
          *
          * @param {String} authenticator_id - the ID of the authenticator
          * @param {String} credential_id - the ID of the credential
+         * @param {WindowProxy} context - Browsing context in which
+         *                                to run the call, or null for the current
+         *                                browsing context.
          *
          * @returns {Promise} fulfilled after the credential is removed, or
          *                    rejected in the cases the WebDriver command
@@ -348,6 +380,9 @@
          * https://w3c.github.io/webauthn/#sctn-automation-remove-all-credentials
          *
          * @param {String} authenticator_id - the ID of the authenticator
+         * @param {WindowProxy} context - Browsing context in which
+         *                                to run the call, or null for the current
+         *                                browsing context.
          *
          * @returns {Promise} fulfilled after the credentials are removed, or
          *                    rejected in the cases the WebDriver command
@@ -366,6 +401,9 @@
          *
          * @param {String} authenticator_id - the ID of the authenticator
          * @param {boolean} uv - the User Verified flag
+         * @param {WindowProxy} context - Browsing context in which
+         *                                to run the call, or null for the current
+         *                                browsing context.
          */
         set_user_verified: function(authenticator_id, uv, context=null) {
             return window.test_driver_internal.set_user_verified(authenticator_id, uv, context);
@@ -385,6 +423,9 @@
          *                                    May be "*" to indicate all origins.
          * @param {String} state - The storage access setting.
          *                         Must be either "allowed" or "blocked".
+         * @param {WindowProxy} context - Browsing context in which
+         *                                to run the call, or null for the current
+         *                                browsing context.
          *
          * @returns {Promise} Fulfilled after the storage access rule has been
          *                    set, or rejected if setting the rule fails.
@@ -407,13 +448,6 @@
          */
         in_automation: false,
 
-        /**
-         * Waits for a user-initiated click
-         *
-         * @param {Element} element - element to be clicked
-         * @param {{x: number, y: number} coords - viewport coordinates to click at
-         * @returns {Promise} fulfilled after click occurs
-         */
         click: function(element, coords) {
             if (this.in_automation) {
                 return Promise.reject(new Error('Not implemented'));
@@ -424,14 +458,6 @@
             });
         },
 
-        /**
-         * Waits for an element to receive a series of key presses
-         *
-         * @param {Element} element - element which should receve key presses
-         * @param {String} keys - keys to expect
-         * @returns {Promise} fulfilled after keys are received or rejected if
-         *                    an incorrect key sequence is received
-         */
         send_keys: function(element, keys) {
             if (this.in_automation) {
                 return Promise.reject(new Error('Not implemented'));
@@ -464,157 +490,51 @@
             });
         },
 
-        /**
-         * Freeze the current page
-         *
-         * @returns {Promise} fulfilled after freeze request is sent, otherwise
-         * it gets rejected
-         */
-        freeze: function() {
+        freeze: function(context=null) {
             return Promise.reject(new Error("unimplemented"));
         },
 
-        /**
-         * Send a sequence of pointer actions
-         *
-         * @returns {Promise} fufilled after actions are sent, rejected if any actions
-         *                    fail
-         */
         action_sequence: function(actions, context=null) {
             return Promise.reject(new Error("unimplemented"));
         },
 
-        /**
-         * Generates a test report on the current page
-         *
-         * @param {String} message - the message to be contained in the report
-         * @returns {Promise} fulfilled after the report is generated, or
-         *                    rejected if the report generation fails
-         */
         generate_test_report: function(message, context=null) {
             return Promise.reject(new Error("unimplemented"));
         },
 
 
-        /**
-         * Sets the state of a permission
-         *
-         * This function simulates a user setting a permission into a particular state as described
-         * in {@link https://w3c.github.io/permissions/#set-permission-command}
-         *
-         * @param {Object} permission_params - a [PermissionSetParameters]{@lint
-         *                                     https://w3c.github.io/permissions/#dictdef-permissionsetparameters}
-         *                                     object
-         * @returns {Promise} fulfilled after the permission is set, or rejected if setting the
-         *                    permission fails
-         */
         set_permission: function(permission_params, context=null) {
             return Promise.reject(new Error("unimplemented"));
         },
 
-        /**
-         * Creates a virtual authenticator
-         *
-         * @param {Object} config - the authenticator configuration
-         * @returns {Promise} fulfilled after the authenticator is added, or
-         *                    rejected in the cases the WebDriver command
-         *                    errors.
-         */
         add_virtual_authenticator: function(config, context=null) {
             return Promise.reject(new Error("unimplemented"));
         },
 
-        /**
-         * Removes a virtual authenticator
-         *
-         * @param {String} authenticator_id - the ID of the authenticator to be
-         *                                    removed.
-         *
-         * @returns {Promise} fulfilled after the authenticator is removed, or
-         *                    rejected in the cases the WebDriver command
-         *                    errors
-         */
         remove_virtual_authenticator: function(authenticator_id, context=null) {
             return Promise.reject(new Error("unimplemented"));
         },
 
-        /**
-         * Adds a credential to a virtual authenticator
-         *
-         * @param {String} authenticator_id - the ID of the authenticator
-         * @param {Object} credential - A [Credential Parameters]{@link
-         *                              https://w3c.github.io/webauthn/#credential-parameters}
-         *                              object
-         *
-         * @returns {Promise} fulfilled after the credential is added, or
-         *                    rejected in the cases the WebDriver command
-         *                    errors
-         *
-         */
         add_credential: function(authenticator_id, credential, context=null) {
             return Promise.reject(new Error("unimplemented"));
         },
 
-        /**
-         * Gets all the credentials stored in an authenticator
-         *
-         * @param {String} authenticator_id - the ID of the authenticator
-         *
-         * @returns {Promise} fulfilled after the credentials are returned, or
-         *                    rejected in the cases the WebDriver command
-         *                    errors. Returns an array of [Credential
-         *                    Parameters]{@link
-         *                    https://w3c.github.io/webauthn/#credential-parameters}
-         *
-         */
         get_credentials: function(authenticator_id, context=null) {
             return Promise.reject(new Error("unimplemented"));
         },
 
-        /**
-         * Remove a credential stored in an authenticator
-         *
-         * @param {String} authenticator_id - the ID of the authenticator
-         * @param {String} credential_id - the ID of the credential
-         *
-         * @returns {Promise} fulfilled after the credential is removed, or
-         *                    rejected in the cases the WebDriver command
-         *                    errors.
-         *
-         */
         remove_credential: function(authenticator_id, credential_id, context=null) {
             return Promise.reject(new Error("unimplemented"));
         },
 
-        /**
-         * Removes all the credentials stored in a virtual authenticator
-         *
-         * @param {String} authenticator_id - the ID of the authenticator
-         *
-         * @returns {Promise} fulfilled after the credentials are removed, or
-         *                    rejected in the cases the WebDriver command
-         *                    errors.
-         *
-         */
         remove_all_credentials: function(authenticator_id, context=null) {
             return Promise.reject(new Error("unimplemented"));
         },
 
-        /**
-         * Sets the User Verified flag on an authenticator
-         *
-         * @param {String} authenticator_id - the ID of the authenticator
-         * @param {boolean} uv - the User Verified flag
-         *
-         */
         set_user_verified: function(authenticator_id, uv, context=null) {
             return Promise.reject(new Error("unimplemented"));
         },
 
-        /**
-         * Sets the storage access policy for a third-party origin when loaded
-         * in the current first party context
-         */
         set_storage_access: function(origin, embedding_origin, blocked, context=null) {
             return Promise.reject(new Error("unimplemented"));
         },

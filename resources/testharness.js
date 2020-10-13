73a674f44bdb1300672f7ee76980f2f99ec0dcbc
Sam Sneddon <gsnedders@apple.com>
2020-10-13 15:42:29 +0100

    fixup! Handle empty arrow functions when determining name in testharness.js

--- resources/testharness.js
+++ resources/testharness.js
@@ -534,9 +534,9 @@ policies and contribution forms [3].
 
             // Check for JS line separators
             if (arrow !== null && !/[\u000A\u000D\u2028\u2029]/.test(func_code)) {
-                var trimmed = arrow[arrow.length].trim();
+                var trimmed = (arrow[1] !== undefined ? arrow[1] : arrow[2]).trim();
                 // drop trailing ; if there's no earlier ones
-                if (/^[^;]*(;+\s*)+$/.test(trimmed)) trimmed = trimmed.substring(0, trimmed.length - 1);
+                trimmed = trimmed.replace(/^([^;]*)(;\s*)+$/, "$1");
                 // ignore this if it's then empty
                 if (trimmed) return trimmed;
             }

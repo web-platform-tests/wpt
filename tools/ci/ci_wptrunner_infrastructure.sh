0e77b1fcf10a5e77ff48116d2ceedf749b1435a0
Stephen McGruer <smcgruer@chromium.org>
2021-01-08 10:03:22 -0500

    [Taskcluster] Run Py3 infrastructure/ tests on Firefox as well as Chrome

--- tools/ci/ci_wptrunner_infrastructure.sh
+++ tools/ci/ci_wptrunner_infrastructure.sh
@@ -11,17 +11,13 @@ test_infrastructure() {
 }
 
 main() {
-    if [[ $# -eq 1 && "$1" = "--py3" ]]; then
-        PRODUCTS=( "chrome" )
-    else
-        PRODUCTS=( "firefox" "chrome" )
-    fi
+    PRODUCTS=( "firefox" "chrome" )
     ./wpt manifest --rebuild -p ~/meta/MANIFEST.json
     for PRODUCT in "${PRODUCTS[@]}"; do
         if [[ "$PRODUCT" == "chrome" ]]; then
             test_infrastructure "--binary=$(which google-chrome-unstable) --channel dev" "$1"
         else
-            test_infrastructure "--binary=~/build/firefox/firefox"
+            test_infrastructure "--binary=~/build/firefox/firefox" "$1"
         fi
     done
 }

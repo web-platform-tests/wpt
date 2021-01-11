be93f28b4ac59d0a628d840e80fde5fc08cd4185
Manuel Rego Casasnovas <rego@igalia.com>
2021-01-11 10:37:04 +0100

    NOT FOR LANDING: testharness.js remove unused code

--- resources/testharness.js
+++ resources/testharness.js
@@ -1956,10 +1956,6 @@ policies and contribution forms [3].
         this.index = null;
 
         this.properties = properties || {};
-        this.timeout_length = settings.test_timeout;
-        if (this.timeout_length !== null) {
-            this.timeout_length *= tests.timeout_multiplier;
-        }
 
         this.message = null;
         this.stack = null;

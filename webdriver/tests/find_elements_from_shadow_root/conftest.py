694cc740f754f386daa446353926a25ef16695b7
Jim Evans <james.evans@salesforce.com>
2021-01-12 10:54:48 -0500

    Fleshing out tests for find elements from shadow root

---
+++ webdriver/tests/find_elements_from_shadow_root/conftest.py
@@ -0,0 +1,15 @@
+@pytest.fixture
+def get_shadow_page(inline, shadow_content):
+    return inline("""
+        <custom-shadow-element></custom-shadow-element>
+        <script>
+            customElements.define('custom-shadow-element',
+                class extends HTMLElement {{
+                    constructor() {{
+                            super();
+                            this.attachShadow({{mode: 'open'}}).innerHTML = `
+                                {{ {0} }}
+                            `;
+                        }}
+                }});
+        </script>""".format(shadow_content))

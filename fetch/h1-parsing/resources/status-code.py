0de7a3caaa0dc148d4dd1721bf731019ce4934f4
Anne van Kesteren <annevk@annevk.nl>
2021-02-01 18:39:54 +0100

    less b

--- fetch/h1-parsing/resources/status-code.py
+++ fetch/h1-parsing/resources/status-code.py
@@ -1,6 +1,6 @@
 def main(request, response):
     output = b"HTTP/1.1 "
     output += request.GET.first(b"input")
-    output += b"\n" + b"header-parsing: is sad" + b"\n"
+    output += b"\nheader-parsing: is sad\n"
     response.writer.write(output)
     response.close_connection = True

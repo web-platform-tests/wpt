2af527f4c0e0f1472eecf72b524337c5efbb2f3f
Jake Archibald <jaffathecake@gmail.com>
2021-02-19 14:20:08 +0000

    Fixes following feedback

--- service-workers/service-worker/resources/no-dynamic-import.js
+++ service-workers/service-worker/resources/no-dynamic-import.js
@@ -1,8 +1,8 @@
 /** @type {[name: string, url: string][]} */
 const importUrlTests = [
-  ["Module URL", "./resources/basic-module.js"],
+  ["Module URL", "./basic-module.js"],
   // In no-dynamic-import-in-module.any.js, this module is also statically imported
-  ["Another module URL", "./resources/basic-module-2.js"],
+  ["Another module URL", "./basic-module-2.js"],
   [
     "Module data: URL",
     "data:text/javascript;charset=utf-8," +
@@ -11,12 +11,9 @@ const importUrlTests = [
 ];
 
 for (const [name, url] of importUrlTests) {
-  promise_test(async () => {
-    try {
-      await import(url);
-      assert_unreached("Import must not fulfill");
-    } catch (err) {
-      assert_equals(err.constructor, TypeError, "TypeError thrown");
-    }
-  }, name);
+  promise_test(
+    async (t) =>
+      promise_rejects_js(t, TypeError, import(url), "Import must reject"),
+    name
+  );
 }

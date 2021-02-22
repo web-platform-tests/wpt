898dfd23762b38e14a86d9739a280bc80df1eeea
Shengfa Lin <shengfa@google.com>
2021-02-22 16:30:33 -0600

    Seperate out bidi_session from session in fixtures.py
    Removed bidi markers and created bidi_session fixtures
    
    Add handling in bidi_session and session start function
    to disguish bidi_session and session so that async functions
    can be awaited.
    
    Modify tests to have better names.

--- webdriver/tests/bidi/new_session/connect.py
+++ webdriver/tests/bidi/new_session/connect.py
@@ -1,43 +1,42 @@
 import pytest
 import asyncio
 import websockets
+import webdriver
 
 # classic session to enable bidi capability manually
 @pytest.mark.asyncio
 @pytest.mark.capabilities({"webSocketUrl": True})
 async def test_websocket_url_connect(session):
+    assert not isinstance(session, webdriver.BidiSession)
     websocket_url = session.capabilities["webSocketUrl"]
     async with websockets.connect(websocket_url) as websocket:
         await websocket.send("Hello world!")
         await websocket.close()
 
 # bidi session following classic session to test session
-# recreation with bidi true in session fixture.
-# Close websocket at the end.
+# recreation in session fixture
+# also test send message via websocket and
+# close websocket at the end.
 @pytest.mark.asyncio
-@pytest.mark.bidi(True)
-async def test_bidi_session_1(session):
-    await session.websocket_transport.send("test_bidi_session_1")
-    await session.websocket_transport.close()
+async def test_bidi_session_send_and_close(bidi_session):
+    assert isinstance(bidi_session, webdriver.BidiSession)
+    await bidi_session.websocket_transport.send("test_bidi_session: send and close")
+    await bidi_session.websocket_transport.close()
 
-# bidi session following a bidi session with the same capabilities
-# but closed websocket to test restart of websocket connection.
+# test send after close
 @pytest.mark.asyncio
-@pytest.mark.bidi(True)
-async def test_bidi_session_2(session):
-    await session.websocket_transport.send("test_bidi_session_2")
-    await session.websocket_transport.close()
+async def test_bidi_session_send_after_close(bidi_session):
+    await bidi_session.websocket_transport.send("test_bidi_session: send after close")
 
 # bidi session following a bidi session with a different capabilities
 # to test session recreation
 @pytest.mark.asyncio
-@pytest.mark.bidi(True)
 @pytest.mark.capabilities({"acceptInsecureCerts": True})
-async def test_bidi_session_3(session):
-    await session.websocket_transport.send("test_bidi_session_3")
+async def test_bidi_session_with_different_capability(bidi_session):
+    await bidi_session.websocket_transport.send("test_bidi_session: different capability")
 
 # classic session following a bidi session to test session
 # recreation
 @pytest.mark.asyncio
-async def test_classic(session):
-    pass
+def test_classic_after_bidi_session(session):
+    assert not isinstance(session, webdriver.BidiSession)

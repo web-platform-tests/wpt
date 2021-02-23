1abbaacbe6091d252040e6a5405e4ee90b3a64f8
Shengfa Lin <shengfa@google.com>
2021-02-23 13:45:24 -0600

    Remove webdriver bidi infrastructure tests
    Also delete some comments requiring specific order for test.
    Added comment for first and last test to avoid breaking classic
    webdriver session tests.

--- webdriver/tests/bidi/new_session/connect.py
+++ webdriver/tests/bidi/new_session/connect.py
@@ -4,6 +4,7 @@ import websockets
 import webdriver
 
 # classic session to enable bidi capability manually
+# Intended to be the first test in this file
 @pytest.mark.asyncio
 @pytest.mark.capabilities({"webSocketUrl": True})
 async def test_websocket_url_connect(session):
@@ -11,22 +12,12 @@ async def test_websocket_url_connect(session):
     websocket_url = session.capabilities["webSocketUrl"]
     async with websockets.connect(websocket_url) as websocket:
         await websocket.send("Hello world!")
-        await websocket.close()
 
-# bidi session following classic session to test session
-# recreation in session fixture
-# also test send message via websocket and
-# close websocket at the end.
+# test bidi_session send
+# using bidi_session is the recommended way to test bidi
 @pytest.mark.asyncio
-async def test_bidi_session_send_and_close(bidi_session):
-    assert isinstance(bidi_session, webdriver.BidiSession)
-    await bidi_session.websocket_transport.send("test_bidi_session: send and close")
-    await bidi_session.websocket_transport.close()
-
-# test send after close
-@pytest.mark.asyncio
-async def test_bidi_session_send_after_close(bidi_session):
-    await bidi_session.websocket_transport.send("test_bidi_session: send after close")
+async def test_bidi_session_send(bidi_session):
+    await bidi_session.websocket_transport.send("test_bidi_session: send")
 
 # bidi session following a bidi session with a different capabilities
 # to test session recreation
@@ -37,6 +28,8 @@ async def test_bidi_session_with_different_capability(bidi_session):
 
 # classic session following a bidi session to test session
 # recreation
+# Intended to be the last test in this file to make sure
+# classic session is not impacted by bidi tests
 @pytest.mark.asyncio
 def test_classic_after_bidi_session(session):
     assert not isinstance(session, webdriver.BidiSession)

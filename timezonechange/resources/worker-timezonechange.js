6112bdb2f9681489d3ccb8aeb6de479fae25026a
Mathias Bynens <mathias@qiwi.be>
2021-02-24 09:56:40 +0100

    Update timezonechange/resources/worker-timezonechange.js

--- timezonechange/resources/worker-timezonechange.js
+++ timezonechange/resources/worker-timezonechange.js
@@ -1,6 +1,6 @@
 const oldtimezone = (new Intl.DateTimeFormat()).resolvedOptions().timeZone;
 ontimezonechange = evt => {
-  let timezone = (new Intl.DateTimeFormat()).resolvedOptions().timeZone;
+  const timezone = (new Intl.DateTimeFormat()).resolvedOptions().timeZone;
   postMessage("SUCCESS:" + timezone);
 };
 postMessage("READY:" + oldtimezone);

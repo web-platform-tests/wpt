5237bd5518401f04a22e6cbb0a86db0cdaa9bfef
Arno Renevier <arenevier@fb.com>
2021-02-25 14:39:26 -0800

    websockets/cookies: specify wpt_flags=https for wss variants
    Right now, wss variants will fail because they are not served over
    https. And an insecure page cannot set a secure cookie.
    
    As a solution, we load those variants with wpt_flags=https. It will
    force the test to be loaded over https.
    
    006.https.html was a previous way of solving the same issue. We can now
    get rid of it, and simply use 006.html with the correct variant set.

--- websockets/constants.js
+++ websockets/constants.js
@@ -4,10 +4,21 @@ var PORT = "{{ports[ws][0]}}";
 var PORT_SSL = "{{ports[wss][0]}}";
 var PORT_H2 = "{{ports[h2][0]}}";
 
+function url_has_variant(variant) {
+  if (typeof URLSearchParams === "function") {
+    let params = new URLSearchParams(location.search);
+    return params.get(variant) === "";
+  } else {
+    let params = location.search.substring(1).split('&');
+    return params.indexOf(variant) !== -1;
+  }
+}
+
+
 var SCHEME_DOMAIN_PORT;
-if (location.search == '?wss') {
+if (url_has_variant('wss')) {
   SCHEME_DOMAIN_PORT = 'wss://{{host}}:' + PORT_SSL;
-} else if (location.search == '?wpt_flags=h2') {
+} else if (url_has_variant('h2')) {
   SCHEME_DOMAIN_PORT = 'wss://{{host}}:' + PORT_H2;
 } else {
   SCHEME_DOMAIN_PORT = 'ws://{{host}}:' + PORT;

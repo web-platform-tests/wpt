92e6b015476546aa7fe8b4c0d7c7c2459c4b6372
Arno Renevier <arenevier@fb.com>
2021-02-26 21:32:29 -0800

    add wpt_flags=h2 variant to tests
    All those tests have been checked to pass manually (or, if the wss
    variant fails, to fail in the same way)
    
    Some websocket handlers needed to be modified to accept lower case
    headers.
    
    We also need to fix detection of h2 in constants.js: h2 is not specified
    with h2 parameter, but with wpt_flags=h2 parameter.

--- websockets/constants.js
+++ websockets/constants.js
@@ -5,20 +5,21 @@ var PORT_SSL = "{{ports[wss][0]}}";
 var PORT_H2 = "{{ports[h2][0]}}";
 
 function url_has_variant(variant) {
-  if (typeof URLSearchParams === "function") {
-    let params = new URLSearchParams(location.search);
-    return params.get(variant) === "";
-  } else {
-    let params = location.search.substring(1).split('&');
-    return params.indexOf(variant) !== -1;
-  }
+  let params = new URLSearchParams(location.search);
+  return params.get(variant) === "";
 }
+
+function url_has_flag(flag) {
+  let params = new URLSearchParams(location.search);
+  return params.getAll("wpt_flags").indexOf(flag) !== -1;
+}
+
 
 
 var SCHEME_DOMAIN_PORT;
 if (url_has_variant('wss')) {
   SCHEME_DOMAIN_PORT = 'wss://{{host}}:' + PORT_SSL;
-} else if (url_has_variant('h2')) {
+} else if (url_has_flag('h2')) {
   SCHEME_DOMAIN_PORT = 'wss://{{host}}:' + PORT_H2;
 } else {
   SCHEME_DOMAIN_PORT = 'ws://{{host}}:' + PORT;

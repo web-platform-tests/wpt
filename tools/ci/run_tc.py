3ad720f462aa8419849d9a619bd4e7c387c54bc8
Stephen McGruer <smcgruer@chromium.org>
2021-03-24 09:15:26 -0400

    Verify end-point: 865011

--- tools/ci/run_tc.py
+++ tools/ci/run_tc.py
@@ -140,7 +140,9 @@ def install_certificates():
 
 def install_chrome(channel):
     # Bisecting from 863581 to 865012
-    url = "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64/865012/chrome-linux.zip"
+    # Verifying range: 863584..865011
+    rev = "865011"
+    url = "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64/{}/chrome-linux.zip".format(rev)
     dest = "/tmp/google-chrome-unstable.zip"
     resp = urlopen(url)
     with open(dest, 'wb') as f:

cdde8ace211445525ee8859734393795bf1774bc
Anne van Kesteren <annevk@annevk.nl>
2021-03-24 16:22:43 +0100

    p3

--- cookies/resources/bytes.py
+++ cookies/resources/bytes.py
@@ -1,13 +1,13 @@
-import urllib
+from six.moves.urllib.parse import quote
 
 def main(request, response):
-    if "set" in request.GET:
+    if b"set" in request.GET:
         response.headers.append(b"Set-Cookie", b"cookiesarebananas=\xFFtest\xFF")
         response.content = "set"
-    elif "get" in request.GET:
-        response.content = urllib.quote(request.headers["Cookie"])
-    elif "delete" in request.GET:
-        request.headers.append(b"Set-Cookie", b"cookiesarebananas=meh;Max-Age=0")
+    elif b"get" in request.GET:
+        response.content = quote(request.headers[b"Cookie"])
+    elif b"delete" in request.GET:
+        response.headers.append(b"Set-Cookie", b"cookiesarebananas=meh;Max-Age=0")
         response.content = "delete"
 
-    response.headers.append(b"Content-Type", "text/plain")
+    response.headers.append(b"Content-Type", b"text/plain")

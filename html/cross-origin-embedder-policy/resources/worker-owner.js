699033558f1b5272f3c40ddab42f4833fc589509
Simon Pieters <zcorpan@gmail.com>
2021-03-31 16:35:08 +0200

    Check for lack of report when none is expected with a timeout

--- html/cross-origin-embedder-policy/resources/worker-owner.js
+++ html/cross-origin-embedder-policy/resources/worker-owner.js
@@ -1,7 +1,7 @@
 const is_worker = !('window' in self);
 const parent_or_self = is_worker ? self : self.parent;
 
-function startWorkerAndObserveReports(worker_url, wait_for_report) {
+function startWorkerAndObserveReports(worker_url) {
   const worker = new Worker(worker_url);
   const result_promise = new Promise(resolve => {
     worker.onmessage = _ => resolve('success');
@@ -17,19 +17,13 @@ function startWorkerAndObserveReports(worker_url, wait_for_report) {
     observer.observe();
   });
 
-  if (wait_for_report) {
-    Promise.all([result_promise, report_promise]).then(results => {
-      parent_or_self.postMessage(results[1]);
-    });
-  } else {
-    result_promise.then(result => {
-      parent_or_self.postMessage([]);
-    });
-  }
+  Promise.all([result_promise, report_promise]).then(results => {
+    parent_or_self.postMessage(results[1]);
+  });
 }
 
 if (is_worker) {
   onmessage = e => {
-    startWorkerAndObserveReports(e.data.worker_url, e.data.wait_for_report);
+    startWorkerAndObserveReports(e.data.worker_url);
   };
 }

7341251d2aa7df26bc006e81fd0cedaeed4cab96
Yoichi Osato <yoichio@chromium.org>
2021-04-27 10:40:08 +0900

    nit 2

--- fetch/api/redirect/redirect-upload.h2.any.js
+++ fetch/api/redirect/redirect-upload.h2.any.js
@@ -8,7 +8,7 @@ const redirectLocation = "top.txt";
 
 async function fetchStreamRedirect(statusCode) {
   const url = RESOURCES_DIR + "redirect.h2.py" +
-   `?redirect_status=${statusCode}` + `&location=${redirectLocation}`;
+   `?redirect_status=${statusCode}&location=${redirectLocation}`;
   const requestInit = {method: "POST"};
   requestInit["body"] = new ReadableStream({start: controller => {
     const encoder = new TextEncoder();
@@ -18,14 +18,14 @@ async function fetchStreamRedirect(statusCode) {
   return fetch(url, requestInit);
 }
 
-promise_test(async function(){
+promise_test(async () => {
   const resp = await fetchStreamRedirect(303);
   assert_equals(resp.status, 200);
   assert_true(new URL(resp.url).pathname.endsWith(redirectLocation),
    "Response's url should be the redirected one");
 }, "Fetch upload streaming should be accepted on 303");
 
-for (var statusCode of [301, 302, 307, 308]) {
+for (const statusCode of [301, 302, 307, 308]) {
   promise_test(t => {
     return promise_rejects_js(t, TypeError, fetchStreamRedirect(statusCode));
   }, `Fetch upload streaming should fail on ${statusCode}`);

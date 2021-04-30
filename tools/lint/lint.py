3a998c015e913902875f4e0f2026fcc97779a0d4
Philip Jägenstedt <philip@foolip.org>
2021-04-30 23:21:37 +0200

    Revert "Small typeing hack to make mypy pass in Python 3.6"
    This reverts commit bbc488398fef18b6053ce0ce283ddfd28c97c015.

--- tools/lint/lint.py
+++ tools/lint/lint.py
@@ -14,7 +14,6 @@ import sys
 import tempfile
 
 from collections import defaultdict
-from typing import cast
 from urllib.parse import urlsplit, urljoin
 
 from . import fnmatch
@@ -845,9 +844,7 @@ def check_file_contents(repo_root, path, f=None):
     :param f: a file-like object with the file contents
     :returns: a list of errors found in ``f``
     """
-    file_ctx = (io.open(os.path.join(repo_root, path), 'rb') if f is None
-                else cast(io.BufferedReader, nullcontext(f)))
-    with file_ctx as real_f:
+    with io.open(os.path.join(repo_root, path), 'rb') if f is None else nullcontext(f) as real_f:
         assert real_f is not None  # Py2: prod mypy -2 into accepting this isn't None
         errors = []
         for file_fn in file_lints:

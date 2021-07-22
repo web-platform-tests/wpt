317a2c085c6f51ea21ab9e3bd2cfb7e2920cf6f7
bashi <bashi@chromium.org>
2021-07-22 11:59:14 +0900

    Remove unused import

--- tools/webtransport/h3/webtransport_h3_server.py
+++ tools/webtransport/h3/webtransport_h3_server.py
@@ -12,7 +12,7 @@ from aioquic.h3.connection import H3_ALPN, H3Connection
 from aioquic.h3.events import H3Event, HeadersReceived, WebTransportStreamDataReceived, DatagramReceived
 from aioquic.quic.configuration import QuicConfiguration
 from aioquic.quic.connection import stream_is_unidirectional
-from aioquic.quic.events import QuicEvent, ProtocolNegotiated, StreamDataReceived, StreamReset
+from aioquic.quic.events import QuicEvent, ProtocolNegotiated, StreamDataReceived
 from aioquic.tls import SessionTicket
 from aioquic.quic.packet import QuicErrorCode
 

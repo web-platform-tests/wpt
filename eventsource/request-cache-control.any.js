94d82aacb730b6970e22d711f95cab395fa32bf6
MierenManz <christiaanmens@gmail.com>
2021-08-15 23:38:59 +0200

    fix: recursive test timing out

--- eventsource/request-cache-control.any.js
+++ eventsource/request-cache-control.any.js
@@ -4,22 +4,32 @@
                     .replace(/\/[^\/]*$/, '/')
 
       // running it twice to check whether it stays consistent
-      function cacheTest(runAgain, url) {
-        var test = async_test()
+      function cacheTest(url) {
+        var test = async_test(url + "1")
+        // Recursive test. This avoids test that timeout
+        var test2 = async_test(url + "2")
         test.step(function() {
           var source = new EventSource(url)
           source.onmessage = function(e) {
             test.step(function() {
               assert_equals(e.data, "no-cache")
               this.close()
-              if(runAgain)
-                cacheTest(false, url) // this nests tests
+              test2.step(function() {
+                var source2 = new EventSource(url)
+                source2.onmessage = function(e) {
+                  test2.step(function() {
+                    assert_equals(e.data, "no-cache")
+                    this.close()
+                  }, this)
+                  test2.done()
+                }
+              })
             }, this)
             test.done()
           }
         })
       }
 
-      cacheTest(true, "resources/cache-control.event_stream?pipe=sub")
-      cacheTest(true, crossdomain + "resources/cors.py?run=cache-control")
+      cacheTest("resources/cache-control.event_stream?pipe=sub")
+      cacheTest(crossdomain + "resources/cors.py?run=cache-control")
 

3fa157157a4e01a30595f354765047a315e9626d
MierenManz <christiaanmens@gmail.com>
2021-08-26 21:38:00 +0200

    add title back

--- eventsource/format-utf-8.any.js
+++ eventsource/format-utf-8.any.js
@@ -1,3 +1,4 @@
+// META: title=EventSource always UTF-8
 async_test().step(function() {
   var source = new EventSource("resources/message.py?mime=text/event-stream%3bcharset=windows-1252&message=data%3Aok%E2%80%A6")
   source.onmessage = this.step_func(function(e) {

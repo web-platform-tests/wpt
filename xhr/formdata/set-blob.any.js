d49e8d6d9aaa77c4c5ebc5681e03304149354d71
feng <f3n67u@outlook.com>
2021-08-30 22:14:28 +0800

    feat: add more asserts

--- xhr/formdata/set-blob.any.js
+++ xhr/formdata/set-blob.any.js
@@ -5,8 +5,10 @@
 const formData = new FormData();
 
 test(() => {
-  formData.set("blob-1", new Blob());
+  const value = new Blob();
+  formData.set("blob-1", value);
   const blob1 = formData.get("blob-1");
+  assert_not_equals(blob1, value);
   assert_equals(blob1.constructor.name, "File");
   assert_equals(blob1.name, "blob");
   assert_equals(blob1.type, "");
@@ -15,8 +17,10 @@ test(() => {
 }, "blob without type");
 
 test(() => {
-  formData.set("blob-2", new Blob([], { type: "text/plain" }));
+  const value = new Blob([], { type: "text/plain" });
+  formData.set("blob-2", value);
   const blob2 = formData.get("blob-2");
+  assert_not_equals(blob2, value);
   assert_equals(blob2.constructor.name, "File");
   assert_equals(blob2.name, "blob");
   assert_equals(blob2.type, "text/plain");
@@ -24,8 +28,10 @@ test(() => {
 }, "blob with type");
 
 test(() => {
-  formData.set("blob-3", new Blob(), "custom name");
+  const value = new Blob();
+  formData.set("blob-3", value, "custom name");
   const blob3 = formData.get("blob-3");
+  assert_not_equals(blob3, value);
   assert_equals(blob3.constructor.name, "File");
   assert_equals(blob3.name, "custom name");
   assert_equals(blob3.type, "");
@@ -33,8 +39,10 @@ test(() => {
 }, "blob with custom name");
 
 test(() => {
-  formData.set("file-1", new File([], "name"));
+  const value = new File([], "name");
+  formData.set("file-1", value);
   const file1 = formData.get("file-1");
+  assert_equals(file1, value);
   assert_equals(file1.constructor.name, "File");
   assert_equals(file1.name, "name");
   assert_equals(file1.type, "");
@@ -42,8 +50,10 @@ test(() => {
 }, "file without lastModified or custom name");
 
 test(() => {
-  formData.set("file-2", new File([], "name", { lastModified: 123 }), "custom name");
+  const value = new File([], "name", { lastModified: 123 });
+  formData.set("file-2", value, "custom name");
   const file2 = formData.get("file-2");
+  assert_not_equals(file2, value);
   assert_equals(file2.constructor.name, "File");
   assert_equals(file2.name, "custom name");
   assert_equals(file2.type, "");

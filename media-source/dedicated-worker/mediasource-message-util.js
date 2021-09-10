03852461a85bd5b94625988deba1c626f72886c1
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-10 16:44:02 +0800

    Delete media-source directory

--- media-source/dedicated-worker/mediasource-message-util.js
+++
@@ -1,9 +0,0 @@
-// This script provides an object with common message subjects to assist main
-// and worker thread communication.
-
-const messageSubject = {
-  ERROR: "error",
-  OBJECT_URL: "object url",
-  STARTED_BUFFERING: "started buffering",
-  FINISHED_BUFFERING: "finished buffering",
-}

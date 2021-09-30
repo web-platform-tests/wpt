172ce5b1fa213ab03d05a83174e05f4de639078c
autofoolip <auto@foolip.org>
2021-09-30 23:07:39 +0000

    Update Safari Technology Preview to 133
    Version as reported by safaridriver --version:
    Included with Safari Technology Preview (Release 133, 16613.1.2.2)
    
    Source: https://developer.apple.com/safari/download/
    Build: https://dev.azure.com/foolip/safari-technology-preview-updater/_build/results?buildId=3723&view=logs

--- tools/ci/azure/safari-technology-preview.rb
+++ tools/ci/azure/safari-technology-preview.rb
@@ -1,19 +1,20 @@
 cask "safari-technology-preview" do
-  if MacOS.version <= :catalina
-    version "125,071-44527-20210526-93430244-0334-4fae-878d-56502a656003"
-    sha256 "6ea830ec5c9101665e18d77f31a7f220a0fc8afc35b9ac036ee29d5ee3dbdd23"
-  else
-    version "125,071-45899-20210526-3fe7359c-0f20-4850-b6ec-da9b197119c2"
-    sha256 "63e4961fd0cca345b58128c0edb33627847d02b99a77613c54c0a89e6402d36e"
+  if MacOS.version == :monterey
+    version "133,002-06914-20210929-5ee8c8d9-eda3-4302-b5f7-85c516c1416a"
+    url "https://secure-appldnld.apple.com/STP/#{version.after_comma}/SafariTechnologyPreview.dmg"
+    sha256 "a60eaee39ffea03ee8be7d1bd2da26fe81962d0d591584fa461f755bf411b3f5"
+  elsif MacOS.version == :big_sur
+    version "133,002-07112-20210929-4d3827b5-bcc2-467f-9bea-c99639cc1cda"
+    url "https://secure-appldnld.apple.com/STP/#{version.after_comma}/SafariTechnologyPreview.dmg"
+    sha256 "9b9578f186e307d1a95765c35b015f61c6ea7f0b06fc305afbf8c96b02283961"
   end
 
-  url "https://secure-appldnld.apple.com/STP/#{version.after_comma}/SafariTechnologyPreview.dmg"
   appcast "https://developer.apple.com/safari/download/"
   name "Safari Technology Preview"
   homepage "https://developer.apple.com/safari/download/"
 
   auto_updates true
-  depends_on macos: ">= :catalina"
+  depends_on macos: ">= :big_sur"
 
   pkg "Safari Technology Preview.pkg"
 

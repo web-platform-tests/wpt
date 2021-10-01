d939309437082d175aadee9750331e96a6ac854f
Rayan Kanso <rayankans@google.com>
2021-10-01 14:27:50 +0100

    Add tests for safelisting simple range headers from preflight
    Fetch PR: https://github.com/whatwg/fetch/pull/1312/

--- cors/cors-safelisted-request-header.any.js
+++ cors/cors-safelisted-request-header.any.js
@@ -40,4 +40,18 @@ function safelist(headers, expectPreflight = false) {
   ["multipart/form-data;\"", true]
 ].forEach(([mimeType, preflight = false]) => {
   safelist({"content-type": mimeType}, preflight);
-})
+});
+
+[
+  ["100-200", true],
+  ["MB=100-200", true],
+  ["bytes=100-200"],
+  ["BYTES=100-200"],
+  ["bytes=100-200,300-400", true],
+  ["bytes=-200", true],
+  ["bytes=200-"],
+  ["bytes=200-100", true],
+  [`bytes=1${'0'.repeat(60)}-2${'0'.repeat(60)}`, true],
+].forEach(([mimeType, preflight = false]) => {
+  safelist({"range": mimeType}, preflight);
+});

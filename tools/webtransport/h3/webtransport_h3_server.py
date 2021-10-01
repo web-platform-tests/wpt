a909aa76c1376a429ed2a27dc1da1a1079b8d768
Yutaka Hirano <yhirano@chromium.org>
2021-10-01 20:12:24 +0900

    fix

--- tools/webtransport/h3/webtransport_h3_server.py
+++ tools/webtransport/h3/webtransport_h3_server.py
@@ -109,6 +109,8 @@ class WebTransportH3Protocol(QuicConnectionProtocol):
 
         if isinstance(event, WebTransportStreamDataReceived) and\
            self._session_stream_id == event.stream_id:
+            if not self._http.supports_h3_datagram_04:
+                raise ProtocolError('Unexpected data on the session stream')
             self._receive_data_on_session_stream(
                 event.data, event.stream_ended)
         elif self._handler is not None:

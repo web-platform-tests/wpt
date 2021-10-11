7fb4ac5328097d5a306ed99dde6c37c2d130685a
Noam Rosenthal <noam@webkit.org>
2021-10-11 18:34:19 +0300

    Use step_timeout

--- service-workers/service-worker/resources/navigation-timing-worker-extended.js
+++ service-workers/service-worker/resources/navigation-timing-worker-extended.js
@@ -1,15 +1,16 @@
+importScripts("/resources/testharness.js");
 const timings = {}
 
 self.addEventListener('activate', event => {
     event.waitUntil(new Promise(resolve => {
         timings.activateWorkerStart = performance.now() + performance.timeOrigin
-        setTimeout(resolve, 500)
+        step_timeout(resolve, 500)
     }).then(() => timings.activateWorkerEnd = performance.now() + performance.timeOrigin));
 })
 
 self.addEventListener('fetch', event => {
     timings.handleFetchEvent = performance.now() + performance.timeOrigin;
-    event.respondWith(new Promise(resolve => setTimeout(resolve, 30)).then(() =>
+    event.respondWith(new Promise(resolve => step_timeout(resolve, 30)).then(() =>
         new Response(new Blob([`
             <script>
                 parent.postMessage(${JSON.stringify(timings)}, "*")

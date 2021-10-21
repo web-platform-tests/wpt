78123484aa1ad247eb842da8dc9e2f4197ada449
Adam Rice <ricea@chromium.org>
2021-10-21 12:31:43 +0900

    Add code= to reset_stream call.
    Co-authored-by: bashi <ishibashi.kenichi@gmail.com>

--- webtransport/handlers/abort-server-initiated-stream.py
+++ webtransport/handlers/abort-server-initiated-stream.py
@@ -1,4 +1,4 @@
 def session_established(session):
     stream_id = session.create_unidirectional_stream()
     session.send_stream_data(stream_id, b'hello')
-    session.reset_stream(stream_id, 0x52e4a40fa8db)
+    session.reset_stream(stream_id, code=0x52e4a40fa8db)

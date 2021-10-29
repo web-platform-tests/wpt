24744ea46dc3c3d806d26bb94cefcdfbc4ef715d
Rakib Hasan <rmhasan@google.com>
2021-10-28 17:02:03 -0700

    adding command line group and command line arg

--- tools/wptrunner/wptrunner/browsers/android_webview.py
+++ tools/wptrunner/wptrunner/browsers/android_webview.py
@@ -55,7 +55,8 @@ def executor_kwargs(logger, test_type, test_environment, run_info_data,
         kwargs.get("package_name", "org.chromium.webview_shell")
     capabilities["goog:chromeOptions"]["androidActivity"] = \
         "org.chromium.webview_shell.WebPlatformTestsActivity"
-    capabilities["goog:chromeOptions"]["androidUseRunningApp"] = kwargs["use_running_browser"]
+    capabilities["goog:chromeOptions"]["androidKeepAppDataDir"] = \
+        kwargs.get("keep_app_data_directory")
     if kwargs.get("device_serial"):
         capabilities["goog:chromeOptions"]["androidDeviceSerial"] = kwargs["device_serial"]
 

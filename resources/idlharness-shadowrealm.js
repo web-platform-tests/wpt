b57afc1a2c6bba162b09aa0c5868c0a0eabe425c
Ms2ger <Ms2ger@gmail.com>
2021-12-10 09:35:34 +0100

    fixup! Alternative

--- resources/idlharness-shadowrealm.js
+++ resources/idlharness-shadowrealm.js
@@ -17,49 +17,45 @@ function idl_test_shadowrealm(srcs, deps) {
         "/resources/WebIDLParser.js",
         "/resources/idlharness.js",
     ];
-    promise_test(t => {
+    promise_test(async t => {
         const realm = new ShadowRealm();
         realm.evaluate("globalThis.self = globalThis; undefined");
 
-        return Promise.all(script_urls.map(url => fetch_text(url)))
-        .then(ss => {
-            for (const s of ss) {
-                realm.evaluate(s);
-            }
-        })
-        .then(_ => Promise.all(srcs.concat(deps).map(fetch_spec)))
-        .then(specs => {
-            const idls = JSON.stringify(specs.map(i => i.idl));
-            const code = `
-                const idls = ${idls};
-                let results;
-                add_completion_callback(function (tests, harness_status, asserts_run) {
-                    results = tests;
-                });
+        const ss = await Promise.all(script_urls.map(url => fetch_text(url)));
+        for (const s of ss) {
+            realm.evaluate(s);
+        }
+        const specs = await Promise.all(srcs.concat(deps).map(fetch_spec));
+        const idls = JSON.stringify(specs.map(i => i.idl));
+        const code = `
+            const idls = ${idls};
+            let results;
+            add_completion_callback(function (tests, harness_status, asserts_run) {
+                results = tests;
+            });
 
-                // Without the wrapping test, testharness.js will think it's done after it has run
-                // the first idlharness test.
-                test(() => {
-                    const idl_array = new IdlArray();
-                    for (let i = 0; i < ${srcs.length}; i++) {
-                        idl_array.add_idls(idls[i]);
-                    }
-                    for (let i = ${srcs.length}; i < ${srcs.length + deps.length}; i++) {
-                        idl_array.add_dependency_idls(idls[i]);
-                    }
-                    idl_array.test();
-                }, "inner setup");
-                String(JSON.stringify(results))
-            `;
+            // Without the wrapping test, testharness.js will think it's done after it has run
+            // the first idlharness test.
+            test(() => {
+                const idl_array = new IdlArray();
+                for (let i = 0; i < ${srcs.length}; i++) {
+                    idl_array.add_idls(idls[i]);
+                }
+                for (let i = ${srcs.length}; i < ${srcs.length + deps.length}; i++) {
+                    idl_array.add_dependency_idls(idls[i]);
+                }
+                idl_array.test();
+            }, "inner setup");
+            String(JSON.stringify(results))
+        `;
 
-            // We ran the tests in the ShadowRealm and gathered the results. Now treat them as if
-            // we'd run them directly here, so we can see them.
-            const results = JSON.parse(realm.evaluate(code));
-            for (const {name, status, message} of results) {
-                // TODO: make this an API in testharness.js - needs RFC?
-                async_test(t => {t.set_status(status, message); t.phase = t.phases.HAS_RESULT; t.done()}, name);
-            }
-        });
+        // We ran the tests in the ShadowRealm and gathered the results. Now treat them as if
+        // we'd run them directly here, so we can see them.
+        const results = JSON.parse(realm.evaluate(code));
+        for (const {name, status, message} of results) {
+            // TODO: make this an API in testharness.js - needs RFC?
+            async_test(t => {t.set_status(status, message); t.phase = t.phases.HAS_RESULT; t.done()}, name);
+        }
     }, "outer setup");
 }
 

90d5c07f4457a3a754a0651ced9aa2b0fa004bed
Daniel Smith <56164590+DanielRyanSmith@users.noreply.github.com>
2021-12-26 14:50:19 -0800

    add closing parens

--- tools/wptrunner/wptrunner/wptcommandline.py
+++ tools/wptrunner/wptrunner/wptcommandline.py
@@ -113,9 +113,9 @@ scheme host and port.""")
                                   dest="verify_chaos_mode",
                                   help="Enable chaos mode when running on Firefox")
     mode_group.add_argument("--verify-max-time", action="store",
-                            default=100,
+                            default=None,
                             help="The maximum number of minutes for the job to run",
-                            type=int
+                            type=int)
     output_results_group = mode_group.add_mutually_exclusive_group()
     output_results_group.add_argument("--verify-no-output-results", action="store_false",
                                       dest="verify_output_results",

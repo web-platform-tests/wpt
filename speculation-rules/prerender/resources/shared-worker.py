743578e05c413e2ad87f1059fed4f8d3d518144b
Noam Rosenthal <noam@webkit.org>
2022-02-02 10:38:44 +0200

    lint

--- speculation-rules/prerender/resources/shared-worker.py
+++ speculation-rules/prerender/resources/shared-worker.py
@@ -3,7 +3,7 @@ def main(request, response):
         with request.server.stash.lock:
             result = request.server.stash.take(request.GET[b"id"])
             response.headers.set(b"Content-Type", b"text/plain")
-            return result            
+            return result
     else:
         with request.server.stash.lock:
             request.server.stash.put(request.GET[b"id"], "ok")

27dcb161d37c8c17eb8c5a53e1c568919cf4997f
Joey Arhar <jarhar@chromium.org>
2022-02-04 15:49:17 -0800

    Remove [Replaceable] from some IDLs
    window.clientInformation and window.scheduler both should not be
    [Replaceable] according to their respective specs.
    
    Fixed: 1293264
    Change-Id: Ic3c3e5fa36d48481eb92e5b17176a24cf9484eeb

--- html/webappapis/system-state-and-capabilities/the-navigator-object/clientinformation.window.js
+++ html/webappapis/system-state-and-capabilities/the-navigator-object/clientinformation.window.js
@@ -1,3 +1,9 @@
 test(() => {
   assert_equals(window.clientInformation, window.navigator);
 }, "window.clientInformation exists and equals window.navigator");
+
+test(() => {
+  const originalClientInformation = window.clientInformation;
+  window.clientInformation = 'hello';
+  assert_equals(window.clientInformation, originalClientInformation);
+}, 'window.clientInformation should not be replaceable.');

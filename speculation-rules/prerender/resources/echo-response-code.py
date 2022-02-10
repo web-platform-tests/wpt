3e9bb9b7fc4e8a457b7a4e34abb5d7413204dadc
Noam Rosenthal <noam@webkit.org>
2022-02-10 12:08:56 +0200

    wip

---
+++ speculation-rules/prerender/resources/echo-response-code.py
@@ -0,0 +1,32 @@
+import time
+
+def main(request, response):
+    mode = request.GET.first(b"mode")
+    uid = request.GET.first(b"uid")
+    purpose = request.headers.get(b"purpose")
+
+    if (mode == b"get"):
+        count = 0
+        with request.server.stash.lock:
+            count = request.server.stash.take(uid)
+            if(count == None):
+              count = 0
+            request.server.stash.put(uid, count)
+        return 200, [], str(count)
+            
+    if (mode == b"echo"):
+        code = request.GET.first(b"code")
+        content = request.GET.first(b"content")
+
+        if (purpose != b'prefetch'):
+            code = "200"
+
+        with request.server.stash.lock:
+            count = request.server.stash.take(uid)
+            if(count == None):
+              count = 0
+            else:
+              code = "200"
+
+            request.server.stash.put(uid, count + 1)
+        return int(code), [], content

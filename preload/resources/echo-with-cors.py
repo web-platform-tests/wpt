a260edc65f34fd812d90d53c6978a7d9eeef1154
Noam Rosenthal <noam@webkit.org>
2022-02-14 14:09:15 +0200

    Tentative test for memory cache behavior

--- preload/resources/echo-with-cors.py
+++ preload/resources/echo-with-cors.py
@@ -1,8 +1,13 @@
 def main(request, response):
     response.headers.set(b"Content-Type", request.GET.first(b"type"))
     origin = request.headers.get('Origin')
+    cache = request.GET.first(b'cache', None)
+
     if origin is not None:
         response.headers.set(b"Access-Control-Allow-Origin", origin)
         response.headers.set(b"Access-Control-Allow-Credentials", b"true")
+        
+    if cache is not None:
+        response.headers.set(b"Cache-Control", cache)
 
     return request.GET.first(b"content")

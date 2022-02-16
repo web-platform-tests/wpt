396705e9a9701ea1ea7d999269f745b33aef2f58
Noam Rosenthal <noam@webkit.org>
2022-02-16 10:41:25 +0200

    async/await

--- speculation-rules/prerender/resources/service-worker.js
+++ speculation-rules/prerender/resources/service-worker.js
@@ -1,11 +1,11 @@
-self.addEventListener("fetch", e => {
+self.addEventListener("fetch", async e => {
     if (e.request.url.endsWith("ping"))
         e.respondWith(new Response('pong'));
     else if (e.request.url.endsWith("client")) {
-        e.respondWith(new Promise(async resolve => {
+        e.respondWith((async () => {
             const client = await clients.get(e.clientId);
             const clientInfo = client ? {id: e.clientId, visibilityState: client.visibilityState, focused: client.focused} : null;
-            resolve(new Response(JSON.stringify({clientInfo}), {headers: {'Content-Type': 'application/json'}}));
-        }));
+            return new Response(JSON.stringify({clientInfo}), {headers: {'Content-Type': 'application/json'}});
+        })());
     }
 });

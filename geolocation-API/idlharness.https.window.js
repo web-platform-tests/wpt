28432c44ac755061da8f0eff3446ccc382c4941e
Marcos Cáceres <marcos@marcosc.com>
2022-02-23 19:14:56 +1100

    bug fix

--- geolocation-API/idlharness.https.window.js
+++ geolocation-API/idlharness.https.window.js
@@ -10,22 +10,19 @@ window.onload = async () => {
   const positionError = await new Promise((resolve, reject) => {
     navigator.geolocation.getCurrentPosition(reject, resolve);
   });
+
   await test_driver.set_permission({ name: "geolocation" }, "granted");
   const position = await new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(reject, resolve);
+    navigator.geolocation.getCurrentPosition(resolve, reject);
   });
-  idl_test(
-    ['geolocation'],
-    ['hr-time', 'html'],
-    idl_array => {
-      idl_array.add_objects({
-        Navigator: ['navigator'],
-        Geolocation: ['navigator.geolocation'],
-        GeolocationPositionError: [positionError],
-        GeolocationPosition: [position],
-        GeolocationCoordinates: [position.coords],
-      });
-    }
-  );
-}
 
+  idl_test(["geolocation"], ["hr-time", "html"], (idl_array) => {
+    idl_array.add_objects({
+      Navigator: ["navigator"],
+      Geolocation: ["navigator.geolocation"],
+      GeolocationPositionError: [positionError],
+      GeolocationPosition: [position],
+      GeolocationCoordinates: [position.coords],
+    });
+  });
+};

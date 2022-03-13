248956b98954a10b8f6770479f4592cf01c382c8
Noam Rosenthal <noam.j.rosenthal@gmail.com>
2022-03-13 10:18:48 +0200

    Check different scenario with subresource Link headers

--- preload/resources/preload_helper.js
+++ preload/resources/preload_helper.js
@@ -43,3 +43,11 @@ function verifyLoadedAndNoDoubleDownload(url) {
         numDownloads, 1,
         url + ' should be downloaded from network at most once');
 }
+
+function getSubresourceWithLinks({href, type}, links) {
+    const params = new URLSearchParams();
+    params.set('links', JSON.stringify(links.map(({href, as}) => `<${href}>;rel=preload;as=${as}`)));
+    params.set('href', href);
+    params.set('type', type);
+    return `/preload/resources/subresource.py?${params.toString()}`;
+}

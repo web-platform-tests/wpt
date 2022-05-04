4c357b48dfdf3de3c8fe445dde7c3d930c99469c
Domenic Denicola <d@domenic.me>
2022-05-04 14:53:28 -0400

    Rewrite some navigation tests to be more linear

--- html/browsers/browsing-the-web/resources/helpers.js
+++ html/browsers/browsing-the-web/resources/helpers.js
@@ -1,3 +1,12 @@
+// This file contains general helpers for navigation/history tests. The goal is
+// to make tests more imperative and ordered, instead of requiring lots of
+// nested callbacks and jumping back and forth. However,
+// html/browsers/browsing-the-web/remote-context-helper/resources/remote-context-helper.js
+// might be even better at that, so prefer that when you can.
+//
+// TODO(domenic): consider unifying with
+// overlapping-navigations-and-traversals/resources/helpers.mjs.
+
 window.openWindow = (url, t) => {
   const w = window.open(url);
   t?.add_cleanup(() => w.close());
@@ -19,6 +28,32 @@ window.addIframe = (url = "/common/blank.html", doc = document) => {
 
 window.waitToAvoidReplace = t => {
   return new Promise(resolve => t.step_timeout(resolve, 0));
+};
+
+window.setupAndWaitForIframe = async (url = "/common/blank.html") => {
+  const iframe = document.createElement("iframe");
+  iframe.src = url;
+  document.body.append(iframe);
+
+  await waitForIframeLoad(iframe);
+
+  return iframe;
+};
+
+window.setupAndWaitForSrcdocIframe = async () => {
+  const iframe = document.createElement("iframe");
+  iframe.srcdoc = `<script>window.parent.postMessage("srcdoc ready", "*")</scr` + `ipt>`;
+  document.body.append(iframe);
+
+  assert_equals(await waitForMessage(iframe.contentWindow), "srcdoc ready");
+
+  return iframe;
+};
+
+window.waitForIframeLoad = iframe => {
+  return new Promise(resolve => {
+    iframe.addEventListener("load", () => resolve(), { once: true });
+  });
 };
 
 window.waitForMessage = expectedSource => {
@@ -46,4 +81,14 @@ window.srcdocThatPostsParentOpener = text => {
       };
     <\/script>
   `;
+};
+
+window.failOnMessage = expectedSource => {
+  return new Promise((_, reject) => {
+    window.addEventListener("message", ({ source, data }) => {
+      if (source === expectedSource) {
+        reject(new Error(`Received message "${data}" but expected to receive no message`));
+      }
+    });
+  });
 };

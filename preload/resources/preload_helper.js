da9929d8932f155cdb6f7710073a501c3a44e20a
Noam Rosenthal <noam.j.rosenthal@gmail.com>
2022-05-09 11:43:39 +0300

    Move subresource to own helper

--- preload/resources/preload_helper.js
+++ preload/resources/preload_helper.js
@@ -44,10 +44,3 @@ function verifyLoadedAndNoDoubleDownload(url) {
         url + ' should be downloaded from network at most once');
 }
 
-function getSubresourceWithLinks({href, type}, links) {
-    const params = new URLSearchParams();
-    params.set('links', JSON.stringify(links.map(({href, as}) => `<${href}>;rel=preload;as=${as}`)));
-    params.set('href', href);
-    params.set('type', type);
-    return `/preload/resources/subresource.py?${params.toString()}`;
-}

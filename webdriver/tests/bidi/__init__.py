6803871dfe6cb16b4d942e623f547fc7f538ec62
Maksim Sadym <sadym@google.com>
2022-05-24 15:02:47 +0200

    Add tests for `script.evaluate`

---
+++ webdriver/tests/bidi/__init__.py
@@ -0,0 +1,20 @@
+# Compares 2 objects recursively ignoring values of specific attributes.
+def recursive_compare(expected, actual, ignore_attributes=[]):
+    assert type(expected) == type(actual)
+    if type(expected) is list:
+        assert len(expected) == len(actual)
+        for index, val in enumerate(expected):
+            recursive_compare(expected[index], actual[index], ignore_attributes)
+        return
+
+    if type(expected) is dict:
+        assert expected.keys() == actual.keys(), \
+            f"Key sets should be the same: " \
+            f"\nNot present: {set(expected.keys()) - set(actual.keys())}" \
+            f"\nUnexpected: {set(actual.keys()) - set(expected.keys())}"
+        for index, val in enumerate(expected):
+            if val not in ignore_attributes:
+                recursive_compare(expected[val], actual[val], ignore_attributes)
+        return
+
+    assert expected == actual
---
+++ webdriver/tests/bidi/script/evaluate/evaluate.py
@@ -0,0 +1,146 @@
+import pytest
+
+from ... import recursive_compare
+
+
+@pytest.mark.asyncio
+async def test_eval(bidi_session, top_context):
+    result = await bidi_session.script.evaluate(
+        expression="1+2",
+        context=top_context["context"])
+
+    assert result == {
+        "result": {
+            "type": "number",
+            "value": 3}}
+
+
+@pytest.mark.asyncio
+async def test_exception(bidi_session, top_context):
+    result = await bidi_session.script.evaluate(
+        expression="throw Error('SOME_ERROR_MESSAGE')",
+        context=top_context["context"])
+
+    recursive_compare({
+        "exceptionDetails": {
+            'columnNumber': 0,
+            'exception': {
+                'objectId': '__any_value__',
+                'type': 'error'},
+            'lineNumber': 0,
+            'stackTrace': {
+                'callFrames': [{
+                    'columnNumber': 6,
+                    'functionName': '',
+                    'lineNumber': 0,
+                    'url': ''}]},
+            'text': '__any_value__'}},
+        result, ["objectId", "text"])
+
+
+@pytest.mark.asyncio
+async def test_interact_with_dom(bidi_session, top_context):
+    result = await bidi_session.script.evaluate(
+        expression="'window.location.href: ' + window.location.href",
+        context=top_context["context"])
+
+    assert result == {
+        "result": {
+            "type": "string",
+            "value": "window.location.href: about:blank"}}
+
+
+@pytest.mark.asyncio
+async def test_resolved_promise_with_wait_promise_false(bidi_session,
+      top_context):
+    result = await bidi_session.script.evaluate(
+        expression="Promise.resolve('SOME_RESOLVED_RESULT')",
+        context=top_context["context"],
+        await_promise=False)
+
+    recursive_compare({
+        "result": {
+            "type": "promise",
+            "objectId": "__any_value__"
+        }}, result, ["objectId"])
+
+
+@pytest.mark.asyncio
+async def test_resolved_promise_with_wait_promise_true(bidi_session,
+      top_context):
+    result = await bidi_session.script.evaluate(
+        expression="Promise.resolve('SOME_RESOLVED_RESULT')",
+        context=top_context["context"],
+        await_promise=True)
+
+    assert result == {
+        "result": {
+            "type": "string",
+            "value": "SOME_RESOLVED_RESULT"
+        }}
+
+
+@pytest.mark.asyncio
+async def test_resolved_promise_with_wait_promise_omitted(bidi_session,
+      top_context):
+    result = await bidi_session.script.evaluate(
+        expression="Promise.resolve('SOME_RESOLVED_RESULT')",
+        context=top_context["context"])
+
+    assert result == {
+        "result": {
+            "type": "string",
+            "value": "SOME_RESOLVED_RESULT"
+        }}
+
+
+@pytest.mark.asyncio
+async def test_rejected_promise_with_wait_promise_false(bidi_session,
+      top_context):
+    result = await bidi_session.script.evaluate(
+        expression="Promise.reject('SOME_REJECTED_RESULT')",
+        context=top_context["context"],
+        await_promise=False)
+
+    recursive_compare({
+        "result": {
+            "type": "promise",
+            "objectId": "__any_value__"
+        }}, result, ["objectId"])
+
+
+@pytest.mark.asyncio
+async def test_rejected_promise_with_wait_promise_true(bidi_session,
+      top_context):
+    result = await bidi_session.script.evaluate(
+        expression="Promise.reject('SOME_REJECTED_RESULT')",
+        context=top_context["context"],
+        await_promise=True)
+
+    recursive_compare({
+        'exceptionDetails': {
+            'columnNumber': 0,
+            'exception': {'type': 'string',
+                          'value': 'SOME_REJECTED_RESULT'},
+            'lineNumber': 0,
+            'stackTrace': {'callFrames': []},
+            'text': '__any_value__'}},
+        result, ["objectId", "text"])
+
+
+@pytest.mark.asyncio
+async def test_rejected_promise_with_wait_promise_omitted(bidi_session,
+      top_context):
+    result = await bidi_session.script.evaluate(
+        expression="Promise.reject('SOME_REJECTED_RESULT')",
+        context=top_context["context"])
+
+    recursive_compare({
+        'exceptionDetails': {
+            'columnNumber': 0,
+            'exception': {'type': 'string',
+                          'value': 'SOME_REJECTED_RESULT'},
+            'lineNumber': 0,
+            'stackTrace': {'callFrames': []},
+            'text': '__any_value__'}},
+        result, ["objectId", "text"])

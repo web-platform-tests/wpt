606135f40993ebf2d9d25285ef63362b3eb99dc6
Gerald Monaco <gbmonaco@google.com>
2022-05-25 15:35:23 +0000

    Replace waitForFrame with waitForPolyfill

--- css/css-contain/container-queries/support/cq-testcommon.js
+++ css/css-contain/container-queries/support/cq-testcommon.js
@@ -12,8 +12,24 @@ function polyfill_declarative_shadow_dom(root) {
   });
 }
 
-function waitForFrame() {
-  return new Promise((resolve, reject) => {
-    window.requestAnimationFrame(resolve);
-  });
+/**
+ * A Container Query polyfill will likely be implemented using asynchronous APIs
+ * like `MutationObserver` or `ResizeObserver`, meaning the effects of writes
+ * will not be synchronously observable.
+ * 
+ * By calling `window.setWaitForPolyfill`, a polyfill can provide a function that
+ * tests can use to wait until it's safe to read the effects of writes. Immediately
+ * prior to attempt to read any layout or style state, tests should await the function,
+ * if it is set:
+ * 
+ *     if (waitForPolyfill) await waitForPolyfill();
+ *     assert_equals(getComputedStyle(target), ...);
+ * 
+ * Note: Even though it would be safe to `await null`, an additional microtask could
+ * hide implementation bugs in browser implementations. Therefore, a test should always
+ * check and only wait `waitForPolyfill` is set.
+ */
+var waitForPolyfill = null;
+window.setWaitForPolyfill = function setWaitForPolyfill(impl) {
+  waitForPolyfill = impl;
 }

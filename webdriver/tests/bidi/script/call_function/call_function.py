e19df6a364f1a4218dd4e565667e6001b21c8e67
Maksim Sadym <sadym@google.com>
2022-06-29 13:15:18 +0200

    Add tests for default `this` and `arguments`

--- webdriver/tests/bidi/script/call_function/call_function.py
+++ webdriver/tests/bidi/script/call_function/call_function.py
@@ -84,6 +84,20 @@ async def test_arguments(bidi_session, top_context):
 
 
 @pytest.mark.asyncio
+async def test_default_arguments(bidi_session, top_context):
+    result = await bidi_session.script.call_function(
+        function_declaration="(...args)=>{return args}",
+        await_promise=False,
+        target=ContextTarget(top_context["context"]))
+
+    recursive_compare({
+        "type": "array",
+        "handle": any_string,
+        "value": []
+    }, result)
+
+
+@pytest.mark.asyncio
 async def test_this(bidi_session, top_context):
     result = await bidi_session.script.call_function(
         function_declaration="function(){return this.some_property}",
@@ -101,6 +115,20 @@ async def test_this(bidi_session, top_context):
     assert result == {
         'type': 'number',
         'value': 42}
+
+
+@pytest.mark.asyncio
+async def test_default_this(bidi_session, top_context):
+    result = await bidi_session.script.call_function(
+        function_declaration="function(){return this}",
+        await_promise=False,
+        target=ContextTarget(top_context["context"]))
+
+    # Note: https://github.com/w3c/webdriver-bidi/issues/251
+    recursive_compare({
+        "type": 'window',
+        "handle": any_string,
+    }, result)
 
 
 @pytest.mark.asyncio

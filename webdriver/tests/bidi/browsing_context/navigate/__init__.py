2c009258e092fc068fa29324b1c90208be0b035b
Maksim Sadym <sadym@google.com>
2022-06-29 15:50:33 +0200

    remove extra format changes

--- webdriver/tests/bidi/browsing_context/navigate/__init__.py
+++ webdriver/tests/bidi/browsing_context/navigate/__init__.py
@@ -7,18 +7,19 @@ async def navigate_and_assert(bidi_session, context, url, wait="complete", expec
     if expected_error:
         with pytest.raises(UnknownErrorException):
             await bidi_session.browsing_context.navigate(
-                context=context["context"], url=url, wait=wait
+                context=context['context'], url=url, wait=wait
             )
+
     else:
         result = await bidi_session.browsing_context.navigate(
-            context=context["context"], url=url, wait=wait
+            context=context['context'], url=url, wait=wait
         )
         assert result["url"] == url
 
         contexts = await bidi_session.browsing_context.get_tree(
-            root=context["context"]
+            root=context['context']
         )
-        assert len(contexts) == 1, f"Should be only 1 context. {len(contexts)} found instead."
+        assert len(contexts) == 1
         assert contexts[0]["url"] == url
 
         return contexts

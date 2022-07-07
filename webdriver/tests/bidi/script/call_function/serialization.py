abaaae91dbfe7f205d4e185f2a8bfe536ce01d0c
Alexandra Borovova <aborovova@mozilla.com>
2022-07-07 11:51:02 +0200

    Make test to check only serialization

--- webdriver/tests/bidi/script/call_function/serialization.py
+++ webdriver/tests/bidi/script/call_function/serialization.py
@@ -5,29 +5,28 @@ from webdriver.bidi.modules.script import ContextTarget
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
-    "argument",
+    "expression, expected",
     [
-        ({"type": "undefined"}),
-        ({"type": "null"}),
-        ({"type": "string", "value": "foobar"}),
-        ({"type": "string", "value": "2"}),
-        ({"type": "number", "value": "NaN"}),
-        ({"type": "number", "value": "-0"}),
-        ({"type": "number", "value": "+Infinity"}),
-        ({"type": "number", "value": "-Infinity"}),
-        ({"type": "number", "value": 3}),
-        ({"type": "number", "value": 1.4}),
-        ({"type": "boolean", "value": True}),
-        ({"type": "boolean", "value": False}),
-        ({"type": "bigint", "value": "42"}),
+        ("undefined", {"type": "undefined"}),
+        ("null", {"type": "null"}),
+        ("'foobar'", {"type": "string", "value": "foobar"}),
+        ("'2'", {"type": "string", "value": "2"}),
+        ("Number.NaN", {"type": "number", "value": "NaN"}),
+        ("-0", {"type": "number", "value": "-0"}),
+        ("Infinity", {"type": "number", "value": "+Infinity"}),
+        ("-Infinity", {"type": "number", "value": "-Infinity"}),
+        ("3", {"type": "number", "value": 3}),
+        ("1.4", {"type": "number", "value": 1.4}),
+        ("true", {"type": "boolean", "value": True}),
+        ("false", {"type": "boolean", "value": False}),
+        ("42n", {"type": "bigint", "value": "42"}),
     ],
 )
-async def test_primitive_values(bidi_session, top_context, argument):
+async def test_primitive_values(bidi_session, top_context, expression, expected):
     result = await bidi_session.script.call_function(
-        function_declaration="(argument) => argument",
-        arguments=[argument],
+        function_declaration=f"() => {expression}",
         await_promise=False,
         target=ContextTarget(top_context["context"]),
     )
 
-    assert result == argument
+    assert result == expected

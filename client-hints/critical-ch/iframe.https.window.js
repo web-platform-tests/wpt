f868fbc2afedec0df8ad24145cdc57819a49da95
removedporn <86824510+removedporn@users.noreply.github.com>
2022-07-23 05:55:57 +0800

    Delete client-hints directory

--- client-hints/critical-ch/iframe.https.window.js
+++
@@ -1,8 +0,0 @@
-// META: script=resources/util.js
-
-async_test((t) => {
-  var iframe = document.createElement("iframe");
-  iframe.src = ECHO_URL;
-  document.body.appendChild(iframe);
-  iframe.contentWindow.addEventListener('message', message_listener(t, "FAIL"));
-}, "Critical-CH iframe");
--- client-hints/critical-ch/non-secure.http.window.js
+++
@@ -1,3 +0,0 @@
-// META: script=resources/util.js
-
-async_test(make_message_test(ECHO_URL, "FAIL"), "Critical-CH navigation non-secure")

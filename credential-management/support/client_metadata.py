c07209c0924ac6b7d998e5f4348df75a99333f0a
removedporn <86824510+removedporn@users.noreply.github.com>
2022-07-23 07:30:13 +0800

    Delete credential-management directory

--- credential-management/support/client_metadata.py
+++
@@ -1,8 +0,0 @@
-def main(request, response):
-  if b"cookie" in request.cookies:
-    return (500, [], "Cookie should not be sent to this endpoint")
-  return """
-{
-  "privacy_policy_url": "https://privacypolicy.com"
-}
-"""

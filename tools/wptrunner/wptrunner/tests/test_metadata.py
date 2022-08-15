750ba046c8e0b8b76ce9755a0d25bfd7a4cd664a
Daniel Smith <56164590+DanielRyanSmith@users.noreply.github.com>
2022-08-15 16:20:10 -0700

    Update test_metadata.py

--- tools/wptrunner/wptrunner/tests/test_metadata.py
+++ tools/wptrunner/wptrunner/tests/test_metadata.py
@@ -13,9 +13,9 @@ def write_properties(tmp_path, data):  # type: ignore
     return path
 
 @pytest.mark.parametrize("data",
-                         [{"properties": ["prop1"]},
+                         [{"properties": ["prop1"]},  # type: ignore
                           {"properties": ["prop1"], "dependents": {"prop1": ["prop2"]}},
-                          ])  # type: ignore
+                          ])
 def test_get_properties_file_valid(tmp_path, data):
     path = write_properties(tmp_path, data)
     expected = data["properties"], data.get("dependents", {})
@@ -23,7 +23,7 @@ def test_get_properties_file_valid(tmp_path, data):
     assert actual == expected
 
 @pytest.mark.parametrize("data",
-                         [{},
+                         [{},  # type: ignore
                           {"properties": "prop1"},
                           {"properties": None},
                           {"properties": ["prop1", 1]},
@@ -32,7 +32,7 @@ def test_get_properties_file_valid(tmp_path, data):
                           {"properties": "prop1", "dependents": None},
                           {"properties": "prop1", "dependents": {"prop1": ["prop2", 2]}},
                           {"properties": ["prop1"], "dependents": {"prop2": ["prop3"]}},
-                          ])  # type: ignore
+                          ])
 def test_get_properties_file_invalid(tmp_path, data):
     path = write_properties(tmp_path, data)
     with pytest.raises(ValueError):

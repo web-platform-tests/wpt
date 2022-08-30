56746bfcf1b8eae23f81f69c144d54bb7441b9c6
Noam Rosenthal <noamr@users.noreply.github.com>
2022-08-30 20:09:01 +0300

    Add multiple tests for prefetch behavior
    See https://github.com/whatwg/html/pull/8111#issuecomment-1229907894
    
    Tests that:
    * preload/prefetch-cache.html
      No 5 minute rule (cache headers must be respected)
    
    * preload/prefetch-document.html
      No special treatment of as=document
      Whether Accept is left as its default value.
      Storage partitioning is applied, including for documents
    
    * preload/prefetch-load-event.html
      Does not block the load event
    
    * preload/prefetch-headers.html
      Whether any additional headers (Sec-Purpose, Purpose, X-moz, X-Purpose) are sent (depending on what we put in the spec)
    
    * preload/prefetch-types.html
      Always uses prefetch destination, and ignores as=""

--- preload/resources/prefetch-helper.js
+++ preload/resources/prefetch-helper.js
@@ -6,6 +6,7 @@ async function get_prefetch_info(href) {
 async function prefetch(p = {}, t) {
     const link = document.createElement("link");
     link.rel = "prefetch";
+    link.as = p.as;
     if (p.crossOrigin)
         link.setAttribute("crossorigin", p.crossOrigin);
     const uid = token();
@@ -13,7 +14,7 @@ async function prefetch(p = {}, t) {
     params.set("key", uid);
     for (const key in p)
         params.set(key, p[key]);
-    const origin = p.origin || '';
+    const origin = p.origin || '.';
     link.href = `${origin}/preload/resources/prefetch-info.py?${params.toString()}`;
     document.head.appendChild(link);
     while (!(await get_prefetch_info(link.href)).length) { }

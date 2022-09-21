039a0cd3ffdab4840e67fb8a2e0820e57f8b1f15
Jeroen Dhollander <jeroendh@google.com>
2022-09-21 01:55:15 -0700

    Revert "[beacon-api] Fix sendondiscard web tests's assertions"
    This reverts commit f3045d98a2d6506341c51a7dd97d8f57881d983a.
    
    Reason for revert: [Sheriff]: Broke build bot https://ci.chromium.org/p/chromium/builders/ci/linux-bfcache-rel
    
    (wpt_internal/pending_beacon/pending_beacon-sendondiscard.tentative.window.html failed for 3 out of the last 4 runs):
      1 https://ci.chromium.org/ui/p/chromium/builders/ci/linux-bfcache-rel/37616/overview
      2 https://ci.chromium.org/ui/p/chromium/builders/ci/linux-bfcache-rel/37615/overview
      3 https://ci.chromium.org/ui/p/chromium/builders/ci/linux-bfcache-rel/37613/overview
    
    Original change's description:
    > [beacon-api] Fix sendondiscard web tests's assertions
    >
    > 1. Move sendondiscard web tests to `wpt_internal/`:
    >
    >    Tests in this file were not working properly as the BackgroundSync
    >    permission is set to `Denied` by default in the web test runner.
    >    Moving to `wpt_internal/` allows the use of
    >    `internals.setPermission()` API.
    >
    > 2. There was also a bug in `expectBeacon()` function which didn't
    >    check the total number of sent beacons properly.
    >
    > This CL fixes the above issues.
    >
    > Bug: 1293679,1364348
    > Change-Id: Ia037080f94303279154fa0d71d33cf364712590c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3907264
    > Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    > Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    > Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1049485}
    
    Bug: 1293679,1364348
    Change-Id: I3d5dfbfc1b72471136030f860280491a0c1ec438
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true

--- pending_beacon/resources/pending_beacon-helper.js
+++ pending_beacon/resources/pending_beacon-helper.js
@@ -113,21 +113,16 @@ function generateSetBeaconURL(uuid, options) {
   return url;
 }
 
-async function poll(asyncFunc, expected) {
-  const maxRetries = 30;
-  const waitInterval = 100;  // milliseconds.
-  const delay = ms => new Promise(res => setTimeout(res, ms));
-
-  let result = {data: []};
-  for (let i = 0; i < maxRetries; i++) {
-    result = await asyncFunc();
-    if (!expected(result)) {
-      await delay(waitInterval);
-      continue;
+async function poll(f, expected) {
+  const interval = 100;  // milliseconds.
+  for (let i = 0; i < 30; i++) {
+    const result = await f();
+    if (expected(result)) {
+      return result;
     }
-    return result;
+    await new Promise(resolve => setTimeout(resolve, interval));
   }
-  return result;
+  return {data: []};
 }
 
 // Waits until the `options.count` number of beacon data available from the
@@ -149,16 +144,10 @@ async function expectBeacon(uuid, options) {
         return res.data.length == expectedCount;
       });
   if (!options || !options.data) {
-    assert_equals(
-        res.data.length, expectedCount,
-        'Number of sent beacons does not match expected count:');
     return;
   }
 
   if (expectedCount == 0) {
-    assert_equals(
-        res.data.length, 0,
-        'Number of sent beacons does not match expected count:');
     return;
   }
 

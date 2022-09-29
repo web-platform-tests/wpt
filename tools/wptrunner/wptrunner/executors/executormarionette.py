b19b0cbf57a2f0220c5a4450090913f0c7e23184
Ari Chivukula <arichiv@chromium.org>
2022-09-29 13:09:24 -0400

    Update executormarionette.py

--- tools/wptrunner/wptrunner/executors/executormarionette.py
+++ tools/wptrunner/wptrunner/executors/executormarionette.py
@@ -453,6 +453,14 @@ class MarionetteCookiesProtocolPart(CookiesProtocolPart):
         self.logger.info("Deleting all cookies")
         return self.marionette.delete_all_cookies()
 
+    def get_all_cookies(self):
+        self.logger.info("Getting all cookies")
+        return self.marionette.get_all_cookies()
+
+    def get_named_cookie(self, name):
+        self.logger.info("Getting cookie named %s" % name)
+        return self.marionette.get_named_cookie()
+
 
 class MarionetteSendKeysProtocolPart(SendKeysProtocolPart):
     def setup(self):

028902f70a4417f082f1ddf1d75f14b7886bb0ad
Jxck <block.rxckin.beats@gmail.com>
2022-10-05 20:10:31 +0900

    adding spec in comment

--- fetch/http-cache/cache-mode.any.js
+++ fetch/http-cache/cache-mode.any.js
@@ -5,7 +5,19 @@
 // META: script=/common/get-host-info.sub.js
 // META: script=http-cache.js
 
+// 4.6. HTTP-network-or-cache fetch
+// https://fetch.spec.whatwg.org/#http-network-or-cache-fetch
 var tests = [
+  // 15. If httpRequest's cache mode is "default"
+  // and httpRequest's header list contains
+  // `If-Modified-Since`, `If-None-Match`, `If-Unmodified-Since`, `If-Match`, or `If-Range`,
+  // then set httpRequest's cache mode to "no-store".
+  // TODO:
+
+  // 16. If httpRequest's cache mode is "no-cache",
+  //     httpRequest's prevent no-cache cache-control header modification flag is unset,
+  //     and httpRequest's header list does not contain `Cache-Control`,
+  //     then append (`Cache-Control`, `max-age=0`) to httpRequest's header list.
   {
     name: "Fetch sends Cache-Control: max-age=0 when cache mode is no-cache",
     requests: [
@@ -25,6 +37,12 @@ var tests = [
       }
     ]
   },
+
+  // 17. If httpRequest's cache mode is "no-store" or "reload", then:
+  //    1. If httpRequest's header list does not contain `Pragma`,
+  //       then append (`Pragma`, `no-cache`) to httpRequest's header list.
+  //    2. If httpRequest's header list does not contain `Cache-Control`,
+  //        then append (`Cache-Control`, `no-cache`) to httpRequest's header list.
   {
     name: "Fetch sends Cache-Control: no-cache and Pragma: no-cache when cache mode is no-store",
     requests: [
@@ -57,5 +75,7 @@ var tests = [
       }
     ]
   }
+
+  // TODO: "reload"
 ];
 run_tests(tests);

1bb856eed5281e2898d031323e8bf4a30be98a84
Jxck <block.rxckin.beats@gmail.com>
2022-10-05 20:26:16 +0900

    adding cache-mode: default test cases

--- fetch/http-cache/cache-mode.any.js
+++ fetch/http-cache/cache-mode.any.js
@@ -12,7 +12,194 @@ var tests = [
   // and httpRequest's header list contains
   // `If-Modified-Since`, `If-None-Match`, `If-Unmodified-Since`, `If-Match`, or `If-Range`,
   // then set httpRequest's cache mode to "no-store".
-  // TODO:
+
+  /// If-Modified-Since
+  {
+    name: "Fetch sends Cache-Control: no-cache and Pragma: no-cache when cache mode is default and header list contains If-Modified-Since",
+    requests: [
+      {
+        cache: "default",
+        request_headers: [['if-modified-since', 'Tue, 22 Feb 2022 22:22:22 GMT']],
+        expected_request_headers: [
+          ['cache-control', 'no-cache'],
+          ['pragma', 'no-cache']
+        ]
+      }
+    ]
+  },
+  {
+    name: "Fetch doesn't touch Cache-Control when cache mode is default, header list contains If-Modified-Since and Cache-Control is already present",
+    requests: [
+      {
+        cache: "default",
+        request_headers: [
+          ['if-modified-since', 'Tue, 22 Feb 2022 22:22:22 GMT'],
+          ['cache-control', 'foo']
+        ],
+        expected_request_headers: [
+          ['cache-control', 'foo']
+          ['pragma', 'no-cache']
+        ]
+      }
+    ]
+  },
+  {
+    name: "Fetch doesn't touch Cache-Control when cache mode is default, header list contains If-Modified-Since and Pragma is already present",
+    requests: [
+      {
+        cache: "default",
+        request_headers: [
+          ['if-modified-since', 'Tue, 22 Feb 2022 22:22:22 GMT'],
+          ['pragma', 'foo']
+        ],
+        expected_request_headers: [
+          ['cache-control', 'no-cache']
+          ['pragma', 'foo']
+        ]
+      }
+    ]
+  },
+
+  /// If-Unmodified-Since
+  {
+    name: "Fetch sends Cache-Control: no-cache and Pragma: no-cache when cache mode is default and header list contains If-Unmodified-Since",
+    requests: [
+      {
+        cache: "default",
+        request_headers: [['if-unmodified-since', 'Tue, 22 Feb 2022 22:22:22 GMT']],
+        expected_request_headers: [
+          ['cache-control', 'no-cache'],
+          ['pragma', 'no-cache']
+        ]
+      }
+    ]
+  },
+  {
+    name: "Fetch doesn't touch Cache-Control when cache mode is default, header list contains If-Unmodified-Since and Cache-Control is already present",
+    requests: [
+      {
+        cache: "default",
+        request_headers: [
+          ['if-unmodified-since', 'Tue, 22 Feb 2022 22:22:22 GMT'],
+          ['cache-control', 'foo']
+        ],
+        expected_request_headers: [
+          ['cache-control', 'foo']
+          ['pragma', 'no-cache']
+        ]
+      }
+    ]
+  },
+  {
+    name: "Fetch doesn't touch Cache-Control when cache mode is default, header list contains If-Unmodified-Since and Pragma is already present",
+    requests: [
+      {
+        cache: "default",
+        request_headers: [
+          ['if-unmodified-since', 'Tue, 22 Feb 2022 22:22:22 GMT'],
+          ['pragma', 'foo']
+        ],
+        expected_request_headers: [
+          ['cache-control', 'no-cache']
+          ['pragma', 'foo']
+        ]
+      }
+    ]
+  },
+
+  /// If-Match
+  {
+    name: "Fetch sends Cache-Control: no-cache and Pragma: no-cache when cache mode is default and header list contains If-Match",
+    requests: [
+      {
+        cache: "default",
+        request_headers: [['if-match', 'foo']],
+        expected_request_headers: [
+          ['cache-control', 'no-cache'],
+          ['pragma', 'no-cache']
+        ]
+      }
+    ]
+  },
+  {
+    name: "Fetch doesn't touch Cache-Control when cache mode is default, header list contains If-Match and Cache-Control is already present",
+    requests: [
+      {
+        cache: "default",
+        request_headers: [
+          ['if-match', 'foo'],
+          ['cache-control', 'foo']
+        ],
+        expected_request_headers: [
+          ['cache-control', 'foo']
+          ['pragma', 'no-cache']
+        ]
+      }
+    ]
+  },
+  {
+    name: "Fetch doesn't touch Cache-Control when cache mode is default, header list contains If-Match and Pragma is already present",
+    requests: [
+      {
+        cache: "default",
+        request_headers: [
+          ['if-match', 'foo'],
+          ['pragma', 'foo']
+        ],
+        expected_request_headers: [
+          ['cache-control', 'no-cache']
+          ['pragma', 'foo']
+        ]
+      }
+    ]
+  },
+
+  /// If-Range
+  {
+    name: "Fetch sends Cache-Control: no-cache and Pragma: no-cache when cache mode is default and header list contains If-Range",
+    requests: [
+      {
+        cache: "default",
+        request_headers: [['if-range', 'foo']],
+        expected_request_headers: [
+          ['cache-control', 'no-cache'],
+          ['pragma', 'no-cache']
+        ]
+      }
+    ]
+  },
+  {
+    name: "Fetch doesn't touch Cache-Control when cache mode is default, header list contains If-Range and Cache-Control is already present",
+    requests: [
+      {
+        cache: "default",
+        request_headers: [
+          ['if-range', 'foo'],
+          ['cache-control', 'foo']
+        ],
+        expected_request_headers: [
+          ['cache-control', 'foo']
+          ['pragma', 'no-cache']
+        ]
+      }
+    ]
+  },
+  {
+    name: "Fetch doesn't touch Cache-Control when cache mode is default, header list contains If-Range and Pragma is already present",
+    requests: [
+      {
+        cache: "default",
+        request_headers: [
+          ['if-range', 'foo'],
+          ['pragma', 'foo']
+        ],
+        expected_request_headers: [
+          ['cache-control', 'no-cache']
+          ['pragma', 'foo']
+        ]
+      }
+    ]
+  },
 
   // 16. If httpRequest's cache mode is "no-cache",
   //     httpRequest's prevent no-cache cache-control header modification flag is unset,

bb6e09fe67139f99436cd695ee3c09bad003612e
Nihar Damarasingu <nihardamar@google.com>
2022-10-19 20:16:50 +0000

    reconfigure stderr and stdout to be utf-8

--- tools/wptrunner/wptrunner/environment.py
+++ tools/wptrunner/wptrunner/environment.py
@@ -94,9 +94,10 @@ class TestEnvironment:
     def __init__(self, test_paths, testharness_timeout_multipler,
                  pause_after_test, debug_test, debug_info, options, ssl_config, env_extras,
                  enable_webtransport=False, mojojs_path=None, inject_script=None):
-
-        sys.__stderr__.reconfigure(encoding='utf-8')
-        sys.__stdout__.reconfigure(encoding='utf-8')
+        if sys.version_info >= (3, 7):
+            sys.__stderr__.reconfigure(encoding='utf-8')
+            sys.__stdout__.reconfigure(encoding='utf-8')
+            
         self.test_paths = test_paths
         self.server = None
         self.config_ctx = None

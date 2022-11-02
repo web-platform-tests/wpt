0784ceda8048bb68136527d3ebe3a807895bdba9
BruceDai <feng.dai@intel.com>
2022-11-03 00:06:16 +0800

    [webnn] Add tests for concat operation.

--- webnn/resources/utils.js
+++ webnn/resources/utils.js
@@ -5,24 +5,33 @@ const ExecutionArray = ['async', 'sync'];
 // https://webmachinelearning.github.io/webnn/#enumdef-mldevicetype
 const DeviceTypeArray = ['cpu', 'gpu'];
 
+// Refer to precision metrics on https://github.com/webmachinelearning/webnn/issues/265#issuecomment-1256242643
+const PrecisionMetrics = {
+  concat: {ULP: {float32: 0, float16: 0}},
+};
+
+const TypedArrayDict = {
+  float32: Float32Array,
+};
+
 /**
  * Get bitwise of the given value.
- * @param {number} value
- * @param {string} dataType A data type string, like "float32", "int8",
+ * @param {Number} value
+ * @param {String} dataType A data type string, like "float32", "int8",
  *     more data type strings, please see:
  *     https://webmachinelearning.github.io/webnn/#enumdef-mloperandtype
- * @return {number} A 64-bit signed integer.
+ * @return {Number} A 64-bit signed integer.
  */
-  function getBitwise(value, dataType) {
+function getBitwise(value, dataType) {
   const buffer = new ArrayBuffer(8);
   const int64Array = new BigInt64Array(buffer);
   int64Array[0] = value < 0 ? ~BigInt(0) : BigInt(0);
   let typedArray;
 
   if (dataType === "float32") {
-      typedArray = new Float32Array(buffer);
+    typedArray = new Float32Array(buffer);
   } else {
-      throw new AssertionError(`Data type ${dataType} is not supported`);
+    throw new AssertionError(`Data type ${dataType} is not supported`);
   }
 
   typedArray[0] = value;
@@ -34,16 +43,14 @@ const DeviceTypeArray = ['cpu', 'gpu'];
  * Assert that each array property in ``actual`` is a number being close enough to the corresponding
  * property in ``expected`` by the acceptable ULP distance ``nulp`` with given ``dataType`` data type.
  *
- * @param {string} op
  * @param {Array} actual - Array of test values.
  * @param {Array} expected - Array of values expected to be close to the values in ``actual``.
- * @param {number} [nulp=0] - A BigInt value indicates acceptable ULP distance, default 0.
- * @param {string} [dataType="float32"] - A data type string, default "float32",
+ * @param {Number} nulp - A BigInt value indicates acceptable ULP distance.
+ * @param {String} [dataType="float32"] - A data type string, default "float32",
  *     more data type strings, please see:
  *     https://webmachinelearning.github.io/webnn/#enumdef-mloperandtype
  */
-function assert_array_approx_equals_ulp(actual, expected, nulp, dataType)
-{
+function assert_array_approx_equals_ulp(actual, expected, nulp, dataType) {
   /*
     * Test if two primitive arrays are equal within acceptable ULP distance
     */
@@ -51,11 +58,27 @@ function assert_array_approx_equals_ulp(actual, expected, nulp, dataType)
               `assert_array_approx_equals_ulp actual length ${actual.length} should be equal to expected length ${expected.length}`);
   let actualBitwise, expectedBitwise, distance;
   for (let i = 0; i < actual.length; i++) {
-      actualBitwise = getBitwise(actual[i], dataType);
-      expectedBitwise = getBitwise(expected[i], dataType);
-      distance = actualBitwise - expectedBitwise;
-      distance = distance >= 0 ? distance : -distance;
-      assert_true(distance <= nulp,
-                  `The distance of ${actual[i]} should be close enough to the distance of ${expected[i]} by the acceptable ULP distance ${nulp}, while current they have ${distance} ULP distance`);
+    actualBitwise = getBitwise(actual[i], dataType);
+    expectedBitwise = getBitwise(expected[i], dataType);
+    distance = actualBitwise - expectedBitwise;
+    distance = distance >= 0 ? distance : -distance;
+    assert_true(distance <= nulp,
+                `actual ${actual[i]} should be close enough to expected ${expected[i]} by the acceptable ${nulp} ULP distance, while they have ${distance} ULP distance`);
   }
+}
+
+function sizeOfShape(array) {
+  return array.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
+}
+
+/**
+ * Get tests JSON information from specified file.
+ * @param {String} file - file URL
+ * @returns {Object}
+ */
+async function getTestsJsonInfo(file) {
+  const response = await fetch(file);
+  const text = await response.text();
+  const jsonObject = JSON.parse(text.replace(/\\"|"(?:\\"|[^"])*"|(\/\/.*|\/\*[\s\S]*?\*\/)/g, (m, g) => g ? "" : m));
+  return jsonObject;
 }

e5d1873e101618b05e8871b8bc4a4bd4b757ed45
Maksim Sadym <69349599+sadym-chromium@users.noreply.github.com>
2022-12-15 16:43:35 +0100

    Update webdriver/tests/bidi/browsing_context/classic_interop/window_handle.py
    Co-authored-by: Henrik Skupin <mail@hskupin.info>

--- webdriver/tests/bidi/browsing_context/classic_interop/window_handle.py
+++ webdriver/tests/bidi/browsing_context/classic_interop/window_handle.py
@@ -3,5 +3,5 @@ import pytest
 pytestmark = pytest.mark.asyncio
 
 
-async def test_top_level_context_has_the_same_ids(top_context, current_session):
+async def test_top_level_context_id_equals_window_handle(top_context, current_session):
     assert top_context["context"] == current_session.window_handle

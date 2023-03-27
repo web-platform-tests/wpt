24e4c951c03e76e164ad741d08ac6ff49a29d773
Anne van Kesteren <annevk@annevk.nl>
2023-03-27 10:14:56 +0200

    align url-origin titles with url-constructor

--- url/url-origin.any.js
+++ url/url-origin.any.js
@@ -1,18 +1,16 @@
 promise_test(() => fetch("resources/urltestdata.json").then(res => res.json()).then(runURLTests), "Loading data…");
 
-function bURL(url, base) {
-  return base !== null ? new URL(url, base) : new URL(url);
-}
-
 function runURLTests(urlTests) {
   for (const expected of urlTests) {
     // Skip comments and tests without "origin" expectation
     if (typeof expected === "string" || !("origin" in expected))
       continue;
 
+    const base = expected.base !== null ? expected.base : undefined;
+
     test(() => {
-      const url = bURL(expected.input, expected.base);
+      const url = new URL(expected.input, base);
       assert_equals(url.origin, expected.origin, "origin");
-    }, "Origin parsing: <" + expected.input + "> against <" + expected.base + ">");
+    }, `Origin parsing: <${expected.input}> ${base ? "against <" + base + ">" : "without base"}`);
   }
 }

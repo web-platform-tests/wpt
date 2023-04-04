5c30b7041ea0361165a4e95bf458713091806502
Maksim Sadym <sadym@google.com>
2023-04-04 09:33:12 +0200

    Do not tolerate `EOF\n`

--- webdriver/tests/support/pdf.py
+++ webdriver/tests/support/pdf.py
@@ -4,5 +4,5 @@ from base64 import decodebytes
 def assert_pdf(value):
     data = decodebytes(value.encode())
 
-    assert data.startswith(b"%PDF-"), "Decoded data starts with the PDF signature"
-    assert data.endswith(b"%%EOF\n") or data.endswith(b"%%EOF"), "Decoded data ends with the EOF flag"
+    assert data.startswith(b"%PDF-"), "Decoded data should start with the PDF signature"
+    assert data.endswith(b"%%EOF"), "Decoded data should end with the EOF flag"

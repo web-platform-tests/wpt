c08a890ccdca2a4bcfe4d05165260553f15a9654
Marcos Caceres <marcos@marcosc.com>
2023-04-04 14:45:31 +1000

    Use assert_throws_js

--- workers/interfaces/WorkerUtils/navigator/008.worker.js
+++ workers/interfaces/WorkerUtils/navigator/008.worker.js
@@ -1,16 +1,12 @@
 "use strict";
 importScripts("/resources/testharness.js");
 test(function () {
-  let log = [];
   for (const x in navigator) {
     // skip functions, as they are settable
     if (typeof navigator[x] === "function") continue;
-
-    // Trying to override a property should throw in strict mode
-    try {
+    assert_throws_js(TypeError, () => {
       navigator[x] = "";
-      log.push(x);
-    } catch (err) {}
+    }, `navigator.${x} is read-only`);
   }
 }, "navigator properties are read-only");
 done();

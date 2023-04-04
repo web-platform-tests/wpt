0290b6f62fdd16d63f8a86b06529aa3f5b96fcb1
Maksim Sadym <sadym@google.com>
2023-04-04 12:05:43 +0200

    Tolerate both `EOF` and `EOF\n`

--- webdriver/tests/support/pdf.py
+++ webdriver/tests/support/pdf.py
@@ -5,4 +5,4 @@ def assert_pdf(value):
     data = decodebytes(value.encode())
 
     assert data.startswith(b"%PDF-"), "Decoded data should start with the PDF signature"
-    assert data.endswith(b"%%EOF"), "Decoded data should end with the EOF flag"
+    assert data.endswith(b"%%EOF\n") or data.endswith(b"%%EOF"), "Decoded data should end with the EOF flag"

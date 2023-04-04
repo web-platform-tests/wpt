315a1796b9636159e82d0f62b5320afe1ee8a89a
Maksim Sadym <sadym@google.com>
2023-04-04 14:07:33 +0200

    Add comment

--- webdriver/tests/support/pdf.py
+++ webdriver/tests/support/pdf.py
@@ -5,4 +5,5 @@ def assert_pdf(value):
     data = decodebytes(value.encode())
 
     assert data.startswith(b"%PDF-"), "Decoded data should start with the PDF signature"
+    # TODO: removed `data.endswith(b"%%EOF")` after https://crbug.com/1430371 is fixed.
     assert data.endswith(b"%%EOF\n") or data.endswith(b"%%EOF"), "Decoded data should end with the EOF flag"

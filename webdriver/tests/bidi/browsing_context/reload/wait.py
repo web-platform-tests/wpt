cfd785431e9bbb529b45e31078efd344b29f43da
Thiago Perrotta <tperrotta@chromium.org>
2023-05-24 22:06:00 +0200

    check order of events

--- webdriver/tests/bidi/browsing_context/reload/wait.py
+++ webdriver/tests/bidi/browsing_context/reload/wait.py
@@ -29,6 +29,7 @@ async def test_expected_url(bidi_session, inline, new_tab, wait):
 )
 async def test_slow_image_blocks_load(bidi_session, inline, new_tab, wait,
                                       expect_timeout):
+
     script_url = "/webdriver/tests/bidi/browsing_context/navigate/support/empty.svg"
     url = inline(f"<img src='{script_url}?pipe=trickle(d10)'>")
 
@@ -67,7 +68,27 @@ async def test_slow_image_blocks_load(bidi_session, inline, new_tab, wait,
         ("complete", True),
     ],
 )
-async def test_slow_page(bidi_session, new_tab, url, wait, expect_timeout):
+async def test_slow_page(bidi_session, new_tab, url, wait, expect_timeout,
+                         subscribe_events, wait_for_event):
+    await subscribe_events(
+        events=["browsingContext.domContentLoaded", "browsingContext.load"],
+        contexts=[new_tab["context"]])
+
+    events = []
+
+    async def on_event(_, data):
+        events.append(data)
+
+    remove_listener_1 = bidi_session.add_event_listener(
+        "browsingContext.domContentLoaded", on_event)
+    remove_listener_2 = bidi_session.add_event_listener(
+        "browsingContext.load", on_event)
+
+    assert len(events) == 0
+
+    on_dom_content_load = wait_for_event("browsingContext.domContentLoaded")
+    on_load = wait_for_event("browsingContext.load")
+
     url = url(
         "/webdriver/tests/bidi/browsing_context/navigate/support/empty.html?pipe=trickle(d10)"
     )
@@ -93,6 +114,15 @@ async def test_slow_page(bidi_session, new_tab, url, wait, expect_timeout):
     # Note that we cannot assert the top context url here, because the navigation
     # is blocked on the initial url for this test case.
 
+    await asyncio.gather(on_load, on_dom_content_load)
+    assert len(events) == 2
+
+    assert events[0] == await on_load
+    assert events[1] == await on_dom_content_load
+
+    remove_listener_2()
+    remove_listener_1()
+
 
 @pytest.mark.parametrize(
     "wait, expect_timeout",
@@ -104,7 +134,28 @@ async def test_slow_page(bidi_session, new_tab, url, wait, expect_timeout):
 )
 async def test_slow_script_blocks_domContentLoaded(bidi_session, inline,
                                                    new_tab, wait,
-                                                   expect_timeout):
+                                                   expect_timeout,
+                                                   subscribe_events,
+                                                   wait_for_event):
+    await subscribe_events(
+        events=["browsingContext.domContentLoaded", "browsingContext.load"],
+        contexts=[new_tab["context"]])
+
+    events = []
+
+    async def on_event(_, data):
+        events.append(data)
+
+    remove_listener_1 = bidi_session.add_event_listener(
+        "browsingContext.domContentLoaded", on_event)
+    remove_listener_2 = bidi_session.add_event_listener(
+        "browsingContext.load", on_event)
+
+    assert len(events) == 0
+
+    on_dom_content_load = wait_for_event("browsingContext.domContentLoaded")
+    on_load = wait_for_event("browsingContext.load")
+
     script_url = "/webdriver/tests/bidi/browsing_context/navigate/support/empty.js"
     url = inline(f"<script src='{script_url}?pipe=trickle(d10)'></script>")
 
@@ -130,3 +181,12 @@ async def test_slow_script_blocks_domContentLoaded(bidi_session, inline,
     # but since we expect both "interactive" and "complete" to timeout the
     # wait_for_navigation helper will resume arbitrarily after 1 second, and
     # there is no guarantee that the URL has been updated.
+
+    await asyncio.gather(on_load, on_dom_content_load)
+    assert len(events) == 2
+
+    assert events[0] == await on_load
+    assert events[1] == await on_dom_content_load
+
+    remove_listener_2()
+    remove_listener_1()

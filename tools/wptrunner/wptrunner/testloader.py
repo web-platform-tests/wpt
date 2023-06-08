9f6e0a8d190c1200399217c3e12f836ed1cee6be
Jonathan Lee <jonathanjlee@google.com>
2023-06-08 15:06:16 -0400

    Do not reorder groups for a single-process run

--- tools/wptrunner/wptrunner/testloader.py
+++ tools/wptrunner/wptrunner/testloader.py
@@ -406,17 +406,18 @@ class TestSource:
         mp = mpcontext.get_context()
         test_queue = mp.Queue()
         groups = cls.make_groups(tests_by_type, **kwargs)
-        group_key = lambda group: (
-            # Place groups of the same test type together to minimize
-            # browser restarts.
-            group.test_type,
-            # Next, run larger groups first to avoid straggler runners. Use
-            # timeout to give slow tests greater relative weight.
-            -sum(test.timeout for test in group.group),
-        )
-        for item in sorted(groups, key=group_key):
-            test_queue.put(item)
         processes = cls.process_count(kwargs["processes"], len(groups))
+        if processes > 1:
+            groups.sort(key=lambda group: (
+                # Place groups of the same test type together to minimize
+                # browser restarts.
+                group.test_type,
+                # Next, run larger groups first to avoid straggler runners. Use
+                # timeout to give slow tests greater relative weight.
+                -sum(test.timeout for test in group.group),
+            ))
+        for item in groups:
+            test_queue.put(item)
         cls.add_sentinal(test_queue, processes)
         return test_queue, processes
 

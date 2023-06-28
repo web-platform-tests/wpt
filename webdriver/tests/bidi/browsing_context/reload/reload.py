05330de2e513f1c8030378b6e1e35c7ff9c80baa
Thiago Perrotta <tperrotta@chromium.org>
2023-06-28 18:43:27 +0200

    add image test

--- webdriver/tests/bidi/browsing_context/reload/reload.py
+++ webdriver/tests/bidi/browsing_context/reload/reload.py
@@ -2,6 +2,8 @@ import pytest
 
 pytestmark = pytest.mark.asyncio
 
+PNG_BLACK_DOT = "/webdriver/tests/bidi/browsing_context/navigate/support/black_dot.png"
+
 
 async def test_return_value(bidi_session, inline, new_tab):
     url = inline("<div>foo</div>")
@@ -14,20 +16,36 @@ async def test_return_value(bidi_session, inline, new_tab):
 
 
 @pytest.mark.parametrize(
-    "url",
-    ["about:blank", "https://example.com/#foo", "data:text/html,<p>foo</p>"],
+    "initial_url",
+    [
+        "about:blank",
+        "https://example.com/#foo",
+        "data:text/html,<p>foo</p>",
+    ],
     ids=[
         "about:blank",
         "hash",
         "data url",
     ],
 )
-async def test_reload(bidi_session, new_tab, url):
+async def test_reload(bidi_session, new_tab, initial_url):
     await bidi_session.browsing_context.navigate(context=new_tab["context"],
-                                                 url=url)
+                                                 url=initial_url)
     await bidi_session.browsing_context.reload(context=new_tab["context"])
 
     contexts = await bidi_session.browsing_context.get_tree(
         root=new_tab['context'])
     assert len(contexts) == 1
-    assert contexts[0]["url"] == url
+    assert contexts[0]["url"] == initial_url
+
+
+async def test_image(bidi_session, new_tab, url):
+    initial_url = url(PNG_BLACK_DOT)
+    await bidi_session.browsing_context.navigate(context=new_tab["context"],
+                                                 url=initial_url)
+    await bidi_session.browsing_context.reload(context=new_tab["context"])
+
+    contexts = await bidi_session.browsing_context.get_tree(
+        root=new_tab['context'])
+    assert len(contexts) == 1
+    assert contexts[0]["url"] == initial_url

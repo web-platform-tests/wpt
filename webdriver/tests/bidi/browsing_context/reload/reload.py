5fe30b0fc33f86ddc17eec1be532707eb7d837f5
Thiago Perrotta <tperrotta@chromium.org>
2023-06-28 18:26:47 +0200

    remove redundant result check in test_reload

--- webdriver/tests/bidi/browsing_context/reload/reload.py
+++ webdriver/tests/bidi/browsing_context/reload/reload.py
@@ -25,9 +25,7 @@ async def test_return_value(bidi_session, inline, new_tab):
 async def test_reload(bidi_session, new_tab, url):
     await bidi_session.browsing_context.navigate(context=new_tab["context"],
                                                  url=url)
-    result = await bidi_session.browsing_context.reload(
-        context=new_tab["context"])
-    assert result == {}
+    await bidi_session.browsing_context.reload(context=new_tab["context"])
 
     contexts = await bidi_session.browsing_context.get_tree(
         root=new_tab['context'])

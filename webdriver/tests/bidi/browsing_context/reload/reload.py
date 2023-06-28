90eb3377dd94260f5d0bb23a5e54c57a88b74b3d
Thiago Perrotta <tperrotta@chromium.org>
2023-06-28 18:15:43 +0200

    move hash to its own file, similarly to navigate tests

--- webdriver/tests/bidi/browsing_context/reload/reload.py
+++ webdriver/tests/bidi/browsing_context/reload/reload.py
@@ -11,15 +11,3 @@ async def test_payload(bidi_session, inline, new_tab):
     result = await bidi_session.browsing_context.reload(
         context=new_tab["context"])
     assert result == {}
-
-
-async def test_hash_is_preserved(bidi_session, new_tab):
-    initial_url = "https://example.com/#foo"
-    await bidi_session.browsing_context.navigate(context=new_tab["context"],
-                                                 url=initial_url)
-    await bidi_session.browsing_context.reload(context=new_tab["context"])
-
-    contexts = await bidi_session.browsing_context.get_tree(
-        root=new_tab['context'])
-    assert len(contexts) == 1
-    assert contexts[0]["url"] == initial_url

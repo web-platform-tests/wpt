d8a2ad0fe5c4e8705346a2f8373aeef690f25e36
Thiago Perrotta <tperrotta@chromium.org>
2023-06-28 18:24:15 +0200

    merge hash and about_blank

--- webdriver/tests/bidi/browsing_context/reload/reload.py
+++ webdriver/tests/bidi/browsing_context/reload/reload.py
@@ -11,3 +11,27 @@ async def test_payload(bidi_session, inline, new_tab):
     result = await bidi_session.browsing_context.reload(
         context=new_tab["context"])
     assert result == {}
+
+
+@pytest.mark.parametrize(
+    "url",
+    [
+        "about:blank",
+        "https://example.com/#foo",
+    ],
+    ids=[
+        "blank",
+        "hash",
+    ],
+)
+async def test_reload(bidi_session, new_tab, url):
+    await bidi_session.browsing_context.navigate(context=new_tab["context"],
+                                                 url=url)
+    result = await bidi_session.browsing_context.reload(
+        context=new_tab["context"])
+    assert result == {}
+
+    contexts = await bidi_session.browsing_context.get_tree(
+        root=new_tab['context'])
+    assert len(contexts) == 1
+    assert contexts[0]["url"] == url

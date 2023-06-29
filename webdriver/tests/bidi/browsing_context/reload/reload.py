5209e96b9de19f08182a06c200e9a3a907b79e20
Thiago Perrotta <tperrotta@chromium.org>
2023-06-29 15:55:53 +0200

    add wait=complete

--- webdriver/tests/bidi/browsing_context/reload/reload.py
+++ webdriver/tests/bidi/browsing_context/reload/reload.py
@@ -35,7 +35,8 @@ async def test_return_value(bidi_session, inline, new_tab):
 async def test_reload(bidi_session, new_tab, initial_url):
     await bidi_session.browsing_context.navigate(context=new_tab["context"],
                                                  url=initial_url)
-    await bidi_session.browsing_context.reload(context=new_tab["context"])
+    await bidi_session.browsing_context.reload(context=new_tab["context"],
+                                               wait="complete")
 
     contexts = await bidi_session.browsing_context.get_tree(
         root=new_tab['context'])
@@ -47,7 +48,8 @@ async def test_image(bidi_session, new_tab, url):
     initial_url = url(PNG_BLACK_DOT)
     await bidi_session.browsing_context.navigate(context=new_tab["context"],
                                                  url=initial_url)
-    await bidi_session.browsing_context.reload(context=new_tab["context"])
+    await bidi_session.browsing_context.reload(context=new_tab["context"],
+                                               wait="complete")
 
     contexts = await bidi_session.browsing_context.get_tree(
         root=new_tab['context'])

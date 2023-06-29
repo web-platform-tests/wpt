6c7fb77c83dde512eec891fe7f6c90989384c3ef
Thiago Perrotta <tperrotta@chromium.org>
2023-06-29 14:45:25 +0200

    inline methods

--- webdriver/tests/bidi/browsing_context/reload/frame.py
+++ webdriver/tests/bidi/browsing_context/reload/frame.py
@@ -2,33 +2,6 @@ import pytest
 
 pytestmark = pytest.mark.asyncio
 
-PAGE_CONTENT = "<div>foo</div>"
-
-
-async def navigate_and_assert(bidi_session, context, url):
-    result = await bidi_session.browsing_context.navigate(
-        context=context['context'], url=url, wait="complete")
-    assert result["url"] == url
-
-    contexts = await bidi_session.browsing_context.get_tree(
-        root=context['context'])
-    assert len(contexts) == 1
-    assert contexts[0]["url"] == url
-
-    return contexts
-
-
-async def reload_and_assert(bidi_session, context):
-    result = await bidi_session.browsing_context.reload(
-        context=context['context'])
-    assert result == {}
-
-    contexts = await bidi_session.browsing_context.get_tree(
-        root=context['context'])
-    assert len(contexts) == 1
-
-    return contexts
-
 
 @pytest.mark.parametrize("domain", ["", "alt"],
                          ids=["same_origin", "cross_origin"])
@@ -36,10 +9,26 @@ async def test_origin(bidi_session, new_tab, inline, domain):
     frame_start_url = inline("frame")
     url_before = inline(f"<iframe src='{frame_start_url}'></iframe>",
                         domain=domain)
-    contexts = await navigate_and_assert(bidi_session, new_tab, url_before)
+
+    # Navigate and assert (top-level).
+    result = await bidi_session.browsing_context.navigate(
+        context=new_tab['context'], url=url_before, wait="complete")
+    assert result["url"] == url_before
+
+    contexts = await bidi_session.browsing_context.get_tree(
+        root=new_tab['context'])
+    assert len(contexts) == 1
+    assert contexts[0]["url"] == url_before
 
     assert len(contexts[0]["children"]) == 1
     frame = contexts[0]["children"][0]
     assert frame["url"] == frame_start_url
 
-    await reload_and_assert(bidi_session, frame)
+    # Reload and assert (frame).
+    result = await bidi_session.browsing_context.reload(
+        context=frame['context'])
+    assert result == {}
+
+    contexts = await bidi_session.browsing_context.get_tree(
+        root=frame['context'])
+    assert len(contexts) == 1

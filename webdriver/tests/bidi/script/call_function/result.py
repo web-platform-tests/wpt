d172fb95b33b6aad6d56a782ecf4dfc4630f6faf
Thiago Perrotta <tperrotta@chromium.org>
2023-06-29 16:33:33 +0200

    remove formatting changes to result.py as they are too noisy

--- webdriver/tests/bidi/script/call_function/result.py
+++ webdriver/tests/bidi/script/call_function/result.py
@@ -9,60 +9,22 @@ pytestmark = pytest.mark.asyncio
 @pytest.mark.parametrize(
     "expression, expected",
     [
-        ("undefined", {
-            "type": "undefined"
-        }),
-        ("null", {
-            "type": "null"
-        }),
-        ("'foobar'", {
-            "type": "string",
-            "value": "foobar"
-        }),
-        ("'2'", {
-            "type": "string",
-            "value": "2"
-        }),
-        ("Number.NaN", {
-            "type": "number",
-            "value": "NaN"
-        }),
-        ("-0", {
-            "type": "number",
-            "value": "-0"
-        }),
-        ("Infinity", {
-            "type": "number",
-            "value": "Infinity"
-        }),
-        ("-Infinity", {
-            "type": "number",
-            "value": "-Infinity"
-        }),
-        ("3", {
-            "type": "number",
-            "value": 3
-        }),
-        ("1.4", {
-            "type": "number",
-            "value": 1.4
-        }),
-        ("true", {
-            "type": "boolean",
-            "value": True
-        }),
-        ("false", {
-            "type": "boolean",
-            "value": False
-        }),
-        ("42n", {
-            "type": "bigint",
-            "value": "42"
-        }),
+        ("undefined", {"type": "undefined"}),
+        ("null", {"type": "null"}),
+        ("'foobar'", {"type": "string", "value": "foobar"}),
+        ("'2'", {"type": "string", "value": "2"}),
+        ("Number.NaN", {"type": "number", "value": "NaN"}),
+        ("-0", {"type": "number", "value": "-0"}),
+        ("Infinity", {"type": "number", "value": "Infinity"}),
+        ("-Infinity", {"type": "number", "value": "-Infinity"}),
+        ("3", {"type": "number", "value": 3}),
+        ("1.4", {"type": "number", "value": 1.4}),
+        ("true", {"type": "boolean", "value": True}),
+        ("false", {"type": "boolean", "value": False}),
+        ("42n", {"type": "bigint", "value": "42"}),
     ],
 )
-async def test_primitive_values(bidi_session, top_context, await_promise,
-                                expression, expected):
+async def test_primitive_values(bidi_session, top_context, await_promise, expression, expected):
     function_declaration = f"()=>{expression}"
     if await_promise:
         function_declaration = "async" + function_declaration
@@ -80,26 +42,15 @@ async def test_primitive_values(bidi_session, top_context, await_promise,
 @pytest.mark.parametrize(
     "expression, expected",
     [
-        ("(Symbol('foo'))", {
-            "type": "symbol"
-        }),
+        ("(Symbol('foo'))", {"type": "symbol"}),
         (
             "[1, 'foo', true, new RegExp(/foo/g), [1]]",
             {
                 "type": "array",
                 "value": [
-                    {
-                        "type": "number",
-                        "value": 1
-                    },
-                    {
-                        "type": "string",
-                        "value": "foo"
-                    },
-                    {
-                        "type": "boolean",
-                        "value": True
-                    },
+                    {"type": "number", "value": 1},
+                    {"type": "string", "value": "foo"},
+                    {"type": "boolean", "value": True},
                     {
                         "type": "regexp",
                         "value": {
@@ -107,9 +58,7 @@ async def test_primitive_values(bidi_session, top_context, await_promise,
                             "flags": "g",
                         },
                     },
-                    {
-                        "type": "array"
-                    },
+                    {"type": "array"},
                 ],
             },
         ),
@@ -118,52 +67,24 @@ async def test_primitive_values(bidi_session, top_context, await_promise,
             {
                 "type": "object",
                 "value": [
-                    ["1", {
-                        "type": "string",
-                        "value": "fred"
-                    }],
-                    ["2", {
-                        "type": "string",
-                        "value": "thud"
-                    }],
-                    ["foo", {
-                        "type": "object"
-                    }],
-                    ["qux", {
-                        "type": "string",
-                        "value": "quux"
-                    }],
+                    ["1", {"type": "string", "value": "fred"}],
+                    ["2", {"type": "string", "value": "thud"}],
+                    ["foo", {"type": "object"}],
+                    ["qux", {"type": "string", "value": "quux"}],
                 ],
             },
         ),
-        ("(()=>{})", {
-            "type": "function"
-        }),
-        ("(function(){})", {
-            "type": "function"
-        }),
-        ("(async ()=>{})", {
-            "type": "function"
-        }),
-        ("(async function(){})", {
-            "type": "function"
-        }),
+        ("(()=>{})", {"type": "function"}),
+        ("(function(){})", {"type": "function"}),
+        ("(async ()=>{})", {"type": "function"}),
+        ("(async function(){})", {"type": "function"}),
         (
             "new RegExp(/foo/g)",
-            {
-                "type": "regexp",
-                "value": {
-                    "pattern": "foo",
-                    "flags": "g"
-                }
-            },
+            {"type": "regexp", "value": {"pattern": "foo", "flags": "g"}},
         ),
         (
             "new Date(1654004849000)",
-            {
-                "type": "date",
-                "value": "2022-05-31T13:47:29.000Z"
-            },
+            {"type": "date", "value": "2022-05-31T13:47:29.000Z"},
         ),
         (
             "new Map([[1, 2], ['foo', 'bar'], [true, false], ['baz', [1]]])",
@@ -171,32 +92,15 @@ async def test_primitive_values(bidi_session, top_context, await_promise,
                 "type": "map",
                 "value": [
                     [
-                        {
-                            "type": "number",
-                            "value": 1
-                        },
-                        {
-                            "type": "number",
-                            "value": 2
-                        },
+                        {"type": "number", "value": 1},
+                        {"type": "number", "value": 2},
                     ],
-                    ["foo", {
-                        "type": "string",
-                        "value": "bar"
-                    }],
+                    ["foo", {"type": "string", "value": "bar"}],
                     [
-                        {
-                            "type": "boolean",
-                            "value": True
-                        },
-                        {
-                            "type": "boolean",
-                            "value": False
-                        },
+                        {"type": "boolean", "value": True},
+                        {"type": "boolean", "value": False},
                     ],
-                    ["baz", {
-                        "type": "array"
-                    }],
+                    ["baz", {"type": "array"}],
                 ],
             },
         ),
@@ -205,24 +109,11 @@ async def test_primitive_values(bidi_session, top_context, await_promise,
             {
                 "type": "set",
                 "value": [
-                    {
-                        "type": "number",
-                        "value": 1
-                    },
-                    {
-                        "type": "string",
-                        "value": "foo"
-                    },
-                    {
-                        "type": "boolean",
-                        "value": True
-                    },
-                    {
-                        "type": "array"
-                    },
-                    {
-                        "type": "map"
-                    },
+                    {"type": "number", "value": 1},
+                    {"type": "string", "value": "foo"},
+                    {"type": "boolean", "value": True},
+                    {"type": "array"},
+                    {"type": "map"},
                 ],
             },
         ),
@@ -262,8 +153,7 @@ async def test_primitive_values(bidi_session, top_context, await_promise,
         ),
     ],
 )
-async def test_remote_values(bidi_session, top_context, await_promise,
-                             expression, expected):
+async def test_remote_values(bidi_session, top_context, await_promise, expression, expected):
     function_declaration = f"()=>{expression}"
     if await_promise:
         function_declaration = "async" + function_declaration

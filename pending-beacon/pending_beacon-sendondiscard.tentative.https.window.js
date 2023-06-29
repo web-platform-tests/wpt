0bc6844b8d7ffd491e5ce95a43a06ee34816f563
Ming-Ying Chung <mych@chromium.org>
2023-06-29 01:02:06 -0700

    [debug] Set up env for testing sending beacons on closing popup
    DO NOT SUBMIT
    
    Change-Id: Iea3fa18bdc5110cce924aae8387ed4cde60c16a6

--- pending-beacon/pending_beacon-sendondiscard.tentative.https.window.js
+++ pending-beacon/pending_beacon-sendondiscard.tentative.https.window.js
@@ -5,6 +5,7 @@
 
 'use strict';
 
+/*
 parallelPromiseTest(async t => {
   const uuid = token();
   const url = generateSetBeaconURL(uuid);
@@ -95,3 +96,46 @@ parallelPromiseTest(async t => {
   await expectBeacon(uuid, {count: total});
 }, `A discarded document sends all its beacons of which backgroundTimeouts are
     not default.`);
+*/
+
+parallelPromiseTest(async t => {
+  const uuid = token();
+  const url = generateSetBeaconURL(uuid);
+  const numPerMethod = 1;
+  const total = numPerMethod * 1;
+
+  // Open a popup that creates `numPerMethod` GET & POST beacons.
+  // Initially, the response is a 204 and the navigation is canceled. As a
+  // result, the popup will be left with the initial empty document and NO
+  // pending navigation.
+  const popup = window.open('/common/blank.html?pipe=status(204)', '_blank');
+  await new Promise(res => t.step_timeout(res, 1000));
+
+  let scriptLoaded =
+      new Promise(res => window.addEventListener('message', e => {
+        if (e.data == 'script loaded')
+          res();
+      }));
+  let allQueued = new Promise(res => window.addEventListener('message', e => {
+    if (e.data == 'all queued')
+      res();
+  }));
+  const script = `
+    window.addEventListener('pagehide', (e) => {
+      const url = "${url}";
+      for (let i = 0; i < ${numPerMethod}; i++) {
+        let get = new PendingGetBeacon(url);
+      }
+      window.opener.postMessage('all queued', "*");
+    });
+    window.opener.postMessage('script loaded', "*");
+  `;
+  popup.document.write(`<script>${script}</script>`);
+  await scriptLoaded;
+  // Close the popup to trigger beacon sending.
+  popup.close();
+  await allQueued;
+
+  // The popup should have sent all beacons.
+  await expectBeacon(uuid, {count: total});
+}, 'A closed popup window send all its beacons with default config.');

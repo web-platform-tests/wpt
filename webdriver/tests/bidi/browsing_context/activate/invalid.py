baf6ecb0e10bdb940e38cfc43d1bca7f9cfa6ee1
Alex Rudenko <alexrudenko@chromium.org>
2023-07-12 10:42:28 +0200

    add more tests

--- webdriver/tests/bidi/browsing_context/activate/invalid.py
+++ webdriver/tests/bidi/browsing_context/activate/invalid.py
@@ -18,3 +18,21 @@ async def test_params_context_invalid_value(bidi_session, value):
         await bidi_session.browsing_context.activate(
             context=value
         )
+
+
+@pytest.mark.asyncio
+async def test_params_context_iframe(bidi_session, new_tab, get_test_page):
+    url = get_test_page(as_frame=True)
+    await bidi_session.browsing_context.navigate(
+        context=new_tab["context"],
+        url=url,
+        wait="complete")
+
+    contexts = await bidi_session.browsing_context.get_tree(root=new_tab["context"])
+    assert len(contexts) == 1
+    frames = contexts[0]["children"]
+    assert len(frames) == 1
+    frame_context = frames[0]["context"]
+
+    with pytest.raises(error.InvalidArgumentException):
+        await bidi_session.browsing_context.activate(context=frame_context)

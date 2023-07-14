d775bf38a8d682e270f4e2137322731bce3325d4
Alex Rudenko <alexrudenko@chromium.org>
2023-07-14 12:36:59 +0200

    add try/finally

--- webdriver/tests/bidi/browsing_context/create/background.py
+++ webdriver/tests/bidi/browsing_context/create/background.py
@@ -8,9 +8,10 @@ from .. import get_visibility_state
 async def test_background_default_false(bidi_session, type_hint):
     new_context = await bidi_session.browsing_context.create(type_hint=type_hint)
 
-    assert await get_visibility_state(bidi_session, new_context) == "visible"
-
-    await bidi_session.browsing_context.close(context=new_context["context"])
+    try:
+      assert await get_visibility_state(bidi_session, new_context) == "visible"
+    finally:
+      await bidi_session.browsing_context.close(context=new_context["context"])
 
 
 @pytest.mark.parametrize("type_hint", ["tab", "window"])
@@ -18,6 +19,7 @@ async def test_background_default_false(bidi_session, type_hint):
 async def test_background(bidi_session, type_hint, background):
     new_context = await bidi_session.browsing_context.create(type_hint=type_hint, background=background)
 
-    assert await get_visibility_state(bidi_session, new_context) == ("hidden" if background else "visible")
-
-    await bidi_session.browsing_context.close(context=new_context["context"])
+    try:
+      assert await get_visibility_state(bidi_session, new_context) == ("hidden" if background else "visible")
+    finally:
+      await bidi_session.browsing_context.close(context=new_context["context"])

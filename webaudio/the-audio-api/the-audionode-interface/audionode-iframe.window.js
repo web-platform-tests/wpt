8bd78e2ad28d22148446bb53bb3e2a8dc1b07d79
Michael Wilson <mjwilson@chromium.org>
2023-08-25 20:41:21 -0700

    Don't skip suspended state when constructing AudioContext
    [WIP]
    
    This change may break applications that rely on the AudioContext
    immediately updating its state before onstatechange fires.
    
    The Web Audio specification says that the AudioContext should start
    in the suspended state, then queue a media element task to transition
    to the running state:
    
    https://webaudio.github.io/web-audio-api/#AudioContext-constructors
    
    However, Chromium's implementation immediately transitions to running
    if the context is allowed to start.
    
    This CL changes our implementation to be more in line with the spec.
    
    In doing so, I saw that all state transitions are specified to happen
    in queued media element tasks.  Some WPTs and other tests thus need
    to be rewritten to wait for onstatechange events before checking the
    audio context state.
    
    Bug:1106380
    Change-Id: Idadfba3a3529b704b8655d2fb4fda2ab76cb8167

--- webaudio/the-audio-api/the-audionode-interface/audionode-iframe.window.js
+++ webaudio/the-audio-api/the-audionode-interface/audionode-iframe.window.js
@@ -8,7 +8,11 @@ test(function() {
   const source = context.createOscillator();
   source.connect(context.destination);
 
-  // AudioContext should be put closed state after iframe destroyed
+  // https://webaudio.github.io/web-audio-api/#dom-audiocontext-close
+  // 3. If this control message is being run in a reaction to the document being
+  // unloaded, abort this algorithm.
+  // There is no need to notify the control thread in this case.
   document.body.removeChild(iframe);
-  assert_equals(context.state, 'closed');
+  // Context will remain in initial state
+  assert_equals(context.state, 'suspended');
 }, 'Call a constructor from iframe page and then destroy the iframe');

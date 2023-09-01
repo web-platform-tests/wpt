20e4ab83e239d810c7f9103bd3adbb487ed4b48a
Benjamin VanderSloot <bvandersloot@mozilla.com>
2023-09-01 13:50:34 -0400

    Rename setCookieHeader.py to match convention

--- storage-access-api/resources/set-cookie-header.py
+++ storage-access-api/resources/set-cookie-header.py
@@ -5,6 +5,7 @@ def main(request, response):
   # Cookies may require whitespace (e.g. in the `Expires` attribute), so the
   # query string should be decoded.
   cookie = unquote(request.url_parts.query)
+  headers = []
   headers.append((b"Set-Cookie", isomorphic_encode(cookie)))
 
   return (200, headers, "")

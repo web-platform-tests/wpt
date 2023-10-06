544fd6c0f6364b5fd9a8b8ac2ffaa46bf12b1b40
Philip Jägenstedt <philip@foolip.org>
2023-10-06 14:42:40 +0200

    Add generated Wasm tests
    These are were built from https://github.com/WebAssembly/spec/tree/main/test

---
+++ wasm/core/js/comments.wast.js
@@ -0,0 +1,15 @@
+(function comments_wast_js() {
+
+// comments.wast:9
+let $1 = instance("\x00\x61\x73\x6d\x01\x00\x00\x00");
+
+// comments.wast:55
+let $2 = instance("\x00\x61\x73\x6d\x01\x00\x00\x00");
+
+// comments.wast:66
+let $3 = instance("\x00\x61\x73\x6d\x01\x00\x00\x00");
+
+// comments.wast:75
+let $4 = instance("\x00\x61\x73\x6d\x01\x00\x00\x00");
+reinitializeRegistry();
+})();

b9fa2bf1d8fdd7dfad4e4b0932acf06a86bdd770
Jim Evans <james.h.evans.jr@gmail.com>
2023-10-26 19:19:44 -0400

    Adding additional tests for maxNodeCount

--- webdriver/tests/bidi/browsing_context/locate_nodes/max_node_count.py
+++ webdriver/tests/bidi/browsing_context/locate_nodes/max_node_count.py
@@ -2,13 +2,140 @@
 import pytest
 
 
-@pytest.mark.parametrize("type,value", [
-    ("css", "div"),
-    ("xpath", "//div"),
-    ("innerText", "foo")
+from ... import any_string, recursive_compare
+
+
+@pytest.mark.parametrize("type,value,max,expected", [
+    ("css", "div", 1, [
+        {
+            "type": "node",
+            "sharedId": any_string,
+            "value": {
+                "attributes": {"data-class":"one"},
+                "childNodeCount": 1,
+                "children": [],
+                "localName": "div",
+                "namespaceURI": "http://www.w3.org/1999/xhtml",
+                "nodeType": 1,
+            },
+        }]
+    ),
+    ("xpath", "//div", 1, [
+        {
+            "type": "node",
+            "sharedId": any_string,
+            "value": {
+                "attributes": {"data-class":"one"},
+                "childNodeCount": 1,
+                "children": [],
+                "localName": "div",
+                "namespaceURI": "http://www.w3.org/1999/xhtml",
+                "nodeType": 1,
+            },
+        }]
+    ),
+    ("innerText", "foo", 1, [
+        {
+            "type": "node",
+            "sharedId": any_string,
+            "value": {
+                "attributes": {"data-class":"one"},
+                "childNodeCount": 1,
+                "children": [],
+                "localName": "div",
+                "namespaceURI": "http://www.w3.org/1999/xhtml",
+                "nodeType": 1,
+            },
+        }]
+    ),
+    ("css", "div", 10, [
+        {
+            "type": "node",
+            "sharedId": any_string,
+            "value": {
+                "attributes": {"data-class":"one"},
+                "childNodeCount": 1,
+                "children": [],
+                "localName": "div",
+                "namespaceURI": "http://www.w3.org/1999/xhtml",
+                "nodeType": 1,
+            },
+        },
+        {
+            "type": "node",
+            "sharedId": any_string,
+            "value": {
+                "attributes": {"data-class":"two"},
+                "childNodeCount": 1,
+                "children": [],
+                "localName": "div",
+                "namespaceURI": "http://www.w3.org/1999/xhtml",
+                "nodeType": 1,
+            },
+        }]
+    ),
+    ("xpath", "//div", 10, [
+        {
+            "type": "node",
+            "sharedId": any_string,
+            "value": {
+                "attributes": {"data-class":"one"},
+                "childNodeCount": 1,
+                "children": [],
+                "localName": "div",
+                "namespaceURI": "http://www.w3.org/1999/xhtml",
+                "nodeType": 1,
+            },
+        },
+        {
+            "type": "node",
+            "sharedId": any_string,
+            "value": {
+                "attributes": {"data-class":"two"},
+                "childNodeCount": 1,
+                "children": [],
+                "localName": "div",
+                "namespaceURI": "http://www.w3.org/1999/xhtml",
+                "nodeType": 1,
+            },
+        }]
+    ),
+    ("innerText", "foo", 10, [
+        {
+            "type": "node",
+            "sharedId": any_string,
+            "value": {
+                "attributes": {"data-class":"one"},
+                "childNodeCount": 1,
+                "children": [],
+                "localName": "div",
+                "namespaceURI": "http://www.w3.org/1999/xhtml",
+                "nodeType": 1,
+            },
+        },
+        {
+            "type": "node",
+            "sharedId": any_string,
+            "value": {
+                "attributes": {"data-class":"two"},
+                "childNodeCount": 1,
+                "children": [],
+                "localName": "div",
+                "namespaceURI": "http://www.w3.org/1999/xhtml",
+                "nodeType": 1,
+            },
+        }]
+    )
+], ids=[
+    "css_single",
+    "xpath_single",
+    "inner_text_single",
+    "css_multiple",
+    "xpath_multiple",
+    "inner_text_multiple"
 ])
 @pytest.mark.asyncio
-async def test_find_by_css_limit_return_count(bidi_session, inline, top_context, type, value):
+async def test_find_by_css_limit_return_count(bidi_session, inline, top_context, type, value, max_count, expected):
     url = inline("""<div data-class="one">foo</div><div data-class="two">foo</div>""")
     await bidi_session.browsing_context.navigate(
         context=top_context["context"], url=url, wait="complete"
@@ -17,16 +144,8 @@ async def test_find_by_css_limit_return_count(bidi_session, inline, top_context,
     result = await bidi_session.browsing_context.locate_nodes(
         context=top_context["context"],
         locator={ "type": type, "value": value },
-        max_node_count = 1
+        max_node_count = max_count
     )
 
     assert result["context"] == top_context["context"]
-    assert result["nodes"].length == 1
-    node_result = result["nodes"][0]
-    assert node_result["type"] == "node"
-    assert node_result["sharedId"] is not None
-    assert node_result["value"] is not None
-    assert node_result["value"]["nodeType"] == 1
-    assert node_result["value"]["localName"] == "div"
-    assert "data-class" in node_result["value"]["attributes"]
-    assert node_result["value"]["attributes"]["data-class"] == "one"
+    recursive_compare(expected, result["nodes"])

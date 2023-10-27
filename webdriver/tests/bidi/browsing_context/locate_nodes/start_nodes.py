aab462168cc122c7d920b10fc7ac916310983166
Jim Evans <james.h.evans.jr@gmail.com>
2023-10-26 20:18:53 -0400

    Add additional testing for startNodes

--- webdriver/tests/bidi/browsing_context/locate_nodes/start_nodes.py
+++ webdriver/tests/bidi/browsing_context/locate_nodes/start_nodes.py
@@ -1,6 +1,8 @@
 import pytest
 
+
 from webdriver.bidi.modules.script import ContextTarget
+from ... import any_string, recursive_compare
 
 
 @pytest.mark.parametrize("type,value", [
@@ -21,21 +23,100 @@ async def test_locate_with_context_nodes(bidi_session, inline, top_context, type
         await_promise=True,
     )
 
-    parent_node_reference = { "sharedId": script_result["result"]["sharedId"] }
+    context_nodes = script_result["result"]
 
     result = await bidi_session.browsing_context.locate_nodes(
         context=top_context["context"],
         locator={ "type": type, "value": value },
-        start_nodes=[parent_node_reference]
+        start_nodes=[context_nodes]
     )
 
-    assert result["context"] == top_context["context"]
-    assert result["nodes"].length == 2
-    result_nodes = result["nodes"]
-    for node_result in result_nodes:
-        assert node_result["type"] == "node"
-        assert node_result["sharedId"] is not None
-        assert node_result["value"] is not None
-        assert node_result["value"]["nodeType"] == 1
-        assert node_result["value"]["localName"] == "div"
+    expected = [
+        {
+            "type": "node",
+            "sharedId": any_string,
+            "value": {
+                "attributes": {"data-class":"one"},
+                "childNodeCount": 1,
+                "children": [],
+                "localName": "div",
+                "namespaceURI": "http://www.w3.org/1999/xhtml",
+                "nodeType": 1,
+            }
+        },
+        {
+            "type": "node",
+            "sharedId": any_string,
+            "value": {
+                "attributes": {"data-class":"two"},
+                "childNodeCount": 1,
+                "children": [],
+                "localName": "div",
+                "namespaceURI": "http://www.w3.org/1999/xhtml",
+                "nodeType": 1,
+            }
+        }
+    ]
 
+    assert result["context"] == top_context["context"]
+    recursive_compare(expected, result["nodes"])
+
+
+@pytest.mark.parametrize("type,value", [
+    ("css", "div[data-class='one']"),
+    ("xpath", ".//div[@data-class='one']"),
+    ("innerText", "foo")
+])
+@pytest.mark.asyncio
+async def test_locate_with_multiple_context_nodes(bidi_session, inline, top_context, type, value):
+    url = inline("""
+                 <p id="parent-one"><div data-class="one">foo</div><div data-class="two">bar</div></p>
+                 <p id="parent-two"><div data-class="one">foo</div><div data-class="two">bar</div></p>
+                 """)
+    await bidi_session.browsing_context.navigate(
+        context=top_context["context"], url=url, wait="complete"
+    )
+
+    script_result = await bidi_session.script.evaluate(
+        expression="""document.querySelectorAll("p")""",
+        target=ContextTarget(top_context["context"]),
+        await_promise=True,
+    )
+
+    context_nodes = script_result["result"]
+
+    result = await bidi_session.browsing_context.locate_nodes(
+        context=top_context["context"],
+        locator={ "type": type, "value": value },
+        start_nodes=context_nodes
+    )
+
+    expected = [
+        {
+            "type": "node",
+            "sharedId": any_string,
+            "value": {
+                "attributes": {"data-class":"one"},
+                "childNodeCount": 1,
+                "children": [],
+                "localName": "div",
+                "namespaceURI": "http://www.w3.org/1999/xhtml",
+                "nodeType": 1,
+            }
+        },
+        {
+            "type": "node",
+            "sharedId": any_string,
+            "value": {
+                "attributes": {"data-class":"one"},
+                "childNodeCount": 1,
+                "children": [],
+                "localName": "div",
+                "namespaceURI": "http://www.w3.org/1999/xhtml",
+                "nodeType": 1,
+            }
+        }
+    ]
+
+    assert result["context"] == top_context["context"]
+    recursive_compare(expected, result["nodes"])

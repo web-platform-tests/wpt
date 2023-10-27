c08bf05a5a26af44b95b557d56e4785d7a152fe8
Alex Rudenko <alexrudenko@chromium.org>
2023-10-27 10:03:22 +0200

    review comments

--- webdriver/tests/bidi/browsing_context/set_viewport/device_pixel_ratio.py
+++ webdriver/tests/bidi/browsing_context/set_viewport/device_pixel_ratio.py
@@ -5,17 +5,20 @@ from ... import get_device_pixel_ratio, get_viewport_dimensions
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("device_pixel_ratio", [1, 2])
-async def test_set_device_pixel_ratio(bidi_session, new_tab, device_pixel_ratio):
+async def test_device_pixel_ratio_only(bidi_session, new_tab, device_pixel_ratio):
+    viewport = await get_viewport_dimensions(bidi_session, new_tab)
+
     await bidi_session.browsing_context.set_viewport(
         context=new_tab["context"],
         device_pixel_ratio=device_pixel_ratio)
 
     assert await get_device_pixel_ratio(bidi_session, new_tab) == device_pixel_ratio
+    assert await get_viewport_dimensions(bidi_session, new_tab) == viewport
 
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("device_pixel_ratio", [1, 2])
-async def test_set_viewport_with_dpr(bidi_session, new_tab, device_pixel_ratio):
+async def test_device_pixel_ratio_with_viewport(bidi_session, new_tab, device_pixel_ratio):
     test_viewport = {"width": 250, "height": 300}
 
     await bidi_session.browsing_context.set_viewport(
@@ -28,7 +31,7 @@ async def test_set_viewport_with_dpr(bidi_session, new_tab, device_pixel_ratio):
 
 
 @pytest.mark.asyncio
-async def test_unset_device_pixel_ratio(bidi_session, new_tab):
+async def test_reset_device_pixel_ratio(bidi_session, new_tab):
     original_dpr = await get_device_pixel_ratio(bidi_session, new_tab)
     test_dpr = original_dpr + 1
 

2436f1767600f9594b49395d7c2cadef83258b40
Henrik Skupin <mail@hskupin.info>
2023-11-03 18:51:47 +0100

    Add mod_pywebsocket dependency for wptserve

--- tools/wptserve/setup.py
+++ tools/wptserve/setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup
 
 PACKAGE_VERSION = '4.0'
-deps = ["h2>=4.1.0"]
+deps = [
+    "h2>=4.1.0",
+    "mod_pywebsocket @ https://github.com/GoogleChromeLabs/pywebsocket3/archive/50602a14f1b6da17e0b619833a13addc6ea78bc2.zip#sha256=4dadd116e67af5625606f883e1973178d4121e8a1dc87b096ba2bb43c692f958",  # noqa
+]
 
 setup(name='wptserve',
       version=PACKAGE_VERSION,

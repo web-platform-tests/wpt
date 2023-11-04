75f2692b940ebe3fdbb18004dc3178f30e80eb33
Guy Bedford <gbedford@fastly.com>
2023-11-03 19:10:27 -0700

    split worker test into with and without source phase

--- wasm/webapi/esm-integration/resources/worker.js
+++ wasm/webapi/esm-integration/resources/worker.js
@@ -1,5 +1,2 @@
 import * as mod from "./worker.wasm";
-import source modSource from "./worker.wasm";
-assert_true(modSource instanceof WebAssembly.Module);
 assert_true(await import("./worker.wasm") === mod);
-assert_true(await import.source("./worker.wasm") === modSource);

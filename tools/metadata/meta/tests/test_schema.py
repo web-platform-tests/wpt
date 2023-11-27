5f7528575a29fc3a2b7c7e879bfcfad6cac46ee5
James Scott <jamescscott@google.com>
2023-11-27 19:32:14 +0000

    Add the ability to parse META.yml files
    Adds SchemaValue which is a container of helper functions to parse
    different types of values for a given key - inspired by quicktype
    
    Add validate_dict to ensure only the given keys in a dictionary are
    allowed.
    
    This functionality will be useful for other YAML files in the future.

---
+++ tools/metadata/meta/tests/test_schema.py
@@ -0,0 +1,67 @@
+# mypy: allow-untyped-defs
+
+from ..schema import MetaFile
+
+import pytest
+import re
+
+@pytest.mark.parametrize(
+    "input,expected_result,expected_exception_type,exception_message",
+    [
+        (
+            {
+                "spec": "spec-value",
+                "suggested_reviewers": ["reviewer_1", "reviewer_2"]
+            },
+            MetaFile(spec="spec-value", suggested_reviewers=["reviewer_1", "reviewer_2"]),
+            None,
+            None
+        ),
+        (
+            {
+                "spec": "spec-value",
+            },
+            MetaFile(spec="spec-value"),
+            None,
+            None
+        ),
+        (
+            {
+                "suggested_reviewers": ["reviewer_1", "reviewer_2"]
+            },
+            MetaFile(suggested_reviewers=["reviewer_1", "reviewer_2"]),
+            None,
+            None
+        ),
+        (
+            {},
+            MetaFile(),
+            None,
+            None
+        ),
+        (
+            {
+                "spec": "spec-value",
+                "suggested_reviewers": ["reviewer_1", 3]
+            },
+            None,
+            ValueError,
+            re.escape("Input value ['reviewer_1', 3] does not fit one of the expected values for the union")
+        ),
+        (
+            {
+                "spec": "spec-value",
+                "suggested_reviewers": ["reviewer_1", "reviewer_2"],
+                "extra": "test"
+            },
+            None,
+            ValueError,
+            "Object contains invalid keys: {'extra'}"
+        ),
+    ])
+def test_from_dict(input, expected_result, expected_exception_type, exception_message):
+    if expected_exception_type:
+        with pytest.raises(expected_exception_type, match=exception_message):
+            MetaFile.from_dict(input)
+    else:
+        assert expected_result == MetaFile.from_dict(input)
---
+++ tools/metadata/yaml/tests/test_file.py
@@ -0,0 +1,22 @@
+# mypy: allow-untyped-defs
+
+from ..load import load_into_object
+from dataclasses import dataclass
+from io import StringIO
+
+@dataclass
+class SampleClass():
+    key: list[str]
+
+def sample_dict_to_object_loader(input):
+    print(input)
+    return SampleClass(**input)
+
+def test_dict_to_object_loader():
+    input_buffer = StringIO("""
+key:
+  - value1
+  - value2
+""")
+    result = load_into_object(sample_dict_to_object_loader, input_buffer)
+    assert result == SampleClass(key=["value1", "value2"])

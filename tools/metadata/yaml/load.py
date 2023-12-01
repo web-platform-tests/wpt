b8b14b0002a5b9d96d33371332b0c554662aa919
James Scott <jamescscott@google.com>
2023-12-01 18:20:32 +0000

    catch yaml load exceptions

--- tools/metadata/yaml/load.py
+++ tools/metadata/yaml/load.py
@@ -1,6 +1,13 @@
-from typing import Any, Dict, Callable, IO
+from typing import Any, Callable, Dict, IO, TypeVar
+from ..meta.schema import SchemaValue
 
 import yaml
 
-def load_into_dict(dict_loader: Callable[[Any], Dict[str, Any]], f: IO[bytes]) -> Dict[str, Any]:
-    return dict_loader(yaml.safe_load(f))
+T = TypeVar("T")
+
+def load_dict_to_obj(obj_loader: Callable[[Dict[str, Any]], T], f: IO[bytes]) -> T:
+    try:
+        raw_data = yaml.safe_load(f)
+        return SchemaValue.from_dict(obj_loader, raw_data)
+    except Exception as e:
+        raise e
--- tools/metadata/yaml/tests/test_file.py
+++ tools/metadata/yaml/tests/test_file.py
@@ -1,22 +1,40 @@
 # mypy: allow-untyped-defs
 
-from ..load import load_into_dict
+from ..load import load_dict_to_obj
 from dataclasses import dataclass
 from io import StringIO
 from typing import Sequence
+
+import pytest
+import yaml
 
 @dataclass
 class SampleClass():
     key: Sequence[str]
 
-def sample_load_to_dict(input):
-    return SampleClass(**input)
+def sample_load_dict_to_obj(input):
+    return SampleClass(key=input.get("key"))
 
-def test_load_to_dict():
+def test_load_dict_to_obj():
     input_buffer = StringIO("""
 key:
   - value1
   - value2
 """)
-    result = load_into_dict(sample_load_to_dict, input_buffer)
+    result = load_dict_to_obj(sample_load_dict_to_obj, input_buffer)
     assert result == SampleClass(key=["value1", "value2"])
+
+def test_load_dict_to_obj_not_dict():
+    input_buffer = StringIO("""
+- key: 2
+""")
+    with pytest.raises(ValueError):
+        load_dict_to_obj(sample_load_dict_to_obj, input_buffer)
+
+def test_load_dict_to_obj_invalid_yaml():
+    input_buffer = StringIO("""
+key: 1
+- test: value
+""")
+    with pytest.raises(yaml.parser.ParserError):
+        load_dict_to_obj(sample_load_dict_to_obj, input_buffer)

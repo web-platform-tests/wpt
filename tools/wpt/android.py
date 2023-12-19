683e49d460e9b622858dc894c949286bff0daaaf
James Graham <james@hoppipolla.co.uk>
2023-12-19 16:53:12 +0000

    Pin emulator version at 32.1.15 on linux
    This is enough to ensure we get a working version in CI; the problem with the
    newer version seems to mostly apply when running in the docker image, so
    hopefully this doesn't disrupt local testing too much

--- tools/wpt/android.py
+++ tools/wpt/android.py
@@ -81,6 +81,43 @@ def get_parser_start():
     return parser
 
 
+def install_fixed_emulator_version(logger, paths):
+    # Downgrade to a pinned emulator version
+    # See https://developer.android.com/studio/emulator_archive for what we're doing here
+    from xml.etree import ElementTree
+
+    version = "32.1.15"
+    urls = {"linux": "https://redirector.gvt1.com/edgedl/android/repository/emulator-linux_x64-10696886.zip"}
+
+    os_name = platform.system().lower()
+    if os_name not in urls:
+        logger.error(f"Don't know how to install old emulator for {os_name}, using latest version")
+        # For now try with the latest version if this fails
+        return
+
+    logger.info(f"Downgrading emulator to {version}")
+    url = urls[os_name]
+
+    emulator_path = os.path.join(paths["sdk"], "emulator")
+    latest_emulator_path = os.path.join(paths["sdk"], "emulator_latest")
+    os.rename(emulator_path, latest_emulator_path)
+
+    download_and_extract(url, paths["sdk"])
+    package_path = os.path.join(emulator_path, "package.xml")
+    shutil.copyfile(os.path.join(latest_emulator_path, "package.xml"),
+                    package_path)
+
+    with open(package_path) as f:
+        tree = ElementTree.parse(f)
+    node = tree.find("localPackage").find("revision")
+    assert len(node) == 3
+    parts = version.split(".")
+    for version_part, node in zip(parts, node):
+        node.text = version_part
+    with open(package_path, "wb") as f:
+        tree.write(f, encoding="utf8")
+
+
 def get_paths(dest):
     os_name = platform.system().lower()
 
@@ -258,6 +295,8 @@ def install(logger, dest=None, reinstall=False, prompt=True):
             install_android_packages(logger, paths, packages, prompt=prompt)
 
             install_avd(logger, paths, prompt=prompt)
+
+            install_fixed_emulator_version(logger, paths)
 
         emulator = get_emulator(paths)
     return emulator

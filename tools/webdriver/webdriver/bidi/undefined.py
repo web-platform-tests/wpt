8a723b17baae510c8f6f9303d8f947ccf9417cf9
Maksim Sadym <sadym@google.com>
2024-01-16 11:47:14 +0100

    Refactor

--- tools/webdriver/webdriver/bidi/undefined.py
+++ tools/webdriver/webdriver/bidi/undefined.py
@@ -1,6 +1,12 @@
+from typing import TypeVar, Dict, Union
+
+
 class Undefined:
     def __init__(self) -> None:
         raise RuntimeError('Import UNDEFINED instead.')
 
+
+_T = TypeVar('_T')
+Maybe = Union[_T, Undefined]
 
 UNDEFINED = Undefined.__new__(Undefined)

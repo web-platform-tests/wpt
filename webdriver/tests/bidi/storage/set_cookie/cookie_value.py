8ff959f51832aa75df8e31aace64a47a8581dff9
Maksim Sadym <sadym@google.com>
2024-01-17 14:55:24 +0100

    Test `value` field with string value

---
+++ webdriver/tests/bidi/storage/set_cookie/cookie_value.py
@@ -0,0 +1,23 @@
+import pytest
+from .. import assert_cookie_is_set, create_cookie
+from webdriver.bidi.modules.network import NetworkStringValue
+
+pytestmark = pytest.mark.asyncio
+
+
+@pytest.mark.parametrize(
+    "str_value",
+    [
+        "simple_value",
+        "special_symbols =!@#$%^&*()_+-{}[]|\\:\"'<>,.?/`~"
+    ])
+async def test_cookie_value_string(bidi_session, top_context, test_page, origin, domain_value, str_value):
+    # Navigate to a secure context.
+    await bidi_session.browsing_context.navigate(context=top_context["context"], url=test_page, wait="complete")
+
+    value = NetworkStringValue(str_value)
+
+    await bidi_session.storage.set_cookie(cookie=create_cookie(domain=domain_value(), value=value))
+    await assert_cookie_is_set(bidi_session, value=value, domain=domain_value(), origin=origin())
+
+# TODO: test `test_cookie_value_base64`.
---
+++ webdriver/tests/bidi/storage/set_cookie/cookie_value_invalid.py
@@ -0,0 +1,23 @@
+import pytest
+from .. import assert_cookie_is_set, create_cookie
+from webdriver.bidi.modules.network import NetworkStringValue
+import webdriver.bidi.error as error
+
+pytestmark = pytest.mark.asyncio
+
+
+@pytest.mark.parametrize(
+    "str_value",
+    [
+        "value\twith\ttab",
+        "value\nwith\nnewline",
+        "value;with;semicolon",
+    ])
+async def test_cookie_value_string_invalid_value(bidi_session, top_context, test_page, origin, domain_value, str_value):
+    # Navigate to a secure context.
+    await bidi_session.browsing_context.navigate(context=top_context["context"], url=test_page, wait="complete")
+
+    value = NetworkStringValue(str_value)
+
+    with pytest.raises(error.UnableToSetCookieException):
+        await bidi_session.storage.set_cookie(cookie=create_cookie(domain=domain_value(), value=value))

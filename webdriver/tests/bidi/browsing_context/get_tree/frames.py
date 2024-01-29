de0b1a6882a59439795933d09dc1ee8fda29d963
Alex Rudenko <alexrudenko@chromium.org>
2024-01-29 17:58:55 +0100

    add a new test

--- webdriver/tests/bidi/browsing_context/get_tree/frames.py
+++ webdriver/tests/bidi/browsing_context/get_tree/frames.py
@@ -5,26 +5,19 @@ from .. import assert_browsing_context
 pytestmark = pytest.mark.asyncio
 
 
-@pytest.mark.parametrize("user_context", ["default", "new"])
 async def test_multiple_frames(
     bidi_session,
-    create_user_context,
-    test_page_multiple_frames,
+    top_context,
     test_page,
     test_page2,
-    user_context
+    test_page_multiple_frames,
 ):
-    if user_context == "new":
-        user_context = await create_user_context()
-
-    context = await bidi_session.browsing_context.create(type_hint="tab", user_context=user_context)
-
     await bidi_session.browsing_context.navigate(
-        context=context["context"], url=test_page_multiple_frames, wait="complete"
+        context=top_context["context"], url=test_page_multiple_frames, wait="complete"
     )
 
     # First retrieve all browsing contexts of the first tab
-    top_level_context_id = context["context"]
+    top_level_context_id = top_context["context"]
     all_contexts = await bidi_session.browsing_context.get_tree(root=top_level_context_id)
 
     assert len(all_contexts) == 1
@@ -35,7 +28,6 @@ async def test_multiple_frames(
         children=2,
         parent=None,
         url=test_page_multiple_frames,
-        user_context=user_context,
     )
 
     child1_info = root_info["children"][0]
@@ -46,7 +38,6 @@ async def test_multiple_frames(
         is_root=False,
         parent=None,
         url=test_page,
-        user_context=user_context,
     )
     assert child1_info["context"] != root_info["context"]
 
@@ -58,7 +49,6 @@ async def test_multiple_frames(
         is_root=False,
         parent=None,
         url=test_page2,
-        user_context=user_context,
     )
     assert child2_info["context"] != root_info["context"]
     assert child2_info["context"] != child1_info["context"]
@@ -98,3 +88,60 @@ async def test_cross_origin(
         url=test_page_cross_origin,
     )
     assert child1_info["context"] != root_info["context"]
+
+
+@pytest.mark.parametrize("user_context", ["default", "new"])
+async def test_user_context(
+    bidi_session,
+    create_user_context,
+    test_page,
+    test_page2,
+    test_page_multiple_frames,
+    user_context
+):
+    user_context_id = await create_user_context() if user_context == "new" else user_context
+
+    context = await bidi_session.browsing_context.create(type_hint="tab", user_context=user_context_id)
+
+    await bidi_session.browsing_context.navigate(
+        context=context["context"], url=test_page_multiple_frames, wait="complete"
+    )
+
+    top_level_context_id = context["context"]
+    all_contexts = await bidi_session.browsing_context.get_tree(root=top_level_context_id)
+
+    assert len(all_contexts) == 1
+    root_info = all_contexts[0]
+    assert_browsing_context(
+        root_info,
+        top_level_context_id,
+        children=2,
+        parent=None,
+        url=test_page_multiple_frames,
+        user_context=user_context_id,
+    )
+
+    child1_info = root_info["children"][0]
+    assert_browsing_context(
+        child1_info,
+        context=None,
+        children=0,
+        is_root=False,
+        parent=None,
+        url=test_page,
+        user_context=user_context_id,
+    )
+    assert child1_info["context"] != root_info["context"]
+
+    child2_info = root_info["children"][1]
+    assert_browsing_context(
+        child2_info,
+        context=None,
+        children=0,
+        is_root=False,
+        parent=None,
+        url=test_page2,
+        user_context=user_context_id,
+    )
+    assert child2_info["context"] != root_info["context"]
+    assert child2_info["context"] != child1_info["context"]

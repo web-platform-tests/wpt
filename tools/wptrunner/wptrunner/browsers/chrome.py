76f6fcdf5a793c26f47741272c43625ae4e95de1
Weizhong Xia <weizhong@google.com>
2024-01-31 00:11:12 +0000

    swap the logic a little bit

--- tools/wptrunner/wptrunner/browsers/chrome.py
+++ tools/wptrunner/wptrunner/browsers/chrome.py
@@ -139,16 +139,16 @@ def executor_kwargs(logger, test_type, test_environment, run_info_data,
     if kwargs["enable_experimental"]:
         chrome_options["args"].extend(["--enable-experimental-web-platform-features"])
 
+    # Copy over any other flags that were passed in via `--binary-arg`
+    for arg in kwargs.get("binary_args", []):
+        if arg not in chrome_options["args"]:
+            chrome_options["args"].append(arg)
+
     # Pass the --headless=new flag to Chrome if WPT's own --headless flag was
     # set. '--headless' should always mean the new headless mode, as the old
     # headless mode is not used anyway.
     if kwargs["headless"] and "--headless=new" not in chrome_options["args"]:
         chrome_options["args"].append("--headless=new")
-
-    # Copy over any other flags that were passed in via `--binary-arg`
-    for arg in kwargs.get("binary_args", []):
-        if arg not in chrome_options["args"]:
-            chrome_options["args"].append(arg)
 
     if test_type == "wdspec":
         executor_kwargs["binary_args"] = chrome_options["args"]

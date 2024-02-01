2f1f4bb0a5df24add2cecab9ce03abfdced708f3
Rahim Abdi <rahim_abdi@apple.com>
2024-02-01 10:06:34 -0800

    Change tabbable/focusable to tab focusable/script focusable

--- common/keyboard-utils.js
+++ common/keyboard-utils.js
@@ -15,15 +15,15 @@ const keyboardUtils = {
   Ex: <div role="button"
         tabindex="0"
         data-testname="div with role button and tabindex=0 is focusable"
-        class="ex-focusable">
+        class="ex-script-focusable">
       </div>
 
-      keyboardUtils.verifyElementsAreFocusable(".ex-focusable")
+      keyboardUtils.verifyElementsAreScriptFocusable(".ex-script-focusable")
   */
-  verifyElementsAreFocusable: function(selector) {
+  verifyElementsAreScriptFocusable: function(selector) {
     const els = document.querySelectorAll(selector);
     if (!els.length) {
-      throw `Selector passed in verifyElementsAreFocusable("${selector}") should match at least one element.`;
+      throw `Selector passed in verifyElementsAreScriptFocusable("${selector}") should match at least one element.`;
     }
     for (const el of els) {
       let testName = el.getAttribute("data-testname");
@@ -61,40 +61,40 @@ const keyboardUtils = {
 
   /*
   Tests that all elements matching selector are
-  tabbable, i.e., present in the keyboard tab order.
+  tab focusable, i.e., present in the keyboard tab order.
 
   Ex: <button style="display: contents;"
         data-testname="button with display: contents is in keyboard tab order"
-        class="ex-tabbable">
+        class="ex-tab-focusable">
       </button>
 
-      keyboardUtils.verifyElementsAreTabbable(".ex-tabbable")
+      keyboardUtils.verifyElementsAreTabFocusable(".ex-tab-focusable")
   */
-  verifyElementsAreTabbable: function(selector) {
+  verifyElementsAreTabFocusable: function(selector) {
     const els = document.querySelectorAll(selector);
-    const focusablePreviousElement = document.createElement("a");
-    focusablePreviousElement.setAttribute("href", "#");
-    focusablePreviousElement.appendChild(document.createTextNode("a focusable link"));
+    const focusablePreviousLinkElement = document.createElement("a");
+    focusablePreviousLinkElement.setAttribute("href", "#");
+    focusablePreviousLinkElement.appendChild(document.createTextNode("a focusable link"));
     if (!els.length) {
-      throw `Selector passed in verifyElementsAreTabbable("${selector}") should match at least one element.`;
+      throw `Selector passed in verifyElementsAreTabFocusable("${selector}") should match at least one element.`;
     }
     for (const el of els) {
       let testName = el.getAttribute("data-testname");
       promise_test(async t => {
-        el.parentNode.insertBefore(focusablePreviousElement, el);
-        focusablePreviousElement.focus();
-        assert_equals(document.activeElement, focusablePreviousElement, "precondition: el's previous focusable element is currently focused");
+        el.parentNode.insertBefore(focusablePreviousLinkElement, el);
+        focusablePreviousLinkElement.focus();
+        assert_equals(document.activeElement, focusablePreviousLinkElement, "precondition: el's previous focusable element is currently focused");
         assert_not_equals(document.activeElement, el, "precondition: el is not focused");
-        await test_driver.send_keys(focusablePreviousElement, keys.Tab);
-        assert_equals(document.activeElement, el, "Element is tabbable");
-        document.body.removeChild(focusablePreviousElement);
+        await test_driver.send_keys(focusablePreviousLinkElement, keys.Tab);
+        assert_equals(document.activeElement, el, "Element is tab focusable");
+        document.body.removeChild(focusablePreviousLinkElement);
       }, `${testName}`);
     }
   },
 
   /*
   Tests that all elements matching selector are
-  tabbable and do not create a focus trap.
+  tab focusable and do not create a focus trap.
 
   Ex: <button style="display: flex;"
         data-testname="button with display: flex does not cause keyboard trap"
@@ -105,9 +105,9 @@ const keyboardUtils = {
   */
   verifyElementsDoNotCauseKeyboardTrap: function(selector) {
     const els = document.querySelectorAll(selector);
-    const focusablePreviousElement = document.createElement("a");
-    focusablePreviousElement.setAttribute("href", "#");
-    focusablePreviousElement.appendChild(document.createTextNode("a focusable link"));
+    const focusablePreviousLinkElement = document.createElement("a");
+    focusablePreviousLinkElement.setAttribute("href", "#");
+    focusablePreviousLinkElement.appendChild(document.createTextNode("a focusable link"));
     if (!els.length) {
       throw `Selector passed in verifyElementsDoNotCauseKeyboardTrap("${selector}") should match at least one element.`;
     }
@@ -116,13 +116,13 @@ const keyboardUtils = {
       promise_test(async t => {
         el.focus();
         assert_equals(document.activeElement, el, "precondition: el is currently focused");
-        el.parentNode.insertBefore(focusablePreviousElement, el);
+        el.parentNode.insertBefore(focusablePreviousLinkElement, el);
         await test_driver.send_keys(el, keys.LeftShiftAndTab);
-        assert_equals(document.activeElement, focusablePreviousElement, "precondition: el's previous focusable element is currently focused");
+        assert_equals(document.activeElement, focusablePreviousLinkElement, "precondition: el's previous focusable element is currently focused");
         assert_not_equals(document.activeElement, el, "precondition: el is not focused");
-        await test_driver.send_keys(focusablePreviousElement, keys.Tab);
+        await test_driver.send_keys(focusablePreviousLinkElement, keys.Tab);
         assert_equals(document.activeElement, el, "el has successfully lost and received focus, and is now focused");
-        document.body.removeChild(focusablePreviousElement);
+        document.body.removeChild(focusablePreviousLinkElement);
       }, `${testName}`);
     }
   },

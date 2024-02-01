18545d82299717beb0aad24c843f11a087860db8
Rahim Abdi <rahim_abdi@apple.com>
2024-01-31 20:23:11 -0800

    Add verifyElementsAreFocusable()

--- common/keyboard-accessibility-utils.js
+++ common/keyboard-accessibility-utils.js
@@ -1,5 +1,29 @@
-/* Utilities for testing keyboard-focused accessibility */
+/* Utilities for keyboard-focused accessibility */
 
-const keyboardAccessibilityUtils {
+const keyboardAccessibilityUtils = {
 
+  /*
+  Tests that all elements matching selector can
+  receive focus (and related) events.
+
+    Ex: <div role="button"
+            tabindex="0"
+            data-testname="div with role button and tabindex is focusable"
+            class="ex">
+
+        keyboardAccessibilityUtils.verifyElementsAreFocusable(".ex-focusable")
+  */
+  verifyElementsAreFocusable: function(selector) {
+    const els = document.querySelectorAll(selector);
+        if (!els.length) {
+            throw `Selector passed in verifyElementsAreFocusable("${selector}") should match at least one element.`;
+        }
+        for (const el of els) {
+            let testName = el.getAttribute("data-testname");
+            test(() => {
+                el.focus();
+                assert_equals(document.activeElement, el, "Element is focusable with element.focus()");
+              }, `${testName}`);
+        }
+    }
 };

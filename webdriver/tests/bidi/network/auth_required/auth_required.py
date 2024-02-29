ebdb5ef6d8de871fb89e5288eb53b865985e648d
Nikolay Vitkov <nvitkov@chromium.org>
2024-02-29 13:37:22 +0100

    [WebDriverBiDi] Remove uses of navigation with `wait=none`

--- webdriver/tests/bidi/network/auth_required/auth_required.py
+++ webdriver/tests/bidi/network/auth_required/auth_required.py
@@ -1,11 +1,13 @@
 import pytest
 
+import asyncio
+
 from .. import assert_response_event, AUTH_REQUIRED_EVENT, PAGE_EMPTY_HTML
 
 
 @pytest.mark.asyncio
 async def test_subscribe_status(
-    bidi_session, new_tab, subscribe_events, wait_for_event, wait_for_future_safe, url
+    bidi_session, new_tab, subscribe_events, wait_for_event, wait_for_future_safe, url, fetch
 ):
     await subscribe_events(events=[AUTH_REQUIRED_EVENT])
 
@@ -15,7 +17,8 @@ async def test_subscribe_status(
     async def on_event(method, data):
         events.append(data)
 
-    remove_listener = bidi_session.add_event_listener(AUTH_REQUIRED_EVENT, on_event)
+    remove_listener = bidi_session.add_event_listener(
+        AUTH_REQUIRED_EVENT, on_event)
 
     auth_url = url(
         "/webdriver/tests/support/http_handlers/authentication.py?realm=testrealm"
@@ -23,13 +26,7 @@ async def test_subscribe_status(
 
     on_auth_required = wait_for_event(AUTH_REQUIRED_EVENT)
 
-    # navigate using wait="none" as other wait conditions would hang because of
-    # the authentication prompt.
-    await bidi_session.browsing_context.navigate(
-        context=new_tab["context"],
-        url=auth_url,
-        wait="none",
-    )
+    asyncio.ensure_future(fetch(url=auth_url, context=new_tab))
 
     await wait_for_future_safe(on_auth_required)
 
@@ -63,7 +60,8 @@ async def test_no_authentication(
     async def on_event(method, data):
         events.append(data)
 
-    remove_listener = bidi_session.add_event_listener(AUTH_REQUIRED_EVENT, on_event)
+    remove_listener = bidi_session.add_event_listener(
+        AUTH_REQUIRED_EVENT, on_event)
 
     # Navigate to a page which should not trigger any authentication.
     await bidi_session.browsing_context.navigate(

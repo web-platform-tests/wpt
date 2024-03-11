1750e02e61645b85c1eabd37c82991d56c7ff65f
BruceDai <feng.dai@intel.com>
2024-03-11 16:05:55 +0800

    [webnn] Rebased code

--- webnn/conformance_tests/resample2d.https.any.js
+++ webnn/conformance_tests/resample2d.https.any.js
@@ -1,6 +1,6 @@
 // META: title=test WebNN API resample2d operation
 // META: global=window,dedicatedworker
-// META: script=./resources/utils.js
+// META: script=../resources/utils.js
 // META: timeout=long
 
 'use strict';

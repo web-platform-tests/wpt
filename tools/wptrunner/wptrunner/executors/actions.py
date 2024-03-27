web-platform-tests/wpt

commit f3d1eb9c1787cf2276ea867b7fda2622b9835c13

[FedCM] Change ConfirmIDPLogin to ClickFedCMDialogButton (#43002)
* [FedCM] Change ConfirmIDPLogin to ClickFedCMDialogButton

* Remove whitespace

tools/wptrunner/wptrunner/executors/actions.py
@@ -300,16 +300,17 @@ class CancelFedCMDialogAction:
         self.logger.debug("Canceling FedCM dialog")
         return self.protocol.fedcm.cancel_fedcm_dialog()
 
-class ConfirmIDPLoginAction:
-    name = "confirm_idp_login"
+class ClickFedCMDialogButtonAction:
+    name = "click_fedcm_dialog_button"
 
     def __init__(self, logger, protocol):
         self.logger = logger
         self.protocol = protocol
 
     def __call__(self, payload):
-        self.logger.debug("Confirming IDP login")
-        return self.protocol.fedcm.confirm_idp_login()
+        dialog_button = payload["dialog_button"]
+        self.logger.debug(f"Clicking FedCM dialog button: {dialog_button}")
+        return self.protocol.fedcm.click_fedcm_dialog_button()
 
 class SelectFedCMAccountAction:
     name = "select_fedcm_account"
@@ -456,7 +457,7 @@ actions = [ClickAction,
            SetSPCTransactionModeAction,
            SetRPHRegistrationModeAction,
            CancelFedCMDialogAction,
-           ConfirmIDPLoginAction,
+           ClickFedCMDialogButtonAction,
            SelectFedCMAccountAction,
            GetFedCMAccountListAction,
            GetFedCMDialogTitleAction,

tools/wptrunner/wptrunner/executors/executorwebdriver.py
@@ -379,6 +379,10 @@ class WebDriverFedCMProtocolPart(FedCMProtocolPart):
     def cancel_fedcm_dialog(self):
         return self.webdriver.send_session_command("POST", "fedcm/canceldialog")
 
+    def click_fedcm_dialog_button(self, dialog_button):
+        body = {"dialogButton": dialog_button}
+        return self.webdriver.send_session_command("POST", "fedcm/clickdialogbutton", body)
+
     def select_fedcm_account(self, account_index):
         body = {"accountIndex": account_index}
         return self.webdriver.send_session_command("POST", "fedcm/selectaccount", body)

tools/wptrunner/wptrunner/executors/protocol.py
@@ -641,8 +641,10 @@ class FedCMProtocolPart(ProtocolPart):
         pass
 
     @abstractmethod
-    def confirm_idp_login(self):
-        """Confirm IDP login"""
+    def click_fedcm_dialog_button(self, dialog_button):
+        """Click a button on the FedCM dialog
+
+        :param str dialog_button: The dialog button to click"""
         pass
 
     @abstractmethod

tools/wptrunner/wptrunner/testdriver-extra.js
@@ -277,8 +277,8 @@
         return create_action("cancel_fedcm_dialog", {context});
     };
 
-    window.test_driver_internal.confirm_idp_login = function(context = null) {
-        return create_action("confirm_idp_login", {context});
+    window.test_driver_internal.click_fedcm_dialog_button = function(dialog_button, context = null) {
+        return create_action("click_fedcm_dialog_button", {dialog_button, context});
     };
 
     window.test_driver_internal.select_fedcm_account = function(account_index, context = null) {

commit f82befd924e5f3bba3a41b9e9dbcbd880dd605b7

[FedCM] Pass the button to click_fedcm_dialog_button
This was caused by f3d1eb9c1787cf2276ea867b7fda2622b9835c13

Should fix this test:
https://wpt.fyi/results/credential-management/fedcm-login-status/confirm-idp-login.https.html?label=experimental&label=master

tools/wptrunner/wptrunner/executors/actions.py
@@ -319,7 +319,7 @@ class ClickFedCMDialogButtonAction:
     def __call__(self, payload):
         dialog_button = payload["dialog_button"]
         self.logger.debug(f"Clicking FedCM dialog button: {dialog_button}")
-        return self.protocol.fedcm.click_fedcm_dialog_button()
+        return self.protocol.fedcm.click_fedcm_dialog_button(dialog_button)
 
 class SelectFedCMAccountAction:
     name = "select_fedcm_account"
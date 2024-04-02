3afeb04de5a78ab97fb20db83f60b1e376a9fb8f
zcorpan <zcorpan@gmail.com>
2024-04-02 14:10:37 +0200

    Test e.inputType

--- html/editing/dnd/drop/support/events.js
+++ html/editing/dnd/drop/support/events.js
@@ -7,16 +7,16 @@ function rAF() {
 const a = document.getElementById('a');
 const b = document.getElementById('b');
 const actualEvents = [];
-const expectedEvents = document.body.dataset.expectedEvents.split(',');
+const expectedEvents = document.body.dataset.expectedEvents.replace(/\s+/g, '').split(',');
 const eventTypes = new Set(expectedEvents.map(s => s.split(':')[1]));
 for (const eventType of eventTypes) {
   if (a) {
     a.addEventListener(eventType, e => {
-      actualEvents.push(`a:${e.type}`);
+      actualEvents.push(`a:${e.type}:${e.inputType || ''}`);
     });
   }
   b.addEventListener(eventType, async (e) => {
-    actualEvents.push(`b:${e.type}`);
+    actualEvents.push(`b:${e.type}:${e.inputType || ''}`);
     if (e.type === "input") {
       await rAF();
       await rAF();

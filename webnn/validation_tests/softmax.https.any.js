520e0264b5ca4fb398d6cb19f2d7d54cda09429a
mei1127 <shanxing.mei@intel.com>
2024-04-26 01:54:39 -0700

    webnn: Migrate Softmax validation tests to WPTs
    This CL adds WPT tests for Softmax and removes the unit tests
    `MLGraphBuilderTest.SoftmaxTest`, `MLGraphTestMojo.SoftmaxTest` and
    `MLGraphTest.SoftmaxTest`.
    
    Bug: 327337526, 328026885
    Change-Id: I4a630194127eb0a065cb819f3ed53cd99e2095aa

--- webnn/validation_tests/softmax.https.any.js
+++ webnn/validation_tests/softmax.https.any.js
@@ -5,3 +5,42 @@
 'use strict';
 
 validateInputFromAnotherBuilder('softmax');
+
+
+const tests = [
+  {
+    name: '[softmax] Test with float32 input.',
+    input: {dataType: 'float32', dimensions: [2, 4]},
+    axis:0,
+    output: {dataType: 'float32', dimensions: [2, 4]}
+  },
+  {
+    name: '[softmax] Throw if building softmax with 4-D input.',
+    input: {dataType: 'float32', dimensions: [1, 1, 2, 4]},
+    axis:0,
+  },
+  {
+    name: '[softmax] Throw if building softmax with int32 input.',
+    input: {dataType: 'int32', dimensions: [3, 4]},
+    axis:0,
+  },
+  {
+    name: '[softmax] Throw if axis is greater than or equal to input\'s rank.',
+    input: {dataType: 'float32', dimensions: [3, 4]},
+    axis: 3,
+  },
+];
+
+tests.forEach(
+    test => promise_test(async t => {
+      const input = builder.input(
+          'input',
+          {dataType: test.input.dataType, dimensions: test.input.dimensions});
+      if (test.output) {
+        const output = builder.softmax(input, test, axis);
+        assert_equals(output.dataType(), test.output.dataType);
+        assert_array_equals(output.shape(), test.output.dimensions);
+      } else {
+        assert_throws_js(TypeError, () => builder.softmax(input, test.axis));
+      }
+    }, test.name));

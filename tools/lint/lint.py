940536a0933f83021255f39d0a855c12838c384b
Sam Sneddon <gsnedders@apple.com>
2024-05-06 10:40:58 -0700

    Add TESTDRIVER-VENDOR-WITHOUT-TESTDRIVER lint
    This checks that testdriver-vendor.js only occurs when testdriver.js
    does, as vendor implementations can have unexpected side effects.

--- tools/lint/lint.py
+++ tools/lint/lint.py
@@ -16,11 +16,6 @@ from typing import (Any, Callable, Dict, IO, Iterable, List, Optional, Sequence,
 
 from urllib.parse import urlsplit, urljoin, parse_qs
 
-try:
-    from xml.etree import cElementTree as ElementTree
-except ImportError:
-    from xml.etree import ElementTree as ElementTree  # type: ignore
-
 from . import fnmatch
 from . import rules
 from .. import localpaths
@@ -483,7 +478,10 @@ def check_parsed(repo_root: Text, path: Text, f: IO[bytes]) -> List[rules.Error]
         if len(testharnessreport_nodes) > 1:
             errors.append(rules.MultipleTestharnessReport.error(path))
 
-    testdriver_vendor_nodes: List[ElementTree.Element] = []
+    testdriver_vendor_nodes = source_file.root.findall(
+        ".//{http://www.w3.org/1999/xhtml}script[@src='/resources/testdriver-vendor.js']"
+    )
+
     if source_file.testdriver_nodes:
         if test_type not in {"testharness", "reftest", "print-reftest", "crashtest", "support"}:
             errors.append(rules.TestdriverInUnsupportedType.error(path, (test_type,)))
@@ -491,16 +489,19 @@ def check_parsed(repo_root: Text, path: Text, f: IO[bytes]) -> List[rules.Error]
         if len(source_file.testdriver_nodes) > 1:
             errors.append(rules.MultipleTestdriver.error(path))
 
-        testdriver_vendor_nodes = source_file.root.findall(".//{http://www.w3.org/1999/xhtml}script[@src='/resources/testdriver-vendor.js']")
         if not testdriver_vendor_nodes:
             errors.append(rules.MissingTestdriverVendor.error(path))
-        else:
-            if len(testdriver_vendor_nodes) > 1:
-                errors.append(rules.MultipleTestdriverVendor.error(path))
 
         required_elements.append("testdriver")
         if len(testdriver_vendor_nodes) > 0:
             required_elements.append("testdriver-vendor")
+
+    if testdriver_vendor_nodes:
+        if not source_file.testdriver_nodes:
+            errors.append(rules.TestdriverVendorWithoutTestdriver.error(path))
+
+        if len(testdriver_vendor_nodes) > 1:
+            errors.append(rules.MultipleTestdriverVendor.error(path))
 
     if required_elements:
         seen_elements = defaultdict(bool)

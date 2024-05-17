d9dc8138f4b9843647ea26282b2cedc1e0db5a98
Alice Boxhall <95208+alice@users.noreply.github.com>
2024-05-17 17:36:30 +1000

    Very minimal first pass

--- tools/wptrunner/wptrunner/executors/executoracacia.py
+++ tools/wptrunner/wptrunner/executors/executoracacia.py
@@ -1,117 +1,27 @@
-import gi
-gi.require_version("Atspi", "2.0")
-from gi.repository import Atspi
-import json
-import threading
 from .protocol import (PlatformAccessibilityProtocolPart)
 
-
-import traceback
-import time
-
-
-def find_active_tab(root):
-    stack = [root]
-    while stack:
-        node = stack.pop()
-
-        if Atspi.Accessible.get_role_name(node) == 'frame':
-            ## Helper: list of string relations, get targets for relation?
-            relationset = Atspi.Accessible.get_relation_set(node)
-            for relation in relationset:
-              if relation.get_relation_type() == Atspi.RelationType.EMBEDS:
-                  return relation.get_target(0)
-            contiue
-
-        for i in range(Atspi.Accessible.get_child_count(node)):
-            child = Atspi.Accessible.get_child_at_index(node, i)
-            stack.append(child)
-
-    return None
-
-def serialize_node(node):
-    node_dictionary = {}
-    node_dictionary['role'] = Atspi.Accessible.get_role_name(node)
-    node_dictionary['name'] = Atspi.Accessible.get_name(node)
-    node_dictionary['description'] = Atspi.Accessible.get_description(node)
-
-    # TODO: serialize other attributes
-    # states, interfaces, attributes, etc.
-
-    return node_dictionary
-
-def find_node(root, dom_id):
-    stack = [root]
-    while stack:
-        node = stack.pop()
-
-        attributes = Atspi.Accessible.get_attributes(node)
-        if 'id' in attributes and attributes['id'] == dom_id:
-            return node
-
-        for i in range(Atspi.Accessible.get_child_count(node)):
-            child = Atspi.Accessible.get_child_at_index(node, i)
-            stack.append(child)
-
-    return None
-
-def find_browser(name):
-    desktop = Atspi.get_desktop(0)
-    child_count = Atspi.Accessible.get_child_count(desktop)
-    for i in range(child_count):
-        app = Atspi.Accessible.get_child_at_index(desktop, i)
-        full_app_name = Atspi.Accessible.get_name(app)
-        if name in full_app_name.lower():
-            return (app, full_app_name)
-    return
+from sys import platform
+linux = False
+mac = False
+if platform == "linux":
+  linux = True
+  from .executoratspi import *
+if platform == "darwin":
+  mac = True
+  from .executoraxapi import *
 
 
 class AcaciaPlatformAccessibilityProtocolPart(PlatformAccessibilityProtocolPart):
-    def start_atspi_listener(self):
-        self._event_listener = Atspi.EventListener.new(self.handle_event)
-        self._event_listener.register("document:load-complete")
-        Atspi.event_main()
-
-
-    def handle_event(self, e):
-        app = Atspi.Accessible.get_application(e.source)
-        app_name = Atspi.Accessible.get_name(app)
-        if (self.full_app_name == app_name and e.any_data):
-            self.load_complete = True
-            self._event_listener.deregister("document:load-complete")
-            Atspi.event_quit()
-
     def setup(self):
         self.product_name = self.parent.product_name
-        self.full_app_name = ''
-        self.root = None
-        self.found_browser = False
-        self.load_complete = False
+        self.impl = None
+        if linux:
+            self.impl = AtspiExecutorImpl()
+            self.impl.setup(self.product_name)
+        if mac:
+            self.impl = AXAPIExecutorImpl()
+            self.impl.setup(self.product_name)
 
-        self.atspi_listener_thread = threading.Thread(target=self.start_atspi_listener)
-
-        (self.root, self.full_app_name) = find_browser(self.product_name);
-        if self.root:
-            self.found_browser = True
-            self.atspi_listener_thread.start()
-        else:
-            print(f"Cannot find root accessibility node for {self.product_name} - did you turn on accessibility?")
 
     def get_accessibility_api_node(self, dom_id):
-        if not self.load_complete:
-          self.atspi_listener_thread.join()
-
-        if not self.found_browser:
-            return json.dumps({"role": "couldn't find browser"})
-
-        active_tab = find_active_tab(self.root)
-        if not active_tab:
-            return json.dumps({"role": "couldn't find active tab"})
-
-        node = find_node(active_tab, dom_id)
-        if not node:
-            return json.dumps({"role": "couldn't find the node with that ID"})
-
-        return json.dumps(serialize_node(node))
-
-
+      return self.impl.get_accessibility_api_node(dom_id)

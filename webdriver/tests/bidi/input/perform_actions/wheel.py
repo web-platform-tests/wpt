01fd4c21f7c9fbf0c361ba61acecb3e27dc687ad
Maksim Sadym <69349599+sadym-chromium@users.noreply.github.com>
2024-05-17 09:59:42 +0200

    Update webdriver/tests/bidi/input/perform_actions/wheel.py
    Co-authored-by: Henrik Skupin <mail@hskupin.info>

--- webdriver/tests/bidi/input/perform_actions/wheel.py
+++ webdriver/tests/bidi/input/perform_actions/wheel.py
@@ -91,7 +91,7 @@ async def test_scroll_iframe(
     async def wait_for_events(_):
         return len(await get_events(bidi_session, top_context["context"])) > 0
 
-    await AsyncPoll(bidi_session, timeout=0.5, interval=0.01, message='No wheel events found').until(wait_for_events)
+    await AsyncPoll(bidi_session, timeout=0.5, interval=0.01, message='No wheel events emitted').until(wait_for_events)
     events = await get_events(bidi_session, top_context["context"])
 
     assert len(events) == 1

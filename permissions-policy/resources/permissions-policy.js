1e25f91ce6c88fee978b5556da95db825c463525
Marcos Cáceres <caceres_m@apple.com>
2024-05-27 14:12:17 +1000

    Update permissions-policy/resources/permissions-policy.js

--- permissions-policy/resources/permissions-policy.js
+++ permissions-policy/resources/permissions-policy.js
@@ -2,6 +2,8 @@
 function assert_permissions_policy_supported() {
   assert_not_equals(document.permissionsPolicy, undefined,
                     'permissions policy is supported');
+  assert_equals(document.featurePolicy, undefined,
+                    'permissions policy is supported');
 }
 // Tests whether a feature that is enabled/disabled by permissions policy works
 // as expected.

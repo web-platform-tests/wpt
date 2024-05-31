web-platform-tests/wpt

commit c5b8a0ad47e54864033f2d938e6a816dde1aa0dc

fix(testharness): fix typo and explanation (#46487)

resources/testharness.js
@@ -857,7 +857,7 @@
             promise = promiseOrConstructor;
             description = descriptionOrPromise;
             assert(maybeDescription === undefined,
-                   "Too many args pased to no-constructor version of promise_rejects_dom");
+                   "Too many args passed to no-constructor version of promise_rejects_dom, or accidentally explicitly passed undefined");
         }
         return bring_promise_to_current_realm(promise)
             .then(test.unreached_func("Should have rejected: " + description))
@@ -2174,7 +2174,7 @@
             func = funcOrConstructor;
             description = descriptionOrFunc;
             assert(maybeDescription === undefined,
-                   "Too many args pased to no-constructor version of assert_throws_dom");
+                   "Too many args passed to no-constructor version of promise_rejects_dom, or accidentally explicitly passed undefined");
         }
         assert_throws_dom_impl(type, func, description, "assert_throws_dom", constructor)
     }

commit 04acfcf57a1ddc332076034588063d7b070539a5

fix(testharness): copy-pasta in assert_throws_dom
This regressed in #46487

resources/testharness.js
@@ -2174,7 +2174,7 @@
             func = funcOrConstructor;
             description = descriptionOrFunc;
             assert(maybeDescription === undefined,
-                   "Too many args passed to no-constructor version of promise_rejects_dom, or accidentally explicitly passed undefined");
+                   "Too many args passed to no-constructor version of assert_throws_dom, or accidentally explicitly passed undefined");
         }
         assert_throws_dom_impl(type, func, description, "assert_throws_dom", constructor)
     }
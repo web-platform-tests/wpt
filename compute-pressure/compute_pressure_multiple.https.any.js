83087999c5dd0d6f9b51e3c3904fe602df3ac9a3
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2024-06-10 06:46:08 -0700

    compute pressure: Track client state in PressureObserverManager, rework PressureClientImpl::Reset()
    This solves two issues:
    
    1. A design issue. PressureClientImpl::State represented a concept that
       only made sense in PressureObserverManager's context, so it should be
       tracked there.
       The different states concern whether WebPressureManager.AddClient()
       has been called for a given source type and whether it has finished
       running.
    
    2. A bug. PressureClientImpl::Reset() was too eager and cleared too many
       variables:
       * Clearing |observers_| means PressureObserver instances would be
         left dangling and never receive any updates even if the given
         PressureClientImpl ended up connecting to the browser process.
       * Resetting |state_| caused PressureObserverManager::DidAddClient()
         to exit early in case the AddClient() handler in the browser
         process reset or dropped the pending PressureClient remote before
         sending back a PressureStatus.
         In this case, existing PressureObserver instances would never be
         notified and any pending promises would never be fulfilled or
         rejected.
    
    Change-Id: Ia671bdbeb8067cb518111810a3281eaab1abdd1d

--- compute-pressure/compute_pressure_multiple.https.any.js
+++ compute-pressure/compute_pressure_multiple.https.any.js
@@ -33,3 +33,47 @@ pressure_test(async (t, mockPressureService) => {
     assert_equals(changes[0].state, 'critical');
   }
 }, 'Three PressureObserver instances receive changes');
+
+pressure_test(async (t, mockPressureService) => {
+  await new Promise(async (resolve) => {
+    const observer1 = new PressureObserver(() => {
+      assert_unreached("The observer callback should not be called");
+    });
+    const observer2 = new PressureObserver(resolve);
+    t.add_cleanup(() => {
+      observer1.disconnect();
+      observer2.disconnect();
+    });
+
+    const promise1 = observer1.observe("cpu");
+    const promise2 = observer2.observe("cpu");
+    observer1.disconnect();
+    await Promise.all([
+      promise_rejects_dom(t, "AbortError", promise1),
+      promise2,
+    ]);
+
+    mockPressureService.setPressureUpdate("cpu", "critical");
+    mockPressureService.startPlatformCollector(/*sampleInterval=*/ 200);
+  });
+}, "Disconnecting an observer does not affect one that is still connecting");
+
+pressure_test(async (t, mockPressureService) => {
+  mockPressureService.setExpectedFailure(
+    new DOMException("", "NotSupportedError")
+  );
+
+  const observer1 = new PressureObserver(() => {
+    assert_unreached("The observer1 callback should not be called");
+  });
+  const observer2 = new PressureObserver(() => {
+    assert_unreached("The observer2 callback should not be called");
+  });
+  t.add_cleanup(() => {
+    observer1.disconnect();
+    observer2.disconnect();
+  });
+
+  await promise_rejects_dom(t, "NotSupportedError", observer1.observe("cpu"));
+  await promise_rejects_dom(t, "NotSupportedError", observer2.observe("cpu"));
+}, "Multiple observers reject with NotSupportedError");

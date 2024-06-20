12f23152d5c280a1574cec97ef6512f0c6baee4c
Nikolay Vitkov <nvitkov@chromium.org>
2024-06-20 16:41:15 +0200

    chore: fix

--- webdriver/tests/bidi/browsing_context/close/prompt_unload.py
+++ webdriver/tests/bidi/browsing_context/close/prompt_unload.py
@@ -11,13 +11,10 @@ USER_PROMPT_OPENED_EVENT = "browsingContext.userPromptOpened"
 
 @pytest.mark.parametrize("type_hint", ["window", "tab"])
 @pytest.mark.parametrize("prompt_unload", [None, False])
-async def test_prompt_unload_not_triggering_dialog(bidi_session, subscribe_events, url, wait_for_event,
-                                                   wait_for_future_safe, type_hint, prompt_unload):
+async def test_prompt_unload_not_triggering_dialog(bidi_session, subscribe_events, setup_beforeunload_page,
+                                                   wait_for_event, wait_for_future_safe, type_hint, prompt_unload):
 
     new_context = await bidi_session.browsing_context.create(type_hint=type_hint)
-
-    page_beforeunload = url(
-        "/webdriver/tests/support/html/beforeunload.html")
 
     # Set up event listener to make sure the "beforeunload" event is not emitted
     await subscribe_events([USER_PROMPT_OPENED_EVENT, CONTEXT_DESTROYED_EVENT])
@@ -31,15 +28,7 @@ async def test_prompt_unload_not_triggering_dialog(bidi_session, subscribe_event
     remove_listener = bidi_session.add_event_listener(
         USER_PROMPT_OPENED_EVENT, on_event)
 
-    await bidi_session.browsing_context.navigate(context=new_context["context"], url=page_beforeunload, wait="complete")
-
-    # We need to interact with the page to trigger the beforeunload event.
-    # https://developer.mozilla.org/en-US/docs/Web/API/Window/beforeunload_event#usage_notes
-    await bidi_session.script.evaluate(
-        expression="document.querySelector('input').click()",
-        target=ContextTarget(new_context["context"]),
-        await_promise=True,
-        user_activation=True)
+    await setup_beforeunload_page(new_context)
 
     on_context_destroyed = wait_for_event(CONTEXT_DESTROYED_EVENT)
 
@@ -53,9 +42,7 @@ async def test_prompt_unload_not_triggering_dialog(bidi_session, subscribe_event
 
 
 @pytest.mark.parametrize("type_hint", ["window", "tab"])
-async def test_prompt_unload_triggering_dialog(bidi_session, url, subscribe_events, wait_for_event, wait_for_future_safe, type_hint):
-    page_beforeunload = url(
-        "/webdriver/tests/support/html/beforeunload.html")
+async def test_prompt_unload_triggering_dialog(bidi_session, setup_beforeunload_page, subscribe_events, wait_for_event, wait_for_future_safe, type_hint):
 
     new_context = await bidi_session.browsing_context.create(type_hint=type_hint)
 
@@ -72,15 +59,7 @@ async def test_prompt_unload_triggering_dialog(bidi_session, url, subscribe_even
     remove_listener = bidi_session.add_event_listener(
         CONTEXT_DESTROYED_EVENT, on_event)
 
-    await bidi_session.browsing_context.navigate(context=new_context["context"], url=page_beforeunload, wait="complete")
-
-    # We need to interact with the page to trigger the beforeunload event.
-    # https://developer.mozilla.org/en-US/docs/Web/API/Window/beforeunload_event#usage_notes
-    await bidi_session.script.evaluate(
-        expression="document.body.click()",
-        target=ContextTarget(new_context["context"]),
-        await_promise=True,
-        user_activation=True)
+    await setup_beforeunload_page(new_context)
 
     close_task = asyncio.create_task(
         bidi_session.browsing_context.close(

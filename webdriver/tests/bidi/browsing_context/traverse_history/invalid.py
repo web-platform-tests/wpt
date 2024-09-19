19e28c48cfd4d67a44324955a9f188e1c64cd2e8
Maksim Sadym <sadym@chromium.org>
2024-09-19 14:19:02 +0200

    remove param

--- webdriver/tests/bidi/browsing_context/traverse_history/invalid.py
+++ webdriver/tests/bidi/browsing_context/traverse_history/invalid.py
@@ -44,12 +44,9 @@ async def test_delta_invalid_value(bidi_session, current_url, new_tab, inline, v
         )
 
 
-@pytest.mark.parametrize("domain", ["", "alt"],
-                         ids=["same_origin", "cross_origin"])
-async def test_iframe(bidi_session, current_url, wait_for_url, new_tab, inline,
-      domain):
+async def test_iframe(bidi_session, current_url, wait_for_url, new_tab, inline):
     iframe_url_1 = inline("page 1")
-    page_url = inline(f"<iframe src='{iframe_url_1}'></iframe>", domain=domain)
+    page_url = inline(f"<iframe src='{iframe_url_1}'></iframe>")
 
     await bidi_session.browsing_context.navigate(
         context=new_tab["context"], url=page_url, wait="complete"

abc74980d48d0ce09ab0d1cffa94bfd8ede91216
Alex Rudenko <alexrudenko@chromium.org>
2024-09-26 10:51:04 +0200

    [wdspec] Add historyUpdated event tests

---
+++ webdriver/tests/bidi/browsing_context/history_updated/history_updated.py
@@ -0,0 +1,265 @@
+import pytest
+
+from webdriver.bidi.modules.script import ContextTarget
+from webdriver.error import TimeoutException
+from tests.support.sync import AsyncPoll
+
+from ... import recursive_compare
+
+pytestmark = pytest.mark.asyncio
+
+EMPTY_PAGE = "/webdriver/tests/bidi/browsing_context/support/empty.html"
+FRAGMENT_NAVIGATED_EVENT = "browsingContext.fragmentNavigated"
+HISTORY_UPDATED_EVENT = "browsingContext.historyUpdated"
+CREATED_EVENT = "browsingContext.contextCreated"
+
+@pytest.mark.parametrize(
+    "hash_before, hash_after, history_method",
+    [
+        ("", "#foo", "pushState"),
+        ("#foo", "#bar", "pushState"),
+        ("#foo", "#foo", "pushState"),
+        ("#bar", "", "pushState"),
+        ("", "#foo", "replaceState"),
+        ("#foo", "#bar", "replaceState"),
+        ("#foo", "#foo", "replaceState"),
+        ("#bar", "", "replaceState"),
+    ]
+)
+async def test_history_url_update(
+    bidi_session, new_tab, url, subscribe_events, hash_before, hash_after, history_method
+):
+    target_context = new_tab["context"]
+
+    await bidi_session.browsing_context.navigate(
+        context=new_tab["context"], url=url(EMPTY_PAGE + hash_before), wait="complete"
+    )
+
+    await subscribe_events([FRAGMENT_NAVIGATED_EVENT, HISTORY_UPDATED_EVENT])
+
+    fragment_navigated_events = []
+    history_updated_events = []
+    async def on_event(method, data):
+        if method == FRAGMENT_NAVIGATED_EVENT:
+          fragment_navigated_events.append(data)
+        if method == HISTORY_UPDATED_EVENT:
+           history_updated_events.append(data)
+
+    remove_fragment_navigated_listener = bidi_session.add_event_listener(FRAGMENT_NAVIGATED_EVENT, on_event)
+    remove_history_updated_listener = bidi_session.add_event_listener(HISTORY_UPDATED_EVENT, on_event)
+
+    try:
+      target_url = url(EMPTY_PAGE + hash_after)
+
+      await bidi_session.script.call_function(
+          raw_result=True,
+          function_declaration="""(method, url) => {
+              history[method](null, null, url);
+          }""",
+          arguments=[
+              {"type": "string", "value": history_method},
+              {"type": "string", "value": target_url},
+          ],
+          await_promise=False,
+          target=ContextTarget(target_context),
+      )
+
+      recursive_compare(
+          [{
+              'context': target_context,
+              'url': target_url
+          }],
+          history_updated_events
+      )
+
+      assert len(fragment_navigated_events) == 0
+    finally:
+       remove_fragment_navigated_listener()
+       remove_history_updated_listener()
+
+
+@pytest.mark.parametrize(
+    "history_method",
+    [
+        ("pushState"),
+        ("replaceState"),
+    ]
+)
+async def test_history_state_update(
+    bidi_session, new_tab, url, subscribe_events, history_method
+):
+    target_context = new_tab["context"]
+
+    target_url = url(EMPTY_PAGE)
+    await bidi_session.browsing_context.navigate(
+        context=new_tab["context"], url=target_url, wait="complete"
+    )
+
+    await subscribe_events([FRAGMENT_NAVIGATED_EVENT, HISTORY_UPDATED_EVENT])
+
+    fragment_navigated_events = []
+    history_updated_events = []
+    async def on_event(method, data):
+        if method == FRAGMENT_NAVIGATED_EVENT:
+          fragment_navigated_events.append(data)
+        if method == HISTORY_UPDATED_EVENT:
+           history_updated_events.append(data)
+
+    remove_fragment_navigated_listener = bidi_session.add_event_listener(FRAGMENT_NAVIGATED_EVENT, on_event)
+    remove_history_updated_listener = bidi_session.add_event_listener(HISTORY_UPDATED_EVENT, on_event)
+
+    try:
+      await bidi_session.script.call_function(
+          raw_result=True,
+          function_declaration="""(method) => {
+              history[method]({}, null);
+          }""",
+          arguments=[
+              {"type": "string", "value": history_method},
+          ],
+          await_promise=False,
+          target=ContextTarget(target_context),
+      )
+
+      recursive_compare(
+          [{
+              'context': target_context,
+              'url': target_url
+          }],
+          history_updated_events
+      )
+
+      assert len(fragment_navigated_events) == 0
+    finally:
+       remove_fragment_navigated_listener()
+       remove_history_updated_listener()
+
+
+async def test_history_about_blank_iframe_insertion(
+    bidi_session, new_tab, url, subscribe_events
+):
+    target_context = new_tab["context"]
+
+    target_url = url(EMPTY_PAGE)
+    await bidi_session.browsing_context.navigate(
+        context=new_tab["context"], url=target_url, wait="complete"
+    )
+
+    await subscribe_events([FRAGMENT_NAVIGATED_EVENT, HISTORY_UPDATED_EVENT, CREATED_EVENT])
+
+    fragment_navigated_events = []
+    history_updated_events = []
+    browsing_context_created_events = []
+
+
+    async def on_event(method, data):
+        if method == FRAGMENT_NAVIGATED_EVENT:
+          fragment_navigated_events.append(data)
+        if method == HISTORY_UPDATED_EVENT:
+           history_updated_events.append(data)
+        if method == CREATED_EVENT:
+           browsing_context_created_events.append(data)
+
+
+    remove_fragment_navigated_listener = bidi_session.add_event_listener(FRAGMENT_NAVIGATED_EVENT, on_event)
+    remove_history_updated_listener = bidi_session.add_event_listener(HISTORY_UPDATED_EVENT, on_event)
+    remove_created_listener = bidi_session.add_event_listener(CREATED_EVENT, on_event)
+
+    try:
+      await bidi_session.script.call_function(
+          raw_result=True,
+          function_declaration="""() => {
+              const iframe = document.createElement("iframe");
+              const { promise, resolve, reject } = Promise.withResolvers();
+              iframe.src = "about:blank?foo";
+              iframe.onload = () => resolve();
+              document.body.append(iframe);
+              return promise;
+          }""",
+          await_promise=True,
+          target=ContextTarget(target_context),
+      )
+
+      recursive_compare(
+          [{
+              'url': 'about:blank'
+          }],
+          browsing_context_created_events
+      )
+
+      # Not implemented yet.
+      # recursive_compare(
+      #     [{
+      #         'url': 'about:blank?foo'
+      #     }],
+      #     history_updated_events
+      # )
+
+      assert len(fragment_navigated_events) == 0
+    finally:
+       remove_fragment_navigated_listener()
+       remove_history_updated_listener()
+       remove_created_listener()
+
+
+async def test_history_iframe_insertion(
+    bidi_session, new_tab, url, subscribe_events
+):
+    target_context = new_tab["context"]
+
+    target_url = url(EMPTY_PAGE)
+    await bidi_session.browsing_context.navigate(
+        context=new_tab["context"], url=target_url, wait="complete"
+    )
+
+    await subscribe_events([FRAGMENT_NAVIGATED_EVENT, HISTORY_UPDATED_EVENT, CREATED_EVENT])
+
+    fragment_navigated_events = []
+    history_updated_events = []
+    browsing_context_created_events = []
+
+
+    async def on_event(method, data):
+        if method == FRAGMENT_NAVIGATED_EVENT:
+          fragment_navigated_events.append(data)
+        if method == HISTORY_UPDATED_EVENT:
+           history_updated_events.append(data)
+        if method == CREATED_EVENT:
+           browsing_context_created_events.append(data)
+
+
+    remove_fragment_navigated_listener = bidi_session.add_event_listener(FRAGMENT_NAVIGATED_EVENT, on_event)
+    remove_history_updated_listener = bidi_session.add_event_listener(HISTORY_UPDATED_EVENT, on_event)
+    remove_created_listener = bidi_session.add_event_listener(CREATED_EVENT, on_event)
+
+    try:
+      await bidi_session.script.call_function(
+          raw_result=True,
+          function_declaration="""(url) => {
+              const iframe = document.createElement("iframe");
+              const { promise, resolve, reject } = Promise.withResolvers();
+              iframe.src = url;
+              iframe.onload = () => resolve();
+              document.body.append(iframe);
+              return promise;
+          }""",
+          arguments=[
+              {"type": "string", "value": target_url},
+          ],
+          await_promise=True,
+          target=ContextTarget(target_context),
+      )
+
+      recursive_compare(
+          [{
+              'url': 'about:blank'
+          }],
+          browsing_context_created_events
+      )
+
+      assert len(history_updated_events) == 0
+      assert len(fragment_navigated_events) == 0
+    finally:
+       remove_fragment_navigated_listener()
+       remove_history_updated_listener()
+       remove_created_listener()

4db7dc692be3aca644252623d69d5db5113541f7
Mirko Brodesser <mbrodesser@igalia.com>
2024-10-14 08:56:35 +0000

    part 6) Add javascript-url navigation via form-submission tests for Trusted Types.
    Differential Revision: https://phabricator.services.mozilla.com/D223596
    
    bugzilla-url: https://bugzilla.mozilla.org/show_bug.cgi?id=1921008
    gecko-commit: 999a70cf9b2dcc6fc81fc8fbe9f7a811f6056db5
    gecko-reviewers: tschuster

--- trusted-types/support/navigation-support.js
+++ trusted-types/support/navigation-support.js
@@ -1,20 +1,24 @@
 const kNavigationAttempted = "navigationattempted=1";
 
 function navigateToJavascriptURL(reportOnly) {
-    const params = new URLSearchParams(location.search);
+    let params = new URLSearchParams(location.search);
+
     if (!!params.get("defaultpolicy")) {
-      trustedTypes.createPolicy("default", {
-          createScript: s => s.replace("continue", "defaultpolicywashere"),
-      });
+        trustedTypes.createPolicy("default", {
+            createScript: s => {
+                return s.replace("continue", "defaultpolicywashere")
+            },
+        });
     }
 
     function bounceEventToOpener(e) {
-      const msg = {};
-      for (const field of ["effectiveDirective", "sample", "type"]) {
-        msg[field] = e[field];
-      }
-      msg["uri"] = location.href;
-      window.opener.postMessage(msg, "*");
+        const msg = {};
+        for (const field of ["effectiveDirective", "sample", "type"]) {
+            msg[field] = e[field];
+        }
+
+        msg["uri"] = location.href;
+        window.opener.postMessage(msg, "*");
     }
 
     // If a navigation is blocked by Trusted Types, we expect this window to
@@ -29,27 +33,43 @@ function navigateToJavascriptURL(reportOnly) {
         // Navigate to the non-report-only version of the test. That has the same
         // event listening setup as this, but is a different target URI.
         target_script = `location.href='${location.href.replace("-report-only", "") +
-            (location.href.includes("?") ? "&" : "?") + kNavigationAttempted + "&continue"}';`;
+            (location.href.endsWith(".html") ? "?" : "&") + kNavigationAttempted + "&continue"}';`;
     } else {
         // We'll use a javascript:-url to navigate to ourselves, so that we can
         // re-use the messageing mechanisms above.
         target_script = `location.href='${location.href + "&" + kNavigationAttempted}&continue';`;
     }
-    const target = `javascript:${target_script}`;
 
-    const anchor = document.getElementById("anchor");
-    anchor.href = target;
+    function getAndPreparareNavigationElement(javaScriptURL) {
+        let target = "_self";
+        if (!!params.get("frame")) {
+            const frame = document.createElement("iframe");
+            frame.src = "frame-without-trusted-types.html";
+            frames.name = "frame";
+            document.body.appendChild(frame);
+            target = "frame";
+        }
 
-    if (!!params.get("frame")) {
-      const frame = document.createElement("iframe");
-      frame.src = "frame-without-trusted-types.html";
-      frames.name = "frame";
-      document.body.appendChild(frame);
-      anchor.target = "frame";
+        if (!!params.get("form-submission")) {
+            const submit = document.getElementById("submit");
+
+            // Careful, the IDL attributes are defined in camel-case.
+            submit.formAction = javaScriptURL;
+            submit.formTarget = target;
+
+            return submit;
+        }
+
+        const anchor = document.getElementById("anchor");
+        anchor.href = javaScriptURL;
+        anchor.target = target;
+        return anchor;
     }
+
+    const navigationElement = getAndPreparareNavigationElement(`javascript:${target_script}`);
 
     // Prevent loops.
     if (!location.search.includes(kNavigationAttempted)) {
-      document.addEventListener("DOMContentLoaded", _ => anchor.click());
+       document.addEventListener("DOMContentLoaded", _ => navigationElement.click());
     }
 }

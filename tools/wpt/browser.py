31106753744fa1c2f1d15e6104a6483f95b6a3c5
Sam Sneddon <gsnedders@apple.com>
2024-11-03 00:44:24 -0700

    fixup! re-vendor html5lib

--- tools/wpt/browser.py
+++ tools/wpt/browser.py
@@ -15,7 +15,7 @@ from urllib.parse import urlsplit, quote
 import requests
 from packaging.specifiers import SpecifierSet
 
-import third_party import html5lib
+from third_party import html5lib
 
 from .utils import (
     call,

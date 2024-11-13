7f30a91b394cd9ec15286a06cfdab6ffe0764891
lionel-rowe <lionel.rowe@gmail.com>
2024-11-13 16:09:16 +0800

    Remove condition

--- webidl/ecmascript-binding/es-exceptions/DOMException-is-error.any.js
+++ webidl/ecmascript-binding/es-exceptions/DOMException-is-error.any.js
@@ -3,7 +3,5 @@
 test(function() {
   // https://github.com/tc39/proposal-is-error/issues/9
   // https://github.com/whatwg/webidl/pull/1421
-  if (typeof Error.isError === 'function') {
-    assert_true(Error.isError(new DOMException()));
-  }
+  assert_true(Error.isError(new DOMException()));
 });

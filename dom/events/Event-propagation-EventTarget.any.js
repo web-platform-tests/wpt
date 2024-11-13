bc224dfb6d7606a3c5a06391f2e61dc205d89845
Philip Chimento <pchimento@igalia.com>
2024-11-13 14:05:00 -0800

    Add non-DOM version of Event-propagation test
    Renames the existing test to Event-propagation-dom-node.window.js, and
    adds a copy in Event-propagation-EventTarget.any.js that uses a plain
    EventTarget instance instead of a DOM node. The difference is that
    EventTarget doesn't have bubbling/capture behaviour, so the expectations
    after stopPropagation() and cancelBubble=true are different.

--- dom/events/Event-propagation-EventTarget.any.js
+++ dom/events/Event-propagation-EventTarget.any.js
@@ -2,43 +2,47 @@
 
 // Author: Aryeh Gregor <ayg@aryeh.name>
 
+// This is a copy of the test in Event-propagation-dom-node.window.js, but on a
+// plain EventTarget instead of a DOM node. The difference is that EventTarget
+// doesn't have bubbling/capture behaviour, so the expectations after
+// stopPropagation() and cancelBubble=true are different.
+
 "use strict";
+
+var target = new EventTarget();
 
 function testPropagationFlag(ev, expected, desc) {
   test(function() {
     var called = false;
     var callback = function() { called = true };
     this.add_cleanup(function() {
-      document.head.removeEventListener("foo", callback)
+      target.removeEventListener("foo", callback)
     });
-    document.head.addEventListener("foo", callback);
-    document.head.dispatchEvent(ev);
+    target.addEventListener("foo", callback);
+    target.dispatchEvent(ev);
     assert_equals(called, expected, "Propagation flag");
     // dispatchEvent resets the propagation flags so it will happily dispatch
     // the event the second time around.
-    document.head.dispatchEvent(ev);
+    target.dispatchEvent(ev);
     assert_equals(called, true, "Propagation flag after first dispatch");
   }, desc);
 }
 
-var ev = document.createEvent("Event");
-ev.initEvent("foo", true, false);
+var ev = new Event("foo", { bubbles: true, cancelable: false });
 testPropagationFlag(ev, true, "Newly-created Event");
 ev.stopPropagation();
-testPropagationFlag(ev, false, "After stopPropagation()");
+testPropagationFlag(ev, true, "After stopPropagation()");
 ev.initEvent("foo", true, false);
 testPropagationFlag(ev, true, "Reinitialized after stopPropagation()");
 
-var ev = document.createEvent("Event");
-ev.initEvent("foo", true, false);
+var ev = new Event("foo", { bubbles: true, cancelable: false });
 ev.stopImmediatePropagation();
 testPropagationFlag(ev, false, "After stopImmediatePropagation()");
 ev.initEvent("foo", true, false);
 testPropagationFlag(ev, true, "Reinitialized after stopImmediatePropagation()");
 
-var ev = document.createEvent("Event");
-ev.initEvent("foo", true, false);
+var ev = new Event("foo", { bubbles: true, cancelable: false });
 ev.cancelBubble = true;
-testPropagationFlag(ev, false, "After cancelBubble=true");
+testPropagationFlag(ev, true, "After cancelBubble=true");
 ev.initEvent("foo", true, false);
 testPropagationFlag(ev, true, "Reinitialized after cancelBubble=true");

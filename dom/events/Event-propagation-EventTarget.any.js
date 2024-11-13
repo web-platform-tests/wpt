4a1933e8abb663303cf873aea8c7648365af3f0b
Philip Chimento <pchimento@igalia.com>
2024-11-13 14:05:00 -0800

    Add event propagation test on an EventTarget
    This adds a copy of the test in Event-propagation.html, but on a plain
    EventTarget instead of a DOM node. The difference is that EventTarget
    doesn't have bubbling/capture behaviour, so the expectations after
    stopPropagation() and cancelBubble=true are different.
    
    This test can be run in every global scope where EventTarget is available,
    not just in ShadowRealm.

---
+++ dom/events/Event-propagation-EventTarget.any.js
@@ -0,0 +1,46 @@
+// META: global=window,worker,shadowrealm
+
+// This is a copy of the test in Event-propagation.html, but on a plain
+// EventTarget instead of a DOM node. The difference is that EventTarget doesn't
+// have bubbling/capture behaviour, so the expectations after stopPropagation()
+// and cancelBubble=true are different.
+
+"use strict";
+
+var target = new EventTarget();
+
+function testPropagationFlag(ev, expected, desc) {
+  test(function() {
+    var called = false;
+    var callback = function() { called = true };
+    this.add_cleanup(function() {
+      target.removeEventListener("foo", callback)
+    });
+    target.addEventListener("foo", callback);
+    target.dispatchEvent(ev);
+    assert_equals(called, expected, "Propagation flag");
+    // dispatchEvent resets the propagation flags so it will happily dispatch
+    // the event the second time around.
+    target.dispatchEvent(ev);
+    assert_equals(called, true, "Propagation flag after first dispatch");
+  }, desc);
+}
+
+var ev = new Event("foo", { bubbles: true, cancelable: false });
+testPropagationFlag(ev, true, "Newly-created Event");
+ev.stopPropagation();
+testPropagationFlag(ev, true, "After stopPropagation()");
+ev.initEvent("foo", true, false);
+testPropagationFlag(ev, true, "Reinitialized after stopPropagation()");
+
+var ev = new Event("foo", { bubbles: true, cancelable: false });
+ev.stopImmediatePropagation();
+testPropagationFlag(ev, false, "After stopImmediatePropagation()");
+ev.initEvent("foo", true, false);
+testPropagationFlag(ev, true, "Reinitialized after stopImmediatePropagation()");
+
+var ev = new Event("foo", { bubbles: true, cancelable: false });
+ev.cancelBubble = true;
+testPropagationFlag(ev, true, "After cancelBubble=true");
+ev.initEvent("foo", true, false);
+testPropagationFlag(ev, true, "Reinitialized after cancelBubble=true");

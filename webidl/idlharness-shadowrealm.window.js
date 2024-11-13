8e70e1cfa31ef78ad49b1be804b3b41d60c638ef
Philip Chimento <pchimento@igalia.com>
2024-11-13 15:46:02 -0800

    Enable ShadowRealm testing for DOMException
    Adds the appropriate metadata for the existing tests for the functionality
    of DOMException to additionally be tested in a ShadowRealm.
    
    Adds the same IDL array to the IDL tests for DOMExceptions in ShadowRealm
    as for DOMExceptions in other realms.

--- webidl/idlharness-shadowrealm.window.js
+++ webidl/idlharness-shadowrealm.window.js
@@ -1,2 +1,12 @@
 // META: script=/resources/idlharness-shadowrealm.js
-idl_test_shadowrealm(["webidl"], []);
+idl_test_shadowrealm(
+  ["webidl"],
+  [],
+  idl_array => {
+    idl_array.add_objects({
+      DOMException: ["new DOMException()",
+                     'new DOMException("my message")',
+                     'new DOMException("my message", "myName")']
+    });
+  }
+);

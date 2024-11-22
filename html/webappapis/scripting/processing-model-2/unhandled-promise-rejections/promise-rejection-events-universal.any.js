f6409225090aba6036e7c85b8403ebc584310d5a
Philip Chimento <pchimento@igalia.com>
2024-11-22 12:22:50 -0800

    Add promise rejection event tests based on queueMicrotask
    promise-rejection-events.any.js tests several combinations of microtask
    queueing with postMessage, MutationObserver, and Promise.resolve().then().
    Add queueMicrotask to this mix, and put it in
    promise-rejection-events-universal.any.js since it is available
    universally.
    
    Not sure if the section "Positive unhandledrejection/rejectionhandled
    tests with delayed attachment" is relevant, as there is no way to queue a
    "macrotask" inside ShadowRealm, so it might not be possible to test the
    case where it's too late to attach a handler.

--- html/webappapis/scripting/processing-model-2/unhandled-promise-rejections/promise-rejection-events-universal.any.js
+++ html/webappapis/scripting/processing-model-2/unhandled-promise-rejections/promise-rejection-events-universal.any.js
@@ -35,6 +35,19 @@ async_test(function(t) {
 
 async_test(function(t) {
   var e = new Error();
+  var p;
+
+  onUnhandledSucceed(t, e, function() { return p; });
+
+  p = new Promise(function(_, reject) {
+    queueMicrotask(function() {
+      reject(e);
+    });
+  });
+}, 'unhandledrejection: from a queueMicrotask-delayed rejection');
+
+async_test(function(t) {
+  var e = new Error();
   var e2 = new Error();
   var promise2;
 
@@ -48,6 +61,49 @@ async_test(function(t) {
     throw e2;
   });
 }, 'unhandledrejection: from a throw in a rejection handler chained off of Promise.reject');
+
+async_test(function(t) {
+  var e = new Error();
+  var e2 = new Error();
+  var promise2;
+
+  onUnhandledSucceed(t, e2, function() { return promise2; });
+
+  var unreached = t.unreached_func('promise should not be fulfilled');
+  promise2 = new Promise(function(_, reject) {
+    queueMicrotask(function() {
+      reject(e);
+    });
+  }).then(unreached, function(reason) {
+    t.step(function() {
+      assert_equals(reason, e);
+    });
+    throw e2;
+  });
+}, 'unhandledrejection: from a throw in a rejection handler chained off of a queueMicrotask-delayed rejection');
+
+async_test(function(t) {
+  var e = new Error();
+  var e2 = new Error();
+  var promise2;
+
+  onUnhandledSucceed(t, e2, function() { return promise2; });
+
+  var promise = new Promise(function(_, reject) {
+    queueMicrotask(function() {
+      reject(e);
+      queueMicrotask(function() {
+        var unreached = t.unreached_func('promise should not be fulfilled');
+        promise2 = promise.then(unreached, function(reason) {
+          t.step(function() {
+            assert_equals(reason, e);
+          });
+          throw e2;
+        });
+      });
+    }, 1);
+  });
+}, 'unhandledrejection: from a throw in a rejection handler attached one microtask after a queueMicrotask-delayed rejection');
 
 async_test(function(t) {
   var e = new Error();
@@ -70,6 +126,21 @@ async_test(function(t) {
     throw e;
   });
 }, 'unhandledrejection: from a throw in a fulfillment handler');
+
+async_test(function(t) {
+  var e = new Error();
+  var p;
+
+  onUnhandledSucceed(t, e, function() { return p; });
+
+  p = Promise.resolve().then(function() {
+    return new Promise(function(_, reject) {
+      queueMicrotask(function() {
+        reject(e);
+      });
+    });
+  });
+}, 'unhandledrejection: from returning a queueMicrotask-delayed rejection in a fulfillment handler');
 
 async_test(function(t) {
   var e = new Error();
@@ -151,3 +222,176 @@ async_test(function(t) {
   });
 }, 'no unhandledrejection/rejectionhandled: rejection handler attached synchronously to a promise created from ' +
    'returning a Promise.reject-created promise in a fulfillment handler');
+
+  async_test(function(t) {
+  var e = new Error();
+  var p;
+
+  onUnhandledFail(t, function() { return p; });
+
+  var unreached = t.unreached_func('promise should not be fulfilled');
+  p = Promise.resolve().then(function() {
+    return new Promise(function(_, reject) {
+      queueMicrotask(function() {
+        reject(e);
+      });
+    });
+  }).then(unreached, function(reason) {
+    t.step(function() {
+      assert_equals(reason, e);
+    });
+  });
+}, 'no unhandledrejection/rejectionhandled: rejection handler attached synchronously to a promise created from ' +
+    'returning a queueMicrotask-delayed rejection in a fulfillment handler');
+
+async_test(function(t) {
+  var e = new Error();
+  var p;
+
+  onUnhandledFail(t, function() { return p; });
+
+  queueMicrotask(function() {
+    p = Promise.resolve().then(function() {
+      return Promise.reject(e);
+    })
+    .catch(function() {});
+  });
+}, 'no unhandledrejection/rejectionhandled: all inside a queued microtask, a rejection handler attached synchronously ' +
+    'to a promise created from returning a Promise.reject-created promise in a fulfillment handler');
+
+//
+// Negative unhandledrejection/rejectionhandled tests with microtask-delayed attachment
+//
+
+async_test(function(t) {
+  var e = new Error();
+  var p;
+
+  onUnhandledFail(t, function() { return p; });
+
+  p = Promise.reject(e);
+  queueMicrotask(function() {
+    var unreached = t.unreached_func('promise should not be fulfilled');
+    p.then(unreached, function() {});
+  });
+}, 'delayed handling: a microtask delay before attaching a handler prevents both events (Promise.reject-created ' +
+   'promise)');
+
+async_test(function(t) {
+  var e = new Error();
+  var p;
+
+  onUnhandledFail(t, function() { return p; });
+
+  p = new Promise(function(_, reject) {
+    reject(e);
+  });
+  queueMicrotask(function() {
+    var unreached = t.unreached_func('promise should not be fulfilled');
+    p.then(unreached, function() {});
+  });
+}, 'delayed handling: a microtask delay before attaching a handler prevents both events (immediately-rejected new ' +
+   'Promise-created promise)');
+
+async_test(function(t) {
+  var e = new Error();
+  var p1;
+  var p2;
+
+  onUnhandledFail(t, function() { return p1; });
+  onUnhandledFail(t, function() { return p2; });
+
+  p1 = new Promise(function(_, reject) {
+    queueMicrotask(function() {
+      reject(e);
+    });
+  });
+  p2 = Promise.all([p1]);
+  queueMicrotask(function() {
+    var unreached = t.unreached_func('promise should not be fulfilled');
+    p2.then(unreached, function() {});
+  });
+}, 'delayed handling: a microtask delay before attaching the handler, and before rejecting the promise, indirected ' +
+   'through Promise.all');
+
+//
+// Negative unhandledrejection/rejectionhandled tests with nested-microtask-delayed attachment
+//
+
+async_test(function(t) {
+  var e = new Error();
+  var p;
+
+  onUnhandledFail(t, function() { return p; });
+
+  p = Promise.reject(e);
+  queueMicrotask(function() {
+    Promise.resolve().then(function() {
+      queueMicrotask(function() {
+        Promise.resolve().then(function() {
+          p.catch(function() {});
+        });
+      });
+    });
+  });
+}, 'microtask nesting: attaching a handler inside a combination of queueMicrotask + promise microtasks');
+
+async_test(function(t) {
+  var e = new Error();
+  var p;
+
+  onUnhandledFail(t, function() { return p; });
+
+  queueMicrotask(function() {
+    p = Promise.reject(e);
+    queueMicrotask(function() {
+      Promise.resolve().then(function() {
+        queueMicrotask(function() {
+          Promise.resolve().then(function() {
+            p.catch(function() {});
+          });
+        });
+      });
+    });
+  });
+}, 'microtask nesting: attaching a handler inside a combination of queueMicrotask + promise microtasks, ' +
+   'all inside a queueMicrotask');
+
+async_test(function(t) {
+  var e = new Error();
+  var p;
+
+  onUnhandledFail(t, function() { return p; });
+
+  p = Promise.reject(e);
+  Promise.resolve().then(function() {
+    queueMicrotask(function() {
+      Promise.resolve().then(function() {
+        queueMicrotask(function() {
+          p.catch(function() {});
+        });
+      });
+    });
+  });
+}, 'microtask nesting: attaching a handler inside a combination of promise microtasks + queueMicrotask');
+
+async_test(function(t) {
+  var e = new Error();
+  var p;
+
+  onUnhandledFail(t, function() { return p; });
+
+  queueMicrotask(function() {
+    p = Promise.reject(e);
+    Promise.resolve().then(function() {
+      queueMicrotask(function() {
+        Promise.resolve().then(function() {
+          queueMicrotask(function() {
+            p.catch(function() {});
+          });
+        });
+      });
+    });
+  });
+}, 'microtask nesting: attaching a handler inside a combination of promise microtasks + queueMicrotask, ' +
+   'all inside a queueMicrotask');

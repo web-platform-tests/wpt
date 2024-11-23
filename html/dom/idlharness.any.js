20a7efab6ee9312ae3774e0066f300c946a02c20
Philip Chimento <pchimento@igalia.com>
2024-11-22 17:41:41 -0800

    Improve idlharness-ShadowRealm integration
    By exposing a fetch_spec() on globalThis in testharness-shadowrealm-inner,
    we no longer need to have IDL tests in separate files for ShadowRealm, as
    the regular idlharness.js will just run there.
    
    Delete all existing idlharness-shadowrealm.window.js files and incorporate
    them into their neighbouring idlharness.any.js files.
    
    It seems sufficient to run the IDL tests only in shadowrealm-in-window;
    repeating them in every ShadowRealm scope seems like overkill.

--- html/dom/idlharness.any.js
+++ html/dom/idlharness.any.js
@@ -1,12 +1,17 @@
-"use strict";
+// META: global=dedicatedworker,shadowrealm-in-window
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
 
-importScripts("/resources/testharness.js");
-importScripts("/resources/WebIDLParser.js", "/resources/idlharness.js");
+"use strict";
 
 idl_test(
   ["html"],
   ["wai-aria", "dom", "cssom", "touch-events", "uievents", "performance-timeline"],
   idlArray => {
+    if (self.GLOBAL.isShadowRealm()) {
+      return;
+    }
+
     idlArray.add_untested_idls('typedef Window WindowProxy;');
     idlArray.add_objects({
       WorkerLocation: ['self.location'],
@@ -18,5 +23,3 @@ idl_test(
     });
   }
 );
-
-done();

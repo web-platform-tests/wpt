2b3c10bc7e4cea11e8ee4601be1c061266d0e765
Alex N. Jose <alexnj@google.com>
2024-12-11 12:12:17 +0530

    Review changes

--- html/syntax/speculative-parsing/expect-no-linked-resources/stash.js
+++
@@ -1,6 +0,0 @@
-// Fetch test results from the Stash
-async function get_results(uuid) {
-  const response = await fetch(`/html/syntax/speculative-parsing/expect-no-linked-resources/stash.py?action=get&uuid=${uuid}`);
-  const text = await response.text();
-  return text;
-}

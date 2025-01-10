6e7cfeeaaa19ac30bfb4850205bdd19495335847
Alex Rudenko <OrKoN@users.noreply.github.com>
2025-01-10 14:03:50 +0100

    Update webdriver/tests/bidi/browsing_context/locate_nodes/invalid.py

--- webdriver/tests/bidi/browsing_context/locate_nodes/invalid.py
+++ webdriver/tests/bidi/browsing_context/locate_nodes/invalid.py
@@ -46,7 +46,7 @@ async def test_params_locator_type_invalid_value(bidi_session, inline, top_conte
         )
 
 
-@pytest.mark.parametrize("type", ["css", "xpath", "innerText", "accessibility"])
+@pytest.mark.parametrize("type", ["css", "xpath", "innerText"])
 @pytest.mark.parametrize("value", [None, False, 42, {}, []])
 async def test_params_locator_value_invalid_type(
     bidi_session, inline, top_context, type, value

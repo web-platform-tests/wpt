b740d5ee86ee3ccda4b4e9f2faa8215e30610f8b
Luke Warlow <lwarlow@igalia.com>
2025-02-06 14:57:59 +0000

    Add violation report tests for setTimeout in workers
    Co-authored-by: Frédéric Wang <fwang@igalia.com>

--- trusted-types/support/block-string-assignment-to-DOMWindowTimers-setTimeout-setInterval-worker.js
+++ trusted-types/support/block-string-assignment-to-DOMWindowTimers-setTimeout-setInterval-worker.js
@@ -2,6 +2,7 @@ const testSetupPolicy = trustedTypes.createPolicy("p", { createScriptURL: s => s
 
 importScripts(testSetupPolicy.createScriptURL("/resources/testharness.js"));
 importScripts(testSetupPolicy.createScriptURL("helper.sub.js"));
+importScripts(testSetupPolicy.createScriptURL("csp-violations.js"));
 
 importScripts(testSetupPolicy.createScriptURL(
     "block-string-assignment-to-DOMWindowTimers-setTimeout-setInterval.js"
--- trusted-types/support/block-string-assignment-to-DOMWindowTimers-setTimeout-setInterval.js
+++ trusted-types/support/block-string-assignment-to-DOMWindowTimers-setTimeout-setInterval.js
@@ -1,5 +1,9 @@
 const globalThisStr = getGlobalThisStr();
 
+// https://html.spec.whatwg.org/multipage/timers-and-user-prompts.html#timer-initialisation-steps,
+// step 9.6.1.1.
+const expectedSink = globalThisStr.includes("Window") ? "Window" : "WorkerGlobalScope";
+
 // setTimeout tests
 // TrustedScript assignments do not throw.
 async_test(t => {
@@ -48,13 +52,21 @@ test(t => {
   });
 }, `\`${globalThisStr}.setInterval(null)\` throws.`);
 
+promise_test(async t => {
+  const input = ";".repeat(100);
+  let violation = await trusted_type_violation_for(TypeError, _ =>
+    globalThis.setTimeout(input)
+  );
+  assert_equals(violation.blockedURI, "trusted-types-sink");
+  assert_equals(violation.sample, `${expectedSink} setTimeout|${clipSampleIfNeeded(input)}`);
+}, `Violation report for ${globalThisStr}.setTimeout`);
+
 const kTimeoutTestString = "timeoutTestString";
 const kIntervalTestString = "intervalTestString";
 
+// After default policy creation string assignment implicitly calls createScript.
+promise_test(async t => {
 let policy = globalThis.trustedTypes.createPolicy("default", { createScript: (x, _, sink) => {
-   // https://html.spec.whatwg.org/multipage/timers-and-user-prompts.html#timer-initialisation-steps,
-  // step 9.6.1.1.
-  const expectedSink = globalThisStr.includes("Window") ? "Window" : "WorkerGlobalScope";
   if (x === kTimeoutTestString) {
     assert_equals(sink, `${expectedSink} setTimeout`);
   } else if (x === kIntervalTestString) {
@@ -62,18 +74,16 @@ let policy = globalThis.trustedTypes.createPolicy("default", { createScript: (x,
   }
   return "0";
 }});
-// After default policy creation string assignment implicitly calls createScript.
-test(t => {
   globalThis.setTimeout(INPUTS.SCRIPT);
   globalThis.setInterval(INPUTS.SCRIPT);
 }, `\`${globalThisStr}.setTimeout(string)\`, \`${globalThisStr}.setInterval(string)\` via default policy (successful Script transformation).`);
 // After default policy creation null assignment implicitly calls createScript.
-test(t => {
+promise_test(async t => {
   globalThis.setTimeout(null);
   globalThis.setInterval(null);
 }, `\`${globalThisStr}.setTimeout(null)\`, \`${globalThisStr}.setInterval(null)\` via default policy (successful Script transformation).`);
 
-test(t => {
+promise_test(async t => {
   globalThis.setTimeout(kTimeoutTestString);
   globalThis.setInterval(kIntervalTestString);
 }, `${globalThisStr}.setTimeout and ${globalThisStr}.setInterval pass the correct sink to the default policy`

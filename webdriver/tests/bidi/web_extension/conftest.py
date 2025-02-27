bd2dbc34affb02def4b4428c74bdfb7cb5735045
Alex Rudenko <alexrudenko@chromium.org>
2025-02-27 11:29:27 +0100

    test: add chrome data

--- webdriver/tests/bidi/web_extension/conftest.py
+++ webdriver/tests/bidi/web_extension/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from tests.support.helpers import get_extension_path, get_base64_for_extension_file
 
 
-extensions_by_browser = {
+EXTENSION_DATA = {
     "firefox": {
         "id": "1FC7D53C-0B0A-49E7-A8C0-47E77496A919@web-platform-tests.org",
         "path": get_extension_path("firefox/unpacked/"),
@@ -11,11 +11,11 @@ extensions_by_browser = {
         "base64": get_base64_for_extension_file("firefox/signed.xpi"),
     },
     "chrome": {
-        "id": "1FC7D53C-0B0A-49E7-A8C0-47E77496A919@web-platform-tests.org",
-        "path": get_extension_path("firefox/unpacked/"),
-        "archivePath": get_extension_path("firefox/signed.xpi"),
-        "archivePathInvalid": get_extension_path("firefox/invalid.xpi"),
-        "base64": get_base64_for_extension_file("firefox/signed.xpi"),
+        "id": None,
+        "path": get_extension_path("chrome/unpacked/"),
+        "archivePath": None,
+        "archivePathInvalid": get_extension_path("chrome/invalid"),
+        "base64": None,
     }
 }
 
@@ -24,4 +24,4 @@ extensions_by_browser = {
 def extension_data(current_session):
     browser_name = current_session.capabilities["browserName"]
 
-    return extensions_by_browser[browser_name]
+    return EXTENSION_DATA[browser_name]

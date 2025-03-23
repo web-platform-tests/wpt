1bf310a71b23d83b8d2fb31b7c723d0ed86974da
Henrik Skupin <mail@hskupin.info>
2025-03-23 13:57:19 +0000

    [wpt] Update Marionette executor to use "--remote-allow-system-access" argument for Firefox.
    Differential Revision: https://phabricator.services.mozilla.com/D241411
    
    bugzilla-url: https://bugzilla.mozilla.org/show_bug.cgi?id=1710425
    gecko-commit: c765284053d2d43d86f0cb197d0e55a1ca41bcb6
    gecko-reviewers: Sasha

--- tools/wptrunner/wptrunner/browsers/firefox.py
+++ tools/wptrunner/wptrunner/browsers/firefox.py
@@ -377,7 +377,8 @@ class FirefoxInstanceManager:
                           self.e10s)
 
         args = self.binary_args[:] if self.binary_args else []
-        args += [cmd_arg("marionette"), "about:blank"]
+        args += [cmd_arg("marionette"),
+                 cmd_arg("remote-allow-system-access"), "about:blank"]
 
         debug_args, cmd = browser_command(self.binary,
                                           args,

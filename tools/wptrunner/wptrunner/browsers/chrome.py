826e8cc993ce46d1d6a3c4611af0588edb1cc3ce
tcaptan-cr <105748547+tcaptan-cr@users.noreply.github.com>
2025-03-24 16:39:57 -0700

    Clean up DisableAhemAntialias
    This feature was removed (in [1]) because it was no longer
    needed since font antialiasing is disabled on all test platforms.
    
    [1]
    https://crrev.com/c/chromium/src/+/6370105

--- tools/wptrunner/wptrunner/browsers/chrome.py
+++ tools/wptrunner/wptrunner/browsers/chrome.py
@@ -161,9 +161,6 @@ def executor_kwargs(logger, test_type, test_environment, run_info_data, subsuite
     # Disable overlay scrollbar animations to prevent flaky wpt screenshots based on timing.
     chrome_options["args"].append("--disable-features=ScrollbarAnimations")
 
-    # Always disable antialiasing on the Ahem font.
-    blink_features = ['DisableAhemAntialias']
-
     if kwargs["enable_mojojs"]:
         blink_features.append('MojoJS')
         blink_features.append('MojoJSTest')

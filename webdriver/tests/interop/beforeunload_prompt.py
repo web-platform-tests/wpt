efe04b037cbd4f92fcc8ac03c04f2c4a03ce8dd1
Maksim Sadym <sadym@chromium.org>
2025-04-15 12:32:14 +0200

    [wdspec] Fix webdriver/tests/interop/beforeunload_prompt.py

--- webdriver/tests/interop/beforeunload_prompt.py
+++ webdriver/tests/interop/beforeunload_prompt.py
@@ -1,15 +1,11 @@
-import asyncio
 import pytest
 import pytest_asyncio
+
 from webdriver.error import NoSuchAlertException
-
-from tests.support.sync import AsyncPoll
-
 from ..bidi import (
     any_string,
     recursive_compare,
 )
-
 
 pytestmark = pytest.mark.asyncio
 
@@ -17,8 +13,9 @@ USER_PROMPT_CLOSED_EVENT = "browsingContext.userPromptClosed"
 USER_PROMPT_OPENED_EVENT = "browsingContext.userPromptOpened"
 
 
+
 @pytest_asyncio.fixture
-async def check_beforeunload_not_implicitly_accepted(
+async def check_beforeunload_implicitly_accepted(
     bidi_session,
     current_session,
     setup_beforeunload_page,
@@ -29,7 +26,7 @@ async def check_beforeunload_not_implicitly_accepted(
     execute_as_async,
     url,
 ):
-    async def check_beforeunload_not_implicitly_accepted(accept):
+    async def check_beforeunload_implicitly_accepted(accepted):
         current_session.window_handle = new_tab["context"]
 
         page_beforeunload = await setup_beforeunload_page(new_tab)
@@ -40,16 +37,11 @@ async def check_beforeunload_not_implicitly_accepted(
 
         await subscribe_events([USER_PROMPT_CLOSED_EVENT, USER_PROMPT_OPENED_EVENT])
 
-        # Using WebDriver classic's navigation command to navigate away from
-        # the page will hang and wait for the beforeunload dialog to close.
-        # As such start the command immediately as task but await for it later
-        # when BiDi closed the prompt.
-        def sync_navigate():
-            current_session.url = page_target
+        # Navigate via classic.
+        current_session.url = page_target
 
-        task_navigate = asyncio.create_task(execute_as_async(sync_navigate))
+        # Wait for BiDi events.
         opened_event = await wait_for_future_safe(on_prompt_opened)
-
         recursive_compare(
             {
                 "context": new_tab["context"],
@@ -59,59 +51,45 @@ async def check_beforeunload_not_implicitly_accepted(
             opened_event,
         )
 
-        # Close the beforeunload prompt and wait for the navigation to finish.
-        await bidi_session.browsing_context.handle_user_prompt(
-            context=new_tab["context"], accept=accept
-        )
         closed_event = await wait_for_future_safe(on_prompt_closed)
-        await task_navigate
-
-        # Check that the beforeunload prompt is closed and the event was sent.
-        with pytest.raises(NoSuchAlertException):
-            current_session.alert.text
-
         recursive_compare(
             {
-                "accepted": accept,
+                "accepted": accepted,
                 "context": new_tab["context"],
                 "type": "beforeunload",
             },
             closed_event,
         )
 
-        if accept:
+        # Assert the alert cannot be closed via classic.
+        with pytest.raises(NoSuchAlertException):
+            current_session.alert.text
+
+        # Assert the classic url changed.
+        if accepted:
             assert current_session.url == page_target
         else:
             assert current_session.url == page_beforeunload
 
-    return check_beforeunload_not_implicitly_accepted
+    return check_beforeunload_implicitly_accepted
 
 
 @pytest.mark.capabilities({"unhandledPromptBehavior": "accept"})
-@pytest.mark.parametrize("accept", [False, True])
-async def test_accept(check_beforeunload_not_implicitly_accepted, accept):
-    await check_beforeunload_not_implicitly_accepted(accept)
+async def test_accept(check_beforeunload_implicitly_accepted):
+    await check_beforeunload_implicitly_accepted(True)
 
 
 @pytest.mark.capabilities({"unhandledPromptBehavior": "accept and notify"})
-@pytest.mark.parametrize("accept", [False, True])
-async def test_accept_and_notify(check_beforeunload_not_implicitly_accepted, accept):
-    await check_beforeunload_not_implicitly_accepted(accept)
+async def test_accept_and_notify(check_beforeunload_implicitly_accepted):
+    await check_beforeunload_implicitly_accepted(True)
 
 
 @pytest.mark.capabilities({"unhandledPromptBehavior": "dismiss"})
-@pytest.mark.parametrize("accept", [False, True])
-async def test_dismiss(check_beforeunload_not_implicitly_accepted, accept):
-    await check_beforeunload_not_implicitly_accepted(accept)
+async def test_dismiss(check_beforeunload_implicitly_accepted):
+    await check_beforeunload_implicitly_accepted(False)
 
 
 @pytest.mark.capabilities({"unhandledPromptBehavior": "dismiss and notify"})
-@pytest.mark.parametrize("accept", [False, True])
-async def test_dismiss_and_notify(check_beforeunload_not_implicitly_accepted, accept):
-    await check_beforeunload_not_implicitly_accepted(accept)
-
-
-@pytest.mark.capabilities({"unhandledPromptBehavior": "ignore"})
-@pytest.mark.parametrize("accept", [False, True])
-async def test_ignore(check_beforeunload_not_implicitly_accepted, accept):
-    await check_beforeunload_not_implicitly_accepted(accept)
+async def test_dismiss_and_notify(
+        check_beforeunload_implicitly_accepted):
+    await check_beforeunload_implicitly_accepted(False)

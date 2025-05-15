2cd27737a8359314340da633c2255a4d0879d2d9
Maksim Sadym <69349599+sadym-chromium@users.noreply.github.com>
2025-05-15 13:46:28 +0200

    Update webdriver/tests/bidi/browser/create_user_context/invalid.py
    Co-authored-by: Alexandra Borovova <lutien@users.noreply.github.com>

--- webdriver/tests/bidi/browser/create_user_context/invalid.py
+++ webdriver/tests/bidi/browser/create_user_context/invalid.py
@@ -5,7 +5,7 @@ import webdriver.bidi.error as error
 pytestmark = pytest.mark.asyncio
 
 
-@pytest.mark.parametrize("value", [42, "foo", {}])
+@pytest.mark.parametrize("value", [42, "foo", {}, []])
 async def test_accept_insecure_certs_invalid_type(bidi_session, value):
     with pytest.raises(error.InvalidArgumentException):
         await bidi_session.browser.create_user_context(

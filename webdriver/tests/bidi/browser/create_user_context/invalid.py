ef1358ba0a9bd0906469393c5d0847e4d97af98c
Maksim Sadym <sadym@chromium.org>
2025-05-21 16:37:55 +0200

    comment

--- webdriver/tests/bidi/browser/create_user_context/invalid.py
+++ webdriver/tests/bidi/browser/create_user_context/invalid.py
@@ -5,6 +5,12 @@ import webdriver.bidi.error as error
 pytestmark = pytest.mark.asyncio
 
 
+# Even though the user context is not expected to be created, if the user agent
+# under the test does not support the parameter, the validation will not fail
+# and unexpected user context will be created and will not be closed. Using
+# `create_user_context` fixture guarantees the mistakenly created user context
+# is destroyed.
+
 @pytest.mark.parametrize("value", [False, 42, "foo", []])
 async def test_proxy_invalid_type(create_user_context, value):
     with pytest.raises(error.InvalidArgumentException):

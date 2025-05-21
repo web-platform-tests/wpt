eb51e88157de37b11ea8b9990ed62ad268d53a69
Maksim Sadym <sadym@chromium.org>
2025-05-21 16:43:51 +0200

    Use `create_user_context` to avoid creating unnecessary user contexts

--- webdriver/tests/bidi/browser/create_user_context/invalid.py
+++ webdriver/tests/bidi/browser/create_user_context/invalid.py
@@ -5,8 +5,13 @@ import webdriver.bidi.error as error
 pytestmark = pytest.mark.asyncio
 
 
+# Even though the user context is not expected to be created, if the user agent
+# under the test does not support the parameter, the validation will not fail
+# and unexpected user context will be created and will not be closed. Using
+# `create_user_context` fixture guarantees the mistakenly created user context
+# is destroyed.
+
 @pytest.mark.parametrize("value", [42, "foo", {}, []])
-async def test_accept_insecure_certs_invalid_type(bidi_session, value):
+async def test_accept_insecure_certs_invalid_type(create_user_context, value):
     with pytest.raises(error.InvalidArgumentException):
-        await bidi_session.browser.create_user_context(
-            accept_insecure_certs=value)
+        await create_user_context(accept_insecure_certs=value)

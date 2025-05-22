93bfa6b3060c0df11899b6e3e1ed26ac36e758be
Maksim Sadym <69349599+sadym-chromium@users.noreply.github.com>
2025-05-22 14:02:14 +0200

    [wdspec] test `browser.createUserContext:proxy` parameter (#52691)
    use `create_user_context` instead of `bidi_session.browser.create_user_context`

--- webdriver/tests/bidi/browser/create_user_context/invalid.py
+++ webdriver/tests/bidi/browser/create_user_context/invalid.py
@@ -15,3 +15,54 @@ pytestmark = pytest.mark.asyncio
 async def test_accept_insecure_certs_invalid_type(create_user_context, value):
     with pytest.raises(error.InvalidArgumentException):
         await create_user_context(accept_insecure_certs=value)
+
+
+@pytest.mark.parametrize("value", [False, 42, "foo", []])
+async def test_proxy_invalid_type(create_user_context, value):
+    with pytest.raises(error.InvalidArgumentException):
+        await create_user_context(
+            proxy=value)
+
+
+@pytest.mark.parametrize("value", [{}])
+async def test_proxy_invalid_value(create_user_context, value):
+    with pytest.raises(error.InvalidArgumentException):
+        await create_user_context(
+            proxy=value)
+
+
+@pytest.mark.parametrize("value", [False, 42, {}, []])
+async def test_proxy_proxy_type_invalid_type(create_user_context, value):
+    with pytest.raises(error.InvalidArgumentException):
+        await create_user_context(
+            proxy={
+                "proxyType": value
+            })
+
+
+async def test_proxy_proxy_type_invalid_value(create_user_context):
+    with pytest.raises(error.InvalidArgumentException):
+        await create_user_context(
+            proxy={
+                "proxyType": "SOME_UNKNOWN_TYPE"
+            })
+
+
+async def test_proxy_proxy_type_manual_socks_version_without_socks_proxy(
+        create_user_context):
+    with pytest.raises(error.InvalidArgumentException):
+        await create_user_context(
+            proxy={
+                "proxyType": "manual",
+                "socksVersion": 0
+            })
+
+
+async def test_proxy_proxy_type_manual_socks_proxy_without_socks_version(
+        create_user_context):
+    with pytest.raises(error.InvalidArgumentException):
+        await create_user_context(
+            proxy={
+                "proxyType": "manual",
+                "socksProxy": "127.0.0.1:1080"
+            })

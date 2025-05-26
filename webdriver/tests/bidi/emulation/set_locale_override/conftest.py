7c676381715fc2797b46cf3c42482aa3f1b45571
Maksim Sadym <sadym@chromium.org>
2025-05-26 14:07:10 +0200

    [wdspec] `emulation.setOrientationOverride` per user context

---
+++ webdriver/tests/bidi/emulation/set_locale_override/conftest.py
@@ -0,0 +1,35 @@
+import pytest
+import pytest_asyncio
+
+from webdriver.bidi.modules.script import ContextTarget
+
+
+@pytest_asyncio.fixture
+async def get_current_locale(bidi_session):
+    async def get_current_locale(context):
+        result = await bidi_session.script.evaluate(
+            expression="new Intl.DateTimeFormat().resolvedOptions().locale",
+            target=ContextTarget(context["context"]),
+            await_promise=False,
+        )
+
+        return result["value"]
+
+    return get_current_locale
+
+
+@pytest_asyncio.fixture
+async def initial_locale(get_current_locale, top_context):
+    return await get_current_locale(top_context)
+
+
+@pytest.fixture
+def some_locale(initial_locale):
+    """Returns some locale which is not equal to initial one."""
+    return "es-ES" if initial_locale != "es-ES" else "it-IT"
+
+
+@pytest.fixture
+def another_locale(initial_locale):
+    """Returns some locale which is not equal to initial one."""
+    return "de-DE" if initial_locale != "de-DE" else "fr-FR"

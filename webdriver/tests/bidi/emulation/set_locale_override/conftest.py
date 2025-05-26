4e34bd1d77984bb84b62b8fced968b51e3ad30e3
Maksim Sadym <sadym@chromium.org>
2025-05-26 15:51:03 +0200

    comment

--- webdriver/tests/bidi/emulation/set_locale_override/conftest.py
+++ webdriver/tests/bidi/emulation/set_locale_override/conftest.py
@@ -20,16 +20,25 @@ async def get_current_locale(bidi_session):
 
 @pytest_asyncio.fixture
 async def initial_locale(get_current_locale, top_context):
+    """
+    Returns default locale.
+    """
     return await get_current_locale(top_context)
 
 
 @pytest.fixture
 def some_locale(initial_locale):
-    """Returns some locale which is not equal to the initial one."""
+    """
+    Returns some locale which is not equal to `initial_locale` nor to
+    `another_locale`.
+    """
     return "es-ES" if initial_locale != "es-ES" else "it-IT"
 
 
 @pytest.fixture
 def another_locale(initial_locale):
-    """Returns some locale which is not equal to the initial one."""
+    """
+    Returns some locale which is not equal to `initial_locale` nor to
+    `some_locale`.
+    """
     return "de-DE" if initial_locale != "de-DE" else "fr-FR"

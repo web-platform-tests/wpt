9f4a6ab65b0dfcd017bb585e92a8ae19595e15f5
Alexandru Marc <amarc@mozila.com>
2025-05-30 17:36:58 +0000

    Revert "Bug 1906051 - [wdspec] Update document open test case in history updated test to add missing event " for causing puppeteer failures @ navigation.spec.js
    This reverts commit e957365872166807959e0419b7f5987d665c379a.
    
    Revert "Bug 1906051 - [wdspec] Update wdspec tests for fragment navigated to check event counts with iframes r=webdriver-reviewers,Sasha"
    
    This reverts commit 9fc589698b0e93ad4319f4399307fc0bb3f00183.
    
    Revert "Bug 1906051 - [bidi] Implement browsingContext.historyUpdated r=webdriver-reviewers,Sasha"
    
    This reverts commit 07cb8711aab82839c986eceea86509724fb39710.
    
    gecko-commit: 46bc88fc05d082a37845b02bf3d4975759cb4b97
    gecko-reviewers: webdriver-reviewers, Sasha

--- webdriver/tests/bidi/browsing_context/fragment_navigated/fragment_navigated.py
+++ webdriver/tests/bidi/browsing_context/fragment_navigated/fragment_navigated.py
@@ -143,16 +143,6 @@ async def test_iframe(
 
     await subscribe_events([FRAGMENT_NAVIGATED_EVENT])
 
-    # Track all received browsingContext.fragmentNavigated events in the events array
-    events = []
-
-    async def on_event(method, data):
-        events.append(data)
-
-    remove_listener = bidi_session.add_event_listener(
-        FRAGMENT_NAVIGATED_EVENT, on_event
-    )
-
     on_fragment_navigated = wait_for_event(FRAGMENT_NAVIGATED_EVENT)
 
     target_url = url(EMPTY_PAGE + '#bar')
@@ -167,10 +157,6 @@ async def test_iframe(
         },
         await wait_for_future_safe(on_fragment_navigated),
     )
-
-    # Check that we only received one event for the iframe navigation.
-    assert len(events) == 1
-    remove_listener()
 
 
 @pytest.mark.parametrize(

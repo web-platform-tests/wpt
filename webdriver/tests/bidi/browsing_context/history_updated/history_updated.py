5501363f2d34ca6febdbed3eddeb202076502a8e
Julian Descottes <jdescottes@mozilla.com>
2025-06-04 14:42:16 +0000

    [wdspec] Reformat history_updated.py using black
    Differential Revision: https://phabricator.services.mozilla.com/D252401
    
    bugzilla-url: https://bugzilla.mozilla.org/show_bug.cgi?id=1970056
    gecko-commit: 17bae64cf8d61c206365d0413b1caf13d3b4a6e4
    gecko-reviewers: webdriver-reviewers, whimboo

--- webdriver/tests/bidi/browsing_context/history_updated/history_updated.py
+++ webdriver/tests/bidi/browsing_context/history_updated/history_updated.py
@@ -13,6 +13,7 @@ FRAGMENT_NAVIGATED_EVENT = "browsingContext.fragmentNavigated"
 HISTORY_UPDATED_EVENT = "browsingContext.historyUpdated"
 CREATED_EVENT = "browsingContext.contextCreated"
 
+
 @pytest.mark.parametrize(
     "hash_before, hash_after, history_method",
     [
@@ -24,10 +25,16 @@ CREATED_EVENT = "browsingContext.contextCreated"
         ("#foo", "#bar", "replaceState"),
         ("#foo", "#foo", "replaceState"),
         ("#bar", "", "replaceState"),
-    ]
+    ],
 )
 async def test_history_url_update(
-    bidi_session, new_tab, url, subscribe_events, hash_before, hash_after, history_method
+    bidi_session,
+    new_tab,
+    url,
+    subscribe_events,
+    hash_before,
+    hash_after,
+    history_method,
 ):
     target_context = new_tab["context"]
 
@@ -39,48 +46,55 @@ async def test_history_url_update(
 
     fragment_navigated_events = []
     history_updated_events = []
+
     async def on_event(method, data):
         if method == FRAGMENT_NAVIGATED_EVENT:
-          fragment_navigated_events.append(data)
+            fragment_navigated_events.append(data)
         if method == HISTORY_UPDATED_EVENT:
-           history_updated_events.append(data)
+            history_updated_events.append(data)
 
-    remove_fragment_navigated_listener = bidi_session.add_event_listener(FRAGMENT_NAVIGATED_EVENT, on_event)
-    remove_history_updated_listener = bidi_session.add_event_listener(HISTORY_UPDATED_EVENT, on_event)
+    remove_fragment_navigated_listener = bidi_session.add_event_listener(
+        FRAGMENT_NAVIGATED_EVENT, on_event
+    )
+    remove_history_updated_listener = bidi_session.add_event_listener(
+        HISTORY_UPDATED_EVENT, on_event
+    )
 
     try:
-      target_url = url(EMPTY_PAGE + hash_after)
+        target_url = url(EMPTY_PAGE + hash_after)
 
-      await bidi_session.script.call_function(
-          raw_result=True,
-          function_declaration="""(method, url) => {
-              history[method](null, null, url);
-          }""",
-          arguments=[
-              {"type": "string", "value": history_method},
-              {"type": "string", "value": target_url},
-          ],
-          await_promise=False,
-          target=ContextTarget(target_context),
-      )
+        await bidi_session.script.call_function(
+            raw_result=True,
+            function_declaration="""(method, url) => {
+                history[method](null, null, url);
+            }""",
+            arguments=[
+                {"type": "string", "value": history_method},
+                {"type": "string", "value": target_url},
+            ],
+            await_promise=False,
+            target=ContextTarget(target_context),
+        )
 
-      recursive_compare(
-          [{
-              'context': target_context,
-              'url': target_url
-          }],
-          history_updated_events
-      )
+        recursive_compare(
+            [
+                {
+                    "context": target_context,
+                    "url": target_url,
+                }
+            ],
+            history_updated_events,
+        )
 
-      # browsingContext.historyUpdated should not contain any navigation id or
-      # any timestamp.
-      assert "navigation" not in history_updated_events[0]
-      assert "timestamp" not in history_updated_events[0]
+        # browsingContext.historyUpdated should not contain any navigation id or
+        # any timestamp.
+        assert "navigation" not in history_updated_events[0]
+        assert "timestamp" not in history_updated_events[0]
 
-      assert len(fragment_navigated_events) == 0
+        assert len(fragment_navigated_events) == 0
     finally:
-       remove_fragment_navigated_listener()
-       remove_history_updated_listener()
+        remove_fragment_navigated_listener()
+        remove_history_updated_listener()
 
 
 @pytest.mark.parametrize(
@@ -88,7 +102,7 @@ async def test_history_url_update(
     [
         ("pushState"),
         ("replaceState"),
-    ]
+    ],
 )
 async def test_history_state_update(
     bidi_session, new_tab, url, subscribe_events, history_method
@@ -104,45 +118,44 @@ async def test_history_state_update(
 
     fragment_navigated_events = []
     history_updated_events = []
+
     async def on_event(method, data):
         if method == FRAGMENT_NAVIGATED_EVENT:
-          fragment_navigated_events.append(data)
+            fragment_navigated_events.append(data)
         if method == HISTORY_UPDATED_EVENT:
-           history_updated_events.append(data)
+            history_updated_events.append(data)
 
-    remove_fragment_navigated_listener = bidi_session.add_event_listener(FRAGMENT_NAVIGATED_EVENT, on_event)
-    remove_history_updated_listener = bidi_session.add_event_listener(HISTORY_UPDATED_EVENT, on_event)
+    remove_fragment_navigated_listener = bidi_session.add_event_listener(
+        FRAGMENT_NAVIGATED_EVENT, on_event
+    )
+    remove_history_updated_listener = bidi_session.add_event_listener(
+        HISTORY_UPDATED_EVENT, on_event
+    )
 
     try:
-      await bidi_session.script.call_function(
-          raw_result=True,
-          function_declaration="""(method) => {
-              history[method]({}, null);
-          }""",
-          arguments=[
-              {"type": "string", "value": history_method},
-          ],
-          await_promise=False,
-          target=ContextTarget(target_context),
-      )
+        await bidi_session.script.call_function(
+            raw_result=True,
+            function_declaration="""(method) => {
+                history[method]({}, null);
+            }""",
+            arguments=[
+                {"type": "string", "value": history_method},
+            ],
+            await_promise=False,
+            target=ContextTarget(target_context),
+        )
 
-      recursive_compare(
-          [{
-              'context': target_context,
-              'url': target_url
-          }],
-          history_updated_events
-      )
+        recursive_compare(
+            [{"context": target_context, "url": target_url}], history_updated_events
+        )
 
-      assert len(fragment_navigated_events) == 0
+        assert len(fragment_navigated_events) == 0
     finally:
-       remove_fragment_navigated_listener()
-       remove_history_updated_listener()
+        remove_fragment_navigated_listener()
+        remove_history_updated_listener()
 
 
-async def test_history_document_open(
-    bidi_session, new_tab, url, subscribe_events
-):
+async def test_history_document_open(bidi_session, new_tab, url, subscribe_events):
     target_context = new_tab["context"]
 
     target_url = url(EMPTY_PAGE)
@@ -150,30 +163,34 @@ async def test_history_document_open(
         context=new_tab["context"], url=target_url, wait="complete"
     )
 
-    await subscribe_events([FRAGMENT_NAVIGATED_EVENT, HISTORY_UPDATED_EVENT, CREATED_EVENT])
+    await subscribe_events(
+        [FRAGMENT_NAVIGATED_EVENT, HISTORY_UPDATED_EVENT, CREATED_EVENT]
+    )
 
     fragment_navigated_events = []
     history_updated_events = []
     browsing_context_created_events = []
 
-
     async def on_event(method, data):
         if method == FRAGMENT_NAVIGATED_EVENT:
-          fragment_navigated_events.append(data)
+            fragment_navigated_events.append(data)
         if method == HISTORY_UPDATED_EVENT:
-           history_updated_events.append(data)
+            history_updated_events.append(data)
         if method == CREATED_EVENT:
-           browsing_context_created_events.append(data)
+            browsing_context_created_events.append(data)
 
-
-    remove_fragment_navigated_listener = bidi_session.add_event_listener(FRAGMENT_NAVIGATED_EVENT, on_event)
-    remove_history_updated_listener = bidi_session.add_event_listener(HISTORY_UPDATED_EVENT, on_event)
+    remove_fragment_navigated_listener = bidi_session.add_event_listener(
+        FRAGMENT_NAVIGATED_EVENT, on_event
+    )
+    remove_history_updated_listener = bidi_session.add_event_listener(
+        HISTORY_UPDATED_EVENT, on_event
+    )
     remove_created_listener = bidi_session.add_event_listener(CREATED_EVENT, on_event)
 
     try:
-      await bidi_session.script.call_function(
-          raw_result=True,
-          function_declaration="""() => {
+        await bidi_session.script.call_function(
+            raw_result=True,
+            function_declaration="""() => {
               const frame = document.createElement("iframe");
               document.body.append(frame);
               frame.contentDocument.open();
@@ -185,49 +202,44 @@ async def test_history_document_open(
                 window.location.hash = "heya";
               });
           }""",
-          await_promise=True,
-          target=ContextTarget(target_context),
-      )
+            await_promise=True,
+            target=ContextTarget(target_context),
+        )
 
-      recursive_compare(
-          [{
-              'url': 'about:blank'
-          }],
-          browsing_context_created_events
-      )
+        recursive_compare([{"url": "about:blank"}], browsing_context_created_events)
 
-      recursive_compare(
-          [{
-              'context': target_context,
-              'url': target_url + '#heya'
-          }],
-          fragment_navigated_events
-      )
+        recursive_compare(
+            [{"context": target_context, "url": target_url + "#heya"}],
+            fragment_navigated_events,
+        )
 
-      # History updated URL should match the target_context's URL
-      # without the fragment per
-      # https://html.spec.whatwg.org/#document-open-steps step 12.2.
-      recursive_compare(
-          [{
-          # This event is for the first document.open before setting the
-          # location hash, per spec it should be set to the parent's document
-          # url.
-              'context': browsing_context_created_events[0]['context'],
-              'url': target_url
-          },{
-          # This is for the second document.open, after setting the hash.
-          # Again this should be set to target_url since the fragment should not
-          # be included.
-              'context': browsing_context_created_events[0]['context'],
-              'url': target_url
-          }],
-          history_updated_events
-      )
+        # History updated URL should match the target_context's URL
+        # without the fragment per
+        # https://html.spec.whatwg.org/#document-open-steps step 12.2.
+        recursive_compare(
+            [
+                {
+                    # This event is for the first document.open before setting the
+                    # location hash, per spec it should be set to the parent's document
+                    # url.
+                    "context": browsing_context_created_events[0]["context"],
+                    "url": target_url,
+                },
+                {
+                    # This is for the second document.open, after setting the hash.
+                    # Again this should be set to target_url since the fragment should not
+                    # be included.
+                    "context": browsing_context_created_events[0]["context"],
+                    "url": target_url,
+                },
+            ],
+            history_updated_events,
+        )
 
     finally:
-       remove_fragment_navigated_listener()
-       remove_history_updated_listener()
-       remove_created_listener()
+        remove_fragment_navigated_listener()
+        remove_history_updated_listener()
+        remove_created_listener()
 
 
 async def test_history_back_forward(

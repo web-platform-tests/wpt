3865a2480e82b40ee940a918b9d197cbd58398c6
Maksim Sadym <sadym@chromium.org>
2025-07-02 16:44:30 +0200

    [wdspec] Extend `browser.createUserContext` with `unhandledPromptBehavior` parameter
    Spec PR: https://github.com/w3c/webdriver-bidi/pull/896

---
+++ webdriver/tests/bidi/browser/create_user_context/unhandled_prompt_behavior.py
@@ -0,0 +1,96 @@
+import asyncio
+import pytest
+
+from webdriver.bidi.modules.script import ContextTarget
+from ... import recursive_compare
+
+pytestmark = pytest.mark.asyncio
+
+USER_PROMPT_OPENED_EVENT = "browsingContext.userPromptOpened"
+
+
+@pytest.mark.parametrize("handle", ["accept", "dismiss", "ignore"])
+async def test_beforeunload(bidi_session, subscribe_events, wait_for_event,
+        wait_for_future_safe, create_user_context, handle,
+        setup_beforeunload_page, inline):
+    user_context = await create_user_context(
+        unhandled_prompt_behavior={"beforeUnload": handle})
+    new_tab = await bidi_session.browsing_context.create(
+        type_hint="tab",
+        user_context=user_context
+    )
+
+    await setup_beforeunload_page(new_tab)
+
+    await subscribe_events(events=[USER_PROMPT_OPENED_EVENT])
+    on_entry = wait_for_event(USER_PROMPT_OPENED_EVENT)
+
+    url_after = inline("<div>foo</div>")
+
+    asyncio.create_task(
+        bidi_session.browsing_context.navigate(context=new_tab["context"],
+                                               url=url_after, wait="none"))
+
+    # Wait for prompt to appear.
+    resp = await wait_for_future_safe(on_entry)
+    recursive_compare({
+        'context': new_tab["context"],
+        'handler': handle,
+        'type': "beforeunload",
+    }, resp)
+
+
+@pytest.mark.parametrize("handle", ["accept", "dismiss", "ignore"])
+@pytest.mark.parametrize("prompt", ["alert", "confirm", "prompt", "default"])
+async def test_js_handle(bidi_session, subscribe_events, wait_for_event,
+        wait_for_future_safe, create_user_context, prompt, handle):
+    script = prompt if (prompt is not None and prompt != 'default') else 'alert'
+    user_context = await create_user_context(
+        unhandled_prompt_behavior={prompt: handle})
+    new_tab = await bidi_session.browsing_context.create(
+        type_hint="tab",
+        user_context=user_context
+    )
+
+    await subscribe_events([USER_PROMPT_OPENED_EVENT],
+                           contexts=[new_tab["context"]])
+    on_entry = wait_for_event(USER_PROMPT_OPENED_EVENT)
+
+    # Schedule script opening prompt but don't wait for the result.
+    asyncio.create_task(
+        bidi_session.script.evaluate(
+            expression=f"window.{script}('some message')",
+            target=ContextTarget(new_tab["context"]),
+            await_promise=False,
+        )
+    )
+
+    # Default behavior for js prompts is `dismiss`.
+    expected_handle = handle if handle is not None \
+        else 'dismiss'
+
+    # Wait for prompt to appear.
+    resp = await wait_for_future_safe(on_entry)
+    recursive_compare({
+        'context': new_tab["context"],
+        'handler': expected_handle,
+        'type': script,
+    }, resp)
+
+
+@pytest.mark.parametrize("handle", ["accept", "dismiss", "ignore"])
+async def test_file(bidi_session, create_user_context, handle,
+        assert_file_dialog_not_canceled, assert_file_dialog_canceled):
+    user_context = await create_user_context(
+        unhandled_prompt_behavior={"file": handle})
+
+    new_tab = await bidi_session.browsing_context.create(
+        type_hint="tab",
+        user_context=user_context
+    )
+
+    # Unless explicitly set to `ignore`, the file behavior is `dismiss`.
+    if handle == 'ignore':
+        assert_file_dialog_not_canceled(new_tab)
+    else:
+        assert_file_dialog_canceled(new_tab)

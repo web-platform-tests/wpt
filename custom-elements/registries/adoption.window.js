b699769278d2c4a373148583a18bb5d3b6bc7933
Anne van Kesteren <annevk@annevk.nl>
2025-07-16 16:30:28 +0200

    DOM: adopting and cloning with registries

---
+++ custom-elements/registries/adoption.window.js
@@ -0,0 +1,155 @@
+test(t => {
+  const contentDocument = document.body.appendChild(document.createElement('iframe')).contentDocument;
+  t.add_cleanup(() => contentDocument.defaultView.frameElement.remove());
+  const element = document.createElement('div');
+  assert_equals(element.customElementRegistry, customElements);
+
+  contentDocument.body.appendChild(element);
+  assert_equals(element.customElementRegistry, contentDocument.defaultView.customElements);
+}, "Adoption with global registry");
+
+test(t => {
+  const contentDocument = document.body.appendChild(document.createElement('iframe')).contentDocument;
+  t.add_cleanup(() => contentDocument.defaultView.frameElement.remove());
+  const element = document.createElement('div', { customElementRegistry: customElements });
+  assert_equals(element.customElementRegistry, customElements);
+
+  contentDocument.body.appendChild(element);
+  assert_equals(element.customElementRegistry, contentDocument.defaultView.customElements);
+}, "Adoption with explicit global registry");
+
+test(t => {
+  const contentDocument = document.body.appendChild(document.createElement('iframe')).contentDocument;
+  t.add_cleanup(() => contentDocument.defaultView.frameElement.remove());
+  const scoped = new CustomElementRegistry();
+  const element = document.createElement('div', { customElementRegistry: scoped });
+  assert_equals(element.customElementRegistry, scoped);
+
+  contentDocument.body.appendChild(element);
+  assert_equals(element.customElementRegistry, scoped);
+}, "Adoption with scoped registry");
+
+test(t => {
+  const contentDocument = document.body.appendChild(document.createElement('iframe')).contentDocument;
+  t.add_cleanup(() => contentDocument.defaultView.frameElement.remove());
+  const element = document.createElement('div');
+  assert_equals(element.customElementRegistry, customElements);
+
+  const scoped = new CustomElementRegistry();
+  const scopedElement = contentDocument.createElement('div', { customElementRegistry: scoped });
+  contentDocument.body.appendChild(scopedElement);
+  assert_equals(scopedElement.customElementRegistry, scoped);
+  scopedElement.appendChild(element);
+  assert_equals(element.customElementRegistry, contentDocument.defaultView.customElements);
+}, "Adoption with global registry into a scoped registry");
+
+test(t => {
+  const contentDocument = document.body.appendChild(document.createElement('iframe')).contentDocument;
+  t.add_cleanup(() => contentDocument.defaultView.frameElement.remove());
+  const element = document.createElement('div', { customElementRegistry: customElements });
+  assert_equals(element.customElementRegistry, customElements);
+
+  const scoped = new CustomElementRegistry();
+  const scopedElement = contentDocument.createElement('div', { customElementRegistry: scoped });
+  contentDocument.body.appendChild(scopedElement);
+  assert_equals(scopedElement.customElementRegistry, scoped);
+  scopedElement.appendChild(element);
+  assert_equals(element.customElementRegistry, contentDocument.defaultView.customElements);
+}, "Adoption with explicit global registry into a scoped registry");
+
+test(t => {
+  const contentDocument = document.body.appendChild(document.createElement('iframe')).contentDocument;
+  t.add_cleanup(() => contentDocument.defaultView.frameElement.remove());
+  const scoped = new CustomElementRegistry();
+  const element = document.createElement('div', { customElementRegistry: scoped });
+  assert_equals(element.customElementRegistry, scoped);
+
+  const scoped2 = new CustomElementRegistry();
+  const scopedElement = contentDocument.createElement('div', { customElementRegistry: scoped2 });
+  contentDocument.body.appendChild(scopedElement);
+  assert_equals(scopedElement.customElementRegistry, scoped2);
+  scopedElement.appendChild(element);
+  assert_equals(element.customElementRegistry, scoped);
+}, "Adoption with scoped registry into a scoped registry");
+
+test(t => {
+  const contentDocument = document.body.appendChild(document.createElement('iframe')).contentDocument;
+  t.add_cleanup(() => contentDocument.defaultView.frameElement.remove());
+  const element = document.createElement('div');
+  const elementShadow = element.attachShadow({ mode: "closed" });
+  assert_equals(elementShadow.customElementRegistry, customElements);
+
+  contentDocument.body.appendChild(element);
+  // In certain implementations touching element.customElementRegistry can poison the results so we
+  // don't do that here.
+  assert_equals(elementShadow.customElementRegistry, contentDocument.defaultView.customElements);
+}, "Adoption including shadow root with global registry");
+
+test(t => {
+  const contentDocument = document.body.appendChild(document.createElement('iframe')).contentDocument;
+  t.add_cleanup(() => contentDocument.defaultView.frameElement.remove());
+  const element = document.createElement('div');
+  const elementShadow = element.attachShadow({ mode: "closed", customElementRegistry: customElements });
+  assert_equals(elementShadow.customElementRegistry, customElements);
+
+  contentDocument.body.appendChild(element);
+  assert_equals(elementShadow.customElementRegistry, contentDocument.defaultView.customElements);
+}, "Adoption including shadow root with explicit global registry");
+
+test(t => {
+  const contentDocument = document.body.appendChild(document.createElement('iframe')).contentDocument;
+  t.add_cleanup(() => contentDocument.defaultView.frameElement.remove());
+  const element = document.createElement('div');
+  const scoped = new CustomElementRegistry();
+  const elementShadow = element.attachShadow({ mode: "closed", customElementRegistry: scoped });
+  assert_equals(elementShadow.customElementRegistry, customElements);
+
+  contentDocument.body.appendChild(element);
+  assert_equals(elementShadow.customElementRegistry, scoped);
+}, "Adoption including shadow root with scoped registry");
+
+test(t => {
+  const contentDocument = document.body.appendChild(document.createElement('iframe')).contentDocument;
+  t.add_cleanup(() => contentDocument.defaultView.frameElement.remove());
+  const element = document.createElement('div');
+  const elementShadow = element.attachShadow({ mode: "closed" });
+  assert_equals(elementShadow.customElementRegistry, customElements);
+
+  const scoped = new CustomElementRegistry();
+  const scopedElement = contentDocument.createElement('div', { customElementRegistry: scoped });
+  contentDocument.body.appendChild(scopedElement);
+  assert_equals(scopedElement.customElementRegistry, scoped);
+  scopedElement.appendChild(element);
+  assert_equals(elementShadow.customElementRegistry, contentDocument.defaultView.customElements);
+}, "Adoption including shadow root with global registry into a scoped registry");
+
+test(t => {
+  const contentDocument = document.body.appendChild(document.createElement('iframe')).contentDocument;
+  t.add_cleanup(() => contentDocument.defaultView.frameElement.remove());
+  const element = document.createElement('div');
+  const elementShadow = element.attachShadow({ mode: "closed", customElementRegistry: customElements });
+  assert_equals(elementShadow.customElementRegistry, customElements);
+
+  const scoped = new CustomElementRegistry();
+  const scopedElement = contentDocument.createElement('div', { customElementRegistry: scoped });
+  contentDocument.body.appendChild(scopedElement);
+  assert_equals(scopedElement.customElementRegistry, scoped);
+  scopedElement.appendChild(element);
+  assert_equals(elementShadow.customElementRegistry, contentDocument.defaultView.customElements);
+}, "Adoption including shadow root with explicit global registry into a scoped registry");
+
+test(t => {
+  const contentDocument = document.body.appendChild(document.createElement('iframe')).contentDocument;
+  t.add_cleanup(() => contentDocument.defaultView.frameElement.remove());
+  const element = document.createElement('div');
+  const scoped = new CustomElementRegistry();
+  const elementShadow = element.attachShadow({ mode: "closed", customElementRegistry: scoped });
+  assert_equals(elementShadow.customElementRegistry, customElements);
+
+  const scoped2 = new CustomElementRegistry();
+  const scopedElement = contentDocument.createElement('div', { customElementRegistry: scoped2 });
+  contentDocument.body.appendChild(scopedElement);
+  assert_equals(scopedElement.customElementRegistry, scoped2);
+  scopedElement.appendChild(element);
+  assert_equals(elementShadow.customElementRegistry, scoped);
+}, "Adoption including shadow root with scoped registry into a scoped registry");

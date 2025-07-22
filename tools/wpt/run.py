7bc6b09e1e5cc2dff419defb0bd5896ad0ee04dd
Ashwin Naren <arihant2math@gmail.com>
2025-07-22 06:54:12 +0000

    update wpt expectations

--- tools/wpt/run.py
+++ tools/wpt/run.py
@@ -10,6 +10,8 @@ from typing import ClassVar, Tuple, Type
 
 wpt_root = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
 sys.path.insert(0, os.path.abspath(os.path.join(wpt_root, "tools")))
+print(os.path.abspath(os.path.join(wpt_root, "tools")))
+print(sys.path)
 
 from . import browser, install, testfiles
 from ..serve import serve

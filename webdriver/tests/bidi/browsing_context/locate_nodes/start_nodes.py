938eced106c4790792136c4a182ebff1b7372888
Alex Rudenko <alexrudenko@chromium.org>
2025-07-30 17:37:19 +0200

    [wdspec] add a test for using svg elements as start nodes in locateNodes

--- webdriver/tests/bidi/browsing_context/locate_nodes/start_nodes.py
+++ webdriver/tests/bidi/browsing_context/locate_nodes/start_nodes.py
@@ -276,3 +276,45 @@ async def test_locate_with_document_context_node(bidi_session, inline, top_conte
     ]
 
     recursive_compare(expected, result["nodes"])
+
+
+@pytest.mark.parametrize("type,value", [
+    ("css", "circle"),
+])
+@pytest.mark.asyncio
+async def test_locate_with_svg_context_node(bidi_session, inline, top_context, type, value):
+    url = inline("""
+      <svg viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="5" cy="5" r="5" />
+      </svg>
+    """)
+    await bidi_session.browsing_context.navigate(
+        context=top_context["context"], url=url, wait="complete"
+    )
+
+    context_node = await bidi_session.script.evaluate(
+        expression="document.querySelector('svg')",
+        target=ContextTarget(top_context["context"]),
+        await_promise=True,
+    )
+
+    result = await bidi_session.browsing_context.locate_nodes(
+        context=top_context["context"],
+        locator={ "type": type, "value": value },
+        start_nodes=[context_node]
+    )
+
+    expected = [
+        {
+            "type": "node",
+            "sharedId": any_string,
+            "value": {
+                "childNodeCount": 0,
+                "localName": "circle",
+                "namespaceURI": "http://www.w3.org/2000/svg",
+                "nodeType": 1,
+            }
+        }
+    ]
+
+    recursive_compare(expected, result["nodes"])

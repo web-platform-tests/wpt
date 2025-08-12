1bdd8a97a3d6fb876c53ec17f0b1379355098bfc
Anne van Kesteren <annevk@annevk.nl>
2025-08-12 18:14:31 +0200

    match Chrome behavior

--- cookiestore/cookieStore_set_path.https.window.js
+++ cookiestore/cookieStore_set_path.https.window.js
@@ -5,13 +5,19 @@
 promise_test(async testCase => {
   const currentUrl = new URL(self.location.href);
   const currentPath = currentUrl.pathname;
-  await cookieStore.delete({ name: 'cookie-name', path: currentPath });
+  const currentDirectory = currentPath.substr(0, currentPath.lastIndexOf('/'));
 
   await cookieStore.set({ name: 'cookie-name', value: 'cookie-value', path: '' });
   testCase.add_cleanup(async () => {
-    await cookieStore.delete({ name: 'cookie-name', path: currentPath });
+    await cookieStore.delete({ name: 'cookie-name', path: currentDirectory });
   });
 
   const internalCookie = await test_driver.get_named_cookie('cookie-name');
-  assert_equals(internalCookie.path, currentPath);
+  assert_equals(internalCookie.path, currentDirectory);
 }, 'CookieListItem - cookieStore.set with empty string path defaults to current URL');
+
+promise_test(async testCase => {
+  const currentUrl = new URL(self.location.href);
+  const currentPath = currentUrl.pathname;
+  return promise_rejects_js(testCase, TypeError, cookieStore.set({ name: '__host-cookie-name', value: 'cookie-value', path: '' }));
+}, 'CookieListItem - cookieStore.set with empty string path defaults to current URL with __host- prefix');

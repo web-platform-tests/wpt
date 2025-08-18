42e22bf72e44a9bd03f026f085a46c998bcc2408
Maksim Sadym <sadym@chromium.org>
2025-08-18 13:55:14 +0200

    introduce `Nullable` type

--- tools/webdriver/webdriver/bidi/undefined.py
+++ tools/webdriver/webdriver/bidi/undefined.py
@@ -5,7 +5,7 @@ from typing import TypeVar, Union
 
 class Undefined(Enum):
     """
-    Class representin special value that indicates that a property is notset.
+    Class representing special value that indicates that a property is notset.
     """
 
     UNDEFINED = enum.auto()
@@ -19,3 +19,7 @@ T = TypeVar("T")
 #: A type hint for a value that can be of a specific type or UNDEFINED.
 #: For example, ``Maybe[str]`` is equivalent to ``Union[str, Undefined]``.
 Maybe = Union[T, Undefined]
+
+#: A type hint which can have protocol values `null`. Intended to be used
+# instead of `Optional` to avoid confusion.
+Nullable = Union[T, None]

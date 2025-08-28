431f09e396fd6852d58e1cef49fcd6c4f4bdcb75
Alexandra Borovova <aborovova@mozilla.com>
2025-08-28 15:10:43 +0000

    [wdspec] Add test for setting a timezone override on the page with iframes.
    Differential Revision: https://phabricator.services.mozilla.com/D259032
    
    bugzilla-url: https://bugzilla.mozilla.org/show_bug.cgi?id=1978027
    gecko-commit: 5ce0cb0faba4e7186eb179cc8b42075983ab3b80
    gecko-reviewers: webdriver-reviewers, jdescottes

--- webdriver/tests/bidi/emulation/set_timezone_override/contexts.py
+++ webdriver/tests/bidi/emulation/set_timezone_override/contexts.py
@@ -48,3 +48,47 @@ async def test_multiple_contexts(bidi_session, new_tab, get_current_timezone,
     # Assert the timezone is restored to the initial one.
     assert await get_current_timezone(new_tab) == default_timezone
     assert await get_current_timezone(new_context) == default_timezone
+
+
+@pytest.mark.parametrize("domain", ["", "alt"], ids=["same_origin", "cross_origin"])
+async def test_iframe(
+    bidi_session,
+    new_tab,
+    domain,
+    inline,
+    get_current_timezone,
+    some_timezone,
+    another_timezone
+):
+    # Set timezone override.
+    await bidi_session.emulation.set_timezone_override(
+        contexts=[new_tab["context"]],
+        timezone=some_timezone
+    )
+
+    # Assert timezone emulated in the required context.
+    assert await get_current_timezone(new_tab) == some_timezone
+
+    iframe_url = inline("<div id='in-iframe'>foo</div>", domain=domain)
+    page_url = inline(f"<iframe src='{iframe_url}'></iframe>")
+
+    # Load the page with iframes.
+    await bidi_session.browsing_context.navigate(
+        context=new_tab["context"],
+        url=page_url,
+        wait="complete",
+    )
+
+    contexts = await bidi_session.browsing_context.get_tree(root=new_tab["context"])
+    iframe = contexts[0]["children"][0]
+
+    # Assert timezone is emulated in the iframe context.
+    assert await get_current_timezone(iframe) == some_timezone
+
+    # Set another timezone override.
+    await bidi_session.emulation.set_timezone_override(
+        contexts=[new_tab["context"]], timezone=another_timezone
+    )
+
+    # Assert timezone is emulated in the iframe context.
+    assert await get_current_timezone(iframe) == another_timezone

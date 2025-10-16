468f251c4ea84cd24946af66f4d7047daffa7352
Maksim Sadym <sadym@chromium.org>
2025-10-16 17:36:50 +0200

    [wdspec] tentative tests for disabled scripting emulation

---
+++ webdriver/tests/bidi/emulation/set_scripting_enabled/enabled_tentative.py
@@ -0,0 +1,110 @@
+import pytest
+
+from webdriver.bidi.modules.script import ContextTarget
+
+pytestmark = pytest.mark.asyncio
+
+
+async def test_node_event_handler_value(bidi_session, inline):
+    context = await bidi_session.browsing_context.create(type_hint="tab")
+    url = inline("""
+        <script>window.document.onload = ()=>{console.log('onload')}</script>
+        """)
+    await bidi_session.browsing_context.navigate(context=context["context"],
+                                                 url=url, wait="complete")
+
+    result = await bidi_session.script.evaluate(
+        expression="window.document.onload",
+        target=ContextTarget(context["context"]),
+        await_promise=False,
+    )
+    assert result == {'type': 'function'}
+
+    # Disable scripting.
+    await bidi_session.emulation.set_scripting_enabled(
+        enabled=False,
+        contexts=[context["context"]],
+    )
+
+    result = await bidi_session.script.evaluate(
+        expression="window.document.onload",
+        target=ContextTarget(context["context"]),
+        await_promise=False,
+    )
+    assert result == {'type': 'function'}
+
+
+async def test_finalization_registry(bidi_session, inline):
+    """
+    The test relies on `window.gc()` to force GC. In Chrome, it is exposed when
+    run with `--js-flags=--expose-gc` flag.
+    """
+    context = await bidi_session.browsing_context.create(type_hint="tab")
+
+    url = inline("""<script>
+            window.finalizationRegistryTriggered = false;
+            const registry = new FinalizationRegistry((heldValue) => {
+                window.finalizationRegistryTriggered = true;
+            });
+
+            function createAndRegister() {
+                const myObject = {};
+                registry.register(myObject);
+            }
+            createAndRegister();
+        </script>
+        """)
+
+    await bidi_session.browsing_context.navigate(context=context["context"],
+                                                 url=url, wait="complete")
+
+    result = await bidi_session.script.evaluate(
+        expression="window.finalizationRegistryTriggered",
+        target=ContextTarget(context["context"]),
+        await_promise=False,
+    )
+    assert result == {'type': 'boolean', 'value': False}
+
+    # Disable scripting.
+    await bidi_session.emulation.set_scripting_enabled(
+        enabled=False,
+        contexts=[context["context"]],
+    )
+
+    result = await bidi_session.script.evaluate(
+        expression="window.finalizationRegistryTriggered",
+        target=ContextTarget(context["context"]),
+        await_promise=False,
+    )
+    assert result == {'type': 'boolean', 'value': False}
+
+    # Force GC to collect `myObject`.
+    await bidi_session.script.evaluate(
+        expression="window.gc()",
+        target=ContextTarget(context["context"]),
+        await_promise=False,
+    )
+
+    result = await bidi_session.script.evaluate(
+        expression="window.finalizationRegistryTriggered",
+        target=ContextTarget(context["context"]),
+        await_promise=False,
+    )
+    assert result == {'type': 'boolean', 'value': True}
+
+
+async def test_enqueue_promise_job(bidi_session, inline):
+    context = await bidi_session.browsing_context.create(type_hint="tab")
+
+    # Disable scripting.
+    await bidi_session.emulation.set_scripting_enabled(
+        enabled=False,
+        contexts=[context["context"]],
+    )
+
+    result = await bidi_session.script.evaluate(
+        expression="Promise.resolve(true).then(x=>x)",
+        target=ContextTarget(context["context"]),
+        await_promise=True,
+    )
+    assert result == {'type': 'boolean', 'value': True}

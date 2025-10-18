fcbd69022b9f425d03bddb2bf6a0954678c739f1
Tim Nguyen <nt1m@users.noreply.github.com>
2025-10-17 17:53:57 -0700

    Update META variant exclusions and inclusions

--- webrtc-encoded-transform/idlharness.https.window.js
+++ webrtc-encoded-transform/idlharness.https.window.js
@@ -1,5 +1,5 @@
-// META: variant=?exclude=(SFrameEncrypterStream.*|SFrameTransform.*)
-// META: variant=?include=(SFrameEncrypterStream.*|SFrameTransform.*)
+// META: variant=?exclude=(SFrameEncrypterStream|SFrameDecrypterStream|SFrameTransform.*)
+// META: variant=?include=(SFrameEncrypterStream|SFrameDecrypterStream|SFrameTransform.*)
 // META: script=/common/subset-tests-by-key.js
 // META: script=/resources/WebIDLParser.js
 // META: script=/resources/idlharness.js

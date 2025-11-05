8654220313d6df622fe15c4711e932a52c8c8ce3
Narcis Beleuzu <nbeleuzu@mozilla.com>
2025-11-05 20:34:29 +0000

    Revert "Bug 1360870, 1989386, 1990925, 1995558: apply code formatting via Lando" for causing the macOS top crash in Bug 1998332
    This reverts commit fdacf289d5a725153e0d9b024542f65219cbc9d6.
    
    This reverts commit 5f838f6b5a52b75fe3b6bede19f0357ffe7cf215.
    
    This reverts commit a9ff68075ff9de39b25b04b8fdcc5b8fb197318d.
    
    This reverts commit 92013428c4f8c2afd0cb7c77cb4bf3ba44749c50.
    
    This reverts commit 62795c77d763101812f1b32416342c95510077df.
    
    This reverts commit 9e90e5f01e3375672d6cec20810500d812d51eb5.
    
    This reverts commit f8438fb258f7e16f7e3ed5ae2ef6da40737828f7.
    
    This reverts commit f7f43476a276d58d1841c7df990287e2712bc53e.
    
    gecko-commit: 9a81a01d61b6342b237a6431865eef20ee487c20

--- service-workers/service-worker/resources/registration-tests-script.js
+++ service-workers/service-worker/resources/registration-tests-script.js
@@ -30,21 +30,21 @@ function registration_tests_script(register_method, type) {
 
   promise_test(function(t) {
       var script = 'resources/malformed-worker.py?undefined-error';
-      var scope = `resources/scope/undefined-error/${type}`;
+      var scope = 'resources/scope/undefined-error';
       return promise_rejects_js(t,
           TypeError,
           register_method(script, {scope: scope}),
           'Registration of script including undefined error should fail.');
-    }, `Registering script including undefined error: ${type}`);
+    }, 'Registering script including undefined error');
 
   promise_test(function(t) {
       var script = 'resources/malformed-worker.py?uncaught-exception';
-      var scope = `resources/scope/uncaught-exception/${type}`;
+      var scope = 'resources/scope/uncaught-exception';
       return promise_rejects_js(t,
           TypeError,
           register_method(script, {scope: scope}),
           'Registration of script including uncaught exception should fail.');
-    }, `Registering script including uncaught exception: ${type}`);
+    }, 'Registering script including uncaught exception');
 
   if (type === 'classic') {
     promise_test(function(t) {

6be6de0b82a9edbce46d792bd329711982092252
Simon Pieters <zcorpan@gmail.com>
2025-12-09 22:02:38 +0100

    Fixup

--- service-workers/service-worker/resources/ancestorOrigins.js
+++ service-workers/service-worker/resources/ancestorOrigins.js
@@ -1,4 +1,4 @@
 self.onmessage = async (evt) => {
-  const client = await clients.get(evt.clientId):
+  const client = await clients.get(evt.clientId);
   client.postMessage({ ancestorOrigins: client.ancestorOrigins });
 };

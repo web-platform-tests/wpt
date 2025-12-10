062b75a6567fb77a70e20d1abb4def7cc95ea05c
Jonathan Lee <jonathan-j-lee@users.noreply.github.com>
2025-12-10 10:04:08 -0800

    [wptrunner] Restore initial test window focus for WebDriver (#56508)
    This short-term workaround restores the WebDriver testharness executor
    behavior prior to web-platform-tests/wpt@bef892c489 to fix broken tests.
    
    https://github.com/web-platform-tests/rfcs/issues/231 is open to codify
    the desired behavior.

--- tools/wptrunner/wptrunner/executors/executorwebdriver.py
+++ tools/wptrunner/wptrunner/executors/executorwebdriver.py
@@ -1275,6 +1275,16 @@ class WebDriverTestharnessExecutor(TestharnessExecutor):
         protocol.base.set_window(test_window)
         # Wait until about:blank has been loaded
         protocol.base.execute_script(self.window_loaded_script, asynchronous=True)
+
+        # Move focus to the test window. This should not be fallible because the
+        # document will always be the initial about:blank, and thus there is no
+        # content in the page to obscure the centre point of the root element.
+        #
+        # TODO(web-platform-tests/rfcs#231): This is only a de facto assumption.
+        # Finalize what the desired behavior should be.
+        selector = protocol.base.execute_script('return document.documentElement;')
+        protocol.click.element(selector)
+
         return test_window
 
 

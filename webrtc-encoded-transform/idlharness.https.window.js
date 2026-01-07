5d759d9e049bfe8e47c4f75dfa7d0188c27afdcb
Tim Nguyen <nt1m@users.noreply.github.com>
2026-01-07 12:34:34 +0900

    Make variants excluding SFrameSenderTransform & SFrameReceiverTransform from Interop 2025

--- webrtc-encoded-transform/idlharness.https.window.js
+++ webrtc-encoded-transform/idlharness.https.window.js
@@ -1,5 +1,5 @@
-// META: variant=?exclude=(SFrameDecrypterStream|SFrameEncrypterStream|SFrameTransform.*)
-// META: variant=?include=(SFrameDecrypterStream|SFrameEncrypterStream|SFrameTransform.*)
+// META: variant=?exclude=(SFrameDecrypterStream|SFrameEncrypterStream|SFrameSenderTransform|SFrameReceiverTransform.*)
+// META: variant=?include=(SFrameDecrypterStream|SFrameEncrypterStream|SFrameSenderTransform|SFrameReceiverTransform.*)
 // META: script=/common/subset-tests-by-key.js
 // META: script=/resources/WebIDLParser.js
 // META: script=/resources/idlharness.js

05d3c7c3ed6ac06d57dc3f656877ee426f072516
Scott Jehl <214783+scottjehl@users.noreply.github.com>
2026-02-12 12:51:51 -0500

    Change HTML Video and Audio Lazy Loading Tests from Tentative to Non-Tentative

--- html/dom/elements-embedded.js
+++ html/dom/elements-embedded.js
@@ -94,9 +94,8 @@ var embeddedElements = {
     loop: "boolean",
     controls: "boolean",
     controlsList: {type: "tokenlist", domAttrName: "controlsList"},
-    defaultMuted: {type: "boolean", domAttrName: "muted"}/*,
-    // Note: once media loading attr is not tentative, uncomment and delete html reflection tests it replaces.
-    loading: {type: "enum", keywords: ["lazy", "eager"], defaultVal: "eager", invalidVal: "eager"}*/
+    defaultMuted: {type: "boolean", domAttrName: "muted"},
+    loading: {type: "enum", keywords: ["lazy", "eager"], defaultVal: "eager", invalidVal: "eager"},
 
     width: "unsigned long",
     height: "unsigned long",
@@ -112,9 +111,8 @@ var embeddedElements = {
     autoplay: "boolean",
     loop: "boolean",
     controls: "boolean",
-    defaultMuted: {type: "boolean", domAttrName: "muted"}/*,
-    // Note: once media loading attr is not tentative, uncomment and delete html reflection tests it replaces.
-    loading: {type: "enum", keywords: ["lazy", "eager"], defaultVal: "eager", invalidVal: "eager"}*/
+    defaultMuted: {type: "boolean", domAttrName: "muted"},
+    loading: {type: "enum", keywords: ["lazy", "eager"], defaultVal: "eager", invalidVal: "eager"}
   },
   source: {
     src: "url",

a2f503744a51935553cce7ce1a5af1e07a6317f2
Morten Stenshorne <mstensho@chromium.org>
2026-03-05 10:28:42 +0100

    Typo. Since I don't know how to run this locally, here's more fun for you bots.

--- tools/manifest/tests/test_sourcefile.py
+++ tools/manifest/tests/test_sourcefile.py
@@ -973,30 +973,30 @@ def test_page_ranges_invalid(page_ranges):
         s.page_ranges
 
 
-@pytest.mark.parametrize("safe_printable_area, expected", [
+@pytest.mark.parametrize("safe_printable_inset, expected", [
     (b"0", 0),
     (b"0.5", 0.5),
     (b"2.54", 2.54),
     (b"100", 100)])
-def test_safe_printable_area(safe_printable_area, expected):
+def test_safe_printable_inset(safe_printable_inset, expected):
     content = b"""<link rel=match href=ref.html>
 <meta name=reftest-pages content="%s">
-""" % safe_printable_area
+""" % safe_printable_inset
 
     s = create("foo/test-print.html", content)
 
-    assert s.safe_printable_area == {"/foo/test-print.html": expected}
+    assert s.safe_printable_inset == {"/foo/test-print.html": expected}
 
 
-@pytest.mark.parametrize("safe_printable_area", [b"banana", b"auto", b"-1", b"0,0"])
-def test_safe_printable_area_invalid(safe_printable_area):
+@pytest.mark.parametrize("safe_printable_inset", [b"banana", b"auto", b"-1", b"0,0"])
+def test_safe_printable_inset_invalid(safe_printable_inset):
     content = b"""<link rel=match href=ref.html>
 <meta name=reftest-pages content="%s">
-""" % safe_printable_area
+""" % safe_printable_inset
 
     s = create("foo/test-print.html", content)
     with pytest.raises(ValueError):
-        s.safe_printable_area
+        s.safe_printable_inset
 
 
 def test_hash():

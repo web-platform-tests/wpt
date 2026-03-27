9f7b1ad49696903d35930b72d760bbb0224f12de
James Scott <jamescscott@google.com>
2026-03-27 21:56:03 +0000

    tidy up

--- preload/resources/echo-preload-header.py
+++ preload/resources/echo-preload-header.py
@@ -10,15 +10,23 @@ def main(request, response):
         response.headers.set(b"Link", link)
 
     if b"file" in request.GET:
-        base_dir = os.path.abspath(os.path.dirname(isomorphic_encode(__file__)))
+        base_dir = os.path.realpath(os.path.dirname(isomorphic_encode(__file__)))
         target_file = request.GET.first(b"file")
-        path = os.path.abspath(os.path.join(base_dir, target_file))
-        if os.path.commonpath([base_dir, path]) != base_dir:
+        path = os.path.realpath(os.path.join(base_dir, target_file))
+
+        try:
+            if os.path.commonpath([base_dir, path]) != base_dir:
+                raise ValueError("Path traversal attempt detected.")
+        except ValueError:
+            # A ValueError is expected in two scenarios:
+            # 1) os.path.commonpath raises it if paths are on different drives
+            #    (e.g., on Windows).
+            # 2) We explicitly raise it when a path traversal attempt is detected.
             response.status = 403
             return b"Forbidden"
 
         try:
-            response.content = open(path, mode=u'rb').read()
+            response.content = open(path, mode='rb').read()
         except IOError:
             response.status = 404
             return b"Not found"

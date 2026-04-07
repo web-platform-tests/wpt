401342ec7941d61697b7e94e339caa62c2bab197
Valerie Young <spectranaut@igalia.com>
2026-04-07 14:17:39 -0700

    Delete unnecessary file

--- core-aam/aamtests/support/platform.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from sys import platform
-
-linux = pytest.mark.skipif(platform != "linux", reason="PRECONDITION_FAILED")
-
-mac = pytest.mark.skipif(platform != "darwin", reason="PRECONDITION_FAILED")
-
-windows = pytest.mark.skipif(platform != "win32", reason="PRECONDITION_FAILED")

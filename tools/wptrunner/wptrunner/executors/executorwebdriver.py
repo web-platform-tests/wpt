cc140ba84b3298227d73be752d6e6ca86e6a412c
Minju Kim <mkim@igalia.com>
2026-04-16 15:29:08 +0200

    Fix typing/style issues in executorwebdriver

--- tools/wptrunner/wptrunner/executors/executorwebdriver.py
+++ tools/wptrunner/wptrunner/executors/executorwebdriver.py
@@ -277,10 +277,11 @@ class WebDriverBidiBrowsingContextProtocolPart(BidiBrowsingContextProtocolPart):
                                  user_text: Optional[str] = None) -> None:
         await self.webdriver.bidi_session.browsing_context.handle_user_prompt(
             context=context, accept=accept, user_text=user_text)
+
     async def set_viewport(self,
                            context: Optional[str],
-                           viewport,
-                           device_pixel_ratio,
+                           viewport: Any,
+                           device_pixel_ratio: Any,
                            user_contexts: Optional[List[str]]) -> None:
         await self.webdriver.bidi_session.browsing_context.set_viewport(
             context=context,

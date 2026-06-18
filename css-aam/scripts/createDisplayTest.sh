58e9951d72b5be3c8807806d54d1748f5c110a71
Zoë Bijl <code@moiety.me>
2026-06-18 12:45:51 +0200

    add option to generate all tests

--- css-aam/scripts/createDisplayTest.sh
+++ css-aam/scripts/createDisplayTest.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-if [ $1 == "-h" ]
-then
-    echo "Usage:"
-    echo "./createDisplayTest.sh [-h] value(s)"
-    echo "./createDisplayTest.sh -h -- show help text"
-    echo "./createDisplayTest.sh \"block\" -- create test file for \"block\""
-    echo "./createDisplayTest.sh \"block\" \"inline-block\" \"block flow-root\" -- create test files for \"block\" \"inline-block\" and \"block flow-root\""
-    
-    exit 0
-fi
+knownvalues=("contents" "flow-root" "inline" "inline-block" "run-in" "list-item" "inline list-item" "flex" "inline-flex" "grid" "inline-grid" "ruby" "block ruby" "table" "inline-table")
 
 if [ $# -eq 0 ]
 then
@@ -17,9 +8,34 @@ then
     exit 1
 fi
 
-basefile="../display/display-block.html"
+if [ $1 == "-h" ]
+then
+    echo "Usage:"
+    echo "./createDisplayTest.sh [-h] [-a] value(s)"
+    echo "./createDisplayTest.sh -h -- show help text"
+    echo "./createDisplayTest.sh -a -- create test files for all known display values (except \"block\")."
+    echo "./createDisplayTest.sh \"block\" -- create test file for \"block\""
+    echo "./createDisplayTest.sh \"block\" \"inline-block\" \"block flow-root\" -- create test files for \"block\" \"inline-block\" and \"block flow-root\""
+    echo
+    echo "Known values:"
+    echo
+    for i in "${knownvalues[@]}"
+    do
+        echo "- \"${i}\""
+    done
+    
+    exit 0
+fi
 
-for i in "${@}"
+basefile="../display/display-block.html"
+values="${@}"
+
+if [[ $1 == "-a" || $1 == "--all" ]]
+then
+    values=("${knownvalues[@]}")
+fi
+
+for i in "${values[@]}"
 do
     # Escape the argument. Spaces become two dashes.
     # This is to prevent conflict with things like "inline-flex" and "inline flex"; both valid values.

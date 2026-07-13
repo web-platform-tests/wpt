c5b64ca15ce25a7c32e74262a7a28724a4567bdf
tannal <mtan@igalia.com>
2026-07-13 11:40:31 +0800

    Refactor the code, add missing element in the attribute test
    Added all mathml elements to `mathml-attributes.py`, sorted alphabetically.
    Removed tag attribute in `mathml-attributes.py`.
    Renamed key in the test data object.
    Remove ATK prefix.
    Added a separate test file for `<mmultiscripts>`

--- mathml-aam/aamtests/attribute/mathml-attributes.py
+++ mathml-aam/aamtests/attribute/mathml-attributes.py
@@ -4,136 +4,108 @@ import pytest
 TEST_DATA_ATTRIBUTES = {
     "annotation": {
         "html": "<math><semantics><mn>2</mn><annotation id='test' encoding='TeX' style='display: inline-block;'>\\text{{two}}</annotation></semantics></math>",
-        "tag": "annotation"
     },
     "annotation-xml": {
         "html": "<math><semantics><mn>2</mn><annotation-xml id='test' encoding='MathML-Presentation' style='display: inline-block;'><mtext>two</mtext></annotation-xml></semantics></math>",
-        "tag": "annotation-xml"
     },
     "maction": {
         "html": "<math><maction id='test' actiontype='toggle'><mn>1</mn><mn>2</mn></maction></math>",
-        "tag": "maction"
     },
     "math": {
         "html": "<math id='test'><mn>1</mn></math>",
-        "tag": "math"
     },
     "merror": {
         "html": "<math><merror id='test'><mtext>Divide by zero</mtext></merror></math>",
-        "tag": "merror"
     },
     "mfrac": {
         "html": "<math><mfrac id='test'><mn id='num'>1</mn><mn id='den'>2</mn></mfrac></math>",
-        "tag": "mfrac",
-        "ax_attrs": {"AXMathFractionNumerator": "num", "AXMathFractionDenominator": "den"}
+        "axapi": {"AXMathFractionNumerator": "num", "AXMathFractionDenominator": "den"}
     },
     "mi": {
         "html": "<math><mi id='test'>x</mi></math>",
-        "tag": "mi"
     },
     # mmultiscripts object reference tested in mathml-aam/aamtests/attribute/mmultiscripts.py
     "mmultiscripts": {
         "html": "<math><mmultiscripts id='test'><mi id='base'>X</mi><mn id='sub'>1</mn><mn id='sup'>2</mn><mprescripts/><mn id='presub'>3</mn><mn id='presup'>4</mn></mmultiscripts></math>",
-        "tag": "mmultiscripts"
     },
     "mn": {
         "html": "<math><mn id='test'>2</mn></math>",
-        "tag": "mn"
     },
     "mo": {
         "html": "<math><mo id='test'>+</mo></math>",
-        "tag": "mo"
     },
     "mover": {
         "html": "<math><mover id='test'><mi id='base'>a</mi><mo id='over'>˙</mo></mover></math>",
-        "tag": "mover",
-        "ax_attrs": {"AXMathBase": "base", "AXMathOver": "over"}
+        "axapi": {"AXMathBase": "base", "AXMathOver": "over"}
     },
     "mpadded": {
         "html": "<math><mpadded id='test' width='+10px'><mn>1</mn></mpadded></math>",
-        "tag": "mpadded"
     },
     "mphantom": {
         "html": "<math><mphantom style='visibility: visible;' id='test'><mn>1</mn></mphantom></math>",
-        "tag": "mphantom"
     },
     "mprescripts": {
         "html": "<math><mmultiscripts><mi>X</mi><mn>1</mn><mn>2</mn><mprescripts id='test'/><mn>3</mn><mn>4</mn></mmultiscripts></math>"
     },
     "mroot": {
         "html": "<math><mroot id='test'><mi id='rad'>x</mi><mn id='idx'>3</mn></mroot></math>",
-        "tag": "mroot",
-        "ax_attrs": {"AXMathRootRadicand": "rad", "AXMathRootIndex": "idx"}
+        "axapi": {"AXMathRootRadicand": "rad", "AXMathRootIndex": "idx"}
     },
     "mrow": {
         "html": "<math><mrow id='test'><mn>1</mn><mo>+</mo><mn>1</mn></mrow></math>",
-        "tag": "mrow"
     },
     "ms": {
         "html": "<math><ms id='test'>string</ms></math>",
-        "tag": "ms"
     },
     "mspace": {
         "html": "<math><mspace id='test' width='2em'/></math>"
     },
     "msqrt": {
         "html": "<math><msqrt id='test'><mi id='rad1'>x</mi></msqrt></math>",
-        "tag": "msqrt",
-        "ax_attrs": {"AXMathRootRadicand": "rad1"}
+        "axapi": {"AXMathRootRadicand": "rad1"}
     },
     "mstyle": {
         "html": "<math><mstyle id='test' mathcolor='blue'><mn>1</mn></mstyle></math>",
-        "tag": "mstyle"
     },
     "msub": {
         "html": "<math><msub id='test'><mi id='base'>x</mi><mn id='sub'>1</mn></msub></math>",
-        "tag": "msub",
-        "ax_attrs": {"AXMathBase": "base", "AXMathSubscript": "sub"}
+        "axapi": {"AXMathBase": "base", "AXMathSubscript": "sub"}
     },
     "msubsup": {
         "html": "<math><msubsup id='test'><mi id='base'>x</mi><mn id='sub'>1</mn><mn id='sup'>2</mn></msubsup></math>",
-        "tag": "msubsup",
-        "ax_attrs": {"AXMathBase": "base", "AXMathSubscript": "sub", "AXMathSuperscript": "sup"}
+        "axapi": {"AXMathBase": "base", "AXMathSubscript": "sub", "AXMathSuperscript": "sup"}
     },
     "msup": {
         "html": "<math><msup id='test'><mi id='base'>x</mi><mn id='sup'>2</mn></msup></math>",
-        "tag": "msup",
-        "ax_attrs": {"AXMathBase": "base", "AXMathSuperscript": "sup"}
+        "axapi": {"AXMathBase": "base", "AXMathSuperscript": "sup"}
     },
     "mtable": {
         "html": "<math><mtable id='test'><mtr><mtd><mn>1</mn></mtd></mtr></mtable></math>",
-        "tag": "mtable",
-        "interface": "AtkTable"
+        "atspi": {"interface": "AtkTable"}
     },
     "mtd": {
         "html": "<math><mtable><mtr><mtd id='test'><mn>1</mn></mtd></mtr></mtable></math>",
-        "tag": "mtd",
-        "interface": "AtkTableCell"
+        "atspi": {"interface": "AtkTableCell"}
     },
     "mtext": {
         "html": "<math><mtext id='test'>hello</mtext></math>",
-        "tag": "mtext"
     },
     "mtr": {
         "html": "<math><mtable><mtr id='test'><mtd><mn>1</mn></mtd></mtr></mtable></math>",
-        "tag": "mtr"
     },
     "munder": {
         "html": "<math><munder id='test'><mi id='base'>x</mi><mo id='under'>_</mo></munder></math>",
-        "tag": "munder",
-        "ax_attrs": {"AXMathBase": "base", "AXMathUnder": "under"}
+        "axapi": {"AXMathBase": "base", "AXMathUnder": "under"}
     },
     "munderover": {
         "html": "<math><munderover id='test'><mi id='base'>x</mi><mo id='under'>_</mo><mo id='over'>˙</mo></munderover></math>",
-        "tag": "munderover",
-        "ax_attrs": {"AXMathBase": "base", "AXMathUnder": "under", "AXMathOver": "over"}
+        "axapi": {"AXMathBase": "base", "AXMathUnder": "under", "AXMathOver": "over"}
     },
     "none": {
         "html": "<math><mmultiscripts><mi>X</mi><mn>1</mn><none id='test'/></mmultiscripts></math>"
     },
     "semantics": {
         "html": "<math><semantics id='test'><mn>5</mn><annotation encoding='TeX'>5</annotation></semantics></math>",
-        "tag": "semantics"
     }
 }
 
@@ -143,21 +115,23 @@ def test_atspi(atspi, session, inline, element_name):
     session.url = inline(data["html"])
     node = atspi.find_node("test", session.url)
 
-    if "tag" in data:
-        obj_attrs = atspi.Accessible.get_attributes(node)
-        assert "tag" in obj_attrs, f"<{element_name}> object attributes missing 'tag' key. Found: {obj_attrs}"
-        assert obj_attrs["tag"] == data["tag"], f"Expected tag '{data['tag']}', got '{obj_attrs['tag']}'"
+    obj_attrs = atspi.Accessible.get_attributes(node)
+    assert "tag" in obj_attrs, f"<{element_name}> object attributes missing 'tag' key. Found: {obj_attrs}"
+    assert obj_attrs["tag"] == element_name, f"Expected tag '{element_name}', got '{obj_attrs['tag']}'"
 
-    if "interface" in data:
-        if data["interface"] == "AtkTable":
+    atspi_config = data.get("atspi", {})
+    interface_name = atspi_config.get("interface")
+
+    if interface_name:
+        if interface_name == "AtkTable":
             table_iface = atspi.Accessible.get_table_iface(node)
-            assert table_iface is not None, "mtable node must implement AtkTable interface"
-        elif data["interface"] == "AtkTableCell":
+            assert table_iface is not None, f"{element_name} node must implement AtkTable interface"
+        elif interface_name == "AtkTableCell":
             cell_iface = atspi.Accessible.get_table_cell(node)
-            assert cell_iface is not None, "mtd node must implement AtkTableCell interface"
+            assert cell_iface is not None, f"{element_name} node must implement AtkTableCell interface"
 
 
-AX_ATTR_CASES = [name for name, d in TEST_DATA_ATTRIBUTES.items() if "ax_attrs" in d]
+AX_ATTR_CASES = [name for name, d in TEST_DATA_ATTRIBUTES.items() if "axapi" in d]
 
 @pytest.mark.parametrize("element_name", AX_ATTR_CASES)
 def test_axapi(axapi, session, inline, element_name):
@@ -165,7 +139,7 @@ def test_axapi(axapi, session, inline, element_name):
     session.url = inline(data["html"])
     node = axapi.find_node("test", session.url)
 
-    expected_relationships = data["ax_attrs"]
+    expected_relationships = data["axapi"]
     
     for ax_attr_key, expected_dom_id in expected_relationships.items():
         attr_res = axapi.AXUIElementCopyAttributeValue(node, ax_attr_key, None)
--- mathml-aam/aamtests/role/mathml-role.py
+++ mathml-aam/aamtests/role/mathml-role.py
@@ -1,192 +1,194 @@
 import pytest
 
 # Testing MathML Core AAM Mappings: https://w3c.github.io/mathml-aam/#mathml-element-mappings
+# FIXME: For role that is not mapped, TBD and nil, we match the chrome behavior for now
+# See https://github.com/w3c/mathml-aam/issues/41
 TEST_DATA = {
     "annotation": {
         "html": "<math><annotation id='test'>XML</annotation></math>",
-        "atk_role": "ATK_ROLE_STATIC",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXMathText" # Not mapped, match chrome
+        "atspi_role": "ROLE_STATIC",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXMathText" # Not mapped, match chrome
     },
     "annotation-xml": {
         "html": "<math><annotation-xml id='test'>XML</annotation-xml></math>",
-        "atk_role": "ATK_ROLE_SECTION",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXMathRow" # Not mapped, match chrome
+        "atspi_role": "ROLE_SECTION",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXMathRow" # Not mapped, match chrome
     },
     "maction": {
         "html": "<math><maction id='test' actiontype='toggle'><mi>A</mi></maction></math>",
-        "atk_role": "ATK_ROLE_SECTION",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXMathRow" # Not mapped, match chrome
+        "atspi_role": "ROLE_SECTION",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXMathRow" # Not mapped, match chrome
     },
     "math": {
         "html": "<math id='test'><mi>x</mi></math>",
-        "atk_role": "ATK_ROLE_MATH",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXDocumentMath"
+        "atspi_role": "ROLE_MATH",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXDocumentMath"
     },
     "merror": {
         "html": "<math><merror id='test'><mtext>error</mtext></merror></math>",
-        "atk_role": "ATK_ROLE_SECTION",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXMathRow"
+        "atspi_role": "ROLE_SECTION",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXMathRow"
     },
     "mfrac": {
         "html": "<math><mfrac id='test'><mi>a</mi><mi>b</mi></mfrac></math>",
-        "atk_role": "ATK_ROLE_MATH_FRACTION",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXMathFraction"
+        "atspi_role": "ROLE_MATH_FRACTION",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXMathFraction"
     },
     "mi": {
         "html": "<math><mi id='test'>x</mi></math>",
-        "atk_role": "ATK_ROLE_STATIC",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXMathIdentifier"
+        "atspi_role": "ROLE_STATIC",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXMathIdentifier"
     },
     "mmultiscripts": {
         "html": "<math><mmultiscripts id='test'><mi>X</mi></mmultiscripts></math>",
-        "atk_role": "ATK_ROLE_SECTION",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXMathMultiscript"
+        "atspi_role": "ROLE_SECTION",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXMathMultiscript"
     },
     "mn": {
         "html": "<math><mn id='test'>2</mn></math>",
-        "atk_role": "ATK_ROLE_STATIC",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXMathNumber"
+        "atspi_role": "ROLE_STATIC",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXMathNumber"
     },
     "mo": {
         "html": "<math><mo id='test'>+</mo></math>",
-        "atk_role": "ATK_ROLE_STATIC",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXMathOperator"
+        "atspi_role": "ROLE_STATIC",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXMathOperator"
     },
     "mover": {
         "html": "<math><mover id='test'><mi>a</mi><mo>˙</mo></mover></math>",
-        "atk_role": "ATK_ROLE_SECTION",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXMathUnderOver"
+        "atspi_role": "ROLE_SECTION",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXMathUnderOver"
     },
     "mpadded": {
         "html": "<math><mpadded id='test'><mi>x</mi></mpadded></math>",
-        "atk_role": "ATK_ROLE_SECTION",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXMathRow" # Not mapped, match chrome
+        "atspi_role": "ROLE_SECTION",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXMathRow" # Not mapped, match chrome
     },
     "mphantom": {
         "html": "<math><mphantom style='visibility: visible;' id='test'><mi>x</mi></mphantom></math>",
-        "atk_role": "ATK_ROLE_SECTION",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXMathRow"
+        "atspi_role": "ROLE_SECTION",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXMathRow"
     },
     "mprescripts": {
         "html": "<math><mmultiscripts><mi>X</mi><mprescripts id='test'/></mmultiscripts></math>",
-        "atk_role": "ATK_ROLE_SECTION",
-        "ax_role": "NSAccessibilityGroupRole", # Not mapped, match chrome 
-        "ax_subrole": "AXMathRow" # Not mapped, match chrome
+        "atspi_role": "ROLE_SECTION",
+        "axapi_role": "NSAccessibilityGroupRole", # Not mapped, match chrome 
+        "axapi_subrole": "AXMathRow" # Not mapped, match chrome
     },
     "mroot": {
         "html": "<math><mroot id='test'><mi>x</mi><mn>3</mn></mroot></math>",
-        "atk_role": "ATK_ROLE_MATH_ROOT",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXMathRoot"
+        "atspi_role": "ROLE_MATH_ROOT",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXMathRoot"
     },
     "mrow": {
         "html": "<math><mrow id='test'><mi>a</mi><mo>+</mo><mi>b</mi></mrow></math>",
-        "atk_role": "ATK_ROLE_SECTION",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXMathRow"
+        "atspi_role": "ROLE_SECTION",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXMathRow"
     },
     "ms": {
         "html": "<math><ms id='test'>string</ms></math>",
-        "atk_role": "ATK_ROLE_STATIC",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": None # nil
+        "atspi_role": "ROLE_STATIC",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": None # nil
     },
     "mspace": {
         "html": "<math><mspace id='test' width='1em'/></math>",
-        "atk_role": "ATK_ROLE_SECTION", # Not mapped, match chrome
-        "ax_role": "NSAccessibilityGroupRole",  # Not mapped, match chrome
-        "ax_subrole": "AXEmptyGroup" # Not mapped, match chrome
+        "atspi_role": "ROLE_SECTION", # Not mapped, match chrome
+        "axapi_role": "NSAccessibilityGroupRole",  # Not mapped, match chrome
+        "axapi_subrole": "AXEmptyGroup" # Not mapped, match chrome
     },
     "msqrt": {
         "html": "<math><msqrt id='test'><mi>x</mi></msqrt></math>",
-        "atk_role": "ATK_ROLE_MATH_ROOT",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXMathSquareRoot"
+        "atspi_role": "ROLE_MATH_ROOT",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXMathSquareRoot"
     },
     "mstyle": {
         "html": "<math><mstyle id='test' mathcolor='red'><mi>x</mi></mstyle></math>",
-        "atk_role": "ATK_ROLE_SECTION",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXMathRow"
+        "atspi_role": "ROLE_SECTION",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXMathRow"
     },
     "msub": {
         "html": "<math><msub id='test'><mi>x</mi><mn>1</mn></msub></math>",
-        "atk_role": "ATK_ROLE_SECTION",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXMathSubscriptSuperscript"
+        "atspi_role": "ROLE_SECTION",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXMathSubscriptSuperscript"
     },
     "msubsup": {
         "html": "<math><msubsup id='test'><mi>x</mi><mn>1</mn><mn>2</mn></msubsup></math>",
-        "atk_role": "ATK_ROLE_SECTION",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXMathSubscriptSuperscript"
+        "atspi_role": "ROLE_SECTION",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXMathSubscriptSuperscript"
     },
     "msup": {
         "html": "<math><msup id='test'><mi>x</mi><mn>2</mn></msup></math>",
-        "atk_role": "ATK_ROLE_SECTION",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXMathSubscriptSuperscript"
+        "atspi_role": "ROLE_SECTION",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXMathSubscriptSuperscript"
     },
     "mtable": {
         "html": "<math><mtable id='test'><mtr><mtd><mn>1</mn></mtd></mtr></mtable></math>",
-        "atk_role": "ATK_ROLE_TABLE",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXMathTable"
+        "atspi_role": "ROLE_TABLE",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXMathTable"
     },
     "mtd": {
         "html": "<math><mtable><mtr><mtd id='test'><mn>1</mn></mtd></mtr></mtable></math>",
-        "atk_role": "ATK_ROLE_TABLE_CELL",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXMathTableCell"
+        "atspi_role": "ROLE_TABLE_CELL",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXMathTableCell"
     },
     "mtext": {
         "html": "<math><mtext id='test'>text</mtext></math>",
-        "atk_role": "ATK_ROLE_STATIC",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXMathText"
+        "atspi_role": "ROLE_STATIC",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXMathText"
     },
     "mtr": {
         "html": "<math><mtable><mtr id='test'><mtd><mn>1</mn></mtd></mtr></mtable></math>",
-        "atk_role": "ATK_ROLE_TABLE_ROW",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXMathTableRow"
+        "atspi_role": "ROLE_TABLE_ROW",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXMathTableRow"
     },
     "munder": {
         "html": "<math><munder id='test'><mi>x</mi><mo>_</mo></munder></math>",
-        "atk_role": "ATK_ROLE_SECTION",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXMathUnderOver"
+        "atspi_role": "ROLE_SECTION",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXMathUnderOver"
     },
     "munderover": {
         "html": "<math><munderover id='test'><mi>x</mi><mo>_</mo><mo>˙</mo></munderover></math>",
-        "atk_role": "ATK_ROLE_SECTION",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXMathUnderOver"
+        "atspi_role": "ROLE_SECTION",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXMathUnderOver"
     },
     "none": {
         "html": "<math><mmultiscripts><mi>X</mi><none id='test'/></mmultiscripts></math>",
-        "atk_role": "ATK_ROLE_SECTION",
-        "ax_role": "NSAccessibilityGroupRole", # Not mapped, match chrome
-        "ax_subrole": "AXMathRow" # Not mapped, match chrome
+        "atspi_role": "ROLE_SECTION",
+        "axapi_role": "NSAccessibilityGroupRole", # Not mapped, match chrome
+        "axapi_subrole": "AXMathRow" # Not mapped, match chrome
     },
     "semantics": {
         "html": "<math><semantics id='test'><mi>x</mi></semantics></math>",
-        "atk_role": "ATK_ROLE_SECTION",
-        "ax_role": "NSAccessibilityGroupRole",
-        "ax_subrole": "AXMathRow" # Not mapped, match chrome
+        "atspi_role": "ROLE_SECTION",
+        "axapi_role": "NSAccessibilityGroupRole",
+        "axapi_subrole": "AXMathRow" # Not mapped, match chrome
     }
 }
 
@@ -199,7 +201,7 @@ def test_atspi(atspi, session, inline, element_name):
 
     actual_role = atspi.Accessible.get_role(node)
 
-    role_attr_name = data["atk_role"].replace("ATK_ROLE_", "")
+    role_attr_name = data["atspi_role"].replace("ROLE_", "")
     expected_role = getattr(atspi.Role, role_attr_name)
 
     assert actual_role == expected_role, f"Expected {expected_role}, but got {actual_role}"
@@ -212,15 +214,15 @@ def test_axapi(axapi, session, inline, element_name):
     session.url = inline(data["html"])
     node = axapi.find_node("test", session.url)
 
-    expected_role = data["ax_role"]
+    expected_role = data["axapi_role"]
     if expected_role == "NSAccessibilityGroupRole":
         expected_role = "AXGroup"
 
     role_result = axapi.AXUIElementCopyAttributeValue(node, "AXRole", None)
     actual_role = role_result[1] if (role_result and len(role_result) > 1) else None
-    assert actual_role == expected_role, f"AXRole mismatch for <{element_name}>. Expected '{data['ax_role']}', got '{actual_role}'"
+    assert actual_role == expected_role, f"AXRole mismatch for <{element_name}>. Expected '{data['axapi_role']}', got '{actual_role}'"
 
     subrole_result = axapi.AXUIElementCopyAttributeValue(node, "AXSubrole", None)
     actual_subrole = subrole_result[1] if (subrole_result and len(subrole_result) > 1) else None
   
-    assert actual_subrole == data["ax_subrole"], f"AXSubrole mismatch for <{element_name}>. Expected '{data['ax_subrole']}', got '{actual_subrole}'"
+    assert actual_subrole == data["axapi_subrole"], f"AXSubrole mismatch for <{element_name}>. Expected '{data['axapi_subrole']}', got '{actual_subrole}'"

857d3de076071af3e5c2b694b8fc1c699afc480a
Filip Skokan <panva.ip@gmail.com>
2026-07-19 00:00:57 +0200

    [WebCryptoAPI] Share RSA key fixtures

--- WebCryptoAPI/sign_verify/rsa_pss.https.any.js
+++ WebCryptoAPI/sign_verify/rsa_pss.https.any.js
@@ -1,5 +1,6 @@
 // META: title=WebCryptoAPI: sign() and verify() Using RSA-PSS
 // META: script=../util/helpers.js
+// META: script=../util/rsa_key_fixtures.js
 // META: script=rsa_pss_vectors.js
 // META: script=rsa.js
 // META: timeout=long

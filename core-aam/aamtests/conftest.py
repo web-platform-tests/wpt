0c573db7b5e8f6a1ae9f3fe8b34760d102fdfca9
Manuel Rego Casasnovas <rego@igalia.com>
2026-08-14 19:06:41 +0200

    Remove unneeded plugins from core-aam/aamtests/conftest.py
    Issue: #59157

--- core-aam/aamtests/conftest.py
+++ core-aam/aamtests/conftest.py
@@ -1,14 +1,5 @@
-import sys
-from pathlib import Path
-
-# Add webdriver to the Python path so we can import the fixtures
-webdriver_tests_path = Path(__file__).parent.parent.parent / "webdriver"
-sys.path.insert(0, str(webdriver_tests_path))
-
 pytest_plugins = (
+    "aamtests.support.fixtures_a11y_api",
     "pytest_fixtures.webdriver",
     "pytest_fixtures.webdriver_classic",
-    "tests.support.fixtures",
-    "tests.support.classic.fixtures",
-    "aamtests.support.fixtures_a11y_api",
 )

17e5d97c0984a20e69d914136e97520d7de1ee6a
James Graham <james@hoppipolla.co.uk>
2026-09-08 11:42:09 +0100

    Shut down processes cleanly when running via run-tc
    This allows them to clean up and makes it much easier to reuse the docker
    container afterwards.

--- tools/ci/run_tc.py
+++ tools/ci/run_tc.py
@@ -449,7 +449,11 @@ def run_tc(*args, **kwargs):
         sys.exit(subprocess.call(cmd))
     finally:
         for process in started_processes:
-            process.kill()
+            process.terminate()
+            try:
+                process.wait(30)
+            except subprocess.TimeoutExpired:
+                process.kill()
 
 
 def main():

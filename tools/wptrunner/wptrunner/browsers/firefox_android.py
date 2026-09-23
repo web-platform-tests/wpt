b858a84a107fc6b1506b5f951347e180a36dbda8
Henrik Skupin <mail@hskupin.info>
2026-09-23 22:33:13 +0000

    [wpt] Use the MOZ_REMOTE_ALLOW_SYSTEM_ACCESS environment variable to enable system access.
    Differential Revision: https://phabricator.services.mozilla.com/D328411
    
    bugzilla-url: https://bugzilla.mozilla.org/show_bug.cgi?id=2054896
    gecko-commit: 1c7fb73211a3174fccef1296e94bd9764c4ed7a2
    gecko-commit-git: 23b35fd480f4d0615c55da188bcfce5ac3840f9b
    gecko-reviewers: webdriver-reviewers, jgraham, jdescottes

--- tools/wptrunner/wptrunner/browsers/firefox_android.py
+++ tools/wptrunner/wptrunner/browsers/firefox_android.py
@@ -313,13 +313,17 @@ class FirefoxAndroidBrowser(Browser):
         self.leak_report_file = None
 
         args = self.binary_args[:] if self.binary_args else []
-        args += [cmd_arg("marionette"),
-                 cmd_arg("remote-allow-system-access"), "about:blank"]
+        args += [cmd_arg("marionette"), "about:blank"]
 
         debug_args, cmd = browser_command(
             self.package_name, args, self.debug_info)
 
         env = get_environ(self.chaos_mode_flags, self.env_extras)
+        # Allow Marionette to execute commands in the chrome scope of the
+        # application. Not set in get_environ() because for wdspec tests the
+        # environment is forwarded to geckodriver via capabilities, which
+        # rejects this variable.
+        env["MOZ_REMOTE_ALLOW_SYSTEM_ACCESS"] = "1"
 
         self.runner = FennecEmulatorRunner(app=self.package_name,
                                            profile=self.profile,
